7023c0599c5a550c5591d5a440beb1964f221d52
Trustin Lee <trustin@gmail.com>
2009-03-12 09:21:45 +0000

    A small rant

--- src/main/java/org/jboss/netty/buffer/CompositeChannelBuffer.java
+++ src/main/java/org/jboss/netty/buffer/CompositeChannelBuffer.java
@@ -220,6 +220,7 @@ public class CompositeChannelBuffer extends AbstractChannelBuffer {
             throws IOException {
         // XXX Gathering write is not supported because of a known issue.
         //     See http://bugs.sun.com/view_bug.do?bug_id=6210541
+        //     This issue appeared in 2004 and is still unresolved!?
         return out.write(toByteBuffer(index, length));
     }
 
