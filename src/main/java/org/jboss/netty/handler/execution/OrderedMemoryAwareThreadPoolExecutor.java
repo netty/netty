1a54f32a770a480049dc75fa687ab010739a3177
Trustin Lee <trustin@gmail.com>
2009-07-21 10:27:39 +0000

    Less indentation

--- src/main/java/org/jboss/netty/handler/execution/OrderedMemoryAwareThreadPoolExecutor.java
+++ src/main/java/org/jboss/netty/handler/execution/OrderedMemoryAwareThreadPoolExecutor.java
@@ -111,13 +111,13 @@ import org.jboss.netty.util.internal.ConcurrentIdentityWeakKeyHashMap;
  *
  * on every 3 seconds:
  *
- *     for (Iterator&lt;Object&gt; i = executor.getChildExecutorKeySet().iterator; i.hasNext();) {
- *         InetAddress ip = (InetAddress) i.next();
- *         if (there is no active connection from 'ip' now &&
- *             there has been no incoming connection from 'ip' for last 10 minutes) {
- *             i.remove();
- *         }
- *     }
+ *   for (Iterator&lt;Object&gt; i = executor.getChildExecutorKeySet().iterator; i.hasNext();) {
+ *       InetAddress ip = (InetAddress) i.next();
+ *       if (there is no active connection from 'ip' now &&
+ *           there has been no incoming connection from 'ip' for last 10 minutes) {
+ *           i.remove();
+ *       }
+ *   }
  * </pre>
  *
  * @author The Netty Project (netty-dev@lists.jboss.org)
