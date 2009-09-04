65e7a351bd87e14415c4135c399c5645df17f6b1
Trustin Lee <trustin@gmail.com>
2009-09-04 03:03:47 +0000

    More explanation

--- src/main/java/org/jboss/netty/example/echo/EchoClient.java
+++ src/main/java/org/jboss/netty/example/echo/EchoClient.java
@@ -24,7 +24,9 @@ import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
 
 /**
  * Sends one message when a connection is open and echoes back any received
- * data to the server.
+ * data to the server.  Simply put, the echo client initiates the ping-pong
+ * traffic between the echo client and server by sending the first message to
+ * the server.
  *
  * @author The Netty Project (netty-dev@lists.jboss.org)
  * @author Trustin Lee (tlee@redhat.com)
