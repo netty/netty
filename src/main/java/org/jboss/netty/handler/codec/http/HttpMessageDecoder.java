b67688fd0f7e2281af16c8d613c2708e7a992b99
Trustin Lee <trustin@gmail.com>
2009-09-07 03:27:26 +0000

    Fixed documentation error

--- src/main/java/org/jboss/netty/handler/codec/http/HttpMessageDecoder.java
+++ src/main/java/org/jboss/netty/handler/codec/http/HttpMessageDecoder.java
@@ -112,8 +112,8 @@ public abstract class HttpMessageDecoder extends ReplayingDecoder<HttpMessageDec
 
     /**
      * Creates a new instance with the default
-     * {@code maxInitialLineLength (4096}}, {@code maxHeaderSize (4096)}, and
-     * {@code maxChunkSize (4096)}.
+     * {@code maxInitialLineLength (4096}}, {@code maxHeaderSize (8192)}, and
+     * {@code maxChunkSize (8192)}.
      */
     protected HttpMessageDecoder() {
         this(4096, 8192, 8192);
