8eba49c0cd8393855cefe5264834ae687990833c
Trustin Lee <trustin@gmail.com>
2009-10-14 06:27:53 +0000

    Resolved issue: NETTY-203 Encoding frames as Google Protocol Buffer base 128 varints (32-bit) * Applied Tomasz's patch * Renamed ProtobufVariant32FieldPrepender to ProtobufVariant32LengthFieldPrepender * Code cleanup

---
+++ src/main/java/org/jboss/netty/handler/codec/protobuf/ProtobufVarint32FrameDecoder.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2009 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jboss.netty.handler.codec.protobuf;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.handler.codec.frame.FrameDecoder;
+
+import com.google.protobuf.CodedInputStream;
+
+/**
+ * A decoder that splits the received {@link ChannelBuffer}s dynamically by the
+ * value of the length field in the message. {@link ProtobufVarint32FrameDecoder}
+ * should be used to decode a binary message which has an integer header field
+ * encoded as Google Protocol Buffer <a href="http://code.google.com/apis/protocolbuffers/docs/encoding.html#varints">Base
+ * 128 Varints</a> (32-bit) integer that represents the length of the message
+ * body.
+ *
+ * @see com.google.protobuf.CodedInputStream
+ *
+ * @author The Netty Project (netty-dev@lists.jboss.org)
+ * @author Tomasz Blachowicz (tblachowicz@gmail.com)
+ *
+ * @version $Rev$, $Date$
+ */
+public class ProtobufVarint32FrameDecoder extends FrameDecoder {
+
+    @Override
+    protected Object decode(ChannelHandlerContext ctx, Channel channel, ChannelBuffer buffer) throws Exception {
+        buffer.markReaderIndex();
+        byte[] buf = new byte[5];
+        for (int i = 0; i < 5; i ++) {
+            if (!buffer.readable()) {
+                break;
+            }
+
+            buf[i] = buffer.readByte();
+            if (buf[i] >= 0) {
+                int messageSize = CodedInputStream.newInstance(buf, 0, i + 1).readRawVarint32();
+                if (buffer.readableBytes() < messageSize) {
+                    break;
+                }
+
+                return buffer.readBytes(messageSize);
+            }
+        }
+
+        buffer.resetReaderIndex();
+        return null;
+    }
+}
---
+++ src/main/java/org/jboss/netty/handler/codec/protobuf/ProtobufVarint32LengthFieldPrepender.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2009 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jboss.netty.handler.codec.protobuf;
+
+import static org.jboss.netty.buffer.ChannelBuffers.*;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBufferOutputStream;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelPipelineCoverage;
+import org.jboss.netty.handler.codec.oneone.OneToOneEncoder;
+
+import com.google.protobuf.CodedOutputStream;
+
+/**
+ * An encoder that prepends the length of the message.  The length value is
+ * prepended as a binary form. encoded as Google Protocol Buffer
+ * <a href="http://code.google.com/apis/protocolbuffers/docs/encoding.html#varints">Base
+ * 128 Varints</a> (32-bit).
+ *
+ * @see com.google.protobuf.CodedOutputStream
+ *
+ * @author The Netty Project (netty-dev@lists.jboss.org)
+ * @author Tomasz Blachowicz (tblachowicz@gmail.com)
+ * @version $Rev$, $Date$
+ */
+@ChannelPipelineCoverage("all")
+public class ProtobufVarint32LengthFieldPrepender extends OneToOneEncoder {
+
+    @Override
+    protected Object encode(ChannelHandlerContext ctx, Channel channel,
+            Object msg) throws Exception {
+        ChannelBuffer body = (ChannelBuffer) msg;
+        int length = body.readableBytes();
+        ChannelBuffer header =
+            channel.getConfig().getBufferFactory().getBuffer(
+                    CodedOutputStream.computeRawVarint32Size(length));
+        CodedOutputStream codedOutputStream = CodedOutputStream
+                .newInstance(new ChannelBufferOutputStream(header));
+        codedOutputStream.writeRawVarint32(length);
+        codedOutputStream.flush();
+        return wrappedBuffer(header, body);
+    }
+
+}
