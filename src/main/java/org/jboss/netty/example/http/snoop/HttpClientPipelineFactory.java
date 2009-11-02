b105461383f761dd48a2aaf8b111c629c24d8ef4
Trustin Lee <trustin@gmail.com>
2009-11-02 09:21:13 +0000

    Split HttpDecompressionHandler into HttpContentDecoder and HttpContentDecompressor

--- src/main/java/org/jboss/netty/example/http/snoop/HttpClientPipelineFactory.java
+++ src/main/java/org/jboss/netty/example/http/snoop/HttpClientPipelineFactory.java
@@ -19,7 +19,7 @@ import static org.jboss.netty.channel.Channels.*;
 
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
-import org.jboss.netty.handler.codec.http.HttpDecompressionHandler;
+import org.jboss.netty.handler.codec.http.HttpContentDecompressor;
 import org.jboss.netty.handler.codec.http.HttpRequestEncoder;
 import org.jboss.netty.handler.codec.http.HttpResponseDecoder;
 
@@ -36,7 +36,7 @@ public class HttpClientPipelineFactory implements ChannelPipelineFactory {
         ChannelPipeline pipeline = pipeline();
         pipeline.addLast("decoder", new HttpResponseDecoder());
         // Remove the following line if you don't want automatic content decompression.
-        pipeline.addLast("inflater", new HttpDecompressionHandler());
+        pipeline.addLast("inflater", new HttpContentDecompressor());
         // Uncomment the following line if you don't want to handle HttpChunks.
         //pipeline.addLast("aggregator", new HttpChunkAggregator(1048576));
         pipeline.addLast("encoder", new HttpRequestEncoder());
