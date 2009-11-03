e0132d58858a9244185a33caec6fc8f39ee41af9
Trustin Lee <trustin@gmail.com>
2009-11-03 06:48:39 +0000

    Updated the HTTP snoop example to fully utilize the compression handlers

--- src/main/java/org/jboss/netty/example/http/snoop/HttpServerPipelineFactory.java
+++ src/main/java/org/jboss/netty/example/http/snoop/HttpServerPipelineFactory.java
@@ -19,6 +19,7 @@ import static org.jboss.netty.channel.Channels.*;
 
 import org.jboss.netty.channel.ChannelPipeline;
 import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.handler.codec.http.HttpContentCompressor;
 import org.jboss.netty.handler.codec.http.HttpRequestDecoder;
 import org.jboss.netty.handler.codec.http.HttpResponseEncoder;
 
@@ -43,6 +44,8 @@ public class HttpServerPipelineFactory implements ChannelPipelineFactory {
         // Uncomment the following line if you don't want to handle HttpChunks.
         //pipeline.addLast("aggregator", new HttpChunkAggregator(1048576));
         pipeline.addLast("encoder", new HttpResponseEncoder());
+        // Remove the following line if you don't want automatic content compression.
+        pipeline.addLast("deflater", new HttpContentCompressor());
         pipeline.addLast("handler", new HttpRequestHandler());
         return pipeline;
     }
