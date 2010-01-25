e8f3c3534c5ef009c8863cee7674df0aa604a913
Trustin Lee <trustin@gmail.com>
2010-01-25 11:50:08 +0000

    Some warning message to confirm that certificate authentication really took place

--- src/main/java/org/jboss/netty/example/securechat/SecureChatTrustManagerFactory.java
+++ src/main/java/org/jboss/netty/example/securechat/SecureChatTrustManagerFactory.java
@@ -43,15 +43,20 @@ public class SecureChatTrustManagerFactory extends TrustManagerFactorySpi {
         }
 
         public void checkClientTrusted(
-                X509Certificate[] arg0, String arg1) throws CertificateException {
+                X509Certificate[] chain, String authType) throws CertificateException {
             // Always trust - it is an example.
             // You should do something in the real world.
+            // You will reach here only if you enabled client certificate auth.
+            System.err.println(
+                    "UNKNOWN CLIENT CERTIFICATE: " + chain[0].getSubjectDN());
         }
 
         public void checkServerTrusted(
-                X509Certificate[] arg0, String arg1) throws CertificateException {
+                X509Certificate[] chain, String authType) throws CertificateException {
             // Always trust - it is an example.
             // You should do something in the real world.
+            System.err.println(
+                    "UNKNOWN SERVER CERTIFICATE: " + chain[0].getSubjectDN());
         }
     };
 
