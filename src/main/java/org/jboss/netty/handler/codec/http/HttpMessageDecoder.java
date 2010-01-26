4ac906d41700768cce166b2f5baf3d1d8fff8968
Trustin Lee <trustin@gmail.com>
2010-01-26 09:55:22 +0000

    enum -> static enum

--- src/main/java/org/jboss/netty/handler/codec/http/HttpMessageDecoder.java
+++ src/main/java/org/jboss/netty/handler/codec/http/HttpMessageDecoder.java
@@ -124,7 +124,7 @@ public abstract class HttpMessageDecoder extends ReplayingDecoder<HttpMessageDec
      *
      * @apiviz.exclude
      */
-    protected enum State {
+    protected static enum State {
         SKIP_CONTROL_CHARS,
         READ_INITIAL,
         READ_HEADER,
