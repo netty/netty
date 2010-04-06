cac77a47b265e4fd2e39fc469d0abb6d32704b72
Trustin Lee <trustin@gmail.com>
2010-04-06 09:40:35 +0000

    Fixed a wrong usage of ExecutionHandler in the example

--- src/main/java/org/jboss/netty/example/local/LocalServerPipelineFactory.java
+++ src/main/java/org/jboss/netty/example/local/LocalServerPipelineFactory.java
@@ -37,17 +37,17 @@ import org.jboss.netty.handler.execution.ExecutionHandler;
  */
 public class LocalServerPipelineFactory implements ChannelPipelineFactory {
 
-    private final Executor eventExecutor;
+    private final ExecutionHandler executionHandler;
 
     public LocalServerPipelineFactory(Executor eventExecutor) {
-        this.eventExecutor = eventExecutor;
+        executionHandler = new ExecutionHandler(eventExecutor);
     }
 
     public ChannelPipeline getPipeline() throws Exception {
         final ChannelPipeline pipeline = Channels.pipeline();
         pipeline.addLast("decoder", new StringDecoder());
         pipeline.addLast("encoder", new StringEncoder());
-        pipeline.addLast("executor", new ExecutionHandler(eventExecutor));
+        pipeline.addLast("executor", executionHandler);
         pipeline.addLast("handler", new EchoCloseServerHandler());
         return pipeline;
     }
