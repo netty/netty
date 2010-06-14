cbe26db0e46c8f6df28abb8319f777f0a02af1a1
Trustin Lee <trustin@gmail.com>
2010-06-14 11:07:44 +0000

    Replaced email with homepage url

--- src/test/java/org/jboss/netty/handler/codec/http/QueryStringDecoderTest.java
+++ src/test/java/org/jboss/netty/handler/codec/http/QueryStringDecoderTest.java
@@ -22,7 +22,7 @@ import org.junit.Test;
 /**
  * @author <a href="http://www.jboss.org/netty/">The Netty Project</a>
  * @author <a href="http://gleamynode.net/">Trustin Lee</a>
- * @author Benoit Sigoure (tsunanet@gmail.com)
+ * @author <a href="http://tsunanet.net/">Benoit Sigoure</a>
  * @version $Rev$, $Date$
  */
 public class QueryStringDecoderTest {
