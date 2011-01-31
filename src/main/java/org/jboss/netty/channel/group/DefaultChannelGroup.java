8eb2d8eb43777acde58423e6341ca6a9edd2c649
Trustin Lee <trustin@gmail.com>
2011-01-31 15:25:22 +0900

    ChannelGroup.write() should not write to a server channel

--- src/main/java/org/jboss/netty/channel/group/DefaultChannelGroup.java
+++ src/main/java/org/jboss/netty/channel/group/DefaultChannelGroup.java
@@ -262,11 +262,11 @@ public class DefaultChannelGroup extends AbstractSet<Channel> implements Channel
             new LinkedHashMap<Integer, ChannelFuture>(size());
         if (message instanceof ChannelBuffer) {
             ChannelBuffer buf = (ChannelBuffer) message;
-            for (Channel c: this) {
+            for (Channel c: nonServerChannels.values()) {
                 futures.put(c.getId(), c.write(buf.duplicate()));
             }
         } else {
-            for (Channel c: this) {
+            for (Channel c: nonServerChannels.values()) {
                 futures.put(c.getId(), c.write(message));
             }
         }
@@ -279,11 +279,11 @@ public class DefaultChannelGroup extends AbstractSet<Channel> implements Channel
             new LinkedHashMap<Integer, ChannelFuture>(size());
         if (message instanceof ChannelBuffer) {
             ChannelBuffer buf = (ChannelBuffer) message;
-            for (Channel c: this) {
+            for (Channel c: nonServerChannels.values()) {
                 futures.put(c.getId(), c.write(buf.duplicate(), remoteAddress));
             }
         } else {
-            for (Channel c: this) {
+            for (Channel c: nonServerChannels.values()) {
                 futures.put(c.getId(), c.write(message, remoteAddress));
             }
         }
