887ca0c688d41eeb9a50241dea578f71bb587b62
iainmcgin <iainmcgin@gmail.com>
2011-04-08 14:31:51 +0100

    fixing formatting for NioSocketChannelConfig, which I had incorrectly merged earlier.

--- src/main/java/org/jboss/netty/channel/socket/nio/NioSocketChannelConfig.java
+++ src/main/java/org/jboss/netty/channel/socket/nio/NioSocketChannelConfig.java
@@ -59,36 +59,36 @@ import org.jboss.netty.channel.socket.SocketChannelConfig;
  */
 public interface NioSocketChannelConfig extends SocketChannelConfig {
 
-   /**
-    * Returns the high water mark of the write buffer.  If the number of bytes
-    * queued in the write buffer exceeds this value, {@link Channel#isWritable()}
-    * will start to return {@code false}.
-    */
+    /**
+     * Returns the high water mark of the write buffer.  If the number of bytes
+     * queued in the write buffer exceeds this value, {@link Channel#isWritable()}
+     * will start to return {@code false}.
+     */
     int getWriteBufferHighWaterMark();
 
-   /**
-    * Sets the high water mark of the write buffer.  If the number of bytes
-    * queued in the write buffer exceeds this value, {@link Channel#isWritable()}
-    * will start to return {@code false}.
-    */
+    /**
+     * Sets the high water mark of the write buffer.  If the number of bytes
+     * queued in the write buffer exceeds this value, {@link Channel#isWritable()}
+     * will start to return {@code false}.
+     */
     void setWriteBufferHighWaterMark(int writeBufferHighWaterMark);
 
-   /**
-    * Returns the low water mark of the write buffer.  Once the number of bytes
-    * queued in the write buffer exceeded the
-    * {@linkplain #setWriteBufferHighWaterMark(int) high water mark} and then
-    * dropped down below this value, {@link Channel#isWritable()} will return
-    * {@code true} again.
-    */
+    /**
+     * Returns the low water mark of the write buffer.  Once the number of bytes
+     * queued in the write buffer exceeded the
+     * {@linkplain #setWriteBufferHighWaterMark(int) high water mark} and then
+     * dropped down below this value, {@link Channel#isWritable()} will return
+     * {@code true} again.
+     */
     int getWriteBufferLowWaterMark();
 
-   /**
-    * Sets the low water mark of the write buffer.  Once the number of bytes
-    * queued in the write buffer exceeded the
-    * {@linkplain #setWriteBufferHighWaterMark(int) high water mark} and then
-    * dropped down below this value, {@link Channel#isWritable()} will return
-    * {@code true} again.
-    */
+    /**
+     * Sets the low water mark of the write buffer.  Once the number of bytes
+     * queued in the write buffer exceeded the
+     * {@linkplain #setWriteBufferHighWaterMark(int) high water mark} and then
+     * dropped down below this value, {@link Channel#isWritable()} will return
+     * {@code true} again.
+     */
     void setWriteBufferLowWaterMark(int writeBufferLowWaterMark);
 
     /**
@@ -144,5 +144,6 @@ public interface NioSocketChannelConfig extends SocketChannelConfig {
      * will be called with the new predictor.  The default factory is
      * <tt>{@link AdaptiveReceiveBufferSizePredictorFactory}(64, 1024, 65536)</tt>.
      */
-    void setReceiveBufferSizePredictorFactory(ReceiveBufferSizePredictorFactory predictorFactory);
+    void setReceiveBufferSizePredictorFactory(
+            ReceiveBufferSizePredictorFactory predictorFactory);
 }
