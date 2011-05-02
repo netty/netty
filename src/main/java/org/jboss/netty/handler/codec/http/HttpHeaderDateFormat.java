658e5070edfd570eebf0c9ced1f82bfcd0b12a41
rogiel <rogiel@Rogiel-PC>
2011-05-02 12:48:45 -0300

    HttpHeaderDateFormat fixes and test case
    Change-Id: I917d70383c71247fd8376c15b361664d3db6436d

--- src/main/java/org/jboss/netty/handler/codec/http/HttpHeaderDateFormat.java
+++ src/main/java/org/jboss/netty/handler/codec/http/HttpHeaderDateFormat.java
@@ -15,11 +15,23 @@
  */
 package org.jboss.netty.handler.codec.http;
 
+import java.text.DateFormat;
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
 /**
+ * This DateFormat decodes 3 formats of {@link Date}, but only encodes the one,
+ * the first:
+ * <ul>
+ * <li>Sun, 06 Nov 1994 08:49:37 GMT: standard specification, the only one with
+ * valid generation</li>
+ * <li>Sun, 06 Nov 1994 08:49:37 GMT: obsolete specification</li>
+ * <li>Sun Nov 6 08:49:37 1994: obsolete specification</li>
+ * </ul>
+ * 
  * @author <a href="http://www.jboss.org/netty/">The Netty Project</a>
  * @author <a href="http://gleamynode.net/">Trustin Lee</a>
  * @author <a href="http://www.rogiel.com/">Rogiel Josias Sulzbach</a>
@@ -27,14 +39,58 @@ import java.util.TimeZone;
  */
 final class HttpHeaderDateFormat extends SimpleDateFormat {
 	private static final long serialVersionUID = -925286159755905325L;
-
-	/*
-	 * Official documentations says that the "E, d-MMM-y HH:mm:ss z" format is
-	 * now obsolete. "E, d MMM yyyy HH:mm:ss z" should be used.
+	/**
+	 * An shared instance of this {@link DateFormat} to avoid unnecessary GC on
+	 * this stateless class.
 	 */
+	public static final HttpHeaderDateFormat SHARED_INSTANCE = new HttpHeaderDateFormat();
 
+	private final SimpleDateFormat format1 = new HttpHeaderDateFormatObsolete1();
+	private final SimpleDateFormat format2 = new HttpHeaderDateFormatObsolete2();
+
+	/**
+	 * Standard date format<p>
+	 * Sun, 06 Nov 1994 08:49:37 GMT -> E, d MMM yyyy HH:mm:ss z
+	 */
 	HttpHeaderDateFormat() {
-		super("E, d MMM yyyy HH:mm:ss z", Locale.ENGLISH);
+		super("E, dd MMM yyyy HH:mm:ss z", Locale.ENGLISH);
 		setTimeZone(TimeZone.getTimeZone("GMT"));
+	}
+
+	@Override
+	public Date parse(String text, ParsePosition pos) {
+		Date date = super.parse(text, pos);
+		if (date == null)
+			date = format1.parse(text, pos);
+		if (date == null)
+			date = format2.parse(text, pos);
+		return date;
+	}
+
+	/**
+	 * First obsolete format<p>
+	 * Sunday, 06-Nov-94 08:49:37 GMT -> E, d-MMM-y HH:mm:ss z
+	 */
+	private final class HttpHeaderDateFormatObsolete1 extends SimpleDateFormat {
+		private static final long serialVersionUID = -3178072504225114298L;
+
+		HttpHeaderDateFormatObsolete1() {
+			super("E, dd-MMM-y HH:mm:ss z", Locale.ENGLISH);
+			setTimeZone(TimeZone.getTimeZone("GMT"));
+		}
+	}
+
+	/**
+	 * Second obsolete format
+	 * <p>
+	 * Sun Nov 6 08:49:37 1994 -> EEE, MMM d HH:mm:ss yyyy
+	 */
+	private final class HttpHeaderDateFormatObsolete2 extends SimpleDateFormat {
+		private static final long serialVersionUID = 3010674519968303714L;
+
+		HttpHeaderDateFormatObsolete2() {
+			super("E MMM d HH:mm:ss yyyy", Locale.ENGLISH);
+			setTimeZone(TimeZone.getTimeZone("GMT"));
+		}
 	}
 }
