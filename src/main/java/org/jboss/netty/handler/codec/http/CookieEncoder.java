1ba786b7448efba306b5e3a22021b719207294b7
Nibin Varghese <nibin012@gmail.com>
2011-07-09 01:37:11 -0700

    If there are no cookie's set, then calling encode() will result in throwing "java.lang.StringIndexOutOfBoundsException" as it sets the length to -1
    This is a minor fix to check whether string builder length is > 0. If yes, then set the length for string builder

--- src/main/java/org/jboss/netty/handler/codec/http/CookieEncoder.java
+++ src/main/java/org/jboss/netty/handler/codec/http/CookieEncoder.java
@@ -204,8 +204,9 @@ public class CookieEncoder {
                 }
             }
         }
-
-        sb.setLength(sb.length() - 1);
+        if (sb.length() > 0) {
+            sb.setLength(sb.length() - 1);
+        }
         return sb.toString();
     }
 
