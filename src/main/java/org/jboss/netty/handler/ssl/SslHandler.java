caf8b391721abd199527fc884595c6a64b37f297
Trustin Lee <trustin@gmail.com>
2011-08-02 07:19:57 +0900

    Fixed a bug where the future associated with the close request is not notified when the channel was closed already when SslHandler attempts to send a closure_notify.
    This shouldn't be a problem to most applications because they usually do not specify their own future object for the close request.

--- src/main/java/org/jboss/netty/handler/ssl/SslHandler.java
+++ src/main/java/org/jboss/netty/handler/ssl/SslHandler.java
@@ -1084,6 +1084,8 @@ public class SslHandler extends FrameDecoder
         public void operationComplete(ChannelFuture closeNotifyFuture) throws Exception {
             if (!(closeNotifyFuture.getCause() instanceof ClosedChannelException)) {
                 Channels.close(context, e.getFuture());
+            } else {
+                e.getFuture().setSuccess();
             }
         }
     }
