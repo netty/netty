4b9b421af25b618af0ffbbe68c5289443b9ef015
Trustin Lee <trustin@gmail.com>
2011-10-21 15:36:06 -0700

    NETTY-439 Call channelBound/Connected from worker
    * Move channelBound() to RegisterTask
    * Simplify the if block in RegisterTask

--- src/main/java/org/jboss/netty/channel/socket/nio/NioAcceptedSocketChannel.java
+++ src/main/java/org/jboss/netty/channel/socket/nio/NioAcceptedSocketChannel.java
@@ -49,6 +49,5 @@ final class NioAcceptedSocketChannel extends NioSocketChannel {
         setConnected();
         
         fireChannelOpen(this);
-        fireChannelBound(this, getLocalAddress());
     }
 }
