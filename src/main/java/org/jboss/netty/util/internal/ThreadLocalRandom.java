a703066406a32d413b92cd9cc477aab93ed072f0
Craig P. Motlin <cmotlin@gmail.com>
2011-11-14 19:50:28 -0500

    Sort modifiers.

--- src/main/java/org/jboss/netty/util/internal/ThreadLocalRandom.java
+++ src/main/java/org/jboss/netty/util/internal/ThreadLocalRandom.java
@@ -49,9 +49,9 @@ import java.util.Random;
  */
 final class ThreadLocalRandom extends Random {
     // same constants as Random, but must be redeclared because private
-    private final static long multiplier = 0x5DEECE66DL;
-    private final static long addend = 0xBL;
-    private final static long mask = (1L << 48) - 1;
+    private static final long multiplier = 0x5DEECE66DL;
+    private static final long addend = 0xBL;
+    private static final long mask = (1L << 48) - 1;
 
     /**
      * The random seed. We can't use super.seed.
