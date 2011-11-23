800a9fc20d7258275f687b29ef8eddea3f944192
alepar <alepar@gmail.com>
2011-11-23 14:48:42 +0400

    making interface public, so that others can implement it jic

--- src/main/java/org/jboss/netty/handler/codec/serialization/ClassResolver.java
+++ src/main/java/org/jboss/netty/handler/codec/serialization/ClassResolver.java
@@ -4,7 +4,7 @@ package org.jboss.netty.handler.codec.serialization;
 /**
  * please use {@link ClassResolvers} as instance factory
  */
-interface ClassResolver {
+public interface ClassResolver {
 
     Class<?> resolve(String className) throws ClassNotFoundException;
 
