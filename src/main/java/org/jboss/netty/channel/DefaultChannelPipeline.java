dcaf6c8645d266ccc3bf0ff56dc2017e47547d66
Trustin Lee <trustin@gmail.com>
2011-11-23 14:06:07 +0900

    Include the duplicate name in an exception message

--- src/main/java/org/jboss/netty/channel/DefaultChannelPipeline.java
+++ src/main/java/org/jboss/netty/channel/DefaultChannelPipeline.java
@@ -665,7 +665,7 @@ public class DefaultChannelPipeline implements ChannelPipeline {
 
     private void checkDuplicateName(String name) {
         if (name2ctx.containsKey(name)) {
-            throw new IllegalArgumentException("Duplicate handler name.");
+            throw new IllegalArgumentException("Duplicate handler name: " + name);
         }
     }
 
