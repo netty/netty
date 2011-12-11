34152857ce1ffc6232ddcbc9e79939112e49ddb1
Jestan Nirojan <jestannirojan@gmail.com>
2011-12-11 23:08:47 +0530

    merged with upstream master

---
+++ src/main/java/org/jboss/netty/util/Version.java
@@ -0,0 +1,13 @@
+// DO NOT MODIFY - WILL BE OVERWRITTEN DURING THE BUILD PROCESS
+package org.jboss.netty.util;
+/**
+ * Provides the version information of Netty.
+ * @apiviz.landmark
+ */
+public final class Version {
+ /** The version identifier. */
+ public static final String ID = "4.0.0.Alpha1-SNAPSHOT-f62bb85";
+ /** Prints out the version identifier to stdout. */
+ public static void main(String[] args) { System.out.println(ID); }
+ private Version() { super(); }
+}
