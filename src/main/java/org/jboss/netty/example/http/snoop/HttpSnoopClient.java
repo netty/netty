26871d2d057cc1d158982a5f7c06ff2b3c6745bb
Norman Maurer <norman@apache.org>
2012-02-12 12:34:18 +0100

    Fix HttpSnoopClient. See #183

--- src/main/java/org/jboss/netty/example/http/snoop/HttpSnoopClient.java
+++ src/main/java/org/jboss/netty/example/http/snoop/HttpSnoopClient.java
@@ -83,7 +83,7 @@ public class HttpSnoopClient {
 
         // Prepare the HTTP request.
         HttpRequest request = new DefaultHttpRequest(
-                HttpVersion.HTTP_1_1, HttpMethod.GET, uri.toASCIIString());
+                HttpVersion.HTTP_1_1, HttpMethod.GET, uri.getRawPath());
         request.setHeader(HttpHeaders.Names.HOST, host);
         request.setHeader(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.CLOSE);
         request.setHeader(HttpHeaders.Names.ACCEPT_ENCODING, HttpHeaders.Values.GZIP);
