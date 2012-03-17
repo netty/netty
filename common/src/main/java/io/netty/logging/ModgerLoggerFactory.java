46b51ad02af0e7798ded41507a4cce1ad25ab164
Cruz Julian Bishop <cruz@CruzPC.(none)>
2012-03-17 18:06:07 +1000

    Initial implementation of Modger logging
    Tests will be in the next commit

---
+++ common/src/main/java/io/netty/logging/ModgerLoggerFactory.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2011 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.logging;
+
+/**
+ * A logger factory for <a href="https://launchpad.net/modger">Modger</a> loggers
+ */
+public class ModgerLoggerFactory extends InternalLoggerFactory {
+
+    /**
+     * Gets a new instance of a Modger logger
+     * 
+     * @param name The name to use
+     * @return The logger
+     */
+    @Override
+    public InternalLogger newInstance(String name) {
+        return new ModgerLogger(name);
+    }
+    
+}
