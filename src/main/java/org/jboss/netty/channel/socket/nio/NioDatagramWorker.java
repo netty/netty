c6ce31db960592a81583b2550e1dbb5e47f8ad06
norman <norman@apache.org>
2012-04-25 09:21:33 +0200

    NioDatagramWorker.ChannelRegistionTask should handle ClosedChannelException gracefully. See #281 and #277

--- src/main/java/org/jboss/netty/channel/socket/nio/NioDatagramWorker.java
+++ src/main/java/org/jboss/netty/channel/socket/nio/NioDatagramWorker.java
@@ -28,6 +28,7 @@ import org.jboss.netty.channel.Channels;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.ReceiveBufferSizePredictor;
 
+import java.io.IOException;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousCloseException;
@@ -200,13 +201,16 @@ public class NioDatagramWorker extends AbstractNioWorker {
                 if (future != null) {
                     future.setSuccess();
                 }
-            } catch (final ClosedChannelException e) {
+            } catch (final IOException e) {
                 if (future != null) {
                     future.setFailure(e);
                 }
                 close(channel, succeededFuture(channel));
-                throw new ChannelException(
-                        "Failed to register a socket to the selector.", e);
+                
+                if (!(e instanceof ClosedChannelException)) {
+                    throw new ChannelException(
+                            "Failed to register a socket to the selector.", e);
+                }
             }
         }
     }
