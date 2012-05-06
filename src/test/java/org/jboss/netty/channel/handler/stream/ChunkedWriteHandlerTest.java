6c95a9843b8b253d2513905cdabe667d3271e9b6
Norman Maurer <norman@apache.org>
2012-05-06 19:29:27 +0200

    Add more tests for ChunkedWriteHandler

--- src/test/java/org/jboss/netty/channel/handler/stream/ChunkedWriteHandlerTest.java
+++ src/test/java/org/jboss/netty/channel/handler/stream/ChunkedWriteHandlerTest.java
@@ -15,32 +15,94 @@
  */package org.jboss.netty.channel.handler.stream;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.Channels;
 
 import junit.framework.Assert;
 
 import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.handler.codec.embedder.EncoderEmbedder;
+import org.jboss.netty.handler.stream.ChunkedFile;
+import org.jboss.netty.handler.stream.ChunkedInput;
+import org.jboss.netty.handler.stream.ChunkedNioFile;
+import org.jboss.netty.handler.stream.ChunkedNioStream;
 import org.jboss.netty.handler.stream.ChunkedStream;
 import org.jboss.netty.handler.stream.ChunkedWriteHandler;
 import org.junit.Test;
 
 public class ChunkedWriteHandlerTest {
     private static final byte[] BYTES = new byte[1024 * 64];
+    private static final File TMP;
+    
     static {
         for (int i = 0; i < BYTES.length; i++) {
             BYTES[i] = (byte) i;
+        }
+        
+        FileOutputStream out = null;
+        try {
+            TMP = File.createTempFile("netty-chunk-", ".tmp");
+            TMP.deleteOnExit();
+            out = new FileOutputStream(TMP);
+            out.write(BYTES);
+            out.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    // ignore
+                }
+            }
         }
     }
 
     // See #310
     @Test
     public void testChunkedStream() {
+        check(new ChunkedStream(new ByteArrayInputStream(BYTES)));
+        
+        check(new ChunkedStream(new ByteArrayInputStream(BYTES)), new ChunkedStream(new ByteArrayInputStream(BYTES)), new ChunkedStream(new ByteArrayInputStream(BYTES)));
+
+    }
+    
+    @Test
+    public void testChunkedNioStream() {
+        check(new ChunkedNioStream(Channels.newChannel(new ByteArrayInputStream(BYTES))));
+        
+        check(new ChunkedNioStream(Channels.newChannel(new ByteArrayInputStream(BYTES))), new ChunkedNioStream(Channels.newChannel(new ByteArrayInputStream(BYTES))), new ChunkedNioStream(Channels.newChannel(new ByteArrayInputStream(BYTES))));
+
+    }
+    
+    
+    @Test
+    public void testChunkedFile() throws IOException {
+        check(new ChunkedFile(TMP));
+
+        check(new ChunkedFile(TMP), new ChunkedFile(TMP), new ChunkedFile(TMP));
+    }
+    
+    @Test
+    public void testChunkedNioFile() throws IOException {
+        check(new ChunkedNioFile(TMP));
+
+        check(new ChunkedNioFile(TMP), new ChunkedNioFile(TMP), new ChunkedNioFile(TMP));
+    }
+    
+    private void check(ChunkedInput... inputs) {
         EncoderEmbedder<ChannelBuffer> embedder = new EncoderEmbedder<ChannelBuffer>(new ChunkedWriteHandler());
-        embedder.offer(new ChunkedStream(new ByteArrayInputStream(BYTES)));
+        for (ChunkedInput input: inputs) {
+            embedder.offer(input);
+        }
 
         Assert.assertTrue(embedder.finish());
 
         int i = 0;
+        int read = 0;
         for (;;) {
             ChannelBuffer buffer = embedder.poll();
             if (buffer == null) {
@@ -48,9 +110,13 @@ public class ChunkedWriteHandlerTest {
             }
             while (buffer.readable()) {
                 Assert.assertEquals(BYTES[i++], buffer.readByte());
+                read++;
+                if (i == BYTES.length) {
+                    i = 0;
+                }
             }
         }
 
-        Assert.assertEquals(BYTES.length, i);
+        Assert.assertEquals(BYTES.length * inputs.length, read);
     }
 }
