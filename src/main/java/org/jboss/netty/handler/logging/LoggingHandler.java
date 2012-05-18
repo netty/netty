09e86c9c9bb76089e13fa36b1e57442887e83ac7
norman <norman@apache.org>
2012-05-18 07:36:38 +0200

    Fix checkstyle

--- src/main/java/org/jboss/netty/handler/logging/LoggingHandler.java
+++ src/main/java/org/jboss/netty/handler/logging/LoggingHandler.java
@@ -56,7 +56,7 @@ public class LoggingHandler implements ChannelUpstreamHandler, ChannelDownstream
             buf.append(i);
             BYTE2HEX[i] = buf.toString();
         }
-        for (;i < 16; i ++) {
+        for (; i < 16; i ++) {
             StringBuilder buf = new StringBuilder(3);
             buf.append(" 0");
             buf.append((char) ('a' + i - 10));
