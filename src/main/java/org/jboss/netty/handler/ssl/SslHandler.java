afb904e109b8537d18bc4b12111dc52587548228
Jeff Pinner <jpinner@twitter.com>
2012-05-18 11:59:11 -0700

    Make SslBufferPool an interface

--- src/main/java/org/jboss/netty/handler/ssl/SslHandler.java
+++ src/main/java/org/jboss/netty/handler/ssl/SslHandler.java
@@ -171,7 +171,7 @@ public class SslHandler extends FrameDecoder
      */
     public static synchronized SslBufferPool getDefaultBufferPool() {
         if (defaultBufferPool == null) {
-            defaultBufferPool = new SslBufferPool();
+            defaultBufferPool = new DefaultSslBufferPool();
         }
         return defaultBufferPool;
     }
