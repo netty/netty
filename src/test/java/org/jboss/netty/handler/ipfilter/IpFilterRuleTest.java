a2b3fc83673e0506bb5f73f7b655b42a217d4740
Norman Maurer <norman@apache.org>
2012-05-19 20:34:24 +0200

    Remove @Override annotation from interface implements

--- src/test/java/org/jboss/netty/handler/ipfilter/IpFilterRuleTest.java
+++ src/test/java/org/jboss/netty/handler/ipfilter/IpFilterRuleTest.java
@@ -39,245 +39,174 @@ public class IpFilterRuleTest extends TestCase
         return h.accept(new ChannelHandlerContext()
         {
 
-            @Override
             public boolean canHandleDownstream()
             {
-                // TODO Auto-generated method stub
                 return false;
             }
 
-            @Override
             public boolean canHandleUpstream()
             {
-                // TODO Auto-generated method stub
                 return false;
             }
 
-            @Override
             public Object getAttachment()
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public Channel getChannel()
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public ChannelHandler getHandler()
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public String getName()
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public ChannelPipeline getPipeline()
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public void sendDownstream(ChannelEvent e)
-            {
-                // TODO Auto-generated method stub
-                
+            {                
             }
 
-            @Override
             public void sendUpstream(ChannelEvent e)
-            {
-                // TODO Auto-generated method stub
-                
+            {                
             }
 
-            @Override
             public void setAttachment(Object attachment)
-            {
-                // TODO Auto-generated method stub
-                
+            {                
             }
             
         }, 
         new UpstreamMessageEvent(new Channel()
         {
 
-            @Override
             public ChannelFuture bind(SocketAddress localAddress)
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public ChannelFuture close()
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public ChannelFuture connect(SocketAddress remoteAddress)
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public ChannelFuture disconnect()
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public ChannelFuture getCloseFuture()
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public ChannelConfig getConfig()
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public ChannelFactory getFactory()
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public Integer getId()
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public int getInterestOps()
             {
-                // TODO Auto-generated method stub
                 return 0;
             }
 
-            @Override
             public SocketAddress getLocalAddress()
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public Channel getParent()
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public ChannelPipeline getPipeline()
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public SocketAddress getRemoteAddress()
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public boolean isBound()
             {
-                // TODO Auto-generated method stub
                 return false;
             }
 
-            @Override
             public boolean isConnected()
             {
-                // TODO Auto-generated method stub
                 return false;
             }
 
-            @Override
             public boolean isOpen()
             {
-                // TODO Auto-generated method stub
                 return false;
             }
 
-            @Override
             public boolean isReadable()
             {
-                // TODO Auto-generated method stub
                 return false;
             }
 
-            @Override
             public boolean isWritable()
             {
-                // TODO Auto-generated method stub
                 return false;
             }
 
-            @Override
             public ChannelFuture setInterestOps(int interestOps)
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public ChannelFuture setReadable(boolean readable)
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public ChannelFuture unbind()
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public ChannelFuture write(Object message)
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public ChannelFuture write(Object message, SocketAddress remoteAddress)
             {
-                // TODO Auto-generated method stub
                 return null;
             }
 
-            @Override
             public int compareTo(Channel o)
             {
-                // TODO Auto-generated method stub
                 return 0;
             }
             
