54047c69cfea531384fff8ca75f390b321ccda0b
Trustin Lee <trustin@gmail.com>
2012-05-30 15:40:51 -0700

    Remove IpSubnet.main()
    - Should be run as a test case

--- handler/src/main/java/io/netty/handler/ipfilter/IpSubnet.java
+++ handler/src/main/java/io/netty/handler/ipfilter/IpSubnet.java
@@ -53,7 +53,7 @@ import java.net.UnknownHostException;
  */
 public class IpSubnet implements IpSet, Comparable<IpSubnet> {
     /** Internal representation */
-    private CIDR cidr;
+    private final CIDR cidr;
 
     /** Create IpSubnet for ALL (used for ALLOW or DENY ALL) */
     public IpSubnet() {
@@ -136,27 +136,5 @@ public class IpSubnet implements IpSet, Comparable<IpSubnet> {
     @Override
     public int compareTo(IpSubnet o) {
         return cidr.toString().compareTo(o.cidr.toString());
-    }
-
-    /**
-     * Simple test functions
-     *
-     * @param args where args[0] is the netmask (standard or CIDR notation) and optional args[1] is
-     *             the inetAddress to test with this IpSubnet
-     */
-    public static void main(String[] args) throws Exception {
-        if (args.length != 0) {
-            IpSubnet ipSubnet = null;
-            try {
-                ipSubnet = new IpSubnet(args[0]);
-            } catch (UnknownHostException e) {
-                return;
-            }
-            System.out.println("IpSubnet: " + ipSubnet.toString() + " from " + ipSubnet.cidr.getBaseAddress() + " to "
-                    + ipSubnet.cidr.getEndAddress() + " mask " + ipSubnet.cidr.getMask());
-            if (args.length > 1) {
-                System.out.println("Is IN: " + args[1] + " " + ipSubnet.contains(args[1]));
-            }
-        }
     }
 }
