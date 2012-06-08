86cf144b8af27cd0fcc3479ae95db8fca383a87a
Trustin Lee <trustin@gmail.com>
2012-06-09 05:07:34 +0900

    Update the checkstyle rules

--- example/src/main/java/io/netty/example/echo/EchoServerHandler.java
+++ example/src/main/java/io/netty/example/echo/EchoServerHandler.java
@@ -16,6 +16,7 @@
 package io.netty.example.echo;
 
 import io.netty.buffer.ChannelBuffer;
+import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundStreamHandlerAdapter;
 
@@ -25,6 +26,7 @@ import java.util.logging.Logger;
 /**
  * Handler implementation for the echo server.
  */
+@Sharable
 public class EchoServerHandler extends ChannelInboundStreamHandlerAdapter {
 
     private static final Logger logger = Logger.getLogger(
