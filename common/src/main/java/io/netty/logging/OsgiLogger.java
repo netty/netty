e94236c11ad238aba25f012ee18d03f32eeaa2d6
Cruz Julian Bishop <cruz@massive-dynamics.biz>
2012-07-02 14:19:23 +1000

    Documented and organized OsgiLogger

--- common/src/main/java/io/netty/logging/OsgiLogger.java
+++ common/src/main/java/io/netty/logging/OsgiLogger.java
@@ -18,15 +18,38 @@ package io.netty.logging;
 import org.osgi.service.log.LogService;
 
 /**
+ * A logger that logs messages to an
  * <a href="http://www.osgi.org/">OSGi</a> {@link LogService} logger.
  */
 class OsgiLogger extends AbstractInternalLogger {
 
+    /**
+     * The parent {@link OsgiLoggerFactory} that generated this instance
+     */
     private final OsgiLoggerFactory parent;
+
+    /**
+     * The {@link InternalLogger} to fall back to if logging to the {@link LogService} fails
+     */
     private final InternalLogger fallback;
+
+    /**
+     * This logger's name
+     */
     private final String name;
+
+    /**
+     * This logger's prefix
+     */
     private final String prefix;
 
+    /**
+     * Initializes a new {@link OsgiLogger}
+     *
+     * @param parent the parent {@link OsgiLoggerFactory} of this instance
+     * @param name the name of the logger
+     * @param fallback the fallback {@link InternalLogger}
+     */
     OsgiLogger(OsgiLoggerFactory parent, String name, InternalLogger fallback) {
         this.parent = parent;
         this.name = name;
@@ -34,106 +57,176 @@ class OsgiLogger extends AbstractInternalLogger {
         prefix = "[" + name + "] ";
     }
 
-    @Override
-    public void debug(String msg) {
-        LogService logService = parent.getLogService();
-        if (logService != null) {
-            logService.log(LogService.LOG_DEBUG, prefix + msg);
-        } else {
-            fallback.debug(msg);
-        }
-    }
-
-    @Override
-    public void debug(String msg, Throwable cause) {
-        LogService logService = parent.getLogService();
-        if (logService != null) {
-            logService.log(LogService.LOG_DEBUG, prefix + msg, cause);
-        } else {
-            fallback.debug(msg, cause);
-        }
-    }
-
-    @Override
-    public void error(String msg) {
-        LogService logService = parent.getLogService();
-        if (logService != null) {
-            logService.log(LogService.LOG_ERROR, prefix + msg);
-        } else {
-            fallback.error(msg);
-        }
-    }
-
-    @Override
-    public void error(String msg, Throwable cause) {
-        LogService logService = parent.getLogService();
-        if (logService != null) {
-            logService.log(LogService.LOG_ERROR, prefix + msg, cause);
-        } else {
-            fallback.error(msg, cause);
-        }
-    }
-
-    @Override
-    public void info(String msg) {
-        LogService logService = parent.getLogService();
-        if (logService != null) {
-            logService.log(LogService.LOG_INFO, prefix + msg);
-        } else {
-            fallback.info(msg);
-        }
-    }
-
-    @Override
-    public void info(String msg, Throwable cause) {
-        LogService logService = parent.getLogService();
-        if (logService != null) {
-            logService.log(LogService.LOG_INFO, prefix + msg, cause);
-        } else {
-            fallback.info(msg, cause);
-        }
-    }
-
+    /**
+     * Checks to see if debugging messages can be logged
+     *
+     * @return true, since all messages can be logged
+     */
     @Override
     public boolean isDebugEnabled() {
         return true;
     }
 
-    @Override
-    public boolean isErrorEnabled() {
-        return true;
-    }
-
+    /**
+     * Checks to see if informational messages can be logged
+     *
+     * @return true, since all messages can be logged
+     */
     @Override
     public boolean isInfoEnabled() {
         return true;
     }
 
+    /**
+     * Checks to see if warning messages can be logged
+     *
+     * @return true, since all messages can be logged
+     */
     @Override
     public boolean isWarnEnabled() {
         return true;
     }
 
+    /**
+     * Checks to see if error messages can be logged
+     *
+     * @return true, since all messages can be logged
+     */
     @Override
-    public void warn(String msg) {
+    public boolean isErrorEnabled() {
+        return true;
+    }
+
+    /**
+     * Logs a message used for debugging
+     *
+     * @param message the message being logged
+     */
+    @Override
+    public void debug(String message) {
         LogService logService = parent.getLogService();
         if (logService != null) {
-            logService.log(LogService.LOG_WARNING, prefix + msg);
+            logService.log(LogService.LOG_DEBUG, prefix + message);
         } else {
-            fallback.warn(msg);
+            fallback.debug(message);
         }
     }
 
+    /**
+     * Logs a message used for debugging with an attached cause
+     *
+     * @param message the message being logged
+     * @param cause the cause of this message
+     */
     @Override
-    public void warn(String msg, Throwable cause) {
+    public void debug(String message, Throwable cause) {
         LogService logService = parent.getLogService();
         if (logService != null) {
-            logService.log(LogService.LOG_WARNING, prefix + msg, cause);
+            logService.log(LogService.LOG_DEBUG, prefix + message, cause);
         } else {
-            fallback.warn(msg, cause);
+            fallback.debug(message, cause);
         }
     }
 
+    /**
+     * Logs a message used for information
+     *
+     * @param message the message being logged
+     */
+    @Override
+    public void info(String message) {
+        LogService logService = parent.getLogService();
+        if (logService != null) {
+            logService.log(LogService.LOG_INFO, prefix + message);
+        } else {
+            fallback.info(message);
+        }
+    }
+
+    /**
+     * Logs a messaged used for information with an attached cause
+     *
+     * @param message the message being logged
+     * @param cause the cause of this message
+     */
+    @Override
+    public void info(String message, Throwable cause) {
+        LogService logService = parent.getLogService();
+        if (logService != null) {
+            logService.log(LogService.LOG_INFO, prefix + message, cause);
+        } else {
+            fallback.info(message, cause);
+        }
+    }
+
+    /**
+     * Logs a message used as a warning
+     *
+     * @param message the message being logged
+     */
+    @Override
+    public void warn(String message) {
+        LogService logService = parent.getLogService();
+        if (logService != null) {
+            logService.log(LogService.LOG_WARNING, prefix + message);
+        } else {
+            fallback.warn(message);
+        }
+    }
+
+    /**
+     * Logs a message used as a warning with an attached cause
+     *
+     * @param message the message being logged
+     * @param cause the cause of this message
+     */
+    @Override
+    public void warn(String message, Throwable cause) {
+        LogService logService = parent.getLogService();
+        if (logService != null) {
+            logService.log(LogService.LOG_WARNING, prefix + message, cause);
+        } else {
+            fallback.warn(message, cause);
+        }
+    }
+
+    /**
+     * Logs a message used as an error
+     *
+     * @param message the message being logged
+     */
+    @Override
+    public void error(String message) {
+        LogService logService = parent.getLogService();
+        if (logService != null) {
+            logService.log(LogService.LOG_ERROR, prefix + message);
+        } else {
+            fallback.error(message);
+        }
+    }
+
+    /**
+     * Logs a message used as an error with an attached cause
+     *
+     * @param message the message being logged
+     * @param cause the cause of this message
+     */
+    @Override
+    public void error(String message, Throwable cause) {
+        LogService logService = parent.getLogService();
+        if (logService != null) {
+            logService.log(LogService.LOG_ERROR, prefix + message, cause);
+        } else {
+            fallback.error(message, cause);
+        }
+    }
+
+    /**
+     * Returns a {@link String}-based representation of this logger.
+     * In this case, it is the logger's name
+     *
+     * @return The name of this logger
+     */
     @Override
     public String toString() {
         return name;
