fae3861c3a5282f5e74654a58e78141229620144
Trustin Lee <trustin@gmail.com>
2012-07-07 14:46:43 +0900

    Fix a compiler warning

--- transport/src/main/java/io/netty/channel/socket/aio/AioServerSocketChannelConfig.java
+++ transport/src/main/java/io/netty/channel/socket/aio/AioServerSocketChannelConfig.java
@@ -50,7 +50,6 @@ public class AioServerSocketChannelConfig extends DefaultChannelConfig
         return getOptions(super.getOptions(), SO_RCVBUF, SO_REUSEADDR, SO_BACKLOG);
     }
 
-    @SuppressWarnings("unchecked")
     @Override
     public <T> T getOption(ChannelOption<T> option) {
         if (option == SO_RCVBUF) {
