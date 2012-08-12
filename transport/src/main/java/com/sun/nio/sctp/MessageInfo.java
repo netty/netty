4550ad88a6c519cec95b18a3802464edef66159a
Jestan Nirojan <jestannirojan@gmail.com>
2012-08-12 20:12:25 +0800

    Initial port of SCTP basic classes and interfaces to Netty 4.0

---
+++ transport/src/main/java/com/sun/nio/sctp/MessageInfo.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2011 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.sun.nio.sctp;
+
+import java.net.SocketAddress;
+
+public abstract class MessageInfo {
+    static {
+        UnsupportedOperatingSystemException.raise();
+    }
+
+    public static MessageInfo createOutgoing(Association association, SocketAddress address, int streamNumber) {
+        return null;
+    }
+
+    public abstract SocketAddress address();
+    public abstract int streamNumber();
+    public abstract MessageInfo streamNumber(int streamNumber);
+    public abstract int payloadProtocolID();
+    public abstract MessageInfo payloadProtocolID(int ppid);
+    public abstract boolean isComplete();
+    public abstract boolean isUnordered();
+    public abstract MessageInfo unordered(boolean b);
+
+}
---
+++ transport/src/main/java/io/netty/channel/socket/DefaultSctpChannelConfig.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright 2011 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.channel.socket;
+
+import com.sun.nio.sctp.SctpChannel;
+import io.netty.channel.ChannelException;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.DefaultChannelConfig;
+
+import java.io.IOException;
+import java.util.Map;
+
+import com.sun.nio.sctp.SctpStandardSocketOptions;
+import static io.netty.channel.ChannelOption.*;
+
+/**
+ * The default {@link SctpChannelConfig} implementation for SCTP.
+ */
+class DefaultSctpChannelConfig extends DefaultChannelConfig implements SctpChannelConfig {
+
+    private SctpChannel channel;
+
+    DefaultSctpChannelConfig(SctpChannel channel) {
+        if (channel == null) {
+            throw new NullPointerException("channel");
+        }
+        this.channel = channel;
+    }
+
+    @Override
+    public Map<ChannelOption<?>, Object> getOptions() {
+        return getOptions(
+                super.getOptions(),
+                SO_RCVBUF, SO_SNDBUF, SCTP_NODELAY, SCTP_INIT_MAXSTREAMS);
+    }
+
+
+    @Override
+    public <T> T getOption(ChannelOption<T> option) {
+        if (option == SO_RCVBUF) {
+            return (T) Integer.valueOf(getReceiveBufferSize());
+        }
+        if (option == SO_SNDBUF) {
+            return (T) Integer.valueOf(getSendBufferSize());
+        }
+        if (option == SCTP_NODELAY) {
+            return (T) Boolean.valueOf(isSctpNoDelay());
+        }
+        return super.getOption(option);
+    }
+
+    @Override
+    public <T> boolean setOption(ChannelOption<T> option, T value) {
+        validate(option, value);
+
+        if (option == SO_RCVBUF) {
+            setReceiveBufferSize((Integer) value);
+        } else if (option == SO_SNDBUF) {
+            setSendBufferSize((Integer) value);
+        } else if (option == SCTP_NODELAY) {
+            setSctpNoDelay((Boolean) value);
+        } else if (option == SCTP_INIT_MAXSTREAMS) {
+            setInitMaxStreams((SctpStandardSocketOptions.InitMaxStreams) value);
+        } else {
+            return super.setOption(option, value);
+        }
+
+        return true;
+    }
+
+
+    @Override
+    public boolean isSctpNoDelay() {
+        try {
+            return channel.getOption(SctpStandardSocketOptions.SCTP_NODELAY);
+        } catch (IOException e) {
+            throw new ChannelException(e);
+        }
+    }
+
+    @Override
+    public void setSctpNoDelay(boolean sctpNoDelay) {
+        try {
+            channel.setOption(SctpStandardSocketOptions.SCTP_NODELAY, sctpNoDelay);
+        } catch (IOException e) {
+            throw new ChannelException(e);
+        }
+    }
+
+    @Override
+    public int getSendBufferSize() {
+        try {
+            return channel.getOption(SctpStandardSocketOptions.SO_SNDBUF);
+        } catch (IOException e) {
+            throw new ChannelException(e);
+        }
+    }
+
+    @Override
+    public void setSendBufferSize(int sendBufferSize) {
+        try {
+            channel.setOption(SctpStandardSocketOptions.SO_SNDBUF, sendBufferSize);
+        } catch (IOException e) {
+            throw new ChannelException(e);
+        }
+    }
+
+    @Override
+    public int getReceiveBufferSize() {
+        try {
+            return channel.getOption(SctpStandardSocketOptions.SO_RCVBUF);
+        } catch (IOException e) {
+            throw new ChannelException(e);
+        }
+    }
+
+    @Override
+    public void setReceiveBufferSize(int receiveBufferSize) {
+        try {
+            channel.setOption(SctpStandardSocketOptions.SO_RCVBUF, receiveBufferSize);
+        } catch (IOException e) {
+            throw new ChannelException(e);
+        }
+    }
+
+    @Override
+    public SctpStandardSocketOptions.InitMaxStreams getInitMaxStreams() {
+        try {
+            return channel.getOption(SctpStandardSocketOptions.SCTP_INIT_MAXSTREAMS);
+        } catch (IOException e) {
+            throw new ChannelException(e);
+        }
+    }
+
+    @Override
+    public void setInitMaxStreams(SctpStandardSocketOptions.InitMaxStreams initMaxStreams) {
+        try {
+            channel.setOption(SctpStandardSocketOptions.SCTP_INIT_MAXSTREAMS, initMaxStreams);
+        } catch (IOException e) {
+            throw new ChannelException(e);
+        }
+    }
+}
