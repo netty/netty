0d11a29af61ac829b61a519ce9822ef2da8f6d5f
Cruz Julian Bishop <cruzjbishop@gmail.com>
2012-08-20 11:51:54 +1000

    Fix issues with Cookie max age test
    These issues were introduced while fixing #534
    
    Signed-off-by: Cruz Julian Bishop <cruzjbishop@gmail.com>

--- codec-http/src/main/java/io/netty/handler/codec/http/Cookie.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/Cookie.java
@@ -87,7 +87,7 @@ public interface Cookie extends Comparable<Cookie> {
     void setComment(String comment);
 
     /**
-     * Returns the maximum age of this {@link Cookie} in seconds or {@link Long#MIN_VALUE} if unspecified
+     * Returns the maximum age of this {@link Cookie} in seconds or {@link Integer#MIN_VALUE} if unspecified
      *
      * @return The maximum age of this {@link Cookie}
      */
@@ -97,7 +97,7 @@ public interface Cookie extends Comparable<Cookie> {
      * Sets the maximum age of this {@link Cookie} in seconds.
      * If an age of {@code 0} is specified, this {@link Cookie} will be
      * automatically removed by browser because it will expire immediately.
-     * If {@link Long#MIN_VALUE} is specified, this {@link Cookie} will be removed when the
+     * If {@link Integer#MIN_VALUE} is specified, this {@link Cookie} will be removed when the
      * browser is closed.
      *
      * @param maxAge The maximum age of this {@link Cookie} in seconds
