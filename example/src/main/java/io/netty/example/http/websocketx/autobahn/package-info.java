24678e2d00e8bd3a6a411da147eabfaf488769fe
Veebs <vibuli@gmail.com>
2012-10-29 03:59:54 -0700

    update docs on running autobahn tests

--- example/src/main/java/io/netty/example/http/websocketx/autobahn/package-info.java
+++ example/src/main/java/io/netty/example/http/websocketx/autobahn/package-info.java
@@ -17,45 +17,48 @@
 /**
  * This package is intended for use with testing against the Python
  * <a href="http://www.tavendo.de/autobahn/testsuite.html">AutoBahn test suite</a>.
+ * 
+ * Autobahn installation documentation can be found <a href="http://autobahn.ws/testsuite/installation">here</a>.
+ * 
+ * <h3>How to run the tests on Ubuntu.</h3>
  *
- * <h3>How to run the tests on Ubuntu</h3>
+ * <p>01. Install <a href="http://python.org/">python</a> (if not already installed).
+ * 
+ * <p>02. Install <a href="http://pypi.python.org/pypi/setuptools">Python Setup Tools</a> if not already 
+ * installed. <tt>sudo apt-get install python-setuptools</tt>
+ * 
+ * <p>03. Add <tt>ppa:twisted-dev/ppa</tt> to your system's Software Sources
  *
- * <p>01. Add <tt>ppa:twisted-dev/ppa</tt> to your system's Software Sources
+ * <p>04. Install Twisted: <tt>sudo apt-get install python-twisted</tt>
  *
- * <p>02. Install Twisted V11: <tt>sudo apt-get install python-twisted</tt>
+ * <p>05. Install AutoBahn: <tt>sudo easy_install autobahntestsuite</tt>.  Test using <tt>wstest --help<tt>.
  *
- * <p>03. Intall Python Setup Tools: <tt>sudo apt-get install python-setuptools</tt>
+ * <p>06. Create a directory for test configuration and results: <tt>mkdir autobahn</tt> <tt>cd autobahn</tt>.
  *
- * <p>04. Install AutoBahn: <tt>sudo easy_install Autobahn</tt>. If you already have Autobahn installed, you may need
- * to upgrade it: <tt>sudo easy_install --upgrade Autobahn</tt>. Make suer v0.4.10 is installed.
- *
- * <p>05. Get AutoBahn testsuite source code: <tt>git clone git@github.com:tavendo/AutobahnPython.git</tt>
- *
- * <p>06. Go to AutoBahn directory: <tt>cd AutobahnPython</tt>
- *
- * <p>07. Checkout stable version: <tt>git checkout v0.4.10</tt>
- *
- * <p>08. Go to test suite directory: <tt>cd testsuite/websockets</tt>
- *
- * <p>09. Edit <tt>fuzzing_clinet_spec.json</tt> and set the hybi specification version to 10 or 17 (RFC 6455).
+ * <p>07. Create <tt>fuzzing_clinet_spec.json</tt> in the above directory
  * <code>
- *    {
- *       "options": {"failByDrop": false},
- *       "servers": [{"agent": "Netty", "url": "ws://localhost:9000", "options": {"version": 17}}],
- *       "cases": ["*"],
- *       "exclude-cases": [],
- *       "exclude-agent-cases": {"FoobarServer*": ["4.*", "1.1.3"]}
- *    }
+ * {
+ *    "options": {"failByDrop": false},
+ *    "outdir": "./reports/servers",
+ *  
+ *    "servers": [
+ *                 {"agent": "Netty4",
+ *                  "url": "ws://localhost:9000",
+ *                  "options": {"version": 18}}
+ *               ],
+ *  
+ *    "cases": ["*"],
+ *    "exclude-cases": [],
+ *    "exclude-agent-cases": {}
+ * }
  * </code>
  *
- * <p>10. Run our <tt>AutobahnServer</tt> located in this package. If you are in Eclipse IDE, right click on
+ * <p>08. Run the <tt>AutobahnServer</tt> located in this package. If you are in Eclipse IDE, right click on
  * <tt>AutobahnServer.java</tt> and select Run As > Java Application.
  *
- * <p>11. Run the Autobahn test <tt>python fuzzing_client.py</tt>. Note that the actual test case python code is
- * located with the easy_install package (e.g. in <tt>/usr/local/lib/python2.7/dist-packages/
- * autobahn-0.4.10-py2.7.egg/autobahn/cases</tt>) and not in the checked out git repository.
+ * <p>09. Run the Autobahn test <tt>wstest -m fuzzingclient -s fuzzingclient.json</tt>.
  *
- * <p>12. See the results in <tt>reports/servers/index.html</tt>
+ * <p>10. See the results in <tt>./reports/servers/index.html</tt>
  */
 package io.netty.example.http.websocketx.autobahn;
 
