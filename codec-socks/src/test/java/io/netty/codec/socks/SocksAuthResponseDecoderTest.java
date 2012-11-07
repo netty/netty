86b079108ce1864e3205d64d8b3f560ddf5bbdba
alexey <alexey@debian-desktop.home.org>
2012-11-07 22:54:00 +0400

    basic support for socks5 codec

---
+++ codec-socks/src/test/java/io/netty/codec/socks/SocksAuthResponseDecoderTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.codec.socks;
+
+import io.netty.channel.embedded.EmbeddedByteChannel;
+import org.junit.Test;
+import org.slf4j.LoggerFactory;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+public class SocksAuthResponseDecoderTest {
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(SocksAuthResponseDecoderTest.class);
+    private void testSocksAuthResponseDecoderWithDifferentParams(SocksMessage.AuthStatus authStatus){
+        logger.debug("Testing SocksAuthResponseDecoder with authStatus: "+ authStatus);
+        SocksAuthResponse msg = new SocksAuthResponse(authStatus);
+        SocksAuthResponseDecoder decoder = new SocksAuthResponseDecoder();
+        EmbeddedByteChannel embedder = new EmbeddedByteChannel(decoder);
+        SocksCommonTestUtils.writeMessageIntoEmbedder(embedder, msg);
+        msg = (SocksAuthResponse) embedder.readInbound();
+        assertTrue(msg.getAuthStatus().equals(authStatus));
+        assertNull(embedder.readInbound());
+    }
+
+    @Test
+    public void testSocksCmdResponseDecoder(){
+        for (SocksMessage.AuthStatus authStatus: SocksMessage.AuthStatus.values()){
+                testSocksAuthResponseDecoderWithDifferentParams(authStatus);
+        }
+    }
+}
---
+++ example/src/main/java/io/netty/example/socksproxy/RelayHandler.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.example.socksproxy;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundByteHandlerAdapter;
+
+
+public final class RelayHandler extends ChannelInboundByteHandlerAdapter {
+    private static final String name = "RELAY_HANDLER";
+
+    public static String getName() {
+        return name;
+    }
+
+    private final Channel relayChannel;
+
+    public RelayHandler(Channel relayChannel) {
+        this.relayChannel = relayChannel;
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void inboundBufferUpdated(ChannelHandlerContext ctx, ByteBuf in) throws Exception {
+        ByteBuf out = relayChannel.outboundByteBuffer();
+        out.discardReadBytes();
+        out.writeBytes(in);
+        in.clear();
+        if (relayChannel.isActive()) {
+            relayChannel.flush();
+        }
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        if (relayChannel.isActive()) {
+            SocksServerUtils.closeOnFlush(relayChannel);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
