fa51f852afb9d18f35b12c390a8114abb2589fe1
Trustin Lee <trustin@gmail.com>
2012-11-10 06:53:34 +0900

    Suppress false positives related with utility class inspections.
    IntelliJ IDEA inspection for private utility classes seems flawed.

--- transport/src/main/java/com/sun/nio/sctp/SctpStandardSocketOptions.java
+++ transport/src/main/java/com/sun/nio/sctp/SctpStandardSocketOptions.java
@@ -17,6 +17,7 @@ package com.sun.nio.sctp;
 
 import java.net.SocketAddress;
 
+@SuppressWarnings("all")
 public class SctpStandardSocketOptions {
     static {
         UnsupportedOperatingSystemException.raise();
