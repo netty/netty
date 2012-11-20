0cb145ed682177bf6721cac7f3fe4fbdf399e83a
Jaen Saul <jaen@saul.ee>
2012-11-20 15:08:32 +0200

    SPDY: fix header block values truncation in decompression

--- src/main/java/org/jboss/netty/handler/codec/spdy/SpdyFrameDecoder.java
+++ src/main/java/org/jboss/netty/handler/codec/spdy/SpdyFrameDecoder.java
@@ -544,8 +544,13 @@ public class SpdyFrameDecoder extends FrameDecoder {
             return true;
         }
         // Perhaps last call to decode filled output buffer
-        headerBlockDecompressor.decode(decompressed);
-        return decompressed.readableBytes() >= bytes;
+        int numBytes;
+        boolean done;
+        do {
+            numBytes = headerBlockDecompressor.decode(decompressed);
+            done = decompressed.readableBytes() >= bytes;
+        } while (!done && numBytes > 0);
+        return done;
     }
 
     private int readLengthField() {
