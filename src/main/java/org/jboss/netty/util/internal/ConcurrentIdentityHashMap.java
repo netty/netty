f262cb07767408029f3ebbbb292e208101550e48
Arya Asemanfar <arya@twitter.com>
2012-12-06 10:36:13 -0800

    clear out the hash table after resizing, and check for table modification in reads

--- src/main/java/org/jboss/netty/util/internal/ConcurrentIdentityHashMap.java
+++ src/main/java/org/jboss/netty/util/internal/ConcurrentIdentityHashMap.java
@@ -315,7 +315,11 @@ public final class ConcurrentIdentityHashMap<K, V> extends AbstractMap<K, V>
 
         V get(Object key, int hash) {
             if (count != 0) { // read-volatile
-                HashEntry<K, V> e = getFirst(hash);
+                HashEntry<K, V>[] tab = table;
+                HashEntry<K, V> e = tab[hash & tab.length - 1];
+                if (tab != table) {
+                    return get(key, hash);
+                }
                 while (e != null) {
                     if (e.hash == hash && keyEq(key, e.key())) {
                         V opaque = e.value();
@@ -333,7 +337,11 @@ public final class ConcurrentIdentityHashMap<K, V> extends AbstractMap<K, V>
 
         boolean containsKey(Object key, int hash) {
             if (count != 0) { // read-volatile
-                HashEntry<K, V> e = getFirst(hash);
+                HashEntry<K, V>[] tab = table;
+                HashEntry<K, V> e = tab[hash & tab.length - 1];
+                if (tab != table) {
+                    return containsKey(key, hash);
+                }
                 while (e != null) {
                     if (e.hash == hash && keyEq(key, e.key())) {
                         return true;
@@ -346,7 +354,8 @@ public final class ConcurrentIdentityHashMap<K, V> extends AbstractMap<K, V>
 
         boolean containsValue(Object value) {
             if (count != 0) { // read-volatile
-                for (HashEntry<K, V> e: table) {
+                HashEntry<K, V>[] tab = table;
+                for (HashEntry<K, V> e: tab) {
                     for (; e != null; e = e.next) {
                         V opaque = e.value();
                         V v;
@@ -361,6 +370,9 @@ public final class ConcurrentIdentityHashMap<K, V> extends AbstractMap<K, V>
                             return true;
                         }
                     }
+                }
+                if (table != tab) {
+                    return containsValue(value);
                 }
             }
             return false;
@@ -503,6 +515,9 @@ public final class ConcurrentIdentityHashMap<K, V> extends AbstractMap<K, V>
                 }
             }
             table = newTable;
+            for (int i = 0; i < oldCapacity; ++i) {
+              oldTable[i] = null;
+            }
             return reduce;
         }
 
