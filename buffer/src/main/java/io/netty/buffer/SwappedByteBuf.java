8e1a6c6cf5c01aab8670a13d9b6f99d7db9e10c9
Trustin Lee <trustin@gmail.com>
2012-12-14 11:49:01 +0900

    Add ByteBuf.maxWritableBytes()
    - Fixes #806

--- buffer/src/main/java/io/netty/buffer/SwappedByteBuf.java
+++ buffer/src/main/java/io/netty/buffer/SwappedByteBuf.java
@@ -13,6 +13,22 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
+
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package io.netty.buffer;
 
 import java.io.IOException;
@@ -129,6 +145,11 @@ public final class SwappedByteBuf implements ByteBuf {
     @Override
     public int writableBytes() {
         return buf.writableBytes();
+    }
+
+    @Override
+    public int maxWritableBytes() {
+        return buf.maxWritableBytes();
     }
 
     @Override
