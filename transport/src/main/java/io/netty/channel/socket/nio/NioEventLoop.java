6cca731bde880ad09faae318970c88953c79a1e1
Trustin Lee <trustin@gmail.com>
2012-12-14 11:17:31 +0900

    Tell NioTask the cause of unregistration
    - Add the 'cause' parameter to the channelUnregistered method

--- transport/src/main/java/io/netty/channel/socket/nio/NioEventLoop.java
+++ transport/src/main/java/io/netty/channel/socket/nio/NioEventLoop.java
@@ -13,6 +13,22 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
+
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package io.netty.channel.socket.nio;
 
 import io.netty.channel.Channel;
@@ -389,25 +405,31 @@ public final class NioEventLoop extends SingleThreadEventLoop {
             if (task == null) {
                 break;
             } else {
-                invokeChannelUnregistered(task, ch.selectionKey());
+                invokeChannelUnregistered(task, ch.selectionKey(), null);
             }
         }
     }
 
     private static void processSelectedKey(SelectionKey k, NioTask<SelectableChannel> task) {
-        boolean success = false;
+        int state = 0;
         try {
             task.channelReady(k.channel(), k);
-            success = true;
+            state = 1;
         } catch (Exception e) {
-            logger.warn("Unexpected exception while running NioTask.channelReady() - cancelling the key", e);
+            k.cancel();
+            invokeChannelUnregistered(task, k, e);
+            state = 2;
         } finally {
-            if (!success) {
+            switch (state) {
+            case 0:
                 k.cancel();
-            }
-
-            if (!k.isValid()) { // Either cancelled by channelReady() or by this method.
-                invokeChannelUnregistered(task, k);
+                invokeChannelUnregistered(task, k, null);
+                break;
+            case 1:
+                if (!k.isValid()) { // Cancelled by channelReady()
+                    invokeChannelUnregistered(task, k, null);
+                }
+                break;
             }
         }
     }
@@ -424,7 +446,7 @@ public final class NioEventLoop extends SingleThreadEventLoop {
                 k.cancel();
                 @SuppressWarnings("unchecked")
                 NioTask<SelectableChannel> task = (NioTask<SelectableChannel>) a;
-                invokeChannelUnregistered(task, k);
+                invokeChannelUnregistered(task, k, null);
             }
         }
 
@@ -434,9 +456,9 @@ public final class NioEventLoop extends SingleThreadEventLoop {
         }
     }
 
-    private static void invokeChannelUnregistered(NioTask<SelectableChannel> task, SelectionKey k) {
+    private static void invokeChannelUnregistered(NioTask<SelectableChannel> task, SelectionKey k, Throwable cause) {
         try {
-            task.channelUnregistered(k.channel());
+            task.channelUnregistered(k.channel(), cause);
         } catch (Exception e) {
             logger.warn("Unexpected exception while running NioTask.channelUnregistered()", e);
         }
