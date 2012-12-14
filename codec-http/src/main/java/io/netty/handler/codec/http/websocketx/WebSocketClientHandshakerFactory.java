92f1a81f479686419d631c5d84f142d4a9ade9ba
Veebs <vibuli@gmail.com>
2012-12-14 14:59:18 -0800

    #751 - Added support for legacy websocket v7 in order to pass SocksJS tests

--- codec-http/src/main/java/io/netty/handler/codec/http/websocketx/WebSocketClientHandshakerFactory.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/websocketx/WebSocketClientHandshakerFactory.java
@@ -81,6 +81,10 @@ public final class WebSocketClientHandshakerFactory {
             return new WebSocketClientHandshaker08(
                     webSocketURL, V08, subprotocol, allowExtensions, customHeaders, maxFramePayloadLength);
         }
+        if (version == V07) {
+            return new WebSocketClientHandshaker07(
+                    webSocketURL, V07, subprotocol, allowExtensions, customHeaders, maxFramePayloadLength);
+        }
         if (version == V00) {
             return new WebSocketClientHandshaker00(
                     webSocketURL, V00, subprotocol, customHeaders, maxFramePayloadLength);
