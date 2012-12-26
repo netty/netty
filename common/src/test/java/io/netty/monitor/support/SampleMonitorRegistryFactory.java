6b9f7065a24324e094b7a067ecdb27861d774287
Trustin Lee <trustin@gmail.com>
2012-12-26 13:48:29 +0900

    Simpler and more comprehensive method naming in monitor

--- common/src/test/java/io/netty/monitor/support/SampleMonitorRegistryFactory.java
+++ common/src/test/java/io/netty/monitor/support/SampleMonitorRegistryFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.netty.monitor.support;
 
 import io.netty.monitor.CounterMonitor;
@@ -66,19 +82,19 @@ public class SampleMonitorRegistryFactory implements MonitorRegistryFactory {
                 }
 
                 @Override
-                public void inc(long delta) {
+                public void increment(long delta) {
                 }
 
                 @Override
-                public void inc() {
+                public void increment() {
                 }
 
                 @Override
-                public void decr(long delta) {
+                public void decrement(long delta) {
                 }
 
                 @Override
-                public void decr() {
+                public void decrement() {
                 }
             };
         }
