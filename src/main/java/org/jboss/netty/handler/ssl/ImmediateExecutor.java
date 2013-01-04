1bdc13451f496e77268b80544ff71146d46e114d
Norman Maurer <nmaurer@redhat.com>
2013-01-04 11:37:30 +0100

    [#890] ImmediateExecutor should be public

--- src/main/java/org/jboss/netty/handler/ssl/ImmediateExecutor.java
+++ src/main/java/org/jboss/netty/handler/ssl/ImmediateExecutor.java
@@ -20,14 +20,18 @@ import java.util.concurrent.Executor;
 /**
  * {@link Executor} which executes the command in the caller thread.
  */
-final class ImmediateExecutor implements Executor {
+public final class ImmediateExecutor implements Executor {
 
     /**
      * The default instance.
      */
-    static final ImmediateExecutor INSTANCE = new ImmediateExecutor();
+    public static final ImmediateExecutor INSTANCE = new ImmediateExecutor();
 
     public void execute(Runnable command) {
         command.run();
+    }
+
+    private ImmediateExecutor() {
+        // should use static instance
     }
 }
