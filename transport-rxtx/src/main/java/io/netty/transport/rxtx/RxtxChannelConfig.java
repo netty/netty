c094abad7b14fb16827b13e5671665753be9ecad
Luke Wood <luke@lwood.me.uk>
2013-01-09 10:40:38 +0000

    [#561] [#912] Add Rxtx transport

---
+++ transport-rxtx/src/main/java/io/netty/transport/rxtx/RxtxChannelConfig.java
@@ -0,0 +1,317 @@
+/*
+ * Copyright 2013 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.transport.rxtx;
+
+import static io.netty.transport.rxtx.RxtxChannelOptions.*;
+
+import io.netty.channel.ChannelOption;
+import io.netty.channel.DefaultChannelConfig;
+
+import java.util.Map;
+
+import gnu.io.SerialPort;
+
+/**
+ * A configuration class for RXTX device connections.
+ */
+public class RxtxChannelConfig extends DefaultChannelConfig {
+
+    public enum Stopbits {
+        /**
+         * 1 stop bit will be sent at the end of every character
+         */
+        STOPBITS_1(SerialPort.STOPBITS_1),
+        /**
+         * 2 stop bits will be sent at the end of every character
+         */
+        STOPBITS_2(SerialPort.STOPBITS_2),
+        /**
+         * 1.5 stop bits will be sent at the end of every character
+         */
+        STOPBITS_1_5(SerialPort.STOPBITS_1_5);
+
+        private final int value;
+
+        Stopbits(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public static Stopbits ofValue(int value) {
+            for (Stopbits stopbit : Stopbits.values()) {
+                if (stopbit.value == value) {
+                    return stopbit;
+                }
+            }
+            throw new IllegalArgumentException("Unknown value for Stopbits: " + value + '.');
+        }
+    }
+
+    public enum Databits {
+        /**
+         * 5 data bits will be used for each character (ie. Baudot code)
+         */
+        DATABITS_5(SerialPort.DATABITS_5),
+        /**
+         * 6 data bits will be used for each character
+         */
+        DATABITS_6(SerialPort.DATABITS_6),
+        /**
+         * 7 data bits will be used for each character (ie. ASCII)
+         */
+        DATABITS_7(SerialPort.DATABITS_7),
+        /**
+         * 8 data bits will be used for each character (ie. binary data)
+         */
+        DATABITS_8(SerialPort.DATABITS_8);
+
+        private final int value;
+
+        Databits(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public static Databits ofValue(int value) {
+            for (Databits databit : Databits.values()) {
+                if (databit.value == value) {
+                    return databit;
+                }
+            }
+            throw new IllegalArgumentException("Unknown value for Databits: " + value + '.');
+        }
+    }
+
+    public enum Paritybit {
+        /**
+         * No parity bit will be sent with each data character at all
+         */
+        NONE(SerialPort.PARITY_NONE),
+        /**
+         * An odd parity bit will be sent with each data character, ie. will be set
+         * to 1 if the data character contains an even number of bits set to 1.
+         */
+        ODD(SerialPort.PARITY_ODD),
+        /**
+         * An even parity bit will be sent with each data character, ie. will be set
+         * to 1 if the data character contains an odd number of bits set to 1.
+         */
+        EVEN(SerialPort.PARITY_EVEN),
+        /**
+         * A mark parity bit (ie. always 1) will be sent with each data character
+         */
+        MARK(SerialPort.PARITY_MARK),
+        /**
+         * A space parity bit (ie. always 0) will be sent with each data character
+         */
+        SPACE(SerialPort.PARITY_SPACE);
+
+        private final int value;
+
+        Paritybit(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public static Paritybit ofValue(int value) {
+            for (Paritybit paritybit : Paritybit.values()) {
+                if (paritybit.value == value) {
+                    return paritybit;
+                }
+            }
+            throw new IllegalArgumentException("Unknown value for paritybit: " + value + '.');
+        }
+    }
+
+    private volatile int baudrate = 115200;
+
+    private volatile boolean dtr;
+
+    private volatile boolean rts;
+
+    private volatile Stopbits stopbits = Stopbits.STOPBITS_1;
+
+    private volatile Databits databits = Databits.DATABITS_8;
+
+    private volatile Paritybit paritybit = Paritybit.NONE;
+
+    public RxtxChannelConfig(RxtxChannel channel) {
+        super(channel);
+    }
+
+    @Override
+    public Map<ChannelOption<?>, Object> getOptions() {
+        return getOptions(super.getOptions(),
+                          BAUD_RATE, DTR, RTS, STOP_BITS, DATA_BITS, PARITY_BIT);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T getOption(ChannelOption<T> option) {
+        if (option == BAUD_RATE) {
+            return (T) Integer.valueOf(getBaudrate());
+        }
+        if (option == DTR) {
+            return (T) Boolean.valueOf(isDtr());
+        }
+        if (option == RTS) {
+            return (T) Boolean.valueOf(isRts());
+        }
+        if (option == STOP_BITS) {
+            return (T) getStopbits();
+        }
+        if (option == DATA_BITS) {
+            return (T) getDatabits();
+        }
+        if (option == PARITY_BIT) {
+            return (T) getParitybit();
+        }
+        return super.getOption(option);
+    }
+
+    @Override
+    public <T> boolean setOption(ChannelOption<T> option, T value) {
+        validate(option, value);
+
+        if (option == BAUD_RATE) {
+            setBaudrate((Integer) value);
+        } else if (option == DTR) {
+            setDtr((Boolean) value);
+        } else if (option == RTS) {
+            setRts((Boolean) value);
+        } else if (option == STOP_BITS) {
+            setStopbits((Stopbits) value);
+        } else if (option == DATA_BITS) {
+            setDatabits((Databits) value);
+        } else if (option == PARITY_BIT) {
+            setParitybit((Paritybit) value);
+        } else {
+            return super.setOption(option, value);
+        }
+        return true;
+    }
+
+    /**
+     * Sets the baud rate (ie. bits per second) for communication with the serial device.
+     * The baud rate will include bits for framing (in the form of stop bits and parity),
+     * such that the effective data rate will be lower than this value.
+     *
+     * @param baudrate The baud rate (in bits per second)
+     */
+    public void setBaudrate(final int baudrate) {
+        this.baudrate = baudrate;
+    }
+
+    /**
+     * Sets the number of stop bits to include at the end of every character to aid the
+     * serial device in synchronising with the data.
+     *
+     * @param stopbits The number of stop bits to use
+     */
+    public void setStopbits(final Stopbits stopbits) {
+        this.stopbits = stopbits;
+    }
+
+    /**
+     * Sets the number of data bits to use to make up each character sent to the serial
+     * device.
+     *
+     * @param databits The number of data bits to use
+     */
+    public void setDatabits(final Databits databits) {
+        this.databits = databits;
+    }
+
+    /**
+     * Sets the type of parity bit to be used when communicating with the serial device.
+     *
+     * @param paritybit The type of parity bit to be used
+     */
+    public void setParitybit(final Paritybit paritybit) {
+        this.paritybit = paritybit;
+    }
+
+    /**
+     * @return The configured baud rate, defaulting to 115200 if unset
+     */
+    public int getBaudrate() {
+        return baudrate;
+    }
+
+    /**
+     * @return The configured stop bits, defaulting to {@link Stopbits#STOPBITS_1} if unset
+     */
+    public Stopbits getStopbits() {
+        return stopbits;
+    }
+
+    /**
+     * @return The configured data bits, defaulting to {@link Databits#DATABITS_8} if unset
+     */
+    public Databits getDatabits() {
+        return databits;
+    }
+
+    /**
+     * @return The configured parity bit, defaulting to {@link Paritybit#NONE} if unset
+     */
+    public Paritybit getParitybit() {
+        return paritybit;
+    }
+
+    /**
+     * @return true if the serial device should support the Data Terminal Ready signal
+     */
+    public boolean isDtr() {
+        return dtr;
+    }
+
+    /**
+     * Sets whether the serial device supports the Data Terminal Ready signal, used for
+     * flow control
+     *
+     * @param dtr true if DTR is supported, false otherwise
+     */
+    public void setDtr(final boolean dtr) {
+        this.dtr = dtr;
+    }
+
+    /**
+     * @return true if the serial device should support the Ready to Send signal
+     */
+    public boolean isRts() {
+        return rts;
+    }
+
+    /**
+     * Sets whether the serial device supports the Request To Send signal, used for flow
+     * control
+     *
+     * @param rts true if RTS is supported, false otherwise
+     */
+    public void setRts(final boolean rts) {
+        this.rts = rts;
+    }
+}
