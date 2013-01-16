fa39ed4a5d549ed741cbed00ee3e84289223a18a
Trustin Lee <trustin@gmail.com>
2013-01-17 00:42:51 +0900

    Fix bad test name

--- codec-http/src/test/java/io/netty/handler/codec/http/HttpObjecctAggregatorTest.java
+++ codec-http/src/test/java/io/netty/handler/codec/http/HttpObjecctAggregatorTest.java
@@ -29,7 +29,7 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class FullHttpMessageDecoderTest {
+public class HttpObjecctAggregatorTest {
 
     @Test
     public void testAggregate() {
