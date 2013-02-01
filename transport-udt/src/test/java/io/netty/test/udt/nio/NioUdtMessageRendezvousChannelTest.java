911dea690a61f6d869c289c40668fcf3d9318385
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2013-02-01 05:51:50 -0600

    [#1010] Add verify module to check osgi bundles

---
+++ transport-udt/src/test/java/io/netty/test/udt/nio/NioUdtMessageRendezvousChannelTest.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.netty.test.udt.nio;
+
+import com.yammer.metrics.Metrics;
+import com.yammer.metrics.core.Meter;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.BufType;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.udt.nio.NioUdtMessageRendezvousChannel;
+import io.netty.test.udt.util.BootHelp;
+import io.netty.test.udt.util.EchoMessageHandler;
+import io.netty.test.udt.util.UnitHelp;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+public class NioUdtMessageRendezvousChannelTest extends AbstractUdtTest {
+
+    protected static final Logger log = LoggerFactory.getLogger(NioUdtByteAcceptorChannelTest.class);
+
+    /**
+     * verify channel meta data
+     */
+    @Test
+    public void metadata() throws Exception {
+
+        assertEquals(BufType.MESSAGE, new NioUdtMessageRendezvousChannel()
+                .metadata().bufferType());
+
+    }
+
+    /**
+     * verify basic echo message rendezvous
+     */
+    @Test(timeout = 10 * 1000)
+    public void basicEcho() throws Exception {
+
+        final int messageSize = 64 * 1024;
+        final int transferLimit = messageSize * 16;
+
+        final Meter rate1 = Metrics.newMeter(
+                NioUdtMessageRendezvousChannelTest.class, "send rate", "bytes",
+                TimeUnit.SECONDS);
+
+        final Meter rate2 = Metrics.newMeter(
+                NioUdtMessageRendezvousChannelTest.class, "send rate", "bytes",
+                TimeUnit.SECONDS);
+
+        final InetSocketAddress addr1 = UnitHelp.localSocketAddress();
+        final InetSocketAddress addr2 = UnitHelp.localSocketAddress();
+
+        final EchoMessageHandler handler1 = new EchoMessageHandler(rate1,
+                messageSize);
+        final EchoMessageHandler handler2 = new EchoMessageHandler(rate2,
+                messageSize);
+
+        final Bootstrap boot1 = BootHelp
+                .messagePeerBoot(addr1, addr2, handler1);
+        final Bootstrap boot2 = BootHelp
+                .messagePeerBoot(addr2, addr1, handler2);
+
+        final ChannelFuture connectFuture1 = boot1.connect();
+        final ChannelFuture connectFuture2 = boot2.connect();
+
+        while (handler1.meter().count() < transferLimit
+                && handler2.meter().count() < transferLimit) {
+
+            NioUdtByteAcceptorChannelTest.log.info("progress : {} {}", handler1.meter().count(), handler2
+                    .meter().count());
+
+            Thread.sleep(1000);
+
+        }
+
+        connectFuture1.channel().close().sync();
+        connectFuture2.channel().close().sync();
+
+        NioUdtByteAcceptorChannelTest.log.info("handler1 : {}", handler1.meter().count());
+        NioUdtByteAcceptorChannelTest.log.info("handler2 : {}", handler2.meter().count());
+
+        assertTrue(handler1.meter().count() >= transferLimit);
+        assertTrue(handler2.meter().count() >= transferLimit);
+
+        assertEquals(handler1.meter().count(), handler2.meter().count());
+
+        boot1.shutdown();
+        boot2.shutdown();
+
+    }
+
+}
---
+++ verify/verify-osgi-deps/src/test/java/io/netty/verify/osgi/DependencyIT.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2013 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.verify.osgi;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+/**
+ * Dependency Integration Tests.
+ */
+public class DependencyIT {
+
+    /**
+     * Default location of generated karaf features file.
+     * <p>
+     * See <a href=
+     * "http://karaf.apache.org/manual/3.0.0-SNAPSHOT/developers-guide/karaf-maven-plugin.html"
+     * >karaf-maven-plugin</a>
+     */
+    public static final String FEATURE = "./target/feature/feature.xml";
+
+    @Test
+    public void verifyKarafFeatureHasNoWrapProtocol() throws Exception {
+
+        final File file = new File(FEATURE);
+
+        final String text = FileUtils.readFileToString(file);
+
+        if (text.contains("wrap:")) {
+            System.err.println(text);
+            fail("karaf feature.xml contains 'wrap:' protocol: some transitive dependencies are not osgi bundles");
+        } else {
+            System.out.println("all transitive dependencies are osgi bundles");
+        }
+
+    }
+
+}
