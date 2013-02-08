44ea0a116f08a49e727c6b18957b4662ff6da9c8
Trustin Lee <trustin@gmail.com>
2013-02-08 23:07:20 +0900

    Replace ChannelHandlerUtil.unfoldAndAdd() with MessageBuf.unfoldAndAdd() / Remove unused methods in ChannelHandlerUtil

--- buffer/src/main/java/io/netty/buffer/MessageBuf.java
+++ buffer/src/main/java/io/netty/buffer/MessageBuf.java
@@ -26,6 +26,17 @@ import java.util.Queue;
 public interface MessageBuf<T> extends Buf, Queue<T> {
 
     /**
+     * Unfold the specified object if necessary, and then add the unfolded objects (or the specified object if
+     * unfonding was not necessary) to this buffer. If the specified object is an object array ({@code Object[]}),
+     * this method adds the elements of the array to this buffer until {@code null} is encountered.  If the specified
+     * object is {@code null}, nothing is added to this buffer.  Otherwise, the specified object is added to this
+     * buffer as-is.
+     *
+     * @return {@code true} if one or more messages were added to this buffer. {@code false} otherwise.
+     */
+    boolean unfoldAndAdd(Object o);
+
+    /**
      * Drain the content of te {@link MessageBuf} to the given {@link Collection}.
      *
      * @param c         the {@link Collection} to drain the content to
