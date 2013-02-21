743bcc07cdc217845f353ac2371c75d525800c49
Trustin Lee <trustin@gmail.com>
2013-02-21 15:49:51 -0800

    Fix regression in DefaultAttributeMap / Add Attribute.key() / More fine-grained locking

--- common/src/main/java/io/netty/util/DefaultAttributeMap.java
+++ common/src/main/java/io/netty/util/DefaultAttributeMap.java
@@ -18,6 +18,7 @@ package io.netty.util;
 import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
 /**
  * Default {@link AttributeMap} implementation which use simple synchronization to keep the memory overhead
@@ -25,29 +26,52 @@ import java.util.concurrent.atomic.AtomicReference;
  */
 public class DefaultAttributeMap implements AttributeMap {
 
-    // Initialize lazily to reduce memory consumption.
-    private Map<AttributeKey<?>, Attribute<?>> map;
+    @SuppressWarnings("rawtypes")
+    private static final AtomicReferenceFieldUpdater<DefaultAttributeMap, Map> updater =
+            AtomicReferenceFieldUpdater.newUpdater(DefaultAttributeMap.class, Map.class, "map");
+
+    // Initialize lazily to reduce memory consumption; updated by AtomicReferenceFieldUpdater above.
+    @SuppressWarnings("UnusedDeclaration")
+    private volatile Map<AttributeKey<?>, Attribute<?>> map;
 
     @Override
-    public synchronized <T> Attribute<T> attr(AttributeKey<T> key) {
+    public <T> Attribute<T> attr(AttributeKey<T> key) {
         Map<AttributeKey<?>, Attribute<?>> map = this.map;
         if (map == null) {
             // Not using ConcurrentHashMap due to high memory consumption.
-            map = this.map = new IdentityHashMap<AttributeKey<?>, Attribute<?>>(2);
+            map = new IdentityHashMap<AttributeKey<?>, Attribute<?>>(2);
+            if (!updater.compareAndSet(this, null, map)) {
+                map = this.map;
+            }
         }
 
-        @SuppressWarnings("unchecked")
-        Attribute<T> attr = (Attribute<T>) map.get(key);
-        if (attr == null) {
-            attr = new DefaultAttribute<T>();
-            map.put(key, attr);
+        synchronized (map) {
+            @SuppressWarnings("unchecked")
+            Attribute<T> attr = (Attribute<T>) map.get(key);
+            if (attr == null) {
+                attr = new DefaultAttribute<T>(map, key);
+                map.put(key, attr);
+            }
+            return attr;
         }
-        return attr;
     }
 
-    private final class DefaultAttribute<T> extends AtomicReference<T> implements Attribute<T> {
+    private static final class DefaultAttribute<T> extends AtomicReference<T> implements Attribute<T> {
 
         private static final long serialVersionUID = -2661411462200283011L;
+
+        private final Map<AttributeKey<?>, Attribute<?>> map;
+        private final AttributeKey<T> key;
+
+        DefaultAttribute(Map<AttributeKey<?>, Attribute<?>> map, AttributeKey<T> key) {
+            this.map = map;
+            this.key = key;
+        }
+
+        @Override
+        public AttributeKey<T> key() {
+            return key;
+        }
 
         @Override
         public T setIfAbsent(T value) {
@@ -61,9 +85,21 @@ public class DefaultAttributeMap implements AttributeMap {
         }
 
         @Override
+        public T getAndRemove() {
+            T oldValue = getAndSet(null);
+            remove0();
+            return oldValue;
+        }
+
+        @Override
         public void remove() {
-            synchronized (DefaultAttributeMap.this) {
-                map.remove(this);
+            set(null);
+            remove0();
+        }
+
+        private void remove0() {
+            synchronized (map) {
+                map.remove(key);
             }
         }
     }
