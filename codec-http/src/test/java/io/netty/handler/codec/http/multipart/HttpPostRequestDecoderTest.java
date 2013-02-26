eb2aa7f6d4aa959bbc1ab5ba84526474743cf75f
Norman Maurer <nmaurer@redhat.com>
2013-02-26 08:01:18 +0100

    Cleanup

--- codec-http/src/test/java/io/netty/handler/codec/http/multipart/HttpPostRequestDecoderTest.java
+++ codec-http/src/test/java/io/netty/handler/codec/http/multipart/HttpPostRequestDecoderTest.java
@@ -29,7 +29,6 @@ import org.junit.Test;
 import java.util.Arrays;
 
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertEquals;
 
 /** {@link HttpPostRequestDecoder} test case. */
 public class HttpPostRequestDecoderTest {
