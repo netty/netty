netty/netty

commit 256c931db20fbfaf597e501b74a34fd850c7f948

Move exceptionCaught(..) back to ChannelHandler. Related to [#1118]

transport/src/main/java/io/netty/channel/DefaultChannelHandlerContext.java
@@ -812,7 +812,7 @@ final class DefaultChannelHandlerContext extends DefaultAttributeMap implements 
             throw new NullPointerException("cause");
         }
 
-        findContextInbound().invokeExceptionCaught(cause);
+        next.invokeExceptionCaught(cause);
         return this;
     }
 

transport/src/main/java/io/netty/channel/ChannelHandler.java
@@ -210,6 +210,11 @@ public interface ChannelHandler {
     void afterRemove(ChannelHandlerContext ctx) throws Exception;
 
     /**
+     * Gets called if a {@link Throwable} was thrown.
+     */
+    void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception;
+
+    /**
      * Indicates that the same instance of the annotated {@link ChannelHandler}
      * can be added to one or more {@link ChannelPipeline}s multiple times
      * without a race condition.

transport/src/main/java/io/netty/channel/ChannelHandlerAdapter.java
@@ -60,4 +60,16 @@ public abstract class ChannelHandlerAdapter implements ChannelHandler {
     public void afterRemove(ChannelHandlerContext ctx) throws Exception {
         // NOOP
     }
+
+    /**
+     * Calls {@link ChannelHandlerContext#fireExceptionCaught(Throwable)} to forward
+     * to the next {@link ChannelHandler} in the {@link ChannelPipeline}.
+     *
+     * Sub-classes may override this method to change behavior.
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+            throws Exception {
+        ctx.fireExceptionCaught(cause);
+    }
 }

transport/src/main/java/io/netty/channel/ChannelStateHandler.java
@@ -57,11 +57,6 @@ public interface ChannelStateHandler extends ChannelHandler {
     void inboundBufferUpdated(ChannelHandlerContext ctx) throws Exception;
 
     /**
-     * Gets called if a {@link Throwable} was thrown.
-     */
-    void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception;
-
-    /**
      * Gets called if an user event was triggered.
      */
     void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception;

transport/src/main/java/io/netty/channel/ChannelStateHandlerAdapter.java
@@ -80,18 +80,6 @@ public abstract class ChannelStateHandlerAdapter extends ChannelHandlerAdapter i
     }
 
     /**
-     * Calls {@link ChannelHandlerContext#fireExceptionCaught(Throwable)} to forward
-     * to the next {@link ChannelHandler} in the {@link ChannelPipeline}.
-     *
-     * Sub-classes may override this method to change behavior.
-     */
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
-            throws Exception {
-        ctx.fireExceptionCaught(cause);
-    }
-
-    /**
      * Calls {@link ChannelHandlerContext#fireUserEventTriggered(Object)} to forward
      * to the next {@link ChannelHandler} in the {@link ChannelPipeline}.
      *

transport/src/main/java/io/netty/channel/DefaultChannelPipeline.java
@@ -1246,6 +1246,11 @@ final class DefaultChannelPipeline implements ChannelPipeline {
             msgSink.release();
             byteSink.release();
         }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            ctx.fireExceptionCaught(cause);
+        }
     }
 
     private static final class ByteHeadHandler extends HeadHandler {

commit 5a7d7ce64194bda3e60aa8ef29d21348982213d6

Fix class-cast exception which is a regression introduces in 256c931db20fbfaf597e501b74a34fd850c7f948

transport/src/main/java/io/netty/channel/DefaultChannelHandlerContext.java
@@ -835,7 +835,7 @@ final class DefaultChannelHandlerContext extends DefaultAttributeMap implements 
     }
 
     private void invokeExceptionCaught0(Throwable cause) {
-        ChannelStateHandler handler = (ChannelStateHandler) handler();
+        ChannelHandler handler = handler();
         try {
             handler.exceptionCaught(this, cause);
         } catch (Throwable t) {