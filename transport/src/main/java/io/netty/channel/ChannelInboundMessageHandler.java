806e9b1f8cfbfcdc0ff22a644de351b02aa7617c
Norman Maurer <nmaurer@redhat.com>
2013-03-08 14:05:40 +0100

    Make sure we handle outbound messages of type ByteBuf special

--- transport/src/main/java/io/netty/channel/ChannelInboundMessageHandler.java
+++ transport/src/main/java/io/netty/channel/ChannelInboundMessageHandler.java
@@ -15,10 +15,15 @@
  */
 package io.netty.channel;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.MessageBuf;
 
 /**
  * Special {@link ChannelInboundHandler} which store the inbound data in a {@link MessageBuf} for futher processing.
+ *
+ * If your {@link ChannelOutboundMessageHandler} handles messages of type {@link ByteBuf} or {@link Object}
+ * and you want to add a {@link ByteBuf} to the next buffer in the {@link ChannelPipeline} use
+ * {@link ChannelHandlerUtil#addToNextInboundBuffer(ChannelHandlerContext, Object)}.
  */
 public interface ChannelInboundMessageHandler<I> extends ChannelInboundHandler {
 
