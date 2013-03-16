e8c40fd5c7a5304805edf6be5f3cdd7e8e5b47b6
ursa <Kuzma.Deretuke@gmail.com>
2013-03-16 23:00:45 +0400

    Fix bug in memory-based HTTP data content initialization with input stream, add test.

---
+++ src/test/java/org/jboss/netty/handler/codec/http/multipart/AbstractMemoryHttpDataTest.java
@@ -0,0 +1,69 @@
+package org.jboss.netty.handler.codec.http.multipart;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.Charset;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Random;
+
+import static org.jboss.netty.util.CharsetUtil.UTF_8;
+import static org.junit.Assert.*;
+
+/** {@link AbstractMemoryHttpData} test cases. */
+public class AbstractMemoryHttpDataTest {
+    /**
+     * Provide content into HTTP data with input stream.
+     *
+     * @throws Exception In case of any exception.
+     */
+    @Test
+    public void testSetContentFromStream() throws Exception {
+        Random random = new SecureRandom();
+
+        for (int i = 0; i < 20; i++) {
+            // Generate input data bytes.
+            int size = random.nextInt(Short.MAX_VALUE);
+            byte[] bytes = new byte[size];
+
+            random.nextBytes(bytes);
+
+            // Generate parsed HTTP data block.
+            TestHttpData data = new TestHttpData("name", UTF_8, 0);
+
+            data.setContent(new ByteArrayInputStream(bytes));
+
+            // Validate stored data.
+            ChannelBuffer buffer = data.getChannelBuffer();
+
+            assertEquals(0, buffer.readerIndex());
+            assertEquals(bytes.length, buffer.writerIndex());
+            assertArrayEquals(bytes, Arrays.copyOf(buffer.array(), bytes.length));
+        }
+    }
+
+
+    /** Memory-based HTTP data implementation for test purposes. */
+    private static final class TestHttpData extends AbstractMemoryHttpData {
+        /**
+         * Constructs HTTP data for tests.
+         *
+         * @param name    Name of parsed data block.
+         * @param charset Used charset for data decoding.
+         * @param size    Expected data block size.
+         */
+        protected TestHttpData(String name, Charset charset, long size) {
+            super(name, charset, size);
+        }
+
+        public InterfaceHttpData.HttpDataType getHttpDataType() {
+            throw new UnsupportedOperationException("Should never be called.");
+        }
+
+        public int compareTo(InterfaceHttpData o) {
+            throw new UnsupportedOperationException("Should never be called.");
+        }
+    }
+}
