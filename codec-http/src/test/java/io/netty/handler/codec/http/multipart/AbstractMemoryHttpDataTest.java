8710a2a6df38faec6174a6e454dff05b93a8418f
ursa <Kuzma.Deretuke@gmail.com>
2013-03-18 00:59:57 +0400

    Add netty-project license header.

--- codec-http/src/test/java/io/netty/handler/codec/http/multipart/AbstractMemoryHttpDataTest.java
+++ codec-http/src/test/java/io/netty/handler/codec/http/multipart/AbstractMemoryHttpDataTest.java
@@ -1,10 +1,18 @@
-/*******************************************************************************
- * Copyright (c) 2012-2013 HFS Project. All Rights Reserved.
+/*
+ * Copyright 2012 The Netty Project
  *
- * This software is the proprietary information of Ursa-HFS Project.
- * Use is subject to license terms.
- ******************************************************************************/
-
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package io.netty.handler.codec.http.multipart;
 
 import io.netty.buffer.ByteBuf;
