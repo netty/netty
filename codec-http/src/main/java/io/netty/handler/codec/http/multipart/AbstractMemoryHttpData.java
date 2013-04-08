3e636f3947d4c7fb6950b33db5f8556e53aafb43
Frédéric Brégier <fredbregier@free.fr>
2013-04-09 01:54:34 +0300

    Update AbstractMemoryHttpData.java

--- codec-http/src/main/java/io/netty/handler/codec/http/multipart/AbstractMemoryHttpData.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/multipart/AbstractMemoryHttpData.java
@@ -38,8 +38,8 @@ public abstract class AbstractMemoryHttpData extends AbstractHttpData {
     private int chunkPosition;
     protected boolean isRenamed;
 
-    protected AbstractMemoryHttpData(String name, Charset charset, long size) {
-        super(name, charset, size);
+    protected AbstractMemoryHttpData(String name, Charset charset, long size, boolean checkBadName) {
+        super(name, charset, size, checkBadName);
     }
 
     @Override
