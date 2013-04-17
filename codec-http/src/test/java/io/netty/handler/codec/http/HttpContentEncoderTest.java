70eee55a48daad4222f8ff37d47f7182f05de473
Trustin Lee <t@motd.kr>
2013-04-17 12:51:22 +0900

    Revamp HttpContentEncoder
    - Use state machine to simplify the code
    - Always produce a chunked response for simplicity
    - Change the signature of beginEncode() - HttpContent was simply unnecessary.
    - Add more test cases
    - Fixes #1280

--- codec-http/src/test/java/io/netty/handler/codec/http/HttpContentEncoderTest.java
+++ codec-http/src/test/java/io/netty/handler/codec/http/HttpContentEncoderTest.java
@@ -23,6 +23,7 @@ import io.netty.channel.embedded.EmbeddedByteChannel;
 import io.netty.channel.embedded.EmbeddedMessageChannel;
 import io.netty.handler.codec.ByteToByteEncoder;
 import io.netty.handler.codec.http.HttpHeaders.Names;
+import io.netty.handler.codec.http.HttpHeaders.Values;
 import io.netty.util.CharsetUtil;
 import org.junit.Test;
 
@@ -31,22 +32,22 @@ import static org.junit.Assert.*;
 
 public class HttpContentEncoderTest {
 
+    private static final class TestEncoder extends HttpContentEncoder {
+        @Override
+        protected Result beginEncode(HttpResponse headers, String acceptEncoding) {
+            return new Result("test", new EmbeddedByteChannel(new ByteToByteEncoder() {
+                @Override
+                protected void encode(ChannelHandlerContext ctx, ByteBuf in, ByteBuf out) {
+                    out.writeBytes(String.valueOf(in.readableBytes()).getBytes(CharsetUtil.US_ASCII));
+                    in.skipBytes(in.readableBytes());
+                }
+            }));
+        }
+    }
+
     @Test
     public void testSplitContent() throws Exception {
-        EmbeddedMessageChannel ch = new EmbeddedMessageChannel(
-                new HttpContentEncoder() {
-                    @Override
-                    protected Result beginEncode(HttpMessage header, HttpContent msg, String acceptEncoding) {
-                        return new Result("test", new EmbeddedByteChannel(new ByteToByteEncoder() {
-                            @Override
-                            protected void encode(ChannelHandlerContext ctx, ByteBuf in, ByteBuf out) {
-                                out.writeBytes(String.valueOf(in.readableBytes()).getBytes(CharsetUtil.US_ASCII));
-                                in.skipBytes(in.readableBytes());
-                            }
-                        }));
-                    }
-                });
-
+        EmbeddedMessageChannel ch = new EmbeddedMessageChannel(new TestEncoder());
         ch.writeInbound(new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/"));
 
         ch.writeOutbound(new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK));
@@ -54,9 +55,34 @@ public class HttpContentEncoderTest {
         ch.writeOutbound(new DefaultHttpContent(Unpooled.wrappedBuffer(new byte[2])));
         ch.writeOutbound(new DefaultLastHttpContent(Unpooled.wrappedBuffer(new byte[1])));
 
-        HttpResponse res = (HttpResponse) ch.readOutbound();
-        assertThat(res.headers().get(Names.TRANSFER_ENCODING), is("chunked"));
-        assertThat(res.headers().get(Names.CONTENT_LENGTH), is(nullValue()));
+        assertEncodedResponse(ch);
+
+        HttpContent chunk;
+        chunk = (HttpContent) ch.readOutbound();
+        assertThat(chunk.data().toString(CharsetUtil.US_ASCII), is("3"));
+        chunk = (HttpContent) ch.readOutbound();
+        assertThat(chunk.data().toString(CharsetUtil.US_ASCII), is("2"));
+        chunk = (HttpContent) ch.readOutbound();
+        assertThat(chunk.data().toString(CharsetUtil.US_ASCII), is("1"));
+
+        assertThat(chunk, is(instanceOf(LastHttpContent.class)));
+        assertThat(ch.readOutbound(), is(nullValue()));
+    }
+
+    @Test
+    public void testChunkedContent() throws Exception {
+        EmbeddedMessageChannel ch = new EmbeddedMessageChannel(new TestEncoder());
+        ch.writeInbound(new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/"));
+
+        HttpResponse res = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+        res.headers().set(Names.TRANSFER_ENCODING, Values.CHUNKED);
+        ch.writeOutbound(res);
+
+        assertEncodedResponse(ch);
+
+        ch.writeOutbound(new DefaultHttpContent(Unpooled.wrappedBuffer(new byte[3])));
+        ch.writeOutbound(new DefaultHttpContent(Unpooled.wrappedBuffer(new byte[2])));
+        ch.writeOutbound(new DefaultLastHttpContent(Unpooled.wrappedBuffer(new byte[1])));
 
         HttpContent chunk;
         chunk = (HttpContent) ch.readOutbound();
@@ -72,20 +98,7 @@ public class HttpContentEncoderTest {
 
     @Test
     public void testFullContent() throws Exception {
-        EmbeddedMessageChannel ch = new EmbeddedMessageChannel(
-                new HttpContentEncoder() {
-                    @Override
-                    protected Result beginEncode(HttpMessage header, HttpContent msg, String acceptEncoding) {
-                        return new Result("test", new EmbeddedByteChannel(new ByteToByteEncoder() {
-                            @Override
-                            protected void encode(ChannelHandlerContext ctx, ByteBuf in, ByteBuf out) {
-                                out.writeBytes(String.valueOf(in.readableBytes()).getBytes(CharsetUtil.US_ASCII));
-                                in.skipBytes(in.readableBytes());
-                            }
-                        }));
-                    }
-                });
-
+        EmbeddedMessageChannel ch = new EmbeddedMessageChannel(new TestEncoder());
         ch.writeInbound(new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/"));
 
         FullHttpResponse res = new DefaultFullHttpResponse(
@@ -93,13 +106,23 @@ public class HttpContentEncoderTest {
         res.headers().set(Names.CONTENT_LENGTH, 42);
         ch.writeOutbound(res);
 
-        HttpResponse encodedRes = (HttpResponse) ch.readOutbound();
-        assertThat(encodedRes.headers().get(Names.TRANSFER_ENCODING), is(nullValue()));
-        assertThat(encodedRes.headers().get(Names.CONTENT_LENGTH), is("2"));
+        assertEncodedResponse(ch);
 
         LastHttpContent c = (LastHttpContent) ch.readOutbound();
         assertThat(c.data().readableBytes(), is(2));
+        assertThat(c.data().toString(CharsetUtil.US_ASCII), is("42"));
 
         assertThat(ch.readOutbound(), is(nullValue()));
+    }
+
+    private static void assertEncodedResponse(EmbeddedMessageChannel ch) {
+        Object o = ch.readOutbound();
+        assertThat(o, is(instanceOf(HttpResponse.class)));
+
+        HttpResponse res = (HttpResponse) o;
+        assertThat(res, is(not(instanceOf(HttpContent.class))));
+        assertThat(res.headers().get(Names.TRANSFER_ENCODING), is("chunked"));
+        assertThat(res.headers().get(Names.CONTENT_LENGTH), is(nullValue()));
+        assertThat(res.headers().get(Names.CONTENT_ENCODING), is("test"));
     }
 }
