c72b5341a38519390f51ce6d4831f3e3df2a20b0
Trustin Lee <t@motd.kr>
2013-04-24 18:46:35 +0900

    ChannelHandler.handlerRemoved() must be invoked *after* the handler has been removed completely and its buffers should be inaccessible.

--- transport/src/main/java/io/netty/channel/ChannelHandler.java
+++ transport/src/main/java/io/netty/channel/ChannelHandler.java
@@ -190,12 +190,13 @@ import java.lang.annotation.Target;
 public interface ChannelHandler {
 
     /**
-     * Gets called after the {@link ChannelHandler} was added to the actual context.
+     * Gets called after the {@link ChannelHandler} was added to the actual context and it's ready to handle events.
      */
     void handlerAdded(ChannelHandlerContext ctx) throws Exception;
 
     /**
-     * Gets called after the {@link ChannelHandler} was removed from the actual context.
+     * Gets called after the {@link ChannelHandler} was removed from the actual context and it doesn't handle events
+     * anymore.
      */
     void handlerRemoved(ChannelHandlerContext ctx) throws Exception;
 
