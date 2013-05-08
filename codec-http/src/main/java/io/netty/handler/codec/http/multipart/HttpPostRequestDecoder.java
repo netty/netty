6c0b2be41234c6593d1ca8283f1a766ecc861026
Frederic Bregier <nomail@nomail.com>
2013-05-08 12:34:02 +0200

    Same fix for Netty V4 for issue #1313
    Remove encodeAttribute/decodeAttribute for Multipart element
    Replace where necessary by cleanString (to remove '"') on name part (not
    on value part)

--- codec-http/src/main/java/io/netty/handler/codec/http/multipart/HttpPostRequestDecoder.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/multipart/HttpPostRequestDecoder.java
@@ -788,7 +788,7 @@ public class HttpPostRequestDecoder implements Iterator<InterfaceHttpData> {
             if (currentAttribute == null) {
                 try {
                     currentAttribute = factory.createAttribute(request,
-                            decodeAttribute(nameAttribute.getValue(), charset));
+                            cleanString(nameAttribute.getValue()));
                 } catch (NullPointerException e) {
                     throw new ErrorDataDecoderException(e);
                 } catch (IllegalArgumentException e) {
@@ -963,8 +963,7 @@ public class HttpPostRequestDecoder implements Iterator<InterfaceHttpData> {
                         Attribute attribute;
                         try {
                             attribute = factory.createAttribute(request,
-                                    decodeAttribute(values[0].trim(), charset),
-                                    decodeAttribute(cleanString(values[1]), charset));
+                                    cleanString(values[0]), values[1]);
                         } catch (NullPointerException e) {
                             throw new ErrorDataDecoderException(e);
                         } catch (IllegalArgumentException e) {
@@ -1024,8 +1023,7 @@ public class HttpPostRequestDecoder implements Iterator<InterfaceHttpData> {
                             Attribute attribute;
                             try {
                                 attribute = factory.createAttribute(request,
-                                        decodeAttribute(contents[0].trim(), charset),
-                                        decodeAttribute(cleanString(contents[i]), charset));
+                                        cleanString(contents[0]), contents[i]);
                             } catch (NullPointerException e) {
                                 throw new ErrorDataDecoderException(e);
                             } catch (IllegalArgumentException e) {
@@ -1126,8 +1124,7 @@ public class HttpPostRequestDecoder implements Iterator<InterfaceHttpData> {
             }
             try {
                 currentFileUpload = factory.createFileUpload(request,
-                        decodeAttribute(nameAttribute.getValue(), charset),
-                        decodeAttribute(filenameAttribute.getValue(), charset),
+                        cleanString(nameAttribute.getValue()), cleanString(filenameAttribute.getValue()),
                         contentTypeAttribute.getValue(), mechanism.value(), localCharset,
                         size);
             } catch (NullPointerException e) {
