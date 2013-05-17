cabff73b948e38dc7b5f6b23bdfb23cc4f46af49
Norman Maurer <nmaurer@redhat.com>
2013-05-17 21:19:59 +0200

    [#1369] Move ImmediateEventExecutor to common and let it access via a static public field
    * Also fix a bug there to return a correct implementation of ProgressivPRomise to work with the
     ImmediateEventExecutor

--- common/src/main/java/io/netty/util/concurrent/ImmediateEventExecutor.java
+++ common/src/main/java/io/netty/util/concurrent/ImmediateEventExecutor.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package io.netty.channel.group;
+package io.netty.util.concurrent;
 
 import io.netty.util.concurrent.AbstractEventExecutor;
 import io.netty.util.concurrent.DefaultPromise;
