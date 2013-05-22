9e7abeb3a4b5795a2adcb049d7112c765729aef5
Razvan Jurca <razvan.jurca@liverail.com>
2013-05-22 14:11:10 +0300

    Refs #1381: Fixed cookie date format to work consitently on all JVM versions

--- src/main/java/org/jboss/netty/handler/codec/http/CookieDateFormat.java
+++ src/main/java/org/jboss/netty/handler/codec/http/CookieDateFormat.java
@@ -24,7 +24,7 @@ final class CookieDateFormat extends SimpleDateFormat {
     private static final long serialVersionUID = 1789486337887402640L;
 
     CookieDateFormat() {
-        super("E, d-MMM-y HH:mm:ss z", Locale.ENGLISH);
+        super("E, d-MMM-yyyy HH:mm:ss z", Locale.ENGLISH);
         setTimeZone(TimeZone.getTimeZone("GMT"));
     }
 }
