524136fac63c57687e829aa156b93ee8d1eae0e1
Norman Maurer <nmaurer@redhat.com>
2013-06-06 12:22:17 +0200

    Add missing javadocs

--- transport/src/main/java/io/netty/channel/Channel.java
+++ transport/src/main/java/io/netty/channel/Channel.java
@@ -144,6 +144,12 @@ public interface Channel extends AttributeMap, ChannelOutboundInvoker, ChannelPr
      */
     ChannelFuture closeFuture();
 
+    /**
+     * Returns {@code true} if and only if the I/O thread will perform the
+     * requested write operation immediately.  Any write requests made when
+     * this method returns {@code false} are queued until the I/O thread is
+     * ready to process the queued write requests.
+     */
     boolean isWritable();
 
     /**
