9e71cd0af1f9bec22dcde2b97b4a129dcdf5deba
Trustin Lee <t@motd.kr>
2013-06-19 11:45:31 +0900

    Fix IllegalStateException triggered by a successful connection attempt
    - Fixes #1467
    - Provide more information about the tasks and promises on exception for easier debugging

--- common/src/main/java/io/netty/util/concurrent/PromiseTask.java
+++ common/src/main/java/io/netty/util/concurrent/PromiseTask.java
@@ -16,15 +16,41 @@
 package io.netty.util.concurrent;
 
 import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
 import java.util.concurrent.RunnableFuture;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 class PromiseTask<V> extends DefaultPromise<V> implements RunnableFuture<V> {
+
+    static <T> Callable<T> toCallable(Runnable runnable, T result) {
+        return new RunnableAdapter<T>(runnable, result);
+    }
+
+    private static final class RunnableAdapter<T> implements Callable<T> {
+        final Runnable task;
+        final T result;
+
+        RunnableAdapter(Runnable task, T result) {
+            this.task = task;
+            this.result = result;
+        }
+
+        @Override
+        public T call() {
+            task.run();
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "Callable(task: " + task + ", result: " + result + ')';
+        }
+    }
 
     protected final Callable<V> task;
 
     PromiseTask(EventExecutor executor, Runnable runnable, V result) {
-        this(executor, Executors.callable(runnable, result));
+        this(executor, toCallable(runnable, result));
     }
 
     PromiseTask(EventExecutor executor, Callable<V> callable) {
@@ -45,8 +71,10 @@ class PromiseTask<V> extends DefaultPromise<V> implements RunnableFuture<V> {
     @Override
     public void run() {
         try {
-            V result = task.call();
-            setSuccessInternal(result);
+            if (setUncancellableInternal()) {
+                V result = task.call();
+                setSuccessInternal(result);
+            }
         } catch (Throwable e) {
             setFailureInternal(e);
         }
@@ -58,6 +86,8 @@ class PromiseTask<V> extends DefaultPromise<V> implements RunnableFuture<V> {
     }
 
     protected final Promise<V> setFailureInternal(Throwable cause) {
+        Logger.getLogger(PromiseTask.class.getName()).log(
+                Level.WARNING, "Calling setFailureInternal(" + cause + ") on " + this, new Exception());
         super.setFailure(cause);
         return this;
     }
@@ -77,6 +107,8 @@ class PromiseTask<V> extends DefaultPromise<V> implements RunnableFuture<V> {
     }
 
     protected final Promise<V> setSuccessInternal(V result) {
+        Logger.getLogger(PromiseTask.class.getName()).log(
+                Level.WARNING, "Calling setSuccessInternal() on " + this, new Exception());
         super.setSuccess(result);
         return this;
     }
@@ -97,5 +129,15 @@ class PromiseTask<V> extends DefaultPromise<V> implements RunnableFuture<V> {
 
     protected final boolean setUncancellableInternal() {
         return super.setUncancellable();
+    }
+
+    @Override
+    protected StringBuilder toStringBuilder() {
+        StringBuilder buf = super.toStringBuilder();
+        buf.setCharAt(buf.length() - 1, ',');
+        buf.append(" task: ");
+        buf.append(task);
+        buf.append(')');
+        return buf;
     }
 }
