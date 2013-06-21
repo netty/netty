netty/netty

commit 0364265f4692b2f2f28ab36037c4ab6f3c242eae

Make AbstractMemoryHttpData generate less garbage

codec-http/src/main/java/io/netty/handler/codec/http/multipart/AbstractMemoryHttpData.java
@@ -16,6 +16,7 @@
 package io.netty.handler.codec.http.multipart;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.CompositeByteBuf;
 import io.netty.handler.codec.http.HttpConstants;
 
 import java.io.File;
@@ -91,8 +92,15 @@ public abstract class AbstractMemoryHttpData extends AbstractHttpData {
             size += localsize;
             if (byteBuf == null) {
                 byteBuf = buffer;
+            } else if (byteBuf instanceof CompositeByteBuf) {
+                CompositeByteBuf cbb = (CompositeByteBuf) byteBuf;
+                cbb.addComponent(buffer);
+                cbb.writerIndex(cbb.writerIndex() + buffer.readableBytes());
             } else {
-                byteBuf = wrappedBuffer(byteBuf, buffer);
+                CompositeByteBuf cbb = compositeBuffer();
+                cbb.addComponent(buffer);
+                cbb.writerIndex(buffer.readableBytes());
+                byteBuf = cbb;
             }
         }
         if (last) {

commit 7b3dc4a7a188497527ffcbca2e3b4bd8acf9f207

Fix regression introduced by 0364265f4692b2f2f28ab36037c4ab6f3c242eae which could lead to corrupt attribute values

codec-http/src/main/java/io/netty/handler/codec/http/multipart/AbstractMemoryHttpData.java
@@ -98,8 +98,8 @@ public abstract class AbstractMemoryHttpData extends AbstractHttpData {
                 cbb.writerIndex(cbb.writerIndex() + buffer.readableBytes());
             } else {
                 CompositeByteBuf cbb = compositeBuffer();
-                cbb.addComponent(buffer);
-                cbb.writerIndex(buffer.readableBytes());
+                cbb.addComponents(byteBuf, buffer);
+                cbb.writerIndex(byteBuf.readableBytes() + buffer.readableBytes());
                 byteBuf = cbb;
             }
         }