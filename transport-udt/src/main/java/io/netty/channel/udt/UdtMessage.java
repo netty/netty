ed9591d93d48dc6de6b12205bd821d9025a1601f
Norman Maurer <nmaurer@redhat.com>
2013-07-08 07:41:40 +0200

    Forgot to commit this class before. Related to [#1433]

--- transport-udt/src/main/java/io/netty/channel/udt/UdtMessage.java
+++ transport-udt/src/main/java/io/netty/channel/udt/UdtMessage.java
@@ -37,6 +37,11 @@ public final class UdtMessage extends DefaultByteBufHolder {
     }
 
     @Override
+    public UdtMessage duplicate() {
+        return new UdtMessage(content().duplicate());
+    }
+
+    @Override
     public UdtMessage retain() {
         super.retain();
         return this;
