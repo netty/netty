1d577b1b8bd3f51bcf8d9d89bb4598a28020af07
Norman Maurer <nmaurer@redhat.com>
2013-07-11 11:07:06 +0200

    [#1561] Fix Securechat example which was broken because Channel was not flushed

--- example/src/main/java/io/netty/example/securechat/SecureChatServerHandler.java
+++ example/src/main/java/io/netty/example/securechat/SecureChatServerHandler.java
@@ -47,10 +47,10 @@ public class SecureChatServerHandler extends SimpleChannelInboundHandler<String>
                 new GenericFutureListener<Future<Channel>>() {
             @Override
             public void operationComplete(Future<Channel> future) throws Exception {
-                ctx.write(
+                ctx.writeAndFlush(
                         "Welcome to " + InetAddress.getLocalHost().getHostName() +
                         " secure chat service!\n");
-                ctx.write(
+                ctx.writeAndFlush(
                         "Your session is protected by " +
                         ctx.pipeline().get(SslHandler.class).engine().getSession().getCipherSuite() +
                         " cipher suite.\n");
@@ -65,10 +65,10 @@ public class SecureChatServerHandler extends SimpleChannelInboundHandler<String>
         // Send the received message to all channels but the current one.
         for (Channel c: channels) {
             if (c != ctx.channel()) {
-                c.write("[" + ctx.channel().remoteAddress() + "] " +
+                c.writeAndFlush("[" + ctx.channel().remoteAddress() + "] " +
                         msg + '\n');
             } else {
-                c.write("[you] " + msg + '\n');
+                c.writeAndFlush("[you] " + msg + '\n');
             }
         }
 
