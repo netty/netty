5629b1880489a02427994147877f8ae3f92dc0e0
Norman Maurer <nmaurer@redhat.com>
2013-07-12 11:21:51 +0200

    Make clear how to use HTTPS in the helloworld example

--- example/src/main/java/io/netty/example/http/helloworld/HttpHelloWorldServerInitializer.java
+++ example/src/main/java/io/netty/example/http/helloworld/HttpHelloWorldServerInitializer.java
@@ -25,6 +25,11 @@ public class HttpHelloWorldServerInitializer extends ChannelInitializer<SocketCh
     public void initChannel(SocketChannel ch) throws Exception {
         ChannelPipeline p = ch.pipeline();
 
+        // Uncomment the following line if you want HTTPS
+        //SSLEngine engine = SecureChatSslContextFactory.getServerContext().createSSLEngine();
+        //engine.setUseClientMode(false);
+        //p.addLast("ssl", new SslHandler(engine));
+
         p.addLast("codec", new HttpServerCodec());
         p.addLast("handler", new HttpHelloWorldServerHandler());
     }
