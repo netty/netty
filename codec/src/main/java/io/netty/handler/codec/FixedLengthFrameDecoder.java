a8af57742334d2962bba108a0773878a3a4af346
Norman Maurer <nmaurer@redhat.com>
2013-07-12 13:49:54 +0200

    Add javadocs to methods that user may override and make the others final

--- codec/src/main/java/io/netty/handler/codec/FixedLengthFrameDecoder.java
+++ codec/src/main/java/io/netty/handler/codec/FixedLengthFrameDecoder.java
@@ -54,13 +54,21 @@ public class FixedLengthFrameDecoder extends ByteToMessageDecoder {
     }
 
     @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+    protected final void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
         Object decoded = decode(ctx, in);
         if (decoded != null) {
             out.add(decoded);
         }
     }
 
+    /**
+     * Create a frame out of the {@link ByteBuf} and return it.
+     *
+     * @param   ctx             the {@link ChannelHandlerContext} which this {@link ByteToMessageDecoder} belongs to
+     * @param   in              the {@link ByteBuf} from which to read data
+     * @return  frame           the {@link ByteBuf} which represent the frame or {@code null} if no frame could
+     *                          be created.
+     */
     protected Object decode(
             @SuppressWarnings("UnusedParameters") ChannelHandlerContext ctx, ByteBuf in) throws Exception {
         if (in.readableBytes() < frameLength) {
