660729bd56a05b3178861a9a73555d850511878b
Trustin Lee <t@motd.kr>
2013-07-12 20:41:29 +0900

    Kill the cruft

--- transport/src/main/java/io/netty/channel/DefaultChannelHandlerContext.java
+++ transport/src/main/java/io/netty/channel/DefaultChannelHandlerContext.java
@@ -4,7 +4,7 @@
  * The Netty Project licenses this file to you under the Apache License,
  * version 2.0 (the "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at:
- *abstract
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
