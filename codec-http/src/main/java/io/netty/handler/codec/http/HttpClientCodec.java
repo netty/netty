eb9baf6a3ed1e6ba83d04c7d0a09b1ee4eac3527
Norman Maurer <nmaurer@redhat.com>
2013-08-05 08:22:04 +0200

    Allow to also write FileRegion and ChunkInput when using HTTP chunked. Fixes [#1693]

--- codec-http/src/main/java/io/netty/handler/codec/http/HttpClientCodec.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/HttpClientCodec.java
@@ -86,7 +86,7 @@ public final class HttpClientCodec
 
         @Override
         protected void encode(
-                ChannelHandlerContext ctx, HttpObject msg, List<Object> out) throws Exception {
+                ChannelHandlerContext ctx, Object msg, List<Object> out) throws Exception {
             if (msg instanceof HttpRequest && !done) {
                 queue.offer(((HttpRequest) msg).getMethod());
             }
