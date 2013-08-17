02cf26ce0a6f0bf5bdbbcf8a311a962d399a9dc8
bgallagher <bgallagher@twitter.com>
2013-08-17 16:40:50 -0400

    use buffers.length instead of INITIAL_CAPACITY

--- transport/src/main/java/io/netty/channel/ChannelOutboundBuffer.java
+++ transport/src/main/java/io/netty/channel/ChannelOutboundBuffer.java
@@ -92,7 +92,7 @@ public final class ChannelOutboundBuffer {
         this.handle = handle;
 
         buffer = new Entry[INITIAL_CAPACITY];
-        for (int i = 0; i < INITIAL_CAPACITY; i++) {
+        for (int i = 0; i < buffer.length; i++) {
             buffer[i] = new Entry();
         }
 
