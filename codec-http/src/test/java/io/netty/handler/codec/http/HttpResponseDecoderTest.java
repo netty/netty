e4a267dcc6cb6497c2b5e553ddd252555fafd77f
Norman Maurer <nmaurer@redhat.com>
2013-08-23 08:11:36 +0200

    More tests with fragemented data

--- codec-http/src/test/java/io/netty/handler/codec/http/HttpResponseDecoderTest.java
+++ codec-http/src/test/java/io/netty/handler/codec/http/HttpResponseDecoderTest.java
@@ -122,6 +122,55 @@ public class HttpResponseDecoderTest {
     }
 
     @Test
+    public void testLastResponseWithTrailingHeaderFragmented() {
+        byte[] data = ("HTTP/1.1 200 OK\r\n" +
+                "Transfer-Encoding: chunked\r\n" +
+                "\r\n" +
+                "0\r\n" +
+                "Set-Cookie: t1=t1v1\r\n" +
+                "Set-Cookie: t2=t2v2; Expires=Wed, 09-Jun-2021 10:18:14 GMT\r\n" +
+                "\r\n").getBytes(CharsetUtil.US_ASCII);
+
+        for (int i = 1; i < data.length; i++) {
+            testLastResponseWithTrailingHeaderFragmented(data, i);
+        }
+    }
+
+    private static void testLastResponseWithTrailingHeaderFragmented(byte[] content, int fragmentSize) {
+        EmbeddedChannel ch = new EmbeddedChannel(new HttpResponseDecoder());
+        int headerLength = 47;
+        // split up the header
+        for (int a = 0; a < headerLength;) {
+            int amount = fragmentSize;
+            if (a + amount > headerLength) {
+                amount = headerLength -  a;
+            }
+
+            // if header is done it should produce a HttpRequest
+            boolean headerDone = a + amount == headerLength;
+            assertEquals(headerDone, ch.writeInbound(Unpooled.wrappedBuffer(content, a, amount)));
+            a += amount;
+        }
+
+        ch.writeInbound(Unpooled.wrappedBuffer(content, headerLength, content.length - headerLength));
+        HttpResponse res = (HttpResponse) ch.readInbound();
+        assertThat(res.getProtocolVersion(), sameInstance(HttpVersion.HTTP_1_1));
+        assertThat(res.getStatus(), is(HttpResponseStatus.OK));
+
+        LastHttpContent lastContent = (LastHttpContent) ch.readInbound();
+        assertThat(lastContent.content().isReadable(), is(false));
+        HttpHeaders headers = lastContent.trailingHeaders();
+        assertEquals(1, headers.names().size());
+        List<String> values = headers.getAll("Set-Cookie");
+        assertEquals(2, values.size());
+        assertTrue(values.contains("t1=t1v1"));
+        assertTrue(values.contains("t2=t2v2; Expires=Wed, 09-Jun-2021 10:18:14 GMT"));
+
+        assertThat(ch.finish(), is(false));
+        assertThat(ch.readInbound(), is(nullValue()));
+    }
+
+    @Test
     public void testResponseWithContentLength() {
         EmbeddedChannel ch = new EmbeddedChannel(new HttpResponseDecoder());
         ch.writeInbound(Unpooled.copiedBuffer(
@@ -147,4 +196,48 @@ public class HttpResponseDecoderTest {
         assertThat(ch.readInbound(), is(nullValue()));
     }
 
+    @Test
+    public void testResponseWithContentLengthFragmented() {
+        byte[] data = ("HTTP/1.1 200 OK\r\n" +
+                "Content-Length: 10\r\n" +
+                "\r\n").getBytes(CharsetUtil.US_ASCII);
+
+        for (int i = 1; i < data.length; i++) {
+            testResponseWithContentLengthFragmented(data, i);
+        }
+    }
+
+    private static void testResponseWithContentLengthFragmented(byte[] header, int fragmentSize) {
+        EmbeddedChannel ch = new EmbeddedChannel(new HttpResponseDecoder());
+        // split up the header
+        for (int a = 0; a < header.length;) {
+            int amount = fragmentSize;
+            if (a + amount > header.length) {
+                amount = header.length -  a;
+            }
+
+            // if header is done it should produce a HttpRequest
+            boolean headerDone = a + amount == header.length;
+            assertEquals(headerDone, ch.writeInbound(Unpooled.wrappedBuffer(header, a, amount)));
+            a += amount;
+        }
+
+        HttpResponse res = (HttpResponse) ch.readInbound();
+        assertThat(res.getProtocolVersion(), sameInstance(HttpVersion.HTTP_1_1));
+        assertThat(res.getStatus(), is(HttpResponseStatus.OK));
+
+        byte[] data = new byte[10];
+        for (int i = 0; i < data.length; i++) {
+            data[i] = (byte) i;
+        }
+        ch.writeInbound(Unpooled.wrappedBuffer(data, 0, data.length / 2));
+        HttpContent content = (HttpContent) ch.readInbound();
+        assertEquals(content.content().readableBytes(), 5);
+
+        ch.writeInbound(Unpooled.wrappedBuffer(data, 5, data.length / 2));
+        LastHttpContent lastContent = (LastHttpContent) ch.readInbound();
+        assertEquals(lastContent.content().readableBytes(), 5);
+        assertThat(ch.finish(), is(false));
+        assertThat(ch.readInbound(), is(nullValue()));
+    }
 }
