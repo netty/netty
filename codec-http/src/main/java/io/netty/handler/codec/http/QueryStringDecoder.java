d885394b8af1267d40ec55fb167fa6d6dc248353
Phillip Schichtel <quickwango@users.noreply.github.com>
2013-09-12 14:09:41 +0200

    [#1833] Replace ; with & only in the QueryString and not the whole URI

--- codec-http/src/main/java/io/netty/handler/codec/http/QueryStringDecoder.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/QueryStringDecoder.java
@@ -113,8 +113,7 @@ public class QueryStringDecoder {
                     "maxParams: " + maxParams + " (expected: a positive integer)");
         }
 
-        // http://en.wikipedia.org/wiki/Query_string
-        this.uri = uri.replace(';', '&');
+        this.uri = uri;
         this.charset = charset;
         this.maxParams = maxParams;
         this.hasPath = hasPath;
@@ -160,10 +159,8 @@ public class QueryStringDecoder {
             hasPath = false;
         }
         // Also take care of cut of things like "http://localhost"
-        String newUri = rawPath + '?' + uri.getRawQuery();
+        this.uri = rawPath + '?' + uri.getRawQuery();
 
-        // http://en.wikipedia.org/wiki/Query_string
-        this.uri = newUri.replace(';', '&');
         this.charset = charset;
         this.maxParams = maxParams;
     }
@@ -222,7 +219,8 @@ public class QueryStringDecoder {
                     name = decodeComponent(s.substring(pos, i), charset);
                 }
                 pos = i + 1;
-            } else if (c == '&') {
+            // http://www.w3.org/TR/html401/appendix/notes.html#h-B.2.2
+            } else if (c == '&' || c == ';') {
                 if (name == null && pos != i) {
                     // We haven't seen an `=' so far but moved forward.
                     // Must be a param of the form '&a&' so add it with
