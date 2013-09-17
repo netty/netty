65f5fc098e663681d7dffcfb68c8935d51b9739b
Michael Grove <mike@clarkparsia.com>
2013-09-17 07:22:58 -0400

    additional style fixes

--- codec-http/src/test/java/io/netty/handler/codec/http/multipart/HttpPostRequestDecoderTest.java
+++ codec-http/src/test/java/io/netty/handler/codec/http/multipart/HttpPostRequestDecoderTest.java
@@ -27,10 +27,6 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.UnpooledByteBufAllocator;
 import io.netty.handler.codec.http.LastHttpContent;
-import io.netty.handler.codec.http.multipart.Attribute;
-import io.netty.handler.codec.http.multipart.DefaultHttpDataFactory;
-import io.netty.handler.codec.http.multipart.HttpPostRequestDecoder;
-import io.netty.handler.codec.http.multipart.InterfaceHttpData;
 import io.netty.util.CharsetUtil;
 import org.junit.Test;
 
@@ -127,7 +123,7 @@ public class HttpPostRequestDecoderTest {
         final HttpPostRequestDecoder decoder = new HttpPostRequestDecoder(inMemoryFactory, req);
         assertFalse(decoder.getBodyHttpDatas().isEmpty());
     }
-    
+
     // See https://github.com/netty/netty/issues/1848
     @Test
     public void testNoZeroOut() throws Exception {
@@ -135,18 +131,18 @@ public class HttpPostRequestDecoderTest {
 
         final DefaultHttpDataFactory aMemFactory = new DefaultHttpDataFactory(false);
 
-        DefaultHttpRequest aRequest = new DefaultHttpRequest(HttpVersion.HTTP_1_1, 
-                                                             HttpMethod.POST, 
+        DefaultHttpRequest aRequest = new DefaultHttpRequest(HttpVersion.HTTP_1_1,
+                                                             HttpMethod.POST,
                                                              "http://localhost");
         aRequest.headers().set(HttpHeaders.Names.CONTENT_TYPE,
                                "multipart/form-data; boundary=" + boundary);
-        aRequest.headers().set(HttpHeaders.Names.TRANSFER_ENCODING, 
+        aRequest.headers().set(HttpHeaders.Names.TRANSFER_ENCODING,
                                HttpHeaders.Values.CHUNKED);
 
         HttpPostRequestDecoder aDecoder = new HttpPostRequestDecoder(aMemFactory, aRequest);
 
         final String aData = "some data would be here. the data should be long enough that it " +
-                             "will be longer than the original buffer length of 256 bytes in "+
+                             "will be longer than the original buffer length of 256 bytes in " +
                              "the HttpPostRequestDecoder in order to trigger the issue. Some more " +
                              "data just to be on the safe side.";
 
@@ -158,9 +154,9 @@ public class HttpPostRequestDecoderTest {
                 aData +
                 "\r\n" +
                 "--" + boundary + "--\r\n";
-                
+
         byte[] aBytes = body.getBytes();
-                
+
         int split = 125;
 
         ByteBufAllocator aAlloc = new UnpooledByteBufAllocator(true);
@@ -177,7 +173,7 @@ public class HttpPostRequestDecoderTest {
 
         assertTrue("Should have a piece of data", aDecoder.hasNext());
 
-	InterfaceHttpData aDecodedData = aDecoder.next();
+        InterfaceHttpData aDecodedData = aDecoder.next();
 
         assertEquals(InterfaceHttpData.HttpDataType.Attribute, aDecodedData.getHttpDataType());
 
