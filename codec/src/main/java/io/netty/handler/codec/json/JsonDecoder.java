ec82b0cd639fcde6d6e17da054b0e2b4c801b420
kerr <hepin1989@gmail.com>
2013-09-27 20:29:50 +0800

    add json codec

---
+++ codec/src/main/java/io/netty/handler/codec/json/JsonDecoder.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.codec.json;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageDecoder;
+
+import java.util.List;
+
+/**
+ * Decodes a received {@link ByteBuf} into a
+ * <a href="http://www.json.org/">JSON</a>
+ * */
+public class JsonDecoder<T> extends MessageToMessageDecoder<ByteBuf> {
+    private static volatile ObjectMapper objectMapper;
+    private static final ObjectMapper defaultObjectMapper = new ObjectMapper();
+    private final Class<T> clazz;
+
+    public JsonDecoder(Class<T> clazz) {
+        this.clazz = clazz;
+    }
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
+        final byte[] array;
+        final int offset;
+        final int length = msg.readableBytes();
+        if (msg.hasArray()) {
+            array = msg.array();
+            offset = msg.arrayOffset() + msg.readerIndex();
+        } else {
+            array = new byte[length];
+            msg.getBytes(msg.readerIndex(), array, 0, length);
+            offset = 0;
+        }
+        T bean = getObjectMapper().readValue(array, offset, length, clazz);
+        System.out.println(bean);
+        out.add(bean);
+    }
+
+    public static ObjectMapper getObjectMapper() {
+        return objectMapper == null ? defaultObjectMapper : objectMapper;
+    }
+
+    public static void setObjectMapper(ObjectMapper objectMapper) {
+        JsonDecoder.objectMapper = objectMapper;
+    }
+}
