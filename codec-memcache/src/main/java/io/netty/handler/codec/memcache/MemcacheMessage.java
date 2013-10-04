c229101305688f7bb428433a497590beb42480c0
Michael Nitschinger <michael@nitschinger.at>
2013-10-04 15:58:50 +0200

    codec-memcache: add override annotations for MemcacheMessage.

--- codec-memcache/src/main/java/io/netty/handler/codec/memcache/MemcacheMessage.java
+++ codec-memcache/src/main/java/io/netty/handler/codec/memcache/MemcacheMessage.java
@@ -25,11 +25,13 @@ public interface MemcacheMessage extends MemcacheObject, ReferenceCounted {
     /**
      * Increases the reference count by {@code 1}.
      */
+    @Override
     MemcacheMessage retain();
 
     /**
      * Increases the reference count by the specified {@code increment}.
      */
+    @Override
     MemcacheMessage retain(int increment);
 
 }
