3e38b41a46d8e33c151396b5577d8ffc3348e673
Trustin Lee <t@motd.kr>
2013-11-08 18:04:40 +0900

    Saner toString() implementation for EmbeddedSocketAddress

--- transport/src/main/java/io/netty/channel/embedded/EmbeddedSocketAddress.java
+++ transport/src/main/java/io/netty/channel/embedded/EmbeddedSocketAddress.java
@@ -19,4 +19,9 @@ import java.net.SocketAddress;
 
 final class EmbeddedSocketAddress extends SocketAddress {
     private static final long serialVersionUID = 1400788804624980619L;
+
+    @Override
+    public String toString() {
+        return "embedded";
+    }
 }
