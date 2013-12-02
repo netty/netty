4af3f0e3a658913847dff05913a5ff634ad3a8fa
Trustin Lee <t@motd.kr>
2013-12-02 18:43:49 +0900

    Optimize and clean up LoggingHandler
    - Use ': ' instead of '(...)' for simpler string concatenation and prettier presentation
    - Optimize the overall performance of format*() methods
    - All format*() methods are now expected to encode the channel information by themselves so that StringBuilder instances are created less often.
    - Use a look-up table for generating per-row prefixes
    - Hid formatByteBuf(), formatByteBufHolder(), and formatNonByteBuf() from user because a user can always override format(ctx, eventName, arg).  For example, to disable hexdump:
    
        protected void format(ChannelHandlerContext ctx, String eventName, Object arg) {
            if (arg instanceof ByteBuf) {
                super.format(ctx, eventName, arg.toString());
            } else {
                super.format(ctx, eventName, arg);
            }
        }

--- handler/src/main/java/io/netty/handler/logging/LoggingHandler.java
+++ handler/src/main/java/io/netty/handler/logging/LoggingHandler.java
@@ -44,6 +44,7 @@ public class LoggingHandler extends ChannelDuplexHandler {
     private static final String[] HEXPADDING = new String[16];
     private static final String[] BYTEPADDING = new String[16];
     private static final char[] BYTE2CHAR = new char[256];
+    private static final String[] HEXDUMP_ROWPREFIXES = new String[65536 >>> 4];
 
     static {
         int i;
@@ -80,6 +81,16 @@ public class LoggingHandler extends ChannelDuplexHandler {
             } else {
                 BYTE2CHAR[i] = (char) i;
             }
+        }
+
+        // Generate the lookup table for the start-offset header in each row (up to 64KiB).
+        for (i = 0; i < HEXDUMP_ROWPREFIXES.length; i ++) {
+            StringBuilder buf = new StringBuilder(12);
+            buf.append(NEWLINE);
+            buf.append(Long.toHexString(i << 4 & 0xFFFFFFFFL | 0x100000000L));
+            buf.setCharAt(buf.length() - 9, '|');
+            buf.append('|');
+            HEXDUMP_ROWPREFIXES[i] = buf.toString();
         }
     }
 
@@ -168,22 +179,13 @@ public class LoggingHandler extends ChannelDuplexHandler {
         return level;
     }
 
-    protected String format(ChannelHandlerContext ctx, String message) {
-        String chStr = ctx.channel().toString();
-        StringBuilder buf = new StringBuilder(chStr.length() + message.length() + 1);
-        buf.append(chStr);
-        buf.append(' ');
-        buf.append(message);
-        return buf.toString();
-    }
-
     @Override
     public void channelRegistered(ChannelHandlerContext ctx)
             throws Exception {
         if (logger.isEnabled(internalLevel)) {
             logger.log(internalLevel, format(ctx, "REGISTERED"));
         }
-        super.channelRegistered(ctx);
+        ctx.fireChannelRegistered();
     }
 
     @Override
@@ -201,7 +203,7 @@ public class LoggingHandler extends ChannelDuplexHandler {
         if (logger.isEnabled(internalLevel)) {
             logger.log(internalLevel, format(ctx, "ACTIVE"));
         }
-        super.channelActive(ctx);
+        ctx.fireChannelActive();
     }
 
     @Override
@@ -210,34 +212,34 @@ public class LoggingHandler extends ChannelDuplexHandler {
         if (logger.isEnabled(internalLevel)) {
             logger.log(internalLevel, format(ctx, "INACTIVE"));
         }
-        super.channelInactive(ctx);
+        ctx.fireChannelInactive();
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx,
             Throwable cause) throws Exception {
         if (logger.isEnabled(internalLevel)) {
-            logger.log(internalLevel, format(ctx, "EXCEPTION: " + cause), cause);
+            logger.log(internalLevel, format(ctx, "EXCEPTION", cause), cause);
         }
-        super.exceptionCaught(ctx, cause);
+        ctx.fireExceptionCaught(cause);
     }
 
     @Override
     public void userEventTriggered(ChannelHandlerContext ctx,
             Object evt) throws Exception {
         if (logger.isEnabled(internalLevel)) {
-            logger.log(internalLevel, format(ctx, "USER_EVENT: " + evt));
+            logger.log(internalLevel, format(ctx, "USER_EVENT", evt));
         }
-        super.userEventTriggered(ctx, evt);
+        ctx.fireUserEventTriggered(evt);
     }
 
     @Override
     public void bind(ChannelHandlerContext ctx,
             SocketAddress localAddress, ChannelPromise promise) throws Exception {
         if (logger.isEnabled(internalLevel)) {
-            logger.log(internalLevel, format(ctx, "BIND(" + localAddress + ')'));
+            logger.log(internalLevel, format(ctx, "BIND", localAddress));
         }
-        super.bind(ctx, localAddress, promise);
+        ctx.bind(localAddress, promise);
     }
 
     @Override
@@ -245,47 +247,51 @@ public class LoggingHandler extends ChannelDuplexHandler {
             SocketAddress remoteAddress, SocketAddress localAddress,
             ChannelPromise promise) throws Exception {
         if (logger.isEnabled(internalLevel)) {
-            logger.log(internalLevel, format(ctx, "CONNECT(" + remoteAddress + ", " + localAddress + ')'));
+            logger.log(internalLevel, format(ctx, "CONNECT", remoteAddress, localAddress));
         }
-        super.connect(ctx, remoteAddress, localAddress, promise);
+        ctx.connect(remoteAddress, localAddress, promise);
     }
 
     @Override
     public void disconnect(ChannelHandlerContext ctx,
             ChannelPromise promise) throws Exception {
         if (logger.isEnabled(internalLevel)) {
-            logger.log(internalLevel, format(ctx, "DISCONNECT()"));
+            logger.log(internalLevel, format(ctx, "DISCONNECT"));
         }
-        super.disconnect(ctx, promise);
+        ctx.disconnect(promise);
     }
 
     @Override
     public void close(ChannelHandlerContext ctx,
             ChannelPromise promise) throws Exception {
         if (logger.isEnabled(internalLevel)) {
-            logger.log(internalLevel, format(ctx, "CLOSE()"));
+            logger.log(internalLevel, format(ctx, "CLOSE"));
         }
-        super.close(ctx, promise);
+        ctx.close(promise);
     }
 
     @Override
     public void deregister(ChannelHandlerContext ctx,
              ChannelPromise promise) throws Exception {
         if (logger.isEnabled(internalLevel)) {
-            logger.log(internalLevel, format(ctx, "DEREGISTER()"));
+            logger.log(internalLevel, format(ctx, "DEREGISTER"));
         }
         super.deregister(ctx, promise);
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        logMessage(ctx, "RECEIVED", msg);
+        if (logger.isEnabled(internalLevel)) {
+            logger.log(internalLevel, format(ctx, "RECEIVED", msg));
+        }
         ctx.fireChannelRead(msg);
     }
 
     @Override
     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
-        logMessage(ctx, "WRITE", msg);
+        if (logger.isEnabled(internalLevel)) {
+            logger.log(internalLevel, format(ctx, "WRITE", msg));
+        }
         ctx.write(msg, promise);
     }
 
@@ -297,90 +303,183 @@ public class LoggingHandler extends ChannelDuplexHandler {
         ctx.flush();
     }
 
-    private void logMessage(ChannelHandlerContext ctx, String eventName, Object msg) {
-        if (logger.isEnabled(internalLevel)) {
-            logger.log(internalLevel, format(ctx, formatMessage(eventName, msg)));
-        }
+    /**
+     * Formats an event and returns the formatted message.
+     *
+     * @param eventName the name of the event
+     */
+    protected String format(ChannelHandlerContext ctx, String eventName) {
+        String chStr = ctx.channel().toString();
+        StringBuilder buf = new StringBuilder(chStr.length() + 1 + eventName.length());
+        buf.append(chStr);
+        buf.append(' ');
+        buf.append(eventName);
+        return buf.toString();
     }
 
-    protected String formatMessage(String eventName, Object msg) {
-        if (msg instanceof ByteBuf) {
-            return formatByteBuf(eventName, (ByteBuf) msg);
-        } else if (msg instanceof ByteBufHolder) {
-            return formatByteBufHolder(eventName, (ByteBufHolder) msg);
+    /**
+     * Formats an event and returns the formatted message.
+     *
+     * @param eventName the name of the event
+     * @param arg       the argument of the event
+     */
+    protected String format(ChannelHandlerContext ctx, String eventName, Object arg) {
+        if (arg instanceof ByteBuf) {
+            return formatByteBuf(ctx, eventName, (ByteBuf) arg);
+        } else if (arg instanceof ByteBufHolder) {
+            return formatByteBufHolder(ctx, eventName, (ByteBufHolder) arg);
         } else {
-            return formatNonByteBuf(eventName, msg);
+            return formatUserMessage(ctx, eventName, arg);
         }
     }
 
     /**
-     * Returns a String which contains all details to log the {@link ByteBuf}
+     * Formats an event and returns the formatted message.
+     *
+     * @param eventName the name of the event
+     * @param firstArg  the first argument of the event
+     * @param secondArg the second argument of the event
      */
-    protected String formatByteBuf(String eventName, ByteBuf buf) {
-        int length = buf.readableBytes();
-        int rows = length / 16 + (length % 15 == 0? 0 : 1) + 4;
-        StringBuilder dump = new StringBuilder(rows * 80 + eventName.length() + 16);
+    protected String format(ChannelHandlerContext ctx, String eventName, Object firstArg, Object secondArg) {
+        String chStr = ctx.channel().toString();
+        String arg1Str = String.valueOf(firstArg);
+        if (secondArg == null) {
+            StringBuilder buf = new StringBuilder(chStr.length() + 1 + eventName + 2 + arg1Str.length());
+            buf.append(chStr).append(' ').append(eventName).append(": ").append(arg1Str);
+            return buf.toString();
+        } else {
+            String arg2Str = secondArg.toString();
+            StringBuilder buf = new StringBuilder(
+                    chStr.length() + 1 + eventName + 2 + arg1Str.length() + 2 + arg2Str.length());
+            buf.append(chStr).append(' ').append(eventName).append(": ").append(arg1Str).append(", ").append(arg2Str);
+            return buf.toString();
+        }
+    }
 
-        dump.append(eventName).append('(').append(length).append('B').append(')');
+    /**
+     * Generates the default log message of the specified event whose argument is a {@link ByteBuf}.
+     */
+    private static String formatByteBuf(ChannelHandlerContext ctx, String eventName, ByteBuf msg) {
+        String chStr = ctx.channel().toString();
+        int length = msg.readableBytes();
+        if (length == 0) {
+            StringBuilder buf = new StringBuilder(chStr.length() + 1 + eventName.length() + 4);
+            buf.append(chStr).append(' ').append(eventName).append(": 0B");
+            return buf.toString();
+        } else {
+            int rows = length / 16 + (length % 15 == 0? 0 : 1) + 4;
+            StringBuilder buf = new StringBuilder(chStr.length() + 1 + eventName.length() + 2 + 10 + 1 + 2 + rows * 80);
+
+            buf.append(chStr).append(' ').append(eventName).append(": ").append(length).append('B');
+            appendHexDump(buf, msg);
+
+            return buf.toString();
+        }
+    }
+
+    /**
+     * Generates the default log message of the specified event whose argument is a {@link ByteBufHolder}.
+     */
+    private static String formatByteBufHolder(ChannelHandlerContext ctx, String eventName, ByteBufHolder msg) {
+        String chStr = ctx.channel().toString();
+        String msgStr = msg.toString();
+        ByteBuf content = msg.content();
+        int length = content.readableBytes();
+        if (length == 0) {
+            StringBuilder buf = new StringBuilder(chStr.length() + 1 + eventName.length() + 2 + msgStr.length() + 4);
+            buf.append(chStr).append(' ').append(eventName).append(", ").append(msgStr).append(", 0B");
+            return buf.toString();
+        } else {
+            int rows = length / 16 + (length % 15 == 0? 0 : 1) + 4;
+            StringBuilder buf = new StringBuilder(
+                    chStr.length() + 1 + eventName.length() + 2 + msgStr.length() + 2 + 10 + 1 + 2 + rows * 80);
+
+            buf.append(chStr).append(' ').append(eventName).append(": ");
+            buf.append(msgStr).append(", ").append(length).append('B');
+            appendHexDump(buf, content);
+
+            return buf.toString();
+        }
+    }
+
+    private static void appendHexDump(StringBuilder dump, ByteBuf buf) {
         dump.append(
                 NEWLINE + "         +-------------------------------------------------+" +
-                        NEWLINE + "         |  0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f |" +
-                        NEWLINE + "+--------+-------------------------------------------------+----------------+");
+                NEWLINE + "         |  0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f |" +
+                NEWLINE + "+--------+-------------------------------------------------+----------------+");
 
         final int startIndex = buf.readerIndex();
         final int endIndex = buf.writerIndex();
+        final int length = endIndex - startIndex;
+        final int fullRows = length >>> 4;
+        final int remainder = length & 0xF;
 
-        int i;
-        for (i = startIndex; i < endIndex; i ++) {
-            int relIdx = i - startIndex;
-            int relIdxMod16 = relIdx & 15;
-            if (relIdxMod16 == 0) {
-                dump.append(NEWLINE);
-                dump.append(Long.toHexString(relIdx & 0xFFFFFFFFL | 0x100000000L));
-                dump.setCharAt(dump.length() - 9, '|');
-                dump.append('|');
+        // Dump the rows which have 16 bytes.
+        for (int row = 0; row < fullRows; row ++) {
+            int rowStartIndex = row << 4;
+
+            // Per-row prefix.
+            appendHexDumpRowPrefix(dump, row, rowStartIndex);
+
+            // Hex dump
+            int rowEndIndex = rowStartIndex + 16;
+            for (int j = rowStartIndex; j < rowEndIndex; j ++) {
+                dump.append(BYTE2HEX[buf.getUnsignedByte(j)]);
             }
-            dump.append(BYTE2HEX[buf.getUnsignedByte(i)]);
-            if (relIdxMod16 == 15) {
-                dump.append(" |");
-                for (int j = i - 15; j <= i; j ++) {
-                    dump.append(BYTE2CHAR[buf.getUnsignedByte(j)]);
-                }
-                dump.append('|');
+            dump.append(" |");
+
+            // ASCII dump
+            for (int j = rowStartIndex; j < rowEndIndex; j ++) {
+                dump.append(BYTE2CHAR[buf.getUnsignedByte(j)]);
             }
+            dump.append('|');
         }
 
-        if ((i - startIndex & 15) != 0) {
-            int remainder = length & 15;
+        // Dump the last row which has less than 16 bytes.
+        if (remainder != 0) {
+            int rowStartIndex = fullRows << 4;
+            appendHexDumpRowPrefix(dump, fullRows, rowStartIndex);
+
+            // Hex dump
+            int rowEndIndex = rowStartIndex + remainder;
+            for (int j = rowStartIndex; j < rowEndIndex; j ++) {
+                dump.append(BYTE2HEX[buf.getUnsignedByte(j)]);
+            }
             dump.append(HEXPADDING[remainder]);
             dump.append(" |");
-            for (int j = i - remainder; j < i; j ++) {
+
+            // Ascii dump
+            for (int j = rowStartIndex; j < rowEndIndex; j ++) {
                 dump.append(BYTE2CHAR[buf.getUnsignedByte(j)]);
             }
             dump.append(BYTEPADDING[remainder]);
             dump.append('|');
         }
 
-        dump.append(
-                NEWLINE + "+--------+-------------------------------------------------+----------------+");
-
-        return dump.toString();
+        dump.append(NEWLINE + "+--------+-------------------------------------------------+----------------+");
     }
 
     /**
-     * Returns a String which contains all details to log the {@link Object}
+     * Appends the prefix of each hex dump row.  Uses the look-up table for the buffer <= 64 KiB.
      */
-    protected String formatNonByteBuf(String eventName, Object msg) {
-        return eventName + ": " + msg;
+    private static void appendHexDumpRowPrefix(StringBuilder dump, int row, int rowStartIndex) {
+        if (row < HEXDUMP_ROWPREFIXES.length) {
+            dump.append(HEXDUMP_ROWPREFIXES[row]);
+        } else {
+            dump.append(NEWLINE);
+            dump.append(Long.toHexString(rowStartIndex & 0xFFFFFFFFL | 0x100000000L));
+            dump.setCharAt(dump.length() - 9, '|');
+            dump.append('|');
+        }
     }
 
     /**
-     * Returns a String which contains all details to log the {@link ByteBufHolder}.
-     *
-     * By default this method just delegates to {@link #formatByteBuf(String, ByteBuf)},
-     * using the content of the {@link ByteBufHolder}. Sub-classes may override this.
+     * Generates the default log message of the specified event whose argument is an arbitrary object.
      */
-    protected String formatByteBufHolder(String eventName, ByteBufHolder msg) {
-        return formatByteBuf(eventName, msg.content());
+    private static String formatUserMessage(ChannelHandlerContext ctx, String eventName, Object msg) {
+        String chStr = ctx.channel().toString();
+        String msgStr = String.valueOf(msg);
+        StringBuilder buf = new StringBuilder(chStr.length() + 1 + eventName.length() + 2 + msgStr.length());
+        return buf.append(chStr).append(' ').append(eventName).append(": ").append(msgStr).toString();
     }
 }
