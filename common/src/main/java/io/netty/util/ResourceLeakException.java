e49244118e5d3ff9d8ec17c8c2920d3a75387083
Trustin Lee <t@motd.kr>
2013-12-05 01:02:38 +0900

    Ensure backward compatibility
    .. by resurrecting the removed methods and system properties.

---
+++ common/src/main/java/io/netty/util/ResourceLeakException.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2013 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.netty.util;
+
+import java.util.Arrays;
+
+/**
+ * @deprecated This class will be removed in the future version.
+ */
+@Deprecated
+public class ResourceLeakException extends RuntimeException {
+
+    private static final long serialVersionUID = 7186453858343358280L;
+
+    private final StackTraceElement[] cachedStackTrace;
+
+    public ResourceLeakException() {
+        cachedStackTrace = getStackTrace();
+    }
+
+    public ResourceLeakException(String message) {
+        super(message);
+        cachedStackTrace = getStackTrace();
+    }
+
+    public ResourceLeakException(String message, Throwable cause) {
+        super(message, cause);
+        cachedStackTrace = getStackTrace();
+    }
+
+    public ResourceLeakException(Throwable cause) {
+        super(cause);
+        cachedStackTrace = getStackTrace();
+    }
+
+    @Override
+    public int hashCode() {
+        StackTraceElement[] trace  = cachedStackTrace;
+        int hashCode = 0;
+        for (StackTraceElement e: trace) {
+            hashCode = hashCode * 31 + e.hashCode();
+        }
+        return hashCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ResourceLeakException)) {
+            return false;
+        }
+        if (o == this) {
+            return true;
+        }
+
+        return Arrays.equals(cachedStackTrace, ((ResourceLeakException) o).cachedStackTrace);
+    }
+}
