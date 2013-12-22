4636749d3c964e21108a8c9d91ef8b1a437b09b5
Trustin Lee <t@motd.kr>
2013-12-22 19:16:49 +0900

    Use LinkedHashSet for HttpHeaders.names() and add Javadoc about the performance of names() and entries()

--- codec-http/src/main/java/io/netty/handler/codec/http/HttpHeaders.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/HttpHeaders.java
@@ -27,9 +27,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
-
-import static io.netty.handler.codec.http.HttpConstants.CR;
-import static io.netty.handler.codec.http.HttpConstants.LF;
+import static io.netty.handler.codec.http.HttpConstants.*;
 
 /**
  * Provides the constants for the standard HTTP header names and values and
@@ -1431,10 +1429,9 @@ public abstract class HttpHeaders implements Iterable<Map.Entry<String, String>>
     }
 
     /**
-     * Returns the all headers that this message contains.
-     *
-     * @return A {@link List} of the header name-value entries, which will be
-     *         empty if no pairs are found
+     * Returns a new {@link List} that contains all headers in this object.  Note that modifying the
+     * returned {@link List} will not affect the state of this object.  If you intend to enumerate over the header
+     * entries only, use {@link #iterator()} instead, which has much less overhead.
      */
     public abstract List<Map.Entry<String, String>> entries();
 
@@ -1459,9 +1456,9 @@ public abstract class HttpHeaders implements Iterable<Map.Entry<String, String>>
     public abstract boolean isEmpty();
 
     /**
-     * Gets a {@link Set} of all header names that this message contains
-     *
-     * @return A {@link Set} of all header names
+     * Returns a new {@link Set} that contains the names of all headers in this object.  Note that modifying the
+     * returned {@link Set} will not affect the state of this object.  If you intend to enumerate over the header
+     * entries only, use {@link #iterator()} instead, which has much less overhead.
      */
     public abstract Set<String> names();
 
