c8251c3f5535650e2da9b2a3e666f8a8229d6cee
Norman Maurer <nmaurer@redhat.com>
2014-01-06 16:38:03 +0100

    Allow to extend the provided tests by the testsuite
    This allows custom transport implementations to just re-use the tests provided by netty and so make sure everything works like expected.

--- testsuite/src/test/java/io/netty/testsuite/transport/socket/AbstractDatagramTest.java
+++ testsuite/src/test/java/io/netty/testsuite/transport/socket/AbstractDatagramTest.java
@@ -18,57 +18,34 @@ package io.netty.testsuite.transport.socket;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.ChannelOption;
-import io.netty.testsuite.transport.socket.SocketTestPermutation.Factory;
+import io.netty.testsuite.transport.AbstractComboTestsuiteTest;
+import io.netty.testsuite.transport.TestsuitePermutation;
 import io.netty.testsuite.util.TestUtils;
 import io.netty.util.NetUtil;
-import io.netty.util.internal.StringUtil;
-import io.netty.util.internal.logging.InternalLogger;
-import io.netty.util.internal.logging.InternalLoggerFactory;
-import org.junit.Rule;
-import org.junit.rules.TestName;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 import java.util.List;
-import java.util.Map.Entry;
 
-public abstract class AbstractDatagramTest {
+public abstract class AbstractDatagramTest extends AbstractComboTestsuiteTest<Bootstrap, Bootstrap> {
 
-    private static final List<Entry<Factory<Bootstrap>, Factory<Bootstrap>>> COMBO = SocketTestPermutation.datagram();
-    private static final List<ByteBufAllocator> ALLOCATORS = SocketTestPermutation.allocator();
-
-    @Rule
-    public final TestName testName = new TestName();
-
-    protected final InternalLogger logger = InternalLoggerFactory.getInstance(getClass());
-
-    protected volatile Bootstrap sb;
-    protected volatile Bootstrap cb;
     protected volatile InetSocketAddress addr;
 
-    protected void run() throws Throwable {
-        for (ByteBufAllocator allocator: ALLOCATORS) {
-            int i = 0;
-            for (Entry<Factory<Bootstrap>, Factory<Bootstrap>> e: COMBO) {
-                sb = e.getKey().newInstance();
-                cb = e.getValue().newInstance();
-                addr = new InetSocketAddress(NetUtil.LOCALHOST4, TestUtils.getFreePort());
-                sb.localAddress(addr);
-                sb.option(ChannelOption.ALLOCATOR, allocator);
-                cb.localAddress(0).remoteAddress(addr);
-                cb.option(ChannelOption.ALLOCATOR, allocator);
-                logger.info(String.format(
-                        "Running: %s %d of %d (%s + %s) with %s",
-                        testName.getMethodName(), ++ i, COMBO.size(), sb, cb, StringUtil.simpleClassName(allocator)));
-                try {
-                    Method m = getClass().getDeclaredMethod(
-                            TestUtils.testMethodName(testName), Bootstrap.class, Bootstrap.class);
-                    m.invoke(this, sb, cb);
-                } catch (InvocationTargetException ex) {
-                    throw ex.getCause();
-                }
-            }
-        }
+    protected AbstractDatagramTest() {
+        super(Bootstrap.class, Bootstrap.class);
+    }
+
+    @Override
+    protected List<TestsuitePermutation.BootstrapComboFactory<Bootstrap, Bootstrap>> newFactories() {
+        return SocketTestPermutation.datagram();
+    }
+
+    @Override
+    protected void configure(Bootstrap bootstrap, Bootstrap bootstrap2, ByteBufAllocator allocator) {
+        addr = new InetSocketAddress(
+                NetUtil.LOCALHOST4, TestUtils.getFreePort());
+        bootstrap.localAddress(addr);
+        bootstrap.option(ChannelOption.ALLOCATOR, allocator);
+        bootstrap2.localAddress(0).remoteAddress(addr);
+        bootstrap2.option(ChannelOption.ALLOCATOR, allocator);
     }
 }
