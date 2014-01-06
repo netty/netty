56f8479b342f51dc58171de7f7ab0030c000e960
Norman Maurer <nmaurer@redhat.com>
2014-01-06 16:38:03 +0100

    Allow to extend the provided tests by the testsuite
    This allows custom transport implementations to just re-use the tests provided by netty and so make sure everything works like expected.

---
+++ testsuite/src/test/java/io/netty/testsuite/transport/AbstractComboTestsuiteTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2014 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.testsuite.transport;
+
+import io.netty.bootstrap.AbstractBootstrap;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.testsuite.util.TestUtils;
+import io.netty.util.internal.StringUtil;
+import io.netty.util.internal.logging.InternalLogger;
+import io.netty.util.internal.logging.InternalLoggerFactory;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+public abstract class AbstractComboTestsuiteTest<SB extends AbstractBootstrap<?, ?>,
+        CB extends AbstractBootstrap<?, ?>> {
+    private final Class<SB> sbClazz;
+    private final Class<CB> cbClazz;
+    protected final InternalLogger logger = InternalLoggerFactory.getInstance(getClass());
+    protected volatile CB cb;
+    protected volatile SB sb;
+
+    protected AbstractComboTestsuiteTest(Class<SB> sbClazz, Class<CB> cbClazz) {
+        this.sbClazz = sbClazz;
+        this.cbClazz = cbClazz;
+    }
+
+    protected abstract List<TestsuitePermutation.BootstrapComboFactory<SB, CB>> newFactories();
+
+    protected List<ByteBufAllocator> newAllocators() {
+        return TestsuitePermutation.allocator();
+    }
+
+    @Rule
+    public final TestName testName = new TestName();
+
+    protected void run() throws Throwable {
+        List<TestsuitePermutation.BootstrapComboFactory<SB, CB>> combos = newFactories();
+        for (ByteBufAllocator allocator: newAllocators()) {
+            int i = 0;
+            for (TestsuitePermutation.BootstrapComboFactory<SB, CB> e: combos) {
+                sb = e.newServerInstance();
+                cb = e.newClientInstance();
+                configure(sb, cb, allocator);
+                logger.info(String.format(
+                        "Running: %s %d of %d (%s + %s) with %s",
+                        testName.getMethodName(), ++ i, combos.size(), sb, cb, StringUtil.simpleClassName(allocator)));
+                try {
+                    Method m = getClass().getMethod(
+                            TestUtils.testMethodName(testName), sbClazz, cbClazz);
+                    m.invoke(this, sb, cb);
+                } catch (InvocationTargetException ex) {
+                    throw ex.getCause();
+                }
+            }
+        }
+    }
+
+    protected abstract void configure(SB sb, CB cb, ByteBufAllocator allocator);
+}
--- testsuite/src/test/java/io/netty/testsuite/transport/socket/AbstractServerSocketTest.java
+++ testsuite/src/test/java/io/netty/testsuite/transport/socket/AbstractServerSocketTest.java
@@ -18,53 +18,33 @@ package io.netty.testsuite.transport.socket;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.ChannelOption;
-import io.netty.testsuite.transport.socket.SocketTestPermutation.Factory;
+import io.netty.testsuite.transport.AbstractTestsuiteTest;
+import io.netty.testsuite.transport.TestsuitePermutation;
 import io.netty.testsuite.util.TestUtils;
 import io.netty.util.NetUtil;
-import io.netty.util.internal.StringUtil;
-import io.netty.util.internal.logging.InternalLogger;
-import io.netty.util.internal.logging.InternalLoggerFactory;
-import org.junit.Rule;
-import org.junit.rules.TestName;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 import java.util.List;
 
-public abstract class AbstractServerSocketTest {
+public abstract class AbstractServerSocketTest extends AbstractTestsuiteTest<ServerBootstrap> {
 
-    private static final List<Factory<ServerBootstrap>> COMBO = SocketTestPermutation.serverSocket();
-    private static final List<ByteBufAllocator> ALLOCATORS = SocketTestPermutation.allocator();
-
-    @Rule
-    public final TestName testName = new TestName();
-
-    protected final InternalLogger logger = InternalLoggerFactory.getInstance(getClass());
-
-    protected volatile ServerBootstrap sb;
     protected volatile InetSocketAddress addr;
 
-    protected void run() throws Throwable {
-        for (ByteBufAllocator allocator: ALLOCATORS) {
-            int i = 0;
-            for (Factory<ServerBootstrap> e: COMBO) {
-                sb = e.newInstance();
-                addr = new InetSocketAddress(NetUtil.LOCALHOST, TestUtils.getFreePort());
-                sb.localAddress(addr);
-                sb.option(ChannelOption.ALLOCATOR, allocator);
-                sb.childOption(ChannelOption.ALLOCATOR, allocator);
+    protected AbstractServerSocketTest() {
+        super(ServerBootstrap.class);
+    }
 
-                logger.info(String.format(
-                        "Running: %s %d of %d (%s) with %s",
-                        testName.getMethodName(), ++ i, COMBO.size(), sb, StringUtil.simpleClassName(allocator)));
-                try {
-                    Method m = getClass().getDeclaredMethod(TestUtils.testMethodName(testName), ServerBootstrap.class);
-                    m.invoke(this, sb);
-                } catch (InvocationTargetException ex) {
-                    throw ex.getCause();
-                }
-            }
-        }
+    @Override
+    protected List<TestsuitePermutation.BootstrapFactory<ServerBootstrap>> newFactories() {
+        return SocketTestPermutation.serverSocket();
+    }
+
+    @Override
+    protected void configure(ServerBootstrap bootstrap, ByteBufAllocator allocator) {
+        addr = new InetSocketAddress(
+                NetUtil.LOCALHOST, TestUtils.getFreePort());
+        bootstrap.localAddress(addr);
+        bootstrap.option(ChannelOption.ALLOCATOR, allocator);
+        bootstrap.childOption(ChannelOption.ALLOCATOR, allocator);
     }
 }
