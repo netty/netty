19f9bcfe8bb8f52c1f6b8636f128c6c885c67bce
Frederic Bregier <fredbregier@free.fr>
2014-01-23 13:21:59 +0100

    Fix fileChannel not closed, preventing delete to occur correctly

--- codec-http/src/main/java/io/netty/handler/codec/http/multipart/AbstractDiskHttpData.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/multipart/AbstractDiskHttpData.java
@@ -237,6 +237,14 @@ public abstract class AbstractDiskHttpData extends AbstractHttpData {
 
     @Override
     public void delete() {
+        if (fileChannel != null) {
+            try {
+                fileChannel.force(false);
+                fileChannel.close();
+            } catch (IOException e) { //ignore
+            }
+            fileChannel = null;
+        }
         if (! isRenamed) {
             if (file != null && file.exists()) {
                 if (!file.delete()) {
