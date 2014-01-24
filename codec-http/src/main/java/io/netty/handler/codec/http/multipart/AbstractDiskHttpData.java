714ed77e178f9499aa33b72a0dbed27f4cd1028f
Trustin Lee <t@motd.kr>
2014-01-24 16:44:22 +0900

    Add a log message on deletion failure

--- codec-http/src/main/java/io/netty/handler/codec/http/multipart/AbstractDiskHttpData.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/multipart/AbstractDiskHttpData.java
@@ -241,7 +241,8 @@ public abstract class AbstractDiskHttpData extends AbstractHttpData {
             try {
                 fileChannel.force(false);
                 fileChannel.close();
-            } catch (IOException e) { //ignore
+            } catch (IOException e) {
+                logger.warn("Failed to close a file.", e);
             }
             fileChannel = null;
         }
