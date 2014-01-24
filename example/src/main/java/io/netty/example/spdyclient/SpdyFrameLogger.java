20d2fb8c2ef375579aac30c2086ae0c51aa0e905
Leonardo Freitas Gomes <leonardo.f.gomes@gmail.com>
2014-01-24 01:25:46 +0100

    SPDY client example
    Demonstrates the usage of SPDY from a client perspective. One can also
    use a SPDY-enabled browser as a client, but it’s easier to understand
    the internals of the protocol from a client point-of-view if you have
    some code you can debug.

---
+++ example/src/main/java/io/netty/example/spdyclient/SpdyFrameLogger.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2014 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.example.spdyclient;
+
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
+import io.netty.handler.codec.spdy.SpdyFrame;
+import io.netty.util.internal.logging.InternalLogLevel;
+import io.netty.util.internal.logging.InternalLogger;
+import io.netty.util.internal.logging.InternalLoggerFactory;
+
+/**
+ * Logs SPDY frames for debugging purposes.
+ */
+public class SpdyFrameLogger extends ChannelHandlerAdapter {
+
+    private enum Direction {
+        INBOUND, OUTBOUND
+    }
+
+    protected final InternalLogger logger;
+    private final InternalLogLevel level;
+
+    public SpdyFrameLogger(InternalLogLevel level) {
+        if (level == null) {
+            throw new NullPointerException("level");
+        }
+
+        this.logger = InternalLoggerFactory.getInstance(getClass());
+        this.level = level;
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (acceptMessage(msg)) {
+            log((SpdyFrame) msg, Direction.INBOUND);
+        }
+        super.channelRead(ctx, msg);
+    }
+
+    @Override
+    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+        if (acceptMessage(msg)) {
+            log((SpdyFrame) msg, Direction.OUTBOUND);
+        }
+        super.write(ctx, msg, promise);
+    }
+
+    private boolean acceptMessage(Object msg) throws Exception {
+        return msg instanceof SpdyFrame;
+    }
+
+    private void log(SpdyFrame msg, Direction d) {
+        if (logger.isEnabled(this.level)) {
+            StringBuilder b = new StringBuilder("\n----------------").append(d.name()).append("--------------------\n");
+            b.append(msg.toString());
+            b.append("\n------------------------------------");
+            logger.log(this.level, b.toString());
+        }
+    }
+}
