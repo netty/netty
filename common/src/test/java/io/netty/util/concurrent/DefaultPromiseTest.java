a921b93c336dbd4e6a40913285bbbb47139289bc
Trustin Lee <t@motd.kr>
2014-02-06 16:06:30 -0800

    Fix a race condition in DefaultPromise
    .. which occurs when a user adds a listener from different threads after the promise is done and the notifications for the listeners, that were added before the promise is done, is in progress.  For instance:
    
       Thread-1: p.addListener(listenerA);
       Thread-1: p.setSuccess(null);
       Thread-2: p.addListener(listenerB);
       Thread-2: p.executor.execute(taskNotifyListenerB);
       Thread-1: p.executor.execute(taskNotifyListenerA);
    
    taskNotifyListenerB should not really notify listenerB until taskNotifyListenerA is finished.
    
    To fix this issue:
    
    - Change the semantic of (listeners == null) to determine if the early
      listeners [1] were notified
    - If a late listener is added before the early listeners are notified,
      the notification of the late listener is deferred until the early
      listeners are notified (i.e. until listeners == null)
    - The late listeners with deferred notifications are stored in a lazily
      instantiated queue to preserve ordering, and then are notified once
      the early listeners are notified.
    
    [1] the listeners that were added before the promise is done
    [2] the listeners that were added after the promise is done

--- common/src/test/java/io/netty/util/concurrent/DefaultPromiseTest.java
+++ common/src/test/java/io/netty/util/concurrent/DefaultPromiseTest.java
@@ -18,6 +18,10 @@ package io.netty.util.concurrent;
 
 import org.junit.Test;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
@@ -73,5 +77,75 @@ public class DefaultPromiseTest {
         for (Promise<Void> a: p) {
             assertThat(a.isSuccess(), is(true));
         }
+    }
+
+    @Test
+    public void testListenerNotifyOrder() throws Exception {
+        SingleThreadEventExecutor executor =
+                new SingleThreadEventExecutor(null, Executors.defaultThreadFactory(), true) {
+                    @Override
+                    protected void run() {
+                        for (;;) {
+                            Runnable task = takeTask();
+                            if (task != null) {
+                                task.run();
+                                updateLastExecutionTime();
+                            }
+
+                            if (confirmShutdown()) {
+                                break;
+                            }
+                        }
+                    }
+                };
+
+        final BlockingQueue<FutureListener<Void>> listeners = new LinkedBlockingQueue<FutureListener<Void>>();
+        int runs = 20000;
+
+        for (int i = 0; i < runs; i++) {
+            final Promise<Void> promise = new DefaultPromise<Void>(executor);
+            final FutureListener<Void> listener1 = new FutureListener<Void>() {
+                @Override
+                public void operationComplete(Future<Void> future) throws Exception {
+                    listeners.add(this);
+                }
+            };
+            final FutureListener<Void> listener2 = new FutureListener<Void>() {
+                @Override
+                public void operationComplete(Future<Void> future) throws Exception {
+                    listeners.add(this);
+                }
+            };
+            final FutureListener<Void> listener4 = new FutureListener<Void>() {
+                @Override
+                public void operationComplete(Future<Void> future) throws Exception {
+                    listeners.add(this);
+                }
+            };
+            final FutureListener<Void> listener3 = new FutureListener<Void>() {
+                @Override
+                public void operationComplete(Future<Void> future) throws Exception {
+                    // Ensure listener4 is notified *after* this method returns to maintain the order.
+                    future.addListener(listener4);
+                    listeners.add(this);
+                }
+            };
+
+            GlobalEventExecutor.INSTANCE.execute(new Runnable() {
+                @Override
+                public void run() {
+                    promise.setSuccess(null);
+                }
+            });
+
+            promise.addListener(listener1).addListener(listener2).addListener(listener3);
+
+            assertSame("Fail during run " + i + " / " + runs, listener1, listeners.take());
+            assertSame("Fail during run " + i + " / " + runs, listener2, listeners.take());
+            assertSame("Fail during run " + i + " / " + runs, listener3, listeners.take());
+            assertSame("Fail during run " + i + " / " + runs, listener4, listeners.take());
+            assertTrue("Fail during run " + i + " / " + runs, listeners.isEmpty());
+        }
+        executor.shutdownGracefully().sync();
     }
 }
