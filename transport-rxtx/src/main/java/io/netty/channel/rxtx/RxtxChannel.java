041d37e0c8b3616dc86468bd2c3b6332638bdf1b
Trustin Lee <t@motd.kr>
2014-02-10 14:04:47 -0800

    Make most outbound operations are cancellable
    - Inspired by #2214
    - It actually reduces the chance of trying to marking a cancelled promise as success again, which raises an IllegalStateException.

--- transport-rxtx/src/main/java/io/netty/channel/rxtx/RxtxChannel.java
+++ transport-rxtx/src/main/java/io/netty/channel/rxtx/RxtxChannel.java
@@ -133,7 +133,7 @@ public class RxtxChannel extends OioByteStreamChannel {
         public void connect(
                 final SocketAddress remoteAddress,
                 final SocketAddress localAddress, final ChannelPromise promise) {
-            if (!ensureOpen(promise)) {
+            if (!promise.setUncancellable() || !ensureOpen(promise)) {
                 return;
             }
 
