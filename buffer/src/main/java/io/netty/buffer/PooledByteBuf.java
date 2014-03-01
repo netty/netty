241d24cbfae48c0fbd6c54148ba99697f899d15e
Norman Maurer <nmaurer@redhat.com>
2014-03-01 15:47:03 +0100

    Implement Thread caches for pooled buffers to minimize conditions. This fixes [#2264] and [#808].
    Motivation:
    Remove the synchronization bottleneck in PoolArena and so speed up things
    
    Modifications:
    
    This implementation uses kind of the same technics as outlined in the jemalloc paper and jemalloc
    blogpost https://www.facebook.com/notes/facebook-engineering/scalable-memory-allocation-using-jemalloc/480222803919.
    
    At the moment we only cache for "known" Threads (that powers EventExecutors) and not for others to keep the overhead
    minimal when need to free up unused buffers in the cache and free up cached buffers once the Thread completes. Here
    we use multi-level caches for tiny, small and normal allocations. Huge allocations are not cached at all to keep the
    memory usage at a sane level. All the different cache configurations can be adjusted via system properties or the constructor
    directly where it makes sense.
    
    Result:
    Less conditions as most allocations can be served by the cache itself

--- buffer/src/main/java/io/netty/buffer/PooledByteBuf.java
+++ buffer/src/main/java/io/netty/buffer/PooledByteBuf.java
@@ -31,7 +31,7 @@ abstract class PooledByteBuf<T> extends AbstractReferenceCountedByteBuf {
     protected T memory;
     protected int offset;
     protected int length;
-    private int maxLength;
+    int maxLength;
 
     private ByteBuffer tmpNioBuf;
 
@@ -142,7 +142,7 @@ abstract class PooledByteBuf<T> extends AbstractReferenceCountedByteBuf {
             final long handle = this.handle;
             this.handle = -1;
             memory = null;
-            chunk.arena.free(chunk, handle);
+            chunk.arena.free(chunk, handle, maxLength);
             recycle();
         }
     }
