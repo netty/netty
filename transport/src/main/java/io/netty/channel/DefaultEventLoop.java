999f60c80daf36454922a44927580e56920b1acb
Trustin Lee <t@motd.kr>
2014-03-24 11:39:55 +0900

    Replace LocalEventLoopGroup with DefaultEventLoopGroup
    Motivation:
    
    LocalEventLoopGroup and LocalEventLoop are not really special for LocalChannels.  It can be used for other channel implementations as long as they don't require special handling.
    
    Modifications:
    
    - Add DefaultEventLoopGroup and DefaultEventLoop
    - Deprecate LocalEventLoopGroup and make it extend DefaultEventLoopGroup
    - Add DefaultEventLoop and remove LocalEventLoop
    - Fix inspector warnings
    
    Result:
    
    - Better class names.

--- transport/src/main/java/io/netty/channel/DefaultEventLoop.java
+++ transport/src/main/java/io/netty/channel/DefaultEventLoop.java
@@ -13,15 +13,36 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package io.netty.channel.local;
+package io.netty.channel;
 
-import io.netty.channel.SingleThreadEventLoop;
+import io.netty.util.concurrent.DefaultThreadFactory;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadFactory;
 
-final class LocalEventLoop extends SingleThreadEventLoop {
+public class DefaultEventLoop extends SingleThreadEventLoop {
 
-    LocalEventLoop(LocalEventLoopGroup parent, Executor executor) {
+    public DefaultEventLoop() {
+        this((EventLoopGroup) null);
+    }
+
+    public DefaultEventLoop(ThreadFactory threadFactory) {
+        this(null, threadFactory);
+    }
+
+    public DefaultEventLoop(Executor executor) {
+        this(null, executor);
+    }
+
+    public DefaultEventLoop(EventLoopGroup parent) {
+        this(parent, new DefaultThreadFactory(DefaultEventLoop.class));
+    }
+
+    public DefaultEventLoop(EventLoopGroup parent, ThreadFactory threadFactory) {
+        super(parent, threadFactory, true);
+    }
+
+    public DefaultEventLoop(EventLoopGroup parent, Executor executor) {
         super(parent, executor, true);
     }
 
