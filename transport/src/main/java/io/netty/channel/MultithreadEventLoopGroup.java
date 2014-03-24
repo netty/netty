6d4c4d9e4b70bf34ddf72a39abe84343f96e2d63
Trustin Lee <t@motd.kr>
2014-03-24 17:05:36 +0900

    Correct the return type of MultithreadEventLoopGroup.newChild()
    Motivation:
    
    MultithreadEventLoopGroup.newChild() does not override MultithreadEventExecutorGroup.newChild() which returns EventExecutor.  MultithreadEventLoopGroup.newChild() should never return an EventExecutor, so this is incorrect.
    
    Modifications:
    
    Override MultithreadEventLoopGroup.newChild() so that it returns EventLoop
    
    Result:
    
    Correct API

--- transport/src/main/java/io/netty/channel/MultithreadEventLoopGroup.java
+++ transport/src/main/java/io/netty/channel/MultithreadEventLoopGroup.java
@@ -68,6 +68,9 @@ public abstract class MultithreadEventLoopGroup extends MultithreadEventExecutor
     }
 
     @Override
+    protected abstract EventLoop newChild(Executor executor, Object... args) throws Exception;
+
+    @Override
     public ChannelFuture register(Channel channel) {
         return next().register(channel);
     }
