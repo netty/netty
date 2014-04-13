b7989f3d3567a96cae292edca2dde46c189c646d
Jeff Pinner <jpinner@twitter.com>
2014-04-13 13:55:29 -0700

    SPDY: refactor frame codec implementation
    Motivation:
    
    Currently, the SPDY frame encoding and decoding code is based upon
    the ChannelHandler abstraction. This requires maintaining multiple
    versions for 3.x and 4.x (and possibly 5.x moving forward).
    
    Modifications:
    
    The SPDY frame encoding and decoding code is separated from the
    ChannelHandler and SpdyFrame abstractions. Also test coverage is
    improved.
    
    Result:
    
    SpdyFrameCodec now implements the ChannelHandler abstraction and is
    responsible for creating and handling SpdyFrame objects.

--- codec-http/src/main/java/io/netty/handler/codec/spdy/SpdySessionHandler.java
+++ codec-http/src/main/java/io/netty/handler/codec/spdy/SpdySessionHandler.java
@@ -42,6 +42,7 @@ public class SpdySessionHandler extends ChannelHandlerAdapter {
     private static final int DEFAULT_WINDOW_SIZE = 64 * 1024; // 64 KB default initial window size
     private int initialSendWindowSize    = DEFAULT_WINDOW_SIZE;
     private int initialReceiveWindowSize = DEFAULT_WINDOW_SIZE;
+    private volatile int initialSessionReceiveWindowSize = DEFAULT_WINDOW_SIZE;
 
     private final SpdySession spdySession = new SpdySession(initialSendWindowSize, initialReceiveWindowSize);
     private int lastGoodStreamId;
@@ -77,6 +78,19 @@ public class SpdySessionHandler extends ChannelHandlerAdapter {
         }
         this.server = server;
         minorVersion = version.getMinorVersion();
+    }
+
+    public void setSessionReceiveWindowSize(int sessionReceiveWindowSize) {
+      if (sessionReceiveWindowSize < 0) {
+        throw new IllegalArgumentException("sessionReceiveWindowSize");
+      }
+      // This will not send a window update frame immediately.
+      // If this value increases the allowed receive window size,
+      // a WINDOW_UPDATE frame will be sent when only half of the
+      // session window size remains during data frame processing.
+      // If this value decreases the allowed receive window size,
+      // the window will be reduced as data frames are processed.
+      initialSessionReceiveWindowSize = sessionReceiveWindowSize;
     }
 
     @Override
@@ -119,8 +133,8 @@ public class SpdySessionHandler extends ChannelHandlerAdapter {
             }
 
             // Send a WINDOW_UPDATE frame if less than half the session window size remains
-            if (newSessionWindowSize <= initialReceiveWindowSize / 2) {
-                int sessionDeltaWindowSize = initialReceiveWindowSize - newSessionWindowSize;
+            if (newSessionWindowSize <= initialSessionReceiveWindowSize / 2) {
+                int sessionDeltaWindowSize = initialSessionReceiveWindowSize - newSessionWindowSize;
                 spdySession.updateReceiveWindowSize(SPDY_SESSION_STREAM_ID, sessionDeltaWindowSize);
                 SpdyWindowUpdateFrame spdyWindowUpdateFrame =
                     new DefaultSpdyWindowUpdateFrame(SPDY_SESSION_STREAM_ID, sessionDeltaWindowSize);
