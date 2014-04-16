1ba5fa4b4bdd923d8db7a5adef5f59d4e6d06e61
Mousom Dhar Gupta <meetmousom@gmail.com>
2014-04-16 14:59:09 -0700

    Add MQTT protocol codec
    MQTT is a open source protocol on top of TCP which is widely used in
    mobile communication and also for IoT (Internet of Things) today. This
    will add an open source implementation of MQTT so that it becomes easier
    for Netty users to implement an MQTT application.
    
    For more information about the MQTT protocol, read this:
    
    http://public.dhe.ibm.com/software/dw/webservices/ws-mqtt/mqtt-v3r1.html

---
+++ codec-mqtt/src/main/java/io/netty/handler/codec/mqtt/MqttConnectMessage.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2014 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.netty.handler.codec.mqtt;
+
+/**
+ * See <a href="http://public.dhe.ibm.com/software/dw/webservices/ws-mqtt/mqtt-v3r1.html#connect">MQTTV3.1/connect</a>
+ */
+public final class MqttConnectMessage extends MqttMessage {
+
+    public MqttConnectMessage(
+            MqttFixedHeader mqttFixedHeader,
+            MqttConnectVariableHeader variableHeader,
+            MqttConnectPayload payload) {
+        super(mqttFixedHeader, variableHeader, payload);
+    }
+
+    @Override
+    public MqttConnectVariableHeader variableHeader() {
+        return (MqttConnectVariableHeader) super.variableHeader();
+    }
+
+    @Override
+    public MqttConnectPayload payload() {
+        return (MqttConnectPayload) super.payload();
+    }
+}
---
+++ codec-mqtt/src/main/java/io/netty/handler/codec/mqtt/MqttMessageIdVariableHeader.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2014 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.netty.handler.codec.mqtt;
+
+import io.netty.util.internal.StringUtil;
+
+/**
+ * Variable Header containing only Message Id
+ * See <a href="http://public.dhe.ibm.com/software/dw/webservices/ws-mqtt/mqtt-v3r1.html#msg-id">MQTTV3.1/msg-id</a>
+ */
+public final class MqttMessageIdVariableHeader {
+
+    private final int messageId;
+
+    public static MqttMessageIdVariableHeader from(int messageId) {
+      if (messageId < 1 || messageId > 0xffff) {
+        throw new IllegalArgumentException(
+            String.format("Message id must be in the range of 1 - 0xffff but %d given ",
+                          messageId));
+      }
+      return new MqttMessageIdVariableHeader(messageId);
+    }
+
+    private MqttMessageIdVariableHeader(int messageId) {
+        this.messageId = messageId;
+    }
+
+    public int messageId() {
+        return messageId;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder(StringUtil.simpleClassName(this)).append('[');
+        builder.append("messageId=").append(messageId);
+        builder.append(']');
+        return builder.toString();
+    }
+}
