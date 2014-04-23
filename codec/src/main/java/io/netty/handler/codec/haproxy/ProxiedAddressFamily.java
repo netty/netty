954d814aab02fe64a73bd48a9a69d77c437b22df
Jon Keys <jon.keys@gmail.com>
2014-04-22 22:25:10 -0400

    Add HAProxy protocol decoder
    Motivation:
    
    The proxy protocol provides client connection information for proxied
    network services. Several implementations exist (e.g. Haproxy, Stunnel,
    Stud, Postfix), but the primary motivation for this implementation is to
    support the proxy protocol feature of Amazon Web Services Elastic Load
    Balancing.
    
    Modifications:
    
    This commit adds a proxy protocol decoder for proxy protocol version 1
    as specified at:
    
      http://haproxy.1wt.eu/download/1.5/doc/proxy-protocol.txt
    
    The foundation for version 2 support is also in this commit but it is
    explicitly NOT supported due to a lack of external implementations to
    test against.
    
    Result:
    
    The proxy protocol decoder can be used to send client connection
    information to inbound handlers in a channel pipeline from services
    which support the proxy protocol.

---
+++ codec/src/main/java/io/netty/handler/codec/haproxy/ProxiedAddressFamily.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2014 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.codec.haproxy;
+
+/**
+ * The address family of an HAProxy proxy protocol header
+ */
+public final class ProxiedAddressFamily implements Comparable<ProxiedAddressFamily> {
+    /**
+     * The highest 4 bits of the transport protocol and address family byte contain the address family
+     */
+    private static final byte FAMILY_MASK = (byte) 0xf0;
+
+    /**
+     * Address family byte constants
+     */
+    private static final byte UNSPECIFIED_BYTE = (byte) 0x00;
+    private static final byte IPV4_BYTE = (byte) 0x10;
+    private static final byte IPV6_BYTE = (byte) 0x20;
+    private static final byte UNIX_BYTE = (byte) 0x30;
+
+    /**
+     * The UNSPECIFIED address family represents a connection which was forwarded for an unkown protocol
+     */
+    public static final ProxiedAddressFamily UNSPECIFIED = new ProxiedAddressFamily("UNSPECIFIED", UNSPECIFIED_BYTE);
+
+    /**
+     * The IPV4 address family represents a connection which was forwarded for an IPV4 client
+     */
+    public static final ProxiedAddressFamily IPV4 = new ProxiedAddressFamily("IPV4", IPV4_BYTE);
+
+    /**
+     * The IPV6 address family represents a connection which was forwarded for an IPV6 client
+     */
+    public static final ProxiedAddressFamily IPV6 = new ProxiedAddressFamily("IPV6", IPV6_BYTE);
+
+    /**
+     * The UNIX address family represents a connection which was forwarded for a unix socket
+     */
+    public static final ProxiedAddressFamily UNIX = new ProxiedAddressFamily("UNIX", UNIX_BYTE);
+
+    private final String name;
+    private final byte addressFamilyByte;
+
+    /**
+     * Creates a new instance
+     */
+    private ProxiedAddressFamily(String name, byte addressFamilyByte) {
+        this.name = name;
+        this.addressFamilyByte = addressFamilyByte;
+    }
+
+    /**
+     * Returns the {@link ProxiedAddressFamily} represented by the specified address family byte
+     *
+     * @param addressFamilyByte  address family byte
+     * @return                   {@link ProxiedAddressFamily} instance OR {@code null} if the
+     *                           address family is not recognized
+     */
+    public static ProxiedAddressFamily valueOf(byte addressFamilyByte) {
+        switch((byte) (addressFamilyByte & FAMILY_MASK)) {
+            case IPV4_BYTE:
+                return IPV4;
+            case IPV6_BYTE:
+                return IPV6;
+            case UNSPECIFIED_BYTE:
+                return UNSPECIFIED;
+            case UNIX_BYTE:
+                return UNIX;
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Returns the name of this address family
+     *
+     * @return the name of this address family
+     */
+    public String name() {
+        return name;
+    }
+
+    /**
+     * Returns the byte value of this address family
+     *
+     * @return the byte value of this address family
+     */
+    public byte byteValue() {
+        return addressFamilyByte;
+    }
+
+    @Override
+    public int hashCode() {
+        return byteValue();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ProxiedAddressFamily)) {
+            return false;
+        }
+
+        ProxiedAddressFamily that = (ProxiedAddressFamily) o;
+        return byteValue() == that.byteValue();
+    }
+
+    @Override
+    public String toString() {
+        return name();
+    }
+
+    @Override
+    public int compareTo(ProxiedAddressFamily o) {
+        return byteValue() - o.byteValue();
+    }
+}
