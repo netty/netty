d7b2affe321edeaa51c1fa7bb3df9a5badb4728a
Jon Keys <jon.keys@gmail.com>
2014-04-22 22:25:10 -0400

    Add HAProxy protocol decoder
    Motivation:
    
    The proxy protocol provides client connection information for proxied
    network services. Several implementations exist (e.g. Haproxy, Stunnel,
    Stud, Postfix), but the primary motivation for this implementation is to
    support the proxy protocol feature of Amazon Web Services Elastic Load
    Balancing.
    
    Modifications:
    
    This commit adds a proxy protocol decoder for proxy protocol version 1
    as specified at:
    
      http://haproxy.1wt.eu/download/1.5/doc/proxy-protocol.txt
    
    The foundation for version 2 support is also in this commit but it is
    explicitly NOT supported due to a lack of external implementations to
    test against.
    
    Result:
    
    The proxy protocol decoder can be used to send client connection
    information to inbound handlers in a channel pipeline from services
    which support the proxy protocol.

---
+++ codec/src/main/java/io/netty/handler/codec/haproxy/HAProxyProtocolVersion.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2014 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.codec.haproxy;
+
+/**
+ * The HAProxy proxy protocol specification version
+ */
+public final class HAProxyProtocolVersion implements Comparable<HAProxyProtocolVersion> {
+    /**
+     * The highest 4 bits of the protocol version and command byte contain the version
+     */
+    private static final byte VERSION_MASK = (byte) 0xf0;
+
+    /**
+     * Version byte constants
+     */
+    private static final byte ONE_BYTE = (byte) 0x10;
+    private static final byte TWO_BYTE = (byte) 0x20;
+
+    /**
+     * The ONE proxy protocol version represents a version 1 (human-readable) header
+     */
+    public static final HAProxyProtocolVersion ONE = new HAProxyProtocolVersion("ONE", ONE_BYTE);
+
+    /**
+     * The TWO proxy protocol version represents a version 2 (binary) header
+     */
+    public static final HAProxyProtocolVersion TWO = new HAProxyProtocolVersion("TWO", TWO_BYTE);
+
+    private final String name;
+    private final byte versionByte;
+
+    /**
+     * Creates a new instance
+     */
+    private HAProxyProtocolVersion(String name, byte versionByte) {
+        this.name = name;
+        this.versionByte = versionByte;
+    }
+
+    /**
+     * Returns the {@link HAProxyProtocolVersion} represented by the specified protocol version and command byte
+     *
+     * @param verCmdByte  protocol version and command byte
+     * @return            {@link HAProxyProtocolVersion} instance OR {@code null} if the
+     *                    version is not recognized
+     */
+    public static HAProxyProtocolVersion valueOf(byte verCmdByte) {
+        switch ((byte) (verCmdByte & VERSION_MASK)) {
+            case TWO_BYTE:
+                return TWO;
+            case ONE_BYTE:
+                return ONE;
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Returns the name of this version
+     *
+     * @return the name of this version
+     */
+    public String name() {
+        return name;
+    }
+
+    /**
+     * Returns the byte value of this version
+     *
+     * @return the byte value of this version
+     */
+    public byte byteValue() {
+        return versionByte;
+    }
+
+    @Override
+    public int hashCode() {
+        return byteValue();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof HAProxyProtocolVersion)) {
+            return false;
+        }
+
+        HAProxyProtocolVersion that = (HAProxyProtocolVersion) o;
+        return byteValue() == that.byteValue();
+    }
+
+    @Override
+    public String toString() {
+        return name();
+    }
+
+    @Override
+    public int compareTo(HAProxyProtocolVersion o) {
+        return byteValue() - o.byteValue();
+    }
+}
