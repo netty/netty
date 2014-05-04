31b01c87e4463b71fcae117ef0ddc13193ce20af
Sun Ning <nsun@avos.com>
2014-05-04 16:29:30 +0800

    Made websocket maxFramePayloadSize configurable from WebSocketServerProtocolHandler.
    Motivation:
    
    Currently there's no way to configure maxFramePayloadSize from
    WebSocketServerProtocolHandler, which is the most used entry point of
    WebSocket server.
    
    Modifications:
    
    Added another constructor for maxFramePayloadSize.
    
    Result:
    
    We can configure max frame size for websocket packet in
    WebSocketServerProtocolHandler. It will also keep backward compatibility
    with default max size: 65536. (65536 is hard-coded max size in previous
    version of Netty)

--- codec-http/src/main/java/io/netty/handler/codec/http/websocketx/WebSocketServerProtocolHandshakeHandler.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/websocketx/WebSocketServerProtocolHandshakeHandler.java
@@ -40,12 +40,14 @@ class WebSocketServerProtocolHandshakeHandler extends ChannelHandlerAdapter {
     private final String websocketPath;
     private final String subprotocols;
     private final boolean allowExtensions;
+    private final int maxFramePayloadSize;
 
     WebSocketServerProtocolHandshakeHandler(String websocketPath, String subprotocols,
-            boolean allowExtensions) {
+            boolean allowExtensions, int maxFrameSize) {
         this.websocketPath = websocketPath;
         this.subprotocols = subprotocols;
         this.allowExtensions = allowExtensions;
+        this.maxFramePayloadSize = maxFrameSize;
     }
 
     @Override
@@ -58,7 +60,8 @@ class WebSocketServerProtocolHandshakeHandler extends ChannelHandlerAdapter {
             }
 
             final WebSocketServerHandshakerFactory wsFactory = new WebSocketServerHandshakerFactory(
-                    getWebSocketLocation(ctx.pipeline(), req, websocketPath), subprotocols, allowExtensions);
+                    getWebSocketLocation(ctx.pipeline(), req, websocketPath), subprotocols,
+                            allowExtensions, maxFramePayloadSize);
             final WebSocketServerHandshaker handshaker = wsFactory.newHandshaker(req);
             if (handshaker == null) {
                 WebSocketServerHandshakerFactory.sendUnsupportedVersionResponse(ctx.channel());
