7bea542259e9e86ce9cbb432d39ad1755450077f
Trustin Lee <t@motd.kr>
2014-05-20 23:24:34 +0900

    Clean up the execution mechanism of examples
    Motivation:
    
    - There's no way to pass an argument to an example.
    - Assigning a Maven profile for each example is an overkill.
      It makes the pom.xml crowded.
    
    Modifications:
    
    - Remove example profiles from example/pom.xml
    - Keep the list of examples in run-example.sh
    - run-example.sh passes all options to exec-maven-plugin.
      For example, we can now do this:
    
        ./run-example.sh -Dssl -Dport=443 http-server
    
    Result:
    
    - It's much easier to add a new example and provide an easy way to
      launch it.
    - We can still pass an arbitrary argument to the example being launched.
      (I'll update all examples to make them get their options from system
      properties rather than from args[].

--- run-example.sh
+++ run-example.sh
@@ -1,15 +1,42 @@
 #!/bin/bash -e
-cd "`dirname "$0"`"/example
-if [[ $# -ne 1 ]]; then
-  echo "Usage: $0 <example-name>" >&2
+declare -A EXAMPLE_MAP=(
+  ['spdy-server']='io.netty.example.spdy.server.SpdyServer'
+  ['spdy-client']='io.netty.example.spdy.client.SpdyClient'
+)
+
+EXAMPLE=''
+EXAMPLE_CLASS=''
+EXAMPLE_ARGS=''
+I=0
+
+while [[ $# -gt 0 ]]; do
+  ARG="$1"
+  shift
+  if [[ "$ARG" =~ (^-.+) ]]; then
+    if [[ -z "$EXAMPLE_ARGS" ]]; then
+      EXAMPLE_ARGS="$ARG"
+    else
+      EXAMPLE_ARGS="$EXAMPLE_ARGS $ARG"
+    fi
+  else
+    EXAMPLE="$ARG"
+    EXAMPLE_CLASS="${EXAMPLE_MAP["$EXAMPLE"]}"
+    break
+  fi
+done
+
+if [[ -z "$EXAMPLE" ]] || [[ -z "$EXAMPLE_CLASS" ]] || [[ $# -ne 0 ]]; then
+  echo "  Usage: $0 [-D<name>[=<value>] ...] <example-name>" >&2
+  echo "Example: $0 -Dport=8443 -Dssl http-server" >&2
   echo >&2
   echo "Available examples:" >&2
-  grep -E '^      <id>[-a-z0-9]*</id>' pom.xml | sed -e 's#\(^.*<id>\|</id>.*$\)##g' | sed -e 's#^#  #' >&2
+  for E in "${!EXAMPLE_MAP[@]}"; do
+    echo "  $E"
+  done | sort >&2
   exit 1
 fi
 
-EXAMPLE_NAME="$1"
-
-echo "[INFO] Running: $EXAMPLE_NAME"
-mvn -X -P "$EXAMPLE_NAME" compile exec:exec
+cd "`dirname "$0"`"/example
+echo "[INFO] Running: $EXAMPLE ($EXAMPLE_CLASS $EXAMPLE_ARGS)"
+exec mvn -nsu compile exec:exec -DargLine.example="$EXAMPLE_ARGS" -DexampleClass="$EXAMPLE_CLASS"
 
