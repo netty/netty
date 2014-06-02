ce29d117ce7b16914e34dd8a77474a4092741c15
Norman Maurer <nmaurer@redhat.com>
2014-06-02 15:00:33 +0000

    DNS codec for Netty which is based on the work of [#1622].
    Motivation:
    As part of GSOC 2013 we had @mbakkar working on a DNS codec but did not integrate it yet as it needs some cleanup. This commit is based on @mbakkar's work and provide the codec for DNS.
    
    Modifications:
    Add DNS codec
    
    Result:
    Reusable DNS codec will be included in netty.
    
    This PR also includes a AsynchronousDnsResolver which allows to resolve DNS entries in a non blocking way by make use
    of the dns codec and netty transport itself.

---
+++ codec-dns/src/main/java/io/netty/handler/codec/dns/DnsQuery.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2013 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.codec.dns;
+
+import java.net.InetSocketAddress;
+
+/**
+ * A DNS query packet which is sent to a server to receive a DNS response packet
+ * with information answering a DnsQuery's questions.
+ */
+public class DnsQuery extends DnsMessage {
+
+    private final InetSocketAddress recipient;
+
+    /**
+     * Constructs a DNS query. By default recursion will be toggled on.
+     */
+    public DnsQuery(int id, InetSocketAddress recipient) {
+        super(id);
+        if (recipient == null) {
+            throw new NullPointerException("recipient");
+        }
+        this.recipient = recipient;
+    }
+
+    /**
+     * Return the {@link InetSocketAddress} of the recipient of the {@link DnsQuery}
+     */
+    public InetSocketAddress recipient() {
+        return recipient;
+    }
+
+    @Override
+    public DnsQuery addAnswer(DnsResource answer) {
+        super.addAnswer(answer);
+        return this;
+    }
+
+    @Override
+    public DnsQuery addQuestion(DnsQuestion question) {
+        super.addQuestion(question);
+        return this;
+    }
+
+    @Override
+    public DnsQuery addAuthorityResource(DnsResource resource) {
+        super.addAuthorityResource(resource);
+        return this;
+    }
+
+    @Override
+    public DnsQuery addAdditionalResource(DnsResource resource) {
+        super.addAdditionalResource(resource);
+        return this;
+    }
+
+    @Override
+    public DnsQuery touch(Object hint) {
+        super.touch(hint);
+        return this;
+    }
+
+    @Override
+    public DnsQuery retain() {
+        super.retain();
+        return this;
+    }
+
+    @Override
+    public DnsQuery retain(int increment) {
+        super.retain(increment);
+        return this;
+    }
+
+    @Override
+    public DnsQuery touch() {
+        super.touch();
+        return this;
+    }
+
+    @Override
+    public DnsQueryHeader header() {
+        return (DnsQueryHeader) super.header();
+    }
+
+    @Override
+    protected DnsQueryHeader newHeader(int id) {
+        return new DnsQueryHeader(this, id);
+    }
+}
---
+++ codec-dns/src/main/java/io/netty/handler/codec/dns/DnsQuestion.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2013 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.codec.dns;
+
+/**
+ * The DNS question class which represents a question being sent to a server via
+ * a query, or the question being duplicated and sent back in a response.
+ * Usually a message contains a single question, and DNS servers often don't
+ * support multiple questions in a single query.
+ */
+public final class DnsQuestion extends DnsEntry {
+
+    /**
+     * Constructs a question with the default class IN (Internet).
+     *
+     * @param name
+     *            the domain name being queried i.e. "www.example.com"
+     * @param type
+     *            the question type, which represents the type of
+     *            {@link DnsResource} record that should be returned
+     */
+    public DnsQuestion(String name, int type) {
+        this(name, type, CLASS_IN);
+    }
+
+    /**
+     * Constructs a question with the given class.
+     *
+     * @param name
+     *            the domain name being queried i.e. "www.example.com"
+     * @param type
+     *            the question type, which represents the type of
+     *            {@link DnsResource} record that should be returned
+     * @param qClass
+     *            the class of a DNS record
+     */
+    public DnsQuestion(String name, int type, int qClass) {
+        super(name, type, qClass);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof DnsQuestion)) {
+            return false;
+        }
+        return super.equals(other);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
