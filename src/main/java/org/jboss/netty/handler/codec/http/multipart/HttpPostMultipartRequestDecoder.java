d4c9121ad07d859e569b8591aa17852c202061e1
Frederic Bregier <fredbregier@free.fr>
2014-06-09 00:49:16 +0200

    Revert "Cumulative fixes for #2542 #2544"
    This reverts commit 0b68e19729e439b8dc4e7574b6dbaf05e447f3b2.

--- src/main/java/org/jboss/netty/handler/codec/http/multipart/HttpPostMultipartRequestDecoder.java
+++ src/main/java/org/jboss/netty/handler/codec/http/multipart/HttpPostMultipartRequestDecoder.java
@@ -817,15 +817,9 @@ public class HttpPostMultipartRequestDecoder implements InterfaceHttpPostRequest
             while (undecodedChunk.readable()) {
                 byte nextByte = undecodedChunk.readByte();
                 if (nextByte == HttpConstants.CR) {
-                    // check but do not changed readerIndex
-                    nextByte = undecodedChunk.getByte(undecodedChunk.readerIndex());
+                    nextByte = undecodedChunk.readByte();
                     if (nextByte == HttpConstants.LF) {
-                        // force read
-                        undecodedChunk.readByte();
                         return line.toString(charset);
-                    } else {
-                        // Write CR (not followed by LF)
-                        line.writeByte(HttpConstants.CR);
                     }
                 } else if (nextByte == HttpConstants.LF) {
                     return line.toString(charset);
@@ -864,10 +858,6 @@ public class HttpPostMultipartRequestDecoder implements InterfaceHttpPostRequest
                         if (nextByte == HttpConstants.LF) {
                             sao.setReadPosition(0);
                             return line.toString(charset);
-                        } else {
-                            // Write CR (not followed by LF)
-                            sao.pos--;
-                            line.writeByte(HttpConstants.CR);
                         }
                     } else {
                         line.writeByte(nextByte);
@@ -1020,11 +1010,6 @@ public class HttpPostMultipartRequestDecoder implements InterfaceHttpPostRequest
                         if (nextByte == HttpConstants.LF) {
                             sao.setReadPosition(0);
                             return sb.toString();
-                        } else {
-                            // error CR without LF
-                            // delimiter not found so break here !
-                            undecodedChunk.readerIndex(readerIndex);
-                            throw new NotEnoughDataDecoderException();
                         }
                     } else {
                         // error since CR must be followed by LF
@@ -1052,11 +1037,6 @@ public class HttpPostMultipartRequestDecoder implements InterfaceHttpPostRequest
                                         if (nextByte == HttpConstants.LF) {
                                             sao.setReadPosition(0);
                                             return sb.toString();
-                                        } else {
-                                            // error CR without LF
-                                            // delimiter not found so break here !
-                                            undecodedChunk.readerIndex(readerIndex);
-                                            throw new NotEnoughDataDecoderException();
                                         }
                                     } else {
                                         // error CR without LF
@@ -1348,13 +1328,7 @@ public class HttpPostMultipartRequestDecoder implements InterfaceHttpPostRequest
                                     newLine = true;
                                     index = 0;
                                     lastPosition = undecodedChunk.readerIndex() - 2;
-                                } else {
-                                    // Unread second nextByte
-                                    lastPosition = undecodedChunk.readerIndex() - 1;
-                                    undecodedChunk.readerIndex(lastPosition);
                                 }
-                            } else {
-                                lastPosition = undecodedChunk.readerIndex() - 1;
                             }
                         } else if (nextByte == HttpConstants.LF) {
                             newLine = true;
@@ -1373,13 +1347,7 @@ public class HttpPostMultipartRequestDecoder implements InterfaceHttpPostRequest
                                 newLine = true;
                                 index = 0;
                                 lastPosition = undecodedChunk.readerIndex() - 2;
-                            } else {
-                                // Unread second nextByte
-                                lastPosition = undecodedChunk.readerIndex() - 1;
-                                undecodedChunk.readerIndex(lastPosition);
                             }
-                        } else {
-                            lastPosition = undecodedChunk.readerIndex() - 1;
                         }
                     } else if (nextByte == HttpConstants.LF) {
                         newLine = true;
@@ -1464,10 +1432,6 @@ public class HttpPostMultipartRequestDecoder implements InterfaceHttpPostRequest
                                     newLine = true;
                                     index = 0;
                                     lastrealpos = sao.pos - 2;
-                                } else {
-                                    // Unread last nextByte
-                                    sao.pos--;
-                                    lastrealpos = sao.pos;
                                 }
                             }
                         } else if (nextByte == HttpConstants.LF) {
@@ -1487,10 +1451,6 @@ public class HttpPostMultipartRequestDecoder implements InterfaceHttpPostRequest
                                 newLine = true;
                                 index = 0;
                                 lastrealpos = sao.pos - 2;
-                            } else {
-                                // Unread last nextByte
-                                sao.pos--;
-                                lastrealpos = sao.pos;
                             }
                         }
                     } else if (nextByte == HttpConstants.LF) {
