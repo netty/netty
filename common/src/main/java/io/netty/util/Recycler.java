84affd4c0d021d652de7ebd3b8907fd4c7fa2d79
Norman Maurer <nmaurer@redhat.com>
2014-06-13 11:56:35 +0200

    FastThreadLocal and faster Recycler.recycle()

--- common/src/main/java/io/netty/util/Recycler.java
+++ common/src/main/java/io/netty/util/Recycler.java
@@ -16,12 +16,21 @@
 
 package io.netty.util;
 
+<<<<<<< HEAD
 import io.netty.util.internal.SystemPropertyUtil;
 import io.netty.util.internal.logging.InternalLogger;
 import io.netty.util.internal.logging.InternalLoggerFactory;
 
 import java.util.IdentityHashMap;
+=======
+import java.lang.ref.WeakReference;
+import java.util.Arrays;
+>>>>>>> b5ceccf... FastThreadLocal and faster Recycler.recycle()
 import java.util.Map;
+import java.util.WeakHashMap;
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+
+import io.netty.util.concurrent.FastThreadLocal;
 
 import io.netty.util.internal.FastThreadLocal;
 
@@ -32,6 +41,7 @@ import io.netty.util.internal.FastThreadLocal;
  */
 public abstract class Recycler<T> {
 
+<<<<<<< HEAD
     private static final InternalLogger logger = InternalLoggerFactory.getInstance(Recycler.class);
 
     private static final int DEFAULT_MAX_CAPACITY;
@@ -62,6 +72,19 @@ public abstract class Recycler<T> {
             return new Stack<T>(Recycler.this, Thread.currentThread(), maxCapacity);
         }
     };
+=======
+    private final FastThreadLocal<Stack<T>> threadLocal;
+
+    public Recycler(FastThreadLocal.Type type) {
+        super();
+        threadLocal = new FastThreadLocal<Stack<T>>(type) {
+            @Override
+            protected Stack<T> initialValue() {
+                return new Stack<T>(Recycler.this, Thread.currentThread());
+            }
+        };
+    }
+>>>>>>> b5ceccf... FastThreadLocal and faster Recycler.recycle()
 
     protected Recycler() {
         this(DEFAULT_MAX_CAPACITY);
@@ -73,33 +96,149 @@ public abstract class Recycler<T> {
 
     public final T get() {
         Stack<T> stack = threadLocal.get();
-        T o = stack.pop();
-        if (o == null) {
-            o = newObject(stack);
+        Handle handle = stack.pop();
+        if (handle == null) {
+            handle = stack.newHandle();
+            handle.value = newObject(handle);
         }
-        return o;
-    }
-
-    public final boolean recycle(T o, Handle handle) {
-        @SuppressWarnings("unchecked")
-        Stack<T> stack = (Stack<T>) handle;
-        if (stack.parent != this) {
-            return false;
-        }
-
-        if (Thread.currentThread() != stack.thread) {
-            return false;
-        }
-
-        stack.push(o);
-        return true;
+        return (T) handle.value;
     }
 
     protected abstract T newObject(Handle handle);
 
-    public interface Handle { }
+    public static final class Handle {
+        private final int id;
+        private Stack stack;
+        private Object value;
+        Handle(int id, Stack stack) {
+            this.id = id;
+            this.stack = stack;
+        }
+        public void recycle() {
+            Thread thread = Thread.currentThread();
+            if (thread == stack.thread) {
+                stack.push(this);
+                return;
+            }
 
-    static final class Stack<T> implements Handle {
+            // we don't want to have a ref to the queue as the value in our weak map
+            // so we null it out; to ensure there are no races with restoring it later
+            // we impose a memory ordering here (no-op on x86)
+            Map<Stack<?>, WeakOrderQueue> delayedRecycled = DELAYED_RECYCLED.get(thread);
+            WeakOrderQueue queue = delayedRecycled.get(stack);
+            if (queue == null) {
+                delayedRecycled.put(stack, queue = new WeakOrderQueue(stack));
+            }
+            queue.add(this);
+        }
+    }
+
+    static final FastThreadLocal<Map<Stack<?>, WeakOrderQueue>> DELAYED_RECYCLED
+    = new FastThreadLocal<Map<Stack<?>, WeakOrderQueue>>(FastThreadLocal.Type.Recycler_DelayedRecyle) {
+        protected Map<Stack<?>, WeakOrderQueue> initialValue() {
+            return new WeakHashMap<Stack<?>, WeakOrderQueue>();
+        }
+    };
+
+    // a queue that makes only moderate guarantees about visibility: items are seen in the correct order,
+    // but we aren't absolutely guaranteed to ever see anything at all, thereby keeping the queue cheap to maintain
+    private static final class WeakOrderQueue {
+        private static final int LINK_CAPACITY = 16;
+        private static final class Link {
+            private int readIndex;
+            private volatile int writeIndex;
+            private final Handle[] elements = new Handle[LINK_CAPACITY];
+            private Link next;
+
+            private static final AtomicIntegerFieldUpdater<Link> writeIndexUpdater
+            = AtomicIntegerFieldUpdater.newUpdater(Link.class, "writeIndex");
+        }
+
+        // chain of data items
+        private Link head, tail;
+        // pointer to another queue of delayed items for the same stack
+        private WeakOrderQueue next;
+        private final WeakReference<Thread> owner;
+
+        public WeakOrderQueue(Stack<?> stack) {
+            head = tail = new Link();
+            owner = new WeakReference<Thread>(Thread.currentThread());
+            synchronized (stack) {
+                this.next = stack.head;
+                stack.head = this;
+            }
+        }
+
+        void add(Handle handle) {
+            Link tail = this.tail;
+            int writeIndex;
+            if ((writeIndex = tail.writeIndex) == LINK_CAPACITY) {
+                this.tail = tail = tail.next = new Link();
+                writeIndex = tail.writeIndex;
+            }
+            tail.elements[writeIndex] = handle;
+            handle.stack = null;
+            // we lazy set to ensure that setting stack to null appears before we unnull it in the owning thread;
+            // this also means we guarantee visibility of an element in the queue if we see the index updated
+            Link.writeIndexUpdater.lazySet(tail, writeIndex + 1);
+        }
+
+        boolean hasFinalData() {
+            return tail.readIndex != tail.writeIndex;
+        }
+
+        // transfer as many items as we can from this queue to the stack, returning true if any were transferred
+        boolean transfer(Stack<?> to) {
+
+            Link head = this.head;
+            if (head == null) {
+                return false;
+            }
+
+            if (head.readIndex == LINK_CAPACITY) {
+                if (head.next == null) {
+                    return false;
+                }
+                this.head = head = head.next;
+            }
+
+            int start = head.readIndex;
+            int end = head.writeIndex;
+            if (start == end) {
+                return false;
+            }
+
+            int count = end - start;
+            if (to.size + count > to.elements.length) {
+                to.elements = Arrays.copyOf(to.elements, (to.size + count) * 2);
+            }
+
+            BitSet present = to.present;
+            Handle[] src = head.elements;
+            Handle[] trg = to.elements;
+            int size = to.size;
+            while (start < end) {
+                Handle element = src[start];
+                if (!present.set(element.id)) {
+                    throw new IllegalStateException("recycled already");
+                }
+                element.stack = to;
+                trg[size++] = element;
+                src[start++] = null;
+            }
+            to.size = size;
+            to.scavenged = true;
+
+            if (end == LINK_CAPACITY & head.next != null) {
+                this.head = head.next;
+            }
+
+            head.readIndex = end;
+            return true;
+        }
+    }
+
+    static final class Stack<T> {
 
         private T[] elements;
         private int size;
@@ -107,13 +246,30 @@ public abstract class Recycler<T> {
 
         private final Map<T, Boolean> map;
 
+        // we keep a queue of per-thread queues, which is appended to once only, each time a new thread other
+        // than the stack owner recycles: when we run out of items in our stack we iterate this collection
+        // to scavenge those that can be reused. this permits us to incur minimal thread synchronisation whilst
+        // still recycling all items; more importantly it permits us to use a BitSet to track ids, as we can
+        // be certain that, under correct usage, the id domain does not grow unbounded
+        private int maxId;
         final Recycler<T> parent;
         final Thread thread;
+<<<<<<< HEAD
+=======
+        private Handle[] elements;
+        private final BitSet present = new BitSet(256);
+        private int size;
+
+        private volatile WeakOrderQueue head;
+        private WeakOrderQueue cursor, prev;
+        private boolean scavenged;
+>>>>>>> b5ceccf... FastThreadLocal and faster Recycler.recycle()
 
         @SuppressWarnings("AssertWithSideEffects")
         Stack(Recycler<T> parent, Thread thread, int maxCapacity) {
             this.parent = parent;
             this.thread = thread;
+<<<<<<< HEAD
             this.maxCapacity = maxCapacity;
             elements = newArray(INITIAL_CAPACITY);
 
@@ -127,21 +283,33 @@ public abstract class Recycler<T> {
             } else {
                 map = null;
             }
+=======
+            elements = new Handle[INITIAL_CAPACITY];
+>>>>>>> b5ceccf... FastThreadLocal and faster Recycler.recycle()
         }
 
-        T pop() {
+        Handle pop() {
             int size = this.size;
             if (size == 0) {
-                return null;
+                if (!scavenge()) {
+                    return null;
+                }
+                size = this.size;
             }
             size --;
+<<<<<<< HEAD
             T ret = elements[size];
             elements[size] = null;
             assert map == null || map.remove(ret) != null;
+=======
+            Handle ret = elements[size];
+            present.clear(ret.id);
+>>>>>>> b5ceccf... FastThreadLocal and faster Recycler.recycle()
             this.size = size;
             return ret;
         }
 
+<<<<<<< HEAD
         void push(T o) {
             assert map == null || map.put(o, Boolean.TRUE) == null: "recycled already";
 
@@ -155,15 +323,115 @@ public abstract class Recycler<T> {
                 T[] newElements = newArray(Math.min(maxCapacity, size << 1));
                 System.arraycopy(elements, 0, newElements, 0, size);
                 elements = newElements;
+=======
+        boolean scavenge() {
+            // continue an existing scavenge, if any
+            if (scavengeSome()) {
+                return true;
             }
 
-            elements[size] = o;
+            if (scavenged) {
+                // if we scavenged anything last loop, try once more before increasing our pool size
+                scavenged = false;
+                prev = null;
+                cursor = head;
+                if (scavengeSome()) {
+                    return true;
+                }
+            }
+
+            // maybe expand our id domain / pool size
+            if (maxId == present.size()) {
+                present.ensureCapacity(maxId * 2);
+            }
+
+            // reset our scavenge cursor
+            prev = null;
+            cursor = head;
+            return false;
+        }
+
+        boolean scavengeSome() {
+            boolean success = false;
+            WeakOrderQueue cursor = this.cursor, prev = this.prev;
+            while (cursor != null) {
+                if (cursor.transfer(this)) {
+                    success = true;
+                    break;
+                }
+                WeakOrderQueue next = cursor.next;
+                if (cursor.owner.get() == null) {
+                    // if the thread associated with the queue is gone, unlink it, after
+                    // performing a volatile read to confirm there is no data left to collect
+                    // we never unlink the first queue, as we don't want to synchronize on updating the head
+                    if (cursor.hasFinalData()) {
+                        while (true) {
+                            if (!cursor.transfer(this)) {
+                                break;
+                            }
+                        }
+                    }
+                    if (prev != null) {
+                        prev.next = next;
+                    }
+                } else {
+                    prev = cursor;
+                }
+                cursor = next;
+            }
+            this.prev = prev;
+            this.cursor = cursor;
+            return success;
+        }
+
+        void push(Handle item) {
+            if (!present.set(item.id)) {
+                throw new IllegalStateException("recycled already");
+            }
+
+            int size = this.size;
+            if (size == elements.length) {
+                elements = Arrays.copyOf(elements, size << 1);
+>>>>>>> b5ceccf... FastThreadLocal and faster Recycler.recycle()
+            }
+
+            elements[size] = item;
             this.size = size + 1;
         }
 
-        @SuppressWarnings({ "unchecked", "SuspiciousArrayCast" })
-        private static <T> T[] newArray(int length) {
-            return (T[]) new Object[length];
+        Handle newHandle() {
+            return new Handle(maxId++, this);
+        }
+    }
+
+    // a simpler bitset than the java.util.BitSet, which performs unnecessary work
+    private static final class BitSet {
+        private long[] bits;
+
+        BitSet(int initialCapacity) {
+            assert (initialCapacity & 63) == 0;
+            bits = new long[initialCapacity >>> 6];
+        }
+
+        boolean set(int index) {
+            int bucket = index >>> 6;
+            long bit = 1L << (index & 63);
+            long prev = bits[bucket];
+            bits[bucket] = prev | bit;
+            return (prev & bit) == 0;
+        }
+
+        void clear(int index) {
+            bits[index >>> 6] &= ~(1L << (index & 63));
+        }
+
+        int size() {
+            return bits.length << 6;
+        }
+
+        void ensureCapacity(int newCapacity) {
+            assert (newCapacity & 63) == 0;
+            bits = Arrays.copyOf(bits, newCapacity);
         }
     }
 }
