3506dedbded071f6930bf4553a9c48ffb2737d2a
Trustin Lee <t@motd.kr>
2014-06-17 15:59:11 +0900

    Fix incorrect method signature of awaitInactivity()
    - Related: #2084

--- common/src/main/java/io/netty/util/ThreadDeathWatcher.java
+++ common/src/main/java/io/netty/util/ThreadDeathWatcher.java
@@ -85,7 +85,7 @@ public final class ThreadDeathWatcher {
      *
      * @return {@code true} if and only if the watcher thread has been terminated
      */
-    public boolean awaitInactivity(long timeout, TimeUnit unit) throws InterruptedException {
+    public static boolean awaitInactivity(long timeout, TimeUnit unit) throws InterruptedException {
         if (unit == null) {
             throw new NullPointerException("unit");
         }
