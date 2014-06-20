81e5f1ad4665d3591b512dc3b6392eb547baeb65
Norman Maurer <nmaurer@redhat.com>
2014-06-20 20:03:22 +0200

    [#2589] LocalServerChannel.doClose() throws NPE when localAddress == null
    Motivation:
    
    LocalServerChannel.doClose() calls LocalChannelRegistry.unregister(localAddress); without check if localAddress is null and so produce a NPE when pass null the used ConcurrentHashMapV8
    
    Modification:
    Check for localAddress != null before try to remove it from Map. Also added a unit test which showed the stacktrace of the error.
    
    Result:
    
    No more NPE during doClose().

--- transport/src/test/java/io/netty/bootstrap/BootstrapTest.java
+++ transport/src/test/java/io/netty/bootstrap/BootstrapTest.java
@@ -27,7 +27,6 @@ import io.netty.channel.ChannelPromise;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.local.LocalAddress;
 import io.netty.channel.local.LocalChannel;
-import io.netty.channel.local.LocalEventLoopGroup;
 import io.netty.channel.local.LocalServerChannel;
 import io.netty.util.concurrent.Future;
 import org.junit.Assert;
@@ -156,6 +155,32 @@ public class BootstrapTest {
                 }
             });
             Assert.assertTrue(queue.take());
+        } finally {
+            group.shutdownGracefully();
+            group.terminationFuture().sync();
+        }
+    }
+
+    @Test
+    public void testLateRegisterFailed() throws Exception {
+        final TestEventLoopGroup group = new TestEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(group);
+            bootstrap.channel(LocalServerChannel.class);
+            bootstrap.childHandler(new DummyHandler());
+            bootstrap.localAddress(new LocalAddress("1"));
+            ChannelFuture future = bootstrap.bind();
+            Assert.assertFalse(future.isDone());
+            group.promise.setFailure(new IllegalStateException());
+            final BlockingQueue<Boolean> queue = new LinkedBlockingQueue<Boolean>();
+            future.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    queue.add(group.next().inEventLoop(Thread.currentThread()));
+                }
+            });
+            Assert.assertFalse(queue.take());
         } finally {
             group.shutdownGracefully();
             group.terminationFuture().sync();
