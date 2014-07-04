fee73fb25d1ca6f48030519a746bc296622c9023
Daniel Bevenius <daniel.bevenius@gmail.com>
2014-07-04 09:57:50 +0200

    Adding payload to the Http2Client example.
    Modifications:
    When trying out the Http2Client example I noticed that adding a request
    payload would not cause a data frame to written. This seems to be
    because writeHeaders completes the promise, and then the writeData
    call ends up in FlowControlWriter.writeFrame, isDone is true and
    the data released and the call aborted and returned.
    Adding a new promise for the writeData method allows a data frame to be
    written.
    
    Result:
    A body/payload can now be sent to the server. The example was updated to
    simply echo the payload received back to the calling client.

--- example/src/main/java/io/netty/example/http2/client/Http2Client.java
+++ example/src/main/java/io/netty/example/http2/client/Http2Client.java
@@ -27,6 +27,7 @@ import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http2.Http2OrHttpChooser.SelectedProtocol;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import io.netty.util.CharsetUtil;
 
 import java.util.Arrays;
 
@@ -77,8 +78,9 @@ public final class Http2Client {
             http2ConnectionHandler.awaitInitialization();
 
             // Create a simple GET request with just headers.
-            FullHttpRequest request = new DefaultFullHttpRequest(HTTP_1_1, GET, "/whatever");
+            FullHttpRequest request = new DefaultFullHttpRequest(HTTP_1_1, POST, "/whatever");
             request.headers().add(HttpHeaders.Names.HOST, HOST + ':' + PORT);
+            request.content().writeBytes("sample data".getBytes(CharsetUtil.UTF_8));
 
             // Send the request to the server.
             System.err.println("Sending request...");
