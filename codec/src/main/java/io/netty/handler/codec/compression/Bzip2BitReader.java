3e7789bd30568faab4e2f0514f675a3503561cb0
Idel Pivnitskiy <Idel.Pivnitskiy@gmail.com>
2014-07-25 23:29:35 +0400

    Improve Bzip2BitReader/Writer
    Motivation:
    
    Before this changes Bzip2BitReader and Bzip2BitWriter accessed to ByteBuf byte by byte. So tests for Bzip2 compression codec takes a lot of time if we ran them with paranoid level of resource leak detection. For more information see comments to #2681 and #2689.
    
    Modifications:
    
    - Increased size of bit buffers from 8 to 64 bits.
    - Improved reading and writing operations.
    - Save link to incoming ByteBuf inside Bzip2BitReader.
    - Added methods to check possible readable bits and bytes in Bzip2BitReader.
    - Updated Bzip2 classes to use new API of Bzip2BitReader.
    - Added new constants to Bzip2Constants.
    
    Result:
    
    Increased size of bit buffers and improved performance of Bzip2 compression codec (for general work by 13% and for tests with paranoid level of resource leak detection by 55%).

--- codec/src/main/java/io/netty/handler/codec/compression/Bzip2BitReader.java
+++ codec/src/main/java/io/netty/handler/codec/compression/Bzip2BitReader.java
@@ -19,14 +19,24 @@ import io.netty.buffer.ByteBuf;
 
 /**
  * An bit reader that allows the reading of single bit booleans, bit strings of
- * arbitrary length (up to 24 bits), and bit aligned 32-bit integers. A single byte
+ * arbitrary length (up to 32 bits), and bit aligned 32-bit integers. A single byte
  * at a time is read from the {@link ByteBuf} when more bits are required.
  */
 class Bzip2BitReader {
     /**
+     * Maximum count of possible readable bytes to check.
+     */
+    private static final int MAX_COUNT_OF_READABLE_BYTES = Integer.MAX_VALUE >>> 3;
+
+    /**
+     * The {@link ByteBuf} from which to read data.
+     */
+    private ByteBuf in;
+
+    /**
      * A buffer of bits read from the input stream that have not yet been returned.
      */
-    private int bitBuffer;
+    private long bitBuffer;
 
     /**
      * The number of bits currently buffered in {@link #bitBuffer}.
@@ -34,53 +44,114 @@ class Bzip2BitReader {
     private int bitCount;
 
     /**
-     * Reads up to 24 bits from the {@link ByteBuf}.
-     * @param count The number of bits to read (maximum {@code 24}, because the {@link #bitBuffer}
-     *              is {@code int} and it can store up to {@code 8} bits before calling)
+     * Set the {@link ByteBuf} from which to read data.
+     */
+    void setByteBuf(ByteBuf in) {
+        this.in = in;
+    }
+
+    /**
+     * Reads up to 32 bits from the {@link ByteBuf}.
+     * @param count The number of bits to read (maximum {@code 32} as a size of {@code int})
      * @return The bits requested, right-aligned within the integer
      */
-    int readBits(ByteBuf in, final int count) {
-        if (count < 0 || count > 24) {
-            throw new IllegalArgumentException("count: " + count + " (expected: 0-24)");
+    int readBits(final int count) {
+        if (count < 0 || count > 32) {
+            throw new IllegalArgumentException("count: " + count + " (expected: 0-32 )");
         }
         int bitCount = this.bitCount;
-        int bitBuffer = this.bitBuffer;
+        long bitBuffer = this.bitBuffer;
 
         if (bitCount < count) {
-            do {
-                int uByte = in.readUnsignedByte();
-                bitBuffer = bitBuffer << 8 | uByte;
-                bitCount += 8;
-            } while (bitCount < count);
+            long readData;
+            int offset;
+            switch (in.readableBytes()) {
+                case 1: {
+                    readData = in.readUnsignedByte();
+                    offset = 8;
+                    break;
+                }
+                case 2: {
+                    readData = in.readUnsignedShort();
+                    offset = 16;
+                    break;
+                }
+                case 3: {
+                    readData = in.readUnsignedMedium();
+                    offset = 24;
+                    break;
+                }
+                default: {
+                    readData = in.readUnsignedInt();
+                    offset = 32;
+                    break;
+                }
+            }
 
+            bitBuffer = bitBuffer << offset | readData;
+            bitCount += offset;
             this.bitBuffer = bitBuffer;
         }
 
         this.bitCount = bitCount -= count;
-        return (bitBuffer >>> bitCount) & ((1 << count) - 1);
+        return (int) (bitBuffer >>> bitCount & (count != 32 ? (1 << count) - 1 : 0xFFFFFFFFL));
     }
 
     /**
      * Reads a single bit from the {@link ByteBuf}.
      * @return {@code true} if the bit read was {@code 1}, otherwise {@code false}
      */
-    boolean readBoolean(ByteBuf in) {
-        return readBits(in, 1) != 0;
+    boolean readBoolean() {
+        return readBits(1) != 0;
     }
 
     /**
      * Reads 32 bits of input as an integer.
      * @return The integer read
      */
-    int readInt(ByteBuf in) {
-        return readBits(in, 16) << 16 | readBits(in, 16);
+    int readInt() {
+        return readBits(32);
+    }
+
+    /**
+     * Refill the {@link ByteBuf} by one byte.
+     */
+    void refill() {
+        int readData = in.readUnsignedByte();
+        bitBuffer = bitBuffer << 8 | readData;
+        bitCount += 8;
     }
 
     /**
      * Checks that at least one bit is available for reading.
      * @return {@code true} if one bit is available for reading, otherwise {@code false}
      */
-    boolean hasBit(ByteBuf in) {
+    boolean isReadable() {
         return bitCount > 0 || in.isReadable();
+    }
+
+    /**
+     * Checks that the specified number of bits available for reading.
+     * @param count The number of bits to check
+     * @return {@code true} if {@code count} bits are available for reading, otherwise {@code false}
+     */
+    boolean hasReadableBits(int count) {
+        if (count < 0) {
+            throw new IllegalArgumentException("count: " + count + " (expected value greater than 0)");
+        }
+        return bitCount >= count || (in.readableBytes() << 3 & Integer.MAX_VALUE) >= count - bitCount;
+    }
+
+    /**
+     * Checks that the specified number of bytes available for reading.
+     * @param count The number of bytes to check
+     * @return {@code true} if {@code count} bytes are available for reading, otherwise {@code false}
+     */
+    boolean hasReadableBytes(int count) {
+        if (count < 0 || count > MAX_COUNT_OF_READABLE_BYTES) {
+            throw new IllegalArgumentException("count: " + count
+                    + " (expected: 0-" + MAX_COUNT_OF_READABLE_BYTES + ')');
+        }
+        return hasReadableBits(count << 3);
     }
 }
