bac833a1e203339cd037d04f1016fee3d65a31a9
Norman Maurer <nmaurer@redhat.com>
2014-08-25 07:36:03 +0200

    Move duplicated code into CodecUtil
    Motivation:
    
    We have some duplicated code that can be reused.
    
    Modifications:
    
    Create package private class called CodecUtil that now contains the shared code / helper method.
    
    Result:
    
    Less code-duplication

--- codec/src/main/java/io/netty/handler/codec/ByteToMessageDecoder.java
+++ codec/src/main/java/io/netty/handler/codec/ByteToMessageDecoder.java
@@ -52,9 +52,7 @@ public abstract class ByteToMessageDecoder extends ChannelInboundHandlerAdapter 
     private boolean first;
 
     protected ByteToMessageDecoder() {
-        if (isSharable()) {
-            throw new IllegalStateException("@Sharable annotation is not allowed");
-        }
+        CodecUtil.ensureNotSharable(this);
     }
 
     /**
