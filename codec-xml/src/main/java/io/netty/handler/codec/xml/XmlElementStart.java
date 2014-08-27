d5207bc35c6484ecf73ce68f3673b21fe360ad8c
Marek Jelen <marek@jelen.biz>
2014-08-27 11:46:51 +0200

    Integrate non-blocking XML parser as Netty codec (#2806)
    Motivation:
    Provide non-blocking XML parser as Netty codec.
    
    Modifications:
    New codec implemented/extracted.
    
    io.netty.handler.codec.xml.XmlDecoder decodes XML fed by Netty without blocking.
    
    Result:
    Non-blocking XML stream parsing.

---
+++ codec-xml/src/main/java/io/netty/handler/codec/xml/XmlElementStart.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2014 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.codec.xml;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Specific {@link XmlElement} representing beginning  of element.
+ */
+public class XmlElementStart extends XmlElement {
+
+    private final List<XmlAttribute> attributes = new LinkedList<XmlAttribute>();
+
+    public XmlElementStart(String name, String namespace, String prefix) {
+        super(name, namespace, prefix);
+    }
+
+    public List<XmlAttribute> attributes() {
+        return attributes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) { return true; }
+        if (o == null || getClass() != o.getClass()) { return false; }
+        if (!super.equals(o)) { return false; }
+
+        XmlElementStart that = (XmlElementStart) o;
+
+        if (attributes != null ? !attributes.equals(that.attributes) : that.attributes != null) { return false; }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (attributes != null ? attributes.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "XmlElementStart{" +
+                "attributes=" + attributes +
+                super.toString() +
+                "} ";
+    }
+
+}
