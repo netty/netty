c187929e192ea7baac603c56981ea916615a8cf8
Marek Jelen <marek@jelen.biz>
2014-08-27 11:46:51 +0200

    Integrate non-blocking XML parser as Netty codec (#2806)
    Motivation:
    Provide non-blocking XML parser as Netty codec.
    
    Modifications:
    New codec implemented/extracted.
    
    io.netty.handler.codec.xml.XmlDecoder decodes XML fed by Netty without blocking.
    
    Result:
    Non-blocking XML stream parsing.

---
+++ codec-xml/src/main/java/io/netty/handler/codec/xml/XmlDecoder.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2014 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.codec.xml;
+
+import com.fasterxml.aalto.AsyncInputFeeder;
+import com.fasterxml.aalto.AsyncXMLInputFactory;
+import com.fasterxml.aalto.AsyncXMLStreamReader;
+import com.fasterxml.aalto.stax.InputFactoryImpl;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import java.util.List;
+
+/**
+ * Async XML decoder based on <a href="https://github.com/FasterXML/aalto-xml">Aalto XML parser</a>.
+ *
+ * Parses the incoming data into one of XML messages defined in this package.
+ */
+
+public class XmlDecoder extends ByteToMessageDecoder {
+
+    private static final AsyncXMLInputFactory XML_INPUT_FACTORY = new InputFactoryImpl();
+    private static final XmlDocumentEnd XML_DOCUMENT_END = XmlDocumentEnd.INSTANCE;
+
+    private final AsyncXMLStreamReader streamReader = XML_INPUT_FACTORY.createAsyncXMLStreamReader();
+    private final AsyncInputFeeder streamFeeder = streamReader.getInputFeeder();
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        byte[] buffer = new byte[in.readableBytes()];
+        in.readBytes(buffer);
+        try {
+            streamFeeder.feedInput(buffer, 0, buffer.length);
+        } catch (XMLStreamException exception) {
+            in.skipBytes(in.readableBytes());
+            throw exception;
+        }
+
+        while (!streamFeeder.needMoreInput()) {
+            int type = streamReader.next();
+            switch (type) {
+                case XMLStreamConstants.START_DOCUMENT:
+                    out.add(new XmlDocumentStart(streamReader.getEncoding(), streamReader.getVersion(),
+                            streamReader.isStandalone(), streamReader.getCharacterEncodingScheme()));
+                    break;
+                case XMLStreamConstants.END_DOCUMENT:
+                    out.add(XML_DOCUMENT_END);
+                    break;
+                case XMLStreamConstants.START_ELEMENT:
+                    XmlElementStart elementStart = new XmlElementStart(streamReader.getLocalName(),
+                            streamReader.getName().getNamespaceURI(), streamReader.getPrefix());
+                    for (int x = 0; x < streamReader.getAttributeCount(); x++) {
+                        XmlAttribute attribute = new XmlAttribute(streamReader.getAttributeType(x),
+                                streamReader.getAttributeLocalName(x), streamReader.getAttributePrefix(x),
+                                streamReader.getAttributeNamespace(x), streamReader.getAttributeValue(x));
+                        elementStart.attributes().add(attribute);
+                    }
+                    for (int x = 0; x < streamReader.getNamespaceCount(); x++) {
+                        XmlNamespace namespace = new XmlNamespace(streamReader.getNamespacePrefix(x),
+                                streamReader.getNamespaceURI(x));
+                        elementStart.namespaces().add(namespace);
+                    }
+                    out.add(elementStart);
+                    break;
+                case XMLStreamConstants.END_ELEMENT:
+                    XmlElementEnd elementEnd = new XmlElementEnd(streamReader.getLocalName(),
+                            streamReader.getName().getNamespaceURI(), streamReader.getPrefix());
+                    for (int x = 0; x < streamReader.getNamespaceCount(); x++) {
+                        XmlNamespace namespace = new XmlNamespace(streamReader.getNamespacePrefix(x),
+                                streamReader.getNamespaceURI(x));
+                        elementEnd.namespaces().add(namespace);
+                    }
+                    out.add(elementEnd);
+                    break;
+                case XMLStreamConstants.PROCESSING_INSTRUCTION:
+                    out.add(new XmlProcessingInstruction(streamReader.getPIData(), streamReader.getPITarget()));
+                    break;
+                case XMLStreamConstants.CHARACTERS:
+                    out.add(new XmlCharacters(streamReader.getText()));
+                    break;
+                case XMLStreamConstants.COMMENT:
+                    out.add(new XmlComment(streamReader.getText()));
+                    break;
+                case XMLStreamConstants.SPACE:
+                    out.add(new XmlSpace(streamReader.getText()));
+                    break;
+                case XMLStreamConstants.ENTITY_REFERENCE:
+                    out.add(new XmlEntityReference(streamReader.getLocalName(), streamReader.getText()));
+                    break;
+                case XMLStreamConstants.DTD:
+                    out.add(new XmlDTD(streamReader.getText()));
+                    break;
+                case XMLStreamConstants.CDATA:
+                    out.add(new XmlCdata(streamReader.getText()));
+                    break;
+            }
+        }
+    }
+
+}
---
+++ codec-xml/src/main/java/io/netty/handler/codec/xml/XmlElement.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2014 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.codec.xml;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Generic XML element in document, {@link XmlElementStart} represents open element and provides access to attributes,
+ * {@link XmlElementEnd} represents closing element.
+ */
+public abstract class XmlElement {
+
+    private final String name;
+    private final String namespace;
+    private final String prefix;
+
+    private final List<XmlNamespace> namespaces = new LinkedList<XmlNamespace>();
+
+    protected XmlElement(String name, String namespace, String prefix) {
+        this.name = name;
+        this.namespace = namespace;
+        this.prefix = prefix;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String namespace() {
+        return namespace;
+    }
+
+    public String prefix() {
+        return prefix;
+    }
+
+    public List<XmlNamespace> namespaces() {
+        return namespaces;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) { return true; }
+        if (o == null || getClass() != o.getClass()) { return false; }
+
+        XmlElement that = (XmlElement) o;
+
+        if (!name.equals(that.name)) { return false; }
+        if (namespace != null ? !namespace.equals(that.namespace) : that.namespace != null) { return false; }
+        if (namespaces != null ? !namespaces.equals(that.namespaces) : that.namespaces != null) { return false; }
+        if (prefix != null ? !prefix.equals(that.prefix) : that.prefix != null) { return false; }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + (namespace != null ? namespace.hashCode() : 0);
+        result = 31 * result + (prefix != null ? prefix.hashCode() : 0);
+        result = 31 * result + (namespaces != null ? namespaces.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return ", name='" + name + '\'' +
+                ", namespace='" + namespace + '\'' +
+                ", prefix='" + prefix + '\'' +
+                ", namespaces=" + namespaces;
+    }
+
+}
