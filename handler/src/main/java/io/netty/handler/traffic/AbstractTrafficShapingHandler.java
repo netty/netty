a25c585f39aefc9cfd45d8190239f3443e729c03
Frederic Bregier <fredbregier@free.fr>
2014-09-20 19:07:23 +0200

    Fix overriden method not called in TrafficShaping
    Motivation:
    handlerAdded and handlerRemoved were overriden but super was never
    called, while it should.
    Also add one missing information in the toString method.
    
    Modifications:
    Add the super corresponding call, and add checkInterval to the
    toString() method
    
    Result;
    super method calls are correctly passed to the super implementation
    part.

--- handler/src/main/java/io/netty/handler/traffic/AbstractTrafficShapingHandler.java
+++ handler/src/main/java/io/netty/handler/traffic/AbstractTrafficShapingHandler.java
@@ -411,9 +411,9 @@ public abstract class AbstractTrafficShapingHandler extends ChannelDuplexHandler
 
     @Override
     public String toString() {
-        return "TrafficShaping with Write Limit: " + writeLimit +
-                " Read Limit: " + readLimit + " and Counter: " +
-                (trafficCounter != null? trafficCounter.toString() : "none");
+        return "TrafficShaping with Write Limit: " + writeLimit + " Read Limit: " + readLimit +
+                " CheckInterval: " + checkInterval + " and Counter: "
+                + (trafficCounter != null ? trafficCounter.toString() : "none");
     }
 
     /**
