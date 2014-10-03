56b8bb30b2915ed3041f9192d3f2db3c5870fd47
Scott Mitchell <scott.k.mitch1@gmail.com>
2014-10-03 18:16:04 -0400

    Backport ALPN and Mutual Auth SSL
    Motivation:
    
    Improvements were made on the main line to support ALPN and mutual
    authentication for TLS. These should be backported.
    
    Modifications:
    
    - Backport commits from the master branch
      - f8af84d5993456426a63ad0146479147b1a4a5e5
      - e74c8edba3fcbfd2e895ed6aac440efeb3aa637f
    
    Result:
    
    Support for ALPN and mutual authentication.

---
+++ handler/src/main/java/io/netty/handler/ssl/JdkDefaultApplicationProtocolNegotiator.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2014 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.ssl;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.net.ssl.SSLEngine;
+
+/**
+ * The {@link JdkApplicationProtocolNegotiator} to use if you do not care about NPN or ALPN and are using
+ * {@link SslProvider#JDK}.
+ */
+final class JdkDefaultApplicationProtocolNegotiator implements JdkApplicationProtocolNegotiator {
+    public static final JdkDefaultApplicationProtocolNegotiator INSTANCE =
+            new JdkDefaultApplicationProtocolNegotiator();
+    private static final SslEngineWrapperFactory DEFAULT_SSL_ENGINE_WRAPPER_FACTORY = new SslEngineWrapperFactory() {
+        @Override
+        public SSLEngine wrapSslEngine(SSLEngine engine, JdkApplicationProtocolNegotiator applicationNegotiator,
+                boolean isServer) {
+            return engine;
+        }
+    };
+
+    private JdkDefaultApplicationProtocolNegotiator() {
+    }
+
+    @Override
+    public SslEngineWrapperFactory wrapperFactory() {
+        return DEFAULT_SSL_ENGINE_WRAPPER_FACTORY;
+    }
+
+    @Override
+    public ProtocolSelectorFactory protocolSelectorFactory() {
+        throw new UnsupportedOperationException("Application protocol negotiation unsupported");
+    }
+
+    @Override
+    public ProtocolSelectionListenerFactory protocolListenerFactory() {
+        throw new UnsupportedOperationException("Application protocol negotiation unsupported");
+    }
+
+    @Override
+    public List<String> protocols() {
+        return Collections.emptyList();
+    }
+}
