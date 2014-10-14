netty/netty

commit de9c81bf6e021ec809d9a52d1f5895682c2cb59d

Add proxy support for client socket connections
Related issue: #1133

Motivation:

There is no support for client socket connections via a proxy server in
Netty.

Modifications:

- Add a new module 'handler-proxy'
- Add ProxyHandler and its subclasses to support SOCKS 4a/5 and HTTP(S)
  proxy connections
- Add a full parameterized test for most scenarios
- Clean up pom.xml

Result:

A user can make an outgoing connection via proxy servers with only
trivial effort.

handler-proxy/src/main/java/io/netty/handler/proxy/ProxyHandler.java
@@ -1,0 +1,449 @@
+/*
+ * Copyright 2014 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.netty.handler.proxy;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
+import io.netty.channel.PendingWriteQueue;
+import io.netty.util.ReferenceCountUtil;
+import io.netty.util.concurrent.DefaultPromise;
+import io.netty.util.concurrent.EventExecutor;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.ScheduledFuture;
+import io.netty.util.internal.OneTimeTask;
+import io.netty.util.internal.logging.InternalLogger;
+import io.netty.util.internal.logging.InternalLoggerFactory;
+
+import java.net.SocketAddress;
+import java.nio.channels.ConnectionPendingException;
+import java.util.concurrent.TimeUnit;
+
+public abstract class ProxyHandler extends ChannelDuplexHandler {
+
+    private static final InternalLogger logger = InternalLoggerFactory.getInstance(ProxyHandler.class);
+
+    /**
+     * The default connect timeout: 10 seconds.
+     */
+    private static final long DEFAULT_CONNECT_TIMEOUT_MILLIS = 10000;
+
+    /**
+     * A string that signifies 'no authentication' or 'anonymous'.
+     */
+    static final String AUTH_NONE = "none";
+
+    private final SocketAddress proxyAddress;
+    private volatile SocketAddress destinationAddress;
+    private volatile long connectTimeoutMillis = DEFAULT_CONNECT_TIMEOUT_MILLIS;
+
+    private volatile ChannelHandlerContext ctx;
+    private PendingWriteQueue pendingWrites;
+    private boolean finished;
+    private boolean suppressChannelReadComplete;
+    private boolean flushedPrematurely;
+    private final LazyChannelPromise connectPromise = new LazyChannelPromise();
+    private ScheduledFuture<?> connectTimeoutFuture;
+    private final ChannelFutureListener writeListener = new ChannelFutureListener() {
+        @Override
+        public void operationComplete(ChannelFuture future) throws Exception {
+            if (!future.isSuccess()) {
+                setConnectFailure(future.cause());
+            }
+        }
+    };
+
+    protected ProxyHandler(SocketAddress proxyAddress) {
+        if (proxyAddress == null) {
+            throw new NullPointerException("proxyAddress");
+        }
+        this.proxyAddress = proxyAddress;
+    }
+
+    /**
+     * Returns the name of the proxy protocol in use.
+     */
+    public abstract String protocol();
+
+    /**
+     * Returns the name of the authentication scheme in use.
+     */
+    public abstract String authScheme();
+
+    /**
+     * Returns the address of the proxy server.
+     */
+    @SuppressWarnings("unchecked")
+    public final <T extends SocketAddress> T proxyAddress() {
+        return (T) proxyAddress;
+    }
+
+    /**
+     * Returns the address of the destination to connect to via the proxy server.
+     */
+    @SuppressWarnings("unchecked")
+    public final <T extends SocketAddress> T destinationAddress() {
+        return (T) destinationAddress;
+    }
+
+    /**
+     * Rerutns {@code true} if and only if the connection to the destination has been established successfully.
+     */
+    public final boolean isConnected() {
+        return connectPromise.isSuccess();
+    }
+
+    /**
+     * Returns a {@link Future} that is notified when the connection to the destination has been established
+     * or the connection attempt has failed.
+     */
+    public final Future<Channel> connectFuture() {
+        return connectPromise;
+    }
+
+    /**
+     * Returns the connect timeout in millis.  If the connection attempt to the destination does not finish within
+     * the timeout, the connection attempt will be failed.
+     */
+    public final long connectTimeoutMillis() {
+        return connectTimeoutMillis;
+    }
+
+    /**
+     * Sets the connect timeout in millis.  If the connection attempt to the destination does not finish within
+     * the timeout, the connection attempt will be failed.
+     */
+    public final void setConnectTimeoutMillis(long connectTimeoutMillis) {
+        if (connectTimeoutMillis <= 0) {
+            connectTimeoutMillis = 0;
+        }
+
+        this.connectTimeoutMillis = connectTimeoutMillis;
+    }
+
+    @Override
+    public final void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        this.ctx = ctx;
+        addCodec(ctx);
+
+        if (ctx.channel().isActive()) {
+            // channelActive() event has been fired already, which means this.channelActive() will
+            // not be invoked. We have to initialize here instead.
+            sendInitialMessage(ctx);
+        } else {
+            // channelActive() event has not been fired yet.  this.channelOpen() will be invoked
+            // and initialization will occur there.
+        }
+    }
+
+    /**
+     * Adds the codec handlers required to communicate with the proxy server.
+     */
+    protected abstract void addCodec(ChannelHandlerContext ctx) throws Exception;
+
+    /**
+     * Removes the encoders added in {@link #addCodec(ChannelHandlerContext)}.
+     */
+    protected abstract void removeEncoder(ChannelHandlerContext ctx) throws Exception;
+
+    /**
+     * Removes the decoders added in {@link #addCodec(ChannelHandlerContext)}.
+     */
+    protected abstract void removeDecoder(ChannelHandlerContext ctx) throws Exception;
+
+    @Override
+    public final void connect(
+            ChannelHandlerContext ctx, SocketAddress remoteAddress, SocketAddress localAddress,
+            ChannelPromise promise) throws Exception {
+
+        if (destinationAddress != null) {
+            promise.setFailure(new ConnectionPendingException());
+            return;
+        }
+
+        destinationAddress = remoteAddress;
+        ctx.connect(proxyAddress, localAddress, promise);
+    }
+
+    @Override
+    public final void channelActive(ChannelHandlerContext ctx) throws Exception {
+        sendInitialMessage(ctx);
+        ctx.fireChannelActive();
+    }
+
+    /**
+     * Sends the initial message to be sent to the proxy server. This method also starts a timeout task which marks
+     * the {@link #connectPromise} as failure if the connection attempt does not success within the timeout.
+     */
+    private void sendInitialMessage(final ChannelHandlerContext ctx) throws Exception {
+        final long connectTimeoutMillis = this.connectTimeoutMillis;
+        if (connectTimeoutMillis > 0) {
+            connectTimeoutFuture = ctx.executor().schedule(new OneTimeTask() {
+                @Override
+                public void run() {
+                    if (!connectPromise.isDone()) {
+                        setConnectFailure(new ProxyConnectException(exceptionMessage("timeout")));
+                    }
+                }
+            }, connectTimeoutMillis, TimeUnit.MILLISECONDS);
+        }
+
+        final Object initialMessage = newInitialMessage(ctx);
+        if (initialMessage != null) {
+            sendToProxyServer(initialMessage);
+        }
+    }
+
+    /**
+     * Returns a new message that is sent at first time when the connection to the proxy server has been established.
+     *
+     * @return the initial message, or {@code null} if the proxy server is expected to send the first message instead
+     */
+    protected abstract Object newInitialMessage(ChannelHandlerContext ctx) throws Exception;
+
+    /**
+     * Sends the specified message to the proxy server.  Use this method to send a response to the proxy server in
+     * {@link #handleResponse(ChannelHandlerContext, Object)}.
+     */
+    protected final void sendToProxyServer(Object msg) {
+        ctx.writeAndFlush(msg).addListener(writeListener);
+    }
+
+    @Override
+    public final void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        if (finished) {
+            ctx.fireChannelInactive();
+        } else {
+            // Disconnected before connected to the destination.
+            setConnectFailure(new ProxyConnectException(exceptionMessage("disconnected")));
+        }
+    }
+
+    @Override
+    public final void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        if (finished) {
+            ctx.fireExceptionCaught(cause);
+        } else {
+            // Exception was raised before the connection attempt is finished.
+            setConnectFailure(cause);
+        }
+    }
+
+    @Override
+    public final void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (finished) {
+            // Received a message after the connection has been established; pass through.
+            suppressChannelReadComplete = false;
+            ctx.fireChannelRead(msg);
+        } else {
+            suppressChannelReadComplete = true;
+            Throwable cause = null;
+            try {
+                boolean done = handleResponse(ctx, msg);
+                if (done) {
+                    setConnectSuccess();
+                }
+            } catch (Throwable t) {
+                cause = t;
+            } finally {
+                ReferenceCountUtil.release(msg);
+                if (cause != null) {
+                    setConnectFailure(cause);
+                }
+            }
+        }
+    }
+
+    /**
+     * Handles the message received from the proxy server.
+     *
+     * @return {@code true} if the connection to the destination has been established,
+     *         {@code false} if the connection to the destination has not been established and more messages are
+     *         expected from the proxy server
+     */
+    protected abstract boolean handleResponse(ChannelHandlerContext ctx, Object response) throws Exception;
+
+    private void setConnectSuccess() {
+        finished = true;
+        if (connectTimeoutFuture != null) {
+            connectTimeoutFuture.cancel(false);
+        }
+
+        if (connectPromise.trySuccess(ctx.channel())) {
+            boolean removedCodec = true;
+
+            removedCodec &= safeRemoveEncoder();
+
+            ctx.fireUserEventTriggered(
+                    new ProxyConnectionEvent(protocol(), authScheme(), proxyAddress, destinationAddress));
+
+            removedCodec &= safeRemoveDecoder();
+
+            if (removedCodec) {
+                writePendingWrites();
+
+                if (flushedPrematurely) {
+                    ctx.flush();
+                }
+            } else {
+                // We are at inconsistent state because we failed to remove all codec handlers.
+                Exception cause = new ProxyConnectException(
+                        "failed to remove all codec handlers added by the proxy handler; bug?");
+                failPendingWrites(cause);
+                ctx.fireExceptionCaught(cause);
+                ctx.close();
+            }
+        }
+    }
+
+    private boolean safeRemoveDecoder() {
+        try {
+            removeDecoder(ctx);
+            return true;
+        } catch (Exception e) {
+            logger.warn("Failed to remove proxy decoders:", e);
+        }
+
+        return false;
+    }
+
+    private boolean safeRemoveEncoder() {
+        try {
+            removeEncoder(ctx);
+            return true;
+        } catch (Exception e) {
+            logger.warn("Failed to remove proxy encoders:", e);
+        }
+
+        return false;
+    }
+
+    private void setConnectFailure(Throwable cause) {
+        finished = true;
+        if (connectTimeoutFuture != null) {
+            connectTimeoutFuture.cancel(false);
+        }
+
+        if (!(cause instanceof ProxyConnectException)) {
+            cause = new ProxyConnectException(
+                    exceptionMessage(cause.toString()), cause);
+        }
+
+        if (connectPromise.tryFailure(cause)) {
+            safeRemoveDecoder();
+            safeRemoveEncoder();
+
+            failPendingWrites(cause);
+            ctx.fireExceptionCaught(cause);
+            ctx.close();
+        }
+    }
+
+    /**
+     * Decorates the specified exception message with the common information such as the current protocol,
+     * authentication scheme, proxy address, and destination address.
+     */
+    protected final String exceptionMessage(String msg) {
+        if (msg == null) {
+            msg = "";
+        }
+
+        StringBuilder buf = new StringBuilder(128 + msg.length());
+
+        buf.append(protocol());
+        buf.append(", ");
+        buf.append(authScheme());
+        buf.append(", ");
+        buf.append(proxyAddress);
+        buf.append(" => ");
+        buf.append(destinationAddress);
+        if (msg.length() > 0) {
+            buf.append(", ");
+            buf.append(msg);
+        }
+
+        return buf.toString();
+    }
+
+    @Override
+    public final void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        if (suppressChannelReadComplete) {
+            suppressChannelReadComplete = false;
+
+            if (!ctx.channel().config().isAutoRead()) {
+                ctx.read();
+            }
+        } else {
+            ctx.fireChannelReadComplete();
+        }
+    }
+
+    @Override
+    public final void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+        if (finished) {
+            writePendingWrites();
+            ctx.write(msg, promise);
+        } else {
+            addPendingWrite(ctx, msg, promise);
+        }
+    }
+
+    @Override
+    public final void flush(ChannelHandlerContext ctx) throws Exception {
+        if (finished) {
+            writePendingWrites();
+            ctx.flush();
+        } else {
+            flushedPrematurely = true;
+        }
+    }
+
+    private void writePendingWrites() {
+        if (pendingWrites != null) {
+            pendingWrites.removeAndWriteAll();
+            pendingWrites = null;
+        }
+    }
+
+    private void failPendingWrites(Throwable cause) {
+        if (pendingWrites != null) {
+            pendingWrites.removeAndFailAll(cause);
+            pendingWrites = null;
+        }
+    }
+
+    private void addPendingWrite(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
+        PendingWriteQueue pendingWrites = this.pendingWrites;
+        if (pendingWrites == null) {
+            this.pendingWrites = pendingWrites = new PendingWriteQueue(ctx);
+        }
+        pendingWrites.add(msg, promise);
+    }
+
+    private final class LazyChannelPromise extends DefaultPromise<Channel> {
+        @Override
+        protected EventExecutor executor() {
+            if (ctx == null) {
+                throw new IllegalStateException();
+            }
+            return ctx.executor();
+        }
+    }
+}

commit d9739126f246a3e7c61294385fef8d7daa5494fd

Finish porting netty-proxy module
- Fix problems introduced by de9c81bf6e021ec809d9a52d1f5895682c2cb59d
- Fix inspector warnings

handler-proxy/src/main/java/io/netty/handler/proxy/ProxyHandler.java
@@ -17,9 +17,9 @@
 package io.netty.handler.proxy;
 
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
 import io.netty.channel.PendingWriteQueue;
@@ -36,7 +36,7 @@ import java.net.SocketAddress;
 import java.nio.channels.ConnectionPendingException;
 import java.util.concurrent.TimeUnit;
 
-public abstract class ProxyHandler extends ChannelDuplexHandler {
+public abstract class ProxyHandler extends ChannelHandlerAdapter {
 
     private static final InternalLogger logger = InternalLoggerFactory.getInstance(ProxyHandler.class);
 

transport-native-epoll/src/main/java/io/netty/channel/epoll/EpollSocketChannel.java
@@ -37,7 +37,6 @@ import io.netty.util.internal.PlatformDependent;
 import io.netty.util.internal.StringUtil;
 
 import java.io.IOException;
-import java.net.ConnectException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
@@ -468,7 +467,6 @@ public final class EpollSocketChannel extends AbstractEpollChannel implements So
     }
 
     final class EpollSocketUnsafe extends AbstractEpollUnsafe {
-        private RecvByteBufAllocator.Handle allocHandle;
 
         private void closeOnRead(ChannelPipeline pipeline) {
             inputShutdown = true;

transport/src/main/java/io/netty/channel/AbstractChannel.java
@@ -435,7 +435,7 @@ public abstract class AbstractChannel extends DefaultAttributeMap implements Cha
         @Override
         public final ChannelHandlerInvoker invoker() {
             // return the unwrapped invoker.
-            return ((PausableChannelEventLoop) eventLoop().asInvoker()).unwrapInvoker();
+            return ((PausableChannelEventExecutor) eventLoop().asInvoker()).unwrapInvoker();
         }
 
         @Override
@@ -509,7 +509,7 @@ public abstract class AbstractChannel extends DefaultAttributeMap implements Cha
                 }
                 doRegister();
                 registered = true;
-                AbstractChannel.this.eventLoop.acceptNewTasks();
+                eventLoop.acceptNewTasks();
                 safeSetSuccess(promise);
                 pipeline.fireChannelRegistered();
                 if (isActive()) {

handler-proxy/src/test/java/io/netty/handler/proxy/ProxyHandlerTest.java
@@ -37,7 +37,7 @@ import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
 import io.netty.util.CharsetUtil;
-import io.netty.util.concurrent.DefaultThreadFactory;
+import io.netty.util.concurrent.DefaultExecutorServiceFactory;
 import io.netty.util.concurrent.Future;
 import io.netty.util.internal.EmptyArrays;
 import io.netty.util.internal.StringUtil;
@@ -77,7 +77,7 @@ public class ProxyHandlerTest {
     private static final String BAD_USERNAME = "badUser";
     private static final String BAD_PASSWORD = "badPassword";
 
-    static final EventLoopGroup group = new NioEventLoopGroup(3, new DefaultThreadFactory("proxy", true));
+    static final EventLoopGroup group = new NioEventLoopGroup(3, new DefaultExecutorServiceFactory("proxy"));
 
     static final SslContext serverSslCtx;
     static final SslContext clientSslCtx;
@@ -381,7 +381,7 @@ public class ProxyHandlerTest {
         }
 
         @Override
-        protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        protected void messageReceived(ChannelHandlerContext ctx, Object msg) throws Exception {
             String str = ((ByteBuf) msg).toString(CharsetUtil.US_ASCII);
             received.add(str);
             if ("2".equals(str)) {
@@ -437,7 +437,7 @@ public class ProxyHandlerTest {
         }
 
         @Override
-        protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        protected void messageReceived(ChannelHandlerContext ctx, Object msg) throws Exception {
             fail("Unexpected message: " + msg);
         }
 

handler-proxy/src/test/java/io/netty/handler/proxy/ProxyServer.java
@@ -24,8 +24,8 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoop;
@@ -158,7 +158,7 @@ abstract class ProxyServer {
         private Channel backend;
 
         @Override
-        protected final void channelRead0(final ChannelHandlerContext ctx, Object msg) throws Exception {
+        protected final void messageReceived(final ChannelHandlerContext ctx, Object msg) throws Exception {
             if (finished) {
                 received.add(ReferenceCountUtil.retain(msg));
                 flush();
@@ -232,7 +232,7 @@ abstract class ProxyServer {
             ctx.close();
         }
 
-        private final class BackendHandler extends ChannelInboundHandlerAdapter {
+        private final class BackendHandler extends ChannelHandlerAdapter {
 
             private final ChannelHandlerContext frontend;
 
@@ -268,7 +268,7 @@ abstract class ProxyServer {
         private boolean finished;
 
         @Override
-        protected final void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        protected final void messageReceived(ChannelHandlerContext ctx, Object msg) throws Exception {
             if (finished) {
                 String str = ((ByteBuf) msg).toString(CharsetUtil.US_ASCII);
                 if ("A\n".equals(str)) {

handler-proxy/src/test/java/io/netty/handler/proxy/UnresponsiveHandler.java
@@ -28,7 +28,7 @@ final class UnresponsiveHandler extends SimpleChannelInboundHandler<Object> {
     private UnresponsiveHandler() { }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+    protected void messageReceived(ChannelHandlerContext ctx, Object msg) throws Exception {
         // Ignore
     }
 }