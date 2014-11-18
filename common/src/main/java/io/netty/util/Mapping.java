bd6369768746cc9b66fefdd7c27d8ba8d2f76d60
Sun Ning <sunng@about.me>
2014-11-18 22:38:50 +0800

    Added support for SSL Server Name Indication.
    Motivation:
    
    When we need to host multiple server name with a single IP, it requires
    the server to support Server Name Indication extension to serve clients
    with proper certificate. So the SniHandler will host multiple
    SslContext(s) and append SslHandler for requested hostname.
    
    Modification:
    
    * Added SniHandler to host multiple certifications in a single server
    * Test case
    
    Result:
    
    User could use SniHandler to host multiple certifcates at a time.
    It's server-side only.

---
+++ common/src/main/java/io/netty/util/Mapping.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2014 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.util;
+
+/**
+ * An mapping which maintains a relationship from type of IN to type of OUT.
+ */
+public interface Mapping<IN, OUT> {
+
+    /**
+     * Returns mapped value of input.
+     */
+    OUT map(IN input);
+}
