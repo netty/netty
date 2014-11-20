d393ea54c646c9d6937dd885cace6c709d33b6ea
Scott Mitchell <scott.k.mitch1@gmail.com>
2014-11-20 13:03:14 -0500

    HTTP/2 Frame Listener Comment Correction
    Motivation:
    Some of the comments in HTTP/2 Frame Listener interface are misleading.
    
    Modifications:
    Clarify comments in Http2FrameListener.
    
    Result:
    Http2FrameListener onDataRead comments are clarified.

--- codec-http2/src/main/java/io/netty/handler/codec/http2/Http2FrameListener.java
+++ codec-http2/src/main/java/io/netty/handler/codec/http2/Http2FrameListener.java
@@ -28,7 +28,7 @@ public interface Http2FrameListener {
      *
      * @param ctx the context from the handler where the frame was read.
      * @param streamId the subject stream for the frame.
-     * @param data payload buffer for the frame. If this buffer will be released by the codec.
+     * @param data payload buffer for the frame. This buffer will be released by the codec.
      * @param padding the number of padding bytes found at the end of the frame.
      * @param endOfStream Indicates whether this is the last frame to be sent from the remote
      *            endpoint for this stream.
