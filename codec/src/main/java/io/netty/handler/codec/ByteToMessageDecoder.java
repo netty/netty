e1b2c55a11763370be50121a8fa197389adbae23
Ronald Chen <Ronald.Chen@elasticpath.com>
2014-12-01 10:05:13 -0800

    documented an additional situation where readBytes does not cause a memory leak

--- codec/src/main/java/io/netty/handler/codec/ByteToMessageDecoder.java
+++ codec/src/main/java/io/netty/handler/codec/ByteToMessageDecoder.java
@@ -64,8 +64,8 @@ import java.util.List;
  * annotated with {@link @Sharable}.
  * <p>
  * Some methods such as {@link ByteBuf.readBytes(int)} will cause a memory leak if the returned buffer
- * is not released. Use derived buffers like {@link ByteBuf.readSlice(int)} to avoid leaking
- * memory.
+ * is not released or added to the <tt>out</tt> {@link List}. Use derived buffers like {@link ByteBuf.readSlice(int)}
+ * to avoid leaking memory.
  */
 public abstract class ByteToMessageDecoder extends ChannelHandlerAdapter {
 
