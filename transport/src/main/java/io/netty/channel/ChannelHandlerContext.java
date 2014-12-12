39820ef3038d7ebf1605e4240d6852bdac69fb38
ysammy <samuel.j.young@gmail.com>
2014-12-12 09:27:22 -0800

    Fix documentation for ChannelHandlerContext#fireChannelReadComplete
    Motivation:
    Fix a minor documentation bug in
    ChannelHandlerContext#fireChannelReadComplete.
    
    Modifications:
    ChannelHandlerContext#fireChannelReadComplete no longer references an
    incorrect method in its javadoc.
    
    Results:
    Documentation is correct.

--- transport/src/main/java/io/netty/channel/ChannelHandlerContext.java
+++ transport/src/main/java/io/netty/channel/ChannelHandlerContext.java
@@ -225,7 +225,7 @@ public interface ChannelHandlerContext
     ChannelHandlerContext fireChannelRead(Object msg);
 
     /**
-     * Triggers an {@link ChannelInboundHandler#channelWritabilityChanged(ChannelHandlerContext)}
+     * Triggers an {@link ChannelInboundHandler#channelReadComplete(ChannelHandlerContext)}
      * event to the next {@link ChannelInboundHandler} in the {@link ChannelPipeline}.
      */
     ChannelHandlerContext fireChannelReadComplete();
