ea773c8db7aebdfd1eec9e98c3a814ae6573adf3
lhkzh <exiaolv@qq.com>
2015-02-14 13:38:33 +0800

    Revert "rewrite XmlFrameDecoder."
    This reverts commit 9d9ffb15d2f3992b7eb8243a8380eda00f8bd204.

--- codec/src/main/java/io/netty/handler/codec/xml/XmlFrameDecoder.java
+++ codec/src/main/java/io/netty/handler/codec/xml/XmlFrameDecoder.java
@@ -13,13 +13,12 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-
 package io.netty.handler.codec.xml;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
-import io.netty.handler.codec.DecoderException;
+import io.netty.handler.codec.CorruptedFrameException;
 import io.netty.handler.codec.TooLongFrameException;
 
 import java.util.List;
@@ -70,181 +69,163 @@ import java.util.List;
  */
 public class XmlFrameDecoder extends ByteToMessageDecoder {
 
-	private enum XmlStat {
-		HEADER,
-		COMMENT,
-		CDATA,
-		HEAD,
-		ATTR,
-		BODY,
-		END,
-		INIT
-	}
-	
-	private int maxObjectLength;
-	private int idx;
-	private int openCount=0;
-	private boolean closeing = false;
-	private XmlStat stat = XmlStat.INIT;
-	private XmlStat lastStat = XmlStat.INIT;
-	
-	public XmlFrameDecoder() {
-		this(1024*1024);
-	}
-	public XmlFrameDecoder(int maxObjectLength) {
-		this.maxObjectLength = maxObjectLength;
-	}
+    private final int maxFrameLength;
 
-	@Override
-	protected void decode(ChannelHandlerContext ctx, ByteBuf in,
-			List<Object> out) throws Exception {
-        // index of next byte to process.
-        int wrtIdx = in.writerIndex();
-        if (wrtIdx > maxObjectLength) {
-            // buffer size exceeded maxObjectLength; discarding the complete buffer.
-            in.skipBytes(in.readableBytes());
-            reset();
-            throw new TooLongFrameException(
-                            "object length exceeds " + maxObjectLength + ": " + wrtIdx + " bytes discarded");
+    public XmlFrameDecoder(int maxFrameLength) {
+        if (maxFrameLength < 1) {
+            throw new IllegalArgumentException("maxFrameLength must be a positive int");
         }
-        int idx = this.idx;
-        if(idx>wrtIdx){
-//        	System.out.println("$$ "+idx+"_"+in.readerIndex()+"_"+wrtIdx);
-        	if(in.readerIndex()==0)idx=0;
-        }
-        for (/* use current idx */; idx < wrtIdx; idx++) {
-            char c = (char)in.getByte(idx);
-//            System.out.println(c);
-//            if(Character.isWhitespace(c))continue;
-            if(stat==XmlStat.INIT){
-            	if(c=='<'){
-            		if((idx+1)>=wrtIdx)return;
-            		byte c_next = in.getByte(idx+1);
-            		if(c_next=='?'){
-            			lastStat = stat;
-            			stat = XmlStat.HEADER;
-            			idx++;
-            		}
-            		else if(c_next=='!'){
-            			lastStat = stat;
-            			stat = XmlStat.HEADER;
-            			idx++;
-            		}
-            		else{
-            			stat = XmlStat.HEAD;
-            			openCount++;
-            		}
-            	}
-            	else if(Character.isWhitespace(c)==false){
-//            	else{
-            		reset();
-            		in.skipBytes(in.readableBytes());
-            		throw new DecoderException("XML_ERR");
-            	}
-            }else if(stat==XmlStat.HEADER){
-            	if(c=='>' && idx>0 &&in.getByte(idx-1)=='?'){
-            		stat = lastStat;
-            	}
-            }else if(stat==XmlStat.COMMENT){
-            	if(c=='-'){
-            		if((idx+2)>=wrtIdx)return;
-            		if(in.getByte(idx+1)=='-'&&in.getByte(idx+2)=='>'){//结束注释
-            			idx+=2;
-            			stat = lastStat;
-            		}
-            	}
-            }else if(stat==XmlStat.HEAD){
-            	if(c=='='){//开始注释
-            		if((idx+1)>=wrtIdx)return;
-            		if(in.getByte(idx+1)=='"'){
-            			idx++;
-            			lastStat = stat;
-            			stat = XmlStat.ATTR;
-            		}
-            	}else if(c=='>'){//结束 头部
-            		if(in.getByte(idx-1)=='/'){//结束节点
-            			openCount--;
-            		}
-            		stat = XmlStat.BODY;
-            	}
-            }else if(stat==XmlStat.ATTR){
-            	if(c=='"'&&in.getByte(idx-1)!='\\'){
-            		stat = lastStat;
-            	}
-            }else if(stat==XmlStat.BODY){
-            	if(c=='<'){
-            		if((idx+1)>=wrtIdx)return;
-            		byte c_next = in.getByte(idx+1);
-            		if(c_next=='/'){
-            			closeing = true;
-            			stat = XmlStat.END;
-            			idx++;
-            		}else if(c_next=='!'){
-            			if((idx+3)>=wrtIdx)return;
-            			if(in.getByte(idx+2)=='-'&&in.getByte(idx+3)=='-'){
-            				lastStat = stat;
-            				stat = XmlStat.COMMENT;
-            				idx+=3;
-            				continue;
-            			}
-            			if((idx+8)>=wrtIdx)return;
-            			//<![CDATA[
-            			if(in.getByte(idx+2)=='['&&in.getByte(idx+3)=='C'&&in.getByte(idx+4)=='D'&&in.getByte(idx+5)=='A'&&in.getByte(idx+6)=='T'&&in.getByte(idx+7)=='A'&&in.getByte(idx+8)=='['){
-            				lastStat = stat;
-            				stat = XmlStat.CDATA;
-            				idx+=8;
-            			}
-            		}else{
-            			openCount ++;
-            			stat = XmlStat.HEAD;
-            		}
-            	}
-            }else if(stat==XmlStat.CDATA){
-            	if((idx+2)>=wrtIdx)return;
-            	if(in.getByte(idx)==']'&&in.getByte(idx+1)==']'&&in.getByte(idx+2)=='>'){
-            		idx +=2;
-            		stat = lastStat;
-            	}
-            }else if(stat==XmlStat.END){
-            	if(c=='>'){
-            		if(closeing){
-            			closeing = false;
-            			stat = XmlStat.BODY;
-            			if(idx>1&&in.getByte(idx-1)=='/'&&in.getByte(idx-2)=='<'){
-            				continue;
-            			}
-            			openCount --;
-            		}else{
-            			reset();
-            			in.skipBytes(in.readableBytes());
-            			throw new DecoderException("XML_ERR");
-            		}
-            	}
-            }
-            if(openCount==0 && stat!=XmlStat.HEADER && stat!=XmlStat.INIT){
-            	int length = idx+1-in.readerIndex();
-            	stat = XmlStat.INIT;
-            	lastStat = stat;
-            	if(length>0){
-            		out.add(in.copy(in.readerIndex(), length));
-            		in.skipBytes(length);
-            		idx = in.readerIndex();
-            		break;
-            	}
-            }
-        }
-        if (in.readableBytes() == 0) {
-            this.idx = 0;
-        } else {
-            this.idx = idx;
-        }
-	}
-    private void reset() {
-    	stat = XmlStat.INIT;
-    	lastStat = stat;
-        idx = 0;
-        openCount = 0;
-        closeing = false;
+        this.maxFrameLength = maxFrameLength;
     }
-	
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        boolean openingBracketFound = false;
+        boolean atLeastOneXmlElementFound = false;
+        boolean inCDATASection = false;
+        long openBracketsCount = 0;
+        int length = 0;
+        int leadingWhiteSpaceCount = 0;
+        final int bufferLength = in.writerIndex();
+
+        if (bufferLength > maxFrameLength) {
+            // bufferLength exceeded maxFrameLength; dropping frame
+            in.skipBytes(in.readableBytes());
+            fail(bufferLength);
+            return;
+        }
+
+        for (int i = in.readerIndex(); i < bufferLength; i++) {
+            final byte readByte = in.getByte(i);
+            if (!openingBracketFound && Character.isWhitespace(readByte)) {
+                // xml has not started and whitespace char found
+                leadingWhiteSpaceCount++;
+            } else if (!openingBracketFound && readByte != '<') {
+                // garbage found before xml start
+                fail(ctx);
+                in.skipBytes(in.readableBytes());
+                return;
+            } else if (!inCDATASection && readByte == '<') {
+                openingBracketFound = true;
+
+                if (i < bufferLength - 1) {
+                    final byte peekAheadByte = in.getByte(i + 1);
+                    if (peekAheadByte == '/') {
+                        // found </, decrementing openBracketsCount
+                        openBracketsCount--;
+                    } else if (isValidStartCharForXmlElement(peekAheadByte)) {
+                        atLeastOneXmlElementFound = true;
+                        // char after < is a valid xml element start char,
+                        // incrementing openBracketsCount
+                        openBracketsCount++;
+                    } else if (peekAheadByte == '!') {
+                        if (isCommentBlockStart(in, i)) {
+                            // <!-- comment --> start found
+                            openBracketsCount++;
+                        } else if (isCDATABlockStart(in, i)) {
+                            // <![CDATA[ start found
+                            openBracketsCount++;
+                            inCDATASection = true;
+                        }
+                    } else if (peekAheadByte == '?') {
+                        // <?xml ?> start found
+                        openBracketsCount++;
+                    }
+                }
+            } else if (!inCDATASection && readByte == '/') {
+                if (i < bufferLength - 1 && in.getByte(i + 1) == '>') {
+                    // found />, decrementing openBracketsCount
+                    openBracketsCount--;
+                }
+            } else if (readByte == '>') {
+                length = i + 1;
+
+                if (i - 1 > -1) {
+                    final byte peekBehindByte = in.getByte(i - 1);
+
+                    if (!inCDATASection) {
+                        if (peekBehindByte == '?') {
+                            // an <?xml ?> tag was closed
+                            openBracketsCount--;
+                        } else if (peekBehindByte == '-' && i - 2 > -1 && in.getByte(i - 2) == '-') {
+                            // a <!-- comment --> was closed
+                            openBracketsCount--;
+                        }
+                    } else if (peekBehindByte == ']' && i - 2 > -1 && in.getByte(i - 2) == ']') {
+                        // a <![CDATA[...]]> block was closed
+                        openBracketsCount--;
+                        inCDATASection = false;
+                    }
+                }
+
+                if (atLeastOneXmlElementFound && openBracketsCount == 0) {
+                    // xml is balanced, bailing out
+                    break;
+                }
+            }
+        }
+
+        final int readerIndex = in.readerIndex();
+
+        if (openBracketsCount == 0 && length > 0) {
+            if (length >= bufferLength) {
+                length = in.readableBytes();
+            }
+            final ByteBuf frame =
+                    extractFrame(in, readerIndex + leadingWhiteSpaceCount, length - leadingWhiteSpaceCount);
+            in.skipBytes(length);
+            out.add(frame);
+        }
+    }
+
+    private void fail(long frameLength) {
+        if (frameLength > 0) {
+            throw new TooLongFrameException(
+                            "frame length exceeds " + maxFrameLength + ": " + frameLength + " - discarded");
+        } else {
+            throw new TooLongFrameException(
+                            "frame length exceeds " + maxFrameLength + " - discarding");
+        }
+    }
+
+    private static void fail(ChannelHandlerContext ctx) {
+        ctx.fireExceptionCaught(new CorruptedFrameException("frame contains content before the xml starts"));
+    }
+
+    private static ByteBuf extractFrame(ByteBuf buffer, int index, int length) {
+        return buffer.copy(index, length);
+    }
+
+    /**
+     * Asks whether the given byte is a valid
+     * start char for an xml element name.
+     * <p/>
+     * Please refer to the
+     * <a href="http://www.w3.org/TR/2004/REC-xml11-20040204/#NT-NameStartChar">NameStartChar</a>
+     * formal definition in the W3C XML spec for further info.
+     *
+     * @param b the input char
+     * @return true if the char is a valid start char
+     */
+    private static boolean isValidStartCharForXmlElement(final byte b) {
+        return b >= 'a' && b <= 'z' || b >= 'A' && b <= 'Z' || b == ':' || b == '_';
+    }
+
+    private static boolean isCommentBlockStart(final ByteBuf in, final int i) {
+        return i < in.writerIndex() - 3
+                && in.getByte(i + 2) == '-'
+                && in.getByte(i + 3) == '-';
+    }
+
+    private static boolean isCDATABlockStart(final ByteBuf in, final int i) {
+        return i < in.writerIndex() - 8
+                && in.getByte(i + 2) == '['
+                && in.getByte(i + 3) == 'C'
+                && in.getByte(i + 4) == 'D'
+                && in.getByte(i + 5) == 'A'
+                && in.getByte(i + 6) == 'T'
+                && in.getByte(i + 7) == 'A'
+                && in.getByte(i + 8) == '[';
+    }
+
 }
