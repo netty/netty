7317431b7f4747a837fbf81c4c1ae5145933f8cc
Jakob Buchgraber <buchgr@google.com>
2015-02-17 12:20:37 -0800

    Document nEventLoops=0 in (Nio|Default|Epoll)EventLoopGroups.
    Motivation:
    
    When setting nEventLoops to zero in the MultithreadedEventLoopGroup constructor
    the EventLoopGroup chooses the number of EventLoops and Threads to use for you.
    We want to make use of this behaviour internally and thus we would like to document
    it as a part of the official API, so that we can rely on it.
    
    Modifications:
    
    Document the behaviour when setting nEventLoops to zero.
    Fix several spelling / sloppiness mistakes in the documentation.
    
    Result:
    
    nEventLoops=0 is now documented as a part of the official API.
    The quality of the documentation is improved.

--- transport/src/main/java/io/netty/channel/DefaultEventLoopGroup.java
+++ transport/src/main/java/io/netty/channel/DefaultEventLoopGroup.java
@@ -25,7 +25,7 @@ import java.util.concurrent.Executor;
 public class DefaultEventLoopGroup extends MultithreadEventLoopGroup {
 
     /**
-     * Create a new instance that uses twice as many {@link EventLoop}s as there processors/cores
+     * Create a new instance that uses twice as many {@link EventLoop}s as there are processors/cores
      * available, as well as the default {@link Executor}.
      *
      * @see io.netty.util.concurrent.DefaultExecutorServiceFactory
@@ -35,34 +35,37 @@ public class DefaultEventLoopGroup extends MultithreadEventLoopGroup {
     }
 
     /**
-     * @param nEventLoops       the number of {@link EventLoop}s that will be used by this instance.
-     *                          If {@code executor} is {@code null} this number will also be the parallelism
-     *                          requested from the default executor. It is generally advised for the number
-     *                          of {@link EventLoop}s and the number of {@link Thread}s used by the
-     *                          {@code executor} to lie very close together.
+     * @param nEventLoops   the number of {@link EventLoop}s that will be used by this instance.
+     *                      This will also be the parallelism requested from the default {@link Executor}.
+     *                      If set to {@code 0} the behaviour is the same as documented in
+     *                      {@link #DefaultEventLoopGroup()}.
      */
     public DefaultEventLoopGroup(int nEventLoops) {
         this(nEventLoops, (Executor) null);
     }
 
     /**
-     * @param nEventLoops       the number of {@link EventLoop}s that will be used by this instance.
-     *                          If {@code executor} is {@code null} this number will also be the parallelism
-     *                          requested from the default executor. It is generally advised for the number
-     *                          of {@link EventLoop}s and the number of {@link Thread}s used by the
-     *                          {@code executor} to lie very close together.
-     * @param executor           the {@link Executor} to use, or {@code null} if the default should be used.
+     * @param nEventLoops   the number of {@link EventLoop}s that will be used by this instance.
+     *                      If {@code executor} is {@code null} this number will also be the parallelism
+     *                      requested from the default {@link Executor}. It is generally advised for the number
+     *                      of {@link EventLoop}s and the number of {@link Thread}s used by the
+     *                      {@code executor} to lie close together.
+     *                      If set to {@code 0} the behaviour is the same as documented in
+     *                      {@link #DefaultEventLoopGroup()}.
+     * @param executor      the {@link Executor} to use, or {@code null} if the default should be used.
      */
     public DefaultEventLoopGroup(int nEventLoops, Executor executor) {
         super(nEventLoops, executor);
     }
 
     /**
-     * @param nEventLoops       the number of {@link EventLoop}s that will be used by this instance.
-     *                           If {@code executor} is {@code null} this number will also be the parallelism
-     *                           requested from the default executor. It is generally advised for the number
-     *                           of {@link EventLoop}s and the number of {@link Thread}s used by the
-     *                           {@code executor} to lie very close together.
+     * @param nEventLoops   the number of {@link EventLoop}s that will be used by this instance.
+     *                      If {@code executorServiceFactory} is {@code null} this number will also be the parallelism
+     *                      requested from the default {@link Executor}. It is generally advised for the number
+     *                      of {@link EventLoop}s and the number of {@link Thread}s used by the
+     *                      {@code executor} to lie close together.
+     *                      If set to {@code 0} the behaviour is the same as documented in
+     *                      {@link #DefaultEventLoopGroup()}.
      * @param executorServiceFactory   the {@link ExecutorServiceFactory} to use, or {@code null} if the default
      *                                 should be used.
      */
