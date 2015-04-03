3354296c9ff955e4315d32e0f85019ab209475f3
nmittler <nathanmittler@google.com>
2015-04-03 13:36:28 -0700

    Auto-generating primitive collections for int and char keys.
    Motivation:
    
    Currently we have IntObjectMap/HashMap, but it will be useful to support other primitive-based maps.
    
    Modifications:
    
    Moved the code int the current maps to template files and run Groovy code from  common/pom.xml to apply the templates.
    
    Result:
    
    Autogeneration of int and char-based hash maps.

---
+++ common/src/main/script/codegen.groovy
@@ -0,0 +1,39 @@
+String[] templateDirs = [properties["collection.template.dir"],
+                         properties["collection.template.test.dir"]]
+String[] outputDirs = [properties["collection.src.dir"],
+                       properties["collection.testsrc.dir"]]
+
+templateDirs.eachWithIndex { templateDir, i ->
+    convertSources templateDir, outputDirs[i]
+}
+
+void convertSources(String templateDir, String outputDir) {
+    String[] keyPrimitives = ["byte", "char", "short", "int", "long"]
+    String[] keyObjects = ["Byte", "Character", "Short", "Integer", "Long"];
+
+    keyPrimitives.eachWithIndex { keyPrimitive, i ->
+        convertTemplates templateDir, outputDir, keyPrimitive, keyObjects[i]
+    }
+}
+
+void convertTemplates(String templateDir,
+                      String outputDir,
+                      String keyPrimitive,
+                      String keyObject) {
+    def keyName = keyPrimitive.capitalize()
+    def replaceFrom = "(^.*)K([^.]+)\\.template\$"
+    def replaceTo = "\\1" + keyName + "\\2.java"
+    def hashCodeFn = keyPrimitive.equals("long") ? "(int)(key ^ (key >>> 32))" : "(int) key"
+    ant.copy(todir: outputDir) {
+        fileset(dir: templateDir) {
+            include(name: "**/*.template")
+        }
+        filterset() {
+            filter(token: "K", value: keyName)
+            filter(token: "k", value: keyPrimitive)
+            filter(token: "O", value: keyObject)
+            filter(token: "HASH_CODE", value: hashCodeFn)
+        }
+        regexpmapper(from: replaceFrom, to: replaceTo)
+    }
+}
