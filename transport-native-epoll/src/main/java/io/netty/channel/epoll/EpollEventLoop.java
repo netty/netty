acb6902f6850fda2e189dd7eabf40b348db2631a
Norman Maurer <norman_maurer@apple.com>
2015-04-10 15:23:00 +0200

    [#3539] Correctly handle EPOLLRDHUP
    Motivation:
    
    As we missed to correctly handle EPOLLRDHUP we produce an IOException which is unnessary. This leads
    to have exceptionCaught(...) methods called.
    
    Modifications:
    
    When EPOLLRDHUP was received just close the socket and fail all pending writes.
    
    Result:
    
    Correctly handle of EPOLLRDHUP and so not miss-leading exceptions.

--- transport-native-epoll/src/main/java/io/netty/channel/epoll/EpollEventLoop.java
+++ transport-native-epoll/src/main/java/io/netty/channel/epoll/EpollEventLoop.java
@@ -307,26 +307,22 @@ final class EpollEventLoop extends SingleThreadEventLoop {
 
                 AbstractEpollChannel ch = channels.get(fd);
                 if (ch != null && ch.isOpen()) {
-                    boolean close = (ev & Native.EPOLLRDHUP) != 0;
-                    boolean read = (ev & Native.EPOLLIN) != 0;
-                    boolean write = (ev & Native.EPOLLOUT) != 0;
 
                     AbstractEpollUnsafe unsafe = (AbstractEpollUnsafe) ch.unsafe();
 
-                    if (close) {
+                    // We need to check if the channel is still open before try to trigger the
+                    // callbacks.
+                    // See https://github.com/netty/netty/issues/3443
+                    if ((ev & Native.EPOLLRDHUP) != 0 && ch.isOpen()) {
                         unsafe.epollRdHupReady();
                     }
 
-                    // We need to check if the channel is still open before try to trigger the
-                    // callbacks as otherwise we may trigger an IllegalStateException when try
-                    // to access the file descriptor.
-                    //
-                    // See https://github.com/netty/netty/issues/3443
-                    if (write && ch.isOpen()) {
+                    if ((ev & Native.EPOLLOUT) != 0 && ch.isOpen()) {
                         // force flush of data as the epoll is writable again
                         unsafe.epollOutReady();
                     }
-                    if (read && ch.isOpen()) {
+
+                    if ((ev & Native.EPOLLIN) != 0 && ch.isOpen()) {
                         // Something is ready to read, so consume it now
                         unsafe.epollInReady();
                     }
