eaf0c0d9a03a3383970f1c8b31ae84d16340b645
Louis Ryan <lryan@google.com>
2015-04-13 15:45:48 -0700

    Have microbenchmarks produce a deployable artifact. Fix some minor miscellaneous issues.
    Motivation:
    Allows for running benchmarks from built jars which is useful in development environments that only take released artifacts.
    
    Modifications:
    Move benchmarks into 'main' from 'test'
    Add @State annotations to benchmarks that are missing them
    Fix timing issue grabbing context during channel initialization
    
    Result:
    Users can run benchmarks more easily.

--- microbench/src/main/java/io/netty/microbench/util/AbstractMicrobenchmark.java
+++ microbench/src/main/java/io/netty/microbench/util/AbstractMicrobenchmark.java
@@ -37,7 +37,7 @@ public class AbstractMicrobenchmark extends AbstractMicrobenchmarkBase {
     static {
         final String[] customArgs = {
         "-Xms768m", "-Xmx768m", "-XX:MaxDirectMemorySize=768m", "-Dharness.executor=CUSTOM",
-        "-Dharness.executor.class=io.netty.microbench.util.AbstractMicrobenchmark$HarnessExecutor" };
+        "-Dharness.executor.class=AbstractMicrobenchmark$HarnessExecutor" };
 
         JVM_ARGS = new String[BASE_JVM_ARGS.length + customArgs.length];
         System.arraycopy(BASE_JVM_ARGS, 0, JVM_ARGS, 0, BASE_JVM_ARGS.length);
