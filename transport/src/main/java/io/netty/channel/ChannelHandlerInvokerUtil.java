37861f332ef531b905b3e9d879b24b59a0ea6f64
Norman Maurer <norman_maurer@apple.com>
2015-04-20 09:10:29 +0200

    Revert "Do not suppress channelReadComplete() when a handler was just added"
    This reverts commit 720faa4df101c22ff591ae4a916e93648951c374.

--- transport/src/main/java/io/netty/channel/ChannelHandlerInvokerUtil.java
+++ transport/src/main/java/io/netty/channel/ChannelHandlerInvokerUtil.java
@@ -80,7 +80,6 @@ public final class ChannelHandlerInvokerUtil {
 
     public static void invokeChannelReadNow(final ChannelHandlerContext ctx, final Object msg) {
         try {
-            ((AbstractChannelHandlerContext) ctx).invokedThisChannelRead = true;
             ((ChannelInboundHandler) ctx.handler()).channelRead(ctx, msg);
         } catch (Throwable t) {
             notifyHandlerException(ctx, t);
