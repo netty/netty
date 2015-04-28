c6f83a4fb8dc4303284bde3330b637a44d36357d
Jakob Buchgraber <buchgr@google.com>
2015-04-28 16:27:54 -0700

    Http2ConnectionHandler should propagate channelActive and channelInactive events.
    Motivation:
    
    The Http2ConnectionHandler incorrectly doesn't propagate channelActive and channelInactive events and thus breaks the pipeline
    for other ChannelHandler.
    
    Modification:
    
    - Add calls to super.channelActive() and super.channelInactive().
    - Remove unused methods.
    
    Result:
    
    - Http2ConnectionHandler can be used with other ChannelHandlers.

--- codec-http2/src/main/java/io/netty/handler/codec/http2/Http2ConnectionHandler.java
+++ codec-http2/src/main/java/io/netty/handler/codec/http2/Http2ConnectionHandler.java
@@ -317,12 +317,14 @@ public class Http2ConnectionHandler extends ByteToMessageDecoder implements Http
             byteDecoder = new PrefaceDecoder(ctx);
         }
         byteDecoder.channelActive(ctx);
+        super.channelActive(ctx);
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
         if (byteDecoder != null) {
             byteDecoder.channelInactive(ctx);
+            super.channelInactive(ctx);
             byteDecoder = null;
         }
     }
@@ -365,26 +367,6 @@ public class Http2ConnectionHandler extends ByteToMessageDecoder implements Http
         } else {
             closeListener = new ClosingChannelFutureListener(ctx, promise);
         }
-    }
-
-    @Override
-    public void deregister(ChannelHandlerContext ctx, ChannelPromise promise) throws Exception {
-        ctx.deregister(promise);
-    }
-
-    @Override
-    public void read(ChannelHandlerContext ctx) throws Exception {
-        ctx.read();
-    }
-
-    @Override
-    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
-        ctx.write(msg, promise);
-    }
-
-    @Override
-    public void flush(ChannelHandlerContext ctx) throws Exception {
-        ctx.flush();
     }
 
     /**
