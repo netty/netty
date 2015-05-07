2927cdd504332b74e20eb943e0ea92d123d2d31c
Eric Anderson <ejona@google.com>
2015-05-07 13:25:48 -0700

    Swap to SslContextBuilder in examples
    Motivation:
    
    Using factory methods of SslContext is deprecated. Code should be using
    SslContextBuilder instead. This would have been done when the old
    methods were deprecated, but memcache and http2 examples didn't exist in
    the 4.0 branch which the PR was against.
    
    Modifications:
    
    Swap to the new construction pattern.
    
    Result:
    
    No more deprecated warnings during build of examples. Users are
    instructed to use the new pattern.

--- example/src/main/java/io/netty/example/http2/client/Http2Client.java
+++ example/src/main/java/io/netty/example/http2/client/Http2Client.java
@@ -33,6 +33,7 @@ import io.netty.handler.ssl.ApplicationProtocolConfig.SelectedListenerFailureBeh
 import io.netty.handler.ssl.ApplicationProtocolConfig.SelectorFailureBehavior;
 import io.netty.handler.ssl.OpenSsl;
 import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.SslProvider;
 import io.netty.handler.ssl.SupportedCipherSuiteFilter;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
@@ -64,19 +65,19 @@ public final class Http2Client {
         final SslContext sslCtx;
         if (SSL) {
             SslProvider provider = OpenSsl.isAlpnSupported() ? SslProvider.OPENSSL : SslProvider.JDK;
-            sslCtx = SslContext.newClientContext(provider,
-                    null, InsecureTrustManagerFactory.INSTANCE,
-                    Http2SecurityUtil.CIPHERS,
-                    /* NOTE: the following filter may not include all ciphers required by the HTTP/2 specification
-                     * Please refer to the HTTP/2 specification for cipher requirements. */
-                    SupportedCipherSuiteFilter.INSTANCE,
-                    new ApplicationProtocolConfig(
-                            Protocol.ALPN,
-                            SelectorFailureBehavior.CHOOSE_MY_LAST_PROTOCOL,
-                            SelectedListenerFailureBehavior.CHOOSE_MY_LAST_PROTOCOL,
-                            SelectedProtocol.HTTP_2.protocolName(),
-                            SelectedProtocol.HTTP_1_1.protocolName()),
-                    0, 0);
+            sslCtx = SslContextBuilder.forClient()
+                .sslProvider(provider)
+                /* NOTE: the cipher filter may not include all ciphers required by the HTTP/2 specification.
+                 * Please refer to the HTTP/2 specification for cipher requirements. */
+                .ciphers(Http2SecurityUtil.CIPHERS, SupportedCipherSuiteFilter.INSTANCE)
+                .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                .applicationProtocolConfig(new ApplicationProtocolConfig(
+                    Protocol.ALPN,
+                    SelectorFailureBehavior.CHOOSE_MY_LAST_PROTOCOL,
+                    SelectedListenerFailureBehavior.CHOOSE_MY_LAST_PROTOCOL,
+                    SelectedProtocol.HTTP_2.protocolName(),
+                    SelectedProtocol.HTTP_1_1.protocolName()))
+                .build();
         } else {
             sslCtx = null;
         }
