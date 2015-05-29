47da75821c15cec9946c8f83703db428bb5b6da0
Norman Maurer <norman_maurer@apple.com>
2015-05-29 09:26:57 +0200

    Add ProtocolDetectionResult and use it in HAProxyMessageDecoder for allow detect HAProxy protocol.
    Motivation:
    
    Sometimes it is useful to detect if a ByteBuf contains a HAProxy header, for example if you want to write something like the PortUnification example.
    
    Modifications:
    
    - Add ProtocolDetectionResult which can be used as a return type for detecting different protocol.
    - Add new method which allows to detect HA Proxy messages.
    
    Result:
    
    Easier to detect protocol.

---
+++ codec/src/main/java/io/netty/handler/codec/ProtocolDetectionState.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2015 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.codec;
+
+/**
+ * The state of the current detection.
+ */
+public enum ProtocolDetectionState {
+    /**
+     * Need more data to detect the protocol.
+     */
+    NEEDS_MORE_DATA,
+
+    /**
+     * The data was invalid.
+     */
+    INVALID,
+
+    /**
+     * Protocol was detected,
+     */
+    DETECTED
+}
