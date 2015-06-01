077508949638bd90c11d81ec1ce3b0ab257e5eb6
Trustin Lee <t@motd.kr>
2015-06-01 16:05:00 +0900

    Replace SpdyOrHttpChooser and Http2OrHttpChooser with ApplicationProtocolNegotiationHandler
    Motivation:
    
    SpdyOrHttpChooser and Http2OrHttpChooser duplicate fair amount code with each other.
    
    Modification:
    
    - Replace SpdyOrHttpChooser and Http2OrHttpChooser with ApplicationProtocolNegotiationHandler
    - Add ApplicationProtocolNames to define the known application-level protocol names
    
    Result:
    
    - Less code duplication
    - A user can perform dynamic pipeline configuration that follows ALPN/NPN for any protocols.

--- example/src/main/java/io/netty/example/http2/helloworld/server/Http2OrHttpHandler.java
+++ example/src/main/java/io/netty/example/http2/helloworld/server/Http2OrHttpHandler.java
@@ -17,25 +17,35 @@ package io.netty.example.http2.helloworld.server;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
-import io.netty.handler.codec.http2.Http2OrHttpChooser;
+import io.netty.handler.ssl.ApplicationProtocolNames;
+import io.netty.handler.ssl.ApplicationProtocolNegotiationHandler;
 
 /**
  * Negotiates with the browser if HTTP2 or HTTP is going to be used. Once decided, the Netty
  * pipeline is setup with the correct handlers for the selected protocol.
  */
-public class Http2OrHttpHandler extends Http2OrHttpChooser {
+public class Http2OrHttpHandler extends ApplicationProtocolNegotiationHandler {
 
     private static final int MAX_CONTENT_LENGTH = 1024 * 100;
 
-    @Override
-    protected void configureHttp2(ChannelHandlerContext ctx) throws Exception {
-        ctx.pipeline().addLast(new HelloWorldHttp2Handler());
+    protected Http2OrHttpHandler() {
+        super(ApplicationProtocolNames.HTTP_1_1);
     }
 
     @Override
-    protected void configureHttp1(ChannelHandlerContext ctx) throws Exception {
-        ctx.pipeline().addLast(new HttpServerCodec(),
-                               new HttpObjectAggregator(MAX_CONTENT_LENGTH),
-                               new HelloWorldHttp1Handler("ALPN Negotiation"));
+    protected void configurePipeline(ChannelHandlerContext ctx, String protocol) throws Exception {
+        if (ApplicationProtocolNames.HTTP_2.equals(protocol)) {
+            ctx.pipeline().addLast(new HelloWorldHttp2Handler());
+            return;
+        }
+
+        if (ApplicationProtocolNames.HTTP_1_1.equals(protocol)) {
+            ctx.pipeline().addLast(new HttpServerCodec(),
+                                   new HttpObjectAggregator(MAX_CONTENT_LENGTH),
+                                   new HelloWorldHttp1Handler("ALPN Negotiation"));
+            return;
+        }
+
+        throw new IllegalStateException("unknown protocol: " + protocol);
     }
 }
