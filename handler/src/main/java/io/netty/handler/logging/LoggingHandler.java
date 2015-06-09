9a30b6860f21c9c66ba65ae7edd0635785d15242
Norman Maurer <norman_maurer@apple.com>
2015-06-09 09:00:41 +0200

    [#3798] Extract dump method to ByteBufUtil
    Motivation:
    
    Dumping the content of a ByteBuf in a hex format is very useful.
    
    Modifications:
    
    Move code into ByteBufUtil so its easy to reuse.
    
    Result:
    
    Easy to reuse dumping code.

--- handler/src/main/java/io/netty/handler/logging/LoggingHandler.java
+++ handler/src/main/java/io/netty/handler/logging/LoggingHandler.java
@@ -22,12 +22,14 @@ import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
-import io.netty.util.internal.StringUtil;
 import io.netty.util.internal.logging.InternalLogLevel;
 import io.netty.util.internal.logging.InternalLogger;
 import io.netty.util.internal.logging.InternalLoggerFactory;
 
 import java.net.SocketAddress;
+
+import static io.netty.buffer.ByteBufUtil.appendPrettyHexDump;
+import static io.netty.util.internal.StringUtil.NEWLINE;
 
 /**
  * A {@link ChannelHandler} that logs all events using a logging framework.
@@ -37,51 +39,6 @@ import java.net.SocketAddress;
 public class LoggingHandler extends ChannelDuplexHandler {
 
     private static final LogLevel DEFAULT_LEVEL = LogLevel.DEBUG;
-
-    private static final String NEWLINE = StringUtil.NEWLINE;
-
-    private static final String[] BYTE2HEX = new String[256];
-    private static final String[] HEXPADDING = new String[16];
-    private static final String[] BYTEPADDING = new String[16];
-    private static final char[] BYTE2CHAR = new char[256];
-
-    static {
-        int i;
-
-        // Generate the lookup table for byte-to-hex-dump conversion
-        for (i = 0; i < BYTE2HEX.length; i ++) {
-            BYTE2HEX[i] = ' ' + StringUtil.byteToHexStringPadded(i);
-        }
-
-        // Generate the lookup table for hex dump paddings
-        for (i = 0; i < HEXPADDING.length; i ++) {
-            int padding = HEXPADDING.length - i;
-            StringBuilder buf = new StringBuilder(padding * 3);
-            for (int j = 0; j < padding; j ++) {
-                buf.append("   ");
-            }
-            HEXPADDING[i] = buf.toString();
-        }
-
-        // Generate the lookup table for byte dump paddings
-        for (i = 0; i < BYTEPADDING.length; i ++) {
-            int padding = BYTEPADDING.length - i;
-            StringBuilder buf = new StringBuilder(padding);
-            for (int j = 0; j < padding; j ++) {
-                buf.append(' ');
-            }
-            BYTEPADDING[i] = buf.toString();
-        }
-
-        // Generate the lookup table for byte-to-char conversion
-        for (i = 0; i < BYTE2CHAR.length; i ++) {
-            if (i <= 0x1f || i >= 0x7f) {
-                BYTE2CHAR[i] = '.';
-            } else {
-                BYTE2CHAR[i] = (char) i;
-            }
-        }
-    }
 
     protected final InternalLogger logger;
     protected final InternalLogLevel internalLevel;
@@ -316,55 +273,21 @@ public class LoggingHandler extends ChannelDuplexHandler {
     /**
      * Returns a String which contains all details to log the {@link ByteBuf}
      */
-    protected String formatByteBuf(String eventName, ByteBuf buf) {
-        int length = buf.readableBytes();
-        int rows = length / 16 + (length % 15 == 0? 0 : 1) + 4;
-        StringBuilder dump = new StringBuilder(rows * 80 + eventName.length() + 16)
+    protected String formatByteBuf(String eventName, ByteBuf msg) {
+        int length = msg.readableBytes();
+        if (length == 0) {
+            StringBuilder buf = new StringBuilder(eventName.length() + 4);
+            buf.append(eventName).append(": 0B");
+            return buf.toString();
+        } else {
+            int rows = length / 16 + (length % 15 == 0? 0 : 1) + 4;
+            StringBuilder buf = new StringBuilder(eventName.length() + 2 + 10 + 1 + 2 + rows * 80);
 
-        .append(eventName).append('(').append(length).append('B').append(')')
-        .append(
-                NEWLINE + "         +-------------------------------------------------+" +
-                        NEWLINE + "         |  0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f |" +
-                        NEWLINE + "+--------+-------------------------------------------------+----------------+");
+            buf.append(eventName).append(": ").append(length).append('B').append(NEWLINE);
+            appendPrettyHexDump(buf, msg);
 
-        final int startIndex = buf.readerIndex();
-        final int endIndex = buf.writerIndex();
-
-        int i;
-        for (i = startIndex; i < endIndex; i ++) {
-            int relIdx = i - startIndex;
-            int relIdxMod16 = relIdx & 15;
-            if (relIdxMod16 == 0) {
-                dump.append(NEWLINE)
-                .append(Long.toHexString(relIdx & 0xFFFFFFFFL | 0x100000000L))
-                .setCharAt(dump.length() - 9, '|');
-                dump.append('|');
-            }
-            dump.append(BYTE2HEX[buf.getUnsignedByte(i)]);
-            if (relIdxMod16 == 15) {
-                dump.append(" |");
-                for (int j = i - 15; j <= i; j ++) {
-                    dump.append(BYTE2CHAR[buf.getUnsignedByte(j)]);
-                }
-                dump.append('|');
-            }
+            return buf.toString();
         }
-
-        if ((i - startIndex & 15) != 0) {
-            int remainder = length & 15;
-            dump.append(HEXPADDING[remainder])
-            .append(" |");
-            for (int j = i - remainder; j < i; j ++) {
-                dump.append(BYTE2CHAR[buf.getUnsignedByte(j)]);
-            }
-            dump.append(BYTEPADDING[remainder])
-            .append('|');
-        }
-
-        dump.append(
-                NEWLINE + "+--------+-------------------------------------------------+----------------+");
-
-        return dump.toString();
     }
 
     /**
@@ -381,7 +304,24 @@ public class LoggingHandler extends ChannelDuplexHandler {
      * using the content of the {@link ByteBufHolder}. Sub-classes may override this.
      */
     protected String formatByteBufHolder(String eventName, ByteBufHolder msg) {
-        return formatByteBuf(eventName, msg.content());
+        String msgStr = msg.toString();
+        ByteBuf content = msg.content();
+        int length = content.readableBytes();
+        if (length == 0) {
+            StringBuilder buf = new StringBuilder(eventName.length() + 2 + msgStr.length() + 4);
+            buf.append(eventName).append(", ").append(msgStr).append(", 0B");
+            return buf.toString();
+        } else {
+            int rows = length / 16 + (length % 15 == 0? 0 : 1) + 4;
+            StringBuilder buf = new StringBuilder(
+                    eventName.length() + 2 + msgStr.length() + 2 + 10 + 1 + 2 + rows * 80);
+
+            buf.append(eventName).append(": ")
+               .append(msgStr).append(", ").append(length).append('B').append(NEWLINE);
+            appendPrettyHexDump(buf, content);
+
+            return buf.toString();
+        }
     }
 }
 
