c4ef1f3478cc8d64e16f78038b3915ffeabc96a1
Trustin Lee <t@motd.kr>
2015-07-12 19:34:05 +0900

    Provide more control over DnsNameResolver.query() / Add NameResolver.resolveAll()
    Related issues:
    - #3971
    - #3973
    - #3976
    - #4035
    
    Motivation:
    
    1. Previously, DnsNameResolver.query() retried the request query by its
    own. It prevents a user from deciding when to retry or stop. It is also
    impossible to get the response object whose code is not NOERROR.
    
    2. NameResolver does not have an operation that resolves a host name
    into multiple addresses, like InetAddress.getAllByName()
    
    Modifications:
    
    - Changes related with DnsNameResolver.query()
      - Make query() not retry
        - Move the retry logic to DnsNameResolver.resolve() instead.
      - Make query() fail the promise only when I/O error occurred or it
        failed to get a response
      - Add DnsNameResolverException and use it when query() fails so that
        the resolver can give more information about the failure
      - query() does not cache anymore.
    
    - Changes related with NameResolver.resolveAll()
      - Add NameResolver.resolveAll()
      - Add SimpleNameResolver.doResolveAll()
    
    - Changes related with DnsNameResolver.resolve() and resolveAll()
      - Make DnsNameResolveContext abstract so that DnsNameResolver can
        decide to get single or multiple addresses from it
      - Re-implement cache so that the cache works for resolve() and
        resolveAll()
      - Add 'traceEnabled' property to enable/disable trace information
    
    - Miscellaneous changes
      - Use ObjectUtil.checkNotNull() wherever possible
      - Add InternetProtocolFamily.addressType() to remove repetitive
        switch-case blocks in DnsNameResolver(Context)
      - Do not raise an exception when decoding a truncated DNS response
    
    Result:
    
    - Full control over query()
    - A user can now retrieve all addresses via (Dns)NameResolver.resolveAll()
    - DNS cache works only for resolve() and resolveAll() now.

---
+++ resolver-dns/src/main/java/io/netty/resolver/dns/DnsNameResolverException.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2015 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.resolver.dns;
+
+import io.netty.handler.codec.dns.DnsQuestion;
+import io.netty.util.internal.EmptyArrays;
+import io.netty.util.internal.ObjectUtil;
+
+import java.net.InetSocketAddress;
+
+/**
+ * A {@link RuntimeException} raised when {@link DnsNameResolver} failed to perform a successful query.
+ */
+public final class DnsNameResolverException extends RuntimeException {
+
+    private static final long serialVersionUID = -8826717909627131850L;
+
+    private final InetSocketAddress remoteAddress;
+    private final DnsQuestion question;
+
+    public DnsNameResolverException(InetSocketAddress remoteAddress, DnsQuestion question) {
+        this.remoteAddress = validateRemoteAddress(remoteAddress);
+        this.question = validateQuestion(question);
+    }
+
+    public DnsNameResolverException(InetSocketAddress remoteAddress, DnsQuestion question, String message) {
+        super(message);
+        this.remoteAddress = validateRemoteAddress(remoteAddress);
+        this.question = validateQuestion(question);
+    }
+
+    public DnsNameResolverException(
+            InetSocketAddress remoteAddress, DnsQuestion question, String message, Throwable cause) {
+        super(message, cause);
+        this.remoteAddress = validateRemoteAddress(remoteAddress);
+        this.question = validateQuestion(question);
+    }
+
+    public DnsNameResolverException(InetSocketAddress remoteAddress, DnsQuestion question, Throwable cause) {
+        super(cause);
+        this.remoteAddress = validateRemoteAddress(remoteAddress);
+        this.question = validateQuestion(question);
+    }
+
+    private static InetSocketAddress validateRemoteAddress(InetSocketAddress remoteAddress) {
+        return ObjectUtil.checkNotNull(remoteAddress, "remoteAddress");
+    }
+
+    private static DnsQuestion validateQuestion(DnsQuestion question) {
+        return ObjectUtil.checkNotNull(question, "question");
+    }
+
+    /**
+     * Returns the {@link DnsQuestion} of the DNS query that has failed.
+     */
+    public DnsQuestion question() {
+        return question;
+    }
+
+    @Override
+    public Throwable fillInStackTrace() {
+        setStackTrace(EmptyArrays.EMPTY_STACK_TRACE);
+        return this;
+    }
+}
