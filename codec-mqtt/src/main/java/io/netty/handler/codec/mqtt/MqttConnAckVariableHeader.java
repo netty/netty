6ff66fe967ed1aa45f2e87d263463f35c959b5cf
Francisco A. Lozano <flozano@gmail.com>
2015-07-29 14:00:18 +0200

    [#4041] Added sessionPresent to MqttConnAckVariableHeader
    Motivation:
    As per http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/os/mqtt-v3.1.1-os.html#_Toc398718033
    
    Modification:
    
    Add isSessionPresent() method and constructor to set it
    
    Result:
    Be able to use it with mqtt 3.1.1

--- codec-mqtt/src/main/java/io/netty/handler/codec/mqtt/MqttConnAckVariableHeader.java
+++ codec-mqtt/src/main/java/io/netty/handler/codec/mqtt/MqttConnAckVariableHeader.java
@@ -25,19 +25,25 @@ public class MqttConnAckVariableHeader {
 
     private final MqttConnectReturnCode connectReturnCode;
 
-    public MqttConnAckVariableHeader(MqttConnectReturnCode connectReturnCode) {
+    private final boolean sessionPresent;
+
+    public MqttConnAckVariableHeader(MqttConnectReturnCode connectReturnCode, boolean sessionPresent) {
         this.connectReturnCode = connectReturnCode;
+        this.sessionPresent = sessionPresent;
     }
 
     public MqttConnectReturnCode connectReturnCode() {
         return connectReturnCode;
     }
 
+    public boolean isSessionPresent() { return sessionPresent; }
+
     @Override
     public String toString() {
         return new StringBuilder(StringUtil.simpleClassName(this))
             .append('[')
             .append("connectReturnCode=").append(connectReturnCode)
+            .append(", sessionPresent=").append(sessionPresent)
             .append(']')
             .toString();
     }
