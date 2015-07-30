3056b80602c2f1636d7156cfc6dc67e05d7f105a
Scott Mitchell <scott_mitchell@apple.com>
2015-07-30 10:33:10 -0700

    Microbench backport issue
    Motivation:
    The microbench code in 4.0 lives in src/test while in 4.1 and master it lives in src/main. A backport of a patch did not account for this.
    
    Modifications:
    - Move the benchmark to the src/test directory
    - Update new benchmark package info
    
    Result:
    4.0 branch can now build again.

--- microbench/src/test/java/io/netty/microbench/common/AppendableCharSequenceBenchmark.java
+++ microbench/src/test/java/io/netty/microbench/common/AppendableCharSequenceBenchmark.java
@@ -12,7 +12,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package io.netty.microbenchmark.common;
+package io.netty.microbench.common;
 
 import java.util.Random;
 
