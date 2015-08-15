452818ca975a16cced051cfc63a6f7cd7cc1b381
Jonas Konrad <me@yawk.at>
2015-08-15 23:19:03 +0200

    Add support for abstract domain sockets
    Motivation:
    
    Because of java custom UTF encoding, it was previously impossible to use
    nul-bytes in domain socket names, which is required for abstract domain
    sockets.
    
    Modifications:
    
    - Pass the encoded string byte array to the native code
    - Modify native code accordingly to work with nul-bytes in the the
    array.
    - Move the string encoding to UTF-8 in java code.
    
    Result:
    
    Unix domain socket addresses will work properly if they contain nul-
    bytes. Address encoding for these addresses changes from UTF-8-like to
    real UTF-8.

---
+++ transport-native-epoll/src/test/java/io/netty/channel/epoll/EpollAbstractDomainSocketEchoTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2015 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.channel.epoll;
+
+import io.netty.channel.unix.DomainSocketAddress;
+import java.net.SocketAddress;
+import java.util.UUID;
+
+public class EpollAbstractDomainSocketEchoTest extends EpollDomainSocketEchoTest {
+    @Override
+    protected SocketAddress newSocketAddress() {
+        // these don't actually show up in the file system so creating a temp file isn't reliable
+        return new DomainSocketAddress("\0/tmp/" + UUID.randomUUID());
+    }
+}
