7d406a8326be3f758f4e147d98b5b0fd28f0cea4
Scott Mitchell <scott_mitchell@apple.com>
2015-08-19 12:42:31 -0700

    HTTP to HTTP/2 tranlation errors
    Motivation:
    HttpUtil.toHttp2Headers is currently not translating HTTP request headers to HTTP/2 request headers correctly.  The path, scheme, and authority are tranlation process are not respecting the HTTP/2 RFC https://tools.ietf.org/html/rfc7540#section-8.1.2.3 and HTTP RFC https://tools.ietf.org/html/rfc7230#section-5.3.
    
    Modifications:
    - path, scheme, authority must be set according to rules defined in https://tools.ietf.org/html/rfc7540#section-8.1.2.3
    - HTTP/1.x URIs must be handled as defined in https://tools.ietf.org/html/rfc7230#section-5.3
    
    Result:
    More correct translation from HTTP/1.x requests to HTTP/2 requests.

--- codec-http/src/main/java/io/netty/handler/codec/http/HttpMethod.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/HttpMethod.java
@@ -15,78 +15,76 @@
  */
 package io.netty.handler.codec.http;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.util.CharsetUtil;
+import static io.netty.util.internal.ObjectUtil.checkNotNull;
+import io.netty.util.AsciiString;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static io.netty.util.internal.ObjectUtil.checkNotNull;
-
 /**
- * The request getMethod of HTTP or its derived protocols, such as
+ * The request method of HTTP or its derived protocols, such as
  * <a href="http://en.wikipedia.org/wiki/Real_Time_Streaming_Protocol">RTSP</a> and
  * <a href="http://en.wikipedia.org/wiki/Internet_Content_Adaptation_Protocol">ICAP</a>.
  */
 public class HttpMethod implements Comparable<HttpMethod> {
     /**
-     * The OPTIONS getMethod represents a request for information about the communication options
-     * available on the request/response chain identified by the Request-URI. This getMethod allows
+     * The OPTIONS method represents a request for information about the communication options
+     * available on the request/response chain identified by the Request-URI. This method allows
      * the client to determine the options and/or requirements associated with a resource, or the
      * capabilities of a server, without implying a resource action or initiating a resource
      * retrieval.
      */
-    public static final HttpMethod OPTIONS = new HttpMethod("OPTIONS", true);
+    public static final HttpMethod OPTIONS = new HttpMethod("OPTIONS");
 
     /**
-     * The GET getMethod means retrieve whatever information (in the form of an entity) is identified
+     * The GET method means retrieve whatever information (in the form of an entity) is identified
      * by the Request-URI.  If the Request-URI refers to a data-producing process, it is the
      * produced data which shall be returned as the entity in the response and not the source text
      * of the process, unless that text happens to be the output of the process.
      */
-    public static final HttpMethod GET = new HttpMethod("GET", true);
+    public static final HttpMethod GET = new HttpMethod("GET");
 
     /**
-     * The HEAD getMethod is identical to GET except that the server MUST NOT return a message-body
+     * The HEAD method is identical to GET except that the server MUST NOT return a message-body
      * in the response.
      */
-    public static final HttpMethod HEAD = new HttpMethod("HEAD", true);
+    public static final HttpMethod HEAD = new HttpMethod("HEAD");
 
     /**
-     * The POST getMethod is used to request that the origin server accept the entity enclosed in the
+     * The POST method is used to request that the origin server accept the entity enclosed in the
      * request as a new subordinate of the resource identified by the Request-URI in the
      * Request-Line.
      */
-    public static final HttpMethod POST = new HttpMethod("POST", true);
+    public static final HttpMethod POST = new HttpMethod("POST");
 
     /**
-     * The PUT getMethod requests that the enclosed entity be stored under the supplied Request-URI.
+     * The PUT method requests that the enclosed entity be stored under the supplied Request-URI.
      */
-    public static final HttpMethod PUT = new HttpMethod("PUT", true);
+    public static final HttpMethod PUT = new HttpMethod("PUT");
 
     /**
-     * The PATCH getMethod requests that a set of changes described in the
+     * The PATCH method requests that a set of changes described in the
      * request entity be applied to the resource identified by the Request-URI.
      */
-    public static final HttpMethod PATCH = new HttpMethod("PATCH", true);
+    public static final HttpMethod PATCH = new HttpMethod("PATCH");
 
     /**
-     * The DELETE getMethod requests that the origin server delete the resource identified by the
+     * The DELETE method requests that the origin server delete the resource identified by the
      * Request-URI.
      */
-    public static final HttpMethod DELETE = new HttpMethod("DELETE", true);
+    public static final HttpMethod DELETE = new HttpMethod("DELETE");
 
     /**
-     * The TRACE getMethod is used to invoke a remote, application-layer loop- back of the request
+     * The TRACE method is used to invoke a remote, application-layer loop- back of the request
      * message.
      */
-    public static final HttpMethod TRACE = new HttpMethod("TRACE", true);
+    public static final HttpMethod TRACE = new HttpMethod("TRACE");
 
     /**
-     * This specification reserves the getMethod name CONNECT for use with a proxy that can dynamically
+     * This specification reserves the method name CONNECT for use with a proxy that can dynamically
      * switch to being a tunnel
      */
-    public static final HttpMethod CONNECT = new HttpMethod("CONNECT", true);
+    public static final HttpMethod CONNECT = new HttpMethod("CONNECT");
 
     private static final Map<String, HttpMethod> methodMap = new HashMap<String, HttpMethod>();
 
@@ -104,7 +102,7 @@ public class HttpMethod implements Comparable<HttpMethod> {
 
     /**
      * Returns the {@link HttpMethod} represented by the specified name.
-     * If the specified name is a standard HTTP getMethod name, a cached instance
+     * If the specified name is a standard HTTP method name, a cached instance
      * will be returned.  Otherwise, a new instance will be returned.
      */
     public static HttpMethod valueOf(String name) {
@@ -112,25 +110,16 @@ public class HttpMethod implements Comparable<HttpMethod> {
         return result != null ? result : new HttpMethod(name);
     }
 
-    private final String name;
-    private final byte[] bytes;
+    private final AsciiString name;
 
     /**
-     * Creates a new HTTP getMethod with the specified name.  You will not need to
-     * create a new getMethod unless you are implementing a protocol derived from
+     * Creates a new HTTP method with the specified name.  You will not need to
+     * create a new method unless you are implementing a protocol derived from
      * HTTP, such as
      * <a href="http://en.wikipedia.org/wiki/Real_Time_Streaming_Protocol">RTSP</a> and
      * <a href="http://en.wikipedia.org/wiki/Internet_Content_Adaptation_Protocol">ICAP</a>
      */
     public HttpMethod(String name) {
-        this(name, false);
-    }
-
-    private HttpMethod(String name, boolean bytes) {
-        if (name == null) {
-            throw new NullPointerException("name");
-        }
-
         name = checkNotNull(name, "name").trim();
         if (name.isEmpty()) {
             throw new IllegalArgumentException("empty name");
@@ -143,18 +132,20 @@ public class HttpMethod implements Comparable<HttpMethod> {
             }
         }
 
-        this.name = name;
-        if (bytes) {
-            this.bytes = name.getBytes(CharsetUtil.US_ASCII);
-        } else {
-            this.bytes = null;
-        }
+        this.name = new AsciiString(name);
     }
 
     /**
-     * Returns the name of this getMethod.
+     * Returns the name of this method.
      */
     public String name() {
+        return name.toString();
+    }
+
+    /**
+     * Returns the name of this method.
+     */
+    public AsciiString asciiName() {
         return name;
     }
 
@@ -175,19 +166,11 @@ public class HttpMethod implements Comparable<HttpMethod> {
 
     @Override
     public String toString() {
-        return name();
+        return name.toString();
     }
 
     @Override
     public int compareTo(HttpMethod o) {
         return name().compareTo(o.name());
-    }
-
-    void encode(ByteBuf buf) {
-        if (bytes == null) {
-            HttpHeaders.encodeAscii0(name, buf);
-        } else {
-            buf.writeBytes(bytes);
-        }
     }
 }
