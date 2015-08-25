13152576d2a1c39f97318ad55d8fef608baf5800
Norman Maurer <norman_maurer@apple.com>
2015-08-25 12:20:23 +0200

    Allow to get details of the Thread that powers a SingleThreadEventExecutor.
    Motivation:
    
    for debugging and metrics reasons its sometimes useful to be able to get details of the the Thread that powers a SingleThreadEventExecutor.
    
    Modifications:
    
    - Expose ThreadProperties
    - Add unit test.
    
    Result:
    
    It's now possible to get details of the Thread that powers a SingleThreadEventExecutor.

---
+++ common/src/test/java/io/netty/util/concurrent/SingleThreadEventExecutorTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2015 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.util.concurrent;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class SingleThreadEventExecutorTest {
+
+    @Test
+    public void testThreadDetails() {
+        final AtomicReference<Thread> threadRef = new AtomicReference<Thread>();
+        SingleThreadEventExecutor executor = new SingleThreadEventExecutor(
+                null, new DefaultThreadFactory("test"), false) {
+            @Override
+            protected void run() {
+                threadRef.set(Thread.currentThread());
+                while (!confirmShutdown()) {
+                    Runnable task = takeTask();
+                    if (task != null) {
+                        task.run();
+                    }
+                }
+            }
+        };
+        ThreadProperties threadProperties = executor.threadProperties();
+        Assert.assertSame(threadProperties, executor.threadProperties());
+
+        Thread thread = threadRef.get();
+        Assert.assertEquals(thread.getId(), threadProperties.id());
+        Assert.assertEquals(thread.getName(), threadProperties.name());
+        Assert.assertEquals(thread.getPriority(), threadProperties.priority());
+        Assert.assertEquals(thread.getState(), threadProperties.state());
+        Assert.assertEquals(thread.isAlive(), threadProperties.isAlive());
+        Assert.assertEquals(thread.isDaemon(), threadProperties.isDaemon());
+        Assert.assertEquals(thread.isInterrupted(), threadProperties.isInterrupted());
+        Assert.assertTrue(threadProperties.stackTrace().length > 0);
+        executor.shutdownGracefully();
+    }
+}
