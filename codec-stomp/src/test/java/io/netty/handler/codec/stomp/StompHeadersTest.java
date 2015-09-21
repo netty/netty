002450f381c95290286117a76c03088b5fa77df6
Scott Mitchell <scott_mitchell@apple.com>
2015-09-21 16:20:28 -0700

    STOMP Headers Name Comparator
    Motivation:
    The HashingStrategy for DefaultStompHeaders was using the java .equals() method which would fail to compare String, AsciiString, and other CharSequence objects as equal.
    
    Modification:
    - Use AsciiString.CASE_SENSITIVE_HASHER for DefaultStompHeaders
    
    Result:
    DefaultStompHeaders work with all CharSequence objects.
    Fixes https://github.com/netty/netty/issues/4247

---
+++ codec-stomp/src/test/java/io/netty/handler/codec/stomp/StompHeadersTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2015 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.codec.stomp;
+
+import io.netty.util.AsciiString;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class StompHeadersTest {
+    @Test
+    public void testHeadersCaseSensative() {
+        DefaultStompHeaders headers = new DefaultStompHeaders();
+        AsciiString foo = new AsciiString("foo");
+        AsciiString value = new AsciiString("value");
+        headers.add(foo, value);
+        assertNull(headers.get("Foo"));
+        assertEquals(value, headers.get(foo));
+        assertEquals(value, headers.get(foo.toString()));
+    }
+}
