a406f69a73a3603acbd59ee5e00beb67abf8caae
Norman Maurer <norman_maurer@apple.com>
2015-09-30 15:04:06 +0200

    Fail build on warnings in the native transport
    Motivation:
    
    We should fail the build on warnings in the JNI/c code.
    
    Modifications:
    
    - Add GCC flag to fail build on warnings.
    - Fix warnings (which also fixed a bug when using splice with offsets).
    
    Result:
    
    Better code quality.

--- transport-native-epoll/src/main/c/io_netty_channel_epoll_Native.h
+++ transport-native-epoll/src/main/c/io_netty_channel_epoll_Native.h
@@ -130,7 +130,7 @@ jint Java_io_netty_channel_epoll_Native_sizeofEpollEvent(JNIEnv* env, jclass cla
 jint Java_io_netty_channel_epoll_Native_offsetofEpollData(JNIEnv* env, jclass clazz);
 
 jlong Java_io_netty_channel_epoll_Native_pipe0(JNIEnv* env, jclass clazz);
-jint Java_io_netty_channel_epoll_Native_splice0(JNIEnv* env, jclass clazz, jint fd, jint offIn, jint fdOut, jint offOut, jint len);
+jint Java_io_netty_channel_epoll_Native_splice0(JNIEnv* env, jclass clazz, jint fd, jlong offIn, jint fdOut, jlong offOut, jlong len);
 
 jint Java_io_netty_channel_epoll_Native_tcpMd5SigMaxKeyLen(JNIEnv* env, jclass clazz);
 void Java_io_netty_channel_epoll_Native_setTcpMd5Sig0(JNIEnv* env, jclass clazz, jint fd, jbyteArray address, jint scopeId, jbyteArray key);
