4fe6d9060850f5097d8e07cb132ca89adf957af9
Norman Maurer <norman_maurer@apple.com>
2015-10-09 21:03:03 +0200

    Add *UnsafeHeapByteBuf for improve performance on systems with sun.misc.Unsafe
    Motivation:
    
    sun.misc.Unsafe allows us to handle heap ByteBuf in a more efficient matter. We should use special ByteBuf implementation when sun.misc.Unsafe can be used to increase performance.
    
    Modifications:
    
    - Add PooledUnsafeHeapByteBuf and UnpooledUnsafeHeapByteBuf that are used when sun.misc.Unsafe is ready to use.
    - Add UnsafeHeapSwappedByteBuf
    
    Result:
    
    Better performance when using heap buffers and sun.misc.Unsafe is ready to use.

--- buffer/src/main/java/io/netty/buffer/UnsafeHeapSwappedByteBuf.java
+++ buffer/src/main/java/io/netty/buffer/UnsafeHeapSwappedByteBuf.java
@@ -19,49 +19,45 @@ package io.netty.buffer;
 import io.netty.util.internal.PlatformDependent;
 
 /**
- * Special {@link SwappedByteBuf} for {@link ByteBuf}s that are backed by a {@code memoryAddress}.
+ * Special {@link SwappedByteBuf} for {@link ByteBuf}s that use unsafe to access the byte array.
  */
-final class UnsafeDirectSwappedByteBuf extends AbstractUnsafeSwappedByteBuf {
+final class UnsafeHeapSwappedByteBuf extends AbstractUnsafeSwappedByteBuf {
 
-    UnsafeDirectSwappedByteBuf(AbstractByteBuf buf) {
+    UnsafeHeapSwappedByteBuf(AbstractByteBuf buf) {
         super(buf);
     }
 
-    private static long addr(AbstractByteBuf wrapped, int index) {
-        // We need to call wrapped.memoryAddress() everytime and NOT cache it as it may change if the buffer expand.
-        // See:
-        // - https://github.com/netty/netty/issues/2587
-        // - https://github.com/netty/netty/issues/2580
-        return wrapped.memoryAddress() + index;
+    private static int idx(ByteBuf wrapped, int index) {
+        return wrapped.arrayOffset() + index;
     }
 
     @Override
     protected long _getLong(AbstractByteBuf wrapped, int index) {
-        return PlatformDependent.getLong(addr(wrapped, index));
+        return PlatformDependent.getLong(wrapped.array(), idx(wrapped, index));
     }
 
     @Override
     protected int _getInt(AbstractByteBuf wrapped, int index) {
-        return PlatformDependent.getInt(addr(wrapped, index));
+        return PlatformDependent.getInt(wrapped.array(), idx(wrapped, index));
     }
 
     @Override
     protected short _getShort(AbstractByteBuf wrapped, int index) {
-        return PlatformDependent.getShort(addr(wrapped, index));
+        return PlatformDependent.getShort(wrapped.array(), idx(wrapped, index));
     }
 
     @Override
     protected void _setShort(AbstractByteBuf wrapped, int index, short value) {
-        PlatformDependent.putShort(addr(wrapped, index), value);
+        PlatformDependent.putShort(wrapped.array(), idx(wrapped, index), value);
     }
 
     @Override
     protected void _setInt(AbstractByteBuf wrapped, int index, int value) {
-        PlatformDependent.putInt(addr(wrapped, index), value);
+        PlatformDependent.putInt(wrapped.array(), idx(wrapped, index), value);
     }
 
     @Override
     protected void _setLong(AbstractByteBuf wrapped, int index, long value) {
-        PlatformDependent.putLong(addr(wrapped, index), value);
+        PlatformDependent.putLong(wrapped.array(), idx(wrapped, index), value);
     }
 }
