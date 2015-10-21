8b0b53955672a43e5007f763a1e0803403ff32fb
Norman Maurer <norman_maurer@apple.com>
2015-10-21 11:24:18 +0200

    Share code between Heap ByteBuf implementations
    Motiviation:
    
    We have a lot of duplicated code which makes it hard to maintain.
    
    Modification:
    
    Move shared code to HeapByteBufUtil and use it in the implementations.
    
    Result:
    
    Less duplicated code and so easier to maintain.

---
+++ buffer/src/main/java/io/netty/buffer/HeapByteBufUtil.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2015 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.buffer;
+
+/**
+ * Utility class for heap buffers.
+ */
+final class HeapByteBufUtil {
+
+    static byte getByte(byte[] memory, int index) {
+        return memory[index];
+    }
+
+    static short getShort(byte[] memory, int index) {
+        return (short) (memory[index] << 8 | memory[index + 1] & 0xFF);
+    }
+
+    static int getUnsignedMedium(byte[] memory, int index) {
+        return  (memory[index]     & 0xff) << 16 |
+                (memory[index + 1] & 0xff) <<  8 |
+                memory[index + 2] & 0xff;
+    }
+
+    static int getInt(byte[] memory, int index) {
+        return  (memory[index]     & 0xff) << 24 |
+                (memory[index + 1] & 0xff) << 16 |
+                (memory[index + 2] & 0xff) <<  8 |
+                memory[index + 3] & 0xff;
+    }
+
+    static long getLong(byte[] memory, int index) {
+        return  ((long) memory[index]     & 0xff) << 56 |
+                ((long) memory[index + 1] & 0xff) << 48 |
+                ((long) memory[index + 2] & 0xff) << 40 |
+                ((long) memory[index + 3] & 0xff) << 32 |
+                ((long) memory[index + 4] & 0xff) << 24 |
+                ((long) memory[index + 5] & 0xff) << 16 |
+                ((long) memory[index + 6] & 0xff) <<  8 |
+                (long) memory[index + 7] & 0xff;
+    }
+
+    static void setByte(byte[] memory, int index, int value) {
+        memory[index] = (byte) value;
+    }
+
+    static void setShort(byte[] memory, int index, int value) {
+        memory[index]     = (byte) (value >>> 8);
+        memory[index + 1] = (byte) value;
+    }
+
+    static void setMedium(byte[] memory, int index, int value) {
+        memory[index]     = (byte) (value >>> 16);
+        memory[index + 1] = (byte) (value >>> 8);
+        memory[index + 2] = (byte) value;
+    }
+
+    static void setInt(byte[] memory, int index, int value) {
+        memory[index]     = (byte) (value >>> 24);
+        memory[index + 1] = (byte) (value >>> 16);
+        memory[index + 2] = (byte) (value >>> 8);
+        memory[index + 3] = (byte) value;
+    }
+
+    static void setLong(byte[] memory, int index, long value) {
+        memory[index]     = (byte) (value >>> 56);
+        memory[index + 1] = (byte) (value >>> 48);
+        memory[index + 2] = (byte) (value >>> 40);
+        memory[index + 3] = (byte) (value >>> 32);
+        memory[index + 4] = (byte) (value >>> 24);
+        memory[index + 5] = (byte) (value >>> 16);
+        memory[index + 6] = (byte) (value >>> 8);
+        memory[index + 7] = (byte) value;
+    }
+
+    private HeapByteBufUtil() { }
+}
