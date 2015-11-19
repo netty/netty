23b28e9e1282da1e0b79a06becfe91c4e7bcbf18
pieteradejong <pieter.a.dejong@gmail.com>
2015-11-19 00:02:45 -0800

    fixed word - issue #4469

--- transport/src/main/java/io/netty/channel/EventLoop.java
+++ transport/src/main/java/io/netty/channel/EventLoop.java
@@ -21,7 +21,7 @@ import io.netty.util.concurrent.EventExecutor;
 /**
  * Will handle all the I/O operations for a {@link Channel} once registered.
  *
- * One {@link EventLoop} instance will usually handle more then one {@link Channel} but this may depend on
+ * One {@link EventLoop} instance will usually handle more than one {@link Channel} but this may depend on
  * implementation details and internals.
  *
  */
