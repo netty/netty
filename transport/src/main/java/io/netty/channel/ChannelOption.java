47d35f89cb5fcc4581969c2392e09529c4b1ba39
Scott Mitchell <scott_mitchell@apple.com>
2015-11-24 15:21:29 -0800

    Cleanup ChannelOption.AUTO_CLOSE javadocs
    Motivation:
    The javadocs for ChannelOption.AUTO_CLOSE say the default is false, but the default is currently true.
    
    Modifications:
    - Make javadocs consistent with code
    
    Result:
    Less confusing docs.

--- transport/src/main/java/io/netty/channel/ChannelOption.java
+++ transport/src/main/java/io/netty/channel/ChannelOption.java
@@ -55,8 +55,8 @@ public class ChannelOption<T> extends UniqueName {
     /**
      * @deprecated From version 5.0, {@link Channel} will not be closed on write failure.
      *
-     * {@code true} if and only if the {@link Channel} is closed automatically and immediately on write failure.
-     * The default is {@code false}.
+     * If {@code true} then the {@link Channel} is closed automatically and immediately on write failure.
+     * The default value is {@code true}.
      */
     @Deprecated
     public static final ChannelOption<Boolean> AUTO_CLOSE = valueOf("AUTO_CLOSE");
