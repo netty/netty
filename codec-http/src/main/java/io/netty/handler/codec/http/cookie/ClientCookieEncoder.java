4154ea08f9293bdd6eca6bbb3c7d9c649c08c1c8
Luke Hutchison <luke.hutch@gmail.com>
2015-11-26 03:51:52 -0800

    Make cookie encoding conform better to RFC 6265 in STRICT mode.
    Motivation:
    
    - On the client, cookies should be sorted in decreasing order of path
      length. From RFC 6265:
    
          5.4.2. The user agent SHOULD sort the cookie-list in the following
          order:
    
            *  Cookies with longer paths are listed before cookies with
               shorter paths.
    
            *  Among cookies that have equal-length path fields, cookies with
               earlier creation-times are listed before cookies with later
               creation-times.
    
          NOTE: Not all user agents sort the cookie-list in this order, but
          this order reflects common practice when this document was
          written, and, historically, there have been servers that
          (erroneously) depended on this order.
    
      Note that the RFC does not define the path length of cookies without a
      path. We sort pathless cookies before cookies with the longest path,
      since pathless cookies inherit the request path (and setting a path
      that is longer than the request path is of limited use, since it cannot
      be read from the context in which it is written).
    
    - On the server, if there are multiple cookies of the same name, only one
      of them should be encoded. RFC 6265 says:
    
          Servers SHOULD NOT include more than one Set-Cookie header field in
          the same response with the same cookie-name.
    
      Note that the RFC does not define which cookie should be set in the case
      of multiple cookies with the same name; we arbitrarily pick the last one.
    
    Modifications:
    
    - Changed the visibility of the 'strict' field to 'protected' in
      CookieEncoder.
    
    - Modified ClientCookieEncoder to sort cookies in decreasing order of path
      length when in strict mode.
    
    - Modified ServerCookieEncoder to return only the last cookie of a given
      name when in strict mode.
    
    - Added a fast path for both strict mode in both client and server code
      for cases with only one cookie, in order avoid the overhead of sorting
      and memory allocation.
    
    - Added unit tests for the new cases.
    
    Result:
    
    - Cookie generation on client and server is now more conformant to RFC 6265.

--- codec-http/src/main/java/io/netty/handler/codec/http/cookie/ClientCookieEncoder.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/cookie/ClientCookieEncoder.java
@@ -15,16 +15,24 @@
  */
 package io.netty.handler.codec.http.cookie;
 
-import static io.netty.handler.codec.http.cookie.CookieUtil.*;
+import static io.netty.handler.codec.http.cookie.CookieUtil.add;
+import static io.netty.handler.codec.http.cookie.CookieUtil.addQuoted;
+import static io.netty.handler.codec.http.cookie.CookieUtil.stringBuilder;
+import static io.netty.handler.codec.http.cookie.CookieUtil.stripTrailingSeparator;
+import static io.netty.handler.codec.http.cookie.CookieUtil.stripTrailingSeparatorOrNull;
 import static io.netty.util.internal.ObjectUtil.checkNotNull;
-
-import java.util.Iterator;
-
 import io.netty.handler.codec.http.HttpRequest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
 /**
- * A <a href="http://tools.ietf.org/html/rfc6265">RFC6265</a> compliant cookie encoder to be used client side,
- * so only name=value pairs are sent.
+ * A <a href="http://tools.ietf.org/html/rfc6265">RFC6265</a> compliant cookie encoder to be used client side, so
+ * only name=value pairs are sent.
  *
  * User-Agents are not supposed to interpret cookies, so, if present, {@link Cookie#rawValue()} will be used.
  * Otherwise, {@link Cookie#value()} will be used unquoted.
@@ -42,13 +50,14 @@ import io.netty.handler.codec.http.HttpRequest;
 public final class ClientCookieEncoder extends CookieEncoder {
 
     /**
-     * Strict encoder that validates that name and value chars are in the valid scope
-     * defined in RFC6265
+     * Strict encoder that validates that name and value chars are in the valid scope and (for methods that accept
+     * multiple cookies) sorts cookies into order of decreasing path length, as specified in RFC6265.
      */
     public static final ClientCookieEncoder STRICT = new ClientCookieEncoder(true);
 
     /**
-     * Lax instance that doesn't validate name and value
+     * Lax instance that doesn't validate name and value, and (for methods that accept multiple cookies) keeps
+     * cookies in the order in which they were given.
      */
     public static final ClientCookieEncoder LAX = new ClientCookieEncoder(false);
 
@@ -59,8 +68,10 @@ public final class ClientCookieEncoder extends CookieEncoder {
     /**
      * Encodes the specified cookie into a Cookie header value.
      *
-     * @param name the cookie name
-     * @param value the cookie value
+     * @param name
+     *            the cookie name
+     * @param value
+     *            the cookie value
      * @return a Rfc6265 style Cookie header value
      */
     public String encode(String name, String value) {
@@ -70,7 +81,8 @@ public final class ClientCookieEncoder extends CookieEncoder {
     /**
      * Encodes the specified cookie into a Cookie header value.
      *
-     * @param specified the cookie
+     * @param specified
+     *            the cookie
      * @return a Rfc6265 style Cookie header value
      */
     public String encode(Cookie cookie) {
@@ -80,9 +92,36 @@ public final class ClientCookieEncoder extends CookieEncoder {
     }
 
     /**
+     * Sort cookies into decreasing order of path length, breaking ties by sorting into increasing chronological
+     * order of creation time, as recommended by RFC 6265.
+     */
+    private static final Comparator<Cookie> COOKIE_COMPARATOR = new Comparator<Cookie>() {
+        @Override
+        public int compare(Cookie c1, Cookie c2) {
+            String path1 = c1.path();
+            String path2 = c2.path();
+            // Cookies with unspecified path default to the path of the request. We don't
+            // know the request path here, but we assume that the length of an unspecified
+            // path is longer than any specified path (i.e. pathless cookies come first),
+            // because setting cookies with a path longer than the request path is of
+            // limited use.
+            int len1 = path1 == null ? Integer.MAX_VALUE : path1.length();
+            int len2 = path2 == null ? Integer.MAX_VALUE : path2.length();
+            int diff = len2 - len1;
+            if (diff != 0) {
+                return diff;
+            }
+            // Rely on Java's sort stability to retain creation order in cases where
+            // cookies have same path length.
+            return -1;
+        }
+    };
+
+    /**
      * Encodes the specified cookies into a single Cookie header value.
      *
-     * @param cookies some cookies
+     * @param cookies
+     *            some cookies
      * @return a Rfc6265 style Cookie header value, null if no cookies are passed.
      */
     public String encode(Cookie... cookies) {
@@ -91,12 +130,51 @@ public final class ClientCookieEncoder extends CookieEncoder {
         }
 
         StringBuilder buf = stringBuilder();
-        for (Cookie c : cookies) {
-            if (c == null) {
-                break;
+        if (strict) {
+            if (cookies.length == 1) {
+                encode(buf, cookies[0]);
+            } else {
+                Cookie[] cookiesSorted = Arrays.copyOf(cookies, cookies.length);
+                Arrays.sort(cookiesSorted, COOKIE_COMPARATOR);
+                for (Cookie c : cookiesSorted) {
+                    encode(buf, c);
+                }
             }
+        } else {
+            for (Cookie c : cookies) {
+                encode(buf, c);
+            }
+        }
+        return stripTrailingSeparatorOrNull(buf);
+    }
 
-            encode(buf, c);
+    /**
+     * Encodes the specified cookies into a single Cookie header value.
+     *
+     * @param cookies
+     *            some cookies
+     * @return a Rfc6265 style Cookie header value, null if no cookies are passed.
+     */
+    public String encode(Collection<? extends Cookie> cookies) {
+        if (checkNotNull(cookies, "cookies").isEmpty()) {
+            return null;
+        }
+
+        StringBuilder buf = stringBuilder();
+        if (strict) {
+            if (cookies.size() == 1) {
+                encode(buf, cookies.iterator().next());
+            } else {
+                Cookie[] cookiesSorted = cookies.toArray(new Cookie[cookies.size()]);
+                Arrays.sort(cookiesSorted, COOKIE_COMPARATOR);
+                for (Cookie c : cookiesSorted) {
+                    encode(buf, c);
+                }
+            }
+        } else {
+            for (Cookie c : cookies) {
+                encode(buf, c);
+            }
         }
         return stripTrailingSeparatorOrNull(buf);
     }
@@ -114,13 +192,26 @@ public final class ClientCookieEncoder extends CookieEncoder {
         }
 
         StringBuilder buf = stringBuilder();
-        while (cookiesIt.hasNext()) {
-            Cookie c = cookiesIt.next();
-            if (c == null) {
-                break;
+        if (strict) {
+            Cookie firstCookie = cookiesIt.next();
+            if (!cookiesIt.hasNext()) {
+                encode(buf, firstCookie);
+            } else {
+                List<Cookie> cookiesList = new ArrayList<Cookie>();
+                cookiesList.add(firstCookie);
+                while (cookiesIt.hasNext()) {
+                    cookiesList.add(cookiesIt.next());
+                }
+                Cookie[] cookiesSorted = cookiesList.toArray(new Cookie[cookiesList.size()]);
+                Arrays.sort(cookiesSorted, COOKIE_COMPARATOR);
+                for (Cookie c : cookiesSorted) {
+                    encode(buf, c);
+                }
             }
-
-            encode(buf, c);
+        } else {
+            while (cookiesIt.hasNext()) {
+                encode(buf, cookiesIt.next());
+            }
         }
         return stripTrailingSeparatorOrNull(buf);
     }
