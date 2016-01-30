7f19856fcc68c32df4bcf7e8acd7e53dc122dbc0
Xiaoyan Lin <linxiaoyan18@gmail.com>
2016-01-30 15:53:11 -0800

    Use AbstractReferenceCounted to clean up the codes for codec-memcache
    Motivation:
    
    Some duplicated methods in message types of codec-memcache can be cleaned using AbstractReferenceCounted.
    
    Modifications:
    
    Use AbstractReferenceCounted to avoid duplicated methods.
    
    Result:
    
    Duplicated methods are cleaned.

--- codec-memcache/src/main/java/io/netty/handler/codec/memcache/binary/DefaultFullBinaryMemcacheResponse.java
+++ codec-memcache/src/main/java/io/netty/handler/codec/memcache/binary/DefaultFullBinaryMemcacheResponse.java
@@ -59,28 +59,20 @@ public class DefaultFullBinaryMemcacheResponse extends DefaultBinaryMemcacheResp
     }
 
     @Override
-    public int refCnt() {
-        return content.refCnt();
-    }
-
-    @Override
     public FullBinaryMemcacheResponse retain() {
         super.retain();
-        content.retain();
         return this;
     }
 
     @Override
     public FullBinaryMemcacheResponse retain(int increment) {
         super.retain(increment);
-        content.retain(increment);
         return this;
     }
 
     @Override
     public FullBinaryMemcacheResponse touch() {
         super.touch();
-        content.touch();
         return this;
     }
 
@@ -92,15 +84,9 @@ public class DefaultFullBinaryMemcacheResponse extends DefaultBinaryMemcacheResp
     }
 
     @Override
-    public boolean release() {
-        super.release();
-        return content.release();
-    }
-
-    @Override
-    public boolean release(int decrement) {
-        super.release(decrement);
-        return content.release(decrement);
+    protected void deallocate() {
+        super.deallocate();
+        content.release();
     }
 
     @Override
