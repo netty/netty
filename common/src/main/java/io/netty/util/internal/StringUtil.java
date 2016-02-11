a1d08e3f580cda45c9bd2ae6bbde82567a65d50d
Xiaoyan Lin <linxiaoyan18@gmail.com>
2016-02-10 20:32:27 -0800

    Add unescapeCsvFields to parse a CSV line and implement CombinedHttpHeaders.getAll
    Motivation:
    
    See #4855
    
    Modifications:
    
    Unfortunately, unescapeCsv cannot be used here because the input could be a CSV line like `"a,b",c`. Hence this patch adds unescapeCsvFields to parse a CSV line and split it into multiple fields and unescaped them. The unit tests should define the behavior of unescapeCsvFields.
    
    Then this patch just uses unescapeCsvFields to implement `CombinedHttpHeaders.getAll`.
    
    Result:
    
    `CombinedHttpHeaders.getAll` will return the unescaped values of a header.

--- common/src/main/java/io/netty/util/internal/StringUtil.java
+++ common/src/main/java/io/netty/util/internal/StringUtil.java
@@ -16,6 +16,7 @@
 package io.netty.util.internal;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Formatter;
 import java.util.List;
 
@@ -425,6 +426,76 @@ public final class StringUtil {
     }
 
     /**
+     * Unescapes the specified escaped CSV fields according to
+     * <a href="https://tools.ietf.org/html/rfc4180#section-2">RFC-4180</a>.
+     *
+     * @param value A string with multiple CSV escaped fields which will be unescaped according to
+     *              <a href="https://tools.ietf.org/html/rfc4180#section-2">RFC-4180</a>
+     * @return {@link List} the list of unescaped fields
+     */
+    public static List<CharSequence> unescapeCsvFields(CharSequence value) {
+        List<CharSequence> unescaped = new ArrayList<CharSequence>(2);
+        StringBuilder current = InternalThreadLocalMap.get().stringBuilder();
+        boolean quoted = false;
+        int last = value.length() - 1;
+        for (int i = 0; i <= last; i++) {
+            char c = value.charAt(i);
+            if (quoted) {
+                switch (c) {
+                    case DOUBLE_QUOTE:
+                        if (i == last) {
+                            // Add the last field and return
+                            unescaped.add(current.toString());
+                            return unescaped;
+                        }
+                        char next = value.charAt(++i);
+                        if (next == DOUBLE_QUOTE) {
+                            // 2 double-quotes should be unescaped to one
+                            current.append(DOUBLE_QUOTE);
+                            break;
+                        }
+                        if (next == COMMA) {
+                            // This is the end of a field. Let's start to parse the next field.
+                            quoted = false;
+                            unescaped.add(current.toString());
+                            current.setLength(0);
+                            break;
+                        }
+                        // double-quote followed by other character is invalid
+                        throw newInvalidEscapedCsvFieldException(value, i - 1);
+                    default:
+                        current.append(c);
+                }
+            } else {
+                switch (c) {
+                    case COMMA:
+                        // Start to parse the next field
+                        unescaped.add(current.toString());
+                        current.setLength(0);
+                        break;
+                    case DOUBLE_QUOTE:
+                        if (current.length() == 0) {
+                            quoted = true;
+                            break;
+                        }
+                        // double-quote appears without being enclosed with double-quotes
+                    case LINE_FEED:
+                    case CARRIAGE_RETURN:
+                        // special characters appears without being enclosed with double-quotes
+                        throw newInvalidEscapedCsvFieldException(value, i);
+                    default:
+                        current.append(c);
+                }
+            }
+        }
+        if (quoted) {
+            throw newInvalidEscapedCsvFieldException(value, last);
+        }
+        unescaped.add(current.toString());
+        return unescaped;
+    }
+
+    /**s
      * Validate if {@code value} is a valid csv field without double-quotes.
      *
      * @throws IllegalArgumentException if {@code value} needs to be encoded with double-quotes.
