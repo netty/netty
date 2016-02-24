0d3eda38e15582d1dac05920787f1c9f3a9f781e
Dmitry Spikhalskiy <dmitry@spikhalskiy.com>
2016-02-24 03:24:08 +0300

    Helper method to get mime-type from Content-Type header of HttpMessage

--- common/src/test/java/io/netty/util/AsciiStringCharacterTest.java
+++ common/src/test/java/io/netty/util/AsciiStringCharacterTest.java
@@ -290,4 +290,28 @@ public class AsciiStringCharacterTest {
         assertEquals("abc", new AsciiString("abc  ").trim().toString());
         assertEquals("abc", new AsciiString("  abc  ").trim().toString());
     }
+
+    @Test
+    public void testIndexOfChar() {
+        assertEquals(-1, AsciiString.indexOf(null, 'a', 0));
+        assertEquals(-1, AsciiString.of("").indexOf('a', 0));
+        assertEquals(-1, AsciiString.of("abc").indexOf('d', 0));
+        assertEquals(-1, AsciiString.of("aabaabaa").indexOf('A', 0));
+        assertEquals(0, AsciiString.of("aabaabaa").indexOf('a', 0));
+        assertEquals(1, AsciiString.of("aabaabaa").indexOf('a', 1));
+        assertEquals(3, AsciiString.of("aabaabaa").indexOf('a', 2));
+        assertEquals(3, AsciiString.of("aabdabaa").indexOf('d', 1));
+    }
+
+    @Test
+    public void testStaticIndexOfChar() {
+        assertEquals(-1, AsciiString.indexOf(null, 'a', 0));
+        assertEquals(-1, AsciiString.indexOf("", 'a', 0));
+        assertEquals(-1, AsciiString.indexOf("abc", 'd', 0));
+        assertEquals(-1, AsciiString.indexOf("aabaabaa", 'A', 0));
+        assertEquals(0, AsciiString.indexOf("aabaabaa", 'a', 0));
+        assertEquals(1, AsciiString.indexOf("aabaabaa", 'a', 1));
+        assertEquals(3, AsciiString.indexOf("aabaabaa", 'a', 2));
+        assertEquals(3, AsciiString.indexOf("aabdabaa", 'd', 1));
+    }
 }
