263201f84de70ea373d5a54d5baf7c9d4ed3f339
Sergey Polovko <jamel@yandex-team.ru>
2016-02-28 00:50:11 +0300

    Handle only those http requests that equal to adjusted websocket path
    Motivation:
    
    It will be easier to support websockets in server application by using WebSocketServerProtocolHandshakeHandler class and not reinvent its functionality. But currently it handles all http requests as if they were websocket handshake requests.
    
    Modifications:
    
    Check if http request path is equals to adjusted websocket path.
    Fixed example of websocket server implementation.
    
    Result:
    
    WebSocketServerProtocolHandshakeHandler handles only websocket handshake requests.

--- example/src/main/java/io/netty/example/http/websocketx/server/WebSocketServerInitializer.java
+++ example/src/main/java/io/netty/example/http/websocketx/server/WebSocketServerInitializer.java
@@ -20,12 +20,15 @@ import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketServerCompressionHandler;
 import io.netty.handler.ssl.SslContext;
 
 /**
  */
 public class WebSocketServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    private static final String WEBSOCKET_PATH = "/websocket";
 
     private final SslContext sslCtx;
 
@@ -42,6 +45,8 @@ public class WebSocketServerInitializer extends ChannelInitializer<SocketChannel
         pipeline.addLast(new HttpServerCodec());
         pipeline.addLast(new HttpObjectAggregator(65536));
         pipeline.addLast(new WebSocketServerCompressionHandler());
-        pipeline.addLast(new WebSocketServerHandler());
+        pipeline.addLast(new WebSocketServerProtocolHandler(WEBSOCKET_PATH, null, true));
+        pipeline.addLast(new WebSocketIndexPageHandler(WEBSOCKET_PATH));
+        pipeline.addLast(new WebSocketFrameHandler());
     }
 }
