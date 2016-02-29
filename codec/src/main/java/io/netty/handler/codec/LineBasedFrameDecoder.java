netty/netty

commit f3e92215fe5cc60ad9d60a7f649828f26992daa9

Use ByteProcessor.FIND_LF to find end of line
Motivation:

Each call of ByteBuf.getByte(int) method does boundary checking. This can be eliminated by using ByteBuf.forEachByte(ByteProcessor) method and ByteProcessor.FIND_LF processor.

Modifications:

Find end of line with ByteProcessor.FIND_LF

Result:

A little better performance of LineBasedFrameDecoder.

codec/src/main/java/io/netty/handler/codec/LineBasedFrameDecoder.java
@@ -17,6 +17,7 @@ package io.netty.handler.codec;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.ByteProcessor;
 
 import java.util.List;
 
@@ -152,15 +153,10 @@ public class LineBasedFrameDecoder extends ByteToMessageDecoder {
      * Returns -1 if no end of line was found in the buffer.
      */
     private static int findEndOfLine(final ByteBuf buffer) {
-        final int n = buffer.writerIndex();
-        for (int i = buffer.readerIndex(); i < n; i ++) {
-            final byte b = buffer.getByte(i);
-            if (b == '\n') {
-                return i;
-            } else if (b == '\r' && i < n - 1 && buffer.getByte(i + 1) == '\n') {
-                return i;  // \r\n
-            }
+        int i = buffer.forEachByte(ByteProcessor.FIND_LF);
+        if (i > 0 && buffer.getByte(i - 1) == '\r') {
+            i--;
         }
-        return -1;  // Not found.
+        return i;
     }
 }

commit 4fad9e89f2c9f25ee6997a3fe04f3b709a1c04b5

Fix compile error introduced by f3e92215fe5cc60ad9d60a7f649828f26992daa9

codec/src/main/java/io/netty/handler/codec/LineBasedFrameDecoder.java
@@ -16,8 +16,8 @@
 package io.netty.handler.codec;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufProcessor;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.util.ByteProcessor;
 
 import java.util.List;
 
@@ -153,7 +153,7 @@ public class LineBasedFrameDecoder extends ByteToMessageDecoder {
      * Returns -1 if no end of line was found in the buffer.
      */
     private static int findEndOfLine(final ByteBuf buffer) {
-        int i = buffer.forEachByte(ByteProcessor.FIND_LF);
+        int i = buffer.forEachByte(ByteBufProcessor.FIND_LF);
         if (i > 0 && buffer.getByte(i - 1) == '\r') {
             i--;
         }