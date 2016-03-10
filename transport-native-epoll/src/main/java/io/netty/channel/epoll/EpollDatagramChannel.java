0a959efc9e183e648baea56fcdb860b1512a44b6
Norman Maurer <norman_maurer@apple.com>
2016-03-10 14:58:46 +0100

    Ensure connect promise is notifed before fireChannelActive() is called. Related to [#4927]
    Motivation:
    
    We should always first notify the promise before trigger an event through the pipeline to be consistent.
    
    Modifications:
    
    Ensure we notify the promise before fire event.
    
    Result:
    
    Consistent behavior

--- transport-native-epoll/src/main/java/io/netty/channel/epoll/EpollDatagramChannel.java
+++ transport-native-epoll/src/main/java/io/netty/channel/epoll/EpollDatagramChannel.java
@@ -502,6 +502,9 @@ public final class EpollDatagramChannel extends AbstractEpollChannel implements 
                     EpollDatagramChannel.this.local = fd().localAddress();
                     success = true;
 
+                    // First notify the promise before notifying the handler.
+                    channelPromise.trySuccess();
+
                     // Regardless if the connection attempt was cancelled, channelActive() event should be triggered,
                     // because what happened is what happened.
                     if (!wasActive && isActive()) {
@@ -511,12 +514,11 @@ public final class EpollDatagramChannel extends AbstractEpollChannel implements 
                     if (!success) {
                         doClose();
                     } else {
-                        channelPromise.setSuccess();
                         connected = true;
                     }
                 }
             } catch (Throwable cause) {
-                channelPromise.setFailure(cause);
+                channelPromise.tryFailure(cause);
             }
         }
 
