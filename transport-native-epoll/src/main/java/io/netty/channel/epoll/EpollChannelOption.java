7ed4434c490782488f848ba09697a027db9e2bfa
Norman Maurer <norman_maurer@apple.com>
2016-03-17 23:09:29 +0100

    [#5029] Fix type of EpollChannelOption.TCP_QUICKACK
    Motivation:
    
    TCP_QUICKACK uses Integer but needs to be Boolean
    
    Modifications:
    
    Fix type
    
    Result:
    
    Be able to use EpollChannelOption.TCP_QUICKACK

--- transport-native-epoll/src/main/java/io/netty/channel/epoll/EpollChannelOption.java
+++ transport-native-epoll/src/main/java/io/netty/channel/epoll/EpollChannelOption.java
@@ -35,7 +35,7 @@ public final class EpollChannelOption<T> extends ChannelOption<T> {
     public static final ChannelOption<Boolean> IP_FREEBIND = ChannelOption.valueOf("IP_FREEBIND");
     public static final ChannelOption<Integer> TCP_FASTOPEN = valueOf(T, "TCP_FASTOPEN");
     public static final ChannelOption<Integer> TCP_DEFER_ACCEPT = ChannelOption.valueOf(T, "TCP_DEFER_ACCEPT");
-    public static final ChannelOption<Integer> TCP_QUICKACK = ChannelOption.valueOf(T, "TCP_QUICKACK");
+    public static final ChannelOption<Boolean> TCP_QUICKACK = ChannelOption.valueOf(T, "TCP_QUICKACK");
 
     public static final ChannelOption<DomainSocketReadMode> DOMAIN_SOCKET_READ_MODE =
             ChannelOption.valueOf(T, "DOMAIN_SOCKET_READ_MODE");
