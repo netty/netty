900db0acff95cc8c6334180024ad6d3ab12f220f
Norman Maurer <norman_maurer@apple.com>
2016-03-24 19:37:43 +0100

    Not attempt to read from fd when channel is closed during read loop. Related to [#5031]
    Motivation:
    
    We need to break out of the read loop for two reasons:
    
    - If the input was shutdown in between (which may be the case when the user did it in the
      fireChannelRead(...) method we should not try to read again to not produce any
      miss-leading exceptions.
    
    - If the user closes the channel we need to ensure we not try to read from it again as
      the filedescriptor may be re-used already by the OS if the system is handling a lot of
      concurrent connections and so needs a lot of filedescriptors. If not do this we risk
      reading data from a filedescriptor that belongs to another socket then the socket that
      was "wrapped" by this Channel implementation.
    
    Modification:
    
    Break the reading loop if the input was shutdown from within the channelRead(...) method.
    
    Result:
    
    No more meaningless exceptions and no risk to read data from wrong socket after the original was closed.
    
    Conflicts:
    	transport-native-epoll/src/main/java/io/netty/channel/epoll/AbstractEpollStreamChannel.java

--- transport-native-epoll/src/main/java/io/netty/channel/epoll/AbstractEpollStreamChannel.java
+++ transport-native-epoll/src/main/java/io/netty/channel/epoll/AbstractEpollStreamChannel.java
@@ -899,6 +899,21 @@ public abstract class AbstractEpollStreamChannel extends AbstractEpollChannel im
                         // pending data
                         break;
                     }
+
+                    if (fd().isInputShutdown()) {
+                        // We need to do this for two reasons:
+                        //
+                        // - If the input was shutdown in between (which may be the case when the user did it in the
+                        //   fireChannelRead(...) method we should not try to read again to not produce any
+                        //   miss-leading exceptions.
+                        //
+                        // - If the user closes the channel we need to ensure we not try to read from it again as
+                        //   the filedescriptor may be re-used already by the OS if the system is handling a lot of
+                        //   concurrent connections and so needs a lot of filedescriptors. If not do this we risk
+                        //   reading data from a filedescriptor that belongs to another socket then the socket that
+                        //   was "wrapped" by this Channel implementation.
+                        break;
+                    }
                 } while (++ messages < maxMessagesPerRead || isRdHup());
 
                 pipeline.fireChannelReadComplete();
