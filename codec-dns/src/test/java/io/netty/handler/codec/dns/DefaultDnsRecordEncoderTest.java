4b38b72a0d789a2e812bd11685244ba6583751bb
Trustin Lee <t@motd.kr>
2016-04-01 20:47:15 +0900

    Fix incorrect name encoding/decoding in DNS records
    Motivation:
    
    - The decoded name should always end with a dot (.), but we currently
      strip it, which is incorrect.
      - (O) 0 -> "."
      - (X) 0 -> ""
      - (O) 5 netty 2 io 0 -> "netty.io."
      - (X) 5 netty 2 io 0 -> "netty.io"
    - The encoded name should end with a null-label, which is a label whose
      length is 0, but we currently append an extra NUL, causing FORMERR(1)
      on a strict DNS server:
      - (O) . -> 0
      - (X) . -> 0 0
      - (O) netty.io. -> 5 netty 2 io 0
      - (X) netty.io. -> 5 netty 2 io 0 0
    
    Modifications:
    
    - Make sure to append '.' when decoding a name.
    - Improve index checks so that the decoder can raise
      CorruptFrameException instead of IIOBE
    - Do not encode extra NUL
    - Add more tests
    
    Result:
    
    Robustness and correctness

--- codec-dns/src/test/java/io/netty/handler/codec/dns/DefaultDnsRecordEncoderTest.java
+++ codec-dns/src/test/java/io/netty/handler/codec/dns/DefaultDnsRecordEncoderTest.java
@@ -16,6 +16,7 @@
 package io.netty.handler.codec.dns;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
 import io.netty.util.internal.StringUtil;
 import org.junit.Test;
@@ -24,18 +25,42 @@ import static org.junit.Assert.assertEquals;
 
 public class DefaultDnsRecordEncoderTest {
 
+    @Test
+    public void testEncodeName() throws Exception {
+        testEncodeName(new byte[] { 5, 'n', 'e', 't', 't', 'y', 2, 'i', 'o', 0 }, "netty.io.");
+    }
+
+    @Test
+    public void testEncodeNameWithoutTerminator() throws Exception {
+        testEncodeName(new byte[] { 5, 'n', 'e', 't', 't', 'y', 2, 'i', 'o', 0 }, "netty.io");
+    }
+
+    @Test
+    public void testEncodeNameWithExtraTerminator() throws Exception {
+        testEncodeName(new byte[] { 5, 'n', 'e', 't', 't', 'y', 2, 'i', 'o', 0 }, "netty.io..");
+    }
+
     // Test for https://github.com/netty/netty/issues/5014
     @Test
     public void testEncodeEmptyName() throws Exception {
+        testEncodeName(new byte[] { 0 }, StringUtil.EMPTY_STRING);
+    }
+
+    @Test
+    public void testEncodeRootName() throws Exception {
+        testEncodeName(new byte[] { 0 }, ".");
+    }
+
+    private static void testEncodeName(byte[] expected, String name) throws Exception {
         DefaultDnsRecordEncoder encoder = new DefaultDnsRecordEncoder();
         ByteBuf out = Unpooled.buffer();
+        ByteBuf expectedBuf = Unpooled.wrappedBuffer(expected);
         try {
-            encoder.encodeName(StringUtil.EMPTY_STRING, out);
-            assertEquals(2, out.readableBytes());
-            assertEquals(0, out.readByte());
-            assertEquals(0, out.readByte());
+            encoder.encodeName(name, out);
+            assertEquals(expectedBuf, out);
         } finally {
             out.release();
+            expectedBuf.release();
         }
     }
 }
