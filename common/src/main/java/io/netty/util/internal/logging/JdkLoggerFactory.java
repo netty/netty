c5e7b64411ceacc32d4ce1af74f566a8b5d0e5d2
Xiaoyan Lin <linxiaoyan18@gmail.com>
2016-04-07 01:12:11 -0700

    Make all InternalLoggerFactory implementations be singletons
    Motivation:
    
    It's better to make all InternalLoggerFactory implementations be singletons according to the discussions in #5047
    
    Modifications:
    
    Make all InternalLoggerFactory implementations be singletons and hide the construtors.
    
    Result:
    
    All InternalLoggerFactory implementations be singletons.

--- common/src/main/java/io/netty/util/internal/logging/JdkLoggerFactory.java
+++ common/src/main/java/io/netty/util/internal/logging/JdkLoggerFactory.java
@@ -25,6 +25,15 @@ import java.util.logging.Logger;
  */
 public class JdkLoggerFactory extends InternalLoggerFactory {
 
+    public static final InternalLoggerFactory INSTANCE = new JdkLoggerFactory();
+
+    /**
+     * @deprecated Use {@link #INSTANCE} instead.
+     */
+    @Deprecated
+    public JdkLoggerFactory() {
+    }
+
     @Override
     public InternalLogger newInstance(String name) {
         return new JdkLogger(Logger.getLogger(name));
--- common/src/main/java/io/netty/util/internal/logging/Log4J2LoggerFactory.java
+++ common/src/main/java/io/netty/util/internal/logging/Log4J2LoggerFactory.java
@@ -19,6 +19,15 @@ import org.apache.logging.log4j.LogManager;
 
 public final class Log4J2LoggerFactory extends InternalLoggerFactory {
 
+    public static final InternalLoggerFactory INSTANCE = new Log4J2LoggerFactory();
+
+    /**
+     * @deprecated Use {@link #INSTANCE} instead.
+     */
+    @Deprecated
+    public Log4J2LoggerFactory() {
+    }
+
     @Override
     public InternalLogger newInstance(String name) {
         return new Log4J2Logger(LogManager.getLogger(name));
