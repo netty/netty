netty/netty

commit 0bc93dda081499dfe4f96a1507d24fa46bf0f31f

NIO autoReadClear should also clear the SelectionKey
Motivation:
Prior to 5b48fc284ebe85ca4974985e3be005d37626e980 setting readPending to false when autoReadClear was called was enough because when/if the EventLoop woke up with a read event we would first check if readPending was true and if it is not, we would return early. Now that readPending will only be set in the EventLoop it is not necessary to check readPending at the start of the read methods, and we should instead remove the OP_READ from the SelectionKey when we also set readPending to false.

Modifications:
- autoReadCleared should call AbstractNioUnsafe.removeReadOp

Result:
NIO is now consistent with EPOLL and removes the READ operation on the selector when autoRead is cleared.

transport/src/main/java/io/netty/channel/nio/AbstractNioChannel.java
@@ -65,7 +65,7 @@ public abstract class AbstractNioChannel extends AbstractChannel {
     private final Runnable clearReadPendingRunnable = new Runnable() {
         @Override
         public void run() {
-            readPending = false;
+            clearReadPending0();
         }
     };
 
@@ -149,17 +149,17 @@ public abstract class AbstractNioChannel extends AbstractChannel {
         if (isRegistered()) {
             EventLoop eventLoop = eventLoop();
             if (eventLoop.inEventLoop()) {
-                this.readPending = readPending;
+                setReadPending0(readPending);
             } else {
                 eventLoop.execute(new OneTimeTask() {
                     @Override
                     public void run() {
-                        AbstractNioChannel.this.readPending = readPending;
+                        setReadPending0(readPending);
                     }
                 });
             }
         } else {
-            this.readPending = readPending;
+            setReadPending0(readPending);
         }
     }
 
@@ -170,14 +170,26 @@ public abstract class AbstractNioChannel extends AbstractChannel {
         if (isRegistered()) {
             EventLoop eventLoop = eventLoop();
             if (eventLoop.inEventLoop()) {
-                readPending = false;
+                clearReadPending0();
             } else {
                 eventLoop.execute(clearReadPendingRunnable);
             }
         } else {
             // Best effort if we are not registered yet clear readPending. This happens during channel initialization.
-            readPending = false;
+            clearReadPending0();
         }
+    }
+
+    private void setReadPending0(boolean readPending) {
+        this.readPending = readPending;
+        if (!readPending) {
+            ((AbstractNioUnsafe) unsafe()).removeReadOp();
+        }
+    }
+
+    private void clearReadPending0() {
+        readPending = false;
+        ((AbstractNioUnsafe) unsafe()).removeReadOp();
     }
 
     /**

commit 2134d6539e7957c2fb29cbc8f4a24398d35f5b5b

Fix an assertion error introduced by 0bc93dda081499dfe4f96a1507d24fa46bf0f31f
Motivation:
    
Commit 0bc93dda081499dfe4f96a1507d24fa46bf0f31f introduced an assertion
failure.

Modifications:

Fix the assertion error.

Result:

Regression removed

transport/src/main/java/io/netty/channel/nio/AbstractNioChannel.java
@@ -176,7 +176,9 @@ public abstract class AbstractNioChannel extends AbstractChannel {
             }
         } else {
             // Best effort if we are not registered yet clear readPending. This happens during channel initialization.
-            clearReadPending0();
+            // NB: We only set the boolean field instead of calling clearReadPending0(), because the SelectionKey is
+            // not set yet so it would produce an assertion failure.
+            readPending = false;
         }
     }
 