967057854d2b7375aa4aa1ba3b0d87dc09b354bf
Norman Maurer <norman_maurer@apple.com>
2016-04-10 08:04:17 +0200

    Fix resource leak in test introduced by 69070c37baf55e181f9270270f7cbf25958ba9b3

--- codec-http/src/test/java/io/netty/handler/codec/http/HttpRequestDecoderTest.java
+++ codec-http/src/test/java/io/netty/handler/codec/http/HttpRequestDecoderTest.java
@@ -86,7 +86,7 @@ public class HttpRequestDecoderTest {
         assertEquals(CONTENT_LENGTH, c.content().readableBytes());
         assertEquals(
                 Unpooled.wrappedBuffer(content, content.length - CONTENT_LENGTH, CONTENT_LENGTH),
-                c.content().readBytes(CONTENT_LENGTH));
+                c.content().readSlice(CONTENT_LENGTH));
         c.release();
 
         assertFalse(channel.finish());
