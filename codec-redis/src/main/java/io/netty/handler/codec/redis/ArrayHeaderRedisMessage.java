705df9ba0c639f518ca24e2379e32f97ad9534ff
Jongyeol Choi <jongyeol.choi@gmail.com>
2016-04-14 07:30:09 +0900

    Implement codec-redis
    Motivation:
    
    - To encode/decode RESP (REdis Serialization Protocol) using Netty
    - http://redis.io/topics/protocol
    
    Modifications:
    
    - Add RedisEncoder, RedisDecoder
    - Add RedisBulkStringAggregator and RedisArrayAggregator
    - Add tests
    
    Result:
    
    - Added codec-redis
    - codec-redis can encode/decode RESP (REdis Serialization Protocol)

---
+++ codec-redis/src/main/java/io/netty/handler/codec/redis/ArrayHeaderRedisMessage.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2016 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License, version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.netty.handler.codec.redis;
+
+import io.netty.util.internal.StringUtil;
+
+/**
+ * Header of Redis Array Message.
+ */
+public class ArrayHeaderRedisMessage implements RedisMessage {
+
+    private final long length;
+
+    /**
+     * Creates a {@link ArrayHeaderRedisMessage} for the given {@code length}.
+     */
+    public ArrayHeaderRedisMessage(long length) {
+        if (length < RedisConstants.NULL_VALUE) {
+            throw new RedisCodecException("length: " + length + " (expected: >= " + RedisConstants.NULL_VALUE + ")");
+        }
+        this.length = length;
+    }
+
+    /**
+     * Get length of this array object.
+     */
+    public final long length() {
+        return length;
+    }
+
+    /**
+     * Returns whether the content of this message is {@code null}.
+     *
+     * @return indicates whether the content of this message is {@code null}.
+     */
+    public boolean isNull() {
+        return length == RedisConstants.NULL_VALUE;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder(StringUtil.simpleClassName(this))
+                .append('[')
+                .append("length=")
+                .append(length)
+                .append(']').toString();
+    }
+}
---
+++ codec-redis/src/test/java/io/netty/handler/codec/redis/RedisCodecTestUtil.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2016 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.netty.handler.codec.redis;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.util.CharsetUtil;
+
+final class RedisCodecTestUtil {
+
+    private RedisCodecTestUtil() {
+    }
+
+    static byte[] bytesOf(long value) {
+        return bytesOf(Long.toString(value));
+    }
+
+    static byte[] bytesOf(String s) {
+        return s.getBytes(CharsetUtil.UTF_8);
+    }
+
+    static byte[] bytesOf(ByteBuf buf) {
+        byte[] data = new byte[buf.readableBytes()];
+        buf.readBytes(data);
+        return data;
+    }
+
+    static String stringOf(ByteBuf buf) {
+        return new String(bytesOf(buf));
+    }
+
+    static ByteBuf byteBufOf(String s) {
+        return byteBufOf(bytesOf(s));
+    }
+
+    static ByteBuf byteBufOf(byte[] data) {
+        return Unpooled.wrappedBuffer(data);
+    }
+}
