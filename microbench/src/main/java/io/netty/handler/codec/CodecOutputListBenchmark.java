0ea4597542c28af4d9d30c0e2d5bd876646cbd58
Norman Maurer <norman_maurer@apple.com>
2016-05-03 16:43:47 +0200

    Introduce CodecOutputList to reduce overhead of encoder/decoder
    Motivation:
    
    99dfc9ea799348430a1c25776ce30a95bc10a1ff introduced some code that will more frequently try to forward messages out of the list of decoded messages to reduce latency and memory footprint. Unfortunally this has the side-effect that RecycleableArrayList.clear() will be called more often and so introduce some overhead as ArrayList will null out the array on each call.
    
    Modifications:
    
    - Introduce a CodecOutputList which allows to not null out the array until we recycle it and also allows to access internal array with extra range checks.
    - Add benchmark that add elements to different List implementations and clear them
    
    Result:
    
    Less overhead when decode / encode messages.
    
    Benchmark                                     (elements)   Mode  Cnt         Score        Error  Units
    CodecOutputListBenchmark.arrayList                     1  thrpt   20  24853764.609 ± 161582.376  ops/s
    CodecOutputListBenchmark.arrayList                     4  thrpt   20  17310636.508 ± 930517.403  ops/s
    CodecOutputListBenchmark.codecOutList                  1  thrpt   20  26670751.661 ± 587812.655  ops/s
    CodecOutputListBenchmark.codecOutList                  4  thrpt   20  25166421.089 ± 166945.599  ops/s
    CodecOutputListBenchmark.recyclableArrayList           1  thrpt   20  24565992.626 ± 210017.290  ops/s
    CodecOutputListBenchmark.recyclableArrayList           4  thrpt   20  18477881.775 ± 157003.777  ops/s
    
    Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 246.748 sec - in io.netty.handler.codec.CodecOutputListBenchmark

---
+++ microbench/src/main/java/io/netty/handler/codec/CodecOutputListBenchmark.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2016 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.codec;
+
+import io.netty.microbench.util.AbstractMicrobenchmark;
+import io.netty.util.internal.RecyclableArrayList;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@State(Scope.Benchmark)
+public class CodecOutputListBenchmark extends AbstractMicrobenchmark {
+
+    private static final Object ELEMENT = new Object();
+    private CodecOutputList codecOutputList;
+    private RecyclableArrayList recycleableArrayList;
+    private List<Object> arrayList;
+
+    @Param({ "1", "4" })
+    public int elements;
+
+    @Setup(Level.Invocation)
+    public void setup() {
+        codecOutputList = CodecOutputList.newInstance();
+        recycleableArrayList = RecyclableArrayList.newInstance(16);
+        arrayList = new ArrayList<Object>(16);
+    }
+
+    @TearDown
+    public void destroy() {
+        codecOutputList.recycle();
+        recycleableArrayList.recycle();
+    }
+
+    @Benchmark
+    public void codecOutList() {
+        benchmarkAddAndClear(codecOutputList, elements);
+    }
+
+    @Benchmark
+    public void recyclableArrayList() {
+        benchmarkAddAndClear(recycleableArrayList, elements);
+    }
+
+    @Benchmark
+    public void arrayList() {
+        benchmarkAddAndClear(arrayList, elements);
+    }
+
+    private static void benchmarkAddAndClear(List<Object> list, int elements) {
+        for (int i = 0; i < elements; i++) {
+            list.add(ELEMENT);
+        }
+        list.clear();
+    }
+}
