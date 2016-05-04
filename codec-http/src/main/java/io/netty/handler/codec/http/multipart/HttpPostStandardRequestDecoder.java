c88761d1d903693e51438cebd6ae1e49777e4d35
Norman Maurer <norman_maurer@apple.com>
2016-05-04 14:16:38 +0200

    [#5202] Correctly throw ErrorDataDecoderException when invalid encoded form parameters are present.
    Motivation:
    
    At the moment we let the IllegalArgumentException escape when parsing form parameters. This is not expected.
    
    Modifications:
    
    Correctly catch IllegalArgumentException and rethrow as ErrorDataDecoderException.
    
    Result:
    
    Throw correct exception.

--- codec-http/src/main/java/io/netty/handler/codec/http/multipart/HttpPostStandardRequestDecoder.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/multipart/HttpPostStandardRequestDecoder.java
@@ -628,6 +628,10 @@ public class HttpPostStandardRequestDecoder implements InterfaceHttpPostRequestD
             // error while decoding
             undecodedChunk.readerIndex(firstpos);
             throw new ErrorDataDecoderException(e);
+        } catch (IllegalArgumentException e) {
+            // error while decoding
+            undecodedChunk.readerIndex(firstpos);
+            throw new ErrorDataDecoderException(e);
         }
     }
 
