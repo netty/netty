5447b0cb29d472c2baf2dd948eb57a0957211832
Norman Maurer <norman_maurer@apple.com>
2016-05-30 11:02:41 +0200

    Fix small race in DefaultChannelPipeline introduced by a729e0fcd94009905d219665bdd069eb31433b7c
    Motivation:
    
    There is a small race while adding handlers to the pipeline because callHandlerAddedForAllHandlers() may not be run when the user calls add* but the Channel is already registered.
    
    Modifications:
    
    Ensure we always delay handlerAdded(..) / handlerRemoved(...) until callHandlerAddedForAllHandlers() was called.
    
    Result:
    
    No more race on pipeline modifications possible.

--- transport/src/test/java/io/netty/channel/DefaultChannelPipelineTest.java
+++ transport/src/test/java/io/netty/channel/DefaultChannelPipelineTest.java
@@ -729,14 +729,14 @@ public class DefaultChannelPipelineTest {
             final AtomicBoolean handlerAdded = new AtomicBoolean();
             final Exception exception = new RuntimeException();
             ChannelPipeline pipeline = new LocalChannel().pipeline();
-            pipeline.addLast(new ChannelHandlerAdapter() {
+            pipeline.addLast(group1, new CheckExceptionHandler(exception, promise));
+            pipeline.addFirst(new ChannelHandlerAdapter() {
                 @Override
                 public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
                     handlerAdded.set(true);
                     throw exception;
                 }
             });
-            pipeline.addLast(group1, new CheckExceptionHandler(exception, promise));
             assertFalse(handlerAdded.get());
             group.register(pipeline.channel());
             promise.syncUninterruptibly();
@@ -778,7 +778,8 @@ public class DefaultChannelPipelineTest {
             final Exception exceptionRemoved = new RuntimeException();
             String handlerName = "foo";
             ChannelPipeline pipeline = new LocalChannel().pipeline();
-            pipeline.addLast(handlerName, new ChannelHandlerAdapter() {
+            pipeline.addLast(group1, new CheckExceptionHandler(exceptionAdded, promise));
+            pipeline.addFirst(handlerName, new ChannelHandlerAdapter() {
                 @Override
                 public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
                     throw exceptionAdded;
@@ -786,11 +787,16 @@ public class DefaultChannelPipelineTest {
 
                 @Override
                 public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
-                    latch.countDown();
+                    // Execute this later so we are sure the exception is handled first.
+                    ctx.executor().execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            latch.countDown();
+                        }
+                    });
                     throw exceptionRemoved;
                 }
             });
-            pipeline.addLast(group1, new CheckExceptionHandler(exceptionAdded, promise));
             group.register(pipeline.channel()).syncUninterruptibly();
             latch.await();
             assertNull(pipeline.context(handlerName));
