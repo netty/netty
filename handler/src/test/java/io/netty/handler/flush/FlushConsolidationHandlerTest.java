5579071c73b3112da66f14e8e447da341984161d
Norman Maurer <norman_maurer@apple.com>
2016-07-05 14:15:37 +0200

    Add FlushConsolidationHandler which consolidates flush operations as these are expensive
    Motivation:
    
    Calling flush() and writeAndFlush(...) are expensive operations in the sense as both will produce a write(...) or writev(...) system call if there are any pending writes in the ChannelOutboundBuffer. Often we can consolidate multiple flush operations into one if currently a read loop is active for a Channel, as we can just flush when channelReadComplete is triggered. Consolidating flushes can give a huge performance win depending on how often is flush is called. The only "downside" may be a bit higher latency in the case of where only one flush is triggered by the user.
    
    Modifications:
    
    Add a FlushConsolidationHandler which will consolidate flushes and so improve the throughput.
    
    Result:
    
    Better performance (throughput). This is especially true for protocols that use some sort of PIPELINING.

---
+++ handler/src/test/java/io/netty/handler/flush/FlushConsolidationHandlerTest.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2016 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.flush;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.embedded.EmbeddedChannel;
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.junit.Assert.*;
+
+public class FlushConsolidationHandlerTest {
+
+    @Test
+    public void testFlushViaReadComplete() {
+        final AtomicInteger flushCount = new AtomicInteger();
+        EmbeddedChannel channel = newChannel(flushCount);
+        // Flush should go through as there is no read loop in progress.
+        channel.flush();
+        assertEquals(1, flushCount.get());
+
+        // Simulate read loop;
+        channel.pipeline().fireChannelRead(1L);
+        assertEquals(1, flushCount.get());
+        channel.pipeline().fireChannelRead(2L);
+        assertEquals(1, flushCount.get());
+        assertNull(channel.readOutbound());
+        channel.pipeline().fireChannelReadComplete();
+        assertEquals(2, flushCount.get());
+        // Now flush again as the read loop is complete.
+        channel.flush();
+        assertEquals(3, flushCount.get());
+        assertEquals(1L, channel.readOutbound());
+        assertEquals(2L, channel.readOutbound());
+        assertNull(channel.readOutbound());
+        assertFalse(channel.finish());
+    }
+
+    @Test
+    public void testFlushViaClose() {
+        final AtomicInteger flushCount = new AtomicInteger();
+        EmbeddedChannel channel = newChannel(flushCount);
+        // Simulate read loop;
+        channel.pipeline().fireChannelRead(1L);
+        assertEquals(0, flushCount.get());
+        assertNull(channel.readOutbound());
+        channel.close();
+        assertEquals(1, flushCount.get());
+        assertEquals(1L, channel.readOutbound());
+        assertNull(channel.readOutbound());
+        assertFalse(channel.finish());
+    }
+
+    @Test
+    public void testFlushViaDisconnect() {
+        final AtomicInteger flushCount = new AtomicInteger();
+        EmbeddedChannel channel = newChannel(flushCount);
+        // Simulate read loop;
+        channel.pipeline().fireChannelRead(1L);
+        assertEquals(0, flushCount.get());
+        assertNull(channel.readOutbound());
+        channel.disconnect();
+        assertEquals(1, flushCount.get());
+        assertEquals(1L, channel.readOutbound());
+        assertNull(channel.readOutbound());
+        assertFalse(channel.finish());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testFlushViaException() {
+        final AtomicInteger flushCount = new AtomicInteger();
+        EmbeddedChannel channel = newChannel(flushCount);
+        // Simulate read loop;
+        channel.pipeline().fireChannelRead(1L);
+        assertEquals(0, flushCount.get());
+        assertNull(channel.readOutbound());
+        channel.pipeline().fireExceptionCaught(new IllegalStateException());
+        assertEquals(1, flushCount.get());
+        assertEquals(1L, channel.readOutbound());
+        assertNull(channel.readOutbound());
+        channel.finish();
+    }
+
+    @Test
+    public void testFlushViaRemoval() {
+        final AtomicInteger flushCount = new AtomicInteger();
+        EmbeddedChannel channel = newChannel(flushCount);
+        // Simulate read loop;
+        channel.pipeline().fireChannelRead(1L);
+        assertEquals(0, flushCount.get());
+        assertNull(channel.readOutbound());
+        channel.pipeline().remove(FlushConsolidationHandler.class);
+        assertEquals(1, flushCount.get());
+        assertEquals(1L, channel.readOutbound());
+        assertNull(channel.readOutbound());
+        assertFalse(channel.finish());
+    }
+
+    private static EmbeddedChannel newChannel(final AtomicInteger flushCount) {
+        return new EmbeddedChannel(new ChannelOutboundHandlerAdapter() {
+            @Override
+            public void flush(ChannelHandlerContext ctx) throws Exception {
+                flushCount.incrementAndGet();
+                ctx.flush();
+            }
+        }, new FlushConsolidationHandler(), new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                ctx.writeAndFlush(msg);
+            }
+        });
+    }
+}
