76e88be6c95cabfec4d3a96e02cc874bdcaf4ea3
Norman Maurer <norman_maurer@apple.com>
2016-07-12 13:25:13 +0200

    Lz4FrameDecoder should reduce memory copies whenever possible
    Motivation:
    
    When the user constructs Lz4FrameDecoder with a Checksum implementation like CRC32 or Adler32 and uses Java8 we can directly use a ByteBuffer to do the checksum work. This way we can eliminate memory copies.
    
    Modifications:
    
    Detect if ByteBuffer can be used for checksum work and if so reduce memory copies.
    
    Result:
    
    Less memory copies when using JDK8

---
+++ codec/src/main/java/io/netty/handler/codec/compression/CompressionUtil.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2016 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.codec.compression;
+
+import io.netty.buffer.ByteBuf;
+
+import java.nio.ByteBuffer;
+
+final class CompressionUtil {
+
+    private CompressionUtil() { }
+
+    static void checkChecksum(ByteBufChecksum checksum, ByteBuf uncompressed, int currentChecksum) {
+        checksum.reset();
+        checksum.update(uncompressed,
+                uncompressed.readerIndex(), uncompressed.readableBytes());
+
+        final int checksumResult = (int) checksum.getValue();
+        if (checksumResult != currentChecksum) {
+            throw new DecompressionException(String.format(
+                    "stream corrupted: mismatching checksum: %d (expected: %d)",
+                    checksumResult, currentChecksum));
+        }
+    }
+
+    static ByteBuffer safeNioBuffer(ByteBuf buffer) {
+        return buffer.nioBufferCount() == 1 ? buffer.internalNioBuffer(buffer.readerIndex(), buffer.readableBytes())
+                : buffer.nioBuffer();
+    }
+}
