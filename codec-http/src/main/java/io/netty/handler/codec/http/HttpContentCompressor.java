7a4a27574f21b2d13346864c976372f765e64226
Dmitriy Dumanskiy <doom369@gmail.com>
2016-08-01 00:20:45 +0300

    small performance fixes : unnecessary unboxing operations removed
    Motivation :
    
    Unboxing operations allocate unnecessary objects when it could be avoided.
    
    Modifications:
    Replaced Float.valueOf with Number.parseFloat where possible.
    
    Result:
    
    Less unnecessary objects allocations.

--- codec-http/src/main/java/io/netty/handler/codec/http/HttpContentCompressor.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/HttpContentCompressor.java
@@ -141,7 +141,7 @@ public class HttpContentCompressor extends HttpContentEncoder {
             int equalsPos = encoding.indexOf('=');
             if (equalsPos != -1) {
                 try {
-                    q = Float.valueOf(encoding.substring(equalsPos + 1));
+                    q = Float.parseFloat(encoding.substring(equalsPos + 1));
                 } catch (NumberFormatException e) {
                     // Ignore encoding
                     q = 0.0f;
