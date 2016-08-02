bd813392d3610d935f60c120f3b1db2fc6d27332
Norman Maurer <norman_maurer@apple.com>
2016-08-02 21:19:30 +0200

    [#4241] Ensure NioEventLoopGroup.shutdownGracefully(...) with no quiet period shutdown as fast as expected.
    Motivation:
    
    If the user uses 0 as quiet period we should shutdown without any delay if possible.
    
    Modifications:
    
    Ensure we not introduce extra delay when a shutdown quit period of 0 is used.
    
    Result:
    
    EventLoop shutdown as fast as expected.

--- transport/src/test/java/io/netty/channel/AbstractEventLoopTest.java
+++ transport/src/test/java/io/netty/channel/AbstractEventLoopTest.java
@@ -21,7 +21,10 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.util.concurrent.DefaultEventExecutorGroup;
 import io.netty.util.concurrent.EventExecutor;
 import io.netty.util.concurrent.EventExecutorGroup;
+import io.netty.util.concurrent.Future;
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
@@ -58,6 +61,24 @@ public abstract class AbstractEventLoopTest {
         EventExecutor executorNew = channel.pipeline().context(TestChannelHandler.class).executor();
         assertNotSame(executor1, executorNew);
         assertSame(executor, future.channel().pipeline().context(TestChannelHandler2.class).executor());
+    }
+
+    @Test(timeout = 5000)
+    public void testShutdownGracefullyNoQuietPeriod() throws Exception {
+        EventLoopGroup loop = newEventLoopGroup();
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(loop)
+                .channel(newChannel())
+                .childHandler(new ChannelInboundHandlerAdapter());
+
+        // Not close the Channel to ensure the EventLoop is still shutdown in time.
+        b.bind(0).sync().channel();
+
+        Future<?> f = loop.shutdownGracefully(0, 1, TimeUnit.MINUTES);
+        assertTrue(loop.awaitTermination(2, TimeUnit.SECONDS));
+        assertTrue(f.syncUninterruptibly().isSuccess());
+        assertTrue(loop.isShutdown());
+        assertTrue(loop.isTerminated());
     }
 
     private static final class TestChannelHandler extends ChannelDuplexHandler { }
