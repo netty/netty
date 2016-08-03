a8e0d954edcbb3c2c46ef9639c87dd856d1f5d00
Dmitriy Dumanskiy <doom369@gmail.com>
2016-08-03 23:28:44 +0300

    deprecated old loggers
    Motivation:
    
    Commons logger is dead and not updated for more than 2 years. #5615.
    
    Modifications:
    
    Added @Deprecated annotation to CommonsLoggerFactory and CommonsLogger.
    
    Result:
    
    Commons logger now deprecated.

--- common/src/main/java/io/netty/util/internal/logging/CommonsLoggerFactory.java
+++ common/src/main/java/io/netty/util/internal/logging/CommonsLoggerFactory.java
@@ -18,14 +18,15 @@ package io.netty.util.internal.logging;
 
 import org.apache.commons.logging.LogFactory;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Logger factory which creates an
  * <a href="http://commons.apache.org/logging/">Apache Commons Logging</a>
  * logger.
+ *
+ * @deprecated Please use {@link Log4J2LoggerFactory} or {@link Log4JLoggerFactory} or
+ * {@link Slf4JLoggerFactory}.
  */
+@Deprecated
 public class CommonsLoggerFactory extends InternalLoggerFactory {
 
     public static final InternalLoggerFactory INSTANCE = new CommonsLoggerFactory();
