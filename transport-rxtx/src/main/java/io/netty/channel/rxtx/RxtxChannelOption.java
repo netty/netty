275435634a175c202054d026e34a79410a635919
Dmitriy Dumanskiy <doom369@gmail.com>
2016-08-03 11:49:08 +0300

    removed unnecessary static holders

--- transport-rxtx/src/main/java/io/netty/channel/rxtx/RxtxChannelOption.java
+++ transport-rxtx/src/main/java/io/netty/channel/rxtx/RxtxChannelOption.java
@@ -25,17 +25,14 @@ import io.netty.channel.rxtx.RxtxChannelConfig.Stopbits;
  */
 public final class RxtxChannelOption<T> extends ChannelOption<T> {
 
-    @SuppressWarnings("rawtypes")
-    private static final Class<RxtxChannelOption> T = RxtxChannelOption.class;
-
-    public static final ChannelOption<Integer> BAUD_RATE = valueOf(T, "BAUD_RATE");
-    public static final ChannelOption<Boolean> DTR = valueOf(T, "DTR");
-    public static final ChannelOption<Boolean> RTS = valueOf(T, "RTS");
-    public static final ChannelOption<Stopbits> STOP_BITS = valueOf(T, "STOP_BITS");
-    public static final ChannelOption<Databits> DATA_BITS = valueOf(T, "DATA_BITS");
-    public static final ChannelOption<Paritybit> PARITY_BIT = valueOf(T, "PARITY_BIT");
-    public static final ChannelOption<Integer> WAIT_TIME = valueOf(T, "WAIT_TIME");
-    public static final ChannelOption<Integer> READ_TIMEOUT = valueOf(T, "READ_TIMEOUT");
+    public static final ChannelOption<Integer> BAUD_RATE = valueOf(RxtxChannelOption.class, "BAUD_RATE");
+    public static final ChannelOption<Boolean> DTR = valueOf(RxtxChannelOption.class, "DTR");
+    public static final ChannelOption<Boolean> RTS = valueOf(RxtxChannelOption.class, "RTS");
+    public static final ChannelOption<Stopbits> STOP_BITS = valueOf(RxtxChannelOption.class, "STOP_BITS");
+    public static final ChannelOption<Databits> DATA_BITS = valueOf(RxtxChannelOption.class, "DATA_BITS");
+    public static final ChannelOption<Paritybit> PARITY_BIT = valueOf(RxtxChannelOption.class, "PARITY_BIT");
+    public static final ChannelOption<Integer> WAIT_TIME = valueOf(RxtxChannelOption.class, "WAIT_TIME");
+    public static final ChannelOption<Integer> READ_TIMEOUT = valueOf(RxtxChannelOption.class, "READ_TIMEOUT");
 
     @SuppressWarnings({ "unused", "deprecation" })
     private RxtxChannelOption() {
