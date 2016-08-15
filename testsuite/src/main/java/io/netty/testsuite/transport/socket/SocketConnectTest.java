65574aca3bec521b2a3ef86f57416c7c512d1611
Norman Maurer <norman_maurer@apple.com>
2016-08-15 14:26:19 +0200

    [#5639] Ensure fireChannelActive() is also called if Channel is closed in connect promise.
    Motivation:
    
    We need to ensure we also call fireChannelActive() if the Channel is directly closed in a ChannelFutureListener that is belongs to the promise for the connect. Otherwise we will see missing active events.
    
    Modifications:
    
    Ensure we always call fireChannelActive() if the Channel was active.
    
    Result:
    
    No missing events.

--- testsuite/src/main/java/io/netty/testsuite/transport/socket/SocketConnectTest.java
+++ testsuite/src/main/java/io/netty/testsuite/transport/socket/SocketConnectTest.java
@@ -18,6 +18,7 @@ package io.netty.testsuite.transport.socket;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.concurrent.ImmediateEventExecutor;
@@ -25,6 +26,8 @@ import io.netty.util.concurrent.Promise;
 import org.junit.Test;
 
 import java.net.InetSocketAddress;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import static org.junit.Assert.*;
 
@@ -63,6 +66,46 @@ public class SocketConnectTest extends AbstractSocketTest {
             }
             if (serverChannel != null) {
                 serverChannel.close().syncUninterruptibly();
+            }
+        }
+    }
+
+    @Test(timeout = 3000)
+    public void testChannelEventsFiredWhenClosedDirectly() throws Throwable {
+        run();
+    }
+
+    public void testChannelEventsFiredWhenClosedDirectly(ServerBootstrap sb, Bootstrap cb) throws Throwable {
+        final BlockingQueue<Integer> events = new LinkedBlockingQueue<Integer>();
+
+        Channel sc = null;
+        Channel cc = null;
+        try {
+            sb.childHandler(new ChannelInboundHandlerAdapter());
+            sc = sb.bind(0).syncUninterruptibly().channel();
+
+            cb.handler(new ChannelInboundHandlerAdapter() {
+                @Override
+                public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                    events.add(0);
+                }
+
+                @Override
+                public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+                    events.add(1);
+                }
+            });
+            // Connect and directly close again.
+            cc = cb.connect(sc.localAddress()).addListener(ChannelFutureListener.CLOSE).
+                    syncUninterruptibly().channel();
+            assertEquals(0, events.take().intValue());
+            assertEquals(1, events.take().intValue());
+        } finally {
+            if (cc != null) {
+                cc.close();
+            }
+            if (sc != null) {
+                sc.close();
             }
         }
     }
