d5b0c8d090d21cb4b8e4ad42a9dc9fb4ae56fa2b
Norman Maurer <norman_maurer@apple.com>
2016-08-15 14:26:19 +0200

    [#5639] Ensure fireChannelActive() is also called if Channel is closed in connect promise.
    Motivation:
    
    We need to ensure we also call fireChannelActive() if the Channel is directly closed in a ChannelFutureListener that is belongs to the promise for the connect. Otherwise we will see missing active events.
    
    Modifications:
    
    Ensure we always call fireChannelActive() if the Channel was active.
    
    Result:
    
    No missing events.

--- transport-native-epoll/src/main/java/io/netty/channel/epoll/AbstractEpollStreamChannel.java
+++ transport-native-epoll/src/main/java/io/netty/channel/epoll/AbstractEpollStreamChannel.java
@@ -834,12 +834,16 @@ public abstract class AbstractEpollStreamChannel extends AbstractEpollChannel im
             }
             active = true;
 
+            // Get the state as trySuccess() may trigger an ChannelFutureListener that will close the Channel.
+            // We still need to ensure we call fireChannelActive() in this case.
+            boolean active = isActive();
+
             // trySuccess() will return false if a user cancelled the connection attempt.
             boolean promiseSet = promise.trySuccess();
 
             // Regardless if the connection attempt was cancelled, channelActive() event should be triggered,
             // because what happened is what happened.
-            if (!wasActive && isActive()) {
+            if (!wasActive && active) {
                 pipeline().fireChannelActive();
             }
 
