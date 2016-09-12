62b3bb1ba16d86c82ed38e60eca7ec10318e4199
buchgr <jakob.buchgraber@tum.de>
2016-09-12 19:40:26 +0200

    make the http2 codec void promise ready.
    Motivation:
    
    Void promises need special treatment, as they don't behave like normal promises. One
    cannot add a listener to a void promise for example.
    
    Modifications:
    
    Inspected the code for addListener() calls, and added extra logic for void promises
    where necessary. Essentially, when writing a frame with a void promise, any errors
    are reported via the channel pipeline's exceptionCaught event.
    
    Result:
    
    The HTTP/2 codec has better support for void promises.

--- codec-http2/src/test/java/io/netty/handler/codec/http2/Http2TestUtil.java
+++ codec-http2/src/test/java/io/netty/handler/codec/http2/Http2TestUtil.java
@@ -16,9 +16,17 @@ package io.netty.handler.codec.http2;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
+import io.netty.channel.DefaultChannelPromise;
 import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.util.AsciiString;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+import io.netty.util.concurrent.ImmediateEventExecutor;
+import junit.framework.AssertionFailedError;
 
 import java.util.List;
 import java.util.Random;
@@ -381,5 +389,53 @@ final class Http2TestUtil {
             listener.onUnknownFrame(ctx, frameType, streamId, flags, payload);
             messageLatch.countDown();
         }
+    }
+
+    static ChannelPromise newVoidPromise(final Channel channel) {
+        return new DefaultChannelPromise(channel, ImmediateEventExecutor.INSTANCE) {
+            @Override
+            public ChannelPromise addListener(
+                    GenericFutureListener<? extends Future<? super Void>> listener) {
+                throw new AssertionFailedError();
+            }
+
+            @Override
+            public ChannelPromise addListeners(
+                    GenericFutureListener<? extends Future<? super Void>>... listeners) {
+                throw new AssertionFailedError();
+            }
+
+            @Override
+            public boolean isVoid() {
+                return true;
+            }
+
+            @Override
+            public boolean tryFailure(Throwable cause) {
+                channel().pipeline().fireExceptionCaught(cause);
+                return true;
+            }
+
+            @Override
+            public ChannelPromise setFailure(Throwable cause) {
+                tryFailure(cause);
+                return this;
+            }
+
+            @Override
+            public ChannelPromise unvoid() {
+                ChannelPromise promise =
+                        new DefaultChannelPromise(channel, ImmediateEventExecutor.INSTANCE);
+                promise.addListener(new ChannelFutureListener() {
+                    @Override
+                    public void operationComplete(ChannelFuture future) throws Exception {
+                        if (!future.isSuccess()) {
+                            channel().pipeline().fireExceptionCaught(future.cause());
+                        }
+                    }
+                });
+                return promise;
+            }
+        };
     }
 }
