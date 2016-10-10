9e8e4d2da3efb74a38a0bcbd1ec36e23fcdc9531
Norman Maurer <norman_maurer@apple.com>
2016-10-10 08:43:27 +0200

    [#5892] Correct handle HttpMessage that is EOF terminated
    Motivation:
    
    We need to ensure we not add the Transfer-Encoding header if the HttpMessage is EOF terminated.
    
    Modifications:
    
    Only add the Transfer-Encoding header if an Content-Length header is present.
    
    Result:
    
    Correctly handle HttpMessage that is EOF terminated.

--- codec-http/src/main/java/io/netty/handler/codec/http/HttpContentDecoder.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/HttpContentDecoder.java
@@ -96,8 +96,12 @@ public abstract class HttpContentDecoder extends MessageToMessageDecoder<HttpObj
             // the correct value can be set only after all chunks are processed/decoded.
             // If buffering is not an issue, add HttpObjectAggregator down the chain, it will set the header.
             // Otherwise, rely on LastHttpContent message.
-            headers.remove(HttpHeaders.Names.CONTENT_LENGTH);
-            headers.set(HttpHeaders.Names.TRANSFER_ENCODING, HttpHeaders.Values.CHUNKED);
+            if (headers.contains(HttpHeaders.Names.CONTENT_LENGTH)) {
+                headers.remove(HttpHeaders.Names.CONTENT_LENGTH);
+                headers.set(HttpHeaders.Names.TRANSFER_ENCODING, HttpHeaders.Values.CHUNKED);
+            }
+            // Either it is already chunked or EOF terminated.
+            // See https://github.com/netty/netty/issues/5892
 
             // set new content encoding,
             CharSequence targetContentEncoding = getTargetContentEncoding(contentEncoding);
