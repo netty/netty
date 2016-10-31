bff951ca07da117f1086dc928b452dd94bc8978d
Moses Nakamura <mnakamura@twitter.com>
2016-10-30 20:51:55 -0700

    codec-http: HttpClientUpgradeHandler can handle streamed responses
    Motivation:
    
    We want to reject the upgrade as quickly as possible, so that we can
    support streamed responses.
    
    Modifications:
    
    Reject the upgrade as soon as we inspect the headers if they're wrong,
    instead of waiting for the entire response body.
    
    Result:
    
    If a remote server doesn't know how to use the http upgrade and tries to
    responsd with a streaming response that never ends, the client doesn't
    buffer forever, but can instead pass it along.  Fixes #5954

--- codec-http/src/main/java/io/netty/handler/codec/http/HttpClientUpgradeHandler.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/HttpClientUpgradeHandler.java
@@ -195,6 +195,20 @@ public class HttpClientUpgradeHandler extends HttpObjectAggregator implements Ch
                 throw new IllegalStateException("Read HTTP response without requesting protocol switch");
             }
 
+            if (msg instanceof HttpResponse) {
+                HttpResponse rep = (HttpResponse) msg;
+                if (!SWITCHING_PROTOCOLS.equals(rep.status())) {
+                    // The server does not support the requested protocol, just remove this handler
+                    // and continue processing HTTP.
+                    // NOTE: not releasing the response since we're letting it propagate to the
+                    // next handler.
+                    ctx.fireUserEventTriggered(UpgradeEvent.UPGRADE_REJECTED);
+                    removeThisHandler(ctx);
+                    ctx.fireChannelRead(msg);
+                    return;
+                }
+            }
+
             if (msg instanceof FullHttpResponse) {
                 response = (FullHttpResponse) msg;
                 // Need to retain since the base class will release after returning from this method.
@@ -210,16 +224,6 @@ public class HttpClientUpgradeHandler extends HttpObjectAggregator implements Ch
 
                 assert out.size() == 1;
                 response = (FullHttpResponse) out.get(0);
-            }
-
-            if (!SWITCHING_PROTOCOLS.equals(response.status())) {
-                // The server does not support the requested protocol, just remove this handler
-                // and continue processing HTTP.
-                // NOTE: not releasing the response since we're letting it propagate to the
-                // next handler.
-                ctx.fireUserEventTriggered(UpgradeEvent.UPGRADE_REJECTED);
-                removeThisHandler(ctx);
-                return;
             }
 
             CharSequence upgradeHeader = response.headers().get(HttpHeaderNames.UPGRADE);
