489f7c33bdc1ceec320461e45d80711f63a4b8f7
Scott Mitchell <scott_mitchell@apple.com>
2016-10-31 10:21:58 -0700

    CompatibleObjectEncoder cached ObjectOutputStream backed by release buffer bug
    Motivation:
    ObjectOutputStream uses a Channel Attribute to cache a ObjectOutputStream which is backed by a ByteBuf that may be released after an object is encoded and the underlying buffer is written to the channel. On subsequent encode operations the cached ObjectOutputStream will be invalid and lead to a reference count exception.
    
    Modifications:
    - CompatibleObjectEncoder should not cache a ObjectOutputStream.
    
    Result:
    CompatibleObjectEncoder doesn't use a cached object backed by a released ByteBuf.

--- codec/src/main/java/io/netty/handler/codec/serialization/CompatibleObjectEncoder.java
+++ codec/src/main/java/io/netty/handler/codec/serialization/CompatibleObjectEncoder.java
@@ -35,10 +35,6 @@ import java.io.Serializable;
  * {@link ObjectInputStream} and {@link ObjectOutputStream}.
  */
 public class CompatibleObjectEncoder extends MessageToByteEncoder<Serializable> {
-
-    private static final AttributeKey<ObjectOutputStream> OOS =
-            AttributeKey.valueOf(CompatibleObjectEncoder.class, "OOS");
-
     private final int resetInterval;
     private int writtenObjects;
 
@@ -77,17 +73,8 @@ public class CompatibleObjectEncoder extends MessageToByteEncoder<Serializable> 
 
     @Override
     protected void encode(ChannelHandlerContext ctx, Serializable msg, ByteBuf out) throws Exception {
-        Attribute<ObjectOutputStream> oosAttr = ctx.attr(OOS);
-        ObjectOutputStream oos = oosAttr.get();
-        if (oos == null) {
-            oos = newObjectOutputStream(new ByteBufOutputStream(out));
-            ObjectOutputStream newOos = oosAttr.setIfAbsent(oos);
-            if (newOos != null) {
-                oos = newOos;
-            }
-        }
-
-        synchronized (oos) {
+        ObjectOutputStream oos = newObjectOutputStream(new ByteBufOutputStream(out));
+        try {
             if (resetInterval != 0) {
                 // Resetting will prevent OOM on the receiving side.
                 writtenObjects ++;
@@ -98,6 +85,8 @@ public class CompatibleObjectEncoder extends MessageToByteEncoder<Serializable> 
 
             oos.writeObject(msg);
             oos.flush();
+        } finally {
+            oos.close();
         }
     }
 }
