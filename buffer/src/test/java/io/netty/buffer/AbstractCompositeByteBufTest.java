netty/netty

commit 4bba7526e2f58018817972f38279cc232f519100

retained[Slice|Duplicate] buffer reference count bug
Motivation:
Currently the ByteBuf created as a result of retained[Slice|Duplicate] maintains its own reference count, and when this reference count is depleated it will release the ByteBuf returned from unwrap(). The unwrap() buffer is designed to be the 'root parent' and will skip all intermediate layers of buffers. If the intermediate layers of buffers contain a retained[Slice|Duplicate] then these reference counts will be ignored during deallocation. This may lead to deallocating the 'root parent' before all derived pooled buffers are actually released. This same issue holds if a retained[Slice|Duplicate] is in the heirachy and a 'regular' slice() or duplicate() buffer is created.

Modifications:
- AbstractPooledDerivedByteBuf must maintain a reference to the direct parent (the buffer which retained[Slice|Duplicate] was called on) and release on this buffer instead of the 'root parent' returned by unwrap()
- slice() and duplicate() buffers created from AbstractPooledDerivedByteBuf must also delegate reference count operations to their immediate parent (or first ancestor which maintains an independent reference count).

Result:
Fixes https://github.com/netty/netty/issues/5999

buffer/src/main/java/io/netty/buffer/AbstractDerivedByteBuf.java
@@ -33,40 +33,68 @@ public abstract class AbstractDerivedByteBuf extends AbstractByteBuf {
 
     @Override
     public final int refCnt() {
+        return refCnt0();
+    }
+
+    int refCnt0() {
         return unwrap().refCnt();
     }
 
     @Override
     public final ByteBuf retain() {
+        return retain0();
+    }
+
+    ByteBuf retain0() {
         unwrap().retain();
         return this;
     }
 
     @Override
     public final ByteBuf retain(int increment) {
+        return retain0(increment);
+    }
+
+    ByteBuf retain0(int increment) {
         unwrap().retain(increment);
         return this;
     }
 
     @Override
     public final ByteBuf touch() {
+        return touch0();
+    }
+
+    ByteBuf touch0() {
         unwrap().touch();
         return this;
     }
 
     @Override
     public final ByteBuf touch(Object hint) {
+        return touch0(hint);
+    }
+
+    ByteBuf touch0(Object hint) {
         unwrap().touch(hint);
         return this;
     }
 
     @Override
     public final boolean release() {
+        return release0();
+    }
+
+    boolean release0() {
         return unwrap().release();
     }
 
     @Override
     public final boolean release(int decrement) {
+        return unwrap().release(decrement);
+    }
+
+    boolean release0(int decrement) {
         return unwrap().release(decrement);
     }
 

buffer/src/main/java/io/netty/buffer/AbstractPooledDerivedByteBuf.java
@@ -17,6 +17,7 @@
 package io.netty.buffer;
 
 import io.netty.util.Recycler.Handle;
+import io.netty.util.ReferenceCounted;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -27,7 +28,14 @@ import java.nio.ByteOrder;
 abstract class AbstractPooledDerivedByteBuf extends AbstractReferenceCountedByteBuf {
 
     private final Handle<AbstractPooledDerivedByteBuf> recyclerHandle;
-    private AbstractByteBuf buffer;
+    private AbstractByteBuf rootParent;
+    /**
+     * Deallocations of a pooled derived buffer should always propagate through the entire chain of derived buffers.
+     * This is because each pooled derived buffer maintains its own reference count and we should respect each one.
+     * If deallocations cause a release of the "root parent" then then we may prematurely release the underlying
+     * content before all the derived buffers have been released.
+     */
+    private ByteBuf parent;
 
     @SuppressWarnings("unchecked")
     AbstractPooledDerivedByteBuf(Handle<? extends AbstractPooledDerivedByteBuf> recyclerHandle) {
@@ -37,14 +45,14 @@ abstract class AbstractPooledDerivedByteBuf extends AbstractReferenceCountedByte
 
     @Override
     public final AbstractByteBuf unwrap() {
-        return buffer;
+        return rootParent;
     }
 
     final <U extends AbstractPooledDerivedByteBuf> U init(
             AbstractByteBuf unwrapped, ByteBuf wrapped, int readerIndex, int writerIndex, int maxCapacity) {
-
-        wrapped.retain(); // Retain up front to ensure the wrapped buffer is accessible before doing more work.
-        this.buffer = unwrapped;
+        wrapped.retain(); // Retain up front to ensure the parent is accessible before doing more work.
+        parent = wrapped;
+        rootParent = unwrapped;
 
         try {
             maxCapacity(maxCapacity);
@@ -57,7 +65,7 @@ abstract class AbstractPooledDerivedByteBuf extends AbstractReferenceCountedByte
             return castThis;
         } finally {
             if (wrapped != null) {
-                this.buffer = null;
+                parent = rootParent = null;
                 wrapped.release();
             }
         }
@@ -65,12 +73,12 @@ abstract class AbstractPooledDerivedByteBuf extends AbstractReferenceCountedByte
 
     @Override
     protected final void deallocate() {
-        // We need to first store a reference to the wrapped buffer before recycle this instance. This is needed as
+        // We need to first store a reference to the parent before recycle this instance. This is needed as
         // otherwise it is possible that the same AbstractPooledDerivedByteBuf is again obtained and init(...) is
-        // called before we actually have a chance to call release(). This leads to call release() on the wrong buffer.
-        ByteBuf wrapped = unwrap();
+        // called before we actually have a chance to call release(). This leads to call release() on the wrong parent.
+        ByteBuf parent = this.parent;
         recyclerHandle.recycle(this);
-        wrapped.release();
+        parent.release();
     }
 
     @Override
@@ -123,5 +131,170 @@ abstract class AbstractPooledDerivedByteBuf extends AbstractReferenceCountedByte
     public final ByteBuf retainedSlice() {
         final int index = readerIndex();
         return retainedSlice(index, writerIndex() - index);
+    }
+
+    @Override
+    public ByteBuf slice(int index, int length) {
+        // All reference count methods should be inherited from this object (this is the "parent").
+        return new PooledNonRetainedSlicedByteBuf(this, unwrap(), index, length);
+    }
+
+    final ByteBuf duplicate0() {
+        // All reference count methods should be inherited from this object (this is the "parent").
+        return new PooledNonRetainedDuplicateByteBuf(this, unwrap());
+    }
+
+    private static final class PooledNonRetainedDuplicateByteBuf extends UnpooledDuplicatedByteBuf {
+        private final ReferenceCounted referenceCountDelegate;
+
+        PooledNonRetainedDuplicateByteBuf(ReferenceCounted referenceCountDelegate, AbstractByteBuf buffer) {
+            super(buffer);
+            this.referenceCountDelegate = referenceCountDelegate;
+        }
+
+        @Override
+        int refCnt0() {
+            return referenceCountDelegate.refCnt();
+        }
+
+        @Override
+        ByteBuf retain0() {
+            referenceCountDelegate.retain();
+            return this;
+        }
+
+        @Override
+        ByteBuf retain0(int increment) {
+            referenceCountDelegate.retain(increment);
+            return this;
+        }
+
+        @Override
+        ByteBuf touch0() {
+            referenceCountDelegate.touch();
+            return this;
+        }
+
+        @Override
+        ByteBuf touch0(Object hint) {
+            referenceCountDelegate.touch(hint);
+            return this;
+        }
+
+        @Override
+        boolean release0() {
+            return referenceCountDelegate.release();
+        }
+
+        @Override
+        boolean release0(int decrement) {
+            return referenceCountDelegate.release(decrement);
+        }
+
+        @Override
+        public ByteBuf duplicate() {
+            return new PooledNonRetainedDuplicateByteBuf(referenceCountDelegate, this);
+        }
+
+        @Override
+        public ByteBuf retainedDuplicate() {
+            return PooledDuplicatedByteBuf.newInstance(unwrap(), this, readerIndex(), writerIndex());
+        }
+
+        @Override
+        public ByteBuf slice(int index, int length) {
+            checkIndex0(index, length);
+            return new PooledNonRetainedSlicedByteBuf(referenceCountDelegate, unwrap(), index, length);
+        }
+
+        @Override
+        public ByteBuf retainedSlice() {
+            // Capacity is not allowed to change for a sliced ByteBuf, so length == capacity()
+            return retainedSlice(readerIndex(), capacity());
+        }
+
+        @Override
+        public ByteBuf retainedSlice(int index, int length) {
+            return PooledSlicedByteBuf.newInstance(unwrap(), this, index, length);
+        }
+    }
+
+    private static final class PooledNonRetainedSlicedByteBuf extends UnpooledSlicedByteBuf {
+        private final ReferenceCounted referenceCountDelegate;
+
+        PooledNonRetainedSlicedByteBuf(ReferenceCounted referenceCountDelegate,
+                                       AbstractByteBuf buffer, int index, int length) {
+            super(buffer, index, length);
+            this.referenceCountDelegate = referenceCountDelegate;
+        }
+
+        @Override
+        int refCnt0() {
+            return referenceCountDelegate.refCnt();
+        }
+
+        @Override
+        ByteBuf retain0() {
+            referenceCountDelegate.retain();
+            return this;
+        }
+
+        @Override
+        ByteBuf retain0(int increment) {
+            referenceCountDelegate.retain(increment);
+            return this;
+        }
+
+        @Override
+        ByteBuf touch0() {
+            referenceCountDelegate.touch();
+            return this;
+        }
+
+        @Override
+        ByteBuf touch0(Object hint) {
+            referenceCountDelegate.touch(hint);
+            return this;
+        }
+
+        @Override
+        boolean release0() {
+            return referenceCountDelegate.release();
+        }
+
+        @Override
+        boolean release0(int decrement) {
+            return referenceCountDelegate.release(decrement);
+        }
+
+        @Override
+        public ByteBuf duplicate() {
+            // Capacity is not allowed to change for a sliced ByteBuf, so length == capacity()
+            final ByteBuf duplicate = slice(0, capacity());
+            duplicate.setIndex(readerIndex(), writerIndex());
+            return duplicate;
+        }
+
+        @Override
+        public ByteBuf retainedDuplicate() {
+            return PooledDuplicatedByteBuf.newInstance(unwrap(), this, readerIndex(), writerIndex());
+        }
+
+        @Override
+        public ByteBuf slice(int index, int length) {
+            checkIndex0(index, length);
+            return new PooledNonRetainedSlicedByteBuf(referenceCountDelegate, unwrap(), idx(index), length);
+        }
+
+        @Override
+        public ByteBuf retainedSlice() {
+            // Capacity is not allowed to change for a sliced ByteBuf, so length == capacity()
+            return retainedSlice(0, capacity());
+        }
+
+        @Override
+        public ByteBuf retainedSlice(int index, int length) {
+            return PooledSlicedByteBuf.newInstance(unwrap(), this, idx(index), length);
+        }
     }
 }

buffer/src/main/java/io/netty/buffer/PooledDuplicatedByteBuf.java
@@ -40,7 +40,7 @@ final class PooledDuplicatedByteBuf extends AbstractPooledDerivedByteBuf {
     static PooledDuplicatedByteBuf newInstance(AbstractByteBuf unwrapped, ByteBuf wrapped,
                                                int readerIndex, int writerIndex) {
         final PooledDuplicatedByteBuf duplicate = RECYCLER.get();
-        duplicate.init(unwrapped, wrapped, readerIndex, writerIndex, wrapped.maxCapacity());
+        duplicate.init(unwrapped, wrapped, readerIndex, writerIndex, unwrapped.maxCapacity());
         duplicate.markReaderIndex();
         duplicate.markWriterIndex();
 
@@ -88,13 +88,15 @@ final class PooledDuplicatedByteBuf extends AbstractPooledDerivedByteBuf {
     }
 
     @Override
-    public ByteBuf slice(int index, int length) {
-        return unwrap().slice(index, length);
+    public ByteBuf retainedSlice(int index, int length) {
+        return PooledSlicedByteBuf.newInstance(unwrap(), this, index, length);
     }
 
     @Override
-    public ByteBuf retainedSlice(int index, int length) {
-        return PooledSlicedByteBuf.newInstance(unwrap(), this, index, length);
+    public ByteBuf duplicate() {
+        ByteBuf duplicate = duplicate0();
+        duplicate.setIndex(readerIndex(), writerIndex());
+        return duplicate;
     }
 
     @Override

buffer/src/main/java/io/netty/buffer/PooledSlicedByteBuf.java
@@ -102,7 +102,7 @@ final class PooledSlicedByteBuf extends AbstractPooledDerivedByteBuf {
     @Override
     public ByteBuf slice(int index, int length) {
         checkIndex0(index, length);
-        return unwrap().slice(idx(index), length);
+        return super.slice(idx(index), length);
     }
 
     @Override
@@ -113,18 +113,15 @@ final class PooledSlicedByteBuf extends AbstractPooledDerivedByteBuf {
 
     @Override
     public ByteBuf duplicate() {
-        // Capacity is not allowed to change for a sliced ByteBuf, so length == capacity()
-        final ByteBuf duplicate = unwrap().slice(adjustment, capacity());
-        duplicate.setIndex(readerIndex(), writerIndex());
+        ByteBuf duplicate = duplicate0();
+        duplicate.setIndex(idx(readerIndex()), adjustment + capacity());
         return duplicate;
     }
 
     @Override
     public ByteBuf retainedDuplicate() {
         // Capacity is not allowed to change for a sliced ByteBuf, so length == capacity()
-        final ByteBuf duplicate = retainedSlice(0, capacity());
-        duplicate.setIndex(readerIndex(), writerIndex());
-        return duplicate;
+        return PooledDuplicatedByteBuf.newInstance(unwrap(), this, idx(readerIndex()), adjustment + capacity());
     }
 
     @Override

commit ff9e86723496831e073027d591d64e2c19079779

Consistency between pooled/unpooled derived buffers
Motivation:
4bba7526e2f58018817972f38279cc232f519100 introduced changes which made pooled and unpooled derived buffers inconsistent in a few ways:
- Pooled derived buffers always generated a duplicate buffer when duplicate() was called and always generated a sliced buffer when slice() was called. Unpooled derived buffers some times generated a sliced buffer when duplicate() was called.
- The indexes that were set for duplicate buffers generated from slices were not always consistent.
There were also some various bugs in the derived pooled buffer implementation.

Modifications:
- Make pooled/unpooled consistently generate duplicate buffers when duplicate() is called and sliced buffers when slice() is called.
- Fix bugs in the derived pooled buffer

Result:
More consistent behavior from the derived pooled/unpooled buffers.

buffer/src/main/java/io/netty/buffer/AbstractDerivedByteBuf.java
@@ -91,7 +91,7 @@ public abstract class AbstractDerivedByteBuf extends AbstractByteBuf {
 
     @Override
     public final boolean release(int decrement) {
-        return unwrap().release(decrement);
+        return release0(decrement);
     }
 
     boolean release0(int decrement) {

buffer/src/main/java/io/netty/buffer/AbstractPooledDerivedByteBuf.java
@@ -269,15 +269,13 @@ abstract class AbstractPooledDerivedByteBuf extends AbstractReferenceCountedByte
 
         @Override
         public ByteBuf duplicate() {
-            // Capacity is not allowed to change for a sliced ByteBuf, so length == capacity()
-            final ByteBuf duplicate = slice(0, capacity());
-            duplicate.setIndex(readerIndex(), writerIndex());
-            return duplicate;
+            return new PooledNonRetainedDuplicateByteBuf(referenceCountDelegate, unwrap())
+                    .setIndex(idx(readerIndex()), idx(writerIndex()));
         }
 
         @Override
         public ByteBuf retainedDuplicate() {
-            return PooledDuplicatedByteBuf.newInstance(unwrap(), this, readerIndex(), writerIndex());
+            return PooledDuplicatedByteBuf.newInstance(unwrap(), this, idx(readerIndex()), idx(writerIndex()));
         }
 
         @Override

buffer/src/main/java/io/netty/buffer/AbstractUnpooledSlicedByteBuf.java
@@ -216,9 +216,7 @@ abstract class AbstractUnpooledSlicedByteBuf extends AbstractDerivedByteBuf {
 
     @Override
     public ByteBuf duplicate() {
-        final ByteBuf duplicate = unwrap().slice(adjustment, length());
-        duplicate.setIndex(readerIndex(), writerIndex());
-        return duplicate;
+        return unwrap().duplicate().setIndex(idx(readerIndex()), idx(writerIndex()));
     }
 
     @Override

buffer/src/main/java/io/netty/buffer/ByteBuf.java
@@ -2135,12 +2135,14 @@ public abstract class ByteBuf implements ReferenceCounted, Comparable<ByteBuf> {
      * Returns a buffer which shares the whole region of this buffer.
      * Modifying the content of the returned buffer or this buffer affects
      * each other's content while they maintain separate indexes and marks.
-     * This method is identical to {@code buf.slice(0, buf.capacity())}.
      * This method does not modify {@code readerIndex} or {@code writerIndex} of
      * this buffer.
      * <p>
      * The reader and writer marks will not be duplicated. Also be aware that this method will
      * NOT call {@link #retain()} and so the reference count will NOT be increased.
+     * @return A buffer whose readable content is equivalent to the buffer returned by {@link #slice()}.
+     * However this buffer will share the capacity of the underlying buffer, and therefore allows access to all of the
+     * underlying content if necessary.
      */
     public abstract ByteBuf duplicate();
 

buffer/src/main/java/io/netty/buffer/PooledDuplicatedByteBuf.java
@@ -94,9 +94,7 @@ final class PooledDuplicatedByteBuf extends AbstractPooledDerivedByteBuf {
 
     @Override
     public ByteBuf duplicate() {
-        ByteBuf duplicate = duplicate0();
-        duplicate.setIndex(readerIndex(), writerIndex());
-        return duplicate;
+        return duplicate0().setIndex(readerIndex(), writerIndex());
     }
 
     @Override

buffer/src/main/java/io/netty/buffer/PooledSlicedByteBuf.java
@@ -113,15 +113,12 @@ final class PooledSlicedByteBuf extends AbstractPooledDerivedByteBuf {
 
     @Override
     public ByteBuf duplicate() {
-        ByteBuf duplicate = duplicate0();
-        duplicate.setIndex(idx(readerIndex()), adjustment + capacity());
-        return duplicate;
+        return duplicate0().setIndex(idx(readerIndex()), idx(writerIndex()));
     }
 
     @Override
     public ByteBuf retainedDuplicate() {
-        // Capacity is not allowed to change for a sliced ByteBuf, so length == capacity()
-        return PooledDuplicatedByteBuf.newInstance(unwrap(), this, idx(readerIndex()), adjustment + capacity());
+        return PooledDuplicatedByteBuf.newInstance(unwrap(), this, idx(readerIndex()), idx(writerIndex()));
     }
 
     @Override

buffer/src/test/java/io/netty/buffer/AbstractByteBufTest.java
@@ -52,7 +52,6 @@ import static io.netty.buffer.Unpooled.buffer;
 import static io.netty.buffer.Unpooled.copiedBuffer;
 import static io.netty.buffer.Unpooled.directBuffer;
 import static io.netty.buffer.Unpooled.wrappedBuffer;
-import static io.netty.util.ReferenceCountUtil.releaseLater;
 import static io.netty.util.internal.EmptyArrays.EMPTY_BYTES;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertArrayEquals;
@@ -1729,19 +1728,15 @@ public abstract class AbstractByteBufTest {
 
         // Make sure all properties are copied.
         ByteBuf duplicate = buffer.duplicate();
-        assertEquals(buffer.readerIndex(), duplicate.readerIndex());
-        assertEquals(buffer.writerIndex(), duplicate.writerIndex());
-        assertEquals(buffer.capacity(), duplicate.capacity());
         assertSame(buffer.order(), duplicate.order());
-        for (int i = 0; i < duplicate.capacity(); i ++) {
-            assertEquals(buffer.getByte(i), duplicate.getByte(i));
-        }
+        assertEquals(buffer.readableBytes(), duplicate.readableBytes());
+        assertEquals(0, buffer.compareTo(duplicate));
 
         // Make sure the buffer content is shared.
         buffer.setByte(readerIndex, (byte) (buffer.getByte(readerIndex) + 1));
-        assertEquals(buffer.getByte(readerIndex), duplicate.getByte(readerIndex));
-        duplicate.setByte(1, (byte) (duplicate.getByte(1) + 1));
-        assertEquals(buffer.getByte(1), duplicate.getByte(1));
+        assertEquals(buffer.getByte(readerIndex), duplicate.getByte(duplicate.readerIndex()));
+        duplicate.setByte(duplicate.readerIndex(), (byte) (duplicate.getByte(duplicate.readerIndex()) + 1));
+        assertEquals(buffer.getByte(readerIndex), duplicate.getByte(duplicate.readerIndex()));
     }
 
     @Test
@@ -3411,6 +3406,15 @@ public abstract class AbstractByteBufTest {
 
         ByteBuf slice2 = slice1.retainedSlice(slice1.readerIndex() + 1, 4);
         assertEquals(0, slice2.compareTo(expected2));
+        assertEquals(0, slice2.compareTo(slice2.duplicate()));
+        assertEquals(0, slice2.compareTo(slice2.slice()));
+
+        ByteBuf tmpBuf = slice2.retainedDuplicate();
+        assertEquals(0, slice2.compareTo(tmpBuf));
+        tmpBuf.release();
+        tmpBuf = slice2.retainedSlice();
+        assertEquals(0, slice2.compareTo(tmpBuf));
+        tmpBuf.release();
 
         ByteBuf slice3 = doSlice1 ? slice2.slice(slice2.readerIndex() + 1, 2) : slice2.duplicate();
         if (doSlice1) {
@@ -3540,6 +3544,15 @@ public abstract class AbstractByteBufTest {
         ByteBuf dup2 = retainedDuplicate2 ? dup1.retainedDuplicate()
                                           : dup1.duplicate().retain();
         assertEquals(0, dup2.compareTo(expected));
+        assertEquals(0, dup2.compareTo(dup2.duplicate()));
+        assertEquals(0, dup2.compareTo(dup2.slice()));
+
+        ByteBuf tmpBuf = dup2.retainedDuplicate();
+        assertEquals(0, dup2.compareTo(tmpBuf));
+        tmpBuf.release();
+        tmpBuf = dup2.retainedSlice();
+        assertEquals(0, dup2.compareTo(tmpBuf));
+        tmpBuf.release();
 
         // The handler created a slice of the slice and is now done with it.
         dup2.release();
@@ -3574,7 +3587,7 @@ public abstract class AbstractByteBufTest {
             ByteBuf b = dup.retainedDuplicate();
             assertEquals(0, dup.compareTo(b));
             b.release();
-            assertEquals(0, dup.compareTo(dup.slice(0, dup.capacity())));
+            assertEquals(0, dup.compareTo(dup.slice(dup.readerIndex(), dup.readableBytes())));
         } finally {
             if (retainedDuplicate) {
                 dup.release();

buffer/src/test/java/io/netty/buffer/AbstractCompositeByteBufTest.java
@@ -27,10 +27,23 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-import static io.netty.buffer.Unpooled.*;
-import static io.netty.util.internal.EmptyArrays.*;
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static io.netty.buffer.Unpooled.EMPTY_BUFFER;
+import static io.netty.buffer.Unpooled.buffer;
+import static io.netty.buffer.Unpooled.compositeBuffer;
+import static io.netty.buffer.Unpooled.directBuffer;
+import static io.netty.buffer.Unpooled.wrappedBuffer;
+import static io.netty.util.internal.EmptyArrays.EMPTY_BYTES;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * An abstract test class for composite channel buffers
@@ -567,13 +580,15 @@ public abstract class AbstractCompositeByteBufTest extends AbstractByteBufTest {
         buf.release();
     }
 
+    @SuppressWarnings("deprecation")
     @Test
-    public void testComponentMustBeSlice() {
+    public void testComponentMustBeDuplicate() {
         CompositeByteBuf buf = compositeBuffer();
-        buf.addComponent(buffer(4).setIndex(1, 3));
-        assertThat(buf.component(0), is(instanceOf(AbstractUnpooledSlicedByteBuf.class)));
-        assertThat(buf.component(0).capacity(), is(2));
-        assertThat(buf.component(0).maxCapacity(), is(2));
+        buf.addComponent(buffer(4, 6).setIndex(1, 3));
+        assertThat(buf.component(0), is(instanceOf(AbstractDerivedByteBuf.class)));
+        assertThat(buf.component(0).capacity(), is(4));
+        assertThat(buf.component(0).maxCapacity(), is(6));
+        assertThat(buf.component(0).readableBytes(), is(2));
         buf.release();
     }
 

buffer/src/test/java/io/netty/buffer/SlicedByteBufTest.java
@@ -16,6 +16,7 @@
 package io.netty.buffer;
 
 import io.netty.util.internal.ThreadLocalRandom;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -138,16 +139,14 @@ public class SlicedByteBufTest extends AbstractByteBufTest {
         // Ignore for SlicedByteBuf
     }
 
-    @Test(expected = UnsupportedOperationException.class)
+    @Ignore("Sliced ByteBuf objects don't allow the capacity to change. So this test would fail and shouldn't be run")
     @Override
     public void testDuplicateCapacityChange() {
-        super.testDuplicateCapacityChange();
     }
 
-    @Test(expected = UnsupportedOperationException.class)
+    @Ignore("Sliced ByteBuf objects don't allow the capacity to change. So this test would fail and shouldn't be run")
     @Override
     public void testRetainedDuplicateCapacityChange() {
-        super.testRetainedDuplicateCapacityChange();
     }
 
     @Test