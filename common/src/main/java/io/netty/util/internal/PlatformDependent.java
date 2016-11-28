ba0542e397f89fe416c4260041530493ed9639eb
Jason Tedor <jason@tedor.me>
2016-11-28 17:25:24 -0500

    Do not try to be unsafe when told not to be unsafe
    Motivation:
    
    Netty has a flag (io.netty.noUnsafe) for specifying to Netty to not be
    unsafe. Yet, when initializing PlatformDependent0, Netty still tries to
    be unsafe. For application that specify to Netty to not be unsafe and
    run under a security manager, this can lead to an obnoxious (debug
    level) stack trace. Since Netty was told not to be unsafe, Netty should
    not try to be unsafe.
    
    Modifications:
    
    The initialization logic in PlatformDependent0 should take into account
    that Netty was told not to be unsafe. This means that we need to
    initialize PlatformDependent#IS_EXPLICIT_NO_UNSAFE as soon as possible,
    before the static initializer for PlatformDependent0 has a chance to
    run. Thus the following modifications are made:
     - initialize PlatformDependent#IS_EXPLICIT_NO_UNSAFE before any other
       code in PlatformDependent causes PlatformDependent0 to initialize
     - expose the value of PlatformDependent#IS_EXPLICIT_NO_UNSAFE for
       reading in PlatformDependent0
     - take the value of PlatformDependent#IS_EXPLICIT_NO_UNSAFE into
       account in PlatformDependent0
    
    Result:
    
    Netty does not try to be unsafe when told not to be unsafe.

--- common/src/main/java/io/netty/util/internal/PlatformDependent.java
+++ common/src/main/java/io/netty/util/internal/PlatformDependent.java
@@ -73,6 +73,9 @@ public final class PlatformDependent {
     private static final Pattern MAX_DIRECT_MEMORY_SIZE_ARG_PATTERN = Pattern.compile(
             "\\s*-XX:MaxDirectMemorySize\\s*=\\s*([0-9]+)\\s*([kKmMgG]?)\\s*$");
 
+    // this must be initialized before any code below triggers initialization of PlatformDependent0
+    private static final boolean IS_EXPLICIT_NO_UNSAFE = explicitNoUnsafe0();
+
     private static final boolean IS_ANDROID = isAndroid0();
     private static final boolean IS_WINDOWS = isWindows0();
     private static volatile Boolean IS_ROOT;
@@ -81,7 +84,6 @@ public final class PlatformDependent {
 
     private static final boolean CAN_ENABLE_TCP_NODELAY_BY_DEFAULT = !isAndroid();
 
-    private static final boolean IS_EXPLICIT_NO_UNSAFE = explicitNoUnsafe0();
     private static final boolean HAS_UNSAFE = hasUnsafe0();
     private static final boolean CAN_USE_CHM_V8 = HAS_UNSAFE && JAVA_VERSION < 8;
     private static final boolean DIRECT_BUFFER_PREFERRED =
@@ -864,6 +866,10 @@ public final class PlatformDependent {
         } else {
             return version[0];
         }
+    }
+
+    static boolean isExplicitNoUnsafe() {
+        return IS_EXPLICIT_NO_UNSAFE;
     }
 
     private static boolean explicitNoUnsafe0() {
