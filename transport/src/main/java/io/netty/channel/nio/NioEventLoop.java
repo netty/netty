118e489656450c42ed798dca9f7508e2e7e52fcf
Scott Mitchell <scott_mitchell@apple.com>
2016-12-06 09:52:13 -0800

    Use unbounded queues from JCTools 2.0.2
    Motivation:
    JCTools 2.0.2 provides an unbounded MPSC linked queue. Before we shaded JCTools we had our own unbounded MPSC linked queue and used it in various places but gave this up because there was no public equivalent available in JCTools at the time.
    
    Modifications:
    - Use JCTool's MPSC linked queue when no upper bound is specified
    
    Result:
    Fixes https://github.com/netty/netty/issues/5951

--- transport/src/main/java/io/netty/channel/nio/NioEventLoop.java
+++ transport/src/main/java/io/netty/channel/nio/NioEventLoop.java
@@ -257,7 +257,8 @@ public final class NioEventLoop extends SingleThreadEventLoop {
     @Override
     protected Queue<Runnable> newTaskQueue(int maxPendingTasks) {
         // This event loop never calls takeTask()
-        return PlatformDependent.newMpscQueue(maxPendingTasks);
+        return maxPendingTasks == Integer.MAX_VALUE ? PlatformDependent.<Runnable>newMpscQueue()
+                                                    : PlatformDependent.<Runnable>newMpscQueue(maxPendingTasks);
     }
 
     @Override
