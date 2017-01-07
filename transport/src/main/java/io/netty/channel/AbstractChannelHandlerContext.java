c1830c8b4ecf15fd3f06787e333dcd3b775dd5fb
Norman Maurer <norman_maurer@apple.com>
2017-01-07 21:08:55 +0100

    Fix missleading comment in AbstractChannelHandlerContext.invokeHandler()
    Motivation:
    
    The comment on AbstractChannelHandlerContext.invokeHandler() is incorrect and missleading. See [#6177]
    
    Modifications:
    
    Change true to false to correct the comment.
    
    Result:
    
    Fix missleading and incorrect comment.

--- transport/src/main/java/io/netty/channel/AbstractChannelHandlerContext.java
+++ transport/src/main/java/io/netty/channel/AbstractChannelHandlerContext.java
@@ -978,7 +978,7 @@ abstract class AbstractChannelHandlerContext extends DefaultAttributeMap
      * Makes best possible effort to detect if {@link ChannelHandler#handlerAdded(ChannelHandlerContext)} was called
      * yet. If not return {@code false} and if called or could not detect return {@code true}.
      *
-     * If this method returns {@code true} we will not invoke the {@link ChannelHandler} but just forward the event.
+     * If this method returns {@code false} we will not invoke the {@link ChannelHandler} but just forward the event.
      * This is needed as {@link DefaultChannelPipeline} may already put the {@link ChannelHandler} in the linked-list
      * but not called {@link ChannelHandler#handlerAdded(ChannelHandlerContext)}.
      */
