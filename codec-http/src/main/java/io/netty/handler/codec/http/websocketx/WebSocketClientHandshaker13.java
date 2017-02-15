efa40cc2296282e2b2181ce2c3e6cb6db3a32479
Stephen E. Baker <baker.stephen.e@gmail.com>
2017-02-15 14:03:59 -0500

    Correct comment for allowMaskMismatch parameter
    The allowMaskMismatch parameter used throughout websocketx allows frames
    with noncompliant masks when set to true, not false.

--- codec-http/src/main/java/io/netty/handler/codec/http/websocketx/WebSocketClientHandshaker13.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/websocketx/WebSocketClientHandshaker13.java
@@ -92,8 +92,8 @@ public class WebSocketClientHandshaker13 extends WebSocketClientHandshaker {
      *            with the websocket specifications. Client applications that communicate with a non-standard server
      *            which doesn't require masking might set this to false to achieve a higher performance.
      * @param allowMaskMismatch
-     *            Allows to loosen the masking requirement on received frames. When this is set to false then also
-     *            frames which are not masked properly according to the standard will still be accepted.
+     *            When set to true, frames which are not masked properly according to the standard will still be
+     *            accepted.
      */
     public WebSocketClientHandshaker13(URI webSocketURL, WebSocketVersion version, String subprotocol,
             boolean allowExtensions, HttpHeaders customHeaders, int maxFramePayloadLength,
