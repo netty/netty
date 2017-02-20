d12248980b19338d83e18345ab3bc25a25da7221
Norman Maurer <norman_maurer@apple.com>
2017-02-20 14:06:48 +0100

    Move AutobahnTestsuite to extra module
    Motivation:
    We should move the AutobahnTestsuite to an extra module. This allows easier to run only the testsuite or only the autobahntestsuite
    
    Modifications:
    
    Create a new module (testsuite-autobahn)
    
    Result:
    
    Better project structure.

--- testsuite-autobahn/src/main/java/io/netty/testsuite/autobahn/AutobahnServerHandler.java
+++ testsuite-autobahn/src/main/java/io/netty/testsuite/autobahn/AutobahnServerHandler.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-package io.netty.testsuite.websockets.autobahn;
+package io.netty.testsuite.autobahn;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
