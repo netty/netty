netty/netty

commit 21457d88c959d2740a898fc739adb0ee39e67c0e

Motivation:
Make the FileRegion comments about which transports are supported more accurate.
Also, eleminate any outstanding references to FileRegion.transfered as the method was renamed for spelling.

Modifications:

Class-level comment on FileRegion, can call renamed method.

Result:

More accurate documentation and less calls to deprecated methods.

transport-native-epoll/src/main/java/io/netty/channel/epoll/AbstractEpollStreamChannel.java
@@ -393,7 +393,7 @@ public abstract class AbstractEpollStreamChannel extends AbstractEpollChannel im
             }
 
             flushedAmount += localFlushedAmount;
-            if (region.transfered() >= regionCount) {
+            if (region.transferred() >= regionCount) {
                 done = true;
                 break;
             }

testsuite/src/main/java/io/netty/testsuite/transport/socket/SocketFileRegionTest.java
@@ -265,7 +265,7 @@ public class SocketFileRegionTest extends AbstractSocketTest {
         @Override
         @Deprecated
         public long transfered() {
-            return region.transfered();
+            return region.transferred();
         }
 
         @Override

transport/src/main/java/io/netty/channel/FileRegion.java
@@ -49,8 +49,6 @@ import java.nio.channels.WritableByteChannel;
  * performance.  For example, sending a large file doesn't work well in Windows.
  *
  * <h3>Not all transports support it</h3>
- *
- * Currently, the NIO transport is the only transport that supports {@link FileRegion}.
  */
 public interface FileRegion extends ReferenceCounted {
 

commit 5493ad8b40dec8afc81a0dd4ddd96deb779cba7c

Fix compile error introduced by 21457d88c959d2740a898fc739adb0ee39e67c0e

transport-native-epoll/src/main/java/io/netty/channel/epoll/AbstractEpollStreamChannel.java
@@ -393,7 +393,7 @@ public abstract class AbstractEpollStreamChannel extends AbstractEpollChannel im
             }
 
             flushedAmount += localFlushedAmount;
-            if (region.transferred() >= regionCount) {
+            if (region.transfered() >= regionCount) {
                 done = true;
                 break;
             }

testsuite/src/main/java/io/netty/testsuite/transport/socket/SocketFileRegionTest.java
@@ -265,7 +265,7 @@ public class SocketFileRegionTest extends AbstractSocketTest {
         @Override
         @Deprecated
         public long transfered() {
-            return region.transferred();
+            return region.transfered();
         }
 
         @Override