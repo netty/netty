36c6a61d33c480798511723486459f4eea04cb47
Scott Mitchell <scott_mitchell@apple.com>
2017-03-29 18:54:44 -0700

    HTTP/2 remove unnecessary buffer operations
    Motivation:
    codec-http2 has some helper methods to write to ByteBuf in a big endian fashion. This is the default memory structure for ByteBuf so these helper methods are not necessary.
    
    Modifications:
    - remove writeUnsignedInt and writeUnsignedShort
    
    Result:
    codec-http2 has less ByteBuf helper methods which are not necessary.

--- codec-http2/src/test/java/io/netty/handler/codec/http2/DefaultHttp2FrameReaderTest.java
+++ codec-http2/src/test/java/io/netty/handler/codec/http2/DefaultHttp2FrameReaderTest.java
@@ -367,7 +367,7 @@ public class DefaultHttp2FrameReaderTest {
             Http2Flags flags, int streamDependency, int weight) throws Http2Exception {
         ByteBuf headerBlock = Unpooled.buffer();
         try {
-            writeUnsignedInt(streamDependency, headerBlock);
+            headerBlock.writeInt(streamDependency);
             headerBlock.writeByte(weight - 1);
             hpackEncoder.encodeHeaders(streamId, headerBlock, headers, Http2HeadersEncoder.NEVER_SENSITIVE);
             writeFrameHeader(output, headerBlock.readableBytes(), HEADERS, flags, streamId);
@@ -393,7 +393,7 @@ public class DefaultHttp2FrameReaderTest {
     private void writePriorityFrame(
             ByteBuf output, int streamId, int streamDependency, int weight) {
         writeFrameHeader(output, 5, PRIORITY, new Http2Flags(), streamId);
-        writeUnsignedInt(streamDependency, output);
+        output.writeInt(streamDependency);
         output.writeByte(weight - 1);
     }
 }
