215b7df5809278e518353114110f55f4da230b4f
Moses Nakamura <mnakamura@twitter.com>
2017-04-04 17:19:54 -0700

    Supply a builder for Http2Codec
    Motivation:
    
    DefaultHttp2FrameWriter has constructors that it would be a hassle to
    expose as configuration parameters on Http2Codec. We should instead
    make a builder for Http2Codec.
    
    Modifications:
    
    Get rid of the public constructors on Http2Codec and instead make sure
    you can always use the builder where you would have used the constructor
    before.
    
    Result:
    
    Http2Codec can be configured more flexibly, and the SensitivityDetector
    can be configured.

--- codec-http2/src/main/java/io/netty/handler/codec/http2/Http2Codec.java
+++ codec-http2/src/main/java/io/netty/handler/codec/http2/Http2Codec.java
@@ -16,11 +16,8 @@
 package io.netty.handler.codec.http2;
 
 import io.netty.channel.ChannelDuplexHandler;
-import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.util.internal.UnstableApi;
-
-import static io.netty.handler.logging.LogLevel.INFO;
 
 /**
  * An HTTP/2 channel handler that adds a {@link Http2FrameCodec} and {@link Http2MultiplexCodec} to the pipeline before
@@ -28,58 +25,9 @@ import static io.netty.handler.logging.LogLevel.INFO;
  */
 @UnstableApi
 public final class Http2Codec extends ChannelDuplexHandler {
-    private static final Http2FrameLogger HTTP2_FRAME_LOGGER = new Http2FrameLogger(INFO, Http2Codec.class);
-
     private final Http2FrameCodec frameCodec;
     private final Http2MultiplexCodec multiplexCodec;
 
-    /**
-     * Construct a new handler whose child channels run in the same event loop as this handler.
-     *
-     * @param server {@code true} this is a server
-     * @param streamHandler the handler added to channels for remotely-created streams. It must be
-     *     {@link ChannelHandler.Sharable}. {@code null} if the event loop from the parent channel should be used.
-     */
-    public Http2Codec(boolean server, ChannelHandler streamHandler) {
-        this(server, new Http2StreamChannelBootstrap().handler(streamHandler), HTTP2_FRAME_LOGGER);
-    }
-
-    /**
-     * Construct a new handler whose child channels run in the same event loop as this handler.
-     *
-     * @param server {@code true} this is a server
-     * @param streamHandler the handler added to channels for remotely-created streams. It must be
-     *     {@link ChannelHandler.Sharable}. {@code null} if the event loop from the parent channel should be used.
-     * @param initialSettings non default initial settings to send to peer
-     */
-    public Http2Codec(boolean server, ChannelHandler streamHandler, Http2Settings initialSettings) {
-        this(server, new Http2StreamChannelBootstrap().handler(streamHandler), HTTP2_FRAME_LOGGER,
-            initialSettings);
-    }
-
-    /**
-     * Construct a new handler whose child channels run in a different event loop.
-     *
-     * @param server {@code true} this is a server
-     * @param bootstrap bootstrap used to instantiate child channels for remotely-created streams.
-     */
-    public Http2Codec(boolean server, Http2StreamChannelBootstrap bootstrap, Http2FrameLogger frameLogger) {
-        this(server, bootstrap, new DefaultHttp2FrameWriter(), frameLogger, new Http2Settings());
-    }
-
-    /**
-     * Construct a new handler whose child channels run in a different event loop.
-     *
-     * @param server {@code true} this is a server
-     * @param bootstrap bootstrap used to instantiate child channels for remotely-created streams.
-     * @param initialSettings non default initial settings to send to peer
-     */
-    public Http2Codec(boolean server, Http2StreamChannelBootstrap bootstrap, Http2FrameLogger frameLogger,
-                      Http2Settings initialSettings) {
-        this(server, bootstrap, new DefaultHttp2FrameWriter(), frameLogger, initialSettings);
-    }
-
-    // Visible for testing
     Http2Codec(boolean server, Http2StreamChannelBootstrap bootstrap, Http2FrameWriter frameWriter,
                Http2FrameLogger frameLogger, Http2Settings initialSettings) {
         frameCodec = new Http2FrameCodec(server, frameWriter, frameLogger, initialSettings);
