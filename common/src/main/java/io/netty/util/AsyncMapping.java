3d0003af81421b9304108c72961b6f593c73f28e
Roger Kapsi <rkapsi@squarespace.com>
2017-04-10 10:17:22 -0400

    undo the AsyncMapping IN/OUT change

--- common/src/main/java/io/netty/util/AsyncMapping.java
+++ common/src/main/java/io/netty/util/AsyncMapping.java
@@ -18,11 +18,11 @@ package io.netty.util;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.Promise;
 
-public interface AsyncMapping<U, R> {
+public interface AsyncMapping<IN, OUT> {
 
     /**
      * Returns the {@link Future} that will provide the result of the mapping. The given {@link Promise} will
      * be fulfilled when the result is available.
      */
-    Future<R> map(U input, Promise<R> promise);
+    Future<OUT> map(IN input, Promise<OUT> promise);
 }
