23fc5d7af731301bb1140cc29d729566d11e560b
Scott Mitchell <scott_mitchell@apple.com>
2017-05-09 12:58:29 -0700

    ByteToMessageDecoder#handlerRemoved may release cumulation buffer prematurely
    Motivation:
    ByteToMessageDecoder#handlerRemoved will immediately release the cumulation buffer, but it is possible that a child class may still be using this buffer, and therefore use a dereferenced buffer.
    
    Modifications:
    - ByteToMessageDecoder#handlerRemoved and ByteToMessageDecoder#decode should coordinate to avoid the case where a child class is using the cumulation buffer but ByteToMessageDecoder releases that buffer.
    
    Result:
    Child classes of ByteToMessageDecoder are less likely to reference a released buffer.

--- codec/src/main/java/io/netty/handler/codec/ReplayingDecoder.java
+++ codec/src/main/java/io/netty/handler/codec/ReplayingDecoder.java
@@ -364,7 +364,7 @@ public abstract class ReplayingDecoder<S> extends ByteToMessageDecoder {
                 S oldState = state;
                 int oldInputLength = in.readableBytes();
                 try {
-                    decode(ctx, replayable, out);
+                    decodeRemovalReentryProtection(ctx, replayable, out);
 
                     // Check if this handler was removed before continuing the loop.
                     // If it was removed, it is not safe to continue to operate on the buffer.
