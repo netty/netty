a17006090f5060365fd44ecca288784e52d6d72a
Julien Viet <julien@julienviet.com>
2017-05-29 14:40:35 +0200

    HttpObjectEncoder#isContentAlwaysEmpty cannot be overridden by subclasses
    Motivation:
    
    Allow subclasses of HttpObjectEncoder other than HttpServerCodec to override the isContentAlwaysEmpty method
    
    Modification:
    
    Change the method visibility from package private to protected
    
    Result:
    
    Fixes #6761

--- codec-http/src/main/java/io/netty/handler/codec/http/HttpObjectEncoder.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/HttpObjectEncoder.java
@@ -195,7 +195,14 @@ public abstract class HttpObjectEncoder<H extends HttpMessage> extends MessageTo
         }
     }
 
-    boolean isContentAlwaysEmpty(@SuppressWarnings("unused") H msg) {
+    /**
+     * Determine whether a message has a content or not. Some message may have headers indicating
+     * a content without having an actual content, e.g the response to an HEAD or CONNECT request.
+     *
+     * @param msg the message to test
+     * @return {@code true} to signal the message has no content
+     */
+    protected boolean isContentAlwaysEmpty(@SuppressWarnings("unused") H msg) {
         return false;
     }
 
