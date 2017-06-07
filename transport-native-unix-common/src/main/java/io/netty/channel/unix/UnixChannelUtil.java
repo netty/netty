95847c2c201056bf109fe821932023395a2959d6
Norman Maurer <norman_maurer@apple.com>
2017-06-07 17:12:19 +0200

    Correctly handle ByteBuf implementations which have no memoryAddress when writing to native transport
    Motivation:
    
    Commit 3c4dfed08ae2ce300f6ac1d0794d42a14c344b79 introduced a regression in handling buffers that have no memoryAddress.
    
    Modifications:
    
    Fix regression and also add unit tests.
    
    Result:
    
    It's possible again to write buffers without memory address.

--- transport-native-unix-common/src/main/java/io/netty/channel/unix/UnixChannelUtil.java
+++ transport-native-unix-common/src/main/java/io/netty/channel/unix/UnixChannelUtil.java
@@ -29,6 +29,6 @@ public final class UnixChannelUtil {
      * (We check this because otherwise we need to make it a new direct buffer.)
      */
     public static boolean isBufferCopyNeededForWrite(ByteBuf byteBuf) {
-        return !(byteBuf.hasMemoryAddress() || byteBuf.isDirect() && byteBuf.nioBufferCount() <= IOV_MAX);
+        return !byteBuf.hasMemoryAddress() || !byteBuf.isDirect() || byteBuf.nioBufferCount() > IOV_MAX;
     }
 }
