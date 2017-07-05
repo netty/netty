df09b84e46e57c565e5b675a168cd1c84370c00e
Nikolay Fedorovskikh <fenik17@gmail.com>
2017-07-05 23:29:34 +0500

    Clarify the appointment of the intermediate collection
    Motivation:
    
    An intermediate list is creating in the `EpollEventLoop#closeAll` to prevent ConcurrentModificationException. But this is not the obvious purpose has no comment.
    
    Modifications:
    
    Add comment to clarify the appointment of the intermediate collection.
    
    Result:
    
    More clear code.

--- transport-native-epoll/src/main/java/io/netty/channel/epoll/EpollEventLoop.java
+++ transport-native-epoll/src/main/java/io/netty/channel/epoll/EpollEventLoop.java
@@ -346,6 +346,8 @@ final class EpollEventLoop extends SingleThreadEventLoop {
         } catch (IOException ignore) {
             // ignore on close
         }
+        // Using the intermediate collection to prevent ConcurrentModificationException.
+        // In the `close()` method, the channel is deleted from `channels` map.
         Collection<AbstractEpollChannel> array = new ArrayList<AbstractEpollChannel>(channels.size());
 
         for (IntObjectMap.Entry<AbstractEpollChannel> entry: channels.entries()) {
