0090c9cad9ff741e53328a56e693ef17ac5e5b87
Scott Mitchell <scott_mitchell@apple.com>
2017-07-20 17:52:54 -0700

    ByteBufs which are not resizable should not throw in ensureWritable(int,boolean)
    Motivation:
    ByteBuf#ensureWritable(int,boolean) returns an int indicating the status of the resize operation. For buffers that are unmodifiable or cannot be resized this method shouldn't throw but just return 1.
    ByteBuf#ensureWriteable(int) should throw unmodifiable buffers.
    
    Modifications:
    - ReadOnlyByteBuf should be updated as described above.
    - Add a unit test to SslHandler which verifies the read only buffer can be tolerated in the aggregation algorithm.
    
    Result:
    Fixes https://github.com/netty/netty/issues/7002.

--- buffer/src/test/java/io/netty/buffer/SlicedByteBufTest.java
+++ buffer/src/test/java/io/netty/buffer/SlicedByteBufTest.java
@@ -24,6 +24,7 @@ import java.nio.ByteBuffer;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * Tests sliced channel buffers
@@ -194,6 +195,35 @@ public class SlicedByteBufTest extends AbstractByteBufTest {
             wrappedBuffer.getBytes(wrappedBuffer.readerIndex(), nioBuffer);
         } finally {
             wrappedBuffer.release();
+        }
+    }
+
+    @Test
+    public void ensureWritableWithEnoughSpaceShouldNotThrow() {
+        ByteBuf slice = newBuffer(10);
+        ByteBuf unwrapped = slice.unwrap();
+        unwrapped.writerIndex(unwrapped.writerIndex() + 5);
+        slice.writerIndex(slice.readerIndex());
+
+        // Run ensureWritable and verify this doesn't change any indexes.
+        int originalWriterIndex = slice.writerIndex();
+        int originalReadableBytes = slice.readableBytes();
+        slice.ensureWritable(originalWriterIndex - slice.writerIndex());
+        assertEquals(originalWriterIndex, slice.writerIndex());
+        assertEquals(originalReadableBytes, slice.readableBytes());
+        slice.release();
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void ensureWritableWithNotEnoughSpaceShouldThrow() {
+        ByteBuf slice = newBuffer(10);
+        ByteBuf unwrapped = slice.unwrap();
+        unwrapped.writerIndex(unwrapped.writerIndex() + 5);
+        try {
+            slice.ensureWritable(1);
+            fail();
+        } finally {
+            slice.release();
         }
     }
 }
