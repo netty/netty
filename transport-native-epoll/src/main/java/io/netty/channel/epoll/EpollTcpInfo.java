b71f5813b11589f64d26e92ae4d2f0a1799e17bc
Norman Maurer <norman_maurer@apple.com>
2017-07-22 16:31:42 +0200

    Correctly handle unsigned int values returned from TCP_INFO
    Motivation:
    
    We used an int[] to store all values that are returned in the struct for TCP_INFO which is not good enough as it uses usigned int values.
    
    Modifications:
    
    - Change int[] to long[] and correctly cast values.
    
    Result:
    
    No more truncated values.

--- transport-native-epoll/src/main/java/io/netty/channel/epoll/EpollTcpInfo.java
+++ transport-native-epoll/src/main/java/io/netty/channel/epoll/EpollTcpInfo.java
@@ -61,133 +61,133 @@ package io.netty.channel.epoll;
  */
 public final class EpollTcpInfo {
 
-    final int[] info = new int[32];
+    final long[] info = new long[32];
 
     public int state() {
-        return info[0] & 0xFF;
+        return (int) info[0];
     }
 
     public int caState() {
-        return info[1] & 0xFF;
+        return (int) info[1];
     }
 
     public int retransmits() {
-        return info[2] & 0xFF;
+        return (int) info[2];
     }
 
     public int probes() {
-        return info[3] & 0xFF;
+        return (int) info[3];
     }
 
     public int backoff() {
-        return info[4] & 0xFF;
+        return (int) info[4];
     }
 
     public int options() {
-        return info[5] & 0xFF;
+        return (int) info[5];
     }
 
     public int sndWscale() {
-        return info[6] & 0xFF;
+        return (int) info[6];
     }
 
     public int rcvWscale() {
-        return info[7] & 0xFF;
+        return (int) info[7];
     }
 
     public long rto() {
-        return info[8] & 0xFFFFFFFFL;
+        return info[8];
     }
 
     public long ato() {
-        return info[9] & 0xFFFFFFFFL;
+        return info[9];
     }
 
     public long sndMss() {
-        return info[10] & 0xFFFFFFFFL;
+        return info[10];
     }
 
     public long rcvMss() {
-        return info[11] & 0xFFFFFFFFL;
+        return info[11];
     }
 
     public long unacked() {
-        return info[12] & 0xFFFFFFFFL;
+        return info[12];
     }
 
     public long sacked() {
-        return info[13] & 0xFFFFFFFFL;
+        return info[13];
     }
 
     public long lost() {
-        return info[14] & 0xFFFFFFFFL;
+        return info[14];
     }
 
     public long retrans() {
-        return info[15] & 0xFFFFFFFFL;
+        return info[15];
     }
 
     public long fackets() {
-        return info[16] & 0xFFFFFFFFL;
+        return info[16];
     }
 
     public long lastDataSent() {
-        return info[17] & 0xFFFFFFFFL;
+        return info[17];
     }
 
     public long lastAckSent() {
-        return info[18] & 0xFFFFFFFFL;
+        return info[18];
     }
 
     public long lastDataRecv() {
-        return info[19] & 0xFFFFFFFFL;
+        return info[19];
     }
 
     public long lastAckRecv() {
-        return info[20] & 0xFFFFFFFFL;
+        return info[20];
     }
 
     public long pmtu() {
-        return info[21] & 0xFFFFFFFFL;
+        return info[21];
     }
 
     public long rcvSsthresh() {
-        return info[22] & 0xFFFFFFFFL;
+        return info[22];
     }
 
     public long rtt() {
-        return info[23] & 0xFFFFFFFFL;
+        return info[23];
     }
 
     public long rttvar() {
-        return info[24] & 0xFFFFFFFFL;
+        return info[24];
     }
 
     public long sndSsthresh() {
-        return info[25] & 0xFFFFFFFFL;
+        return info[25];
     }
 
     public long sndCwnd() {
-        return info[26] & 0xFFFFFFFFL;
+        return info[26];
     }
 
     public long advmss() {
-        return info[27] & 0xFFFFFFFFL;
+        return info[27];
     }
 
     public long reordering() {
-        return info[28] & 0xFFFFFFFFL;
+        return info[28];
     }
 
     public long rcvRtt() {
-        return info[29] & 0xFFFFFFFFL;
+        return info[29];
     }
 
     public long rcvSpace() {
-        return info[30] & 0xFFFFFFFFL;
+        return info[30];
     }
 
     public long totalRetrans() {
-        return info[31] & 0xFFFFFFFFL;
+        return info[31];
     }
 }
