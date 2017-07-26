b958ce190c1591eb47942d3be2d75a74d762d4f7
Norman Maurer <norman_maurer@apple.com>
2017-07-26 20:00:31 +0200

    DefaultChannelPipeline.estimatorHandle needs to be volatile
    Motivation:
    
    DefaultChannelPipeline.estimatorHandle needs to be volatile as its accessed from different threads.
    
    Modifications:
    
    Make DefaultChannelPipeline.estimatorHandle volatile and correctly init it via CAS
    
    Result:
    
    No more race.

--- transport/src/main/java/io/netty/channel/DefaultChannelPipeline.java
+++ transport/src/main/java/io/netty/channel/DefaultChannelPipeline.java
@@ -35,6 +35,7 @@ import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.WeakHashMap;
 import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
 /**
  * The default {@link ChannelPipeline} implementation.  It is usually created
@@ -55,12 +56,15 @@ public class DefaultChannelPipeline implements ChannelPipeline {
         }
     };
 
+    private static final AtomicReferenceFieldUpdater<DefaultChannelPipeline, MessageSizeEstimator.Handle> ESTIMATOR =
+            AtomicReferenceFieldUpdater.newUpdater(
+                    DefaultChannelPipeline.class, MessageSizeEstimator.Handle.class, "estimatorHandle");
     final AbstractChannelHandlerContext head;
     final AbstractChannelHandlerContext tail;
 
     private final Channel channel;
     private Map<EventExecutorGroup, EventExecutor> childExecutors;
-    private MessageSizeEstimator.Handle estimatorHandle;
+    private volatile MessageSizeEstimator.Handle estimatorHandle;
     private boolean firstRegistration = true;
 
     /**
@@ -90,10 +94,14 @@ public class DefaultChannelPipeline implements ChannelPipeline {
     }
 
     final MessageSizeEstimator.Handle estimatorHandle() {
-        if (estimatorHandle == null) {
-            estimatorHandle = channel.config().getMessageSizeEstimator().newHandle();
+        MessageSizeEstimator.Handle handle = estimatorHandle;
+        if (handle == null) {
+            handle = channel.config().getMessageSizeEstimator().newHandle();
+            if (!ESTIMATOR.compareAndSet(this, null, handle)) {
+                handle = estimatorHandle;
+            }
         }
-        return estimatorHandle;
+        return handle;
     }
 
     private AbstractChannelHandlerContext newContext(EventExecutorGroup group, String name, ChannelHandler handler) {
