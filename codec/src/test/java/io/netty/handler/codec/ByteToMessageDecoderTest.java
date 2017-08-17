netty/netty

commit d63bb4811ed8ccd5d9e45853f3ac6aee9da7ecab

Only call ctx.fireChannelReadComplete() if ByteToMessageDecoder decoded at least one message.
Motivation:

Its wasteful and also confusing that channelReadComplete() is called even if there was no message forwarded to the next handler.

Modifications:

- Only call ctx.fireChannelReadComplete() if at least one message was decoded
- Add unit test

Result:

Less confusing behavior. Fixes [#4312].

codec/src/main/java/io/netty/handler/codec/ByteToMessageDecoder.java
@@ -313,15 +313,18 @@ public abstract class ByteToMessageDecoder extends ChannelInboundHandlerAdapter 
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        channelReadComplete(ctx, !decodeWasNull);
+    }
+
+    protected final void channelReadComplete(ChannelHandlerContext ctx, boolean readData) throws Exception {
         numReads = 0;
         discardSomeReadBytes();
-        if (decodeWasNull) {
-            decodeWasNull = false;
-            if (!ctx.channel().config().isAutoRead()) {
-                ctx.read();
-            }
+        decodeWasNull = false;
+        if (readData) {
+            ctx.fireChannelReadComplete();
+        } else if (!ctx.channel().config().isAutoRead()) {
+            ctx.read();
         }
-        ctx.fireChannelReadComplete();
     }
 
     protected final void discardSomeReadBytes() {

codec/src/test/java/io/netty/handler/codec/ByteToMessageDecoderTest.java
@@ -26,6 +26,8 @@ import org.junit.Test;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -304,5 +306,42 @@ public class ByteToMessageDecoderTest {
         assertFalse(channel.writeInbound(Unpooled.buffer(8).writeByte(1).asReadOnly()));
         assertFalse(channel.writeInbound(Unpooled.wrappedBuffer(new byte[] { (byte) 2 })));
         assertFalse(channel.finish());
+    }
+
+    @Test
+    public void testFireChannelReadComplete() {
+        final AtomicBoolean readCompleteExpected = new AtomicBoolean();
+        final AtomicInteger readCompleteCount = new AtomicInteger();
+        EmbeddedChannel channel = new EmbeddedChannel(new ByteToMessageDecoder() {
+
+            @Override
+            protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+                if (in.readableBytes() > 1) {
+                    readCompleteExpected.set(true);
+                    out.add(in.readBytes(in.readableBytes()));
+                }
+            }
+        }, new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+                assertTrue(readCompleteExpected.get());
+                readCompleteCount.incrementAndGet();
+            }
+        });
+
+        assertFalse(channel.writeInbound(Unpooled.wrappedBuffer(new byte[] {'a'})));
+        assertTrue(channel.writeInbound(Unpooled.wrappedBuffer(new byte[] {'b'})));
+        ByteBuf b = channel.readInbound();
+
+        ByteBuf expected = Unpooled.wrappedBuffer(new byte[] {'a', 'b'});
+        assertEquals(expected, b);
+        b.release();
+        expected.release();
+
+        assertTrue(readCompleteExpected.get());
+
+        assertFalse(channel.finish());
+
+        assertEquals(1, readCompleteCount.get());
     }
 }

codec-http/src/main/java/io/netty/handler/codec/spdy/SpdyFrameCodec.java
@@ -125,13 +125,10 @@ public class SpdyFrameCodec extends ByteToMessageDecoder
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        if (!read) {
-            if (!ctx.channel().config().isAutoRead()) {
-                ctx.read();
-            }
-        }
+        boolean wasRead = read;
         read = false;
-        super.channelReadComplete(ctx);
+
+        channelReadComplete(ctx, wasRead);
     }
 
     @Override

handler/src/main/java/io/netty/handler/ssl/SslHandler.java
@@ -1167,14 +1167,15 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        // Discard bytes of the cumulation buffer if needed.
-        discardSomeReadBytes();
-
         flushIfNeeded(ctx);
-        readIfNeeded(ctx);
-
+        boolean readData = firedChannelRead;
         firedChannelRead = false;
-        ctx.fireChannelReadComplete();
+        // if readData is false channelReadComplete(....) will take care of calling read.
+        if (readData && !handshakePromise.isDone() && !ctx.channel().config().isAutoRead()) {
+            // If handshake is not finished yet, we need more data.
+            ctx.read();
+        }
+        channelReadComplete(ctx, readData);
     }
 
     private void readIfNeeded(ChannelHandlerContext ctx) {

commit 3c47db22740ff56916c851ec4b9186818855d80f

ByteToMessageDecoder must call fireChannelReadComplete if any channelRead(...) produced data.
Motivation:

Commit d63bb4811ed8ccd5d9e45853f3ac6aee9da7ecab introduced a regression which could lead to missing calls of fireChannelReadComplete(). The problem could happen when we had multiple channelRead(...) calls and the last call of it not produced any data while the previous ones did. In this case we missed to call fireChannelReadComplete() as decodeWasNull was previous set to true while it should be false.

Modifications:

- Correctly only update decodeWasNull to true if no previous channelRead(...) call produced data.
- Add unit test.

Result:

Correctly call fireChannelReadComplete() in all cases.

codec/src/main/java/io/netty/handler/codec/ByteToMessageDecoder.java
@@ -136,7 +136,7 @@ public abstract class ByteToMessageDecoder extends ChannelInboundHandlerAdapter 
     ByteBuf cumulation;
     private Cumulator cumulator = MERGE_CUMULATOR;
     private boolean singleDecode;
-    private boolean decodeWasNull;
+    private boolean decodeWasNull = true;
     private boolean first;
     /**
      * A bitmask where the bits are defined as
@@ -280,7 +280,9 @@ public abstract class ByteToMessageDecoder extends ChannelInboundHandlerAdapter 
                 }
 
                 int size = out.size();
-                decodeWasNull = !out.insertSinceRecycled();
+                // Only update decodeWasNull if we did not produce data in a previous channelRead(...) call.
+                decodeWasNull &= !out.insertSinceRecycled();
+
                 fireChannelRead(ctx, out, size);
                 out.recycle();
             }
@@ -319,7 +321,8 @@ public abstract class ByteToMessageDecoder extends ChannelInboundHandlerAdapter 
     protected final void channelReadComplete(ChannelHandlerContext ctx, boolean readData) throws Exception {
         numReads = 0;
         discardSomeReadBytes();
-        decodeWasNull = false;
+        // Set back to true we will adjust it in channelRead(...) again if needed
+        decodeWasNull = true;
         if (readData) {
             ctx.fireChannelReadComplete();
         } else if (!ctx.channel().config().isAutoRead()) {

codec/src/test/java/io/netty/handler/codec/ByteToMessageDecoderTest.java
@@ -29,6 +29,7 @@ import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import static io.netty.buffer.Unpooled.wrappedBuffer;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
@@ -50,7 +51,7 @@ public class ByteToMessageDecoderTest {
             }
         });
 
-        ByteBuf buf = Unpooled.wrappedBuffer(new byte[] {'a', 'b', 'c'});
+        ByteBuf buf = wrappedBuffer(new byte[] {'a', 'b', 'c'});
         channel.writeInbound(buf.copy());
         ByteBuf b = channel.readInbound();
         assertEquals(b, buf.skipBytes(1));
@@ -77,7 +78,7 @@ public class ByteToMessageDecoderTest {
         });
 
         channel.writeInbound(buf.copy());
-        ByteBuf expected = Unpooled.wrappedBuffer(new byte[] {'b', 'c'});
+        ByteBuf expected = wrappedBuffer(new byte[] {'b', 'c'});
         ByteBuf b = channel.readInbound();
         assertEquals(expected, b);
         expected.release();
@@ -107,7 +108,7 @@ public class ByteToMessageDecoderTest {
         EmbeddedChannel channel = newInternalBufferTestChannel();
         assertTrue(channel.writeInbound(buf));
         assertTrue(channel.finish());
-        ByteBuf expected = Unpooled.wrappedBuffer(new byte[] {'b'});
+        ByteBuf expected = wrappedBuffer(new byte[] {'b'});
         ByteBuf b = channel.readInbound();
         assertEquals(expected, b);
         assertNull(channel.readInbound());
@@ -150,7 +151,7 @@ public class ByteToMessageDecoderTest {
         byte[] bytes = new byte[1024];
         PlatformDependent.threadLocalRandom().nextBytes(bytes);
 
-        assertTrue(channel.writeInbound(Unpooled.wrappedBuffer(bytes)));
+        assertTrue(channel.writeInbound(wrappedBuffer(bytes)));
         assertTrue(channel.finishAndReleaseAll());
     }
 
@@ -225,7 +226,7 @@ public class ByteToMessageDecoderTest {
             }
         });
 
-        ByteBuf buf = Unpooled.wrappedBuffer(new byte[] { 'a', 'b', 'c' });
+        ByteBuf buf = wrappedBuffer(new byte[] { 'a', 'b', 'c' });
         assertTrue(channel.writeInbound(buf.copy()));
         ByteBuf b = channel.readInbound();
         assertEquals(b, buf.skipBytes(1));
@@ -247,8 +248,8 @@ public class ByteToMessageDecoderTest {
         byte[] bytes = new byte[1024];
         PlatformDependent.threadLocalRandom().nextBytes(bytes);
 
-        assertTrue(channel.writeInbound(Unpooled.wrappedBuffer(bytes)));
-        assertBuffer(Unpooled.wrappedBuffer(bytes), (ByteBuf) channel.readInbound());
+        assertTrue(channel.writeInbound(wrappedBuffer(bytes)));
+        assertBuffer(wrappedBuffer(bytes), (ByteBuf) channel.readInbound());
         assertNull(channel.readInbound());
         assertFalse(channel.finish());
         assertNull(channel.readInbound());
@@ -279,11 +280,11 @@ public class ByteToMessageDecoderTest {
         byte[] bytes = new byte[1024];
         PlatformDependent.threadLocalRandom().nextBytes(bytes);
 
-        assertTrue(channel.writeInbound(Unpooled.wrappedBuffer(bytes)));
-        assertBuffer(Unpooled.wrappedBuffer(bytes, 0, bytes.length - 1), (ByteBuf) channel.readInbound());
+        assertTrue(channel.writeInbound(wrappedBuffer(bytes)));
+        assertBuffer(wrappedBuffer(bytes, 0, bytes.length - 1), (ByteBuf) channel.readInbound());
         assertNull(channel.readInbound());
         assertTrue(channel.finish());
-        assertBuffer(Unpooled.wrappedBuffer(bytes, bytes.length - 1, 1), (ByteBuf) channel.readInbound());
+        assertBuffer(wrappedBuffer(bytes, bytes.length - 1, 1), (ByteBuf) channel.readInbound());
         assertNull(channel.readInbound());
     }
 
@@ -304,7 +305,7 @@ public class ByteToMessageDecoderTest {
             }
         });
         assertFalse(channel.writeInbound(Unpooled.buffer(8).writeByte(1).asReadOnly()));
-        assertFalse(channel.writeInbound(Unpooled.wrappedBuffer(new byte[] { (byte) 2 })));
+        assertFalse(channel.writeInbound(wrappedBuffer(new byte[] { (byte) 2 })));
         assertFalse(channel.finish());
     }
 
@@ -329,11 +330,57 @@ public class ByteToMessageDecoderTest {
             }
         });
 
-        assertFalse(channel.writeInbound(Unpooled.wrappedBuffer(new byte[] {'a'})));
-        assertTrue(channel.writeInbound(Unpooled.wrappedBuffer(new byte[] {'b'})));
+        assertFalse(channel.writeInbound(wrappedBuffer(new byte[] {'a'})));
+        assertTrue(channel.writeInbound(wrappedBuffer(new byte[] {'b'})));
         ByteBuf b = channel.readInbound();
 
-        ByteBuf expected = Unpooled.wrappedBuffer(new byte[] {'a', 'b'});
+        ByteBuf expected = wrappedBuffer(new byte[] {'a', 'b'});
+        assertEquals(expected, b);
+        b.release();
+        expected.release();
+
+        assertTrue(readCompleteExpected.get());
+
+        assertFalse(channel.finish());
+
+        assertEquals(1, readCompleteCount.get());
+    }
+
+    @Test
+    public void testFireChannelReadCompleteWhenSecondProduceNoData() {
+        final AtomicBoolean readCompleteExpected = new AtomicBoolean();
+        final AtomicInteger readCompleteCount = new AtomicInteger();
+        EmbeddedChannel channel = new EmbeddedChannel(new ByteToMessageDecoder() {
+
+            private boolean decoded;
+
+            @Override
+            protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+                if (!decoded && in.readableBytes() > 1) {
+                    readCompleteExpected.set(true);
+                    out.add(in.readBytes(in.readableBytes()));
+                    decoded = true;
+                }
+            }
+        }, new ChannelInboundHandlerAdapter() {
+            @Override
+            public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+                assertTrue(readCompleteExpected.get());
+                readCompleteCount.incrementAndGet();
+            }
+        });
+
+        channel.pipeline().fireChannelRead(wrappedBuffer(new byte[] {'a'}));
+        channel.pipeline().fireChannelReadComplete();
+        assertEquals(0, readCompleteCount.get());
+
+        channel.pipeline().fireChannelRead(wrappedBuffer(new byte[] {'b'}));
+        channel.pipeline().fireChannelRead(wrappedBuffer(new byte[] {'a'}));
+        channel.pipeline().fireChannelReadComplete();
+
+        ByteBuf b = channel.readInbound();
+
+        ByteBuf expected = wrappedBuffer(new byte[] {'a', 'b'});
         assertEquals(expected, b);
         b.release();
         expected.release();