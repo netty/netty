2e8bd479a1be2dd379899202bdfc900e114d79dc
durigon <durigon@nate.com>
2017-09-16 01:46:30 +0900

    Add missing comments

--- codec-http2/src/test/java/io/netty/handler/codec/http2/InboundHttp2ToHttpAdapterTest.java
+++ codec-http2/src/test/java/io/netty/handler/codec/http2/InboundHttp2ToHttpAdapterTest.java
@@ -498,6 +498,7 @@ public class InboundHttp2ToHttpAdapterTest {
             assertEquals(request, capturedRequests.get(0));
 
             final Http2Headers http2Headers = new DefaultHttp2Headers().status(new AsciiString("200"));
+            // The PUSH_PROMISE frame includes a header block that contains a
             // complete set of request header fields that the server attributes to
             // the request.
             // https://tools.ietf.org/html/rfc7540#section-8.2.1
