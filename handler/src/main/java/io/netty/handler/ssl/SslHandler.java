netty/netty

commit 86e653e04fb452c92154e39cd7189615dc0ec323

SslHandler aggregation of plaintext data on write
Motivation:
Each call to SSL_write may introduce about ~100 bytes of overhead. The OpenSslEngine (based upon OpenSSL) is not able to do gathering writes so this means each wrap operation will incur the ~100 byte overhead. This commit attempts to increase goodput by aggregating the plaintext in chunks of <a href="https://tools.ietf.org/html/rfc5246#section-6.2">2^14</a>. If many small chunks are written this can increase goodput, decrease the amount of calls to SSL_write, and decrease overall encryption operations.

Modifications:
- Introduce SslHandlerCoalescingBufferQueue in SslHandler which will aggregate up to 2^14 chunks of plaintext by default
- Introduce SslHandler#setWrapDataSize to control how much data should be aggregated for each write. Aggregation can be disabled by setting this value to <= 0.

Result:
Better goodput when using SslHandler and the OpenSslEngine.

handler/src/main/java/io/netty/handler/ssl/SslHandler.java
@@ -20,6 +20,7 @@ import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.CompositeByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.AbstractCoalescingBufferQueue;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelConfig;
 import io.netty.channel.ChannelException;
@@ -31,7 +32,6 @@ import io.netty.channel.ChannelOutboundHandler;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.ChannelPromise;
 import io.netty.channel.ChannelPromiseNotifier;
-import io.netty.channel.PendingWriteQueue;
 import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.handler.codec.UnsupportedMessageTypeException;
 import io.netty.util.ReferenceCounted;
@@ -43,6 +43,7 @@ import io.netty.util.concurrent.ImmediateExecutor;
 import io.netty.util.concurrent.Promise;
 import io.netty.util.internal.PlatformDependent;
 import io.netty.util.internal.ThrowableUtil;
+import io.netty.util.internal.UnstableApi;
 import io.netty.util.internal.logging.InternalLogger;
 import io.netty.util.internal.logging.InternalLoggerFactory;
 
@@ -66,6 +67,14 @@ import javax.net.ssl.SSLEngineResult.Status;
 import javax.net.ssl.SSLException;
 import javax.net.ssl.SSLSession;
 
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLEngineResult;
+import javax.net.ssl.SSLEngineResult.HandshakeStatus;
+import javax.net.ssl.SSLEngineResult.Status;
+import javax.net.ssl.SSLException;
+import javax.net.ssl.SSLSession;
+
+import static io.netty.buffer.ByteBufUtil.ensureWritableSuccess;
 import static io.netty.handler.ssl.SslUtils.getEncryptedPacketLength;
 
 /**
@@ -179,6 +188,12 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
             new SSLException("handshake timed out"), SslHandler.class, "handshake(...)");
     private static final ClosedChannelException CHANNEL_CLOSED = ThrowableUtil.unknownStackTrace(
             new ClosedChannelException(), SslHandler.class, "channelInactive(...)");
+
+    /**
+     * <a href="https://tools.ietf.org/html/rfc5246#section-6.2">2^14</a> which is the maximum sized plaintext chunk
+     * allowed by the TLS RFC.
+     */
+    private static final int MAX_PLAINTEXT_LENGTH = 16 * 1024;
 
     private enum SslEngineType {
         TCNATIVE(true, COMPOSITE_CUMULATOR) {
@@ -344,8 +359,7 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
     private boolean sentFirstMessage;
     private boolean flushedBeforeHandshake;
     private boolean readDuringHandshake;
-    private PendingWriteQueue pendingUnencryptedWrites;
-
+    private final SslHandlerCoalescingBufferQueue pendingUnencryptedWrites = new SslHandlerCoalescingBufferQueue(16);
     private Promise<Channel> handshakePromise = new LazyChannelPromise();
     private final LazyChannelPromise sslClosePromise = new LazyChannelPromise();
 
@@ -443,6 +457,31 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
                     "handshakeTimeoutMillis: " + handshakeTimeoutMillis + " (expected: >= 0)");
         }
         this.handshakeTimeoutMillis = handshakeTimeoutMillis;
+    }
+
+    /**
+     * Sets the number of bytes to pass to each {@link SSLEngine#wrap(ByteBuffer[], int, int, ByteBuffer)} call.
+     * <p>
+     * This value will partition data which is passed to write
+     * {@link #write(ChannelHandlerContext, Object, ChannelPromise)}. The partitioning will work as follows:
+     * <ul>
+     * <li>If {@code wrapDataSize <= 0} then we will write each data chunk as is.</li>
+     * <li>If {@code wrapDataSize > data size} then we will attempt to aggregate multiple data chunks together.</li>
+     * <li>If {@code wrapDataSize > data size}  Else if {@code wrapDataSize <= data size} then we will divide the data
+     * into chunks of {@code wrapDataSize} when writing.</li>
+     * </ul>
+     * <p>
+     * If the {@link SSLEngine} doesn't support a gather wrap operation (e.g. {@link SslProvider#OPENSSL}) then
+     * aggregating data before wrapping can help reduce the ratio between TLS overhead vs data payload which will lead
+     * to better goodput. Writing fixed chunks of data can also help target the underlying transport's (e.g. TCP)
+     * frame size. Under lossy/congested network conditions this may help the peer get full TLS packets earlier and
+     * be able to do work sooner, as opposed to waiting for the all the pieces of the TLS packet to arrive.
+     * @param wrapDataSize the number of bytes which will be passed to each
+     *      {@link SSLEngine#wrap(ByteBuffer[], int, int, ByteBuffer)} call.
+     */
+    @UnstableApi
+    public final void setWrapDataSize(int wrapDataSize) {
+        pendingUnencryptedWrites.wrapDataSize = wrapDataSize;
     }
 
     /**
@@ -610,7 +649,8 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
     public void handlerRemoved0(ChannelHandlerContext ctx) throws Exception {
         if (!pendingUnencryptedWrites.isEmpty()) {
             // Check if queue is not empty first because create a new ChannelException is expensive
-            pendingUnencryptedWrites.removeAndFailAll(new ChannelException("Pending write on removal of SslHandler"));
+            pendingUnencryptedWrites.releaseAndFailAll(ctx,
+                    new ChannelException("Pending write on removal of SslHandler"));
         }
         if (engine instanceof ReferenceCounted) {
             ((ReferenceCounted) engine).release();
@@ -660,7 +700,7 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
             promise.setFailure(new UnsupportedMessageTypeException(msg, ByteBuf.class));
             return;
         }
-        pendingUnencryptedWrites.add(msg, promise);
+        pendingUnencryptedWrites.add((ByteBuf) msg, promise);
     }
 
     @Override
@@ -669,7 +709,7 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
         // created with startTLS flag turned on.
         if (startTls && !sentFirstMessage) {
             sentFirstMessage = true;
-            pendingUnencryptedWrites.removeAndWriteAll();
+            pendingUnencryptedWrites.writeAndRemoveAll(ctx);
             forceFlush(ctx);
             return;
         }
@@ -709,15 +749,18 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
         ByteBufAllocator alloc = ctx.alloc();
         boolean needUnwrap = false;
         try {
+            final int wrapDataSize = pendingUnencryptedWrites.wrapDataSize;
             // Only continue to loop if the handler was not removed in the meantime.
             // See https://github.com/netty/netty/issues/5860
             while (!ctx.isRemoved()) {
-                Object msg = pendingUnencryptedWrites.current();
-                if (msg == null) {
+                promise = ctx.newPromise();
+                ByteBuf buf = wrapDataSize > 0 ?
+                        pendingUnencryptedWrites.remove(alloc, wrapDataSize, promise) :
+                        pendingUnencryptedWrites.removeFirst(promise);
+                if (buf == null) {
                     break;
                 }
 
-                ByteBuf buf = (ByteBuf) msg;
                 if (out == null) {
                     out = allocateOutNetBuf(ctx, buf.readableBytes(), buf.nioBufferCount());
                 }
@@ -725,15 +768,18 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
                 SSLEngineResult result = wrap(alloc, engine, buf, out);
 
                 if (result.getStatus() == Status.CLOSED) {
+                    buf.release();
+                    promise.tryFailure(SSLENGINE_CLOSED);
+                    promise = null;
                     // SSLEngine has been closed already.
                     // Any further write attempts should be denied.
-                    pendingUnencryptedWrites.removeAndFailAll(SSLENGINE_CLOSED);
+                    pendingUnencryptedWrites.releaseAndFailAll(ctx, SSLENGINE_CLOSED);
                     return;
                 } else {
-                    if (!buf.isReadable()) {
-                        promise = pendingUnencryptedWrites.remove();
+                    if (buf.isReadable()) {
+                        pendingUnencryptedWrites.addFirst(buf);
                     } else {
-                        promise = null;
+                        buf.release();
                     }
 
                     switch (result.getHandshakeStatus()) {
@@ -1424,7 +1470,7 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
             notifyHandshakeFailure(cause);
         } finally {
             // Ensure we remove and fail all pending writes in all cases and so release memory quickly.
-            pendingUnencryptedWrites.removeAndFailAll(cause);
+            pendingUnencryptedWrites.releaseAndFailAll(ctx, cause);
         }
     }
 
@@ -1485,7 +1531,6 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
     @Override
     public void handlerAdded(final ChannelHandlerContext ctx) throws Exception {
         this.ctx = ctx;
-        pendingUnencryptedWrites = new PendingWriteQueue(ctx);
 
         if (ctx.channel().isActive() && engine.getUseClientMode()) {
             // Begin the initial handshake.
@@ -1738,6 +1783,73 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
      */
     private ByteBuf allocateOutNetBuf(ChannelHandlerContext ctx, int pendingBytes, int numComponents) {
         return allocate(ctx, engineType.calculateWrapBufferCapacity(this, pendingBytes, numComponents));
+    }
+
+    /**
+     * Each call to SSL_write will introduce about ~100 bytes of overhead. This coalescing queue attempts to increase
+     * goodput by aggregating the plaintext in chunks of {@link #wrapDataSize}. If many small chunks are written
+     * this can increase goodput, decrease the amount of calls to SSL_write, and decrease overall encryption operations.
+     */
+    private static final class SslHandlerCoalescingBufferQueue extends AbstractCoalescingBufferQueue {
+        volatile int wrapDataSize = MAX_PLAINTEXT_LENGTH;
+
+        SslHandlerCoalescingBufferQueue(int initSize) {
+            super(initSize);
+        }
+
+        @Override
+        protected ByteBuf compose(ByteBufAllocator alloc, ByteBuf cumulation, ByteBuf next) {
+            final int wrapDataSize = this.wrapDataSize;
+            if (cumulation instanceof CompositeByteBuf) {
+                CompositeByteBuf composite = (CompositeByteBuf) cumulation;
+                int numComponents = composite.numComponents();
+                if (numComponents == 0 ||
+                        !attemptCopyToCumulation(composite.internalComponent(numComponents - 1), next, wrapDataSize)) {
+                    composite.addComponent(true, next);
+                }
+                return composite;
+            }
+            if (attemptCopyToCumulation(cumulation, next, wrapDataSize)) {
+                return cumulation;
+            }
+            CompositeByteBuf composite = alloc.compositeDirectBuffer(size() + 2);
+            composite.addComponent(true, cumulation);
+            composite.addComponent(true, next);
+            return composite;
+        }
+
+        @Override
+        protected ByteBuf composeFirst(ByteBufAllocator allocator, ByteBuf first) {
+            if (first instanceof CompositeByteBuf) {
+                CompositeByteBuf composite = (CompositeByteBuf) first;
+                first = allocator.directBuffer(composite.readableBytes());
+                first.writeBytes(composite);
+                composite.release();
+            }
+            return first;
+        }
+
+        @Override
+        protected ByteBuf removeEmptyValue() {
+            return null;
+        }
+
+        private static boolean attemptCopyToCumulation(ByteBuf cumulation, ByteBuf next, int wrapDataSize) {
+            final int inReadableBytes = next.readableBytes();
+            final int cumulationCapacity = cumulation.capacity();
+            if (wrapDataSize - cumulation.readableBytes() >= inReadableBytes &&
+                    // Avoid using the same buffer if next's data would make cumulation exceed the wrapDataSize.
+                    // Only copy if there is enough space available and the capacity is large enough, and attempt to
+                    // resize if the capacity is small.
+                    (cumulation.isWritable(inReadableBytes) && cumulationCapacity >= wrapDataSize ||
+                            cumulationCapacity < wrapDataSize &&
+                                    ensureWritableSuccess(cumulation.ensureWritable(inReadableBytes, false)))) {
+                cumulation.writeBytes(next);
+                next.release();
+                return true;
+            }
+            return false;
+        }
     }
 
     private final class LazyChannelPromise extends DefaultPromise<Channel> {

commit 398677640de5accd0457ec12fd425ce1ad00a843

Ensure Channel.isWritable() works with SslHandlerCoalescingBufferQueue
Motivation:

A regression is introduced by commit 86e653e04fb452c92154e39cd7189615dc0ec323.
The new SslHandlerCoalescingBufferQueue does not update the pending outbound bytes.
Thus the Channel.isWritable() does not work anymore. External implementations that
rely on the ChannelInboundHandlerAdapter.channelWritabilityChanged do not receive
this event anymore if SslHandler in the pipeline.
This is related to issues #2752 and #7264

Modifications:

- Introduce PendingWritesTrackerFactory extracted from PendingWriteQueue
- increment/decrement the pending outbound bytes when adding/removing
  content via AbstractCoalescingBufferQueue methods

Result:

Channel.isWritable() works as expected. External implementations receive
the event ChannelInboundHandlerAdapter.channelWritabilityChanged

handler/src/main/java/io/netty/handler/ssl/SslHandler.java
@@ -32,6 +32,8 @@ import io.netty.channel.ChannelOutboundHandler;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.ChannelPromise;
 import io.netty.channel.ChannelPromiseNotifier;
+import io.netty.channel.PendingWritesTrackerFactory;
+import io.netty.channel.PendingWritesTrackerFactory.PendingWritesTracker;
 import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.handler.codec.UnsupportedMessageTypeException;
 import io.netty.util.ReferenceCounted;
@@ -352,7 +354,8 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
     private boolean sentFirstMessage;
     private boolean flushedBeforeHandshake;
     private boolean readDuringHandshake;
-    private final SslHandlerCoalescingBufferQueue pendingUnencryptedWrites = new SslHandlerCoalescingBufferQueue(16);
+    private SslHandlerCoalescingBufferQueue pendingUnencryptedWrites;
+    private volatile int wrapDataSize = MAX_PLAINTEXT_LENGTH;
     private Promise<Channel> handshakePromise = new LazyChannelPromise();
     private final LazyChannelPromise sslClosePromise = new LazyChannelPromise();
 
@@ -474,7 +477,10 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
      */
     @UnstableApi
     public final void setWrapDataSize(int wrapDataSize) {
-        pendingUnencryptedWrites.wrapDataSize = wrapDataSize;
+        this.wrapDataSize = wrapDataSize;
+        if (this.pendingUnencryptedWrites != null) {
+            this.pendingUnencryptedWrites.wrapDataSize = wrapDataSize;
+        }
     }
 
     /**
@@ -1535,6 +1541,8 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
     @Override
     public void handlerAdded(final ChannelHandlerContext ctx) throws Exception {
         this.ctx = ctx;
+        this.pendingUnencryptedWrites = new SslHandlerCoalescingBufferQueue(16, ctx);
+        this.pendingUnencryptedWrites.wrapDataSize = this.wrapDataSize;
 
         if (ctx.channel().isActive() && engine.getUseClientMode()) {
             // Begin the initial handshake.
@@ -1794,11 +1802,13 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
      * goodput by aggregating the plaintext in chunks of {@link #wrapDataSize}. If many small chunks are written
      * this can increase goodput, decrease the amount of calls to SSL_write, and decrease overall encryption operations.
      */
-    private static final class SslHandlerCoalescingBufferQueue extends AbstractCoalescingBufferQueue {
-        volatile int wrapDataSize = MAX_PLAINTEXT_LENGTH;
+    static final class SslHandlerCoalescingBufferQueue extends AbstractCoalescingBufferQueue {
+        volatile int wrapDataSize;
+        private final PendingWritesTracker tracker;
 
-        SslHandlerCoalescingBufferQueue(int initSize) {
+        SslHandlerCoalescingBufferQueue(int initSize, ChannelHandlerContext ctx) {
             super(initSize);
+            this.tracker = PendingWritesTrackerFactory.newPendingWritesTracker(ctx);
         }
 
         @Override
@@ -1853,6 +1863,16 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
                 return true;
             }
             return false;
+        }
+
+        @Override
+        protected void incrementPendingOutboundBytes(long bytes) {
+            this.tracker.incrementPendingOutboundBytes(bytes);
+        }
+
+        @Override
+        protected void decrementPendingOutboundBytes(long bytes) {
+            this.tracker.decrementPendingOutboundBytes(bytes);
         }
     }
 

transport/src/main/java/io/netty/channel/AbstractCoalescingBufferQueue.java
@@ -41,6 +41,7 @@ public abstract class AbstractCoalescingBufferQueue {
      */
     public final void addFirst(ByteBuf buf) {
         incrementReadableBytes(buf.readableBytes());
+        incrementPendingOutboundBytes(buf.readableBytes());
         // Listener would be added here, but since it is null there is no need. The assumption is there is already a
         // listener at the front of the queue, or there is a buffer at the front of the queue, which was spliced from
         // buf via remove().
@@ -76,6 +77,7 @@ public abstract class AbstractCoalescingBufferQueue {
         // buffers are added before promises so that we naturally 'consume' the entire buffer during removal
         // before we complete it's promise.
         incrementReadableBytes(buf.readableBytes());
+        incrementPendingOutboundBytes(buf.readableBytes());
         bufAndListenerPairs.add(buf);
         if (listener != null) {
             bufAndListenerPairs.add(listener);
@@ -97,6 +99,8 @@ public abstract class AbstractCoalescingBufferQueue {
 
         readableBytes -= result.readableBytes();
         assert readableBytes >= 0;
+
+        decrementPendingOutboundBytes(result.readableBytes());
 
         entry = bufAndListenerPairs.peek();
         if (entry instanceof ChannelFutureListener) {
@@ -156,6 +160,9 @@ public abstract class AbstractCoalescingBufferQueue {
         }
         readableBytes -= originalBytes - bytes;
         assert readableBytes >= 0;
+
+        decrementPendingOutboundBytes(originalBytes - bytes);
+
         return toReturn;
     }
 
@@ -195,6 +202,9 @@ public abstract class AbstractCoalescingBufferQueue {
      */
     public final void writeAndRemoveAll(ChannelHandlerContext ctx) {
         readableBytes = 0;
+
+        decrementPendingOutboundBytes(readableBytes);
+
         Throwable pending = null;
         ByteBuf previousBuf = null;
         for (;;) {
@@ -261,6 +271,9 @@ public abstract class AbstractCoalescingBufferQueue {
 
     private void releaseAndCompleteAll(ChannelFuture future) {
         readableBytes = 0;
+
+        decrementPendingOutboundBytes(readableBytes);
+
         Throwable pending = null;
         for (;;) {
             Object entry = bufAndListenerPairs.poll();
@@ -292,5 +305,13 @@ public abstract class AbstractCoalescingBufferQueue {
             throw new IllegalStateException("buffer queue length overflow: " + readableBytes + " + " + increment);
         }
         readableBytes = nextReadableBytes;
+    }
+
+    protected void incrementPendingOutboundBytes(long bytes) {
+        // no-op
+    }
+
+    protected void decrementPendingOutboundBytes(long bytes) {
+        // no-op
     }
 }

transport/src/main/java/io/netty/channel/PendingWriteQueue.java
@@ -15,6 +15,7 @@
  */
 package io.netty.channel;
 
+import io.netty.channel.PendingWritesTrackerFactory.PendingWritesTracker;
 import io.netty.util.Recycler;
 import io.netty.util.ReferenceCountUtil;
 import io.netty.util.concurrent.PromiseCombiner;
@@ -38,7 +39,7 @@ public final class PendingWriteQueue {
             SystemPropertyUtil.getInt("io.netty.transport.pendingWriteSizeOverhead", 64);
 
     private final ChannelHandlerContext ctx;
-    private final PendingTracker tracker;
+    private final PendingWritesTracker tracker;
 
     // head and tail pointers for the linked-list structure. If empty head and tail are null.
     private PendingWrite head;
@@ -46,80 +47,9 @@ public final class PendingWriteQueue {
     private int size;
     private long bytes;
 
-    private interface PendingTracker extends MessageSizeEstimator.Handle {
-        void incrementPendingOutboundBytes(long bytes);
-        void decrementPendingOutboundBytes(long bytes);
-    }
-
     public PendingWriteQueue(ChannelHandlerContext ctx) {
         this.ctx = ObjectUtil.checkNotNull(ctx, "ctx");
-        if (ctx.pipeline() instanceof DefaultChannelPipeline) {
-            final DefaultChannelPipeline pipeline = (DefaultChannelPipeline) ctx.pipeline();
-            tracker = new PendingTracker() {
-                @Override
-                public void incrementPendingOutboundBytes(long bytes) {
-                    pipeline.incrementPendingOutboundBytes(bytes);
-                }
-
-                @Override
-                public void decrementPendingOutboundBytes(long bytes) {
-                    pipeline.decrementPendingOutboundBytes(bytes);
-                }
-
-                @Override
-                public int size(Object msg) {
-                    return pipeline.estimatorHandle().size(msg);
-                }
-            };
-        } else {
-            final MessageSizeEstimator.Handle estimator = ctx.channel().config().getMessageSizeEstimator().newHandle();
-            final ChannelOutboundBuffer buffer = ctx.channel().unsafe().outboundBuffer();
-            if (buffer == null) {
-                tracker = new PendingTracker() {
-                    @Override
-                    public void incrementPendingOutboundBytes(long bytes) {
-                        // noop
-                    }
-
-                    @Override
-                    public void decrementPendingOutboundBytes(long bytes) {
-                        // noop
-                    }
-
-                    @Override
-                    public int size(Object msg) {
-                        return estimator.size(msg);
-                    }
-                };
-            } else {
-                tracker = new PendingTracker() {
-                    @Override
-                    public void incrementPendingOutboundBytes(long bytes) {
-                        // We need to guard against null as channel.unsafe().outboundBuffer() may returned null
-                        // if the channel was already closed when constructing the PendingWriteQueue.
-                        // See https://github.com/netty/netty/issues/3967
-                        if (buffer != null) {
-                            buffer.incrementPendingOutboundBytes(bytes);
-                        }
-                    }
-
-                    @Override
-                    public void decrementPendingOutboundBytes(long bytes) {
-                        // We need to guard against null as channel.unsafe().outboundBuffer() may returned null
-                        // if the channel was already closed when constructing the PendingWriteQueue.
-                        // See https://github.com/netty/netty/issues/3967
-                        if (buffer != null) {
-                            buffer.decrementPendingOutboundBytes(bytes);
-                        }
-                    }
-
-                    @Override
-                    public int size(Object msg) {
-                        return estimator.size(msg);
-                    }
-                };
-            }
-        }
+        this.tracker = PendingWritesTrackerFactory.newPendingWritesTracker(ctx);
     }
 
     /**

transport/src/main/java/io/netty/channel/PendingWritesTrackerFactory.java
@@ -1,0 +1,97 @@
+/*
+ * Copyright 2017 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License, version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.netty.channel;
+
+public final class PendingWritesTrackerFactory {
+
+    private PendingWritesTrackerFactory() {
+    }
+
+    public static PendingWritesTracker newPendingWritesTracker(ChannelHandlerContext ctx) {
+        if (ctx.pipeline() instanceof DefaultChannelPipeline) {
+            final DefaultChannelPipeline pipeline = (DefaultChannelPipeline) ctx.pipeline();
+            return new PendingWritesTracker() {
+                @Override
+                public void incrementPendingOutboundBytes(long bytes) {
+                    pipeline.incrementPendingOutboundBytes(bytes);
+                }
+
+                @Override
+                public void decrementPendingOutboundBytes(long bytes) {
+                    pipeline.decrementPendingOutboundBytes(bytes);
+                }
+
+                @Override
+                public int size(Object msg) {
+                    return pipeline.estimatorHandle().size(msg);
+                }
+            };
+        } else {
+            final MessageSizeEstimator.Handle estimator = ctx.channel().config().getMessageSizeEstimator().newHandle();
+            final ChannelOutboundBuffer buffer = ctx.channel().unsafe().outboundBuffer();
+            if (buffer == null) {
+                return new PendingWritesTracker() {
+                    @Override
+                    public void incrementPendingOutboundBytes(long bytes) {
+                        // noop
+                    }
+
+                    @Override
+                    public void decrementPendingOutboundBytes(long bytes) {
+                        // noop
+                    }
+
+                    @Override
+                    public int size(Object msg) {
+                        return estimator.size(msg);
+                    }
+                };
+            } else {
+                return new PendingWritesTracker() {
+                    @Override
+                    public void incrementPendingOutboundBytes(long bytes) {
+                        // We need to guard against null as channel.unsafe().outboundBuffer() may returned null
+                        // if the channel was already closed when constructing the PendingWriteQueue.
+                        // See https://github.com/netty/netty/issues/3967
+                        if (buffer != null) {
+                            buffer.incrementPendingOutboundBytes(bytes);
+                        }
+                    }
+
+                    @Override
+                    public void decrementPendingOutboundBytes(long bytes) {
+                        // We need to guard against null as channel.unsafe().outboundBuffer() may returned null
+                        // if the channel was already closed when constructing the PendingWriteQueue.
+                        // See https://github.com/netty/netty/issues/3967
+                        if (buffer != null) {
+                            buffer.decrementPendingOutboundBytes(bytes);
+                        }
+                    }
+
+                    @Override
+                    public int size(Object msg) {
+                        return estimator.size(msg);
+                    }
+                };
+            }
+        }
+    }
+
+    public interface PendingWritesTracker extends MessageSizeEstimator.Handle {
+        void incrementPendingOutboundBytes(long bytes);
+        void decrementPendingOutboundBytes(long bytes);
+    }
+
+}