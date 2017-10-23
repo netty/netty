f795eb808035ec271da630842b801439d645e174
Nikolay Fedorovskikh <fenik17@gmail.com>
2017-10-24 00:53:31 +0500

    Fixes a LoggingHandler#format method with two arguments
    Motivation:
    Bug in capacity calculation: occurs auto convert to string instead of sum up.
    
    Modifications:
    Use `eventName.length()` in sum.
    
    Result:
    Less trash in logs.

--- handler/src/main/java/io/netty/handler/logging/LoggingHandler.java
+++ handler/src/main/java/io/netty/handler/logging/LoggingHandler.java
@@ -312,7 +312,7 @@ public class LoggingHandler extends ChannelDuplexHandler {
         String arg1Str = String.valueOf(firstArg);
         String arg2Str = secondArg.toString();
         StringBuilder buf = new StringBuilder(
-                chStr.length() + 1 + eventName + 2 + arg1Str.length() + 2 + arg2Str.length());
+                chStr.length() + 1 + eventName.length() + 2 + arg1Str.length() + 2 + arg2Str.length());
         buf.append(chStr).append(' ').append(eventName).append(": ").append(arg1Str).append(", ").append(arg2Str);
         return buf.toString();
     }
