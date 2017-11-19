netty/netty

commit 4732fabb1664acae0a545b6496066acb8d39ecb7

HttpObjectEncoder buffer size estimation
Motivation:
HttpObjectEncoder allocates a new buffer when encoding the initial line and headers, and also allocates a buffer when encoding the trailers. The allocation always uses the default size of 256. This may lead to consistent under allocation and require a few resize/copy operations which can cause GC/memory pressure.

Modifications:
- Introduce a weighted average which tracks the historical size of encoded data and uses this as an estimate for future buffer allocations

Result:
Better approximation of buffer sizes.

codec-http/src/main/java/io/netty/handler/codec/http/HttpObjectEncoder.java
@@ -47,6 +47,10 @@ public abstract class HttpObjectEncoder<H extends HttpMessage> extends MessageTo
     private static final ByteBuf CRLF_BUF = unreleasableBuffer(directBuffer(CRLF.length).writeBytes(CRLF));
     private static final ByteBuf ZERO_CRLF_CRLF_BUF = unreleasableBuffer(directBuffer(ZERO_CRLF_CRLF.length)
             .writeBytes(ZERO_CRLF_CRLF));
+    private static final float HEADERS_WEIGHT_NEW = 1 / 5f;
+    private static final float HEADERS_WEIGHT_HISTORICAL = 1 - HEADERS_WEIGHT_NEW;
+    private static final float TRAILERS_WEIGHT_NEW = HEADERS_WEIGHT_NEW;
+    private static final float TRAILERS_WEIGHT_HISTORICAL = HEADERS_WEIGHT_HISTORICAL;
 
     private static final int ST_INIT = 0;
     private static final int ST_CONTENT_NON_CHUNK = 1;
@@ -55,6 +59,18 @@ public abstract class HttpObjectEncoder<H extends HttpMessage> extends MessageTo
 
     @SuppressWarnings("RedundantFieldInitialization")
     private int state = ST_INIT;
+
+    /**
+     * Used to calculate an exponential moving average of the encoded size of the initial line and the headers for
+     * a guess for future buffer allocations.
+     */
+    private float headersEncodedSizeAccumulator = 256;
+
+    /**
+     * Used to calculate an exponential moving average of the encoded size of the trailers for
+     * a guess for future buffer allocations.
+     */
+    private float trailersEncodedSizeAccumulator = 256;
 
     @Override
     protected void encode(ChannelHandlerContext ctx, Object msg, List<Object> out) throws Exception {
@@ -67,10 +83,12 @@ public abstract class HttpObjectEncoder<H extends HttpMessage> extends MessageTo
             @SuppressWarnings({ "unchecked", "CastConflictsWithInstanceof" })
             H m = (H) msg;
 
-            buf = ctx.alloc().buffer();
+            buf = ctx.alloc().buffer((int) headersEncodedSizeAccumulator);
             // Encode the message.
             encodeInitialLine(buf, m);
             encodeHeaders(m.headers(), buf);
+            headersEncodedSizeAccumulator = HEADERS_WEIGHT_NEW * padSizeForAccumulation(buf.readableBytes()) +
+                                            HEADERS_WEIGHT_HISTORICAL * headersEncodedSizeAccumulator;
             buf.writeBytes(CRLF);
             state = isContentAlwaysEmpty(m) ? ST_CONTENT_ALWAYS_EMPTY :
                     HttpHeaders.isTransferEncodingChunked(m) ? ST_CONTENT_CHUNK : ST_CONTENT_NON_CHUNK;
@@ -167,11 +185,12 @@ public abstract class HttpObjectEncoder<H extends HttpMessage> extends MessageTo
             if (headers.isEmpty()) {
                 out.add(ZERO_CRLF_CRLF_BUF.duplicate());
             } else {
-                ByteBuf buf = ctx.alloc().buffer();
+                ByteBuf buf = ctx.alloc().buffer((int) trailersEncodedSizeAccumulator);
                 buf.writeBytes(ZERO_CRLF);
                 encodeHeaders(headers, buf);
                 buf.writeBytes(CRLF);
-                out.add(buf);
+                trailersEncodedSizeAccumulator = TRAILERS_WEIGHT_NEW * padSizeForAccumulation(buf.readableBytes()) +
+                                                 TRAILERS_WEIGHT_HISTORICAL * trailersEncodedSizeAccumulator;
             }
         } else if (contentLength == 0) {
             // Need to produce some output otherwise an
@@ -213,6 +232,16 @@ public abstract class HttpObjectEncoder<H extends HttpMessage> extends MessageTo
             return ((FileRegion) msg).count();
         }
         throw new IllegalStateException("unexpected message type: " + StringUtil.simpleClassName(msg));
+    }
+
+    /**
+     * Add some additional overhead to the buffer. The rational is that it is better to slightly over allocate and waste
+     * some memory, rather than under allocate and require a resize/copy.
+     * @param readableBytes The readable bytes in the buffer.
+     * @return The {@code readableBytes} with some additional padding.
+     */
+    private static int padSizeForAccumulation(int readableBytes) {
+        return (readableBytes << 2) / 3;
     }
 
     @Deprecated

commit a8951ec6045498027b689becdec01f901fe5532d

Fix writing of empty LastHttpContent with trailers
Motivation:

4732fabb1664acae0a545b6496066acb8d39ecb7 introduced a regression in HttpObjectEncoder which will lead to buffer leak and IllegalStateException when a LastHttpContent with trailers is written.

Modifications:

- Correctly add the buffer to the encoded list.
- Add testcases

Result:

Fixes [#7418]

codec-http/src/main/java/io/netty/handler/codec/http/HttpObjectEncoder.java
@@ -206,6 +206,7 @@ public abstract class HttpObjectEncoder<H extends HttpMessage> extends MessageTo
                 buf.writeBytes(CRLF);
                 trailersEncodedSizeAccumulator = TRAILERS_WEIGHT_NEW * padSizeForAccumulation(buf.readableBytes()) +
                                                  TRAILERS_WEIGHT_HISTORICAL * trailersEncodedSizeAccumulator;
+                out.add(buf);
             }
         } else if (contentLength == 0) {
             // Need to produce some output otherwise an

codec-http/src/test/java/io/netty/handler/codec/http/HttpRequestEncoderTest.java
@@ -135,16 +135,26 @@ public class HttpRequestEncoderTest {
     }
 
     @Test
-    public void testEmptyContentChunked() throws Exception {
-        testEmptyContent(true);
+    public void testEmptyContentsChunked() throws Exception {
+        testEmptyContents(true, false);
     }
 
     @Test
-    public void testEmptyContentNotChunked() throws Exception {
-        testEmptyContent(false);
+    public void testEmptyContentsChunkedWithTrailers() throws Exception {
+        testEmptyContents(true, true);
     }
 
-    private void testEmptyContent(boolean chunked) throws Exception {
+    @Test
+    public void testEmptyContentsNotChunked() throws Exception {
+        testEmptyContents(false, false);
+    }
+
+    @Test
+    public void testEmptyContentNotsChunkedWithTrailers() throws Exception {
+        testEmptyContents(false, true);
+    }
+
+    private void testEmptyContents(boolean chunked, boolean trailers) throws Exception {
         HttpRequestEncoder encoder = new HttpRequestEncoder();
         EmbeddedChannel channel = new EmbeddedChannel(encoder);
         HttpRequest request = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, "/");
@@ -157,7 +167,11 @@ public class HttpRequestEncoderTest {
         assertTrue(channel.writeOutbound(new DefaultHttpContent(contentBuffer)));
 
         ByteBuf lastContentBuffer = Unpooled.buffer();
-        assertTrue(channel.writeOutbound(new DefaultHttpContent(lastContentBuffer)));
+        LastHttpContent last = new DefaultLastHttpContent(lastContentBuffer);
+        if (trailers) {
+            last.trailingHeaders().set("X-Netty-Test", "true");
+        }
+        assertTrue(channel.writeOutbound(last));
 
         // Ensure we only produce ByteBuf instances.
         ByteBuf head = (ByteBuf) channel.readOutbound();

codec-http/src/test/java/io/netty/handler/codec/http/HttpResponseEncoderTest.java
@@ -286,4 +286,55 @@ public class HttpResponseEncoderTest {
         buffer = (ByteBuf) channel.readOutbound();
         buffer.release();
     }
+
+    @Test
+    public void testEmptyContentsChunked() throws Exception {
+        testEmptyContents(true, false);
+    }
+
+    @Test
+    public void testEmptyContentsChunkedWithTrailers() throws Exception {
+        testEmptyContents(true, true);
+    }
+
+    @Test
+    public void testEmptyContentsNotChunked() throws Exception {
+        testEmptyContents(false, false);
+    }
+
+    @Test
+    public void testEmptyContentNotsChunkedWithTrailers() throws Exception {
+        testEmptyContents(false, true);
+    }
+
+    private void testEmptyContents(boolean chunked, boolean trailers) throws Exception {
+        HttpResponseEncoder encoder = new HttpResponseEncoder();
+        EmbeddedChannel channel = new EmbeddedChannel(encoder);
+        HttpResponse request = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+        if (chunked) {
+            HttpHeaders.setTransferEncodingChunked(request);
+        }
+        assertTrue(channel.writeOutbound(request));
+
+        ByteBuf contentBuffer = Unpooled.buffer();
+        assertTrue(channel.writeOutbound(new DefaultHttpContent(contentBuffer)));
+
+        ByteBuf lastContentBuffer = Unpooled.buffer();
+        LastHttpContent last = new DefaultLastHttpContent(lastContentBuffer);
+        if (trailers) {
+            last.trailingHeaders().set("X-Netty-Test", "true");
+        }
+        assertTrue(channel.writeOutbound(last));
+
+        // Ensure we only produce ByteBuf instances.
+        ByteBuf head = (ByteBuf) channel.readOutbound();
+        assertTrue(head.release());
+
+        ByteBuf content = (ByteBuf) channel.readOutbound();
+        content.release();
+
+        ByteBuf lastContent = (ByteBuf) channel.readOutbound();
+        lastContent.release();
+        assertFalse(channel.finish());
+    }
 }