aabb73a9d2caaafd0d989945ea176609803a17d9
Norman Maurer <norman_maurer@apple.com>
2017-12-01 11:26:01 +0100

    Add SniCompletionEvent which allows to easily retrieve the hostname that was used to select the SslContext.
    Motivation:
    
    At the moment its a bit "hacky" to retrieve the hostname that was used during SNI as you need to hold a reference to SniHandler and then call hostname() once the selection is done. It would be better to fire an event to let the user know we did the selection.
    
    Modifications:
    
    Add a SniCompletionEvent that can be used to get the hostname that was used to do the selection and was included in the SNI extension.
    
    Result:
    
    Easier usage of SNI.

---
+++ handler/src/main/java/io/netty/handler/ssl/SniCompletionEvent.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2017 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.ssl;
+
+import io.netty.util.internal.UnstableApi;
+
+/**
+ * Event that is fired once we did a selection of a {@link SslContext} based on the {@code SNI hostname},
+ * which may be because it was successful or there was an error.
+ */
+@UnstableApi
+public final class SniCompletionEvent extends SslCompletionEvent {
+    private final String hostname;
+
+    SniCompletionEvent(String hostname) {
+        this.hostname = hostname;
+    }
+
+    SniCompletionEvent(String hostname, Throwable cause) {
+        super(cause);
+        this.hostname = hostname;
+    }
+
+    SniCompletionEvent(Throwable cause) {
+        this(null, cause);
+    }
+
+    /**
+     * Returns the SNI hostname send by the client if we were able to parse it, {@code null} otherwise.
+     */
+    public String hostname() {
+        return hostname;
+    }
+
+    @Override
+    public String toString() {
+        final Throwable cause = cause();
+        return cause == null ? getClass().getSimpleName() + "(SUCCESS='"  + hostname + "'\")":
+                getClass().getSimpleName() +  '(' + cause + ')';
+    }
+}
