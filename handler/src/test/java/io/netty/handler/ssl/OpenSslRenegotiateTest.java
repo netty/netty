ccecc20124abf83540d5d0258c0fba988eefb798
Scott Mitchell <scott_mitchell@apple.com>
2018-01-02 10:10:13 -0800

    Remove remote initiated renegotiation support
    Motivation:
    We recently removed support for renegotiation, but there are still some hooks to attempt to allow remote initiated renegotiation to succeed. The remote initated renegotiation can be even more problematic from a security stand point and should also be removed.
    
    Modifications:
    - Remove state related to remote iniated renegotiation from OpenSslEngine
    
    Result:
    More renegotiation code removed from the OpenSslEngine code path.

---
+++ handler/src/test/java/io/netty/handler/ssl/OpenSslRenegotiateTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2017 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.ssl;
+
+import org.junit.BeforeClass;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.net.ssl.SSLException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assume.assumeTrue;
+
+public class OpenSslRenegotiateTest extends RenegotiateTest {
+
+    @BeforeClass
+    public static void checkOpenSsl() {
+        assumeTrue(OpenSsl.isAvailable());
+    }
+
+    @Override
+    protected SslProvider serverSslProvider() {
+        return SslProvider.OPENSSL;
+    }
+
+    protected void verifyResult(AtomicReference<Throwable> error) throws Throwable {
+        Throwable cause = error.get();
+        // Renegotation is not supported by the OpenSslEngine.
+        assertThat(cause, is(instanceOf(SSLException.class)));
+    }
+}
