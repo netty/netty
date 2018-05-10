902fd0090ced161c5bc81555358db82afe53ad1a
Norman Maurer <norman_maurer@apple.com>
2018-05-10 08:36:17 +0200

    Make build pass on Java 10 (#7922)
    Motivation:
    
    Java 10 is out so we should be able to build netty with it (and run the tests).
    
    Modifications:
    
    - Update Mockito and JBoss Marshalling to support Java 10
    - Fix unit test to not depend on specific cipher which is not present in Java 10 anymore
    
    Result:
    
    Netty builds (and runs all tests) when using Java 10

--- testsuite/src/main/java/io/netty/testsuite/transport/socket/SocketSslClientRenegotiateTest.java
+++ testsuite/src/main/java/io/netty/testsuite/transport/socket/SocketSslClientRenegotiateTest.java
@@ -162,7 +162,7 @@ public class SocketSslClientRenegotiateTest extends AbstractSocketTest {
         Future<Channel> clientHandshakeFuture = clientSslHandler.handshakeFuture();
         clientHandshakeFuture.sync();
 
-        String renegotiation = "SSL_RSA_WITH_3DES_EDE_CBC_SHA";
+        String renegotiation = clientSslHandler.engine().getSupportedCipherSuites()[0];
         clientSslHandler.engine().setEnabledCipherSuites(new String[] { renegotiation });
         clientSslHandler.renegotiate().await();
         serverChannel.close().awaitUninterruptibly();
