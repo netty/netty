4233ab20e186202c67cee8ee75585bdd369112af
Norman Maurer <norman_maurer@apple.com>
2018-09-07 07:34:22 +0200

    Log the correct line-number when using SLF4j with netty if possible. (#8258)
    * Log the correct line-number when using SLF4j with netty if possible.
    
    Motivation:
    
    At the moment we do not log the correct line number in many cases as it will log the line number of the logger wrapper itself. Slf4j does have an extra interface that can be used to filter out these nad make it more usable with logging wrappers.
    
    Modifications:
    
    Detect if the returned logger implements LocationAwareLogger and if so make use of its extra methods to be able to log the correct origin of the log request.
    
    Result:
    
    Better logging when using slf4j.

--- common/src/main/java/io/netty/util/internal/logging/Slf4JLoggerFactory.java
+++ common/src/main/java/io/netty/util/internal/logging/Slf4JLoggerFactory.java
@@ -16,8 +16,10 @@
 package io.netty.util.internal.logging;
 
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.helpers.NOPLoggerFactory;
+import org.slf4j.spi.LocationAwareLogger;
 
 /**
  * Logger factory which creates a <a href="http://www.slf4j.org/">SLF4J</a>
@@ -44,6 +46,12 @@ public class Slf4JLoggerFactory extends InternalLoggerFactory {
 
     @Override
     public InternalLogger newInstance(String name) {
-        return new Slf4JLogger(LoggerFactory.getLogger(name));
+        return wrapLogger(LoggerFactory.getLogger(name));
+    }
+
+    // package-private for testing.
+    static InternalLogger wrapLogger(Logger logger) {
+        return logger instanceof LocationAwareLogger ?
+                new LocationAwareSlf4JLogger((LocationAwareLogger) logger) : new Slf4JLogger(logger);
     }
 }
