5a441df4b335c0ee63c816142d743812c06cadad
Norman Maurer <norman_maurer@apple.com>
2018-09-14 14:33:11 +0200

    Return an ExtendSSLSession whenever possible to allow more strict checking when using OpenSSL (#8281)
    Motivation:
    
    When an ExtendedSSLSession is used its possible to do more strict checking of the keys during handshake. We should do this whenever possible.
    
    Modification:
    
    - Return an ExtendedSSLSession when using client-mode and Java7+
    - Add unit test
    - Simplify unit tests
    
    Result:
    
    More consistent behaviour.

--- handler/src/test/java/io/netty/handler/ssl/SniClientJava8TestUtil.java
+++ handler/src/test/java/io/netty/handler/ssl/SniClientJava8TestUtil.java
@@ -29,14 +29,42 @@ import io.netty.channel.local.LocalChannel;
 import io.netty.channel.local.LocalServerChannel;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
+import io.netty.handler.ssl.util.SimpleTrustManagerFactory;
 import io.netty.util.concurrent.Promise;
+import io.netty.util.internal.EmptyArrays;
 import io.netty.util.internal.ThrowableUtil;
+import org.junit.Assert;
 
+import javax.net.ssl.ExtendedSSLSession;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.KeyManagerFactorySpi;
+import javax.net.ssl.ManagerFactoryParameters;
+import javax.net.ssl.SNIHostName;
 import javax.net.ssl.SNIMatcher;
 import javax.net.ssl.SNIServerName;
+import javax.net.ssl.SSLEngine;
 import javax.net.ssl.SSLException;
 import javax.net.ssl.SSLParameters;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509ExtendedKeyManager;
+import javax.net.ssl.X509ExtendedTrustManager;
+import java.io.IOException;
+import java.net.Socket;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.Principal;
+import java.security.PrivateKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * In extra class to be able to run tests with java7 without trying to load classes that not exists in java7.
@@ -123,6 +151,178 @@ final class SniClientJava8TestUtil {
                 sc.close().syncUninterruptibly();
             }
             group.shutdownGracefully();
+        }
+    }
+
+    static void assertSSLSession(SSLSession session, String name) {
+        assertSSLSession(session, new SNIHostName(name));
+    }
+
+    private static void assertSSLSession(SSLSession session, SNIServerName name) {
+        Assert.assertNotNull(session);
+        if (session instanceof ExtendedSSLSession) {
+            ExtendedSSLSession extendedSSLSession = (ExtendedSSLSession) session;
+            List<SNIServerName> names = extendedSSLSession.getRequestedServerNames();
+            Assert.assertEquals(1, names.size());
+            Assert.assertEquals(name, names.get(0));
+            Assert.assertTrue(extendedSSLSession.getLocalSupportedSignatureAlgorithms().length > 0);
+        }
+    }
+
+    static TrustManagerFactory newSniX509TrustmanagerFactory(String name) {
+        return new SniX509TrustmanagerFactory(new SNIHostName(name));
+    }
+
+    private static final class SniX509TrustmanagerFactory extends SimpleTrustManagerFactory {
+
+        private final SNIServerName name;
+
+        SniX509TrustmanagerFactory(SNIServerName name) {
+            this.name = name;
+        }
+
+        @Override
+        protected void engineInit(KeyStore keyStore) throws Exception {
+            // NOOP
+        }
+
+        @Override
+        protected void engineInit(ManagerFactoryParameters managerFactoryParameters) throws Exception {
+            // NOOP
+        }
+
+        @Override
+        protected TrustManager[] engineGetTrustManagers() {
+            return new TrustManager[] { new X509ExtendedTrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] x509Certificates, String s, Socket socket)
+                        throws CertificateException {
+                    Assert.fail();
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] x509Certificates, String s, Socket socket)
+                        throws CertificateException {
+                    Assert.fail();
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] x509Certificates, String s, SSLEngine sslEngine)
+                        throws CertificateException {
+                    Assert.fail();
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] x509Certificates, String s, SSLEngine sslEngine)
+                        throws CertificateException {
+                    assertSSLSession(sslEngine.getHandshakeSession(), name);
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] x509Certificates, String s)
+                        throws CertificateException {
+                    Assert.fail();
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] x509Certificates, String s)
+                        throws CertificateException {
+                    Assert.fail();
+                }
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return EmptyArrays.EMPTY_X509_CERTIFICATES;
+                }
+            } };
+        }
+    }
+
+    static KeyManagerFactory newSniX509KeyManagerFactory(SelfSignedCertificate cert, String hostname)
+            throws NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException,
+                   IOException, CertificateException {
+        return new SniX509KeyManagerFactory(
+                new SNIHostName(hostname), SslContext.buildKeyManagerFactory(
+                new X509Certificate[] { cert.cert() }, cert.key(), null, null));
+    }
+
+    private static final class SniX509KeyManagerFactory extends KeyManagerFactory {
+
+        SniX509KeyManagerFactory(final SNIServerName name, final KeyManagerFactory factory) {
+            super(new KeyManagerFactorySpi() {
+                @Override
+                protected void engineInit(KeyStore keyStore, char[] chars)
+                        throws KeyStoreException, NoSuchAlgorithmException, UnrecoverableKeyException {
+                    factory.init(keyStore, chars);
+                }
+
+                @Override
+                protected void engineInit(ManagerFactoryParameters managerFactoryParameters)
+                        throws InvalidAlgorithmParameterException {
+                    factory.init(managerFactoryParameters);
+                }
+
+                @Override
+                protected KeyManager[] engineGetKeyManagers() {
+                    List<KeyManager> managers = new ArrayList<KeyManager>();
+                    for (final KeyManager km: factory.getKeyManagers()) {
+                        if (km instanceof X509ExtendedKeyManager) {
+                            managers.add(new X509ExtendedKeyManager() {
+                                @Override
+                                public String[] getClientAliases(String s, Principal[] principals) {
+                                    return ((X509ExtendedKeyManager) km).getClientAliases(s, principals);
+                                }
+
+                                @Override
+                                public String chooseClientAlias(String[] strings, Principal[] principals,
+                                                                Socket socket) {
+                                    return ((X509ExtendedKeyManager) km).chooseClientAlias(strings, principals, socket);
+                                }
+
+                                @Override
+                                public String[] getServerAliases(String s, Principal[] principals) {
+                                    return ((X509ExtendedKeyManager) km).getServerAliases(s, principals);
+                                }
+
+                                @Override
+                                public String chooseServerAlias(String s, Principal[] principals, Socket socket) {
+                                    return ((X509ExtendedKeyManager) km).chooseServerAlias(s, principals, socket);
+                                }
+
+                                @Override
+                                public X509Certificate[] getCertificateChain(String s) {
+                                    return ((X509ExtendedKeyManager) km).getCertificateChain(s);
+                                }
+
+                                @Override
+                                public PrivateKey getPrivateKey(String s) {
+                                    return ((X509ExtendedKeyManager) km).getPrivateKey(s);
+                                }
+
+                                @Override
+                                public String chooseEngineClientAlias(String[] strings, Principal[] principals,
+                                                                      SSLEngine sslEngine) {
+                                    return ((X509ExtendedKeyManager) km)
+                                            .chooseEngineClientAlias(strings, principals, sslEngine);
+                                }
+
+                                @Override
+                                public String chooseEngineServerAlias(String s, Principal[] principals,
+                                                                      SSLEngine sslEngine) {
+
+                                    SSLSession session = sslEngine.getHandshakeSession();
+                                    assertSSLSession(session, name);
+                                    return ((X509ExtendedKeyManager) km)
+                                            .chooseEngineServerAlias(s, principals, sslEngine);
+                                }
+                            });
+                        } else {
+                            managers.add(km);
+                        }
+                    }
+                    return managers.toArray(new KeyManager[0]);
+                }
+            }, factory.getProvider(), factory.getAlgorithm());
         }
     }
 }
