73acac13f48be32e04f936f3d09e202e7590f0a0
Norman Maurer <norman_maurer@apple.com>
2018-09-28 10:54:05 +0200

    Check if hostname validation is supported before trying to use in test. (#8333)
    Motivation:
    
    a208f6dc7c775ce8d7934f252cd6fa7527643f76 added a testcase which uses hostname validation which may not be supported by OpenSSL depending on the version that is used. We should check first before we try to use it.
    
    Modifications:
    
    Add assumeTrue(...) check to ensure hostname validation is supported before trying to run the test.
    
    Result:
    
    No more test-failures on OpenSSL versions < 1.0.2.

--- handler/src/test/java/io/netty/handler/ssl/SSLEngineTest.java
+++ handler/src/test/java/io/netty/handler/ssl/SSLEngineTest.java
@@ -46,6 +46,7 @@ import io.netty.util.internal.PlatformDependent;
 import io.netty.util.internal.StringUtil;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -2421,6 +2422,12 @@ public abstract class SSLEngineTest {
 
     @Test
     public void testUsingX509TrustManagerVerifiesHostname() throws Exception {
+        SslProvider clientProvider = sslClientProvider();
+        if (clientProvider == SslProvider.OPENSSL || clientProvider == SslProvider.OPENSSL_REFCNT) {
+            // Need to check if we support hostname validation in the current used OpenSSL version before running
+            // the test.
+            Assume.assumeTrue(OpenSsl.supportsHostnameValidation());
+        }
         SelfSignedCertificate cert = new SelfSignedCertificate();
         clientSslCtx = SslContextBuilder
                 .forClient()
