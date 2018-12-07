8f60a81ae99fa664e3ef748d7937f8b4b1c0a818
多巴胺 <1330456228zjp@gmail.com>
2018-12-07 13:43:48 +0800

    Resolve conversation

--- handler/src/test/java/io/netty/handler/ipfilter/UniqueIpFilterTest.java
+++ handler/src/test/java/io/netty/handler/ipfilter/UniqueIpFilterTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package io.netty.handler.ipfilter;
 
 import io.netty.channel.embedded.EmbeddedChannel;
@@ -14,33 +29,31 @@ public class UniqueIpFilterTest {
     public void testUniqueIpFilterHandler() throws ExecutionException, InterruptedException {
         final CyclicBarrier barrier = new CyclicBarrier(2);
         ExecutorService executorService = Executors.newFixedThreadPool(2);
-        for (int round = 0; round < 10000; round++) {
-            System.out.println("round = " + round);
-            final UniqueIpFilter handler = new UniqueIpFilter();
-            java.util.concurrent.Future<EmbeddedChannel> future1 = submit(handler, barrier, executorService);
-            java.util.concurrent.Future<EmbeddedChannel> future2 = submit(handler, barrier, executorService);
-            EmbeddedChannel channel1 = future1.get();
-            EmbeddedChannel channel2 = future2.get();
-            Assert.assertTrue(channel1.isActive() || channel2.isActive());
-            Assert.assertFalse(channel1.isActive() && channel2.isActive());
+        try {
+            for (int round = 0; round < 10000; round++) {
+                final UniqueIpFilter handler = new UniqueIpFilter();
+                java.util.concurrent.Future<EmbeddedChannel> future1 = submit(handler, barrier, executorService);
+                java.util.concurrent.Future<EmbeddedChannel> future2 = submit(handler, barrier, executorService);
+                EmbeddedChannel channel1 = future1.get();
+                EmbeddedChannel channel2 = future2.get();
+                Assert.assertTrue(channel1.isActive() || channel2.isActive());
+                Assert.assertFalse(channel1.isActive() && channel2.isActive());
 
-            barrier.reset();
-            channel1.close().await();
-            channel2.close().await();
+                barrier.reset();
+                channel1.close().await();
+                channel2.close().await();
+            }
+        } finally {
+            executorService.shutdown();
         }
+
     }
 
     private java.util.concurrent.Future<EmbeddedChannel> submit(final UniqueIpFilter handler, final CyclicBarrier barrier, ExecutorService executorService) {
         return executorService.submit(new Callable<EmbeddedChannel>() {
             @Override
             public EmbeddedChannel call() throws Exception {
-                try {
-                    barrier.await();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (BrokenBarrierException e) {
-                    e.printStackTrace();
-                }
+                barrier.await();
                 return new EmbeddedChannel(handler) {
                     @Override
                     protected SocketAddress remoteAddress0() {
