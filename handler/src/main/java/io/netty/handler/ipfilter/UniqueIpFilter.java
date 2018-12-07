0288570b06eeb995719ddbbec0250a4ea1edca6f
多巴胺 <1330456228zjp@gmail.com>
2018-12-07 20:50:00 +0800

    Fix concurrency problem in UniqueIpFilter (#8635)
    Motivation:
    
    If two requests from the same IP are reached at the same time, `connected.contains(remoteIp)` may return false in both threads.
    
    Modifications:
    
    Check if there is already a connection with the same IP using return values.
    
    Result:
    
    Become thread safe.

--- handler/src/main/java/io/netty/handler/ipfilter/UniqueIpFilter.java
+++ handler/src/main/java/io/netty/handler/ipfilter/UniqueIpFilter.java
@@ -38,10 +38,9 @@ public class UniqueIpFilter extends AbstractRemoteAddressFilter<InetSocketAddres
     @Override
     protected boolean accept(ChannelHandlerContext ctx, InetSocketAddress remoteAddress) throws Exception {
         final InetAddress remoteIp = remoteAddress.getAddress();
-        if (connected.contains(remoteIp)) {
+        if (!connected.add(remoteIp)) {
             return false;
         } else {
-            connected.add(remoteIp);
             ctx.channel().closeFuture().addListener(new ChannelFutureListener() {
                 @Override
                 public void operationComplete(ChannelFuture future) throws Exception {
