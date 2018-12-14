29d185b7960863a8aec5347fc0d32016b3687403
Norman Maurer <norman_maurer@apple.com>
2018-12-14 18:24:30 +0100

    Revert "Support 1012, 1013 and 1014 WebSocket status code"
    This reverts commit db6d94f82a0f860131392aa483f4d1e21dc74249.

--- codec-http/src/test/java/io/netty/handler/codec/http/websocketx/WebSocket08FrameDecoderTest.java
+++ codec-http/src/test/java/io/netty/handler/codec/http/websocketx/WebSocket08FrameDecoderTest.java
@@ -12,19 +12,10 @@
  */
 package io.netty.handler.codec.http.websocketx;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.UnpooledByteBufAllocator;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 import org.mockito.Mockito;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class WebSocket08FrameDecoderTest {
 
@@ -34,51 +25,5 @@ public class WebSocket08FrameDecoderTest {
         final ChannelHandlerContext ctx = Mockito.mock(ChannelHandlerContext.class);
         decoder.channelInactive(ctx);
         Mockito.verify(ctx).fireChannelInactive();
-    }
-
-    @Test
-    public void supportIanaStatusCodes() throws Exception {
-        Set<Integer> forbiddenIanaCodes = new HashSet<Integer>();
-        forbiddenIanaCodes.add(1004);
-        forbiddenIanaCodes.add(1005);
-        forbiddenIanaCodes.add(1006);
-        Set<Integer> validIanaCodes = new HashSet<Integer>();
-        for (int i = 1000; i < 1015; i++) {
-            validIanaCodes.add(i);
-        }
-        validIanaCodes.removeAll(forbiddenIanaCodes);
-
-        ChannelHandlerContext ctx = Mockito.mock(ChannelHandlerContext.class);
-        Mockito.when(ctx.alloc()).thenReturn(UnpooledByteBufAllocator.DEFAULT);
-
-        Channel channel = Mockito.mock(Channel.class);
-        Mockito.when(channel.isActive()).thenReturn(false);
-        Mockito.when(ctx.channel()).thenReturn(channel);
-
-        List<Object> out = new ArrayList<Object>();
-
-        for (int statusCode: validIanaCodes) {
-            WebSocket08FrameEncoder encoder = new WebSocket08FrameEncoder(true);
-            WebSocket08FrameDecoder decoder = new WebSocket08FrameDecoder(true, true, 65535, false);
-
-            CloseWebSocketFrame inputFrame = new CloseWebSocketFrame(statusCode, "Bye");
-            try {
-                encoder.encode(ctx, inputFrame, out);
-                ByteBuf serializedCloseFrame = (ByteBuf) out.get(0);
-                out.clear();
-
-                decoder.decode(ctx, serializedCloseFrame, out);
-                CloseWebSocketFrame outputFrame = (CloseWebSocketFrame) out.get(0);
-                out.clear();
-
-                try {
-                    assertEquals(statusCode, outputFrame.statusCode());
-                } finally {
-                    outputFrame.release();
-                }
-            } finally {
-                inputFrame.release();
-            }
-        }
     }
 }
