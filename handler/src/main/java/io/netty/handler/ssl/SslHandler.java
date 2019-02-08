79e07c3581d3f8d9fdcbe7047538c53a930dedd7
Norman Maurer <norman_maurer@apple.com>
2019-02-08 12:11:24 +0100

    Comment

--- handler/src/main/java/io/netty/handler/ssl/SslHandler.java
+++ handler/src/main/java/io/netty/handler/ssl/SslHandler.java
@@ -1682,6 +1682,7 @@ public class SslHandler extends ByteToMessageDecoder implements ChannelOutboundH
                     }
                 }
             } catch (final Throwable cause) {
+                processTask = false;
                 handleException(cause);
             }
         }
