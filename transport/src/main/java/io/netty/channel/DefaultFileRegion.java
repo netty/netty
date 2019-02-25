dc54ad677a446271d9b3a90d741d75eabcc6c461
Norman Maurer <norman_maurer@apple.com>
2019-02-25 15:05:27 +0100

    DefaultFileRegion.transferTo with invalid count may cause busy-spin
    Motivation:
    
    `DefaultFileRegion.transferTo` will return 0 all the time when we request more data then the actual file size. This may result in a busy spin while processing the fileregion during writes.
    
    Modifications:
    
    - If we wrote 0 bytes check if the underlying file size is smaller then the requested count and if so throw an IOException
    - Add DefaultFileRegionTest
    - Add a test to the testsuite
    
    Result:
    
    Fixes https://github.com/netty/netty/issues/8868.

--- transport/src/main/java/io/netty/channel/DefaultFileRegion.java
+++ transport/src/main/java/io/netty/channel/DefaultFileRegion.java
@@ -139,6 +139,12 @@ public class DefaultFileRegion extends AbstractReferenceCounted implements FileR
         long written = file.transferTo(this.position + position, count, target);
         if (written > 0) {
             transferred += written;
+        } else if (written == 0) {
+            // If the amount of written data is 0 we need to check if the requested count is bigger then the
+            // actual file itself as it may have been truncated on disk.
+            //
+            // See https://github.com/netty/netty/issues/8868
+            validate(this, position);
         }
         return written;
     }
@@ -181,5 +187,17 @@ public class DefaultFileRegion extends AbstractReferenceCounted implements FileR
     @Override
     public FileRegion touch(Object hint) {
         return this;
+    }
+
+    static void validate(DefaultFileRegion region, long position) throws IOException {
+        // If the amount of written data is 0 we need to check if the requested count is bigger then the
+        // actual file itself as it may have been truncated on disk.
+        //
+        // See https://github.com/netty/netty/issues/8868
+        long size = region.file.size();
+        long count = region.count - position;
+        if (region.position + count + position > size) {
+            throw new IOException("Underlying file size " + size + " smaller then requested count " + region.count);
+        }
     }
 }
