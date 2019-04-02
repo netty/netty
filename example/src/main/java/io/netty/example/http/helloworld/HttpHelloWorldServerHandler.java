919644dcd072db9342c14c04f5a17b39a190f804
Oleksii Kachaiev <kachayev@gmail.com>
2019-04-02 12:10:11 -0700

    Carefully manage Keep-Alive/Close connection headers in all examples (#8966)
    Motivation:
    
    "Connection: close" header should be specified each time we're going
    to close an underlying TCP connection when sending HTTP/1.1 reply.
    
    Modifications:
    
    Introduces changes made in #8914 for the following examples:
    
    * WebSocket index page and WebSocket server handler
    * HelloWorld server
    * SPDY server handler
    * HTTP/1.1 server handler from HTTP/2 HelloWorld example
    * HTTP/1.1 server handler from tiles example
    
    Result:
    
    Keep-Alive connections management conforms with RFCs.

--- example/src/main/java/io/netty/example/http/helloworld/HttpHelloWorldServerHandler.java
+++ example/src/main/java/io/netty/example/http/helloworld/HttpHelloWorldServerHandler.java
@@ -16,26 +16,27 @@
 package io.netty.example.http.helloworld;
 
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpObject;
-import io.netty.handler.codec.http.HttpUtil;
 import io.netty.handler.codec.http.HttpRequest;
-import io.netty.util.AsciiString;
+import io.netty.handler.codec.http.HttpUtil;
+import io.netty.handler.codec.http.HttpVersion;
 
-import static io.netty.handler.codec.http.HttpResponseStatus.*;
-import static io.netty.handler.codec.http.HttpVersion.*;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
+import static io.netty.handler.codec.http.HttpHeaderValues.CLOSE;
+import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 public class HttpHelloWorldServerHandler extends SimpleChannelInboundHandler<HttpObject> {
     private static final byte[] CONTENT = { 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd' };
-
-    private static final AsciiString CONTENT_TYPE = AsciiString.cached("Content-Type");
-    private static final AsciiString CONTENT_LENGTH = AsciiString.cached("Content-Length");
-    private static final AsciiString CONNECTION = AsciiString.cached("Connection");
-    private static final AsciiString KEEP_ALIVE = AsciiString.cached("keep-alive");
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) {
@@ -52,11 +53,17 @@ public class HttpHelloWorldServerHandler extends SimpleChannelInboundHandler<Htt
             response.headers().set(CONTENT_TYPE, "text/plain");
             response.headers().setInt(CONTENT_LENGTH, response.content().readableBytes());
 
-            if (!keepAlive) {
-                ctx.write(response).addListener(ChannelFutureListener.CLOSE);
-            } else {
+            if (keepAlive && req.protocolVersion().equals(HttpVersion.HTTP_1_0)) {
                 response.headers().set(CONNECTION, KEEP_ALIVE);
-                ctx.write(response);
+            } else {
+                // Tell the client we're going to close the connection.
+                response.headers().set(CONNECTION, CLOSE);
+            }
+
+            ChannelFuture f = ctx.write(response);
+
+            if (!keepAlive) {
+                f.addListener(ChannelFutureListener.CLOSE);
             }
         }
     }
