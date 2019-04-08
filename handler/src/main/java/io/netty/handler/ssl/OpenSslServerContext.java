d9472dd470933f3b429e058b58385382877430a9
Norman Maurer <norman_maurer@apple.com>
2019-04-08 15:20:14 +0200

    We should fail fast if the given PrivateKey or X509Certificate chain is not supported by the used SslProvider. (#9009)
    Motivation:
    
    Some SslProvider do support different types of keys and chains. We should fail fast if we can not support the type.
    
    Related to https://github.com/netty/netty-tcnative/issues/455.
    
    Modifications:
    
    - Try to parse key / chain first and if if this fails throw and SslException
    - Add tests.
    
    Result:
    
    Fail fast.

--- handler/src/main/java/io/netty/handler/ssl/OpenSslServerContext.java
+++ handler/src/main/java/io/netty/handler/ssl/OpenSslServerContext.java
@@ -344,9 +344,11 @@ public final class OpenSslServerContext extends OpenSslContext {
             boolean enableOcsp) throws SSLException {
         super(ciphers, cipherFilter, apn, sessionCacheSize, sessionTimeout, SSL.SSL_MODE_SERVER, keyCertChain,
                 clientAuth, protocols, startTls, enableOcsp);
+
         // Create a new SSL_CTX and configure it.
         boolean success = false;
         try {
+            OpenSslKeyMaterialProvider.validateKeyMaterialSupported(keyCertChain, key, keyPassword);
             sessionContext = newSessionContext(this, ctx, engineMap, trustCertCollection, trustManagerFactory,
                                                keyCertChain, key, keyPassword, keyManagerFactory);
             success = true;
