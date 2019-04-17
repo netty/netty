28485275c4d1932cafe843cee0cd9c6c3680652c
qeesung <1245712564@qq.com>
2019-04-18 00:07:40 +0800

    Add different record type
        1. A record
        2. NS record
        3. CNAME record
        4. AAAA record
        5. PTR record

---
+++ codec-dns/src/main/java/io/netty/handler/codec/dns/record/DnsCNAMERecord.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2019 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package io.netty.handler.codec.dns.record;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.dns.DefaultDnsRawRecord;
+import io.netty.handler.codec.dns.DnsRecordType;
+import io.netty.handler.codec.dns.rdata.DnsTextRdataDecoder;
+
+public class DnsCNAMERecord extends DefaultDnsRawRecord {
+    private String target;
+
+    public DnsCNAMERecord(String name, int dnsClass, long timeToLive, ByteBuf content) {
+        super(name, DnsRecordType.CNAME, dnsClass, timeToLive, content);
+    }
+
+    public String target() {
+        return target;
+    }
+
+    @Override
+    public void decodeRdata(ByteBuf in, int length) {
+        target = DnsTextRdataDecoder.DEFAULT.decodeRdata(in, length);
+    }
+}
