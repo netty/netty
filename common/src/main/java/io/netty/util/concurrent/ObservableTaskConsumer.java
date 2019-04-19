b33be587d90041a008279c9236eb4404cc70334f
Francesco Nigro <nigro.fra@gmail.com>
2019-04-19 14:29:39 +0200

    EventExecutorGroup lightweight progress metrics
    Motivation:
    
    EventExecutorGroup doesn't allow to monitor submitted and completed
    tasks.
    
    Modifications:
    
    JCtools QueueProgressIndicators exposes lightweight progression metrics:
    if an executor is using a proper tasks queue, it will expose such
    metrics for free.
    MultithreadedEventExecutorGroup can expose global metrics (collected
    using children metrics) or fine-grain ones (per-child).
    
    Result:
    
    An external observer can easily monitor executors utilization
    without affecting executor performance.

---
+++ common/src/main/java/io/netty/util/concurrent/ObservableTaskConsumer.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2019 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.util.concurrent;
+
+import io.netty.util.internal.UnstableApi;
+
+@UnstableApi
+public interface ObservableTaskConsumer {
+
+    long UNSUPPORTED = -1;
+
+    /**
+     * Returns the number of submitted tasks: it's safe to be read by different threads.<br>
+     * <p>
+     * If {@code this} don't support this metrics it returns a negative value, likely {@link #UNSUPPORTED}.
+     */
+    long submittedTasks();
+
+    /**
+     * Returns the number of completed tasks: it's safe to be read by different threads.<br>
+     * <p>
+     * If {@code this} don't support this metrics it returns a negative value, likely {@link #UNSUPPORTED}.
+     */
+    long completedTasks();
+}
