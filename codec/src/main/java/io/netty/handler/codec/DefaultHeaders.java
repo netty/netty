3c11af965ad589f76c83e2a75f980483172596b7
Scott Mitchell <scott_mitchell@apple.com>
2019-04-27 11:32:50 -0700

    DefaultHeaders#valueIterator doesn't remove from the in bucket list (#9090)
    Motivation:
    DefaultHeaders entries maintains two linked lists. 1 for overall insertion order
    and 1 for "in bucket" order. DefaultHeaders#valueIterator removal (introduced in 1d9090aab231ab737bd6459e0369b30d752296b2) only reliably
    removes the entry from the overall insertion order, but may not remove from the
    bucket unless the element is the first entry.
    
    Modifications:
    - DefaultHeaders$ValueIterator should track 2 elements behind the next entry so
    that the single linked "in bucket" list can be patched up when removing the
    previous entry.
    
    Result:
    More correct DefaultHeaders#valueIterator removal.

--- codec/src/main/java/io/netty/handler/codec/DefaultHeaders.java
+++ codec/src/main/java/io/netty/handler/codec/DefaultHeaders.java
@@ -1007,14 +1007,18 @@ public class DefaultHeaders<K, V, T extends Headers<K, V, T>> implements Headers
         return value;
     }
 
-    private void remove0(HeaderEntry<K, V> entry) {
+    private HeaderEntry<K, V> remove0(HeaderEntry<K, V> entry, HeaderEntry<K, V> previous) {
         int i = index(entry.hash);
         HeaderEntry<K, V> e = entries[i];
         if (e == entry) {
             entries[i] = entry.next;
+            previous = entries[i];
+        } else {
+            previous.next = entry.next;
         }
         entry.remove();
         --size;
+        return previous;
     }
 
     @SuppressWarnings("unchecked")
@@ -1060,6 +1064,7 @@ public class DefaultHeaders<K, V, T extends Headers<K, V, T>> implements Headers
     private final class ValueIterator implements Iterator<V> {
         private final K name;
         private final int hash;
+        private HeaderEntry<K, V> removalPrevious;
         private HeaderEntry<K, V> previous;
         private HeaderEntry<K, V> next;
 
@@ -1079,6 +1084,9 @@ public class DefaultHeaders<K, V, T extends Headers<K, V, T>> implements Headers
             if (!hasNext()) {
                 throw new NoSuchElementException();
             }
+            if (previous != null) {
+                removalPrevious = previous;
+            }
             previous = next;
             calculateNext(next.next);
             return previous.value;
@@ -1089,7 +1097,7 @@ public class DefaultHeaders<K, V, T extends Headers<K, V, T>> implements Headers
             if (previous == null) {
                 throw new IllegalStateException();
             }
-            remove0(previous);
+            removalPrevious = remove0(previous, removalPrevious);
             previous = null;
         }
 
