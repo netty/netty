265c745d9ad728f4084a80f7445a37d8c1daa873
Norman Maurer <norman_maurer@apple.com>
2019-06-24 21:09:19 +0200

    EmptyByteBuf.getCharSequence(0,...) must return empty String (#9272)
    Motivation:
    
    At the moment EmptyByteBuf.getCharSequence(0,...) will return null while it must return a "".
    
    Modifications:
    
    - Let EmptyByteBuf.getCharSequence(0,...) return ""
    - Add unit test
    
    Result:
    
    Fixes https://github.com/netty/netty/issues/9271.

--- buffer/src/test/java/io/netty/buffer/EmptyByteBufTest.java
+++ buffer/src/test/java/io/netty/buffer/EmptyByteBufTest.java
@@ -14,6 +14,7 @@
  * under the License.
  */package io.netty.buffer;
 
+import io.netty.util.CharsetUtil;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.*;
@@ -93,4 +94,11 @@ public class EmptyByteBufTest {
         assertTrue(emptyAbstract.release());
         assertFalse(empty.release());
     }
+
+    @Test
+    public void testGetCharSequence() {
+        EmptyByteBuf empty = new EmptyByteBuf(UnpooledByteBufAllocator.DEFAULT);
+        assertEquals("", empty.readCharSequence(0, CharsetUtil.US_ASCII));
+    }
+
 }
