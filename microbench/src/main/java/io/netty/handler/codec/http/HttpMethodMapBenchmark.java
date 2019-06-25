e233407e01a1839d5165207066de7af82412b3a0
Alex Blewitt <alblue@apple.com>
2019-06-25 20:46:26 +0100

    Replace accumulation with blackhole.consume (#9275)
    Motivation:
    
    SpotJMHBugs reports that accumulating a value as a way of eliding dead code
    elimination may be inadvisable, as discussed in
    `JMHSample_34_SafeLooping::measureWrong_2`. Change the test so that it consumes
    the response with `Blackhole::consume` instead.
    
    Modifications:
    
    - Replace addition of results with explicit `blackhole.consume()` call
    
    Result:
    
    Tests work as before, but with different benchmark numbers.

--- microbench/src/main/java/io/netty/handler/codec/http/HttpMethodMapBenchmark.java
+++ microbench/src/main/java/io/netty/handler/codec/http/HttpMethodMapBenchmark.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 The Netty Project
+ * Copyright 2019 The Netty Project
  *
  * The Netty Project licenses this file to you under the Apache License,
  * version 2.0 (the "License"); you may not use this file except in compliance
@@ -22,6 +22,7 @@ import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.infra.Blackhole;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -151,68 +152,56 @@ public class HttpMethodMapBenchmark extends AbstractMicrobenchmark {
     }
 
     @Benchmark
-    public int oldMapKnownMethods() throws Exception {
-        int x = 0;
+    public void oldMapKnownMethods(Blackhole bh) throws Exception {
         for (int i = 0; i < KNOWN_METHODS.length; ++i) {
-            x += OLD_MAP.get(KNOWN_METHODS[i]).toString().length();
+            bh.consume(OLD_MAP.get(KNOWN_METHODS[i]));
         }
-        return x;
     }
 
     @Benchmark
-    public int newMapKnownMethods() throws Exception {
-        int x = 0;
+    public void newMapKnownMethods(Blackhole bh) throws Exception {
         for (int i = 0; i < KNOWN_METHODS.length; ++i) {
-            x += NEW_MAP.get(KNOWN_METHODS[i]).toString().length();
+            bh.consume(NEW_MAP.get(KNOWN_METHODS[i]));
         }
-        return x;
     }
 
     @Benchmark
-    public int oldMapMixMethods() throws Exception {
-        int x = 0;
+    public void oldMapMixMethods(Blackhole bh) throws Exception {
         for (int i = 0; i < MIXED_METHODS.length; ++i) {
             HttpMethod method = OLD_MAP.get(MIXED_METHODS[i]);
             if (method != null) {
-                x += method.toString().length();
+                bh.consume(method);
             }
         }
-        return x;
     }
 
     @Benchmark
-    public int newMapMixMethods() throws Exception {
-        int x = 0;
+    public void newMapMixMethods(Blackhole bh) throws Exception {
         for (int i = 0; i < MIXED_METHODS.length; ++i) {
             HttpMethod method = NEW_MAP.get(MIXED_METHODS[i]);
             if (method != null) {
-                x += method.toString().length();
+                bh.consume(method);
             }
         }
-        return x;
     }
 
     @Benchmark
-    public int oldMapUnknownMethods() throws Exception {
-        int x = 0;
+    public void oldMapUnknownMethods(Blackhole bh) throws Exception {
         for (int i = 0; i < UNKNOWN_METHODS.length; ++i) {
             HttpMethod method = OLD_MAP.get(UNKNOWN_METHODS[i]);
             if (method != null) {
-                x += method.toString().length();
+                bh.consume(method);
             }
         }
-        return x;
     }
 
     @Benchmark
-    public int newMapUnknownMethods() throws Exception {
-        int x = 0;
+    public void newMapUnknownMethods(Blackhole bh) throws Exception {
         for (int i = 0; i < UNKNOWN_METHODS.length; ++i) {
             HttpMethod method = NEW_MAP.get(UNKNOWN_METHODS[i]);
             if (method != null) {
-                x += method.toString().length();
+                bh.consume(method);
             }
         }
-        return x;
     }
 }
