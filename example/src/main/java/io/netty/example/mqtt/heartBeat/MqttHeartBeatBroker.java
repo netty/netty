b02ee1106f81a97334c076e5510d7f90d4f4e224
Robin Gong <i.robingong@gmail.com>
2019-07-10 18:18:53 +0800

    feat(example-mqtt): new MQTT heartBeat broker and client examples (#9336)
    Motivation:
    
    Recently I'm going to build MQTT broker and client based on Netty. I had MQTT encoder and decoder founded, while no basic examples. So I'm going to share my simple heartBeat MQTT broker and client as an example.
    
    Modification:
    
    New MQTT heartBeat example under io.netty.example/mqtt/heartBeat/.
    
    Result:
    
    Client would send CONNECT and PINGREQ(heartBeat message).
      - CONNECT: once channel active
      - PINGREQ: once IdleStateEvent triggered, which is 20 seconds in this example
    Client would discard all messages it received.
    MQTT broker could handle CONNECT, PINGREQ and DISCONNECT messages.
      - CONNECT: send CONNACK back
      - PINGREQ: send PINGRESP back
      - DISCONNECT: close the channel
    Broker would close the channel if 2 heartBeat lost, which set to 45 seconds in this example.

---
+++ example/src/main/java/io/netty/example/mqtt/heartBeat/MqttHeartBeatBroker.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2019 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.example.mqtt.heartBeat;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.mqtt.MqttDecoder;
+import io.netty.handler.codec.mqtt.MqttEncoder;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+public final class MqttHeartBeatBroker {
+
+    private MqttHeartBeatBroker() {
+    }
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup);
+            b.option(ChannelOption.SO_BACKLOG, 1024);
+            b.channel(NioServerSocketChannel.class);
+            b.childHandler(new ChannelInitializer<SocketChannel>() {
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast("encoder", MqttEncoder.INSTANCE);
+                    ch.pipeline().addLast("decoder", new MqttDecoder());
+                    ch.pipeline().addLast("heartBeatHandler", new IdleStateHandler(45, 0, 0, TimeUnit.SECONDS));
+                    ch.pipeline().addLast("handler", MqttHeartBeatBrokerHandler.INSTANCE);
+                }
+            });
+
+            ChannelFuture f = b.bind(1883).sync();
+            System.out.println("Broker initiated...");
+
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
