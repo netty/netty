c479b1064934be4d3023657b64d2a7cb5cb3aadb
jingene <jingene@linecorp.com>
2019-07-24 18:57:36 +0900

    Remove ResourceLeakDetector manipulation in testcase

--- buffer/src/test/java/io/netty/buffer/CompositeByteBufTest.java
+++ buffer/src/test/java/io/netty/buffer/CompositeByteBufTest.java
@@ -15,31 +15,23 @@
  */
 package io.netty.buffer;
 
-import io.netty.util.ResourceLeakDetector;
 import org.junit.Test;
 
 import static io.netty.buffer.Unpooled.*;
 import static org.junit.Assert.*;
 
-public class CompositeByteBufTest {
+public class CompositeByteBufTest extends AdvancedLeakAwareByteBufTest {
 
     @Test
     public void testAddComponentWithLeakAwareByteBuf() {
-        ResourceLeakDetector.Level level = ResourceLeakDetector.getLevel();
-        ResourceLeakDetector.setLevel(ResourceLeakDetector.Level.PARANOID);
+        ByteBuf buffer = newBuffer("hello world".length());
+        ByteBuf sliced = buffer.slice(6, 5);
 
-        try {
-            ByteBuf buffer = wrappedBuffer("hello world".getBytes()).slice(6, 5);
-            ByteBuf leakAwareBuffer = UnpooledByteBufAllocator.toLeakAwareBuffer(buffer);
+        CompositeByteBuf composite = compositeBuffer();
+        composite.addComponents(true, sliced);
+        byte[] result = new byte[5];
 
-            CompositeByteBuf composite = compositeBuffer();
-            composite.addComponents(true, leakAwareBuffer);
-            byte[] result = new byte[5];
-
-            composite.component(0).readBytes(result);
-            assertArrayEquals("world".getBytes(), result);
-        } finally {
-            ResourceLeakDetector.setLevel(level);
-        }
+        composite.component(0).readBytes(result);
+        assertArrayEquals("world".getBytes(), result);
     }
 }
