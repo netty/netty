8da1276cfd719d61b717983384dd1139b1e6cf9a
jingene <jingene@linecorp.com>
2019-07-24 17:02:44 +0900

    Add LICENSE comments

--- buffer/src/test/java/io/netty/buffer/CompositeByteBufTest.java
+++ buffer/src/test/java/io/netty/buffer/CompositeByteBufTest.java
@@ -1,6 +1,22 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package io.netty.buffer;
 
-import io.netty.util.ResourceLeakDetector;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static io.netty.buffer.Unpooled.*;
@@ -8,9 +24,21 @@ import static org.junit.Assert.*;
 
 public class CompositeByteBufTest {
 
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("io.netty.leakDetection.level", "paranoid");
+        System.setProperty("io.netty.leakDetection.samplingInterval", "1");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.clearProperty("io.netty.leakDetection.level");
+        System.clearProperty("io.netty.leakDetection.samplingInterval");
+    }
+
     @Test
     public void testAddComponentWithLeakAwareByteBuf() {
-        ResourceLeakDetector.setLevel(ResourceLeakDetector.Level.PARANOID);
+
         ByteBuf buffer = wrappedBuffer("hello world".getBytes()).slice(6, 5);
         ByteBuf leakAwareBuffer = UnpooledByteBufAllocator.toLeakAwareBuffer(buffer);
 
@@ -21,4 +49,6 @@ public class CompositeByteBufTest {
         composite.component(0).readBytes(result);
         assertArrayEquals("world".getBytes(), result);
     }
+
+
 }
