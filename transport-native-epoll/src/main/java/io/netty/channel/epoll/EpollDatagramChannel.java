netty/netty

commit e17ce934da501788c3737631a02d02ee89ebf9df

Correctly detect InternetProtocolFamily when EpollDatagramChannel is created with existing FileDescriptor (#9185)
Motivation:

When EpollDatagramChannel is created with an existing FileDescriptor we should detect the correct InternetProtocolFamily.

Modifications:

Obtain the InternetProtocolFamily from the given FD

Result:

Use correct InternetProtocolFamily when EpollDatagramChannel is created via existing FileDescriptor

transport-native-epoll/src/main/java/io/netty/channel/epoll/EpollDatagramChannel.java
@@ -59,7 +59,6 @@ public final class EpollDatagramChannel extends AbstractEpollChannel implements 
             StringUtil.simpleClassName(InetSocketAddress.class) + ">, " +
             StringUtil.simpleClassName(ByteBuf.class) + ')';
 
-    final InternetProtocolFamily family;
     private final EpollDatagramChannelConfig config;
     private volatile boolean connected;
 
@@ -68,7 +67,7 @@ public final class EpollDatagramChannel extends AbstractEpollChannel implements 
      * on the Operation Systems default which will be chosen.
      */
     public EpollDatagramChannel() {
-        this(null);
+        this((InternetProtocolFamily) null);
     }
 
     /**
@@ -76,17 +75,8 @@ public final class EpollDatagramChannel extends AbstractEpollChannel implements 
      * on the Operation Systems default which will be chosen.
      */
     public EpollDatagramChannel(InternetProtocolFamily family) {
-        super(family == null ?
+        this(family == null ?
                 newSocketDgram(Socket.isIPv6Preferred()) : newSocketDgram(family == InternetProtocolFamily.IPv6));
-        this.family = internetProtocolFamily(family);
-        config = new EpollDatagramChannelConfig(this);
-    }
-
-    private static InternetProtocolFamily internetProtocolFamily(InternetProtocolFamily family) {
-        if (family == null) {
-            return Socket.isIPv6Preferred() ? InternetProtocolFamily.IPv6 : InternetProtocolFamily.IPv4;
-        }
-        return family;
     }
 
     /**
@@ -94,12 +84,11 @@ public final class EpollDatagramChannel extends AbstractEpollChannel implements 
      * on the Operation Systems default which will be chosen.
      */
     public EpollDatagramChannel(int fd) {
-        this(new LinuxSocket(fd), null);
+        this(new LinuxSocket(fd));
     }
 
-    private EpollDatagramChannel(LinuxSocket fd, InternetProtocolFamily family) {
+    private EpollDatagramChannel(LinuxSocket fd) {
         super(null, fd, true);
-        this.family = internetProtocolFamily(family);
         config = new EpollDatagramChannelConfig(this);
     }
 

transport-native-epoll/src/main/java/io/netty/channel/epoll/EpollDatagramChannelConfig.java
@@ -367,7 +367,7 @@ public final class EpollDatagramChannelConfig extends EpollChannelConfig impleme
     public EpollDatagramChannelConfig setNetworkInterface(NetworkInterface networkInterface) {
         try {
             EpollDatagramChannel datagramChannel = (EpollDatagramChannel) channel;
-            datagramChannel.socket.setNetworkInterface(networkInterface, datagramChannel.family);
+            datagramChannel.socket.setNetworkInterface(networkInterface);
             return this;
         } catch (IOException e) {
             throw new ChannelException(e);

transport-native-epoll/src/main/java/io/netty/channel/epoll/LinuxSocket.java
@@ -44,6 +44,10 @@ final class LinuxSocket extends Socket {
         super(fd);
     }
 
+    private InternetProtocolFamily family() {
+        return ipv6 ? InternetProtocolFamily.IPv6 : InternetProtocolFamily.IPv4;
+    }
+
     int sendmmsg(NativeDatagramPacketArray.NativeDatagramPacket[] msgs,
                                int offset, int len) throws IOException {
         return Native.sendmmsg(intValue(), ipv6, msgs, offset, len);
@@ -58,10 +62,10 @@ final class LinuxSocket extends Socket {
         setInterface(intValue(), ipv6, a.address(), a.scopeId(), interfaceIndex(address));
     }
 
-    void setNetworkInterface(NetworkInterface netInterface, InternetProtocolFamily family) throws IOException {
-        InetAddress address = deriveInetAddress(netInterface, family == InternetProtocolFamily.IPv6);
-        if (address.equals(family == InternetProtocolFamily.IPv4 ? INET_ANY : INET6_ANY)) {
-            throw new IOException("NetworkInterface does not support " + family);
+    void setNetworkInterface(NetworkInterface netInterface) throws IOException {
+        InetAddress address = deriveInetAddress(netInterface, family() == InternetProtocolFamily.IPv6);
+        if (address.equals(family() == InternetProtocolFamily.IPv4 ? INET_ANY : INET6_ANY)) {
+            throw new IOException("NetworkInterface does not support " + family());
         }
         final NativeInetAddress nativeAddress = NativeInetAddress.newInstance(address);
         setInterface(intValue(), ipv6, nativeAddress.address(), nativeAddress.scopeId(), interfaceIndex(netInterface));

commit 60a97dfb70a57bec6f02de9b50c83550824456bf

Do not cache local/remote address when creating EpollDatagramChannel with InternetProtocolFamily
Motivation:

EpollDatagramChannel#localAddress returns wrong information when
EpollDatagramChannel is created with InternetProtocolFamily,
and EpollDatagramChannel#localAddress is invoked BEFORE the actual binding.

This is a regression caused by change
https://github.com/netty/netty/commit/e17ce934da501788c3737631a02d02ee89ebf9df

Modifications:

EpollDatagramChannel() and EpollDatagramChannel(InternetProtocolFamily family)
do not cache local/remote address

Result:

Rebinding on the same address without "reuse port" works
EpollDatagramChannel#localAddress returns correct address

transport-native-epoll/src/main/java/io/netty/channel/epoll/EpollDatagramChannel.java
@@ -67,7 +67,7 @@ public final class EpollDatagramChannel extends AbstractEpollChannel implements 
      * on the Operation Systems default which will be chosen.
      */
     public EpollDatagramChannel() {
-        this((InternetProtocolFamily) null);
+        this(null);
     }
 
     /**
@@ -76,7 +76,8 @@ public final class EpollDatagramChannel extends AbstractEpollChannel implements 
      */
     public EpollDatagramChannel(InternetProtocolFamily family) {
         this(family == null ?
-                newSocketDgram(Socket.isIPv6Preferred()) : newSocketDgram(family == InternetProtocolFamily.IPv6));
+                newSocketDgram(Socket.isIPv6Preferred()) : newSocketDgram(family == InternetProtocolFamily.IPv6),
+                false);
     }
 
     /**
@@ -84,11 +85,11 @@ public final class EpollDatagramChannel extends AbstractEpollChannel implements 
      * on the Operation Systems default which will be chosen.
      */
     public EpollDatagramChannel(int fd) {
-        this(new LinuxSocket(fd));
+        this(new LinuxSocket(fd), true);
     }
 
-    private EpollDatagramChannel(LinuxSocket fd) {
-        super(null, fd, true);
+    private EpollDatagramChannel(LinuxSocket fd, boolean active) {
+        super(null, fd, active);
         config = new EpollDatagramChannelConfig(this);
     }
 

transport-native-epoll/src/test/java/io/netty/channel/epoll/EpollReuseAddrTest.java
@@ -23,6 +23,8 @@ import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import io.netty.util.NetUtil;
 import io.netty.util.ReferenceCountUtil;
 import io.netty.util.ResourceLeakDetector;
@@ -78,7 +80,7 @@ public class EpollReuseAddrTest {
     }
 
     private static void testMultipleBindDatagramChannelWithoutReusePortFails0(AbstractBootstrap<?, ?> bootstrap) {
-        bootstrap.handler(new DummyHandler());
+        bootstrap.handler(new LoggingHandler(LogLevel.ERROR));
         ChannelFuture future = bootstrap.bind().syncUninterruptibly();
         try {
             bootstrap.bind(future.channel().localAddress()).syncUninterruptibly();