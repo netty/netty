04b03906761bb405d7b46fd432033ed608d46ced
nickhill <nickhill@us.ibm.com>
2019-09-03 18:15:40 -0700

    Avoid CancellationException construction in DefaultPromise
    Motivation
    
    #9152 reverted some static exception reuse optimizations due to the
    problem with Throwable#addSuppressed() raised in #9151. This introduced
    a performance issue when promises are cancelled at a high frequency due
    to the construction cost of CancellationException at the time that
    DefaultPromise#cancel() is called.
    
    Modifications
    
    - Reinstate the prior static CANCELLATION_CAUSE_HOLDER but use it just
    as a sentinel to indicate cancellation, constructing a new
    CancellationException only if/when one needs to be explicitly
    returned/thrown
    - Subclass CancellationException, overriding fillInStackTrace() to
    minimize the construction cost in these cases
    
    Result
    
    Promises are much cheaper to cancel. Fixes #9522.

--- common/src/main/java/io/netty/util/concurrent/DefaultPromise.java
+++ common/src/main/java/io/netty/util/concurrent/DefaultPromise.java
@@ -19,6 +19,7 @@ import io.netty.util.internal.InternalThreadLocalMap;
 import io.netty.util.internal.PlatformDependent;
 import io.netty.util.internal.StringUtil;
 import io.netty.util.internal.SystemPropertyUtil;
+import io.netty.util.internal.ThrowableUtil;
 import io.netty.util.internal.logging.InternalLogger;
 import io.netty.util.internal.logging.InternalLoggerFactory;
 
@@ -40,6 +41,8 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
             AtomicReferenceFieldUpdater.newUpdater(DefaultPromise.class, Object.class, "result");
     private static final Object SUCCESS = new Object();
     private static final Object UNCANCELLABLE = new Object();
+    private static final CauseHolder CANCELLATION_CAUSE_HOLDER = new CauseHolder(ThrowableUtil.unknownStackTrace(
+            new CancellationException(), DefaultPromise.class, "cancel(...)"));
 
     private volatile Object result;
     private final EventExecutor executor;
@@ -131,9 +134,26 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
         return result == null;
     }
 
+    private static class LeanCancellationException extends CancellationException {
+        private static final long serialVersionUID = 2794674970981187807L;
+
+        @Override
+        public Throwable fillInStackTrace() {
+            setStackTrace(CANCELLATION_CAUSE_HOLDER.cause.getStackTrace());
+            return this;
+        }
+        @Override
+        public String toString() {
+            return CancellationException.class.getName();
+        }
+    }
+
     @Override
     public Throwable cause() {
         Object result = this.result;
+        if (result == CANCELLATION_CAUSE_HOLDER) {
+            throw new LeanCancellationException();
+        }
         return (result instanceof CauseHolder) ? ((CauseHolder) result).cause : null;
     }
 
@@ -301,8 +321,7 @@ public class DefaultPromise<V> extends AbstractFuture<V> implements Promise<V> {
      */
     @Override
     public boolean cancel(boolean mayInterruptIfRunning) {
-        if (RESULT_UPDATER.get(this) == null &&
-                RESULT_UPDATER.compareAndSet(this, null, new CauseHolder(new CancellationException()))) {
+        if (RESULT_UPDATER.compareAndSet(this, null, CANCELLATION_CAUSE_HOLDER)) {
             if (checkNotifyWaiters()) {
                 notifyListeners();
             }
