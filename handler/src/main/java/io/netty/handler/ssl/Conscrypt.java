027aec23b839f00639f30641e08c95700cc9b9a5
Norman Maurer <norman_maurer@apple.com>
2019-09-07 22:32:15 +0200

    Allow to build on powerpc
    Motivation:
    
    At the moment it is not possible to build netty on a power 8 systems.
    
    Modifications:
    
    - Improve detection of the possibility of using Conscrypt
    - Skip testsuite-shading when not on x86_64 as this is the only platform for which we build tcnative atm
    - Only include classifier if on x86_64 for tcnative as dependency as this is the only platform for which we build tcnative atm
    - Better detect if UDT test can be run
    
    Result:
    
    Fixes https://github.com/netty/netty/issues/9479

--- handler/src/main/java/io/netty/handler/ssl/Conscrypt.java
+++ handler/src/main/java/io/netty/handler/ssl/Conscrypt.java
@@ -26,6 +26,7 @@ final class Conscrypt {
     // This class exists to avoid loading other conscrypt related classes using features only available in JDK8+,
     // because we need to maintain JDK6+ runtime compatibility.
     private static final Method IS_CONSCRYPT_SSLENGINE = loadIsConscryptEngine();
+    private static final boolean CAN_INSTANCE_PROVIDER = canInstanceProvider();
 
     private static Method loadIsConscryptEngine() {
         try {
@@ -38,11 +39,22 @@ final class Conscrypt {
         }
     }
 
+    private static boolean canInstanceProvider() {
+        try {
+            Class<?> providerClass = Class.forName("org.conscrypt.OpenSSLProvider", true,
+                    ConscryptAlpnSslEngine.class.getClassLoader());
+            providerClass.newInstance();
+            return true;
+        } catch (Throwable ignore) {
+            return false;
+        }
+    }
+
     /**
      * Indicates whether or not conscrypt is available on the current system.
      */
     static boolean isAvailable() {
-        return IS_CONSCRYPT_SSLENGINE != null;
+        return CAN_INSTANCE_PROVIDER && IS_CONSCRYPT_SSLENGINE != null;
     }
 
     static boolean isEngineSupported(SSLEngine engine) {
