08ada34a7f5714fbcc3e042c3a9263442bc641f8
601947961 <601947961@qq.com>
2019-09-15 22:11:36 +0800

    optimize the QueryStringEncoder

--- codec-http/src/main/java/io/netty/handler/codec/http/QueryStringEncoder.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/QueryStringEncoder.java
@@ -17,12 +17,11 @@ package io.netty.handler.codec.http;
 
 import io.netty.util.internal.ObjectUtil;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
-import java.nio.charset.UnsupportedCharsetException;
+import java.util.BitSet;
 
 /**
  * Creates an URL-encoded URI from a path string and key-value parameter pairs.
@@ -37,9 +36,30 @@ import java.nio.charset.UnsupportedCharsetException;
  */
 public class QueryStringEncoder {
 
-    private final String charsetName;
+    private final Charset charset;
     private final StringBuilder uriBuilder;
     private boolean hasParams;
+    private static BitSet dontNeedEncoding;
+    private static final int CASE_DIFF = 'a' - 'A';
+
+    static {
+        dontNeedEncoding = new BitSet(256);
+        int i;
+        for (i = 'a'; i <= 'z'; i++) {
+            dontNeedEncoding.set(i);
+        }
+        for (i = 'A'; i <= 'Z'; i++) {
+            dontNeedEncoding.set(i);
+        }
+        for (i = '0'; i <= '9'; i++) {
+            dontNeedEncoding.set(i);
+        }
+
+        dontNeedEncoding.set('-');
+        dontNeedEncoding.set('_');
+        dontNeedEncoding.set('.');
+        dontNeedEncoding.set('*');
+    }
 
     /**
      * Creates a new encoder that encodes a URI that starts with the specified
@@ -55,7 +75,7 @@ public class QueryStringEncoder {
      */
     public QueryStringEncoder(String uri, Charset charset) {
         uriBuilder = new StringBuilder(uri);
-        charsetName = charset.name();
+        this.charset = charset;
     }
 
     /**
@@ -69,10 +89,11 @@ public class QueryStringEncoder {
             uriBuilder.append('?');
             hasParams = true;
         }
-        appendComponent(name, charsetName, uriBuilder);
+
+        encodeComponent(name);
         if (value != null) {
             uriBuilder.append('=');
-            appendComponent(value, charsetName, uriBuilder);
+            encodeComponent(value);
         }
     }
 
@@ -95,27 +116,54 @@ public class QueryStringEncoder {
         return uriBuilder.toString();
     }
 
-    private static void appendComponent(String s, String charset, StringBuilder sb) {
-        try {
-            s = URLEncoder.encode(s, charset);
-        } catch (UnsupportedEncodingException ignored) {
-            throw new UnsupportedCharsetException(charset);
-        }
-        // replace all '+' with "%20"
-        int idx = s.indexOf('+');
-        if (idx == -1) {
-            sb.append(s);
-            return;
-        }
-        sb.append(s, 0, idx).append("%20");
-        int size = s.length();
-        idx++;
-        for (; idx < size; idx++) {
-            char c = s.charAt(idx);
-            if (c != '+') {
-                sb.append(c);
+    /**
+     * encode the String as per RFC 3986, Section 2.
+     * <p>
+     * There is a little different between the JDK's encode method : {@link URLEncoder#encode(String, String)}.
+     * The JDK's encoder encode the space to {@code +} and this method directly encode the blank to {@code %20}
+     * beyond that , this method reuse the {@link #uriBuilder} in this class rather then create a new one,
+     * thus generates less garbage for the GC.
+     *
+     * @param s The String to encode
+     */
+    private void encodeComponent(String s) {
+        //allocate memory until needed
+        char[] buf = null;
+
+        for (int i = 0; i < s.length();) {
+            int c = s.charAt(i);
+            if (dontNeedEncoding.get(c)) {
+                uriBuilder.append((char) c);
+                i++;
             } else {
-                sb.append("%20");
+                int index = 0;
+                if (buf == null) {
+                    buf = new char[s.length() - i];
+                }
+
+                do {
+                    buf[index] = (char) c;
+                    index++;
+                    i++;
+                } while (i < s.length() && !dontNeedEncoding.get(c = s.charAt(i)));
+
+                byte[] bytes = new String(buf, 0, index).getBytes(charset);
+
+                for (byte b : bytes) {
+                    uriBuilder.append('%');
+
+                    char ch = Character.forDigit((b >> 4) & 0xF, 16);
+                    if (ch >= 'a' && ch <= 'f') {
+                        ch -= CASE_DIFF;
+                    }
+                    uriBuilder.append(ch);
+
+                    ch = Character.forDigit(b & 0xF, 16);
+                    if (ch >= 'a' && ch <= 'f') {
+                        ch -= CASE_DIFF;
+                    }
+                    uriBuilder.append(ch);
+                }
             }
         }
     }
