a48bd4d154ff5d61da1cd7baad1e91d3b1fa44ed
nickhill <nickhill@us.ibm.com>
2019-11-01 10:36:25 -0700

    Add comment

--- common/src/main/java/io/netty/util/concurrent/AbstractScheduledEventExecutor.java
+++ common/src/main/java/io/netty/util/concurrent/AbstractScheduledEventExecutor.java
@@ -245,6 +245,7 @@ public abstract class AbstractScheduledEventExecutor extends AbstractEventExecut
     }
 
     final void scheduleFromEventLoop(final ScheduledFutureTask<?> task) {
+        // nextTaskId a long and so there is no chance it will overflow back to 0
         scheduledTaskQueue().add(task.setId(++nextTaskId));
     }
 
