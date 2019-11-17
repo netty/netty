2e449a67698fd566f27d14b881f8dffcdb9849e2
stroller <fujian1115@gmail.com>
2019-11-18 00:35:24 +0800

    Improve java doc  for MINIMAL_WAIT (#9779)
    Motivation:
    
    MINIMAL_WAIT is the key constant. Thus, When we see the constant, we must read more code logic to see if it is ms or ns. So improving java doc will be better.
    
    Modifications:
    Improve java doc by add "10ms" such as DEFAULT_CHECK_INTERVAL with "1s".
    
    Result:
    
    Easy to know it is ms and keep same java doc style with other constants such as DEFAULT_CHECK_INTERVAL.

--- handler/src/main/java/io/netty/handler/traffic/AbstractTrafficShapingHandler.java
+++ handler/src/main/java/io/netty/handler/traffic/AbstractTrafficShapingHandler.java
@@ -68,7 +68,7 @@ public abstract class AbstractTrafficShapingHandler implements ChannelHandler {
     static final long DEFAULT_MAX_SIZE = 4 * 1024 * 1024L;
 
     /**
-     * Default minimal time to wait
+     * Default minimal time to wait: 10ms
      */
     static final long MINIMAL_WAIT = 10;
 
