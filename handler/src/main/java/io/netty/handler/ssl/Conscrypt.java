ca4570cf23cae4f05e1be0d671cbe98ed0441b93
Norman Maurer <norman_maurer@apple.com>
2020-04-24 12:22:43 +0200

    Update testsuite / pom.xml to be able to build with Java15
    Motivation:
    
    We need to make some slightly changes to be able to build on Java15 as some previous deprecated methods now throw UnsupportedOperationException
    
    Modifications:
    
    - Add code to handle UnsupportedOperationException
    - Revert previous applied workaround for bug that was fixed in Java15
    - Add maven profile
    
    Result:
    
    Be able to build with latest Java15 EA release

--- handler/src/main/java/io/netty/handler/ssl/Conscrypt.java
+++ handler/src/main/java/io/netty/handler/ssl/Conscrypt.java
@@ -57,7 +57,9 @@ final class Conscrypt {
      */
     static boolean isAvailable() {
         return CAN_INSTANCE_PROVIDER && IS_CONSCRYPT_SSLENGINE != null &&
-            (PlatformDependent.javaVersion() >= 8 || PlatformDependent.isAndroid());
+                ((PlatformDependent.javaVersion() >= 8 &&
+                        // Only works on Java14 and earlier for now
+                        PlatformDependent.javaVersion() < 15) || PlatformDependent.isAndroid());
     }
 
     static boolean isEngineSupported(SSLEngine engine) {
