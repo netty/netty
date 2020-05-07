a2d2ae036da7256195602f876d2c98a622b1e883
Aayush Atharva <aayush@shieldblaze.com>
2020-05-07 15:35:51 +0530

    Add license, make class final and generate DNS query ID randomly

--- example/src/main/java/io/netty/example/dns/dot/DoTClient.java
+++ example/src/main/java/io/netty/example/dns/dot/DoTClient.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package io.netty.example.dns.dot;
 
 import io.netty.bootstrap.Bootstrap;
@@ -29,9 +44,10 @@ import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.util.NetUtil;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-public class DoTClient {
+public final class DoTClient {
     private static final String QUERY_DOMAIN = "www.example.com";
     private static final int DNS_SERVER_PORT = 853;
     private static final String DNS_SERVER_HOST = "8.8.8.8";
@@ -86,7 +102,8 @@ public class DoTClient {
                     });
             final Channel ch = b.connect(DNS_SERVER_HOST, DNS_SERVER_PORT).sync().channel();
 
-            DnsQuery query = new DefaultDnsQuery(5252, DnsOpCode.QUERY)
+            int randomID = new Random().nextInt(60000-1000) + 1000;
+            DnsQuery query = new DefaultDnsQuery(randomID, DnsOpCode.QUERY)
                     .setRecord(DnsSection.QUESTION, new DefaultDnsQuestion(QUERY_DOMAIN, DnsRecordType.A));
             ch.writeAndFlush(query).sync();
             boolean succ = ch.closeFuture().await(10, TimeUnit.SECONDS);
