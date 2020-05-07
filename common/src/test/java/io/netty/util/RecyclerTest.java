b4d71bfedcf57db0d2c09efc79eecbf429b0c2ab
louyl <louyl@fenbi.com>
2020-05-07 16:39:34 +0800

    Add option to configure recycler delayed queue drop ratio (#10251)
    Motivation
    
    - Recycler stack and delayed queue drop ratio can only be configured with
    the same value. The overall drop ratio is ratio^2.
    
    - #10251 shows that enable drop in `WeakOrderQueue` may introduce
    performance degradation. Though the final reason is not clear now,
    it would be better to add option to configure delayed queue drop ratio separately.
    
    Modification
    
    - "io.netty.recycler.delayedQueue.ratio" as the drop ratio of delayed queue
    - default "delayedQueue.ratio" is same as "ratio"
    
    Results
    
    Able to configure recycler delayed queue drop ratio separately

--- common/src/test/java/io/netty/util/RecyclerTest.java
+++ common/src/test/java/io/netty/util/RecyclerTest.java
@@ -27,13 +27,14 @@ import static org.junit.Assert.*;
 public class RecyclerTest {
 
     private static Recycler<HandledObject> newRecycler(int maxCapacityPerThread) {
-        return newRecycler(maxCapacityPerThread, 2, 8, 2);
+        return newRecycler(maxCapacityPerThread, 2, 8, 2, 8);
     }
 
     private static Recycler<HandledObject> newRecycler(int maxCapacityPerThread, int maxSharedCapacityFactor,
-                                                       int ratio, int maxDelayedQueuesPerThread) {
+                                                       int ratio, int maxDelayedQueuesPerThread,
+                                                       int delayedQueueRatio) {
         return new Recycler<HandledObject>(maxCapacityPerThread, maxSharedCapacityFactor, ratio,
-                maxDelayedQueuesPerThread) {
+                maxDelayedQueuesPerThread, delayedQueueRatio) {
             @Override
             protected HandledObject newObject(
                     Recycler.Handle<HandledObject> handle) {
@@ -141,7 +142,7 @@ public class RecyclerTest {
 
     @Test
     public void testRecycleDisableDrop() {
-        Recycler<HandledObject> recycler = newRecycler(1024, 2, 0, 2);
+        Recycler<HandledObject> recycler = newRecycler(1024, 2, 0, 2, 0);
         HandledObject object = recycler.get();
         object.recycle();
         HandledObject object2 = recycler.get();
@@ -150,6 +151,27 @@ public class RecyclerTest {
         HandledObject object3 = recycler.get();
         assertSame(object, object3);
         object3.recycle();
+    }
+
+    @Test
+    public void testRecycleDisableDelayedQueueDrop() throws Exception {
+        final Recycler<HandledObject> recycler = newRecycler(1024, 2, 1, 2, 0);
+        final HandledObject o = recycler.get();
+        final HandledObject o2 = recycler.get();
+        final HandledObject o3 = recycler.get();
+        final Thread thread = new Thread() {
+            @Override
+            public void run() {
+                o.recycle();
+                o2.recycle();
+                o3.recycle();
+            }
+        };
+        thread.start();
+        thread.join();
+        // In reverse order
+        assertSame(o3, recycler.get());
+        assertSame(o, recycler.get());
     }
 
     /**
@@ -184,13 +206,7 @@ public class RecyclerTest {
 
     @Test
     public void testRecycleAtDifferentThread() throws Exception {
-        final Recycler<HandledObject> recycler = new Recycler<HandledObject>(256, 10, 2, 10) {
-            @Override
-            protected HandledObject newObject(Recycler.Handle<HandledObject> handle) {
-                return new HandledObject(handle);
-            }
-        };
-
+        final Recycler<HandledObject> recycler = newRecycler(256, 10, 2, 10, 2);
         final HandledObject o = recycler.get();
         final HandledObject o2 = recycler.get();
 
