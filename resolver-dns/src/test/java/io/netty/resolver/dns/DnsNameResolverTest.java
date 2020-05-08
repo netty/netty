netty/netty

commit 5650db582607a8eb95022cd73cdc490cc94c0e1c

Add cache for CNAME mappings resolved during lookup of DNS entries. (#8314)
* Add cache for CNAME mappings resolved during lookup of DNS entries.

Motivation:

If the CNAMEd hostname is backed by load balancing component, typically the final A or AAAA DNS records have small TTL. However, the CNAME record itself is setup with longer TTL.

For example:
* x.netty.io could be CNAMEd to y.netty.io with TTL of 5 min
* A / AAAA records for y.netty.io has a TTL of 0.5 min

In current Netty implementation, original hostname is saved in resolved cached with the TTL of final A / AAAA records. When that cache entry expires, Netty recursive resolver sends at least two queries — 1st one to be resolved as CNAME record and the 2nd one to resolve the hostname in CNAME record.
If CNAME record was cached, only the 2nd query would be needed most of the time. 1st query would be needed less frequently.

Modifications:

Add a new CnameCache that will be used to cache CNAMEs and so may reduce queries.

Result:

Less queries needed when CNAME is used.

resolver-dns/src/main/java/io/netty/resolver/dns/DefaultDnsCnameCache.java
@@ -1,0 +1,99 @@
+/*
+ * Copyright 2018 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.resolver.dns;
+
+import io.netty.channel.EventLoop;
+import io.netty.util.AsciiString;
+import io.netty.util.internal.UnstableApi;
+
+import java.util.List;
+
+import static io.netty.util.internal.ObjectUtil.*;
+
+/**
+ * Default implementation of a {@link DnsCnameCache}.
+ */
+@UnstableApi
+public final class DefaultDnsCnameCache implements DnsCnameCache {
+    private final int minTtl;
+    private final int maxTtl;
+
+    private final Cache<String> cache = new Cache<String>() {
+        @Override
+        protected boolean shouldReplaceAll(String entry) {
+            // Only one 1:1 mapping is supported as specified in the RFC.
+            return true;
+        }
+
+        @Override
+        protected boolean equals(String entry, String otherEntry) {
+            return AsciiString.contentEqualsIgnoreCase(entry, otherEntry);
+        }
+    };
+
+    /**
+     * Create a cache that respects the TTL returned by the DNS server.
+     */
+    public DefaultDnsCnameCache() {
+        this(0, Cache.MAX_SUPPORTED_TTL_SECS);
+    }
+
+    /**
+     * Create a cache.
+     *
+     * @param minTtl the minimum TTL
+     * @param maxTtl the maximum TTL
+     */
+    public DefaultDnsCnameCache(int minTtl, int maxTtl) {
+        this.minTtl = Math.min(Cache.MAX_SUPPORTED_TTL_SECS, checkPositiveOrZero(minTtl, "minTtl"));
+        this.maxTtl = Math.min(Cache.MAX_SUPPORTED_TTL_SECS, checkPositive(maxTtl, "maxTtl"));
+        if (minTtl > maxTtl) {
+            throw new IllegalArgumentException(
+                    "minTtl: " + minTtl + ", maxTtl: " + maxTtl + " (expected: 0 <= minTtl <= maxTtl)");
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public String get(String hostname) {
+        checkNotNull(hostname, "hostname");
+        List<? extends String> cached =  cache.get(hostname);
+        if (cached == null || cached.isEmpty()) {
+            return null;
+        }
+        // We can never have more then one record.
+        return cached.get(0);
+    }
+
+    @Override
+    public void cache(String hostname, String cname, long originalTtl, EventLoop loop) {
+        checkNotNull(hostname, "hostname");
+        checkNotNull(cname, "cname");
+        checkNotNull(loop, "loop");
+        cache.cache(hostname, cname, Math.max(minTtl, (int) Math.min(maxTtl, originalTtl)), loop);
+    }
+
+    @Override
+    public void clear() {
+        cache.clear();
+    }
+
+    @Override
+    public boolean clear(String hostname) {
+        checkNotNull(hostname, "hostname");
+        return cache.clear(hostname);
+    }
+}

resolver-dns/src/main/java/io/netty/resolver/dns/DnsCnameCache.java
@@ -1,0 +1,59 @@
+/*
+ * Copyright 2018 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.resolver.dns;
+
+import io.netty.channel.EventLoop;
+import io.netty.util.internal.UnstableApi;
+
+/**
+ * A cache for {@code CNAME}s.
+ */
+@UnstableApi
+public interface DnsCnameCache {
+
+    /**
+     * Returns the cached cname for the given hostname.
+     *
+     * @param hostname the hostname
+     * @return the cached entries or an {@code null} if none.
+     */
+    String get(String hostname);
+
+    /**
+     * Caches a cname entry that should be used for the given hostname.
+     *
+     * @param hostname the hostname
+     * @param cname the cname mapping.
+     * @param originalTtl the TTL as returned by the DNS server
+     * @param loop the {@link EventLoop} used to register the TTL timeout
+     */
+    void cache(String hostname, String cname, long originalTtl, EventLoop loop);
+
+    /**
+     * Clears all cached nameservers.
+     *
+     * @see #clear(String)
+     */
+    void clear();
+
+    /**
+     * Clears the cached nameservers for the specified hostname.
+     *
+     * @return {@code true} if and only if there was an entry for the specified host name in the cache and
+     *         it has been removed by this method
+     */
+    boolean clear(String hostname);
+}

resolver-dns/src/main/java/io/netty/resolver/dns/DnsNameResolver.java
@@ -174,6 +174,7 @@ public class DnsNameResolver extends InetNameResolver {
      */
     private final DnsCache resolveCache;
     private final AuthoritativeDnsServerCache authoritativeDnsServerCache;
+    private final DnsCnameCache cnameCache;
 
     private final FastThreadLocal<DnsServerAddressStream> nameServerAddrStream =
             new FastThreadLocal<DnsServerAddressStream>() {
@@ -225,9 +226,7 @@ public class DnsNameResolver extends InetNameResolver {
      * @param ndots the ndots value
      * @param decodeIdn {@code true} if domain / host names should be decoded to unicode when received.
      *                        See <a href="https://tools.ietf.org/html/rfc3492">rfc3492</a>.
-     * @deprecated Use {@link DnsNameResolver(EventLoop, ChannelFactory, DnsCache, AuthoritativeDnsServerCache,
-     * DnsQueryLifecycleObserverFactory, long, ResolvedAddressTypes, boolean, int, boolean, int, boolean,
-     * HostsFileEntriesResolver, DnsServerAddressStreamProvider, String[], int, boolean)}
+     * @deprecated Use {@link DnsNameResolverBuilder}.
      */
     @Deprecated
     public DnsNameResolver(
@@ -279,11 +278,38 @@ public class DnsNameResolver extends InetNameResolver {
      * @param ndots the ndots value
      * @param decodeIdn {@code true} if domain / host names should be decoded to unicode when received.
      *                        See <a href="https://tools.ietf.org/html/rfc3492">rfc3492</a>.
+     * @deprecated Use {@link DnsNameResolverBuilder}.
      */
+    @Deprecated
     public DnsNameResolver(
             EventLoop eventLoop,
             ChannelFactory<? extends DatagramChannel> channelFactory,
             final DnsCache resolveCache,
+            final AuthoritativeDnsServerCache authoritativeDnsServerCache,
+            DnsQueryLifecycleObserverFactory dnsQueryLifecycleObserverFactory,
+            long queryTimeoutMillis,
+            ResolvedAddressTypes resolvedAddressTypes,
+            boolean recursionDesired,
+            int maxQueriesPerResolve,
+            boolean traceEnabled,
+            int maxPayloadSize,
+            boolean optResourceEnabled,
+            HostsFileEntriesResolver hostsFileEntriesResolver,
+            DnsServerAddressStreamProvider dnsServerAddressStreamProvider,
+            String[] searchDomains,
+            int ndots,
+            boolean decodeIdn) {
+        this(eventLoop, channelFactory, resolveCache, NoopDnsCnameCache.INSTANCE, authoritativeDnsServerCache,
+             dnsQueryLifecycleObserverFactory, queryTimeoutMillis, resolvedAddressTypes, recursionDesired,
+             maxQueriesPerResolve, traceEnabled, maxPayloadSize, optResourceEnabled, hostsFileEntriesResolver,
+             dnsServerAddressStreamProvider, searchDomains, ndots, decodeIdn);
+    }
+
+    DnsNameResolver(
+            EventLoop eventLoop,
+            ChannelFactory<? extends DatagramChannel> channelFactory,
+            final DnsCache resolveCache,
+            final DnsCnameCache cnameCache,
             final AuthoritativeDnsServerCache authoritativeDnsServerCache,
             DnsQueryLifecycleObserverFactory dnsQueryLifecycleObserverFactory,
             long queryTimeoutMillis,
@@ -309,6 +335,7 @@ public class DnsNameResolver extends InetNameResolver {
         this.dnsServerAddressStreamProvider =
                 checkNotNull(dnsServerAddressStreamProvider, "dnsServerAddressStreamProvider");
         this.resolveCache = checkNotNull(resolveCache, "resolveCache");
+        this.cnameCache = checkNotNull(cnameCache, "cnameCache");
         this.dnsQueryLifecycleObserverFactory = traceEnabled ?
                 dnsQueryLifecycleObserverFactory instanceof NoopDnsQueryLifecycleObserverFactory ?
                         new TraceDnsQueryLifeCycleObserverFactory() :
@@ -382,6 +409,7 @@ public class DnsNameResolver extends InetNameResolver {
             @Override
             public void operationComplete(ChannelFuture future) {
                 resolveCache.clear();
+                cnameCache.clear();
                 authoritativeDnsServerCache.clear();
             }
         });
@@ -431,6 +459,13 @@ public class DnsNameResolver extends InetNameResolver {
      */
     public DnsCache resolveCache() {
         return resolveCache;
+    }
+
+    /**
+     * Returns the {@link DnsCnameCache}.
+     */
+    DnsCnameCache cnameCache() {
+        return cnameCache;
     }
 
     /**

resolver-dns/src/main/java/io/netty/resolver/dns/DnsNameResolverBuilder.java
@@ -40,6 +40,7 @@ public final class DnsNameResolverBuilder {
     private EventLoop eventLoop;
     private ChannelFactory<? extends DatagramChannel> channelFactory;
     private DnsCache resolveCache;
+    private DnsCnameCache cnameCache;
     private AuthoritativeDnsServerCache authoritativeDnsServerCache;
     private Integer minTtl;
     private Integer maxTtl;
@@ -120,6 +121,17 @@ public final class DnsNameResolverBuilder {
      */
     public DnsNameResolverBuilder resolveCache(DnsCache resolveCache) {
         this.resolveCache  = resolveCache;
+        return this;
+    }
+
+    /**
+     * Sets the cache for {@code CNAME} mappings.
+     *
+     * @param cnameCache the cache used to cache {@code CNAME} mappings for a domain.
+     * @return {@code this}
+     */
+    public DnsNameResolverBuilder cnameCache(DnsCnameCache cnameCache) {
+        this.cnameCache  = cnameCache;
         return this;
     }
 
@@ -376,6 +388,11 @@ public final class DnsNameResolverBuilder {
                 new NameServerComparator(DnsNameResolver.preferredAddressType(resolvedAddressTypes).addressType()));
     }
 
+    private DnsCnameCache newCnameCache() {
+        return new DefaultDnsCnameCache(
+                intValue(minTtl, 0), intValue(maxTtl, Integer.MAX_VALUE));
+    }
+
     /**
      * Set if domain / host names should be decoded to unicode when received.
      * See <a href="https://tools.ietf.org/html/rfc3492">rfc3492</a>.
@@ -407,12 +424,14 @@ public final class DnsNameResolverBuilder {
         }
 
         DnsCache resolveCache = this.resolveCache != null ? this.resolveCache : newCache();
+        DnsCnameCache cnameCache = this.cnameCache != null ? this.cnameCache : newCnameCache();
         AuthoritativeDnsServerCache authoritativeDnsServerCache = this.authoritativeDnsServerCache != null ?
                 this.authoritativeDnsServerCache : newAuthoritativeDnsServerCache();
         return new DnsNameResolver(
                 eventLoop,
                 channelFactory,
                 resolveCache,
+                cnameCache,
                 authoritativeDnsServerCache,
                 dnsQueryLifecycleObserverFactory,
                 queryTimeoutMillis,
@@ -449,6 +468,9 @@ public final class DnsNameResolverBuilder {
             copiedBuilder.resolveCache(resolveCache);
         }
 
+        if (cnameCache != null) {
+            copiedBuilder.cnameCache(cnameCache);
+        }
         if (maxTtl != null && minTtl != null) {
             copiedBuilder.ttl(minTtl, maxTtl);
         }

resolver-dns/src/main/java/io/netty/resolver/dns/NoopDnsCnameCache.java
@@ -1,0 +1,47 @@
+/*
+ * Copyright 2018 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.resolver.dns;
+
+import io.netty.channel.EventLoop;
+import io.netty.util.internal.UnstableApi;
+
+@UnstableApi
+public final class NoopDnsCnameCache implements DnsCnameCache {
+
+    public static final NoopDnsCnameCache INSTANCE = new NoopDnsCnameCache();
+
+    private NoopDnsCnameCache() { }
+
+    @Override
+    public String get(String hostname) {
+        return null;
+    }
+
+    @Override
+    public void cache(String hostname, String cname, long originalTtl, EventLoop loop) {
+        // NOOP
+    }
+
+    @Override
+    public void clear() {
+        // NOOP
+    }
+
+    @Override
+    public boolean clear(String hostname) {
+        return false;
+    }
+}

resolver-dns/src/test/java/io/netty/resolver/dns/DnsNameResolverTest.java
@@ -82,11 +82,13 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Queue;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static io.netty.handler.codec.dns.DnsRecordType.A;
@@ -2284,6 +2286,121 @@ public class DnsNameResolverTest {
             fail();
         } catch (Exception e) {
             assertSame(exception, e);
+        }
+    }
+
+    @Test
+    public void testCNameCached() throws Exception {
+        final Map<String, String> cache = new ConcurrentHashMap<String, String>();
+        final AtomicInteger cnameQueries = new AtomicInteger();
+        final AtomicInteger aQueries = new AtomicInteger();
+
+        TestDnsServer dnsServer2 = new TestDnsServer(new RecordStore() {
+
+            @Override
+            public Set<ResourceRecord> getRecords(QuestionRecord question) {
+                if ("cname.netty.io".equals(question.getDomainName())) {
+                    aQueries.incrementAndGet();
+
+                    return Collections.<ResourceRecord>singleton(new TestDnsServer.TestResourceRecord(
+                            question.getDomainName(), RecordType.A,
+                            Collections.<String, Object>singletonMap(
+                                    DnsAttribute.IP_ADDRESS.toLowerCase(), "10.0.0.99")));
+                }
+                if ("x.netty.io".equals(question.getDomainName())) {
+                    cnameQueries.incrementAndGet();
+
+                    return Collections.<ResourceRecord>singleton(new TestDnsServer.TestResourceRecord(
+                            question.getDomainName(), RecordType.CNAME,
+                            Collections.<String, Object>singletonMap(
+                                    DnsAttribute.DOMAIN_NAME.toLowerCase(), "cname.netty.io")));
+                }
+                if ("y.netty.io".equals(question.getDomainName())) {
+                    cnameQueries.incrementAndGet();
+
+                    return Collections.<ResourceRecord>singleton(new TestDnsServer.TestResourceRecord(
+                            question.getDomainName(), RecordType.CNAME,
+                            Collections.<String, Object>singletonMap(
+                                    DnsAttribute.DOMAIN_NAME.toLowerCase(), "x.netty.io")));
+                }
+                return Collections.emptySet();
+            }
+        });
+        dnsServer2.start();
+        DnsNameResolver resolver = null;
+        try {
+            DnsNameResolverBuilder builder = newResolver()
+                    .recursionDesired(true)
+                    .resolvedAddressTypes(ResolvedAddressTypes.IPV4_ONLY)
+                    .maxQueriesPerResolve(16)
+                    .nameServerProvider(new SingletonDnsServerAddressStreamProvider(dnsServer2.localAddress()))
+                    .resolveCache(NoopDnsCache.INSTANCE)
+                    .cnameCache(new DnsCnameCache() {
+                        @Override
+                        public String get(String hostname) {
+                            assertTrue(hostname, hostname.endsWith("."));
+                            return cache.get(hostname);
+                        }
+
+                        @Override
+                        public void cache(String hostname, String cname, long originalTtl, EventLoop loop) {
+                            assertTrue(hostname, hostname.endsWith("."));
+                            cache.put(hostname, cname);
+                        }
+
+                        @Override
+                        public void clear() {
+                            // NOOP
+                        }
+
+                        @Override
+                        public boolean clear(String hostname) {
+                            return false;
+                        }
+                    });
+            resolver = builder.build();
+            List<InetAddress> resolvedAddresses =
+                    resolver.resolveAll("x.netty.io").syncUninterruptibly().getNow();
+            assertEquals(1, resolvedAddresses.size());
+            assertTrue(resolvedAddresses.contains(InetAddress.getByAddress(new byte[] { 10, 0, 0, 99 })));
+
+            assertEquals("cname.netty.io.", cache.get("x.netty.io."));
+            assertEquals(1, cnameQueries.get());
+            assertEquals(1, aQueries.get());
+
+            resolvedAddresses =
+                    resolver.resolveAll("x.netty.io").syncUninterruptibly().getNow();
+            assertEquals(1, resolvedAddresses.size());
+            assertTrue(resolvedAddresses.contains(InetAddress.getByAddress(new byte[] { 10, 0, 0, 99 })));
+
+            // Should not have queried for the CNAME again.
+            assertEquals(1, cnameQueries.get());
+            assertEquals(2, aQueries.get());
+
+            resolvedAddresses =
+                    resolver.resolveAll("y.netty.io").syncUninterruptibly().getNow();
+            assertEquals(1, resolvedAddresses.size());
+            assertTrue(resolvedAddresses.contains(InetAddress.getByAddress(new byte[] { 10, 0, 0, 99 })));
+
+            assertEquals("x.netty.io.", cache.get("y.netty.io."));
+
+            // Will only query for one CNAME
+            assertEquals(2, cnameQueries.get());
+            assertEquals(3, aQueries.get());
+
+            resolvedAddresses =
+                    resolver.resolveAll("y.netty.io").syncUninterruptibly().getNow();
+            assertEquals(1, resolvedAddresses.size());
+            assertTrue(resolvedAddresses.contains(InetAddress.getByAddress(new byte[] { 10, 0, 0, 99 })));
+
+            // Should not have queried for the CNAME again.
+            assertEquals(2, cnameQueries.get());
+            assertEquals(4, aQueries.get());
+        } finally {
+            dnsServer2.stop();
+            if (resolver != null) {
+                resolver.close();
+            }
         }
     }
 }

commit 8f31dcf601235dc1b565dfc0370d828724b6ea56

Disable caching of CNAMEs to workaround resolving issues
Motivation:

In netty there is a seperate CNAME cache that is used to cache CNAMEs that are resolved during resolving an A / AAAA record. While this sounded like a great idea when I first implemented this in netty it turned out this is problematic and may produce errors while resolving hostnames later on.
The problem with the CNAME cache is that we may end up with entries in the CNAME cache wile the original cached resolution was already removed from the cache (due TTL). In this case we may find an entry in teh CNAME cache that we will then try to resolve via the specified nameserver. This resolution may fail as the nameserver may not return an entry for the previous cached CNAME. This usually happens when we receive the full "resolution chain" during query for an A / AAAA record.

This problem was introduced by https://github.com/netty/netty/pull/8314

Modifications:

- Use the NoopDnsCnameCache by default
- Deprecate methods that allow to set a custom DnsCnameCache
- Deprecate DnsCnameCache and all the implementations
- Add unit test

Result:

Fixes resolution problem caused by cached cname records

resolver-dns/src/main/java/io/netty/resolver/dns/DefaultDnsCnameCache.java
@@ -24,7 +24,9 @@ import static io.netty.util.internal.ObjectUtil.*;
 
 /**
  * Default implementation of a {@link DnsCnameCache}.
+ * @deprecated will be removed as caching CNAME's during resolution is considered problematic
  */
+@Deprecated
 public final class DefaultDnsCnameCache implements DnsCnameCache {
     private final int minTtl;
     private final int maxTtl;

resolver-dns/src/main/java/io/netty/resolver/dns/DnsCnameCache.java
@@ -19,7 +19,9 @@ import io.netty.channel.EventLoop;
 
 /**
  * A cache for {@code CNAME}s.
+ * @deprecated will be removed as caching CNAME's during resolution is considered problematic
  */
+@Deprecated
 public interface DnsCnameCache {
 
     /**

resolver-dns/src/main/java/io/netty/resolver/dns/DnsNameResolver.java
@@ -362,6 +362,7 @@ public class DnsNameResolver extends InetNameResolver {
              dnsServerAddressStreamProvider, searchDomains, ndots, decodeIdn, false);
     }
 
+    @SuppressWarnings("deprecation")
     DnsNameResolver(
             EventLoop eventLoop,
             ChannelFactory<? extends DatagramChannel> channelFactory,

resolver-dns/src/main/java/io/netty/resolver/dns/DnsNameResolverBuilder.java
@@ -160,7 +160,9 @@ public final class DnsNameResolverBuilder {
      *
      * @param cnameCache the cache used to cache {@code CNAME} mappings for a domain.
      * @return {@code this}
+     * @deprecated will be removed as caching CNAME's during resolution is considered problematic
      */
+    @Deprecated
     public DnsNameResolverBuilder cnameCache(DnsCnameCache cnameCache) {
         this.cnameCache  = cnameCache;
         return this;
@@ -431,9 +433,9 @@ public final class DnsNameResolverBuilder {
                 new NameServerComparator(DnsNameResolver.preferredAddressType(resolvedAddressTypes).addressType()));
     }
 
+    @SuppressWarnings("deprecation")
     private DnsCnameCache newCnameCache() {
-        return new DefaultDnsCnameCache(
-                intValue(minTtl, 0), intValue(maxTtl, Integer.MAX_VALUE));
+        return NoopDnsCnameCache.INSTANCE;
     }
 
     /**

resolver-dns/src/main/java/io/netty/resolver/dns/NoopDnsCnameCache.java
@@ -17,6 +17,10 @@ package io.netty.resolver.dns;
 
 import io.netty.channel.EventLoop;
 
+/**
+ * @deprecated will be removed as caching CNAME's during resolution is considered problematic
+ */
+@Deprecated
 public final class NoopDnsCnameCache implements DnsCnameCache {
 
     public static final NoopDnsCnameCache INSTANCE = new NoopDnsCnameCache();

resolver-dns/src/test/java/io/netty/resolver/dns/DnsNameResolverTest.java
@@ -2226,6 +2226,62 @@ public class DnsNameResolverTest {
     }
 
     @Test
+    public void testCNAMECachedWhenFullChainInResponse() throws IOException {
+        final String firstName = "firstname.com";
+        final String secondName = "secondname.com";
+        final String ipv4Addr = "1.2.3.4";
+        TestDnsServer dnsServer2 = new TestDnsServer(new RecordStore() {
+            @Override
+            public Set<ResourceRecord> getRecords(QuestionRecord question) {
+                if (question.getDomainName().equals(firstName)) {
+                    Set<ResourceRecord> records = new LinkedHashSet<ResourceRecord>();
+                    ResourceRecordModifier rm = new ResourceRecordModifier();
+                    rm.setDnsClass(RecordClass.IN);
+                    rm.setDnsName(question.getDomainName());
+                    rm.setDnsTtl(100);
+                    rm.setDnsType(RecordType.CNAME);
+                    rm.put(DnsAttribute.DOMAIN_NAME, secondName);
+                    records.add(rm.getEntry());
+
+                    rm = new ResourceRecordModifier();
+                    rm.setDnsClass(RecordClass.IN);
+                    rm.setDnsName(question.getDomainName());
+                    rm.setDnsTtl(100);
+                    rm.setDnsType(RecordType.A);
+                    rm.put(DnsAttribute.IP_ADDRESS, ipv4Addr);
+                    records.add(rm.getEntry());
+                    return records;
+                } else {
+                    return null;
+                }
+            }
+        });
+        dnsServer2.start();
+        DnsNameResolver resolver = null;
+        try {
+            DnsNameResolverBuilder builder = newResolver()
+                    .resolveCache(NoopDnsCache.INSTANCE)
+                    .recursionDesired(true)
+                    .maxQueriesPerResolve(16)
+                    .nameServerProvider(new SingletonDnsServerAddressStreamProvider(dnsServer2.localAddress()));
+            builder.resolvedAddressTypes(ResolvedAddressTypes.IPV4_PREFERRED);
+            resolver = builder.build();
+            InetAddress resolvedAddress = resolver.resolve(firstName).syncUninterruptibly().getNow();
+            assertEquals(ipv4Addr, resolvedAddress.getHostAddress());
+            assertEquals(firstName, resolvedAddress.getHostName());
+
+            resolvedAddress = resolver.resolve(firstName).syncUninterruptibly().getNow();
+            assertEquals(ipv4Addr, resolvedAddress.getHostAddress());
+            assertEquals(firstName, resolvedAddress.getHostName());
+        } finally {
+            dnsServer2.stop();
+            if (resolver != null) {
+                resolver.close();
+            }
+        }
+    }
+
+    @Test
     public void testSearchDomainQueryFailureForSingleAddressTypeCompletes() {
         expectedException.expect(UnknownHostException.class);
         testSearchDomainQueryFailureCompletes(ResolvedAddressTypes.IPV4_ONLY);