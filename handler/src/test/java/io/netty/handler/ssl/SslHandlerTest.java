1fe92e30772dc893ef08e6a307a3cc627c952449
Idel Pivnitskiy <ipivnitskiy@apple.com>
2020-05-18 05:25:04 -0700

    Do not require BoringSSL for testSessionTicketsWithTLSv12AndNoKey (#10301)
    Motivation:
    
    `SslHandlerTest.testSessionTicketsWithTLSv12AndNoKey` does not require
    BoringSSL and works with OpenSSL as well.
    
    Modifications:
    
    - Remove assume statement that expected BoringSSL;
    
    Result:
    
    Test works for any implementation of `OPENSSL` provider.

--- handler/src/test/java/io/netty/handler/ssl/SslHandlerTest.java
+++ handler/src/test/java/io/netty/handler/ssl/SslHandlerTest.java
@@ -1103,7 +1103,6 @@ public class SslHandlerTest {
 
     @Test(timeout = 5000L)
     public void testSessionTicketsWithTLSv12AndNoKey() throws Throwable {
-        assumeTrue(OpenSsl.isBoringSSL());
         testSessionTickets(SslUtils.PROTOCOL_TLS_V1_2, false);
     }
 
