5bea1e5aa0406c335d4e18395f05aeec299c09fa
Josef Grieb <josef.grieb@gmail.com>
2020-06-26 09:10:08 +0200

    Cleanup
    Motivation:
    licenses and netty dependencies are missing
    
    Modification:
    added licenses and netty dependencies
    
    Result:
    netty io_uring is buidable

--- transport-native-io_uring/src/main/java/io/netty/channel/uring/package-file.java
+++ transport-native-io_uring/src/main/java/io/netty/channel/uring/package-file.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 The Netty Project
+ * Copyright 2020 The Netty Project
  *
  * The Netty Project licenses this file to you under the Apache License,
  * version 2.0 (the "License"); you may not use this file except in compliance
@@ -13,7 +13,6 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-
 /**
  * io_uring
  */
