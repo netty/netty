bb184794341255cf02f9b7156c5ad828b172fdab
Norman Maurer <norman_maurer@apple.com>
2020-08-10 14:03:23 +0200

    Fix compilation error on JDK 15 (#10462)
    Motivation:
    
    AlgorithmId.sha256WithRSAEncryption_oid was removed in JDK15 and later so we should not depend on it as otherwise we will see compilation errors
    
    Modifications:
    
    Replace AlgorithmId.sha256WithRSAEncryption_oid usage with specify the OID directly
    
    Result:
    
    Compiles on JDK15+

--- handler/src/main/java/io/netty/handler/ssl/util/OpenJdkSelfSignedCertGenerator.java
+++ handler/src/main/java/io/netty/handler/ssl/util/OpenJdkSelfSignedCertGenerator.java
@@ -66,7 +66,8 @@ final class OpenJdkSelfSignedCertGenerator {
         info.set(X509CertInfo.VALIDITY, new CertificateValidity(notBefore, notAfter));
         info.set(X509CertInfo.KEY, new CertificateX509Key(keypair.getPublic()));
         info.set(X509CertInfo.ALGORITHM_ID,
-                new CertificateAlgorithmId(new AlgorithmId(AlgorithmId.sha256WithRSAEncryption_oid)));
+                // sha256WithRSAEncryption
+                new CertificateAlgorithmId(AlgorithmId.get("1.2.840.113549.1.1.11")));
 
         // Sign the cert to identify the algorithm that's used.
         X509CertImpl cert = new X509CertImpl(info);
