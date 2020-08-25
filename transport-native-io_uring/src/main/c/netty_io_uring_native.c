03c47b364b24ac4c1b37cbf5360f4b7be75285b8
Norman Maurer <norman_maurer@apple.com>
2020-08-25 18:39:21 +0200

    Correctly handle eventfd in io_uring
    Motivation:
    
    We use eventfd in our io_uring based transport to wakeup the eventloop. When doing so we need to be careful that we read any data previous written to it.
    
    Modification:
    
    - Correctly read data that was written to eventfd before submit another event related to it to the submission queue as otherwise we will see another completion event related to it asap
    - Ensure we not remove the wrong event from the storted event ids (we did remove the wrong before because we reused the Event object)
    - ensure we only use the submission queue from the EventLoop thread in all cases
    - add another unit test
    
    Result:
    
    Wakeups via eventfd work as expected

--- transport-native-io_uring/src/main/c/netty_io_uring_native.c
+++ transport-native-io_uring/src/main/c/netty_io_uring_native.c
@@ -201,7 +201,17 @@ static jint netty_io_uring_unregister_event_fd(JNIEnv *env, jclass class1, jint 
 	return 0;
 }
 
-static void netty_epoll_native_eventFdWrite(JNIEnv* env, jclass clazz, jint fd, jlong value) {
+
+static void netty_io_uring_eventFdRead(JNIEnv* env, jclass clazz, jint fd) {
+    uint64_t eventfd_t;
+
+    if (eventfd_read(fd, &eventfd_t) != 0) {
+        // something is serious wrong
+        netty_unix_errors_throwRuntimeException(env, "eventfd_read() failed");
+    }
+}
+
+static void netty_io_uring_eventFdWrite(JNIEnv* env, jclass clazz, jint fd, jlong value) {
     uint64_t val;
 
     for (;;) {
@@ -325,7 +335,8 @@ static const JNINativeMethod method_table[] = {
     {"createFile", "()I", (void *) netty_create_file},
     {"ioUringEnter", "(IIII)I", (void *)netty_io_uring_enter},
     {"eventFd", "()I", (void *) netty_epoll_native_eventFd},
-    {"eventFdWrite", "(IJ)V", (void *) netty_epoll_native_eventFdWrite },
+    {"eventFdWrite", "(IJ)V", (void *) netty_io_uring_eventFdWrite },
+    {"eventFdRead", "(I)V", (void *) netty_io_uring_eventFdRead },
     {"ioUringRegisterEventFd", "(II)I", (void *) netty_io_uring_register_event_fd},
     {"ioUringUnregisterEventFd", "(I)I", (void *) netty_io_uring_unregister_event_fd}
     };
