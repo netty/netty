d2c03c9a29ecb3ff5fb2d203e314fad88fa07509
Francesco Nigro <nigro.fra@gmail.com>
2020-08-31 10:32:33 +0200

    Improve MqttMessageType::valueOf cost (#10400)
    Motivation:
    
    MqttMessageType::valueOf has O(N) cost
    
    Modifications:
    
    MqttMessageType::valueOf uses a const lookup table
    
    Result:
    
    MqttMessageType::valueOf has O(1) cost

--- codec-mqtt/src/main/java/io/netty/handler/codec/mqtt/MqttMessageType.java
+++ codec-mqtt/src/main/java/io/netty/handler/codec/mqtt/MqttMessageType.java
@@ -36,6 +36,22 @@ public enum MqttMessageType {
     DISCONNECT(14),
     AUTH(15);
 
+    private static final MqttMessageType[] VALUES;
+
+    static {
+        // this prevent values to be assigned with the wrong order
+        // and ensure valueOf to work fine
+        final MqttMessageType[] values = values();
+        VALUES = new MqttMessageType[values.length + 1];
+        for (MqttMessageType mqttMessageType : values) {
+            final int value = mqttMessageType.value;
+            if (VALUES[value] != null) {
+                throw new AssertionError("value already in use: " + value);
+            }
+            VALUES[value] = mqttMessageType;
+        }
+    }
+
     private final int value;
 
     MqttMessageType(int value) {
@@ -47,12 +63,10 @@ public enum MqttMessageType {
     }
 
     public static MqttMessageType valueOf(int type) {
-        for (MqttMessageType t : values()) {
-            if (t.value == type) {
-                return t;
-            }
+        if (type <= 0 || type >= VALUES.length) {
+            throw new IllegalArgumentException("unknown message type: " + type);
         }
-        throw new IllegalArgumentException("unknown message type: " + type);
+        return VALUES[type];
     }
 }
 
