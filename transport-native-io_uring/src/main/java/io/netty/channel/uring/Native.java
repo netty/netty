4b54a7c1f7f7dcae4dea0ab6daae5c52b929d0b5
Norman Maurer <norman_maurer@apple.com>
2020-09-11 08:33:05 +0200

    Address comment

--- transport-native-io_uring/src/main/java/io/netty/channel/uring/Native.java
+++ transport-native-io_uring/src/main/java/io/netty/channel/uring/Native.java
@@ -30,7 +30,7 @@ import java.util.Locale;
 
 final class Native {
     private static final InternalLogger logger = InternalLoggerFactory.getInstance(Native.class);
-    static final int DEFAULT_RING_SIZE = SystemPropertyUtil.getInt("io.netty.uring.ringSize", 4096);
+    static final int DEFAULT_RING_SIZE = Math.max(64, SystemPropertyUtil.getInt("io.netty.uring.ringSize", 4096));
 
     static {
         Selector selector = null;
