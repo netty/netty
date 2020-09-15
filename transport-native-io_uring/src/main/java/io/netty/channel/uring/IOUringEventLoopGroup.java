3d85fe7a348fedaebe6177e840922650ae768d0b
Norman Maurer <norman_maurer@apple.com>
2020-09-15 10:59:03 +0200

    Allow to configure if IOSEQ_ASYNC is used per EventLoopGroup
    Motivation:
    
    There may be situations when the user dont want to use IOSEQ_ASYNC so we
    should allow to configure this
    
    Modifications:
    
    Make it configurable if IOSEQ_ASYNC should be used
    
    Result:
    
    More flexible configuration

--- transport-native-io_uring/src/main/java/io/netty/channel/uring/IOUringEventLoopGroup.java
+++ transport-native-io_uring/src/main/java/io/netty/channel/uring/IOUringEventLoopGroup.java
@@ -48,48 +48,48 @@ public final class IOUringEventLoopGroup extends MultithreadEventLoopGroup {
      * Create a new instance using the default number of threads and the given {@link ThreadFactory}.
      */
     public IOUringEventLoopGroup(ThreadFactory threadFactory) {
-        this(0, threadFactory, 0);
+        this(0, threadFactory, 0, Native.DEFAULT_USE_IOSEQ_ASYNC);
     }
 
     /**
      * Create a new instance using the specified number of threads and the given {@link ThreadFactory}.
      */
     public IOUringEventLoopGroup(int nThreads, ThreadFactory threadFactory) {
-        this(nThreads, threadFactory, 0);
+        this(nThreads, threadFactory, 0, Native.DEFAULT_USE_IOSEQ_ASYNC);
     }
 
     public IOUringEventLoopGroup(int nThreads, Executor executor) {
-        this(nThreads, executor, 0);
+        this(nThreads, executor, 0, Native.DEFAULT_USE_IOSEQ_ASYNC);
     }
 
     /**
      * Create a new instance using the specified number of threads, the given {@link ThreadFactory} and the given
      * maximal size of the used ringbuffer.
      */
-    public IOUringEventLoopGroup(int nThreads, ThreadFactory threadFactory, int ringSize) {
-        this(nThreads, threadFactory == null ? null : new ThreadPerTaskExecutor(threadFactory), ringSize);
+    public IOUringEventLoopGroup(int nThreads, ThreadFactory threadFactory, int ringSize, boolean iosqeAsync) {
+        this(nThreads, threadFactory == null ? null : new ThreadPerTaskExecutor(threadFactory), ringSize, iosqeAsync);
     }
 
-    public IOUringEventLoopGroup(int nThreads, Executor executor, int ringsize) {
+    public IOUringEventLoopGroup(int nThreads, Executor executor, int ringsize, boolean iosqeAsync) {
         this(nThreads, executor, DefaultEventExecutorChooserFactory.INSTANCE,
-                ringsize, RejectedExecutionHandlers.reject());
+                ringsize, iosqeAsync, RejectedExecutionHandlers.reject());
     }
 
     private IOUringEventLoopGroup(int nThreads, Executor executor, EventExecutorChooserFactory chooserFactory,
-                                  int ringSize, RejectedExecutionHandler rejectedExecutionHandler) {
-        this(nThreads, executor, chooserFactory, ringSize, rejectedExecutionHandler, null);
+                                  int ringSize, boolean iosqeAsync, RejectedExecutionHandler rejectedExecutionHandler) {
+        this(nThreads, executor, chooserFactory, ringSize, iosqeAsync, rejectedExecutionHandler, null);
     }
 
     private IOUringEventLoopGroup(int nThreads, Executor executor, EventExecutorChooserFactory chooserFactory,
-                                  int ringSize, RejectedExecutionHandler rejectedExecutionHandler,
+                                  int ringSize, boolean iosqeAsync, RejectedExecutionHandler rejectedExecutionHandler,
                                   EventLoopTaskQueueFactory queueFactory) {
-        super(nThreads, executor, chooserFactory, ringSize, rejectedExecutionHandler, queueFactory);
+        super(nThreads, executor, chooserFactory, ringSize, iosqeAsync, rejectedExecutionHandler, queueFactory);
     }
 
     //Todo
     @Override
     protected EventLoop newChild(Executor executor, Object... args) {
-        if (args.length != 3) {
+        if (args.length != 4) {
             throw new IllegalArgumentException("Illegal amount of extra arguments");
         }
         int ringSize = (Integer) args[0];
@@ -97,8 +97,9 @@ public final class IOUringEventLoopGroup extends MultithreadEventLoopGroup {
         if (ringSize == 0) {
             ringSize = Native.DEFAULT_RING_SIZE;
         }
-        RejectedExecutionHandler rejectedExecutionHandler = (RejectedExecutionHandler) args[1];
-        EventLoopTaskQueueFactory taskQueueFactory = (EventLoopTaskQueueFactory) args[2];
-        return new IOUringEventLoop(this, executor, ringSize, rejectedExecutionHandler, taskQueueFactory);
+        boolean iosqeAsync = (Boolean) args[1];
+        RejectedExecutionHandler rejectedExecutionHandler = (RejectedExecutionHandler) args[2];
+        EventLoopTaskQueueFactory taskQueueFactory = (EventLoopTaskQueueFactory) args[3];
+        return new IOUringEventLoop(this, executor, ringSize, iosqeAsync, rejectedExecutionHandler, taskQueueFactory);
     }
 }
