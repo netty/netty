db73538737cb60d89723a1c3f081a8e558e5b75b
Norman Maurer <norman_maurer@apple.com>
2020-09-25 08:36:39 +0200

    Include header files to allow building on older machines (#10609)
    Motivation:
    
    liburing ships its own iouring headers so its possible to build on older
    machines as well. We should do the same.
    
    Modifications:
    
    - Include header files and so not depend on kernel version
    - Fix license files and header for attribution
    
    Result:
    
    Be able to build easier

--- transport-native-io_uring/src/main/c/syscall.c
+++ transport-native-io_uring/src/main/c/syscall.c
@@ -13,6 +13,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
+  /* SPDX-License-Identifier: MIT */
 #include "syscall.h"
 #include <signal.h>
 #include <sys/syscall.h>
