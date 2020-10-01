1034422ec7d549d59796bc92bdf1e0bfc17a22ac
Aayush Atharva <aayush@shieldblaze.com>
2020-10-01 13:35:14 +0530

    improve encoder

--- codec/src/main/java/io/netty/handler/codec/compression/BrotliEncoder.java
+++ codec/src/main/java/io/netty/handler/codec/compression/BrotliEncoder.java
@@ -1,40 +1,53 @@
 package io.netty.handler.codec.compression;
 
+import com.nixxcode.jvmbrotli.common.BrotliLoader;
 import com.nixxcode.jvmbrotli.enc.BrotliOutputStream;
+import com.nixxcode.jvmbrotli.enc.Encoder;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufOutputStream;
 import io.netty.buffer.ByteBufUtil;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
+import io.netty.util.internal.logging.InternalLogger;
+import io.netty.util.internal.logging.InternalLoggerFactory;
 
 public class BrotliEncoder extends MessageToByteEncoder<ByteBuf> {
 
-    private final int totalLength;
-    private int writtenLength;
+    private static final InternalLogger logger = InternalLoggerFactory.getInstance(BrotliEncoder.class);
 
-    public BrotliEncoder(int totalLength) {
-        this.totalLength = totalLength;
+    static {
+        logger.info("Brotli Loader Status: {}", BrotliLoader.isBrotliAvailable());
     }
 
+    private final Encoder.Parameters params;
     private BrotliOutputStream compressorOS;
     private ByteBufOutputStream byteBufOutputStream;
+
+    public BrotliEncoder(int quality) {
+        this.params = new Encoder.Parameters().setQuality(quality);
+    }
 
     @Override
     protected void encode(ChannelHandlerContext ctx, ByteBuf msg, ByteBuf out) throws Exception {
         if (compressorOS == null) {
             byteBufOutputStream = new ByteBufOutputStream(out);
-            compressorOS = new BrotliOutputStream(byteBufOutputStream);
+            compressorOS = new BrotliOutputStream(byteBufOutputStream, params);
         }
 
-        writtenLength += msg.readableBytes();
-        compressorOS.write(ByteBufUtil.getBytes(msg));
-
-        if (writtenLength == totalLength) {
-            byteBufOutputStream.close();
-            compressorOS.close();
+        if (msg.hasArray()) {
+            compressorOS.write(msg.array());
         } else {
-            byteBufOutputStream.flush();
-            compressorOS.flush();
+            compressorOS.write(ByteBufUtil.getBytes(msg));
         }
+
+        byteBufOutputStream.flush();
+        compressorOS.flush();
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        super.handlerRemoved(ctx);
+        byteBufOutputStream.close();
+        compressorOS.close();
     }
 }
