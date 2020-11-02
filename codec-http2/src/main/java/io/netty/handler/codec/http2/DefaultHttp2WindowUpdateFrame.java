0c8403d202ac6b3bcdd6cde75f127936be62ca7a
Aayush Atharva <aayush@shieldblaze.com>
2020-11-02 16:58:16 +0530

    Swap positions

--- codec-http2/src/main/java/io/netty/handler/codec/http2/DefaultHttp2WindowUpdateFrame.java
+++ codec-http2/src/main/java/io/netty/handler/codec/http2/DefaultHttp2WindowUpdateFrame.java
@@ -49,6 +49,6 @@ public class DefaultHttp2WindowUpdateFrame extends AbstractHttp2StreamFrame impl
     @Override
     public String toString() {
         return StringUtil.simpleClassName(this) +
-                "(windowUpdateIncrement=" + windowUpdateIncrement + ", streamId=" + stream().id() + ')';
+                "(streamId=" + stream() + ", windowUpdateIncrement=" + windowUpdateIncrement + ')';
     }
 }
