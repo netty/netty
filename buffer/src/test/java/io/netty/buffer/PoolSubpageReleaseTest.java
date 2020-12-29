a177cc80cb555392553ab2892e6dcc839ab37d7b
zxingy <zxingy@servyou.com.cn>
2020-12-29 17:18:20 +0800

    add copyright header

--- buffer/src/test/java/io/netty/buffer/PoolSubpageReleaseTest.java
+++ buffer/src/test/java/io/netty/buffer/PoolSubpageReleaseTest.java
@@ -1,10 +1,31 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package io.netty.buffer;
 
 import org.junit.platform.commons.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
+
 import static org.junit.Assert.assertEquals;
 
 public class PoolSubpageReleaseTest {
+    public PoolSubpageReleaseTest() {
+    }
+
     public static void main(String[] args) throws Exception {
         // int elemSize = 1234;
         // int elemSize = 4096;
@@ -27,23 +48,26 @@ public class PoolSubpageReleaseTest {
         // by reflection acquire chunk.poolSubpages
         PoolSubpage<Object>[] subpages = byReflectionGetSubpages(fixByteBuf[0]);
 
-        // fixSubpagesCount ==  when ( PoolThreadCache.MemoryRegionCache.queue.offer(entry) == false ), the chunk.poolSubpages.size
+        // fixSubpagesCount ==  when ( PoolThreadCache.MemoryRegionCache.queue.offer(entry) == false )
+        // the chunk.poolSubpages.size
         int fixSubpagesCount = countSubpages(subpages);
 
         for (int i = 0; i < oneLength; i++) {
             ones[i] = ByteBufAllocator.DEFAULT.buffer(elemSize, elemSize);
         }
 
-        //first release the 'tows', in oder to filled of PoolThreadCache.MemoryRegionCache.queue, ( PoolThreadCache.MemoryRegionCache.queue.offer(entry) == false )
+        //first release the 'tows', in oder to filled of PoolThreadCache.MemoryRegionCache.queue
+        // ( PoolThreadCache.MemoryRegionCache.queue.offer(entry) == false )
         for (int i = 0; i < fixLength; i++) {
             fixByteBuf[i].release();
         }
         // release the 'ones'
         for (int i = 0; i < oneLength; i++) {
             ones[i].release();
-            System.out.println("subpages size: " + countSubpages(subpages));
+            // System.out.println("subpages size: " + countSubpages(subpages));
         }
-        // after release all but in PoolThreadCache.MemoryRegionCache.queue,  chunk.poolSubpages.size == fixSubpagesCount + 1
+        // after release all but in PoolThreadCache.MemoryRegionCache.queue
+        // chunk.poolSubpages.size == fixSubpagesCount + 1
         assertEquals(countSubpages(subpages), fixSubpagesCount + 1);
     }
 
@@ -61,7 +85,8 @@ public class PoolSubpageReleaseTest {
         Object chunk;
         try {
             chunk = ReflectionUtils.tryToReadFieldValue(PooledByteBuf.class.getDeclaredField("chunk"), one).get();
-            return (PoolSubpage[]) ReflectionUtils.tryToReadFieldValue(PoolChunk.class.getDeclaredField("subpages"), chunk).get();
+            Field subpagesField = PoolChunk.class.getDeclaredField("subpages");
+            return (PoolSubpage[]) ReflectionUtils.tryToReadFieldValue(subpagesField, chunk).get();
         } catch (Exception e) {
             e.printStackTrace();
         }
