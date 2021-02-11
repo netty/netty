3efaccbcddc88e255892bd4911ad2fce00defd99
Norman Maurer <norman_maurer@apple.com>
2021-02-11 21:35:36 +0100

    Add workflow to cut releases (#11019)
    Motivation:
    
    Doing releases manually is error-prone, it would be better if we could do it via a workflow
    
    Modification:
    
    - Add workflow to cut releases
    - Add related scripts
    
    Result:
    
    Be able to easily cut a release via a workflow

---
+++ .github/scripts/release_rollback.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+if [ "$#" -ne 3 ]; then
+    echo "Expected release.properties file, repository name and branch"
+    exit 1
+fi
+
+TAG=$(grep scm.tag= "$1" | cut -d'=' -f2)
+git remote set-url origin git@github.com:"$2".git
+git fetch
+git checkout "$3"
+mvn -B --file pom.xml release:rollback
+git push origin :"$TAG"
