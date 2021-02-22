a0b1984c50438e60703294aa684327a85caea350
Norman Maurer <norman_maurer@apple.com>
2021-02-22 08:04:22 +0100

    Update timeout to 10 minutes
    Motivation:
    
    We should use a higher timeout as sometimes the verification process in oss.sonatype.org is very slow.
    
    Modifications:
    
    Bump up timeout to 10 minutes
    
    Result:
    
    Less likely to see timeouts

--- scripts/finish_release.sh
+++ scripts/finish_release.sh
@@ -29,6 +29,6 @@ export JAVA_HOME="$JAVA8_HOME"
 
 ./mvnw -Psonatype-oss-release,full,uber-staging -pl all,tarball clean package gpg:sign org.sonatype.plugins:nexus-staging-maven-plugin:deploy -DstagingRepositoryId="$1" -DnexusUrl=https://oss.sonatype.org -DserverId=sonatype-nexus-staging -DskipTests=true
 
-./mvnw org.sonatype.plugins:nexus-staging-maven-plugin:rc-close org.sonatype.plugins:nexus-staging-maven-plugin:rc-release -DstagingRepositoryId="$1" -DnexusUrl=https://oss.sonatype.org -DserverId=sonatype-nexus-staging -DskipTests=true
+./mvnw org.sonatype.plugins:nexus-staging-maven-plugin:rc-close org.sonatype.plugins:nexus-staging-maven-plugin:rc-release -DstagingRepositoryId="$1" -DnexusUrl=https://oss.sonatype.org -DserverId=sonatype-nexus-staging -DskipTests=true -DstagingProgressTimeoutMinutes=10
 
 git checkout "$BRANCH"
