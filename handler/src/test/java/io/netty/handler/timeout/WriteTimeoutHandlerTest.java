2ce03e0a08ea6ab534bbbb8d2b2f8a272d908042
Norman Maurer <norman_maurer@apple.com>
2021-03-04 15:27:02 +0100

    Fix NPE that can happen in the WriteTimeoutHandler when multiple executors are used (#11056)
    Motivation:
    
    In WriteTimeoutHandler we did make the assumption that the executor which is used to schedule the timeout is the same that is backing the write promise. This may not be true which will cause concurrency issues
    
    Modifications:
    
    Ensure we are on the right thread when try to modify the doubly-linked-list and if not schedule it on the right thread.
    
    Result:
    
    Fixes https://github.com/netty/netty/issues/11053

---
+++ handler/src/test/java/io/netty/handler/timeout/WriteTimeoutHandlerTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2021 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.timeout;
+
+import io.netty.channel.DefaultChannelPromise;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.util.concurrent.DefaultEventExecutorGroup;
+import io.netty.util.concurrent.EventExecutorGroup;
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+
+import static org.junit.Assert.assertTrue;
+
+public class WriteTimeoutHandlerTest {
+
+    @Test
+    public void testPromiseUseDifferentExecutor() throws Exception {
+        EventExecutorGroup group1 = new DefaultEventExecutorGroup(1);
+        EmbeddedChannel channel = new EmbeddedChannel(false, false);
+        try {
+            channel.pipeline().addLast(new WriteTimeoutHandler(10000));
+            final CountDownLatch latch = new CountDownLatch(1);
+            channel.register();
+            channel.writeAndFlush("something", new DefaultChannelPromise(channel, group1.next())).addListener(f -> {
+                latch.countDown();
+            });
+
+            latch.await();
+            assertTrue(channel.finishAndReleaseAll());
+        } finally {
+            group1.shutdownGracefully();
+        }
+    }
+}
