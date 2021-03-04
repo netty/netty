bfea65ef5280d17de2a758a786cbc217b2272dcb
Chris Vest <christianvest_hansen@apple.com>
2021-03-04 18:20:12 +0100

    Remove support for JZlib (#11058)
    Motivation:
    This library is obsolete; hasn't been updated since 2013.
    
    Modification:
    Remove jzlib dependency, integration code and tests.
    
    Result:
    - No more jzlib support.
    - Less code.
    - The JdkZlib* code can now be simplified because it no longer share anything with jzlib.

--- codec/src/main/java/io/netty/handler/codec/compression/ZlibCodecFactory.java
+++ codec/src/main/java/io/netty/handler/codec/compression/ZlibCodecFactory.java
@@ -15,32 +15,11 @@
  */
 package io.netty.handler.codec.compression;
 
-import io.netty.util.internal.SystemPropertyUtil;
-import io.netty.util.internal.logging.InternalLogger;
-import io.netty.util.internal.logging.InternalLoggerFactory;
-
 /**
  * Creates a new {@link ZlibEncoder} and a new {@link ZlibDecoder}.
  */
 public final class ZlibCodecFactory {
-    private static final InternalLogger logger = InternalLoggerFactory.getInstance(ZlibCodecFactory.class);
-
-    private static final int DEFAULT_JDK_WINDOW_SIZE = 15;
-    private static final int DEFAULT_JDK_MEM_LEVEL = 8;
-
-    private static final boolean noJdkZlibDecoder;
-    private static final boolean noJdkZlibEncoder;
-    private static final boolean supportsWindowSizeAndMemLevel;
-
-    static {
-        noJdkZlibDecoder = SystemPropertyUtil.getBoolean("io.netty.noJdkZlibDecoder", false);
-        logger.debug("-Dio.netty.noJdkZlibDecoder: {}", noJdkZlibDecoder);
-
-        noJdkZlibEncoder = SystemPropertyUtil.getBoolean("io.netty.noJdkZlibEncoder", false);
-        logger.debug("-Dio.netty.noJdkZlibEncoder: {}", noJdkZlibEncoder);
-
-        supportsWindowSizeAndMemLevel = true;
-    }
+    private static final boolean supportsWindowSizeAndMemLevel = true;
 
     /**
      * Returns {@code true} if specify a custom window size and mem level is supported.
@@ -50,85 +29,43 @@ public final class ZlibCodecFactory {
     }
 
     public static ZlibEncoder newZlibEncoder(int compressionLevel) {
-        if (noJdkZlibEncoder) {
-            return new JZlibEncoder(compressionLevel);
-        } else {
-            return new JdkZlibEncoder(compressionLevel);
-        }
+        return new JdkZlibEncoder(compressionLevel);
     }
 
     public static ZlibEncoder newZlibEncoder(ZlibWrapper wrapper) {
-        if (noJdkZlibEncoder) {
-            return new JZlibEncoder(wrapper);
-        } else {
-            return new JdkZlibEncoder(wrapper);
-        }
+        return new JdkZlibEncoder(wrapper);
     }
 
     public static ZlibEncoder newZlibEncoder(ZlibWrapper wrapper, int compressionLevel) {
-        if (noJdkZlibEncoder) {
-            return new JZlibEncoder(wrapper, compressionLevel);
-        } else {
-            return new JdkZlibEncoder(wrapper, compressionLevel);
-        }
+        return new JdkZlibEncoder(wrapper, compressionLevel);
     }
 
     public static ZlibEncoder newZlibEncoder(ZlibWrapper wrapper, int compressionLevel, int windowBits, int memLevel) {
-        if (noJdkZlibEncoder ||
-                windowBits != DEFAULT_JDK_WINDOW_SIZE || memLevel != DEFAULT_JDK_MEM_LEVEL) {
-            return new JZlibEncoder(wrapper, compressionLevel, windowBits, memLevel);
-        } else {
-            return new JdkZlibEncoder(wrapper, compressionLevel);
-        }
+        return new JdkZlibEncoder(wrapper, compressionLevel);
     }
 
     public static ZlibEncoder newZlibEncoder(byte[] dictionary) {
-        if (noJdkZlibEncoder) {
-            return new JZlibEncoder(dictionary);
-        } else {
-            return new JdkZlibEncoder(dictionary);
-        }
+        return new JdkZlibEncoder(dictionary);
     }
 
     public static ZlibEncoder newZlibEncoder(int compressionLevel, byte[] dictionary) {
-        if (noJdkZlibEncoder) {
-            return new JZlibEncoder(compressionLevel, dictionary);
-        } else {
-            return new JdkZlibEncoder(compressionLevel, dictionary);
-        }
+        return new JdkZlibEncoder(compressionLevel, dictionary);
     }
 
     public static ZlibEncoder newZlibEncoder(int compressionLevel, int windowBits, int memLevel, byte[] dictionary) {
-        if (noJdkZlibEncoder ||
-                windowBits != DEFAULT_JDK_WINDOW_SIZE || memLevel != DEFAULT_JDK_MEM_LEVEL) {
-            return new JZlibEncoder(compressionLevel, windowBits, memLevel, dictionary);
-        } else {
-            return new JdkZlibEncoder(compressionLevel, dictionary);
-        }
+        return new JdkZlibEncoder(compressionLevel, dictionary);
     }
 
     public static ZlibDecoder newZlibDecoder() {
-        if (noJdkZlibDecoder) {
-            return new JZlibDecoder();
-        } else {
-            return new JdkZlibDecoder(true);
-        }
+        return new JdkZlibDecoder(true);
     }
 
     public static ZlibDecoder newZlibDecoder(ZlibWrapper wrapper) {
-        if (noJdkZlibDecoder) {
-            return new JZlibDecoder(wrapper);
-        } else {
-            return new JdkZlibDecoder(wrapper, true);
-        }
+        return new JdkZlibDecoder(wrapper, true);
     }
 
     public static ZlibDecoder newZlibDecoder(byte[] dictionary) {
-        if (noJdkZlibDecoder) {
-            return new JZlibDecoder(dictionary);
-        } else {
-            return new JdkZlibDecoder(dictionary);
-        }
+        return new JdkZlibDecoder(dictionary);
     }
 
     private ZlibCodecFactory() {
