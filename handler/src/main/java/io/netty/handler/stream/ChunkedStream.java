b09e749c0e386b862eff72e23d0b5d8d6243805d
Chris Vest <christianvest_hansen@apple.com>
2021-04-09 12:54:15 +0200

    Less blocking in ChunkedStream
    Motivation:
    We should avoid blocking in the event loop as much as possible.
    The InputStream.read() is a blocking method, and we don't need to call it if available() returns a positive number.
    
    Modification:
    Bypass calling InputStream.read() if available() returns a positive number.
    
    Result:
    Fewer blocking calls in the event loop, in general, when ChunkedStream is used.
    
    Fixes #11147

--- handler/src/main/java/io/netty/handler/stream/ChunkedStream.java
+++ handler/src/main/java/io/netty/handler/stream/ChunkedStream.java
@@ -79,6 +79,9 @@ public class ChunkedStream implements ChunkedInput<ByteBuf> {
         if (closed) {
             return true;
         }
+        if (in.available() > 0) {
+            return false;
+        }
 
         int b = in.read();
         if (b < 0) {
