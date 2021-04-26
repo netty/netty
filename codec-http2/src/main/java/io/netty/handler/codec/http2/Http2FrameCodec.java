29e851987f7fc9f67d0c15a6e6586227f7ca516d
NiteshKant <nitesh_kant@apple.com>
2021-04-26 13:26:26 -0700

    Improve `Http2FrameCodecBuilder` usability
    __Motivation__
    
     `Http2FrameCodecBuilder` constructor calls `server()` internally which disallows using certain methods on the builder later. Additionally, the constructor is package private which limits extension of the `Builder` as well as usage outside the available `forClient()` and `forServer()` static methods.
    
     __Modification__
    
     - Introduce a `public` no-arg constructor to `Http2FrameCodecBuilder`.
     - Increase visibility of `Http2FrameCodec` constructor to `protected` to allow extensions of `Builder` to instantiate the codec.
    
     __Result__
    
     `Http2FrameCodecBuilder` can now be used to create the codec with `Http2Connection` or `Http2ConnectionDecoder` and `Http2ConnectionEncoder` which was earlier not possible due to implicit call to `server()` by the `Http2FrameCodecBuilder` constructor.

--- codec-http2/src/main/java/io/netty/handler/codec/http2/Http2FrameCodec.java
+++ codec-http2/src/main/java/io/netty/handler/codec/http2/Http2FrameCodec.java
@@ -163,8 +163,8 @@ public class Http2FrameCodec extends Http2ConnectionHandler {
     private final IntObjectMap<DefaultHttp2FrameStream> frameStreamToInitializeMap =
             new IntObjectHashMap<DefaultHttp2FrameStream>(8);
 
-    Http2FrameCodec(Http2ConnectionEncoder encoder, Http2ConnectionDecoder decoder, Http2Settings initialSettings,
-                    boolean decoupleCloseAndGoAway) {
+    protected Http2FrameCodec(Http2ConnectionEncoder encoder, Http2ConnectionDecoder decoder,
+                              Http2Settings initialSettings, boolean decoupleCloseAndGoAway) {
         super(decoder, encoder, initialSettings, decoupleCloseAndGoAway);
 
         decoder.frameListener(new FrameListener());
