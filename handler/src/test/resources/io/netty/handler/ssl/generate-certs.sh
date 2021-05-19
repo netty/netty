f0c919768b1aeafd3ed32caa91a135e30824051d
Norman Maurer <norman_maurer@apple.com>
2021-05-19 14:06:42 +0200

    Add license header to our scripts and workflows (#11282)
    Motivation:
    
    We should have license header whenever possible.
    
    Modifications:
    
    Add header to scripts and workflow config
    
    Result:
    
    More clear licensing

--- handler/src/test/resources/io/netty/handler/ssl/generate-certs.sh
+++ handler/src/test/resources/io/netty/handler/ssl/generate-certs.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+# ----------------------------------------------------------------------------
+# Copyright 2021 The Netty Project
+#
+# The Netty Project licenses this file to you under the Apache License,
+# version 2.0 (the "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at:
+#
+#   https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+# ----------------------------------------------------------------------------
 
 # Generate a new, self-signed root CA
 openssl req -extensions v3_ca -new -x509 -days 30 -nodes -subj "/CN=NettyTestRoot" -newkey rsa:2048 -sha512 -out mutual_auth_ca.pem -keyout mutual_auth_ca.key
