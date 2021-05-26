a1e672b5f5b98e0b6730571884409becc12d995e
Riley Park <riley.park@meino.net>
2021-05-26 01:17:27 -0700

    Migrate codec-stomp tests to JUnit 5 (#11312)
    Motivation:
    
    JUnit 5 is more expressive, extensible, and composable in many ways, and it's better able to run tests in parallel.
    
    Modifications:
    
    Use JUnit5 in codec-stomp tests
    
    Result:
    
    Related to https://github.com/netty/netty/issues/10757

--- codec-stomp/src/test/java/io/netty/handler/codec/stomp/DefaultStompFrameTest.java
+++ codec-stomp/src/test/java/io/netty/handler/codec/stomp/DefaultStompFrameTest.java
@@ -16,12 +16,12 @@
 package io.netty.handler.codec.stomp;
 
 import io.netty.util.AsciiString;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DefaultStompFrameTest {
 
