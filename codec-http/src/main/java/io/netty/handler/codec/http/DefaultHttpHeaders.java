02c3712067418cd119a3ef83be7eb1ee60d99852
skyguard1 <qhdxssm@qq.com>
2021-05-28 14:47:53 +0800

    Add default block in DefaultHttpHeaders (#11329)
    Motivation:
    
    Every switch block should also have a default case.
    
    Modification:
    
    Add default block in DefaultHttpHeaders to ensure we not fall-through by mistake
    
    Result:
    
    Cleanup
    
    Signed-off-by: xingrufei <xingrufei@sogou-inc.com>

--- codec-http/src/main/java/io/netty/handler/codec/http/DefaultHttpHeaders.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/DefaultHttpHeaders.java
@@ -464,6 +464,8 @@ public class DefaultHttpHeaders extends HttpHeaders {
                     throw new IllegalArgumentException("a header value contains a prohibited character '\\v': " + seq);
                 case '\f':
                     throw new IllegalArgumentException("a header value contains a prohibited character '\\f': " + seq);
+                default:
+                    break;
                 }
             }
 
@@ -475,6 +477,8 @@ public class DefaultHttpHeaders extends HttpHeaders {
                             return 1;
                         case '\n':
                             return 2;
+                        default:
+                            break;
                     }
                     break;
                 case 1:
@@ -490,6 +494,8 @@ public class DefaultHttpHeaders extends HttpHeaders {
                         default:
                             throw new IllegalArgumentException("only ' ' and '\\t' are allowed after '\\n': " + seq);
                     }
+                default:
+                    break;
             }
             return state;
         }
