bf12a17c22107126c424d5ff8759cabfe0c4fb98
xingrufei <xingrufei@sogou-inc.com>
2021-06-02 14:50:20 +0800

    Move static import to a separate place
    Signed-off-by: xingrufei <xingrufei@sogou-inc.com>

--- handler/src/test/java/io/netty/handler/ipfilter/IpSubnetFilterTest.java
+++ handler/src/test/java/io/netty/handler/ipfilter/IpSubnetFilterTest.java
@@ -23,14 +23,15 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.util.internal.SocketUtils;
 import org.junit.Assert;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class IpSubnetFilterTest {
 
