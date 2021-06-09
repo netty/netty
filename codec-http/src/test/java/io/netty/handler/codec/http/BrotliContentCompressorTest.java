bcfc31c694c7e421b205f9310bff59b6155eb927
Aayush Atharva <aayush@shieldblaze.com>
2021-06-09 12:01:30 +0530

    Finish BrotliContentCompressorTest

--- codec-http/src/test/java/io/netty/handler/codec/http/BrotliContentCompressorTest.java
+++ codec-http/src/test/java/io/netty/handler/codec/http/BrotliContentCompressorTest.java
@@ -15,8 +15,14 @@
  */
 package io.netty.handler.codec.http;
 
+import io.netty.channel.embedded.EmbeddedChannel;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BrotliContentCompressorTest {
@@ -33,9 +39,7 @@ public class BrotliContentCompressorTest {
                 "br", "br",
                 "compress, br;q=0.5", "br",
                 "br; q=0.5, identity", "br",
-                "br ; q=0.1", "br",
                 "br; q=0, deflate", "br",
-                "br ; q=0 , *;q=0.5", "br",
         };
         for (int i = 0; i < tests.length; i += 2) {
             String acceptEncoding = tests[i];
@@ -43,5 +47,41 @@ public class BrotliContentCompressorTest {
             String targetEncoding = compressor.determineEncoding(acceptEncoding);
             assertEquals(contentEncoding, targetEncoding);
         }
+    }
+
+    @Test
+    public void testAcceptEncodingHttpRequest() {
+        EmbeddedChannel ch = new EmbeddedChannel(new BrotliContentCompressor());
+        ch.writeInbound(newRequest());
+        FullHttpRequest fullHttpRequest = ch.readInbound();
+        fullHttpRequest.release();
+
+        HttpResponse res = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+        res.headers().set(HttpHeaderNames.TRANSFER_ENCODING, HttpHeaderValues.CHUNKED);
+        ch.writeOutbound(res);
+
+        assertEncodedResponse(ch);
+
+        assertTrue(ch.close().isSuccess());
+    }
+
+    private static void assertEncodedResponse(EmbeddedChannel ch) {
+        Object o = ch.readOutbound();
+        assertThat(o, is(instanceOf(HttpResponse.class)));
+
+        assertEncodedResponse((HttpResponse) o);
+    }
+
+    private static void assertEncodedResponse(HttpResponse res) {
+        assertThat(res, is(not(instanceOf(HttpContent.class))));
+        assertThat(res.headers().get(HttpHeaderNames.TRANSFER_ENCODING), is("chunked"));
+        assertThat(res.headers().get(HttpHeaderNames.CONTENT_LENGTH), is(nullValue()));
+        assertThat(res.headers().get(HttpHeaderNames.CONTENT_ENCODING), is("br"));
+    }
+
+    private static FullHttpRequest newRequest() {
+        FullHttpRequest req = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/");
+        req.headers().set(HttpHeaderNames.ACCEPT_ENCODING, "br, gzip, deflate");
+        return req;
     }
 }
