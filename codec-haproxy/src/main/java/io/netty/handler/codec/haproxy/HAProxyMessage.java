b1288bb26e14de1096192f0a1643008dff9deac6
Adrian Antkowiak <adiantek@gmail.com>
2021-07-10 10:03:00 +0200

    HAProxyMessage.sourceAddress() can be null
    It happens if and only if we've `check` && `send-proxy-v2`. Changing `send-proxy-v2` to `send-proxy` makes no error in the console, removing `check` too.

--- codec-haproxy/src/main/java/io/netty/handler/codec/haproxy/HAProxyMessage.java
+++ codec-haproxy/src/main/java/io/netty/handler/codec/haproxy/HAProxyMessage.java
@@ -502,6 +502,7 @@ public final class HAProxyMessage extends AbstractReferenceCounted {
 
     /**
      * Returns the human-readable source address of this {@link HAProxyMessage}.
+     * Returns null if HAProxy performs health check with send-proxy-v2
      */
     public String sourceAddress() {
         return sourceAddress;
