7ec198ddf9480d77ca8557649fd5f45701af6657
Aayush Atharva <aayush@shieldblaze.com>
2021-07-13 19:54:58 +0530

    Fix JavaDoc and Run Brotli tests on Windows (#11468)
    Motivation:
    JavaDoc of StandardCompressionOptions should point towards public methods. Also, Brotli tests were failing on Windows.
    
    Modification:
    Fixed JavaDoc and enabled Brotli tests on Windows.
    
    Result:
    Better JavaDoc and Brotli tests will run on Windows
    
    Co-authored-by: Norman Maurer <norman_maurer@apple.com>

--- codec/src/main/java/io/netty/handler/codec/compression/GzipOptions.java
+++ codec/src/main/java/io/netty/handler/codec/compression/GzipOptions.java
@@ -23,30 +23,14 @@ package io.netty.handler.codec.compression;
 public final class GzipOptions extends DeflateOptions {
 
     /**
-     * Default implementation of {@link GzipOptions} with
-     * {@link #compressionLevel()} set to 6, {@link #windowBits()} set to 15
-     * and {@link #memLevel()} set to 8.
+     * @see StandardCompressionOptions#gzip()
      */
     static final GzipOptions DEFAULT = new GzipOptions(
             6, 15, 8
     );
 
     /**
-     * Create a new {@link GzipOptions} Instance
-     *
-     * @param compressionLevel {@code 1} yields the fastest compression and {@code 9} yields the
-     *                         best compression.  {@code 0} means no compression.  The default
-     *                         compression level is {@code 6}.
-     *
-     * @param windowBits       The base two logarithm of the size of the history buffer.  The
-     *                         value should be in the range {@code 9} to {@code 15} inclusive.
-     *                         Larger values result in better compression at the expense of
-     *                         memory usage.  The default value is {@code 15}.
-     *
-     * @param memLevel         How much memory should be allocated for the internal compression
-     *                         state.  {@code 1} uses minimum memory and {@code 9} uses maximum
-     *                         memory.  Larger values result in better and faster compression
-     *                         at the expense of memory usage.  The default value is {@code 8}
+     * @see StandardCompressionOptions#gzip(int, int, int)
      */
     GzipOptions(int compressionLevel, int windowBits, int memLevel) {
         super(compressionLevel, windowBits, memLevel);
