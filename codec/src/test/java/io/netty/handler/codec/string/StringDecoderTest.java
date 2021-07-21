a1f13e17dbe266ab3baa9a26979b60a0cf8af42e
skyguard1 <qhdxssm@qq.com>
2021-07-21 20:31:14 +0800

    Add StringDecoder test (#11496)
    Motivation:
    
    There is no test case of `StringDecoder` here
    
    Modification:
    
    Need to add `StringDecoder` test case
    
    Result:
    
    Added test case of `StringDecoder`
    
    Signed-off-by: xingrufei <xingrufei@sogou-inc.com>
    Co-authored-by: xingrufei <xingrufei@sogou-inc.com>
    Co-authored-by: Norman Maurer <norman_maurer@apple.com>

--- codec/src/test/java/io/netty/handler/codec/string/StringDecoderTest.java
+++ codec/src/test/java/io/netty/handler/codec/string/StringDecoderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 The Netty Project
+ * Copyright 2021 The Netty Project
  *
  * The Netty Project licenses this file to you under the Apache License,
  * version 2.0 (the "License"); you may not use this file except in compliance
@@ -16,27 +16,27 @@
 package io.netty.handler.codec.string;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.util.CharsetUtil;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-public class StringEncoderTest {
+public class StringDecoderTest {
 
     @Test
-    public void testEncode() {
-        String msg = "Test";
-        EmbeddedChannel channel = new EmbeddedChannel(new StringEncoder());
-        Assertions.assertTrue(channel.writeOutbound(msg));
-        Assertions.assertTrue(channel.finish());
-        ByteBuf buf = channel.readOutbound();
-        byte[] data = new byte[buf.readableBytes()];
-        buf.readBytes(data);
-        Assertions.assertArrayEquals(msg.getBytes(CharsetUtil.UTF_8), data);
-        Assertions.assertNull(channel.readOutbound());
-        buf.release();
+    public void testDecode() {
+        String msg = "abc123";
+        ByteBuf byteBuf = Unpooled.copiedBuffer(msg, CharsetUtil.UTF_8);
+        EmbeddedChannel channel = new EmbeddedChannel(new StringDecoder());
+        assertTrue(channel.writeInbound(byteBuf));
+        String result = channel.readInbound();
+        assertEquals(msg, result);
+        assertNull(channel.readInbound());
         assertFalse(channel.finish());
     }
 }
