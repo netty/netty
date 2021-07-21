f12e20dc4653ef3bf8acba34f3e8ec4d0ccc4b7e
Norman Maurer <norman_maurer@apple.com>
2021-07-21 10:11:43 +0200

    Update codec/src/test/java/io/netty/handler/codec/string/StringDecoderTest.java

--- codec/src/test/java/io/netty/handler/codec/string/StringDecoderTest.java
+++ codec/src/test/java/io/netty/handler/codec/string/StringDecoderTest.java
@@ -22,6 +22,7 @@ import io.netty.util.CharsetUtil;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
