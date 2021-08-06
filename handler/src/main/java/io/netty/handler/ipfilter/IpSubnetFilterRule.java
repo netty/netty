a3c4c9ebfda456fe7887277d4c0765b0f54dadc6
Aayush Atharva <aayush@shieldblaze.com>
2021-08-06 12:46:32 +0530

    Simplify Bitwise operations (#11547)
    Motivation:
    We should keep bitwise operations simple and easy to understand.
    
    Modification:
    Simplify few Bitwise operations.
    
    Result:
    Less complicated bitwise operation code

--- handler/src/main/java/io/netty/handler/ipfilter/IpSubnetFilterRule.java
+++ handler/src/main/java/io/netty/handler/ipfilter/IpSubnetFilterRule.java
@@ -167,7 +167,7 @@ public final class IpSubnetFilterRule implements IpFilterRule, Comparable<IpSubn
              *
              * Also see https://github.com/netty/netty/issues/2767
              */
-            return (int) ((-1L << 32 - cidrPrefix) & 0xffffffff);
+            return (int) (-1L << 32 - cidrPrefix);
         }
     }
 
