a0b535b496aa39455741f534c93e8dc2bcc0d0d7
Chris Vest <christianvest_hansen@apple.com>
2021-08-23 11:08:58 +0200

    Turn the PromiseOperator interface into a Futures class with static methods, and remove the map/flatMap instance methods from Future

--- common/src/main/java/io/netty/util/concurrent/Future.java
+++ common/src/main/java/io/netty/util/concurrent/Future.java
@@ -274,51 +274,6 @@ public interface Future<V> extends java.util.concurrent.Future<V> {
     boolean cancel(boolean mayInterruptIfRunning);
 
     /**
-     * Create a new {@link Future} that will complete with the result of this {@link Future} mapped through the given
-     * mapper function.
-     * <p>
-     * If this future fails, then the returned future will fail as well, with the same exception. Cancellation of either
-     * future will cancel the other. If the mapper function throws, the returned future will fail, but this future will
-     * be unaffected.
-     *
-     * @param mapper The function that will convert the result of this future into the result of the returned future.
-     * @param <R>    The result type of the mapper function, and of the returned future.
-     * @return A new future instance that will complete with the mapped result of this future.
-     */
-    default <R> Future<R> map(Function<V, R> mapper) {
-        Promise<R> promise = executor().newPromise();
-        addListener(promise, PromiseOperator.map(mapper));
-        promise.addListener(this, PromiseNotifier::propagateCancel);
-        return promise;
-    }
-
-    /**
-     * Create a new {@link Future} that will complete with the result of this {@link Future} flat-mapped through the
-     * given mapper function.
-     * <p>
-     * The "flat" in "flat-map" means the given mapper function produces a result that itself is a future-of-R, yet this
-     * method also returns a future-of-R, rather than a future-of-future-of-R. In other words, if the same mapper
-     * function was used with the {@link #map(Function)} method, you would get back a {@code Future<Future<R>>}. These
-     * nested futures are "flattened" into a {@code Future<R>} by this method. Note that the future returned by this
-     * method is not the same instance as the one the mapper function returns. The reason is that this method needs to
-     * return immediately, but the mapper function cannot be applied before this future has completed.
-     * <p>
-     * If this future fails, then the returned future will fail as well, with the same exception. Cancellation of either
-     * future will cancel the other. If the mapper function throws, the returned future will fail, but this future will
-     * be unaffected.
-     *
-     * @param mapper The function that will convert the result of this future into the result of the returned future.
-     * @param <R>    The result type of the mapper function, and of the returned future.
-     * @return A new future instance that will complete with the mapped result of this future.
-     */
-    default <R> Future<R> flatMap(Function<V, Future<R>> mapper) {
-        Promise<R> promise = executor().newPromise();
-        addListener(promise, PromiseOperator.flatMap(mapper));
-        promise.addListener(this, PromiseNotifier::propagateCancel);
-        return promise;
-    }
-
-    /**
      * Returns the {@link EventExecutor} that is tied to this {@link Future}.
      */
     EventExecutor executor();
---
+++ common/src/main/java/io/netty/util/concurrent/Futures.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright 2021 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.util.concurrent;
+
+import io.netty.util.internal.logging.InternalLogger;
+import io.netty.util.internal.logging.InternalLoggerFactory;
+
+import java.util.Objects;
+import java.util.function.Function;
+
+import static io.netty.util.internal.PromiseNotificationUtil.tryCancel;
+import static io.netty.util.internal.PromiseNotificationUtil.tryFailure;
+
+/**
+ * Combinator operations on {@linkplain Future futures}.
+ *
+ * @implNote The operations themselves are implemented as static inner classes instead of lambdas to aid debugging.
+ */
+public final class Futures {
+    private static final InternalLogger LOGGER = InternalLoggerFactory.getInstance(Futures.class);
+    private static final PassThrough<?> PASS_THROUGH_CONSTANT = new PassThrough<Object>();
+
+    /**
+     * Create a new {@link Future} that will complete with the result of this {@link Future} mapped through the given
+     * mapper function.
+     * <p>
+     * If this future fails, then the returned future will fail as well, with the same exception. Cancellation of either
+     * future will cancel the other. If the mapper function throws, the returned future will fail, but this future will
+     * be unaffected.
+     *
+     * @param mapper The function that will convert the result of this future into the result of the returned future.
+     * @param <R>    The result type of the mapper function, and of the returned future.
+     * @return A new future instance that will complete with the mapped result of this future.
+     */
+    public static <V, R> Future<R> map(Future<V> future, Function<V, R> mapper) {
+        Objects.requireNonNull(future, "The future cannot be null.");
+        Objects.requireNonNull(mapper, "The mapper function cannot be null.");
+        Promise<R> promise = future.executor().newPromise();
+        future.addListener(new Mapper<>(promise, mapper));
+        promise.addListener(future, PromiseNotifier::propagateCancel);
+        return promise;
+    }
+
+    /**
+     * Create a new {@link Future} that will complete with the result of this {@link Future} flat-mapped through the
+     * given mapper function.
+     * <p>
+     * The "flat" in "flat-map" means the given mapper function produces a result that itself is a future-of-R, yet this
+     * method also returns a future-of-R, rather than a future-of-future-of-R. In other words, if the same mapper
+     * function was used with the {@link #map(Future, Function)} method, you would get back a {@code Future<Future<R>>}.
+     * These nested futures are "flattened" into a {@code Future<R>} by this method. Note that the future returned by
+     * this method is not the same instance as the one the mapper function returns. The reason is that this method needs
+     * to return immediately, but the mapper function cannot be applied before this future has completed.
+     * <p>
+     * If this future fails, then the returned future will fail as well, with the same exception. Cancellation of either
+     * future will cancel the other. If the mapper function throws, the returned future will fail, but this future will
+     * be unaffected.
+     *
+     * @param mapper The function that will convert the result of this future into the result of the returned future.
+     * @param <R>    The result type of the mapper function, and of the returned future.
+     * @return A new future instance that will complete with the mapped result of this future.
+     */
+    public static <V, R> Future<R> flatMap(Future<V> future, Function<V, Future<R>> mapper) {
+        Promise<R> promise = future.executor().newPromise();
+        future.addListener(new FlatMapper<>(promise, mapper));
+        promise.addListener(future, PromiseNotifier::propagateCancel);
+        return promise;
+    }
+
+    private Futures() {
+    }
+
+    @SuppressWarnings("unchecked")
+    static <R> FutureContextListener<Promise<R>, Object> passThrough() {
+        return (FutureContextListener<Promise<R>, Object>) (FutureContextListener<?, ?>) PASS_THROUGH_CONSTANT;
+    }
+
+    static <A, B> void propagateUncommonCompletion(Future<? extends A> completed, Promise<B> recipient) {
+        if (completed.isCancelled()) {
+            tryCancel(recipient, LOGGER);
+        } else {
+            Throwable cause = completed.cause();
+            tryFailure(recipient, cause, LOGGER);
+        }
+    }
+
+    private static class PassThrough<R> implements FutureContextListener<Promise<R>, Object> {
+        @Override
+        public void operationComplete(Promise<R> recipient, Future<?> completed) throws Exception {
+            if (completed.isSuccess()) {
+                try {
+                    @SuppressWarnings("unchecked")
+                    R result = (R) completed.getNow();
+                    recipient.trySuccess(result);
+                } catch (RuntimeException e) {
+                    tryFailure(recipient, e, LOGGER);
+                }
+            } else {
+                propagateUncommonCompletion(completed, recipient);
+            }
+        }
+    }
+
+    private static class Mapper<R, T> implements FutureListener<Object> {
+        private final Promise<R> recipient;
+        private final Function<T, R> mapper;
+
+        Mapper(Promise<R> recipient, Function<T, R> mapper) {
+            this.recipient = recipient;
+            this.mapper = mapper;
+        }
+
+        @Override
+        public void operationComplete(Future<?> completed) throws Exception {
+            if (completed.isSuccess()) {
+                try {
+                    @SuppressWarnings("unchecked")
+                    T result = (T) completed.getNow();
+                    if (recipient.setUncancellable()) {
+                        R mapped = mapper.apply(result);
+                        recipient.setSuccess(mapped);
+                    }
+                } catch (RuntimeException e) {
+                    tryFailure(recipient, e, LOGGER);
+                }
+            } else {
+                propagateUncommonCompletion(completed, recipient);
+            }
+        }
+    }
+
+    private static class FlatMapper<R, T> implements FutureListener<Object> {
+        private final Promise<R> recipient;
+        private final Function<T, Future<R>> mapper;
+
+        FlatMapper(Promise<R> recipient, Function<T, Future<R>> mapper) {
+            this.recipient = recipient;
+            this.mapper = mapper;
+        }
+
+        @Override
+        public void operationComplete(Future<?> completed) throws Exception {
+            if (completed.isSuccess()) {
+                try {
+                    @SuppressWarnings("unchecked")
+                    T result = (T) completed.getNow();
+                    if (recipient.setUncancellable()) {
+                        Future<R> future = mapper.apply(result);
+                        future.addListener(recipient, passThrough());
+                        recipient.addListener(future, PromiseNotifier::propagateCancel);
+                    }
+                } catch (Exception e) {
+                    tryFailure(recipient, e, LOGGER);
+                }
+            } else {
+                propagateUncommonCompletion(completed, recipient);
+            }
+        }
+    }
+}
