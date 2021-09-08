3152ec76db0a4e0818f3e2bb7f1941c49c5a09dd
Ran <ran-su@users.noreply.github.com>
2021-09-07 23:32:34 -0700

    Throw exceptions when rule violating UDS paths been passed in. (#11663)
    Motivation:
    
    Currently, Netty is silently truncating all over the limit UDS paths and ignoring the `sun_path`'s null-termination role, which hurts compatibility with other UDS clients and servers.
    
    Modifications:
    
    Adding a validation in the JNI code, if the UDS path is not satisfying the system limit or Linux spec throw a NativeIoException.
    
    Result:
    
    All UDS paths Netty can successfully bind are connectable by other programs.

--- transport-native-epoll/src/test/java/io/netty/channel/epoll/LinuxSocketTest.java
+++ transport-native-epoll/src/test/java/io/netty/channel/epoll/LinuxSocketTest.java
@@ -15,6 +15,10 @@
  */
 package io.netty.channel.epoll;
 
+import io.netty.channel.unix.DomainSocketAddress;
+import io.netty.channel.unix.Errors.NativeIoException;
+import io.netty.channel.unix.Socket;
+import java.util.UUID;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -49,6 +53,27 @@ public class LinuxSocketTest {
             assertThrows(IOException.class,
                 () -> socket.connect(new InetSocketAddress(InetAddress.getByAddress(new byte[]{
                     '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'}), 1234)));
+        } finally {
+            socket.close();
+        }
+    }
+
+    @Test
+    public void testUnixDomainSocketTooLongPathFails() throws IOException {
+        // Most systems has a limit for UDS path of 108, 255 is generally too long.
+        StringBuilder socketPath = new StringBuilder("/tmp/");
+        while (socketPath.length() < 255) {
+            socketPath.append(UUID.randomUUID());
+        }
+
+        final DomainSocketAddress domainSocketAddress = new DomainSocketAddress(
+            socketPath.toString());
+        final Socket socket = Socket.newSocketDomain();
+        try {
+            Exception exception = assertThrows(NativeIoException.class, () -> {
+                socket.bind(domainSocketAddress);
+            });
+            Assertions.assertTrue(exception.getMessage().contains("too long"));
         } finally {
             socket.close();
         }
