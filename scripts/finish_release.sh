dcf1e12556c73cea9cff1219feebf88ee25f6a85
Norman Maurer <norman_maurer@apple.com>
2021-09-08 21:45:40 +0200

    Add support for mac m1 (#11666)
    Motivation:
    
    As more and more people switch to a mac m1 we should support it
    
    Modifications:
    
    - Add profiles for cross-compile for mac m1
    - Adjust script to finish release
    
    Result:
    
    Mac m1 is supported

--- scripts/finish_release.sh
+++ scripts/finish_release.sh
@@ -41,6 +41,7 @@ git checkout "$2"
 export JAVA_HOME="$JAVA8_HOME"
 
 ./mvnw -Psonatype-oss-release -am -pl resolver-dns-native-macos,transport-native-unix-common,transport-native-kqueue clean package gpg:sign org.sonatype.plugins:nexus-staging-maven-plugin:deploy -DstagingRepositoryId="$1" -DnexusUrl=https://oss.sonatype.org -DserverId=sonatype-nexus-staging -DskipTests=true
+./mvnw -Psonatype-oss-release,mac-m1-cross-compile -am -pl resolver-dns-native-macos,transport-native-unix-common,transport-native-kqueue clean package gpg:sign org.sonatype.plugins:nexus-staging-maven-plugin:deploy -DstagingRepositoryId="$1" -DnexusUrl=https://oss.sonatype.org -DserverId=sonatype-nexus-staging -DskipTests=true
 
 ./mvnw -Psonatype-oss-release,full,uber-staging -pl all clean package gpg:sign org.sonatype.plugins:nexus-staging-maven-plugin:deploy -DstagingRepositoryId="$1" -DnexusUrl=https://oss.sonatype.org -DserverId=sonatype-nexus-staging -DskipTests=true
 
