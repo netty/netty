9d29e65859b8407697f8b1c6b2d6c192ca62d446
xingrufei <xingrufei@sogou-inc.com>
2021-09-14 15:19:30 +0800

    Update by review
    Signed-off-by: xingrufei <xingrufei@sogou-inc.com>

--- buffer/src/main/java/io/netty/buffer/ByteBufUtil.java
+++ buffer/src/main/java/io/netty/buffer/ByteBufUtil.java
@@ -245,8 +245,8 @@ public final class ByteBufUtil {
             return 0;
         }
 
-        // When the needle has only one byte that can be read,
-        // the firstIndexOf method needs to be called
+        // When the needle has only one byte that can be read and haystack is AbstractByteBuf,
+        // the firstIndexOf method can be used
         if (m == 1 && haystack instanceof  AbstractByteBuf) {
             return firstIndexOf((AbstractByteBuf) haystack, haystack.readerIndex(),
                     haystack.writerIndex(), needle.getByte(needle.readerIndex()));
