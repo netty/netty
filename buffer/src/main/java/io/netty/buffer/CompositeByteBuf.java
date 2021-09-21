984fa86f55a4521f0158333b95138b90320e0d8f
Norman Maurer <norman_maurer@apple.com>
2021-09-21 09:03:35 +0200

    One more test

--- buffer/src/main/java/io/netty/buffer/CompositeByteBuf.java
+++ buffer/src/main/java/io/netty/buffer/CompositeByteBuf.java
@@ -747,7 +747,7 @@ public class CompositeByteBuf extends AbstractReferenceCountedByteBuf implements
         do {
             Component component = components[++componentId];
             // It's important to use srcBuf and NOT buf as we need to return the "original" source buffer and not the
-            // unwrapped one as otherwise me could loose the ability to correctly update the reference count on the
+            // unwrapped one as otherwise we could loose the ability to correctly update the reference count on the
             // returned buffer.
             slice = component.srcBuf.slice(component.srcIdx(component.offset),
                     Math.min(component.length(), bytesToSlice));
