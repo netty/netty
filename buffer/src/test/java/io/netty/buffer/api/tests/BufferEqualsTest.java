0b36e774b88a4ce3b7051b7d2ef3ed54f9087a5e
NiteshKant <nitesh_kant@apple.com>
2021-10-04 16:21:56 -0700

    Moar tests

--- buffer/src/test/java/io/netty/buffer/api/tests/BufferEqualsTest.java
+++ buffer/src/test/java/io/netty/buffer/api/tests/BufferEqualsTest.java
@@ -52,6 +52,24 @@ public class BufferEqualsTest extends BufferTestSupport {
         }
     }
 
+    @ParameterizedTest
+    @MethodSource("allocators")
+    void sameReadableContentBuffersAreEqual(Fixture fixture) {
+        byte[] data1 = "foo".getBytes(UTF_8);
+        byte[] data2 = "notfoomaybe".getBytes(UTF_8);
+        try (BufferAllocator allocator = fixture.createAllocator();
+             Buffer buf1 = allocator.allocate(data1.length);
+             Buffer buf2 = allocator.allocate(data2.length)) {
+            buf1.writeBytes(data1);
+            buf2.writeBytes(data2);
+            buf2.skipReadable(3);
+            buf2.skipWritable(-5);
+
+            Assertions.assertEquals(buf1, buf2);
+            Assertions.assertEquals(buf2, buf1);
+        }
+    }
+
     private static void testEqualsCommutative(Fixture fixture, String value) {
         final byte[] data = value.getBytes(UTF_8);
         try (BufferAllocator allocator = fixture.createAllocator();
