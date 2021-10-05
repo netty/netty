af2e29c146d5f820687068de7c623611dbc57cbb
Chris Vest <christianvest_hansen@apple.com>
2021-10-05 18:04:10 +0200

    Add server-side TCP FastOpen support for macOS
    Motivation:
    This completes the story of TCP FastOpen support for macOS.
    
    Modification:
    On macOS, to enable TCP FastOpen for a listening socket, we need to set it as a socket option after the bind(2) and listen(2) calls (as opposed to before the listen call, like on Linux, or at config time).
    Native methods are added to BsdSocket for getting and setting the TCP_FASTOPEN socket option.
    Since activation happens at bind-time, a field is also added to KQueueServerChannelConfig, to track if TCP FastOpen should be enabled or not.
    By default, it is off, which leaves this socket option unset to the operating system.
    
    Result:
    We now have both client and server support for TCP FastOpen on macOS.

--- transport-native-kqueue/src/main/java/io/netty/channel/kqueue/KQueueServerSocketChannelConfig.java
+++ transport-native-kqueue/src/main/java/io/netty/channel/kqueue/KQueueServerSocketChannelConfig.java
@@ -31,7 +31,7 @@ import static io.netty.channel.kqueue.KQueueChannelOption.SO_ACCEPTFILTER;
 import static io.netty.channel.unix.UnixChannelOption.SO_REUSEPORT;
 
 @UnstableApi
-public class KQueueServerSocketChannelConfig extends KQueueServerChannelConfig implements ServerSocketChannelConfig {
+public class KQueueServerSocketChannelConfig extends KQueueServerChannelConfig {
     KQueueServerSocketChannelConfig(KQueueServerSocketChannel channel) {
         super(channel);
 
@@ -133,6 +133,12 @@ public class KQueueServerSocketChannelConfig extends KQueueServerChannelConfig i
     @Override
     public KQueueServerSocketChannelConfig setBacklog(int backlog) {
         super.setBacklog(backlog);
+        return this;
+    }
+
+    @Override
+    public KQueueServerSocketChannelConfig setTcpFastOpen(boolean enableTcpFastOpen) {
+        super.setTcpFastOpen(enableTcpFastOpen);
         return this;
     }
 
