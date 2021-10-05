88389900b037387a84d4a490d59e39eb01fbeea8
Norman Maurer <norman_maurer@apple.com>
2021-10-05 13:23:12 +0200

    Use install_tool_name and codesign to patch id of shaded library
    Motivation:
    
    How we did patch the id of the shaded library could lead to JVM crashes if signature validation was used on macOS. This is the case on m1 for example.
    
    Modifications:
    
    - Replace our "hand-rolled" way of patching the id with using install_tool_name
    - Re-sign the library after patching the id
    
    Result:
    
    No more crashes due adjusted id on m1 when shading is used

--- common/src/test/java/io/netty/util/internal/NativeLibraryLoaderTest.java
+++ common/src/test/java/io/netty/util/internal/NativeLibraryLoaderTest.java
@@ -15,19 +15,11 @@
  */
 package io.netty.util.internal;
 
-import io.netty.util.CharsetUtil;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -71,72 +63,6 @@ public class NativeLibraryLoaderTest {
             assertTrue(expectedSuppressedExceptionClass.isInstance(suppressed[0]));
         } catch (Exception e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    @Test
-    public void testPatchingId() throws IOException {
-        testPatchingId0(true, false);
-    }
-
-    @Test
-    public void testPatchingIdWithOsArch() throws IOException {
-        testPatchingId0(true, true);
-    }
-
-    @Test
-    public void testPatchingIdNotMatch() throws IOException {
-        testPatchingId0(false, false);
-    }
-
-    @Test
-    public void testPatchingIdWithOsArchNotMatch() throws IOException {
-        testPatchingId0(false, true);
-    }
-
-    private static void testPatchingId0(boolean match, boolean withOsArch) throws IOException {
-        byte[] bytes = new byte[1024];
-        PlatformDependent.threadLocalRandom().nextBytes(bytes);
-        byte[] idBytes = ("/workspace/netty-tcnative/boringssl-static/target/" +
-                "native-build/target/lib/libnetty_tcnative-2.0.20.Final.jnilib").getBytes(CharsetUtil.UTF_8);
-
-        String originalName;
-        if (match) {
-            originalName = "netty-tcnative";
-        } else {
-            originalName = "nonexist_tcnative";
-        }
-        String name = "shaded_" + originalName;
-        if (withOsArch) {
-            name += "_osx_x86_64";
-        }
-
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        out.write(bytes, 0, bytes.length);
-        out.write(idBytes, 0, idBytes.length);
-        out.write(bytes, 0 , bytes.length);
-
-        out.flush();
-        byte[] inBytes = out.toByteArray();
-        out.close();
-
-        InputStream inputStream = new ByteArrayInputStream(inBytes);
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        try {
-            assertEquals(match,
-                    NativeLibraryLoader.patchShadedLibraryId(inputStream, outputStream, originalName, name));
-
-            outputStream.flush();
-            byte[] outputBytes = outputStream.toByteArray();
-            assertArrayEquals(bytes, Arrays.copyOfRange(outputBytes, 0, bytes.length));
-            byte[] patchedId = Arrays.copyOfRange(outputBytes, bytes.length, bytes.length + idBytes.length);
-            assertEquals(!match, Arrays.equals(idBytes, patchedId));
-            assertArrayEquals(bytes,
-                    Arrays.copyOfRange(outputBytes, bytes.length + idBytes.length, outputBytes.length));
-            assertEquals(inBytes.length, outputBytes.length);
-        } finally {
-            inputStream.close();
-            outputStream.close();
         }
     }
 }
