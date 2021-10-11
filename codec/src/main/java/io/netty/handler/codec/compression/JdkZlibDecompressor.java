796690f3122b1679423a69a5c1353fbcddf1f648
Norman Maurer <norman_maurer@apple.com>
2021-10-11 10:17:52 +0200

    optimize

--- codec/src/main/java/io/netty/handler/codec/compression/JdkZlibDecompressor.java
+++ codec/src/main/java/io/netty/handler/codec/compression/JdkZlibDecompressor.java
@@ -244,7 +244,11 @@ public final class JdkZlibDecompressor implements Decompressor {
             if (in.hasArray()) {
                 inflater.setInput(in.array(), in.arrayOffset() + in.readerIndex(), readableBytes);
             } else {
-                inflater.setInput(in.nioBuffer(in.readerIndex(), readableBytes));
+                if (in.nioBufferCount() == 1) {
+                    inflater.setInput(in.internalNioBuffer(in.readerIndex(), readableBytes));
+                } else {
+                    inflater.setInput(in.nioBuffer(in.readerIndex(), readableBytes));
+                }
             }
         }
 
