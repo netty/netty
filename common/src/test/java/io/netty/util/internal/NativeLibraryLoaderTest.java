ba3a58e2efc14e6754dfd3d7c8a8b59f91c7f87e
Martin Grigorov <martin-g@users.noreply.github.com>
2021-11-27 09:47:20 +0200

    Do not allow third parties to provide Netty's native libraries (#11856)
    Throw an exception when there are multiple netty-tcnative-** libraries with the same path in the classpath
    
    Motivation:
    
    Currently Netty loads the first resource in the classpath with a given name.
    It seems there are [libraries](https://github.com/netty/netty-tcnative/issues/681#issuecomment-977752064) which provide Netty's native libraries themselves.
    
    Modification:
    
    From now on Netty will look for all resources with the given name and throw an exception if there are more than one.
    The user application needs to make sure that there is at most one provider of Netty's native libraties (netty-tcnative-**)
    
    Result:
    
    Fixes https://github.com/netty/netty-tcnative/issues/681.

--- common/src/test/java/io/netty/util/internal/NativeLibraryLoaderTest.java
+++ common/src/test/java/io/netty/util/internal/NativeLibraryLoaderTest.java
@@ -16,17 +16,32 @@
 package io.netty.util.internal;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIf;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.function.Executable;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.condition.OS.LINUX;
 
-public class NativeLibraryLoaderTest {
+class NativeLibraryLoaderTest {
+
+    private static final String OS_ARCH = System.getProperty("os.arch");
+    private boolean is_x86_64() {
+        return "x86_64".equals(OS_ARCH) || "amd64".equals(OS_ARCH);
+    }
 
     @Test
-    public void testFileNotFound() {
+    void testFileNotFound() {
         try {
             NativeLibraryLoader.load(UUID.randomUUID().toString(), NativeLibraryLoaderTest.class.getClassLoader());
             fail();
@@ -37,7 +52,7 @@ public class NativeLibraryLoaderTest {
     }
 
     @Test
-    public void testFileNotFoundWithNullClassLoader() {
+    void testFileNotFoundWithNullClassLoader() {
         try {
             NativeLibraryLoader.load(UUID.randomUUID().toString(), null);
             fail();
@@ -47,14 +62,46 @@ public class NativeLibraryLoaderTest {
         }
     }
 
+    @Test
+    @EnabledOnOs(LINUX)
+    @EnabledIf("is_x86_64")
+    void testMultipleResourcesInTheClassLoader() throws MalformedURLException {
+        URL url1 = new File("src/test/data/NativeLibraryLoader/1").toURI().toURL();
+        URL url2 = new File("src/test/data/NativeLibraryLoader/2").toURI().toURL();
+        final URLClassLoader loader = new URLClassLoader(new URL[] {url1, url2});
+        final String resourceName = "test1";
+
+        Exception ise = assertThrows(IllegalStateException.class, new Executable() {
+            @Override
+            public void execute() {
+                NativeLibraryLoader.load(resourceName, loader);
+            }
+        });
+        assertTrue(ise.getMessage()
+                    .contains("Multiple resources found for 'META-INF/native/lib" + resourceName + ".so'"));
+    }
+
+    @Test
+    @EnabledOnOs(LINUX)
+    @EnabledIf("is_x86_64")
+    void testSingleResourceInTheClassLoader() throws MalformedURLException {
+        URL url1 = new File("src/test/data/NativeLibraryLoader/1").toURI().toURL();
+        URL url2 = new File("src/test/data/NativeLibraryLoader/2").toURI().toURL();
+        URLClassLoader loader = new URLClassLoader(new URL[] {url1, url2});
+        String resourceName = "test2";
+
+        NativeLibraryLoader.load(resourceName, loader);
+        assertTrue(true);
+    }
+
     private static void verifySuppressedException(UnsatisfiedLinkError error,
             Class<?> expectedSuppressedExceptionClass) {
         try {
             Throwable[] suppressed = error.getCause().getSuppressed();
-            assertTrue(suppressed.length == 1);
+            assertEquals(1, suppressed.length);
             assertTrue(suppressed[0] instanceof UnsatisfiedLinkError);
             suppressed = (suppressed[0]).getSuppressed();
-            assertTrue(suppressed.length == 1);
+            assertEquals(1, suppressed.length);
             assertTrue(expectedSuppressedExceptionClass.isInstance(suppressed[0]));
         } catch (Exception e) {
             throw new RuntimeException(e);
