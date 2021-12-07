927e82f9c04d8378b826610fa8eb7528e4ae7b8f
Norman Maurer <norman_maurer@apple.com>
2021-12-07 15:24:05 +0100

    Ensure we always run the register task of native libraries (#11887)
    Motivation:
    
    At some point we introduced guards to ensure we not bind native methods multiple times. While this seemed to have fixed the problem it introduced other problems
    as these methods sometimes not only bind native methods but also init static fields. This could later then cause things like segfaults if we depend on static fields to not be NULL.
    
    Modification:
    
    Remove atomic guard
    Use static synchronized for proper sequencing
    Always call Socket.initialize()
    Result:
    
    No more segfaults due NULL fields

--- transport-native-unix-common/src/main/java/io/netty/channel/unix/Unix.java
+++ transport-native-unix-common/src/main/java/io/netty/channel/unix/Unix.java
@@ -53,11 +53,9 @@ public final class Unix {
      * @param registerTask The task to run if this thread caused registration.
      */
     @UnstableApi
-    public static void registerInternal(Runnable registerTask) {
-        if (registered.compareAndSet(false, true)) {
-            registerTask.run();
-            Socket.initialize();
-        }
+    public static synchronized void registerInternal(Runnable registerTask) {
+        registerTask.run();
+        Socket.initialize();
     }
 
     /**
