netty/netty

commit ea2742b4f628dfae9523c49b22472df921d1386c

Do not allow third parties to provide Netty's native libraries (#11856)
Throw an exception when there are multiple netty-tcnative-** libraries with the same path in the classpath

Motivation:

Currently Netty loads the first resource in the classpath with a given name.
It seems there are [libraries](https://github.com/netty/netty-tcnative/issues/681#issuecomment-977752064) which provide Netty's native libraries themselves.

Modification:

From now on Netty will look for all resources with the given name and throw an exception if there are more than one.
The user application needs to make sure that there is at most one provider of Netty's native libraties (netty-tcnative-**)

Result:

Fixes https://github.com/netty/netty-tcnative/issues/681.

common/src/main/java/io/netty/util/internal/NativeLibraryLoader.java
@@ -33,7 +33,9 @@ import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.EnumSet;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Set;
 
@@ -94,6 +96,7 @@ public final class NativeLibraryLoader {
         for (String name : names) {
             try {
                 load(name, loader);
+                logger.debug("Loaded library with name '{}'", name);
                 return;
             } catch (Throwable t) {
                 suppressed.add(t);
@@ -145,22 +148,13 @@ public final class NativeLibraryLoader {
         InputStream in = null;
         OutputStream out = null;
         File tmpFile = null;
-        URL url;
-        if (loader == null) {
-            url = ClassLoader.getSystemResource(path);
-        } else {
-            url = loader.getResource(path);
-        }
+        URL url = getResource(path, loader);
         try {
             if (url == null) {
                 if (PlatformDependent.isOsx()) {
                     String fileName = path.endsWith(".jnilib") ? NATIVE_RESOURCE_HOME + "lib" + name + ".dynlib" :
                             NATIVE_RESOURCE_HOME + "lib" + name + ".jnilib";
-                    if (loader == null) {
-                        url = ClassLoader.getSystemResource(fileName);
-                    } else {
-                        url = loader.getResource(fileName);
-                    }
+                    url = getResource(fileName, loader);
                     if (url == null) {
                         FileNotFoundException fnf = new FileNotFoundException(fileName);
                         ThrowableUtil.addSuppressedAndClear(fnf, suppressed);
@@ -236,6 +230,30 @@ public final class NativeLibraryLoader {
         }
     }
 
+    private static URL getResource(String path, ClassLoader loader) {
+        final Enumeration<URL> urls;
+        try {
+            if (loader == null) {
+                urls = ClassLoader.getSystemResources(path);
+            } else {
+                urls = loader.getResources(path);
+            }
+        } catch (IOException iox) {
+            throw new RuntimeException("An error occurred while getting the resources for " + path, iox);
+        }
+
+        List<URL> urlsList = Collections.list(urls);
+        int size = urlsList.size();
+        switch (size) {
+            case 0:
+                return null;
+            case 1:
+                return urlsList.get(0);
+            default:
+                throw new IllegalStateException("Multiple resources found for '" + path + "': " + urlsList);
+        }
+    }
+
     static void tryPatchShadedLibraryIdAndSign(File libraryFile, String originalName) {
         String newId = new String(generateUniqueId(originalName.length()), CharsetUtil.UTF_8);
         if (!tryExec("install_name_tool -id " + newId + " " + libraryFile.getAbsolutePath())) {
@@ -288,7 +306,7 @@ public final class NativeLibraryLoader {
         Throwable suppressed = null;
         try {
             try {
-                // Make sure the helper is belong to the target ClassLoader.
+                // Make sure the helper belongs to the target ClassLoader.
                 final Class<?> newHelper = tryToLoadClass(loader, NativeLibraryUtil.class);
                 loadLibraryByHelper(newHelper, name, absolute);
                 logger.debug("Successfully loaded the library {}", name);

common/src/test/java/io/netty/util/internal/NativeLibraryLoaderTest.java
@@ -16,17 +16,31 @@
 package io.netty.util.internal;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIf;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.function.Executable;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.condition.OS.LINUX;
 
-public class NativeLibraryLoaderTest {
+class NativeLibraryLoaderTest {
+
+    private static final String OS_ARCH = System.getProperty("os.arch");
+    private boolean is_x86_64() {
+        return "x86_64".equals(OS_ARCH) || "amd64".equals(OS_ARCH);
+    }
 
     @Test
-    public void testFileNotFound() {
+    void testFileNotFound() {
         try {
             NativeLibraryLoader.load(UUID.randomUUID().toString(), NativeLibraryLoaderTest.class.getClassLoader());
             fail();
@@ -39,7 +53,7 @@ public class NativeLibraryLoaderTest {
     }
 
     @Test
-    public void testFileNotFoundWithNullClassLoader() {
+    void testFileNotFoundWithNullClassLoader() {
         try {
             NativeLibraryLoader.load(UUID.randomUUID().toString(), null);
             fail();
@@ -49,6 +63,38 @@ public class NativeLibraryLoaderTest {
                 verifySuppressedException(error, ClassNotFoundException.class);
             }
         }
+    }
+
+    @Test
+    @EnabledOnOs(LINUX)
+    @EnabledIf("is_x86_64")
+    void testMultipleResourcesInTheClassLoader() throws MalformedURLException {
+        URL url1 = new File("src/test/data/NativeLibraryLoader/1").toURI().toURL();
+        URL url2 = new File("src/test/data/NativeLibraryLoader/2").toURI().toURL();
+        final URLClassLoader loader = new URLClassLoader(new URL[] {url1, url2});
+        final String resourceName = "test1";
+
+        Exception ise = assertThrows(IllegalStateException.class, new Executable() {
+            @Override
+            public void execute() {
+                NativeLibraryLoader.load(resourceName, loader);
+            }
+        });
+        assertTrue(ise.getMessage()
+                    .contains("Multiple resources found for 'META-INF/native/lib" + resourceName + ".so'"));
+    }
+
+    @Test
+    @EnabledOnOs(LINUX)
+    @EnabledIf("is_x86_64")
+    void testSingleResourceInTheClassLoader() throws MalformedURLException {
+        URL url1 = new File("src/test/data/NativeLibraryLoader/1").toURI().toURL();
+        URL url2 = new File("src/test/data/NativeLibraryLoader/2").toURI().toURL();
+        URLClassLoader loader = new URLClassLoader(new URL[] {url1, url2});
+        String resourceName = "test2";
+
+        NativeLibraryLoader.load(resourceName, loader);
+        assertTrue(true);
     }
 
     @SuppressJava6Requirement(reason = "uses Java 7+ Throwable#getSuppressed but is guarded by version checks")

commit 1bf02cb1165e147acc387ec6195c48d7a1c93a7d

Allow to disable duplicate native library check
Motivation:

ea2742b4f628dfae9523c49b22472df921d1386c introduced a change to fail loading the native lib if the lib is included multiple times in the classpath. While this makes a lot of sense it is not always easy for people to fix this easily in a fast-manner. We should allow to opt-out of it.

Modifications:

- Add 'io.netty.native.detectNativeLibraryDuplicates' property that allows to disable the check. Default is enabled
- When check is disabled we log via warn to notify the user about the need to fix it

Result:

Easier for people to upgrade

common/src/main/java/io/netty/util/internal/NativeLibraryLoader.java
@@ -53,6 +53,7 @@ public final class NativeLibraryLoader {
     private static final File WORKDIR;
     private static final boolean DELETE_NATIVE_LIB_AFTER_LOADING;
     private static final boolean TRY_TO_PATCH_SHADED_ID;
+    private static final boolean DETECT_NATIVE_LIBRARY_DUPLICATES;
 
     // Just use a-Z and numbers as valid ID bytes.
     private static final byte[] UNIQUE_ID_BYTES =
@@ -84,6 +85,10 @@ public final class NativeLibraryLoader {
         TRY_TO_PATCH_SHADED_ID = SystemPropertyUtil.getBoolean(
                 "io.netty.native.tryPatchShadedId", true);
         logger.debug("-Dio.netty.native.tryPatchShadedId: {}", TRY_TO_PATCH_SHADED_ID);
+
+        DETECT_NATIVE_LIBRARY_DUPLICATES = SystemPropertyUtil.getBoolean(
+                "io.netty.native.detectNativeLibraryDuplicates", true);
+        logger.debug("-Dio.netty.native.detectNativeLibraryDuplicates: {}", DETECT_NATIVE_LIBRARY_DUPLICATES);
     }
 
     /**
@@ -252,33 +257,39 @@ public final class NativeLibraryLoader {
             case 1:
                 return urlsList.get(0);
             default:
-                try {
-                    MessageDigest md = MessageDigest.getInstance("SHA-256");
-                    // We found more than 1 resource with the same name. Let's check if the content of the file is the
-                    // same as in this case it will not have any bad effect.
-                    URL url = urlsList.get(0);
-                    byte[] digest = digest(md, url);
-                    boolean allSame = true;
-                    if (digest != null) {
-                        for (int i = 1; i < size; i++) {
-                            byte[] digest2 = digest(md, urlsList.get(i));
-                            if (digest2 == null || !Arrays.equals(digest, digest2)) {
-                                allSame = false;
-                                break;
+                if (DETECT_NATIVE_LIBRARY_DUPLICATES) {
+                    try {
+                        MessageDigest md = MessageDigest.getInstance("SHA-256");
+                        // We found more than 1 resource with the same name. Let's check if the content of the file is
+                        // the same as in this case it will not have any bad effect.
+                        URL url = urlsList.get(0);
+                        byte[] digest = digest(md, url);
+                        boolean allSame = true;
+                        if (digest != null) {
+                            for (int i = 1; i < size; i++) {
+                                byte[] digest2 = digest(md, urlsList.get(i));
+                                if (digest2 == null || !Arrays.equals(digest, digest2)) {
+                                    allSame = false;
+                                    break;
+                                }
                             }
+                        } else {
+                            allSame = false;
                         }
-                    } else {
-                        allSame = false;
+                        if (allSame) {
+                            return url;
+                        }
+                    } catch (NoSuchAlgorithmException e) {
+                        logger.debug("Don't support SHA-256, can't check if resources have same content.", e);
                     }
-                    if (allSame) {
-                        return url;
-                    }
-                } catch (NoSuchAlgorithmException e) {
-                    logger.debug("Don't support SHA-256, can't check if resources have same content.", e);
-                }
 
-                throw new IllegalStateException(
-                        "Multiple resources found for '" + path + "' with different content: " + urlsList);
+                    throw new IllegalStateException(
+                            "Multiple resources found for '" + path + "' with different content: " + urlsList);
+                } else {
+                    logger.warn("Multiple resources found for '" + path + "' with different content: " +
+                            urlsList + ". Please fix your dependency graph.");
+                    return urlsList.get(0);
+                }
         }
     }
 