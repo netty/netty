5d8963f61175f4da7c09d187f89207352dde0eed
huibinliu <huibinliu.coder@gmail.com>
2022-01-11 18:15:49 +0800

    fix check style errors in AbstracChannel

--- transport/src/main/java/io/netty/channel/AbstractChannel.java
+++ transport/src/main/java/io/netty/channel/AbstractChannel.java
@@ -651,12 +651,13 @@ public abstract class AbstractChannel extends DefaultAttributeMap implements Cha
                     new ChannelOutputShutdownException("Channel output shutdown") :
                     new ChannelOutputShutdownException("Channel output shutdown", cause);
 
-            // When a side enables SO_LINGER and calls showdownOutput(...) to start TCP half-closure, we can not call doDeregister here
-            // because we should ensure this side in fin_wait2 state can still receive and process the data which is send by another side in the close_wait state。
+            // When a side enables SO_LINGER and calls showdownOutput(...) to start TCP half-closure
+            // we can not call doDeregister here because we should ensure this side in fin_wait2 state
+            // can still receive and process the data which is send by another side in the close_wait state。
             // See https://github.com/netty/netty/issues/11981
             try {
-                // The shutdown function does not block regardless of the SO_LINGER setting on the socket,so we don't need to use
-                // GlobalEventExecutor to execute the shutdown
+                // The shutdown function does not block regardless of the SO_LINGER setting on the socket
+                // so we don't need to use GlobalEventExecutor to execute the shutdown
                 doShutdownOutput();
                 promise.setSuccess();
             } catch (Throwable err) {
