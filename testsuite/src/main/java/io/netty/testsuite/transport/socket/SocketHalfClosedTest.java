5b358951ddcea6d05632b7cd68588222d63844ab
Bin <huibinliu.coder@gmail.com>
2022-01-11 17:16:41 +0800

    Update testsuite/src/main/java/io/netty/testsuite/transport/socket/SocketHalfClosedTest.java
    Co-authored-by: Norman Maurer <norman_maurer@apple.com>

--- testsuite/src/main/java/io/netty/testsuite/transport/socket/SocketHalfClosedTest.java
+++ testsuite/src/main/java/io/netty/testsuite/transport/socket/SocketHalfClosedTest.java
@@ -44,7 +44,6 @@ import org.junit.jupiter.api.Timeout;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
