68d1b99ceeedb584449b82a654785d251aa91e16
Dmitry Spikhalskiy <dmitry@spikhalskiy.com>
2022-01-18 07:38:41 -0500

    Provide clarifications why SNI hostname has to be FQDN (#12014)
    Motivation:
    
    It's not immediately clear why Netty discards non fully qualified domain names from the SNI extension and it may look like a bug.
    
    Modification:
    
    Provided clarifications why SNI hostname has to be FQDN with links to the RFC
    
    Result:
    
    Related to #12005
    
    Co-authored-by: Norman Maurer <norman_maurer@apple.com>

--- handler/src/main/java/io/netty/handler/ssl/SslUtils.java
+++ handler/src/main/java/io/netty/handler/ssl/SslUtils.java
@@ -477,7 +477,10 @@ final class SslUtils {
      * Validate that the given hostname can be used in SNI extension.
      */
     static boolean isValidHostNameForSNI(String hostname) {
+        // See  https://datatracker.ietf.org/doc/html/rfc6066#section-3
         return hostname != null &&
+               // SNI HostName has to be a FQDN according to TLS SNI Extension spec (see [1]),
+               // which means that is has to have at least a host name and a domain part.
                hostname.indexOf('.') > 0 &&
                !hostname.endsWith(".") && !hostname.startsWith("/") &&
                !NetUtil.isValidIpV4Address(hostname) &&
