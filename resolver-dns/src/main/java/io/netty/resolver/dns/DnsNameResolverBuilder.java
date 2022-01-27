561a7cbdbf710f87a3de664081a85d287b4b574f
Norman Maurer <norman_maurer@apple.com>
2022-01-27 11:51:40 +0100

    Copy localAddress as well when DnsNameResolverBuilder.copy() is called (#12050)
    Motivation:
    
    We missed to also copy the localAddress when copy() is called
    
    Modifications:
    
    Correctly set the localAddress as well
    
    Result:
    
    Make a complete copy

--- resolver-dns/src/main/java/io/netty/resolver/dns/DnsNameResolverBuilder.java
+++ resolver-dns/src/main/java/io/netty/resolver/dns/DnsNameResolverBuilder.java
@@ -204,7 +204,7 @@ public final class DnsNameResolverBuilder {
     }
 
     /**
-     * Configure the address that will be used to bind too. If `null` the default will be used.
+     * Configure the address that will be used to bind too. If {@code null} the default will be used.
      * @param localAddress the bind address
      * @return {@code this}
      */
@@ -572,7 +572,7 @@ public final class DnsNameResolverBuilder {
         copiedBuilder.ndots(ndots);
         copiedBuilder.decodeIdn(decodeIdn);
         copiedBuilder.completeOncePreferredResolved(completeOncePreferredResolved);
-
+        copiedBuilder.localAddress(localAddress);
         return copiedBuilder;
     }
 }
