a1be2f040b2eff475612a609acec5e0de090b556
Andrey Mizurov <avmizurov@gmail.com>
2022-03-21 10:46:48 +0100

    Fix checkstyle again

--- codec-http/src/main/java/io/netty/handler/codec/http/websocketx/WebSocketServerHandshaker.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/websocketx/WebSocketServerHandshaker.java
@@ -297,8 +297,8 @@ public abstract class WebSocketServerHandshaker {
         String aggregatorCtx = ctx.name();
         if (HttpUtil.isContentLengthSet(req) || HttpUtil.isTransferEncodingChunked(req) ||
             version == WebSocketVersion.V00) {
-            // Add aggregator and ensure we feed the HttpRequest so it is aggregated. A limit of 8192 should be more then
-            // enough for the websockets handshake payload.
+            // Add aggregator and ensure we feed the HttpRequest so it is aggregated. A limit of 8192 should be
+            // more then enough for the websockets handshake payload.
             aggregatorCtx = "httpAggregator";
             p.addAfter(ctx.name(), aggregatorCtx, new HttpObjectAggregator(8192));
         }
