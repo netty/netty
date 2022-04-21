4230e0cd42b0812d6def2196f8c4145c12c1cb2e
Norman Maurer <norman_maurer@apple.com>
2022-04-21 15:43:07 +0200

    Not use jctools in microbenchmark module

--- microbench/src/main/java/io/netty/handler/codec/http/DecodeHexBenchmark.java
+++ microbench/src/main/java/io/netty/handler/codec/http/DecodeHexBenchmark.java
@@ -16,9 +16,9 @@
 package io.netty.handler.codec.http;
 
 import io.netty.microbench.util.AbstractMicrobenchmark;
+import io.netty.util.internal.MathUtil;
 import io.netty.util.internal.PlatformDependent;
 import io.netty.util.internal.StringUtil;
-import org.jctools.util.Pow2;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.CompilerControl;
 import org.openjdk.jmh.annotations.CompilerControl.Mode;
@@ -59,7 +59,7 @@ public class DecodeHexBenchmark extends AbstractMicrobenchmark {
     public void init() {
         final char[] hexCh = hex.toCharArray();
         next = 0;
-        inputs = Pow2.roundToPowerOfTwo(inputs);
+        inputs = MathUtil.findNextPositivePowerOfTwo(inputs);
         hexDigits = new char[inputs][];
         hexDigits[0] = hexCh;
         if (inputs > 1) {
