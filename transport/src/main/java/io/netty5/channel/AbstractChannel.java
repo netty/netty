e8d24f976aaf51470615bd16e248989b045c9c61
Norman Maurer <norman_maurer@apple.com>
2022-06-23 18:14:06 -0700

    Final

--- transport/src/main/java/io/netty5/channel/AbstractChannel.java
+++ transport/src/main/java/io/netty5/channel/AbstractChannel.java
@@ -210,13 +210,13 @@ public abstract class AbstractChannel extends DefaultAttributeMap implements Cha
     }
 
     @Override
-    public boolean isWritable() {
+    public final boolean isWritable() {
         ChannelOutboundBuffer buf = unsafe.outboundBuffer();
         return buf != null && buf.isWritable();
     }
 
     @Override
-    public long bytesBeforeUnwritable() {
+    public final long bytesBeforeUnwritable() {
         ChannelOutboundBuffer buf = unsafe.outboundBuffer();
         // isWritable() is currently assuming if there is no outboundBuffer then the channel is not writable.
         // We should be consistent with that here.
@@ -224,7 +224,7 @@ public abstract class AbstractChannel extends DefaultAttributeMap implements Cha
     }
 
     @Override
-    public long bytesBeforeWritable() {
+    public final long bytesBeforeWritable() {
         ChannelOutboundBuffer buf = unsafe.outboundBuffer();
         // isWritable() is currently assuming if there is no outboundBuffer then the channel is not writable.
         // We should be consistent with that here.
