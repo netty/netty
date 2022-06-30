75067af50f3d7d2e130ba7c394279995f206c2b7
Norman Maurer <norman_maurer@apple.com>
2022-06-30 14:35:58 +0200

    Check correct reference for exception during test run
    Motivation:
    
    While working on some other problem I noticed that we didnt check the correct reference and so could end up in a timeout without seeing the correct cause of it.
    
    Modifications:
    
    Check the correct reference
    
    Result:
    
    Fix unit test

--- testsuite/src/main/java/io/netty5/testsuite/transport/socket/SocketSslEchoTest.java
+++ testsuite/src/main/java/io/netty5/testsuite/transport/socket/SocketSslEchoTest.java
@@ -348,7 +348,7 @@ public class SocketSslEchoTest extends AbstractSocketTest {
             if (serverException.get() != null) {
                 break;
             }
-            if (serverException.get() != null) {
+            if (clientException.get() != null) {
                 break;
             }
 
