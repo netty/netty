1b1126896e8e07841121183a69409e31480ab447
Norman Maurer <norman_maurer@apple.com>
2022-07-20 20:00:32 +0200

    Add profile to cross-compile for intel on m1 mac (#12631)
    Motivation:
    
    As the new hw has a m1 / m2 chip we need to ensure we can also cross-compile for intel and do a release
    
    Modifications:
    
    - Add profile for cross-compile to intel
    - Adjust finish_release.sh script to select the correct profile for cross compilation
    
    Result:
    
    Be able to cross compile on m1 and also do the release

--- scripts/finish_release.sh
+++ scripts/finish_release.sh
@@ -22,6 +22,8 @@ if [ "$#" -ne 2 ]; then
 fi
 
 OS=$(uname)
+ARCH=$(uname -p)
+
 
 if [ "$OS" != "Darwin" ]; then
     echo "Needs to be executed on macOS"
@@ -35,13 +37,18 @@ if git tag | grep -q "$2" ; then
     git tag -d "$2"
 fi
 
+CROSS_COMPILE_PROFILE="mac-m1-cross-compile"
+if [ "$ARCH" == "arm" ]; then
+    PROFILE="mac-intel-cross-compile"
+fi
+
 git fetch
 git checkout "$2"
 
 export JAVA_HOME="$JAVA8_HOME"
 
 ./mvnw -Psonatype-oss-release -am -pl resolver-dns-native-macos,transport-native-unix-common,transport-native-kqueue clean package gpg:sign org.sonatype.plugins:nexus-staging-maven-plugin:deploy -DstagingRepositoryId="$1" -DnexusUrl=https://oss.sonatype.org -DserverId=sonatype-nexus-staging -DskipTests=true
-./mvnw -Psonatype-oss-release,mac-m1-cross-compile -am -pl resolver-dns-native-macos,transport-native-unix-common,transport-native-kqueue clean package gpg:sign org.sonatype.plugins:nexus-staging-maven-plugin:deploy -DstagingRepositoryId="$1" -DnexusUrl=https://oss.sonatype.org -DserverId=sonatype-nexus-staging -DskipTests=true
+./mvnw -Psonatype-oss-release,"$CROSS_COMPILE_PROFILE" -am -pl resolver-dns-native-macos,transport-native-unix-common,transport-native-kqueue clean package gpg:sign org.sonatype.plugins:nexus-staging-maven-plugin:deploy -DstagingRepositoryId="$1" -DnexusUrl=https://oss.sonatype.org -DserverId=sonatype-nexus-staging -DskipTests=true
 
 ./mvnw -Psonatype-oss-release,full,native-dependencies,staging -pl all clean package gpg:sign org.sonatype.plugins:nexus-staging-maven-plugin:deploy -DstagingRepositoryId="$1" -DnexusUrl=https://oss.sonatype.org -DserverId=sonatype-nexus-staging -DskipTests=true
 
