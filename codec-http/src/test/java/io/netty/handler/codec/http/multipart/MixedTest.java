9b5b428fee214162e2636ea250ab5889ea96c0f2
yawkat <jonas.konrad@oracle.com>
2022-07-27 12:06:07 +0200

    Track independent refCnt in MixedAttribute and MixedFileUpload
    Motivation:
    
    Before this patch, MixedAttribute and MixedFileUpload delegated reference counting to their underlying memory or file representation. When the representation changes from memory to disk because the memory size limit was exceeded, the reference count of the old representation is not transferred, so the reference attribute resets to 1. This may leak the memory of the old representation, and may lead to exceptions when users try to release the mixed data.
    
    Modification:
    
    Introduce a new AbstractMixedHttpData class, analogous to the existing abstract classes for disk and memory data. This new class does its own reference counting, and unifies the swapping mechanism for attributes and file uploads.
    
    The change is fairly big, but it's mostly copied from the old implementations. Makes it a bit annoying to review though, so I might have missed a bug.
    
    I tried inheriting from AbstractHttpData, but it didn't really reduce the code size much, and makes some code less clear because it leads to two copies of the name, charset, etc.
    
    Regarding compatibility, I managed to fix most compat issues reported by revapi. However inheriting from a new class in a non-final class is apparently a breaking change no matter what we do, and additionally AbstractReferenceCounted.setRefCnt is problematic. I think both of these errors are unlikely to affect anyone, and are justifiable. I've added an exclusion to the pom.
    
    There is a new test class to test the retain/refCnt behavior. I didn't add it to the HttpDataTest, because the data defined there does not have a size limit we can use to test easily.
    
    Result:
    
    MixedAttribute and MixedFileUpload now behave like any other reference counted object.

---
+++ codec-http/src/test/java/io/netty/handler/codec/http/multipart/MixedTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2022 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.handler.codec.http.multipart;
+
+import io.netty.buffer.Unpooled;
+import io.netty.util.CharsetUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class MixedTest {
+    @Test
+    public void mixedAttributeRefCnt() throws IOException {
+        MixedAttribute attribute = new MixedAttribute("foo", 100);
+        Assertions.assertEquals(1, attribute.refCnt());
+        attribute.retain();
+        Assertions.assertEquals(2, attribute.refCnt());
+
+        attribute.addContent(Unpooled.wrappedBuffer(new byte[90]), false);
+        Assertions.assertEquals(2, attribute.refCnt());
+
+        attribute.addContent(Unpooled.wrappedBuffer(new byte[90]), true);
+        Assertions.assertEquals(2, attribute.refCnt());
+
+        attribute.release(2);
+    }
+
+    @Test
+    public void mixedFileUploadRefCnt() throws IOException {
+        MixedFileUpload upload = new MixedFileUpload("foo", "foo", "foo", "UTF-8", CharsetUtil.UTF_8, 0, 100);
+        Assertions.assertEquals(1, upload.refCnt());
+        upload.retain();
+        Assertions.assertEquals(2, upload.refCnt());
+
+        upload.addContent(Unpooled.wrappedBuffer(new byte[90]), false);
+        Assertions.assertEquals(2, upload.refCnt());
+
+        upload.addContent(Unpooled.wrappedBuffer(new byte[90]), true);
+        Assertions.assertEquals(2, upload.refCnt());
+
+        upload.release(2);
+    }
+}
