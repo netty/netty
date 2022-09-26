e67fb4b83eeff82710f657c0f48ae35e1b5cd4e3
franz1981 <nigro.fra@gmail.com>
2022-09-26 21:47:53 +0200

    Adding duplex/non-duplex

--- microbench/src/main/java/io/netty/microbench/channel/DefaultChannelPipelineDuplexHandlerBenchmark.java
+++ microbench/src/main/java/io/netty/microbench/channel/DefaultChannelPipelineDuplexHandlerBenchmark.java
@@ -18,12 +18,13 @@ package io.netty.microbench.channel;
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.microbench.util.AbstractMicrobenchmark;
-import io.netty.util.internal.PlatformDependent;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
@@ -31,9 +32,6 @@ import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
-import org.openjdk.jmh.profile.LinuxPerfAsmProfiler;
-import org.openjdk.jmh.profile.LinuxPerfC2CProfiler;
-import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
 
 @Warmup(iterations = 5, time = 1)
 @Measurement(iterations = 5, time = 1)
@@ -42,6 +40,9 @@ public class DefaultChannelPipelineDuplexHandlerBenchmark extends AbstractMicrob
 
     private ChannelPipeline pipeline;
     private EmbeddedChannel channel;
+
+    @Param({"true", "false"})
+    private boolean duplex;
 
     @Setup
     public void setup() {
@@ -62,23 +63,44 @@ public class DefaultChannelPipelineDuplexHandlerBenchmark extends AbstractMicrob
         });
         // Duplex handlers implements both out/in interfaces causing a scalability issue
         // see https://bugs.openjdk.org/browse/JDK-8180450
-        pipeline.addLast(new ChannelDuplexHandler() {
-            @Override
-            public void channelReadComplete(final ChannelHandlerContext ctx) {
-                ctx.fireChannelReadComplete();
-            }
+        if (duplex) {
+            pipeline.addLast(new ChannelDuplexHandler() {
+                @Override
+                public void channelReadComplete(final ChannelHandlerContext ctx) {
+                    ctx.fireChannelReadComplete();
+                }
 
-            @Override
-            public void flush(final ChannelHandlerContext ctx) {
-                ctx.flush();
-            }
-        });
-        pipeline.addLast(new ChannelDuplexHandler() {
-            @Override
-            public void channelReadComplete(final ChannelHandlerContext ctx) {
-                ctx.flush();
-            }
-        });
+                @Override
+                public void flush(final ChannelHandlerContext ctx) {
+                    ctx.flush();
+                }
+            });
+            pipeline.addLast(new ChannelDuplexHandler() {
+                @Override
+                public void channelReadComplete(final ChannelHandlerContext ctx) {
+                    ctx.flush();
+                }
+            });
+        } else {
+            pipeline.addLast(new ChannelInboundHandlerAdapter() {
+                @Override
+                public void channelReadComplete(final ChannelHandlerContext ctx) {
+                    ctx.fireChannelReadComplete();
+                }
+            });
+            pipeline.addLast(new ChannelOutboundHandlerAdapter() {
+                @Override
+                public void flush(final ChannelHandlerContext ctx) {
+                    ctx.flush();
+                }
+            });
+            pipeline.addLast(new ChannelInboundHandlerAdapter() {
+                @Override
+                public void channelReadComplete(final ChannelHandlerContext ctx) {
+                    ctx.flush();
+                }
+            });
+        }
     }
 
     @TearDown
@@ -95,15 +117,5 @@ public class DefaultChannelPipelineDuplexHandlerBenchmark extends AbstractMicrob
     @Threads(4)
     public void parallelPropagateEvent(Blackhole hole) {
         hole.consume(pipeline.fireChannelReadComplete());
-    }
-
-    @Override
-    protected ChainedOptionsBuilder newOptionsBuilder() throws Exception {
-        if (PlatformDependent.isOsx() || PlatformDependent.isWindows()) {
-            return super.newOptionsBuilder();
-        }
-        return super.newOptionsBuilder()
-                .addProfiler(LinuxPerfAsmProfiler.class)
-                .addProfiler(LinuxPerfC2CProfiler.class);
     }
 }
