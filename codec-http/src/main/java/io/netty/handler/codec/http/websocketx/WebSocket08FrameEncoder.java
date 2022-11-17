6e765c5e862715201b032cf82a989947e7364fc0
Maksym Ostroverkhov <m.ostroverkhov@gmail.com>
2022-11-17 20:40:04 +0200

    WebSocket frame encoding: fix frame mask calculation - avoid sign extension on widening primitive conversion

--- codec-http/src/main/java/io/netty/handler/codec/http/websocketx/WebSocket08FrameEncoder.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/websocketx/WebSocket08FrameEncoder.java
@@ -185,8 +185,10 @@ public class WebSocket08FrameEncoder extends MessageToMessageEncoder<WebSocketFr
                     int end = data.writerIndex();
 
                     if (srcOrder == dstOrder) {
-                        // Use the optimized path only when byte orders match
-                        long longMask = mask | (long) mask << 32;
+                        // Use the optimized path only when byte orders match.
+                        // Avoid sign extension on widening primitive conversion
+                        long longMask = (long) mask & 0xFFFFFFFFL;
+                        longMask |= longMask << 32;
 
                         // If the byte order of our buffers it little endian we have to bring our mask
                         // into the same format, because getInt() and writeInt() will use a reversed byte order
