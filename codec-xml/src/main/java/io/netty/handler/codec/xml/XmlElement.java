ec6000fffa2f12ae9158352728ffe9b0cfd17eb2
gamecraft <roll1987@163.com>
2022-11-24 17:49:07 +0800

    Replace LinkedList with ArrayList

--- codec-xml/src/main/java/io/netty/handler/codec/xml/XmlElement.java
+++ codec-xml/src/main/java/io/netty/handler/codec/xml/XmlElement.java
@@ -28,7 +28,7 @@ public abstract class XmlElement {
     private final String namespace;
     private final String prefix;
 
-    private final List<XmlNamespace> namespaces = new LinkedList<XmlNamespace>();
+    private final List<XmlNamespace> namespaces = new ArrayList<XmlNamespace>();
 
     protected XmlElement(String name, String namespace, String prefix) {
         this.name = name;
