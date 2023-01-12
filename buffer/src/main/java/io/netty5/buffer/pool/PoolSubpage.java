5f4804b6d90e5be7845b21f47049326095988a55
jchrys <jchrys@me.com>
2023-01-13 02:32:00 +0900

    Remove redundant array initialization
    Motivation:
    JLS-10.3: An array initializer creates an array and provides initial values for all its components.
    
    This change was applied to Netty4(#12992), but not to Netty5
    
    Modification:
    Removed redundant array initialization
    
    Result:
    More efficient code

--- buffer/src/main/java/io/netty5/buffer/pool/PoolSubpage.java
+++ buffer/src/main/java/io/netty5/buffer/pool/PoolSubpage.java
@@ -68,10 +68,6 @@ final class PoolSubpage implements PoolSubpageMetric {
             if ((maxNumElems & 63) != 0) {
                 bitmapLength ++;
             }
-
-            for (int i = 0; i < bitmapLength; i ++) {
-                bitmap[i] = 0;
-            }
         }
         addToPool(head);
     }
