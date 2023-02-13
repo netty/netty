903fdccd61c7ea41e575e2d29989866fca18be8e
franz1981 <nigro.fra@gmail.com>
2023-02-13 14:12:39 +0100

    Revert "Revert "Speed-up HTTP 1.1 header and line parsing (#12321)""
    This reverts commit 9993e07356ea39edf14008789d3377d2bb62ea92.

--- common/src/main/java/io/netty/util/AsciiString.java
+++ common/src/main/java/io/netty/util/AsciiString.java
@@ -94,7 +94,9 @@ public final class AsciiString implements CharSequence, Comparable<CharSequence>
      */
     public AsciiString(byte[] value, int start, int length, boolean copy) {
         if (copy) {
-            this.value = Arrays.copyOfRange(value, start, start + length);
+            final byte[] rangedCopy = new byte[length];
+            System.arraycopy(value, start, rangedCopy, 0, rangedCopy.length);
+            this.value = rangedCopy;
             this.offset = 0;
         } else {
             if (isOutOfBounds(start, length, value.length)) {
--- microbench/src/main/java/io/netty/microbench/http/HttpRequestDecoderUtils.java
+++ microbench/src/main/java/io/netty/microbench/http/HttpRequestDecoderUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 The Netty Project
+ * Copyright 2023 The Netty Project
  *
  * The Netty Project licenses this file to you under the Apache License,
  * version 2.0 (the "License"); you may not use this file except in compliance
@@ -15,31 +15,15 @@
  */
 package io.netty.microbench.http;
 
-import io.netty.buffer.Unpooled;
-import io.netty.channel.embedded.EmbeddedChannel;
-import io.netty.handler.codec.http.HttpRequestDecoder;
-import io.netty.microbench.util.AbstractMicrobenchmark;
 import io.netty.util.CharsetUtil;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Param;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
 
-/**
- * This benchmark is based on HttpRequestDecoderTest class.
- */
-@State(Scope.Benchmark)
-@Warmup(iterations = 10)
-@Measurement(iterations = 20)
-public class HttpRequestDecoderBenchmark extends AbstractMicrobenchmark {
+final class HttpRequestDecoderUtils {
 
-    private static final byte[] CONTENT_MIXED_DELIMITERS = createContent("\r\n", "\n");
-    private static final int CONTENT_LENGTH = 120;
+    private HttpRequestDecoderUtils() {
+    }
 
-    @Param({ "2", "4", "8", "16", "32" })
-    public int step;
+    static final byte[] CONTENT_MIXED_DELIMITERS = createContent("\r\n", "\n");
+    static final int CONTENT_LENGTH = 120;
 
     private static byte[] createContent(String... lineDelimiters) {
         String lineDelimiter;
@@ -67,7 +51,7 @@ public class HttpRequestDecoderBenchmark extends AbstractMicrobenchmark {
                 "Sec-WebSocket-Key2: 8 Xt754O3Q3QW 0   _60" + lineDelimiter +
                 "Content-Type: application/x-www-form-urlencoded" + lineDelimiter2 +
                 "Content-Length: " + CONTENT_LENGTH + lineDelimiter +
-                "\r\n"  +
+                "\r\n" +
                 "1234567890\r\n" +
                 "1234567890\r\n" +
                 "1234567890\r\n" +
@@ -81,31 +65,4 @@ public class HttpRequestDecoderBenchmark extends AbstractMicrobenchmark {
         ).getBytes(CharsetUtil.US_ASCII);
     }
 
-    @Benchmark
-    public void testDecodeWholeRequestInMultipleStepsMixedDelimiters() {
-        testDecodeWholeRequestInMultipleSteps(CONTENT_MIXED_DELIMITERS, step);
-    }
-
-    private static void testDecodeWholeRequestInMultipleSteps(byte[] content, int fragmentSize) {
-        final EmbeddedChannel channel = new EmbeddedChannel(new HttpRequestDecoder());
-
-        final int headerLength = content.length - CONTENT_LENGTH;
-
-        // split up the header
-        for (int a = 0; a < headerLength;) {
-            int amount = fragmentSize;
-            if (a + amount > headerLength) {
-                amount = headerLength -  a;
-            }
-
-            // if header is done it should produce an HttpRequest
-            channel.writeInbound(Unpooled.wrappedBuffer(content, a, amount).asReadOnly());
-            a += amount;
-        }
-
-        for (int i = CONTENT_LENGTH; i > 0; i --) {
-            // Should produce HttpContent
-            channel.writeInbound(Unpooled.wrappedBuffer(content, content.length - i, 1).asReadOnly());
-        }
-    }
 }
