19e80f1c59b63e37c5f50eb66d71e9166d17d54f
Violeta Georgieva <violetag@vmware.com>
2023-05-17 09:08:11 +0300

    Do not throw exception when resolveCache and TTLs are both configured
    Motivation:
    The configured DnsNameResolverBuilder#TTLs (minTtl, maxTtl and negativeTtl) are used for creating
    default resolveCache, cnameCache and authoritativeDnsServerCache. Configuring a custom
    resolveCache(or cnameCache, or authoritativeDnsServerCache) does not mean that TTLs are not needed anymore
    for the rest of the caches that are not configured.
    With the current implementation there is no way for the user to specify a custom resolveCache,
    but to leave Netty to create cnameCache and authoritativeDnsServerCache.
    
    Modifications:
    - DnsNameResolverBuilder does not throw IllegalStateException, but logs when a custom
    resolveCache/cnameCache/authoritativeDnsServerCache and TTLs are configured, indicating
    that for the custom resolveCache/cnameCache/authoritativeDnsServerCache, the TTLs will be ignored.
    - junit tests are added
    
    Result:
    Fixes #13384

--- resolver-dns/src/main/java/io/netty/resolver/dns/DnsNameResolverBuilder.java
+++ resolver-dns/src/main/java/io/netty/resolver/dns/DnsNameResolverBuilder.java
@@ -25,6 +25,8 @@ import io.netty.resolver.HostsFileEntriesResolver;
 import io.netty.resolver.ResolvedAddressTypes;
 import io.netty.util.concurrent.Future;
 import io.netty.util.internal.ObjectUtil;
+import io.netty.util.internal.logging.InternalLogger;
+import io.netty.util.internal.logging.InternalLoggerFactory;
 
 import java.net.SocketAddress;
 import java.util.ArrayList;
@@ -38,6 +40,9 @@ import static io.netty.util.internal.ObjectUtil.intValue;
  * A {@link DnsNameResolver} builder.
  */
 public final class DnsNameResolverBuilder {
+
+    private static final InternalLogger logger = InternalLoggerFactory.getInstance(DnsNameResolverBuilder.class);
+
     volatile EventLoop eventLoop;
     private ChannelFactory<? extends DatagramChannel> channelFactory;
     private ChannelFactory<? extends SocketChannel> socketChannelFactory;
@@ -491,11 +496,15 @@ public final class DnsNameResolverBuilder {
         }
 
         if (resolveCache != null && (minTtl != null || maxTtl != null || negativeTtl != null)) {
-            throw new IllegalStateException("resolveCache and TTLs are mutually exclusive");
+            logger.debug("resolveCache and TTLs are mutually exclusive. TTLs are ignored.");
+        }
+
+        if (cnameCache != null && (minTtl != null || maxTtl != null || negativeTtl != null)) {
+            logger.debug("cnameCache and TTLs are mutually exclusive. TTLs are ignored.");
         }
 
         if (authoritativeDnsServerCache != null && (minTtl != null || maxTtl != null || negativeTtl != null)) {
-            throw new IllegalStateException("authoritativeDnsServerCache and TTLs are mutually exclusive");
+            logger.debug("authoritativeDnsServerCache and TTLs are mutually exclusive. TTLs are ignored.");
         }
 
         DnsCache resolveCache = this.resolveCache != null ? this.resolveCache : newCache();
