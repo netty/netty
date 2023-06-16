198b7604b75b43d98232d19ab5b89076eb6ae8b0
Norman Maurer <norman_maurer@apple.com>
2023-06-16 09:24:01 +0200

    Reuse Bootstrap to create SocketChannel for DNS TCP
    Motivation:
    
    There is no need to create a new Bootstrap each time we open a new connection.
    
    Modifications:
    
    - Create the Bootstrap only once
    - Tighten up some visibility
    
    Result:
    
    Produce less GC

--- resolver-dns/src/main/java/io/netty/resolver/dns/DnsResolveContext.java
+++ resolver-dns/src/main/java/io/netty/resolver/dns/DnsResolveContext.java
@@ -19,6 +19,7 @@ package io.netty.resolver.dns;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufHolder;
 import io.netty.channel.AddressedEnvelope;
+import io.netty.channel.Channel;
 import io.netty.channel.EventLoop;
 import io.netty.handler.codec.CorruptedFrameException;
 import io.netty.handler.codec.dns.DefaultDnsQuestion;
@@ -82,6 +83,7 @@ abstract class DnsResolveContext<T> {
             DnsResolveContext.class, "tryToFinishResolve(..)");
 
     final DnsNameResolver parent;
+    private final Channel channel;
     private final Promise<?> originalPromise;
     private final DnsServerAddressStream nameServerAddrs;
     private final String hostname;
@@ -98,12 +100,13 @@ abstract class DnsResolveContext<T> {
     private boolean triedCNAME;
     private boolean completeEarly;
 
-    DnsResolveContext(DnsNameResolver parent, Promise<?> originalPromise,
+    DnsResolveContext(DnsNameResolver parent, Channel channel, Promise<?> originalPromise,
                       String hostname, int dnsClass, DnsRecordType[] expectedTypes,
                       DnsRecord[] additionals, DnsServerAddressStream nameServerAddrs, int allowedQueries) {
         assert expectedTypes.length > 0;
 
         this.parent = parent;
+        this.channel = channel;
         this.originalPromise = originalPromise;
         this.hostname = hostname;
         this.dnsClass = dnsClass;
@@ -148,6 +151,13 @@ abstract class DnsResolveContext<T> {
     }
 
     /**
+     * The {@link Channel} used.
+     */
+    Channel channel() {
+        return channel;
+    }
+
+    /**
      * The {@link DnsCache} to use while resolving.
      */
     DnsCache resolveCache() {
@@ -171,7 +181,8 @@ abstract class DnsResolveContext<T> {
     /**
      * Creates a new context with the given parameters.
      */
-    abstract DnsResolveContext<T> newResolverContext(DnsNameResolver parent, Promise<?> originalPromise,
+    abstract DnsResolveContext<T> newResolverContext(DnsNameResolver parent, Channel channel,
+                                                     Promise<?> originalPromise,
                                                      String hostname,
                                                      int dnsClass, DnsRecordType[] expectedTypes,
                                                      DnsRecord[] additionals,
@@ -281,7 +292,7 @@ abstract class DnsResolveContext<T> {
     }
 
     void doSearchDomainQuery(String hostname, Promise<List<T>> nextPromise) {
-        DnsResolveContext<T> nextContext = newResolverContext(parent, originalPromise, hostname, dnsClass,
+        DnsResolveContext<T> nextContext = newResolverContext(parent, channel, originalPromise, hostname, dnsClass,
                                                               expectedTypes, additionals, nameServerAddrs,
                 parent.maxQueriesPerResolve());
         nextContext.internalResolve(hostname, nextPromise);
@@ -432,7 +443,7 @@ abstract class DnsResolveContext<T> {
             return;
         }
         final Promise<AddressedEnvelope<? extends DnsResponse, InetSocketAddress>> queryPromise =
-                parent.ch.eventLoop().newPromise();
+                channel.eventLoop().newPromise();
 
         final long queryStartTimeNanos;
         final boolean isFeedbackAddressStream;
@@ -540,7 +551,7 @@ abstract class DnsResolveContext<T> {
         if (!DnsNameResolver.doResolveAllCached(nameServerName, additionals, resolverPromise, resolveCache,
                 parent.resolvedInternetProtocolFamiliesUnsafe())) {
 
-            new DnsAddressResolveContext(parent, originalPromise, nameServerName, additionals,
+            new DnsAddressResolveContext(parent, channel, originalPromise, nameServerName, additionals,
                                          parent.newNameServerAddressStream(nameServerName),
                                          // Resolving the unresolved nameserver must be limited by allowedQueries
                                          // so we eventually fail
@@ -843,7 +854,7 @@ abstract class DnsResolveContext<T> {
                         if (logger.isDebugEnabled()) {
                             logger.debug("{} Ignoring record {} for [{}: {}] as it contains a different name than " +
                                             "the question name [{}]. Cnames: {}, Search domains: {}",
-                                    parent.ch, r.toString(), response.id(), envelope.sender(), questionName, cnames,
+                                    channel, r.toString(), response.id(), envelope.sender(), questionName, cnames,
                                     parent.searchDomains());
                         }
                         continue;
@@ -856,7 +867,7 @@ abstract class DnsResolveContext<T> {
                 if (logger.isDebugEnabled()) {
                     logger.debug("{} Ignoring record {} for [{}: {}] as the converted record is null. "
                                     + "Hostname [{}], Additionals: {}",
-                            parent.ch, r.toString(), response.id(), envelope.sender(), hostname, additionals);
+                            channel, r.toString(), response.id(), envelope.sender(), hostname, additionals);
                 }
                 continue;
             }
