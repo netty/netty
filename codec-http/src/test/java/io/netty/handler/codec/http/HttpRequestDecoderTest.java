1c79a72d96504d35b76512cc3c4ee4523853752f
Chris Vest <christianvest_hansen@apple.com>
2023-07-13 21:54:19 -0700

    Fix test

--- codec-http/src/test/java/io/netty/handler/codec/http/HttpRequestDecoderTest.java
+++ codec-http/src/test/java/io/netty/handler/codec/http/HttpRequestDecoderTest.java
@@ -633,6 +633,7 @@ public class HttpRequestDecoderTest {
         assertEquals("x1", headers.get("X-1-Header"));
         assertEquals("x2", headers.get("X-2-Header"));
         assertEquals("x3", headers.get("X-3-Header"));
+        assertEquals(LastHttpContent.EMPTY_LAST_CONTENT, channel.readInbound());
         assertFalse(channel.finish());
     }
 
