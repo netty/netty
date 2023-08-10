50fde6012c3ed4542fac370012b06135ecccb864
Jacob Kiefer <jacobkiefer@google.com>
2023-08-10 10:18:11 -0700

    Marks lazily-initialized 'codeClass' field as final to fix TSAN data race (#13537)
    Motivation:
    
    Besides the `codeClass` field, `HttpStatusClass` has only final fields, so it's safe to do an unsynchronized write to codeClass in the Java memory model.
    However, TSAN complains about unsafe writes to the `codeClass` field.
    
    Modifications:
    
    Make the 'codeClass' field as `final` and no longer lazily-initialized.
    
    Result:
    
    All fields in `HttpResponseStatus` are now consistently `final`.
    TSAN checks are happy.

--- codec-http/src/main/java/io/netty5/handler/codec/http/HttpResponseStatus.java
+++ codec-http/src/main/java/io/netty5/handler/codec/http/HttpResponseStatus.java
@@ -527,7 +527,7 @@ public class HttpResponseStatus implements Comparable<HttpResponseStatus> {
 
     private final int code;
     private final AsciiString codeAsText;
-    private HttpStatusClass codeClass;
+    private final HttpStatusClass codeClass;
 
     private final String reasonPhrase;
     private final byte[] bytes;
@@ -563,6 +563,7 @@ public class HttpResponseStatus implements Comparable<HttpResponseStatus> {
         }
 
         this.code = code;
+        codeClass = HttpStatusClass.valueOf(code);
         String codeString = Integer.toString(code);
         codeAsText = new AsciiString(codeString);
         this.reasonPhrase = reasonPhrase;
@@ -598,11 +599,7 @@ public class HttpResponseStatus implements Comparable<HttpResponseStatus> {
      * Returns the class of this {@link HttpResponseStatus}
      */
     public HttpStatusClass codeClass() {
-        HttpStatusClass type = codeClass;
-        if (type == null) {
-            codeClass = type = HttpStatusClass.valueOf(code);
-        }
-        return type;
+        return codeClass;
     }
 
     @Override
