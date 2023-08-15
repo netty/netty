1c6e09114db3c04ebba971ef049eb89e3e27a512
Lincong Li <lincong.li@databricks.com>
2023-08-14 22:08:40 -0700

    wip 4

--- codec-http2/src/test/java/io/netty/handler/codec/http2/HpackEncoderTest.java
+++ codec-http2/src/test/java/io/netty/handler/codec/http2/HpackEncoderTest.java
@@ -206,40 +206,44 @@ public class HpackEncoderTest {
 
     @Test
     public void testSanitization() throws Http2Exception {
-        int smallHeaderValueSize = 10;
-        int mediumHeaderValueSize = 300;
-        int largeHeaderValueSize = 2000;
-        verifyHeaderValueSanitization(smallHeaderValueSize);
-        verifyHeaderValueSanitization(mediumHeaderValueSize);
-        verifyHeaderValueSanitization(largeHeaderValueSize);
+        final int headerValueSize = 300;
+        StringBuilder actualHeaderValueBuilder = new StringBuilder();
+        StringBuilder expectedHeaderValueBuilder = new StringBuilder();
+
+        for (int i = 0; i < headerValueSize; i++) {
+            actualHeaderValueBuilder.append((char) i); // Use the index as the code point value of the character.
+            if (i <= 255) {
+                expectedHeaderValueBuilder.append((char) i);
+            } else {
+                expectedHeaderValueBuilder.append('?'); // Expect this character to be sanitized.
+            }
+        }
+        String actualHeaderValue = actualHeaderValueBuilder.toString();
+        String expectedHeaderValue = expectedHeaderValueBuilder.toString();
+        HpackEncoder encoderWithHuffmanEncoding =
+                new HpackEncoder(false, 64, 0); // Low Huffman code threshold.
+        HpackEncoder encoderWithoutHuffmanEncoding =
+                new HpackEncoder(false, 64, Integer.MAX_VALUE); // High Huffman code threshold.
+
+        // Expect the same decoded header value regardless of whether Huffman encoding is enabled or not.
+        verifyHeaderValueSanitization(encoderWithHuffmanEncoding, actualHeaderValue, expectedHeaderValue);
+        verifyHeaderValueSanitization(encoderWithoutHuffmanEncoding, actualHeaderValue, expectedHeaderValue);
     }
 
-    private void verifyHeaderValueSanitization(int headerValueSize) throws Http2Exception {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < headerValueSize; i++) {
-            sb.append((char) i); // Use the index as the code point value of the character.
-        }
-        String headerValue = sb.toString();
-        String headerKey = "some-key";
-        Http2Headers toBeEncodedHeaders = new DefaultHttp2Headers().add(headerKey, headerValue);
+    private void verifyHeaderValueSanitization(
+            HpackEncoder encoder,
+            String actualHeaderValue,
+            String expectedHeaderValue
+    ) throws Http2Exception {
 
-        hpackEncoder.encodeHeaders(0, buf, toBeEncodedHeaders, Http2HeadersEncoder.NEVER_SENSITIVE);
+        String headerKey = "some-key";
+        Http2Headers toBeEncodedHeaders = new DefaultHttp2Headers().add(headerKey, actualHeaderValue);
+        encoder.encodeHeaders(0, buf, toBeEncodedHeaders, Http2HeadersEncoder.NEVER_SENSITIVE);
         DefaultHttp2Headers decodedHeaders = new DefaultHttp2Headers();
         hpackDecoder.decode(0, buf, decodedHeaders, true);
         buf.clear();
-
-        CharSequence decodedHeaderValue = decodedHeaders.get(headerKey);
-        System.out.println(decodedHeaderValue);
-        for (int i = 0; i < headerValueSize; i++) {
-            char c = decodedHeaderValue.charAt(i);
-            if (i > 255) {
-                // All characters outside the range should be sanitized.
-                Assertions.assertEquals('?', c, "Should be sanitized.");
-            } else {
-                char expectedChar = (char) i;
-                Assertions.assertEquals(expectedChar, c, "Should not be sanitized.");
-            }
-        }
+        String decodedHeaderValue = decodedHeaders.get(headerKey).toString();
+        Assertions.assertEquals(expectedHeaderValue, decodedHeaderValue);
     }
 
     private void setMaxTableSize(int maxHeaderTableSize) throws Http2Exception {
