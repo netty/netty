4e6147166c224e45ba88a16865383edf20581c75
jchrys <jchrys@me.com>
2023-08-20 00:56:59 +0900

    Enhance JavaDoc

--- common/src/main/java/io/netty/util/AsciiStringUtil.java
+++ common/src/main/java/io/netty/util/AsciiStringUtil.java
@@ -410,29 +410,56 @@ public final class AsciiStringUtil {
         return true;
     }
 
+    /**
+     * A collection methods for performing operations using SWAR (SIMD within a register) techniques.
+     */
     public static final class SWARByteUtil {
+
+        /**
+         * Compiles given byte into a long pattern suitable for SWAR operations.
+         */
         public static long compilePattern(final byte byteToFind) {
             return (byteToFind & 0xFFL) * 0x101010101010101L;
         }
 
+        /**
+         * Returns the index of the first occurrence of the specified pattern in the specified word.
+         * If no pattern is found, returns 8.
+         */
+        public static int firstAnyPattern(final long word, final long pattern, final boolean leading) {
+            long tmp = applyPattern(word, pattern);
+            final int binaryPosition = leading? Long.numberOfLeadingZeros(tmp) : Long.numberOfTrailingZeros(tmp);
+            return binaryPosition >>> 3;
+        }
+
+        /**
+         * Applies a compiled pattern to given word.
+         * Returns a word where each byte that matches the pattern has the highest bit set.
+         */
         private static long applyPattern(final long word, final long pattern) {
             long input = word ^ pattern;
             long tmp = (input & 0x7F7F7F7F7F7F7F7FL) + 0x7F7F7F7F7F7F7F7FL;
             return ~(tmp | input | 0x7F7F7F7F7F7F7F7FL);
         }
 
-        public static int getIndex(final long mask, final boolean isBigEndian) {
-            return isBigEndian? Long.numberOfLeadingZeros(mask) >>> 3 : Long.numberOfTrailingZeros(mask) >>> 3;
+        /**
+         * Returns the index of the first occurrence of byte that specificied in the pattern.
+         * If no pattern is found, returns 8.
+         *
+         * @param word     the return value of {@link #applyPattern(long, long)}
+         * @param isNative if ture, if given word is big endian
+         *                 if false, if given word is little endian
+         * @return the index of the first occurrence of the specified pattern in the specified word.
+         * If no pattern is found, returns 8.
+         */
+        public static int getIndex(final long word, final boolean isNative) {
+            final int tmp = isNative? Long.numberOfLeadingZeros(word) : Long.numberOfTrailingZeros(word);
+            return tmp >>> 3;
         }
 
-        public static int firstAnyPattern(final long word, final long pattern, final boolean leading) {
-            long input = word ^ pattern;
-            long tmp = (input & 0x7F7F7F7F7F7F7F7FL) + 0x7F7F7F7F7F7F7F7FL;
-            tmp = ~(tmp | input | 0x7F7F7F7F7F7F7F7FL);
-            final int binaryPosition = leading? Long.numberOfLeadingZeros(tmp) : Long.numberOfTrailingZeros(tmp);
-            return binaryPosition >>> 3;
-        }
-
+        /**
+         * Returns a word where each ASCII uppercase byte has the highest bit set.
+         */
         private static long applyUpperCasePattern(final long word) {
             long rotated = word & 0x7F7F7F7F7F7F7F7FL;
             rotated += 0x2525252525252525L;
@@ -443,6 +470,9 @@ public final class AsciiStringUtil {
             return rotated;
         }
 
+        /**
+         * Returns a word where each ASCII uppercase byte has the highest bit set.
+         */
         private static int applyUpperCasePattern(final int word) {
             int rotated = word & 0x7F7F7F7F;
             rotated += 0x25252525;
@@ -453,6 +483,9 @@ public final class AsciiStringUtil {
             return rotated;
         }
 
+        /**
+         * Returns a word where each ASCII lowercase byte has the highest bit set.
+         */
         private static long applyLowerCasePattern(final long word) {
             long rotated = word & 0x7F7F7F7F7F7F7F7FL;
             rotated += 0x0505050505050505L;
@@ -463,6 +496,9 @@ public final class AsciiStringUtil {
             return rotated;
         }
 
+        /**
+         * Returns a word where each lowercase ASCII byte has the highest bit set.
+         */
         private static int applyLowerCasePattern(final int word) {
             int rotated = word & 0x7F7F7F7F;
             rotated += 0x05050505;
@@ -473,37 +509,61 @@ public final class AsciiStringUtil {
             return rotated;
         }
 
+        /**
+         * Returns true if the given word contains at least one ASCII uppercase byte.
+         */
         static boolean containsUpperCase(final long word) {
             return applyUpperCasePattern(word) != 0;
         }
 
+        /**
+         * Returns true if the given word contains at least one ASCII uppercase byte.
+         */
         static boolean containsUpperCase(final int word) {
             return applyUpperCasePattern(word) != 0;
         }
 
+        /**
+         * Returns true if the given word contains at least one ASCII lowercase byte.
+         */
         static boolean containsLowerCase(final long word) {
             return applyLowerCasePattern(word) != 0;
         }
 
+        /**
+         * Returns true if the given word contains at least one ASCII lowercase byte.
+         */
         static boolean containsLowerCase(final int word) {
             return applyLowerCasePattern(word) != 0;
         }
 
+        /**
+         * Returns a word with all bytes converted to lowercase ASCII.
+         */
         static long toLowerCase(final long word) {
             final long mask = applyUpperCasePattern(word) >>> 2;
             return word | mask;
         }
 
+        /**
+         * Returns a word with all bytes converted to lowercase ASCII.
+         */
         static int toLowerCase(final int word) {
             final int mask = applyUpperCasePattern(word) >>> 2;
             return word | mask;
         }
 
+        /**
+         * Returns a word with all bytes converted to uppercase ASCII.
+         */
         static long toUpperCase(final long word) {
             final long mask = applyLowerCasePattern(word) >>> 2;
             return word & ~mask;
         }
 
+        /**
+         * Returns a word with all bytes converted to uppercase ASCII.
+         */
         static int toUpperCase(final int word) {
             final int mask = applyLowerCasePattern(word) >>> 2;
             return word & ~mask;
