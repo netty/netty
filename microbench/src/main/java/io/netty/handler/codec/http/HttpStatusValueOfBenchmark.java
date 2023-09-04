161fa11a487630b9881ea28b6141feaea59933bf
laosijikaichele <laosijikaichele>
2023-09-04 20:46:39 +0800

    Optimize JMH test code

--- microbench/src/main/java/io/netty/handler/codec/http/HttpStatusValueOfBenchmark.java
+++ microbench/src/main/java/io/netty/handler/codec/http/HttpStatusValueOfBenchmark.java
@@ -15,6 +15,7 @@
  */
 package io.netty.handler.codec.http;
 import io.netty.microbench.util.AbstractMicrobenchmark;
+import io.netty.util.internal.SuppressJava6Requirement;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Level;
@@ -25,12 +26,12 @@ import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
-import io.netty.util.internal.SuppressJava6Requirement;
-import org.openjdk.jmh.profile.LinuxPerfNormProfiler;
+import org.openjdk.jmh.profile.*;
 import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
-
-import java.math.BigDecimal;
+import org.openjdk.jmh.runner.options.ProfilerConfig;
 import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.SplittableRandom;
 import java.util.concurrent.TimeUnit;
 
@@ -38,153 +39,168 @@ import java.util.concurrent.TimeUnit;
 @Warmup(iterations = 10, time = 1)
 @Measurement(iterations = 10, time = 1)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
+@SuppressJava6Requirement(reason = "suppress")
 public class HttpStatusValueOfBenchmark extends AbstractMicrobenchmark {
-    private int[] data;
-    private HttpStatusClass[] result;
     @Param({"1300", "2600", "5300", "11000", "23000"})
     private int size;
-    private final BigDecimal bdZero = new BigDecimal("0.00");
-    private final BigDecimal bdOne = new BigDecimal("1.00");
-    private final BigDecimal bdLowest = new BigDecimal("0.01");
-    private final DecimalFormat df = new DecimalFormat("##.##%");
+    private static final SplittableRandom random = new SplittableRandom();
+    private static final DecimalFormat df = new DecimalFormat("##.##%");
+    private CircularLink head;
+    private static final CircularLink polluteDistributeHead;
+    private static final Map<Integer, CircularLink> sizeMap = new HashMap<Integer, CircularLink>();
+
+    static {
+        sizeMap.put(1300, initCircularLink(1300));
+        sizeMap.put(2600, initCircularLink(2600));
+        sizeMap.put(5300, initCircularLink(5300));
+        sizeMap.put(11000, initCircularLink(11000));
+        sizeMap.put(23000, initCircularLink(23000));
+        polluteDistributeHead = initCircularLink(16000);
+        fillPolluteDistributeData(16000, polluteDistributeHead);
+    }
+
+    @Setup(Level.Invocation)
+    public void setup(Blackhole bh) {
+        // Pollute the branch predictor.
+        CircularLink cl = polluteDistributeHead;
+        do {
+            bh.consume(HttpStatusClass.valueOf(cl.value));
+        } while ((cl = cl.next) != polluteDistributeHead);
+
+        head = sizeMap.get(size);
+        // Fill benchmark data.
+        fillBenchMarkData(size, head);
+    }
+
+    @Benchmark
+    public void valueOf(Blackhole bh) {
+        CircularLink cl = head;
+        do {
+            bh.consume(HttpStatusClass.valueOf(cl.value));
+        } while ((cl = cl.next) != head);
+    }
+
     public HttpStatusValueOfBenchmark() {
         // disable assertion
         super(true);
     }
 
-    @Setup(Level.Iteration)
-    @SuppressJava6Requirement(reason = "suppress")
-    public void setup(Blackhole bh) {
-        if (size < 100) {
-            throw new IllegalArgumentException("The size MUST > 100");
-        }
-        final SplittableRandom random = new SplittableRandom();
-        // Equal the branch predictor.
-        int equalDistributedArraySize = 16000;
-        int[] dataArrayIndex = new int[equalDistributedArraySize];
-        initDistributedData("dataArrayIndex", dataArrayIndex, random,
-                0.166, 0.166, 0.166, 0.166,
-                0.166, 0.166, 0.0);
-        for (int i = 0; i < equalDistributedArraySize; i++) {
-            HttpStatusClass rs = HttpStatusClass.valueOf(dataArrayIndex[i]);
-            bh.consume(rs);
-        }
-        data = new int[size];
-        result = new HttpStatusClass[size];
-        // Generate bench mark data.
-        initDistributedData("data", data, random, 0.38, 0.30, 0.15,
-                0.10, 0.05, 0.02, 0.0);
-    }
-
-    @SuppressJava6Requirement(reason = "suppress")
-    private void initDistributedData(String desc, int[] setUpData, SplittableRandom random, double informationalRatio,
-                                     double successRatio, double redirectionRatio, double clientErrorRatio,
-                                     double serverErrorRatio, double unknownRatio, double negativeRatio) {
-        BigDecimal[] bdArray = { BigDecimal.valueOf(informationalRatio), BigDecimal.valueOf(successRatio),
-                BigDecimal.valueOf(redirectionRatio), BigDecimal.valueOf(clientErrorRatio),
-                BigDecimal.valueOf(serverErrorRatio), BigDecimal.valueOf(unknownRatio),
-                BigDecimal.valueOf(negativeRatio) };
-        validateRatios(bdArray);
-
-        int totalCount = 0;
-        int informationalCount = (int) (setUpData.length * informationalRatio);
-        totalCount += informationalCount;
-        int successCount = (int) (setUpData.length * successRatio);
-        totalCount += successCount;
-        int redirectionCount = (int) (setUpData.length * redirectionRatio);
-        totalCount += redirectionCount;
-        int clientErrorCount = (int) (setUpData.length * clientErrorRatio);
-        totalCount += clientErrorCount;
-        int serverErrorCount = (int) (setUpData.length * serverErrorRatio);
-        totalCount += serverErrorCount;
-        int unknownCount = (int) (setUpData.length * unknownRatio);
-        totalCount += unknownCount;
-        int negativeCount = (int) (setUpData.length * negativeRatio);
-        totalCount += negativeCount;
-
-        double c1x = 0, c2x = 0, c3x = 0, c4x = 0, c5x = 0, c6x = 0, c7x = 0;
-        for (int i = 0; i < totalCount;) {
-            int code = random.nextInt(100, 800);
-            if (HttpStatusClass.INFORMATIONAL.contains(code) && informationalCount-- > 0) {
-                setUpData[i++] = code;
+    private static void fillBenchMarkData(int size, CircularLink head) {
+        double c1x = 0, c2x = 0, c3x = 0, c4x = 0, c5x = 0, c6x = 0;
+        CircularLink cl = head;
+        do {
+            // [0, 100)
+            int code = random.nextInt(0, 100);
+            // 38%
+            if (code < 38) {
+                cl.value = random.nextInt(100, 200);
                 ++c1x;
+                continue;
             }
-            if (HttpStatusClass.SUCCESS.contains(code) && successCount-- > 0) {
-                setUpData[i++] = code;
+            // 30%
+            if (code < 68) {
+                cl.value = random.nextInt(200, 300);
                 ++c2x;
+                continue;
             }
-            if (HttpStatusClass.REDIRECTION.contains(code) && redirectionCount-- > 0) {
-                setUpData[i++] = code;
+            // 15%
+            if (code < 83) {
+                cl.value = random.nextInt(300, 400);
                 ++c3x;
+                continue;
             }
-            if (HttpStatusClass.CLIENT_ERROR.contains(code) && clientErrorCount-- > 0) {
-                setUpData[i++] = code;
+            // 10%
+            if (code < 93) {
+                cl.value = random.nextInt(400, 500);
                 ++c4x;
+                continue;
             }
-            if (HttpStatusClass.SERVER_ERROR.contains(code) && serverErrorCount-- > 0) {
-                setUpData[i++] = code;
+            // 5%
+            if (code < 98) {
+                cl.value = random.nextInt(500, 600);
                 ++c5x;
+                continue;
             }
-            // UNKNOWN:[600,700)
-            if (code >= 600 && code < 700 && unknownCount-- > 0) {
-                int origin = BigDecimal.valueOf(negativeRatio).compareTo(bdZero) > 0 ? 0 : Integer.MIN_VALUE;
-                // Re-generate 'UNKNOWN' code.
-                do {
-                    code = random.nextInt(origin, Integer.MAX_VALUE);
-                } while (code >= 100 && code < 600);
-                setUpData[i++] = code;
-                ++c6x;
-            }
-            // Negative:[700,800)
-            if (code >= 700 && negativeCount-- > 0) {
-                // Re-generate Negative code.
-                code = random.nextInt(Integer.MIN_VALUE, 0);
-                setUpData[i++] = code;
-                ++c7x;
-            }
-        }
+            // 2%
+            cl.value = random.nextInt(-50, 50);
+            ++c6x;
+        } while (head != (cl = cl.next));
+//        printCodePercentage("initBenchMarkData", size, c1x, c2x, c3x, c4x, c5x, c6x);
+    }
 
-        for (int i = totalCount - 1; i < setUpData.length; i++) {
-            // Generate remaining elements from scope 1xx to 5xx
-            int code = random.nextInt(100, 600);
-            setUpData[i] = code;
-            if (HttpStatusClass.INFORMATIONAL.contains(code)) {
+    private static void fillPolluteDistributeData(int size, CircularLink head) {
+        double c1x = 0, c2x = 0, c3x = 0, c4x = 0, c5x = 0, c6x = 0;
+        CircularLink cl = head;
+        do {
+            // [0, 96)
+            int code = random.nextInt(0, 96);
+            // (100/6) %
+            if (code < 16) {
+                cl.value = random.nextInt(100, 200);
                 ++c1x;
+                continue;
             }
-            if (HttpStatusClass.SUCCESS.contains(code)) {
+            // (100/6) %
+            if (code < 32) {
+                cl.value = random.nextInt(200, 300);
                 ++c2x;
+                continue;
             }
-            if (HttpStatusClass.REDIRECTION.contains(code)) {
+            // (100/6) %
+            if (code < 48) {
+                cl.value = random.nextInt(300, 400);
                 ++c3x;
+                continue;
             }
-            if (HttpStatusClass.CLIENT_ERROR.contains(code)) {
+            // (100/6) %
+            if (code < 64) {
+                cl.value = random.nextInt(400, 500);
                 ++c4x;
+                continue;
             }
-            if (HttpStatusClass.SERVER_ERROR.contains(code)) {
+            // (100/6) %
+            if (code < 80) {
+                cl.value = random.nextInt(500, 600);
                 ++c5x;
+                continue;
             }
-        }
-//        printCodePercentage(desc, setUpData.length, c1x, c2x, c3x, c4x, c5x, c6x, c7x);
+            // (100/6) %
+            cl.value = random.nextInt(-50, 50);
+            ++c6x;
+        } while (head != (cl = cl.next));
+//        printCodePercentage("fillEqualDistributeData", size, c1x, c2x, c3x, c4x, c5x, c6x);
     }
 
-    private void validateRatios(BigDecimal[] bdArray) {
-        BigDecimal bdSum = new BigDecimal("0.00");
-        for (BigDecimal bdParam : bdArray) {
-            if (bdParam.compareTo(bdZero) < 0) {
-                throw new IllegalArgumentException("Ratio can NOT be negative");
-            }
-            if (bdParam.compareTo(bdZero) > 0 && bdParam.compareTo(bdLowest) < 0) {
-                throw new IllegalArgumentException("If ratio != 0, then the ratio MUST >= 0.01");
-            }
-            bdSum = bdSum.add(bdParam);
+    private static CircularLink initCircularLink(int size) {
+        CircularLink cl = new CircularLink();
+        CircularLink head = cl;
+        for (int i = 0 ; i < size - 1; i++) {
+            cl.next = new CircularLink();
+            cl = cl.next;
         }
-        if (bdSum.compareTo(bdOne) > 0) {
-            throw new IllegalArgumentException("Sum of ratios MUST <= 1");
-        }
+        cl.next = head;
+        return head;
     }
 
-    private void printCodePercentage(String desc, int length, double c1x, double c2x, double c3x, double c4x,
-                                     double c5x, double c6x, double c7x) {
+    private static final class CircularLink {
+        private CircularLink next = null;
+        private int value;
+    }
+
+    @Override
+    protected ChainedOptionsBuilder newOptionsBuilder() throws Exception {
+        Class<LinuxPerfNormProfiler> profilerClass = LinuxPerfNormProfiler.class;
+        try {
+            ProfilerFactory.getProfilerOrException(new ProfilerConfig(profilerClass.getCanonicalName()));
+        } catch (ProfilerException t) {
+            // Fall back to default.
+            return super.newOptionsBuilder();
+        }
+        return super.newOptionsBuilder().addProfiler(profilerClass);
+    }
+
+    private static void printCodePercentage(String desc, int length, double c1x, double c2x, double c3x, double c4x,
+                                            double c5x, double c6x) {
         System.out.println("\n" + desc + "===>"
                 + "INFORMATIONAL:" + df.format(c1x / length)
                 + ", SUCCESS:" + df.format(c2x / length)
@@ -192,20 +208,6 @@ public class HttpStatusValueOfBenchmark extends AbstractMicrobenchmark {
                 + ", CLIENT_ERROR:" + df.format(c4x / length)
                 + ", SERVER_ERROR:" + df.format(c5x / length)
                 + ", UNKNOWN:" + df.format(c6x / length)
-                + ", NEGATIVE:" + df.format(c7x / length)
         );
-    }
-
-    @Override
-    protected ChainedOptionsBuilder newOptionsBuilder() throws Exception {
-        return super.newOptionsBuilder().addProfiler(LinuxPerfNormProfiler.class);
-    }
-
-    @Benchmark
-    public HttpStatusClass[] valueOf() {
-        for (int i = 0; i < size; ++i) {
-            result[i] = HttpStatusClass.valueOf(data[i]);
-        }
-        return result;
     }
 }
