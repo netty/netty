93ac861f917ebdc489b0f6089dd8cabbfad5b7b3
yawkat <jonas.konrad@oracle.com>
2023-12-15 11:19:58 +0100

    Failing test: Reentrant close in HttpRequestDecoder Discovered by fuzzing, but I'm not sure whether this can actually happen outside EmbeddedChannel.
    Normally, HttpRequestDecoder will strictly follow the pattern `(HttpRequest HttpContent* LastHttpContent)*` for downstream handlers. However, when a downstream handler closes the channel inside a `channelRead` for a `HttpRequest`, this may reorder inputs, eg `HttpRequest HttpRequest LastHttpContent LastHttpContent`. This is mostly ByteToMessageDecoder's fault, and I'm not sure how to fix it in a performance-neutral way. Basically, `HttpRequestDecoder.channelInactive` is called while `HttpRequestDecoder.channelRead` is still running, so the next request gets inserted mid-request.
    
    If this can only happen in EmbeddedChannel (which I'm not sure of), then we should just fix EmbeddedChannel instead.

--- codec-http/src/test/java/io/netty/handler/codec/http/HttpRequestDecoderTest.java
+++ codec-http/src/test/java/io/netty/handler/codec/http/HttpRequestDecoderTest.java
@@ -17,9 +17,13 @@ package io.netty.handler.codec.http;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.util.AsciiString;
 import io.netty.util.CharsetUtil;
+import io.netty.util.ReferenceCountUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -636,6 +640,44 @@ public class HttpRequestDecoderTest {
         LastHttpContent last = channel.readInbound();
         assertEquals(LastHttpContent.EMPTY_LAST_CONTENT, last);
         last.release();
+        assertFalse(channel.finish());
+    }
+
+    @Test
+    void reentrantClose() throws Exception {
+        String requestStr = "GET / HTTP/1.1\r\n" +
+                "Host: example.com\r\n" +
+                "Content-Length: 0\r\n" +
+                "\r\n" +
+                "GET / HTTP/1.1\r\n" +
+                "Host: example.com\r\n" +
+                "Content-Length: 0\r\n" +
+                "\r\n";
+        EmbeddedChannel channel = new EmbeddedChannel(new HttpRequestDecoder(), new ChannelInboundHandlerAdapter() {
+            int i = 0;
+
+            @Override
+            public void channelRead(ChannelHandlerContext ctx, Object msg) {
+                if (i == 0) {
+                    Assertions.assertInstanceOf(HttpRequest.class, msg);
+                } else if (i == 1) {
+                    Assertions.assertInstanceOf(LastHttpContent.class, msg);
+                } else if (i == 2) {
+                    Assertions.assertInstanceOf(HttpRequest.class, msg);
+                } else if (i == 3) {
+                    Assertions.assertInstanceOf(LastHttpContent.class, msg);
+                }
+                ReferenceCountUtil.release(msg);
+                i++;
+
+                if (i == 1) {
+                    // first request
+                    ctx.close();
+                }
+            }
+        });
+
+        assertFalse(channel.writeInbound(Unpooled.copiedBuffer(requestStr, CharsetUtil.US_ASCII)));
         assertFalse(channel.finish());
     }
 
