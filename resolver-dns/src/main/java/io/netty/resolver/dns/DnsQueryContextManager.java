aef3ad0faf1cc2b8e753fa936492730572bfb24c
Norman Maurer <norman_maurer@apple.com>
2024-01-23 07:32:53 -0800

    DnsNameResolver: Add DnsQueryIdSpace class to reduce overhead while g… (#13795)
    …enerating IDs
    
    Motivation:
    
    How we currently implement the selection of the next id to use for a
    query is very inefficient, especially once we already have a lot of
    inflight queries. This is problematic as we basically "block" the
    eventloop while doing the for loop.
    
    Modifications:
    
    Add new datastructure for storing and selecting ids that provides better
    performance
    
    Result:
    
    Less overhead while selecting ids

--- resolver-dns/src/main/java/io/netty/resolver/dns/DnsQueryContextManager.java
+++ resolver-dns/src/main/java/io/netty/resolver/dns/DnsQueryContextManager.java
@@ -19,7 +19,6 @@ package io.netty.resolver.dns;
 import io.netty.util.NetUtil;
 import io.netty.util.collection.IntObjectHashMap;
 import io.netty.util.collection.IntObjectMap;
-import io.netty.util.internal.PlatformDependent;
 
 import java.net.Inet4Address;
 import java.net.Inet6Address;
@@ -154,29 +153,17 @@ final class DnsQueryContextManager {
     }
 
     private static final class DnsQueryContextMap {
-        private static final int MAX_ID = 65535;
-        private static final int MAX_TRIES = MAX_ID << 1;
+
+        private final DnsQueryIdSpace idSpace = new DnsQueryIdSpace();
 
         // We increment on every usage so start with -1, this will ensure we start with 0 as first id.
         private final IntObjectMap<DnsQueryContext> map = new IntObjectHashMap<DnsQueryContext>();
 
         synchronized int add(DnsQueryContext ctx) {
-            int tries = 0;
-            int id = PlatformDependent.threadLocalRandom().nextInt(MAX_ID - 1) + 1;
-            for (;;) {
-                // Let's directly use put as its very unlikely that we still have the id in use.
-                DnsQueryContext oldCtx = map.put(id, ctx);
-                if (oldCtx == null) {
-                    return id;
-                }
-                // Restore the mapping to the old context.
-                map.put(id, oldCtx);
-
-                id = id + 1 & 0xFFFF;
-                if (++tries >= MAX_TRIES) {
-                    return -1;
-                }
-            }
+            int id = idSpace.nextId();
+            DnsQueryContext oldCtx = map.put(id, ctx);
+            assert oldCtx == null;
+            return id;
         }
 
         synchronized DnsQueryContext get(int id) {
@@ -184,6 +171,7 @@ final class DnsQueryContextManager {
         }
 
         synchronized DnsQueryContext remove(int id) {
+            idSpace.pushId(id);
             return map.remove(id);
         }
     }
