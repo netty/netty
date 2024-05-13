1e1a66a18d31e2669f49d2cd5789671bd3115dbd
Francesco Nigro <nigro.fra@gmail.com>
2024-05-13 13:03:11 +0200

    Added the bonzini version

--- codec/src/main/java/io/netty/handler/codec/protobuf/ProtobufVarint32FrameDecoder.java
+++ codec/src/main/java/io/netty/handler/codec/protobuf/ProtobufVarint32FrameDecoder.java
@@ -84,12 +84,18 @@ public class ProtobufVarint32FrameDecoder extends ByteToMessageDecoder {
         int shiftToHighlightTheWhole = 32 - bitsToKeep;
         // this is not brilliant, we have a data dependency here
         int wholeWithContinuations = (wholeOrMore << shiftToHighlightTheWhole) >> shiftToHighlightTheWhole;
-        // mix them up as per varint spec while dropping the continuation bits
-        int result = wholeWithContinuations & 0x7F |
-                     (((wholeWithContinuations >> 8) & 0x7F) << 7) |
-                     (((wholeWithContinuations >> 16) & 0x7F) << 14) |
-                     (((wholeWithContinuations >> 24) & 0x7F) << 21);
-        return result;
+        // mix them up as per varint spec while dropping the continuation bits:
+        // 0x7F007F isolate the first byte and the third byte dropping the continuation bits
+        // 0x7F007F00 isolate the second byte and the fourth byte dropping the continuation bits
+        // the second and fourth byte are shifted to the right by 1, filling the gaps left by the first and third byte
+        // it means that the first and second bytes now occupy the first 14 bits (7 bits each)
+        // and the third and fourth bytes occupy the next 14 bits (7 bits each), with a gap between the 2s of 2 bytes
+        // and another gap of 2 bytes after the forth and third.
+        wholeWithContinuations = (wholeWithContinuations & 0x7F007F) | ((wholeWithContinuations & 0x7F007F00) >> 1);
+        // 0x3FFF isolate the first 14 bits i.e. the first and second bytes
+        // 0x3FFF0000 isolate the next 14 bits i.e. the third and forth bytes
+        // the third and forth bytes are shifted to the right by 2, filling the gaps left by the first and second bytes
+        return (wholeWithContinuations & 0x3FFF) | ((wholeWithContinuations & 0x3FFF0000) >> 2);
     }
 
     private static int readRawVarint40(ByteBuf buffer, int wholeOrMore) {
