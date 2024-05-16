netty/netty

commit 7ed972d070f9c1f8d89cc226568e61e3b5f2a6f5

Introduce IoEvent to prepare support for io_uring (#14024)
Motiviation:

To support things like io_uring we need to support different things like
IoOps as a result for notifications. Like for example you would issue a
connect via the IoRegistration when using io_uring but receive some
specific event on the completion queue once it was done.

Modification:

- Add new IoEvent interface
- Change IoHandle.handle(...) signature to use IoEvent as a param
- Rewrite existing implementations to use new signature.

Result:

More flexible API which can support also things like io_uring

transport-classes-kqueue/src/main/java/io/netty/channel/kqueue/KQueueIoHandler.java
@@ -324,12 +324,12 @@ public final class KQueueIoHandler implements IoHandler {
     }
 
     @Override
-    public KQueueIoRegistration register(IoEventLoop eventLoop, IoHandle handle, IoOps initialOps) {
+    public KQueueIoRegistration register(IoEventLoop eventLoop, IoHandle handle) {
         final KQueueIoHandle kqueueHandle = cast(handle);
         if (kqueueHandle.ident() == KQUEUE_WAKE_UP_IDENT) {
             throw new IllegalArgumentException("ident " + KQUEUE_WAKE_UP_IDENT + " is reserved for internal usage");
         }
-        KQueueEventIoOps eventIoOps = cast(initialOps);
+
         DefaultKqueueIoRegistration registration = new DefaultKqueueIoRegistration(
                 eventLoop, kqueueHandle);
         DefaultKqueueIoRegistration old = registrations.put(kqueueHandle.ident(), registration);
@@ -339,7 +339,6 @@ public final class KQueueIoHandler implements IoHandler {
             throw new IllegalStateException("registration for the KQueueIoHandle.ident() already exists");
         }
 
-        registration.addOps(eventIoOps);
         if (kqueueHandle instanceof AbstractKQueueChannel.AbstractKQueueUnsafe) {
             numChannels++;
         }
@@ -353,9 +352,9 @@ public final class KQueueIoHandler implements IoHandler {
         throw new IllegalArgumentException("IoHandle of type " + StringUtil.simpleClassName(handle) + " not supported");
     }
 
-    private static KQueueEventIoOps cast(IoOps ops) {
-        if (ops instanceof KQueueEventIoOps) {
-            return (KQueueEventIoOps) ops;
+    private static KQueueIoOps cast(IoOps ops) {
+        if (ops instanceof KQueueIoOps) {
+            return (KQueueIoOps) ops;
         }
         throw new IllegalArgumentException("IoOps of type " + StringUtil.simpleClassName(ops) + " not supported");
     }
@@ -366,7 +365,7 @@ public final class KQueueIoHandler implements IoHandler {
     }
 
     private final class DefaultKqueueIoRegistration extends AtomicBoolean implements KQueueIoRegistration {
-        private final KQueueEventIoOps readyEventIoOps = new KQueueEventIoOps();
+        private final KQueueIoEvent event = new KQueueIoEvent();
 
         final KQueueIoHandle handle;
 
@@ -378,17 +377,18 @@ public final class KQueueIoHandler implements IoHandler {
         }
 
         @Override
-        public void addOps(KQueueEventIoOps ops) {
-            if (ops.ident() != handle.ident()) {
+        public void submit(IoOps ops) {
+            KQueueIoOps kQueueIoOps = cast(ops);
+            if (kQueueIoOps.ident() != handle.ident()) {
                 throw new IllegalArgumentException("ident does not match KQueueIoHandle.ident()");
             }
             if (!isValid()) {
                 return;
             }
             if (eventLoop.inEventLoop()) {
-                evSet(ops.filter(), ops.flags(), ops.fflags());
+                evSet(event.filter(), event.flags(), event.fflags());
             } else {
-                eventLoop.execute(() -> evSet(ops.filter(), ops.flags(), ops.fflags()));
+                eventLoop.execute(() -> evSet(event.filter(), event.flags(), event.fflags()));
             }
         }
 
@@ -398,8 +398,8 @@ public final class KQueueIoHandler implements IoHandler {
         }
 
         void handle(int ident, short filter, short flags, int fflags, long data) {
-            readyEventIoOps.update(ident, filter, flags, fflags, data);
-            handle.handle(this, readyEventIoOps);
+            event.update(ident, filter, flags, fflags, data);
+            handle.handle(this, event);
         }
 
         private void evSet(short filter, short flags, int fflags) {

pom.xml
@@ -1478,7 +1478,7 @@
                 <item>
                   <ignore>true</ignore>
                   <code>java.method.finalMethodAddedToNonFinalClass</code>
-                  <new>method io.netty.util.concurrent.Future&lt;io.netty.channel.IoRegistration&gt; io.netty.channel.SingleThreadIoEventLoop::register(io.netty.channel.IoHandle, io.netty.channel.IoOps) @ io.netty.channel.epoll.EpollEventLoop</new>
+                  <new>method io.netty.util.concurrent.Future&lt;io.netty.channel.IoRegistration&gt; io.netty.channel.SingleThreadIoEventLoop::register(io.netty.channel.IoHandle) @ io.netty.channel.epoll.EpollEventLoop</new>
                 </item>
 
                 <item>

transport-classes-epoll/src/main/java/io/netty/channel/epoll/AbstractEpollChannel.java
@@ -30,8 +30,8 @@ import io.netty.channel.ChannelOutboundBuffer;
 import io.netty.channel.ChannelPromise;
 import io.netty.channel.ConnectTimeoutException;
 import io.netty.channel.EventLoop;
+import io.netty.channel.IoEvent;
 import io.netty.channel.IoEventLoop;
-import io.netty.channel.IoOps;
 import io.netty.channel.IoRegistration;
 import io.netty.channel.RecvByteBufAllocator;
 import io.netty.channel.socket.ChannelInputShutdownEvent;
@@ -75,7 +75,8 @@ abstract class AbstractEpollChannel extends AbstractChannel implements UnixChann
     private EpollIoRegistration registration;
     boolean inputClosedSeenErrorOnRead;
     boolean epollInReadyRunnablePending;
-    volatile EpollIoOps initialOps;
+    private EpollIoOps ops;
+    private EpollIoOps inital;
     protected volatile boolean active;
 
     AbstractEpollChannel(Channel parent, LinuxSocket fd, boolean active, EpollIoOps initialOps) {
@@ -88,7 +89,7 @@ abstract class AbstractEpollChannel extends AbstractChannel implements UnixChann
             this.local = fd.localAddress();
             this.remote = fd.remoteAddress();
         }
-        this.initialOps = initialOps;
+        this.ops = initialOps;
     }
 
     AbstractEpollChannel(Channel parent, LinuxSocket fd, SocketAddress remote, EpollIoOps initialOps) {
@@ -99,7 +100,15 @@ abstract class AbstractEpollChannel extends AbstractChannel implements UnixChann
         // See https://github.com/netty/netty/issues/2359
         this.remote = remote;
         this.local = fd.localAddress();
-        this.initialOps = initialOps;
+        this.ops = initialOps;
+    }
+
+    void add(EpollIoOps add) {
+        ops = ops.with(add);
+    }
+
+    void remove(EpollIoOps remove) {
+        ops = ops.without(remove);
     }
 
     static boolean isSoErrorZero(Socket fd) {
@@ -111,18 +120,31 @@ abstract class AbstractEpollChannel extends AbstractChannel implements UnixChann
     }
 
     protected void setFlag(int flag) throws IOException {
+        ops = ops.with(EpollIoOps.valueOf(flag));
         if (isRegistered()) {
             EpollIoRegistration registration = registration();
-            registration.updateInterestOps(registration.interestOps().with(EpollIoOps.valueOf(flag)));
+            try {
+                registration.submit(ops);
+            } catch (IOException e) {
+                throw e;
+            } catch (Exception e) {
+                throw new IllegalStateException(e);
+            }
         } else {
-            initialOps = initialOps.with(EpollIoOps.valueOf(flag));
+            ops = ops.with(EpollIoOps.valueOf(flag));
         }
     }
 
     void clearFlag(int flag) throws IOException {
         EpollIoRegistration registration = registration();
-        registration.updateInterestOps(
-                registration.interestOps().without(EpollIoOps.valueOf(flag)));
+        ops = ops.without(EpollIoOps.valueOf(flag));
+        try {
+            registration.submit(ops);
+        } catch (IOException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     protected final EpollIoRegistration registration() {
@@ -131,7 +153,7 @@ abstract class AbstractEpollChannel extends AbstractChannel implements UnixChann
     }
 
     boolean isFlagSet(int flag) {
-        return (registration().interestOps().value & flag) != 0;
+        return (ops.value & flag) != 0;
     }
 
     @Override
@@ -217,6 +239,7 @@ abstract class AbstractEpollChannel extends AbstractChannel implements UnixChann
     protected void doDeregister() throws Exception {
         EpollIoRegistration registration = this.registration;
         if (registration != null) {
+            ops = inital;
             registration.cancel();
         }
     }
@@ -278,7 +301,7 @@ abstract class AbstractEpollChannel extends AbstractChannel implements UnixChann
         } else  {
             // The EventLoop is not registered atm so just update the flags so the correct value
             // will be used once the channel is registered
-            initialOps = initialOps.without(EpollIoOps.EPOLLIN);
+            ops = ops.without(EpollIoOps.EPOLLIN);
         }
     }
 
@@ -288,9 +311,11 @@ abstract class AbstractEpollChannel extends AbstractChannel implements UnixChann
         // make sure the epollInReadyRunnablePending variable is reset so we will be able to execute the Runnable on the
         // new EventLoop.
         epollInReadyRunnablePending = false;
-        ((IoEventLoop) eventLoop()).register((AbstractEpollUnsafe) unsafe(), initialOps).addListener(f -> {
+        ((IoEventLoop) eventLoop()).register((AbstractEpollUnsafe) unsafe()).addListener(f -> {
             if (f.isSuccess()) {
                 registration = (EpollIoRegistration) f.getNow();
+                registration.submit(ops);
+                inital = ops;
                 promise.setSuccess();
             } else {
                 promise.setFailure(f.cause());
@@ -451,8 +476,9 @@ abstract class AbstractEpollChannel extends AbstractChannel implements UnixChann
         }
 
         @Override
-        public void handle(IoRegistration registration, IoOps readyOps) {
-            EpollIoOps epollOps = (EpollIoOps)  readyOps;
+        public void handle(IoRegistration registration, IoEvent event) {
+            EpollIoEvent epollEvent = (EpollIoEvent) event;
+            EpollIoOps epollOps = epollEvent.ops();
 
             // Don't change the ordering of processing EPOLLOUT | EPOLLRDHUP / EPOLLIN if you're not 100%
             // sure about it!
@@ -639,9 +665,10 @@ abstract class AbstractEpollChannel extends AbstractChannel implements UnixChann
             assert eventLoop().inEventLoop();
             try {
                 readPending = false;
+                ops = ops.without(EpollIoOps.EPOLLIN);
                 EpollIoRegistration registration = registration();
-                registration.updateInterestOps(registration.interestOps().without(EpollIoOps.EPOLLIN));
-            } catch (IOException e) {
+                registration.submit(ops);
+            } catch (Exception e) {
                 // When this happens there is something completely wrong with either the filedescriptor or epoll,
                 // so fire the exception through the pipeline and close the Channel.
                 pipeline().fireExceptionCaught(e);

transport-classes-epoll/src/main/java/io/netty/channel/epoll/EpollChannelConfig.java
@@ -198,14 +198,13 @@ public class EpollChannelConfig extends DefaultChannelConfig {
         ObjectUtil.checkNotNull(mode, "mode");
 
         AbstractEpollChannel epollChannel = (AbstractEpollChannel) channel;
-        EpollIoOps initial = epollChannel.initialOps;
         checkChannelNotRegistered();
         switch (mode) {
             case EDGE_TRIGGERED:
-                epollChannel.initialOps = initial.with(EpollIoOps.EPOLLET);
+                epollChannel.add(EpollIoOps.EPOLLET);
                 break;
             case LEVEL_TRIGGERED:
-                epollChannel.initialOps = initial.without(EpollIoOps.EPOLLET);
+                epollChannel.remove(EpollIoOps.EPOLLET);
                 break;
             default:
                 throw new Error();

transport-classes-epoll/src/main/java/io/netty/channel/epoll/EpollIoEvent.java
@@ -15,13 +15,17 @@
  */
 package io.netty.channel.epoll;
 
-import io.netty.channel.IoRegistration;
+import io.netty.channel.IoEvent;
 
 /**
- * Registration with an {@link EpollIoHandler}.
+ * {@link IoEvent} that must be handled by the {@link EpollIoHandle}.
  */
-public interface EpollIoRegistration extends IoRegistration {
+public interface EpollIoEvent extends IoEvent {
 
-    @Override
-    EpollIoHandler ioHandler();
+    /**
+     * Returns the {@link EpollIoOps} which did trigger the {@link EpollIoEvent}.
+     *
+     * @return  ops.
+     */
+    EpollIoOps ops();
 }

transport-classes-epoll/src/main/java/io/netty/channel/epoll/EpollIoHandler.java
@@ -260,32 +260,24 @@ public class EpollIoHandler implements IoHandler {
         private final IoEventLoop eventLoop;
         final EpollIoHandle handle;
 
-        private volatile EpollIoOps currentOps;
-
-        DefaultEpollIoRegistration(IoEventLoop eventLoop, EpollIoHandle handle, EpollIoOps initialOps) {
+        DefaultEpollIoRegistration(IoEventLoop eventLoop, EpollIoHandle handle) {
             this.eventLoop = eventLoop;
             this.handle = handle;
-            this.currentOps = initialOps;
         }
 
         @Override
-        public void updateInterestOps(EpollIoOps ops) throws IOException {
-            currentOps = ops;
+        public void submit(IoOps ops) throws Exception {
+            EpollIoOps epollIoOps = cast(ops);
             try {
                 if (!isValid()) {
                     return;
                 }
-                Native.epollCtlMod(epollFd.intValue(), handle.fd().intValue(), ops.value);
+                Native.epollCtlMod(epollFd.intValue(), handle.fd().intValue(), epollIoOps.value);
             } catch (IOException e) {
                 throw e;
             } catch (Exception e) {
                 throw new IOException(e);
             }
-        }
-
-        @Override
-        public EpollIoOps interestOps() {
-            return currentOps;
         }
 
         @Override
@@ -342,19 +334,17 @@ public class EpollIoHandler implements IoHandler {
         }
 
         void handle(long ev) {
-            handle.handle(this, EpollIoOps.valueOf((int) ev));
+            handle.handle(this, EpollIoOps.eventOf((int) ev));
         }
     }
 
     @Override
-    public EpollIoRegistration register(IoEventLoop eventLoop, IoHandle handle,
-                                      IoOps initialOps)
+    public EpollIoRegistration register(IoEventLoop eventLoop, IoHandle handle)
             throws Exception {
         final EpollIoHandle epollHandle = cast(handle);
-        EpollIoOps ops = cast(initialOps);
-        DefaultEpollIoRegistration registration = new DefaultEpollIoRegistration(eventLoop, epollHandle, ops);
+        DefaultEpollIoRegistration registration = new DefaultEpollIoRegistration(eventLoop, epollHandle);
         int fd = epollHandle.fd().intValue();
-        Native.epollCtlAdd(epollFd.intValue(), fd, registration.interestOps().value);
+        Native.epollCtlAdd(epollFd.intValue(), fd, EpollIoOps.EPOLLERR.value);
         DefaultEpollIoRegistration old = registrations.put(fd, registration);
 
         // We either expect to have no registration in the map with the same FD or that the FD of the old registration

transport-classes-epoll/src/main/java/io/netty/channel/epoll/EpollIoOps.java
@@ -29,7 +29,8 @@ public final class EpollIoOps implements IoOps {
     }
 
     /**
-     * Interested in IO events that should be handled by accepting new connections
+     * Interested in IO events which tell that the underlying channel is writable again or a connection
+     * attempt can be continued.
      */
     public static final EpollIoOps EPOLLOUT = new EpollIoOps(Native.EPOLLOUT);
 
@@ -39,7 +40,7 @@ public final class EpollIoOps implements IoOps {
     public static final EpollIoOps EPOLLIN = new EpollIoOps(Native.EPOLLIN);
 
     /**
-     * Interested in IO events which tell that the underlying channel is writable again.
+     * Error condition happened on the associated file descriptor.
      */
     public static final EpollIoOps EPOLLERR = new EpollIoOps(Native.EPOLLERR);
 
@@ -51,21 +52,20 @@ public final class EpollIoOps implements IoOps {
     public static final EpollIoOps EPOLLET = new EpollIoOps(Native.EPOLLET);
 
     // Just use an array to store often used values.
-    private static final EpollIoOps[] OPS;
+    private static final EpollIoEvent[] EVENTS;
 
     static {
-        EpollIoOps all = new EpollIoOps(
-                EPOLLOUT.value | EPOLLIN.value | EPOLLERR.value | EPOLLRDHUP.value);
-        OPS = new EpollIoOps[all.value + 1];
-        addToArray(OPS, EPOLLOUT);
-        addToArray(OPS, EPOLLIN);
-        addToArray(OPS, EPOLLERR);
-        addToArray(OPS, EPOLLRDHUP);
-        addToArray(OPS, all);
+        EpollIoOps all = new EpollIoOps(EPOLLOUT.value | EPOLLIN.value | EPOLLERR.value | EPOLLRDHUP.value);
+        EVENTS = new EpollIoEvent[all.value + 1];
+        addToArray(EVENTS, EPOLLOUT);
+        addToArray(EVENTS, EPOLLIN);
+        addToArray(EVENTS, EPOLLERR);
+        addToArray(EVENTS, EPOLLRDHUP);
+        addToArray(EVENTS, all);
     }
 
-    private static void addToArray(EpollIoOps[] array, EpollIoOps ops) {
-        array[ops.value] = ops;
+    private static void addToArray(EpollIoEvent[] array, EpollIoOps ops) {
+        array[ops.value] = new DefaultEpollIoEvent(ops);
     }
 
     final int value;
@@ -142,15 +142,60 @@ public final class EpollIoOps implements IoOps {
      * @return  the {@link EpollIoOps}.
      */
     public static EpollIoOps valueOf(int value) {
-        final EpollIoOps ops;
-        if (value > 0 && value < OPS.length) {
-            ops = OPS[value];
-            if (ops != null) {
-                return ops;
+        return eventOf(value).ops();
+    }
+
+    @Override
+    public String toString() {
+        return "EpollIoOps{" +
+                "value=" + value +
+                '}';
+    }
+
+    static EpollIoEvent eventOf(int value) {
+        if (value > 0 && value < EVENTS.length) {
+            EpollIoEvent event = EVENTS[value];
+            if (event != null) {
+                return event;
             }
-        } else if (value == EPOLLET.value) {
-            return EPOLLET;
         }
-        return new EpollIoOps(value);
+        return new DefaultEpollIoEvent(new EpollIoOps(value));
+    }
+
+    private static final class DefaultEpollIoEvent implements EpollIoEvent {
+        private final EpollIoOps ops;
+
+        DefaultEpollIoEvent(EpollIoOps ops) {
+            this.ops = ops;
+        }
+
+        @Override
+        public EpollIoOps ops() {
+            return ops;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            EpollIoEvent event = (EpollIoEvent) o;
+            return event.ops().equals(ops());
+        }
+
+        @Override
+        public int hashCode() {
+            return ops().hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return "DefaultEpollIoEvent{" +
+                    "ops=" + ops +
+                    '}';
+        }
     }
 }

transport-classes-kqueue/src/main/java/io/netty/channel/kqueue/AbstractKQueueChannel.java
@@ -30,8 +30,8 @@ import io.netty.channel.ChannelOutboundBuffer;
 import io.netty.channel.ChannelPromise;
 import io.netty.channel.ConnectTimeoutException;
 import io.netty.channel.EventLoop;
+import io.netty.channel.IoEvent;
 import io.netty.channel.IoEventLoop;
-import io.netty.channel.IoOps;
 import io.netty.channel.IoRegistration;
 import io.netty.channel.RecvByteBufAllocator;
 import io.netty.channel.socket.ChannelInputShutdownEvent;
@@ -73,10 +73,10 @@ abstract class AbstractKQueueChannel extends AbstractChannel implements UnixChan
     private boolean readFilterEnabled;
     private boolean writeFilterEnabled;
 
-    private final KQueueEventIoOps readEnabledOps;
-    private final KQueueEventIoOps writeEnabledOps;
-    private final KQueueEventIoOps readDisabledOps;
-    private final KQueueEventIoOps writeDisabledOps;
+    private final KQueueIoOps readEnabledOps;
+    private final KQueueIoOps writeEnabledOps;
+    private final KQueueIoOps readDisabledOps;
+    private final KQueueIoOps writeDisabledOps;
 
     boolean readReadyRunnablePending;
     boolean inputClosedSeenErrorOnRead;
@@ -95,10 +95,10 @@ abstract class AbstractKQueueChannel extends AbstractChannel implements UnixChan
             remote = fd.remoteAddress();
         }
 
-        readEnabledOps = KQueueEventIoOps.newOps(fd().intValue(), Native.EVFILT_READ, Native.EV_ADD_CLEAR_ENABLE, 0);
-        writeEnabledOps = KQueueEventIoOps.newOps(fd().intValue(), Native.EVFILT_WRITE, Native.EV_ADD_CLEAR_ENABLE, 0);
-        readDisabledOps = KQueueEventIoOps.newOps(fd().intValue(), Native.EVFILT_READ, Native.EV_DELETE_DISABLE, 0);
-        writeDisabledOps = KQueueEventIoOps.newOps(fd().intValue(), Native.EVFILT_WRITE, Native.EV_DELETE_DISABLE, 0);
+        readEnabledOps = KQueueIoOps.newOps(fd().intValue(), Native.EVFILT_READ, Native.EV_ADD_CLEAR_ENABLE, 0);
+        writeEnabledOps = KQueueIoOps.newOps(fd().intValue(), Native.EVFILT_WRITE, Native.EV_ADD_CLEAR_ENABLE, 0);
+        readDisabledOps = KQueueIoOps.newOps(fd().intValue(), Native.EVFILT_READ, Native.EV_DELETE_DISABLE, 0);
+        writeDisabledOps = KQueueIoOps.newOps(fd().intValue(), Native.EVFILT_WRITE, Native.EV_DELETE_DISABLE, 0);
     }
 
     AbstractKQueueChannel(Channel parent, BsdSocket fd, SocketAddress remote) {
@@ -110,10 +110,10 @@ abstract class AbstractKQueueChannel extends AbstractChannel implements UnixChan
         this.remote = remote;
         local = fd.localAddress();
 
-        readEnabledOps = KQueueEventIoOps.newOps(fd().intValue(), Native.EVFILT_READ, Native.EV_ADD_CLEAR_ENABLE, 0);
-        writeEnabledOps = KQueueEventIoOps.newOps(fd().intValue(), Native.EVFILT_WRITE, Native.EV_ADD_CLEAR_ENABLE, 0);
-        readDisabledOps = KQueueEventIoOps.newOps(fd().intValue(), Native.EVFILT_READ, Native.EV_DELETE_DISABLE, 0);
-        writeDisabledOps = KQueueEventIoOps.newOps(fd().intValue(), Native.EVFILT_WRITE, Native.EV_DELETE_DISABLE, 0);
+        readEnabledOps = KQueueIoOps.newOps(fd().intValue(), Native.EVFILT_READ, Native.EV_ADD_CLEAR_ENABLE, 0);
+        writeEnabledOps = KQueueIoOps.newOps(fd().intValue(), Native.EVFILT_WRITE, Native.EV_ADD_CLEAR_ENABLE, 0);
+        readDisabledOps = KQueueIoOps.newOps(fd().intValue(), Native.EVFILT_READ, Native.EV_DELETE_DISABLE, 0);
+        writeDisabledOps = KQueueIoOps.newOps(fd().intValue(), Native.EVFILT_WRITE, Native.EV_DELETE_DISABLE, 0);
     }
 
     @Override
@@ -189,8 +189,16 @@ abstract class AbstractKQueueChannel extends AbstractChannel implements UnixChan
     }
 
     private void clearRdHup0() {
-        registration.addOps(KQueueEventIoOps.newOps(fd().intValue(),
+        submit(KQueueIoOps.newOps(fd().intValue(),
                 Native.EVFILT_SOCK, Native.EV_DELETE_DISABLE, Native.NOTE_RDHUP));
+    }
+
+    private void submit(KQueueIoOps ops) {
+        try {
+            registration.submit(ops);
+        } catch (Exception e) {
+            throw new ChannelException(e);
+        }
     }
 
     @Override
@@ -213,9 +221,7 @@ abstract class AbstractKQueueChannel extends AbstractChannel implements UnixChan
 
     @Override
     protected void doRegister(ChannelPromise promise) {
-        KQueueEventIoOps initialOps = KQueueEventIoOps.newOps(
-                fd().intValue(), Native.EVFILT_SOCK, Native.EV_ADD, Native.NOTE_RDHUP);
-        ((IoEventLoop) eventLoop()).register((AbstractKQueueUnsafe) unsafe(), initialOps).addListener(f -> {
+        ((IoEventLoop) eventLoop()).register((AbstractKQueueUnsafe) unsafe()).addListener(f -> {
             if (f.isSuccess()) {
                 this.registration = (KQueueIoRegistration) f.getNow();
                 // Just in case the previous EventLoop was shutdown abruptly, or an event is still pending on the old
@@ -224,12 +230,15 @@ abstract class AbstractKQueueChannel extends AbstractChannel implements UnixChan
                 readReadyRunnablePending = false;
 
                 int ident = fd().intValue();
+                submit(KQueueIoOps.newOps(
+                        ident, Native.EVFILT_SOCK, Native.EV_ADD, Native.NOTE_RDHUP));
+
                 // Add the write event first so we get notified of connection refused on the client side!
                 if (writeFilterEnabled) {
-                    registration.addOps(writeEnabledOps);
+                    submit(writeEnabledOps);
                 }
                 if (readFilterEnabled) {
-                    registration.addOps(readEnabledOps);
+                    submit(readEnabledOps);
                 }
                 promise.setSuccess();
             } else {
@@ -372,14 +381,14 @@ abstract class AbstractKQueueChannel extends AbstractChannel implements UnixChan
     void readFilter(boolean readFilterEnabled) throws IOException {
         if (this.readFilterEnabled != readFilterEnabled) {
             this.readFilterEnabled = readFilterEnabled;
-            registration().addOps(readFilterEnabled ? readEnabledOps : readDisabledOps);
+            submit(readFilterEnabled ? readEnabledOps : readDisabledOps);
         }
     }
 
     void writeFilter(boolean writeFilterEnabled) throws IOException {
         if (this.writeFilterEnabled != writeFilterEnabled) {
             this.writeFilterEnabled = writeFilterEnabled;
-            registration().addOps(writeFilterEnabled ? writeEnabledOps : writeDisabledOps);
+            submit(writeFilterEnabled ? writeEnabledOps : writeDisabledOps);
         }
     }
 
@@ -411,12 +420,12 @@ abstract class AbstractKQueueChannel extends AbstractChannel implements UnixChan
         }
 
         @Override
-        public void handle(IoRegistration registration, IoOps readyOps) {
-            KQueueEventIoOps ops = (KQueueEventIoOps) readyOps;
-            final short filter = ops.filter();
-            final short flags = ops.flags();
-            final int fflags = ops.fflags();
-            final long data = ops.data();
+        public void handle(IoRegistration registration, IoEvent event) {
+            KQueueIoEvent kqueueEvent = (KQueueIoEvent) event;
+            final short filter = kqueueEvent.filter();
+            final short flags = kqueueEvent.flags();
+            final int fflags = kqueueEvent.fflags();
+            final long data = kqueueEvent.data();
 
             // First check for EPOLLOUT as we may need to fail the connect ChannelPromise before try
             // to read from the file descriptor.

commit a7b2a251027c578bb145fdaf6716f16f23823f90

KQueueIoHandler should pass IoOps to evSet
Motivation:

7ed972d070f9c1f8d89cc226568e61e3b5f2a6f5 introduced a change but did break our native kqueue implementation

Modifications:

Correctly use values of KQueueIoOps when calling evSet

Result:

KQueue tests pass again

transport-classes-kqueue/src/main/java/io/netty/channel/kqueue/KQueueIoHandler.java
@@ -385,10 +385,13 @@ public final class KQueueIoHandler implements IoHandler {
             if (!isValid()) {
                 return -1;
             }
+            short filter = kQueueIoOps.filter();
+            short flags = kQueueIoOps.flags();
+            int fflags = kQueueIoOps.fflags();
             if (eventLoop.inEventLoop()) {
-                evSet(event.filter(), event.flags(), event.fflags());
+                evSet(filter, flags, fflags);
             } else {
-                eventLoop.execute(() -> evSet(event.filter(), event.flags(), event.fflags()));
+                eventLoop.execute(() -> evSet(filter, flags, fflags));
             }
             return 0;
         }