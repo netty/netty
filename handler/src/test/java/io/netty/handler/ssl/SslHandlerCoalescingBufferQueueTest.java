9c4b4efb69d319e3a10312a6f63768185c1d12b1
Lari Hotari <lhotari@apache.org>
2024-05-30 18:09:15 +0300

    Add more test scenarios for cumulation test

--- handler/src/test/java/io/netty/handler/ssl/SslHandlerCoalescingBufferQueueTest.java
+++ handler/src/test/java/io/netty/handler/ssl/SslHandlerCoalescingBufferQueueTest.java
@@ -21,19 +21,82 @@ import io.netty.buffer.UnpooledByteBufAllocator;
 import io.netty.channel.ChannelPromise;
 import io.netty.channel.embedded.EmbeddedChannel;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import java.nio.ByteBuffer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SslHandlerCoalescingBufferQueueTest {
+    static final Supplier<ByteBuf> BYTEBUF_SUPPLIER = new Supplier<ByteBuf>() {
+        @Override
+        public ByteBuf get() {
+            ByteBuf buf = Unpooled.directBuffer(128);
+            buf.writeLong(1);
+            buf.writeLong(2);
+            return buf;
+        }
+    };
+    static final Function<ByteBuf, ByteBuf> NO_WRAPPER = new Function<ByteBuf, ByteBuf>() {
+        @Override
+        public ByteBuf apply(ByteBuf byteBuf) {
+            return byteBuf;
+        }
+    };
+    static final Function<ByteBuf, ByteBuf> DUPLICATE_WRAPPER = new Function<ByteBuf, ByteBuf>() {
+        @Override
+        public ByteBuf apply(ByteBuf byteBuf) {
+            return byteBuf.duplicate();
+        }
+    };
+    static final Function<ByteBuf, ByteBuf> SLICE_WRAPPER = new Function<ByteBuf, ByteBuf>() {
+        @Override
+        public ByteBuf apply(ByteBuf byteBuf) {
+            return byteBuf.slice();
+        }
+    };
+    static ByteBuffer createNioBuffer() {
+        ByteBuffer nioBuffer = ByteBuffer.allocateDirect(128);
+        nioBuffer.putLong(1);
+        nioBuffer.putLong(2);
+        nioBuffer.position(nioBuffer.limit());
+        nioBuffer.flip();
+        return nioBuffer;
+    }
+
+    enum CumulationTestScenario {
+        BASIC_NIO_BUFFER(new Supplier() {
+            @Override
+            public ByteBuf get() {
+                return Unpooled.wrappedBuffer(createNioBuffer());
+            }
+        }, NO_WRAPPER),
+        READ_ONLY_AND_DUPLICATE_NIO_BUFFER(new Supplier() {
+            @Override
+            public ByteBuf get() {
+                return Unpooled.wrappedBuffer(createNioBuffer().asReadOnlyBuffer());
+            }
+        }, DUPLICATE_WRAPPER),
+        BASIC_DIRECT_BUFFER(BYTEBUF_SUPPLIER, NO_WRAPPER),
+        DUPLICATE_DIRECT_BUFFER(BYTEBUF_SUPPLIER, DUPLICATE_WRAPPER),
+        SLICED_DIRECT_BUFFER(BYTEBUF_SUPPLIER, SLICE_WRAPPER);
+
+        final Supplier<ByteBuf> bufferSupplier;
+        final Function<ByteBuf, ByteBuf> bufferWrapper;
+
+        CumulationTestScenario(Supplier<ByteBuf> bufferSupplier, Function<ByteBuf, ByteBuf> bufferWrapper) {
+            this.bufferSupplier = bufferSupplier;
+            this.bufferWrapper = bufferWrapper;
+        }
+    }
 
     @ParameterizedTest
-    @ValueSource(booleans = { true, false })
-    public void testCumulation(boolean readOnlyAndDuplicate) {
+    @EnumSource(CumulationTestScenario.class)
+    public void testCumulation(CumulationTestScenario testScenario) {
         EmbeddedChannel channel = new EmbeddedChannel();
         SslHandlerCoalescingBufferQueue queue = new SslHandlerCoalescingBufferQueue(channel, 16, false) {
             @Override
@@ -42,20 +105,9 @@ public class SslHandlerCoalescingBufferQueueTest {
             }
         };
 
-        ByteBuffer nioBuffer = ByteBuffer.allocateDirect(128);
-        nioBuffer.putLong(1);
-        nioBuffer.putLong(2);
-        // use the whole buffer so that the wrapper will have the capacity of 128
-        nioBuffer.position(nioBuffer.limit());
-        nioBuffer.flip();
-        ByteBuf first;
-        if (readOnlyAndDuplicate) {
-            first = Unpooled.wrappedBuffer(nioBuffer.asReadOnlyBuffer()).duplicate();
-            first.writerIndex(8);
-        } else {
-            first = Unpooled.wrappedBuffer(nioBuffer);
-            first.writerIndex(8);
-        }
+        ByteBuf original = testScenario.bufferSupplier.get();
+        original.writerIndex(8);
+        ByteBuf first = testScenario.bufferWrapper.apply(original);
         first.retain();
         queue.add(first);
         ByteBuf second = Unpooled.copyLong(3);
@@ -72,8 +124,9 @@ public class SslHandlerCoalescingBufferQueueTest {
             buffer.release();
         }
         assertTrue(queue.isEmpty());
-        assertEquals(8, first.writerIndex());
-        assertEquals(2, first.getLong(first.writerIndex()));
+        assertEquals(8, original.writerIndex());
+        original.writerIndex(original.capacity());
+        assertEquals(2, original.getLong(8));
         first.release();
         assertEquals(0, first.refCnt());
         assertEquals(0, second.refCnt());
