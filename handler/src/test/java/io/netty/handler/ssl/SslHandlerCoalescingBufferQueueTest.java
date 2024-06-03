20a11b32f00a3d119bd2840e0963c3e9688b73a7
Lari Hotari <lhotari@users.noreply.github.com>
2024-06-04 01:59:22 +0300

    Optimize wrap buffer cumulation in SslHandler and don't mutate input buffers  (#14086)
    Motivation:
    
    SslHandler doesn't support sharing the input buffers since the input
    buffers get mutated. This issue has been reported as #14069. Fixes have
    already been made to support the use case using read-only buffers as
    input. However, this is not useful for existing Netty applications.
    
    I checked the Netty code once again and noticed that it's possible to
    optimize the current implementation in SslHandler and avoid the buffer
    mutation.
    
    The default behavior in SslHandler causes very hard to debug problems.
    In Pulsar, there have been issues open for multiple years that have been
    caused by the SslHandler's default behavior of mutating the input
    buffers (apache/pulsar#22601 is one starting point to the issues).
    
    The current implementation of SslHandler will mutate the input buffers
    by using the input buffer as a cumulation buffer when there's available
    capacity. This is not optimal at all. This PR contains an improvement
    where the cumulation buffer is allocated to the final size immediately
    so that cumulation is more optimal. That's why I think that this will be
    more optimal in the end and won't cause performance regressions.
    
    After these changes, it should be possible to pass a slice or duplicate
    of a buffer and the input buffer doesn't get mutated. The input buffer's
    readIndex will be modified so multiple threads cannot share the same
    input buffer without a slice or duplicate.
    
    I created a failing test case that shows that a duplicate buffer doesn't
    prevent input buffer mutation.
    
    ![image](https://github.com/netty/netty/assets/66864/0abec333-6079-40bd-8bc1-ccfd3df6fae1)
    It's a very surprising behavior of SslHandler that passing a
    `.retainedDuplicate()` buffer could result in mutations to the original
    buffer. That's another reason why I think that SslHandler shouldn't ever
    mutate the inputs.
    
    Modification:
    
    - add `protected ByteBuf composeFirst(ByteBufAllocator allocator,
    ByteBuf first, int bufferSize)` to AbstractCoalescingBufferQueue to
    support allocating an optimal cumulation buffer instead of using the
    input buffer as a cumulation buffer
    - remove dead code from `compose` where there was handling for
    CompositeByteBuf
    - `composeFirst` will be called before `compose` so this case isn't
    needed.
    
    Result:
    
    Fixes #14069 without the need to set the input buffer as read-only
    
    ---------
    
    Co-authored-by: Norman Maurer <norman_maurer@apple.com>

--- handler/src/test/java/io/netty/handler/ssl/SslHandlerCoalescingBufferQueueTest.java
+++ handler/src/test/java/io/netty/handler/ssl/SslHandlerCoalescingBufferQueueTest.java
@@ -21,19 +21,82 @@ import io.netty.buffer.UnpooledByteBufAllocator;
 import io.netty.channel.ChannelPromise;
 import io.netty.channel.embedded.EmbeddedChannel;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import java.nio.ByteBuffer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SslHandlerCoalescingBufferQueueTest {
+    static final Supplier<ByteBuf> BYTEBUF_SUPPLIER = new Supplier<ByteBuf>() {
+        @Override
+        public ByteBuf get() {
+            ByteBuf buf = Unpooled.directBuffer(128);
+            buf.writeLong(1);
+            buf.writeLong(2);
+            return buf;
+        }
+    };
+    static final Function<ByteBuf, ByteBuf> NO_WRAPPER = new Function<ByteBuf, ByteBuf>() {
+        @Override
+        public ByteBuf apply(ByteBuf byteBuf) {
+            return byteBuf;
+        }
+    };
+    static final Function<ByteBuf, ByteBuf> DUPLICATE_WRAPPER = new Function<ByteBuf, ByteBuf>() {
+        @Override
+        public ByteBuf apply(ByteBuf byteBuf) {
+            return byteBuf.duplicate();
+        }
+    };
+    static final Function<ByteBuf, ByteBuf> SLICE_WRAPPER = new Function<ByteBuf, ByteBuf>() {
+        @Override
+        public ByteBuf apply(ByteBuf byteBuf) {
+            return byteBuf.slice();
+        }
+    };
+    static ByteBuffer createNioBuffer() {
+        ByteBuffer nioBuffer = ByteBuffer.allocateDirect(128);
+        nioBuffer.putLong(1);
+        nioBuffer.putLong(2);
+        nioBuffer.position(nioBuffer.limit());
+        nioBuffer.flip();
+        return nioBuffer;
+    }
+
+    enum CumulationTestScenario {
+        BASIC_NIO_BUFFER(new Supplier() {
+            @Override
+            public ByteBuf get() {
+                return Unpooled.wrappedBuffer(createNioBuffer());
+            }
+        }, NO_WRAPPER),
+        READ_ONLY_AND_DUPLICATE_NIO_BUFFER(new Supplier() {
+            @Override
+            public ByteBuf get() {
+                return Unpooled.wrappedBuffer(createNioBuffer().asReadOnlyBuffer());
+            }
+        }, DUPLICATE_WRAPPER),
+        BASIC_DIRECT_BUFFER(BYTEBUF_SUPPLIER, NO_WRAPPER),
+        DUPLICATE_DIRECT_BUFFER(BYTEBUF_SUPPLIER, DUPLICATE_WRAPPER),
+        SLICED_DIRECT_BUFFER(BYTEBUF_SUPPLIER, SLICE_WRAPPER);
+
+        final Supplier<ByteBuf> bufferSupplier;
+        final Function<ByteBuf, ByteBuf> bufferWrapper;
+
+        CumulationTestScenario(Supplier<ByteBuf> bufferSupplier, Function<ByteBuf, ByteBuf> bufferWrapper) {
+            this.bufferSupplier = bufferSupplier;
+            this.bufferWrapper = bufferWrapper;
+        }
+    }
 
     @ParameterizedTest
-    @ValueSource(booleans = { true, false })
-    public void testCumulation(boolean readOnlyAndDuplicate) {
+    @EnumSource(CumulationTestScenario.class)
+    public void testCumulation(CumulationTestScenario testScenario) {
         EmbeddedChannel channel = new EmbeddedChannel();
         SslHandlerCoalescingBufferQueue queue = new SslHandlerCoalescingBufferQueue(channel, 16, false) {
             @Override
@@ -42,21 +105,12 @@ public class SslHandlerCoalescingBufferQueueTest {
             }
         };
 
-        ByteBuffer nioBuffer = ByteBuffer.allocateDirect(128);
-        nioBuffer.putLong(0);
-        nioBuffer.putLong(0);
-        nioBuffer.putLong(0);
-        nioBuffer.flip();
-        ByteBuf first;
-        if (readOnlyAndDuplicate) {
-            first = Unpooled.wrappedBuffer(nioBuffer.asReadOnlyBuffer()).duplicate();
-            first.writerIndex(8);
-        } else {
-            first = Unpooled.wrappedBuffer(nioBuffer);
-            first.writerIndex(8);
-        }
+        ByteBuf original = testScenario.bufferSupplier.get();
+        original.writerIndex(8);
+        ByteBuf first = testScenario.bufferWrapper.apply(original);
+        first.retain();
         queue.add(first);
-        ByteBuf second = Unpooled.copyLong(1);
+        ByteBuf second = Unpooled.copyLong(3);
         queue.add(second);
 
         ChannelPromise promise = channel.newPromise();
@@ -64,12 +118,16 @@ public class SslHandlerCoalescingBufferQueueTest {
         ByteBuf buffer = queue.remove(UnpooledByteBufAllocator.DEFAULT, 128, promise);
         try {
             assertEquals(16, buffer.readableBytes());
-            assertEquals(0, buffer.readLong());
             assertEquals(1, buffer.readLong());
+            assertEquals(3, buffer.readLong());
         } finally {
             buffer.release();
         }
         assertTrue(queue.isEmpty());
+        assertEquals(8, original.writerIndex());
+        original.writerIndex(original.capacity());
+        assertEquals(2, original.getLong(8));
+        first.release();
         assertEquals(0, first.refCnt());
         assertEquals(0, second.refCnt());
     }
