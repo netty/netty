78e263d849ef79cc60218b9e7909339ab2f77cf3
Aayush Atharva <aayush@shieldblaze.com>
2024-06-24 16:23:40 +0530

    Check for brackets only if the address is unresolved.

--- codec-http/src/main/java/io/netty/handler/codec/http/HttpUtil.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/HttpUtil.java
@@ -547,15 +547,14 @@ public final class HttpUtil {
         if (NetUtil.isValidIpV6Address(hostString)) {
             if (!addr.isUnresolved()) {
                 hostString = NetUtil.toAddressString(addr.getAddress());
+            } else {
+                // If IPv6 address already contains brackets, let's return as is.
+                if (hostString.charAt(0) == '[' && hostString.charAt(hostString.length() - 1) == ']') {
+                    return hostString;
+                }
             }
 
-            // If IPv6 address already contains brackets, let's return as is.
-            if (hostString.charAt(0) == '[' && hostString.charAt(hostString.length() - 1) == ']') {
-                return hostString;
-            } else {
-                // Add brackets to the IPv6 address.
-                return '[' + hostString + ']';
-            }
+            return '[' + hostString + ']';
         }
         return hostString;
     }
