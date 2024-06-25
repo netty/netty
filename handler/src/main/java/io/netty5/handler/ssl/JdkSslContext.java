d9f386059a595b9da5795a447f7f010b187b3483
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2024-06-24 23:32:37 -0700

    Null value doesn't disable endpoint verification for JDK provider (#14142)
    Motivation:
    
    If Java ever changes the default endpoint identification algorithm of if
    users use a 3-party provider that overrides the default, Netty users
    won't be able to change it via `SslContextBuilder` API by providing a
    null value. Only an empty string will disable it, but this behavior is
    not documented in `endpointIdentificationAlgorithm(String)` javadoc.
    
    Modifications:
    - Set `endpointIdentificationAlgorithm` unconditionally in
    `JdkSslContext`, similar to `ReferenceCountedOpenSslServerContext`.
    
    Result:
    
    `SslContextBuilder.endpointIdentificationAlgorithm(null)` works as
    expected with `SslProvider.JDK`.

--- handler/src/main/java/io/netty5/handler/ssl/JdkSslContext.java
+++ handler/src/main/java/io/netty5/handler/ssl/JdkSslContext.java
@@ -372,9 +372,7 @@ public class JdkSslContext extends SslContext {
             return ((JdkApplicationProtocolNegotiator.AllocatorAwareSslEngineWrapperFactory) factory)
                     .wrapSslEngine(engine, alloc, apn, isServer());
         }
-        if (endpointIdentificationAlgorithm != null) {
-            engine.getSSLParameters().setEndpointIdentificationAlgorithm(endpointIdentificationAlgorithm);
-        }
+        engine.getSSLParameters().setEndpointIdentificationAlgorithm(endpointIdentificationAlgorithm);
         return factory.wrapSslEngine(engine, apn, isServer());
     }
 
