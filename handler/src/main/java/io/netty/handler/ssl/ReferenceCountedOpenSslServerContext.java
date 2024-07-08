aba6519009343f2b5f431d4cb44bda71be14cba4
Chris Vest <christianvest_hansen@apple.com>
2024-07-08 14:48:10 -0700

    Backport the SslContextBuilder.endpointIdentificationAlgorithm method
    Motivation:
    We added this method in 4.2, and set the default endpoint identification algorithm to "HTTPS".
    To keep the APIs as similar as possible, and to allow integrators to start referencing the new method early, we backport it to 4.1.
    The default endpoint identification algorithm default remains unchanged in 4.1, where it is disabled as it always has been.
    At least now it's easier to enable.
    
    Modification:
    - Backport the API changes from #14127 (8372a09092e84b98e55facfe3bd4c23448c32918) and #14144 (b9552ea9d81427252303352ad7005b8651ea52b7).
    - Keep the default setting the same, so no tests needs to change.
    - Guard the actual SSLParameters API call by a Java 7 version check, which is now necessary since the compatibility baseline is Java 6.
    
    Result:
    Netty 4.2 and 4.1 now share the same API for enabling hostname verification.
    It's easier to enable hostname verification.

--- handler/src/main/java/io/netty/handler/ssl/ReferenceCountedOpenSslServerContext.java
+++ handler/src/main/java/io/netty/handler/ssl/ReferenceCountedOpenSslServerContext.java
@@ -72,7 +72,9 @@ public final class ReferenceCountedOpenSslServerContext extends ReferenceCounted
             boolean enableOcsp, String keyStore, Map.Entry<SslContextOption<?>, Object>... options)
             throws SSLException {
         super(ciphers, cipherFilter, apn, SSL.SSL_MODE_SERVER, keyCertChain,
-              clientAuth, protocols, startTls, enableOcsp, true, options);
+                clientAuth, protocols, startTls,
+                null, // No endpoint validation for servers.
+                enableOcsp, true, options);
         // Create a new SSL_CTX and configure it.
         boolean success = false;
         try {
