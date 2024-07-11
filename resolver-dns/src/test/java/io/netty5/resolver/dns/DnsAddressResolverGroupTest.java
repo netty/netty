3b1fa5820c17765cc9d41e8e5c0b6702a18879a2
qnnn <65326092+qnnn@users.noreply.github.com>
2024-07-12 00:47:15 +0800

    Fix potential DNS cache invalidation across different EventLoops (#14147)
    Motivation:
    
    By default, `DnsAddressResolverGroup` sets up a separate `DNSCache`
    for each `EventLoop`. This can lead to cache invalidation when
    performing DNS resolution across different EventLoops due to the
    potential absence of cached DNS records.
    
    Modification:
    
    - initializing the DNSCache in `DnsNameResolverBuilder` during
    `DnsAddressResolverGroup` initialization to enable sharing of DNSCache
    among EventLoops under the same `DnsAddressResolverGroup`.
    - Add unit test: testSharedDNSCacheAcrossEventLoops
    
    Result:
    
    Fixes #14046.
    
    ---------
    
    Co-authored-by: Norman Maurer <norman_maurer@apple.com>
    Co-authored-by: Bryce Anderson <bryce_anderson@apple.com>

--- resolver-dns/src/test/java/io/netty5/resolver/dns/DnsAddressResolverGroupTest.java
+++ resolver-dns/src/test/java/io/netty5/resolver/dns/DnsAddressResolverGroupTest.java
@@ -22,14 +22,20 @@ import io.netty5.channel.local.LocalIoHandler;
 import io.netty5.channel.nio.NioIoHandler;
 import io.netty5.channel.socket.nio.NioDatagramChannel;
 import io.netty5.resolver.AddressResolver;
+import io.netty5.resolver.InetSocketAddressResolver;
 import io.netty5.util.concurrent.Promise;
+
 import org.junit.jupiter.api.Test;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.channels.UnsupportedAddressTypeException;
+import java.util.concurrent.ExecutionException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DnsAddressResolverGroupTest {
@@ -62,5 +68,46 @@ public class DnsAddressResolverGroupTest {
             group.shutdownGracefully();
             defaultEventLoopGroup.shutdownGracefully();
         }
+    }
+
+    @Test
+    public void testSharedDNSCacheAcrossEventLoops() throws InterruptedException, ExecutionException {
+        MultithreadEventLoopGroup group = new MultithreadEventLoopGroup(3, NioIoHandler.newFactory());
+        final EventLoop loop = group.next();
+        DnsNameResolverBuilder builder = new DnsNameResolverBuilder()
+                .eventLoop(loop).channelType(NioDatagramChannel.class);
+        DnsAddressResolverGroup resolverGroup = new DnsAddressResolverGroup(builder);
+        EventLoop eventLoop1 = group.next();
+        EventLoop eventLoop2 = group.next();
+        try {
+            final Promise<InetSocketAddress> promise1 = loop.newPromise();
+            InetSocketAddressResolver resolver1 = (InetSocketAddressResolver) resolverGroup.getResolver(eventLoop1);
+            InetAddress address1 =
+                    resolve(resolver1, InetSocketAddress.createUnresolved("netty.io", 80), promise1);
+            final Promise<InetSocketAddress> promise2 = loop.newPromise();
+            InetSocketAddressResolver resolver2 = (InetSocketAddressResolver) resolverGroup.getResolver(eventLoop2);
+            InetAddress address2 =
+                    resolve(resolver2, InetSocketAddress.createUnresolved("netty.io", 80), promise2);
+            assertSame(address1, address2);
+        } finally {
+            resolverGroup.close();
+            group.shutdownGracefully();
+        }
+    }
+
+    private InetAddress resolve(InetSocketAddressResolver resolver, SocketAddress socketAddress,
+                                final Promise<InetSocketAddress> promise)
+            throws InterruptedException, ExecutionException {
+        resolver.resolve(socketAddress)
+                .addListener(f -> {
+                    try {
+                        promise.setSuccess(f.getNow());
+                    } catch (Throwable cause) {
+                        promise.setFailure(cause);
+                    }
+                });
+        promise.asFuture().asStage().sync();
+        InetSocketAddress inetSocketAddress = promise.getNow();
+        return inetSocketAddress.getAddress();
     }
 }
