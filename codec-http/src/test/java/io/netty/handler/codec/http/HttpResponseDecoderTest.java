netty/netty

commit 3c06dd680d05b3fa32ed9d6b82e9ef62de1c9b87

Reject request if NUL is present in the request line (#14180)
Motivation:

We should not allow extra whitespaces or control chars in the request
line.

Modifications:

- Check if the there are extra chars at the end.
- Add unit test

Result:

Fix https://github.com/netty/netty/issues/14166

---------

Co-authored-by: Bryce Anderson <bryce_anderson@apple.com>

codec-http/src/main/java/io/netty/handler/codec/http/HttpObjectDecoder.java
@@ -948,6 +948,13 @@ public abstract class HttpObjectDecoder extends ByteToMessageDecoder {
 
         final int end = startContent + asciiBuffer.readableBytes();
 
+        byte lastByte = asciiBytes[end - 1];
+        if (isControlOrWhitespaceAsciiChar(lastByte)) {
+            // There should no extra control or whitespace char.
+            // See https://datatracker.ietf.org/doc/html/rfc2616#section-5.1
+            throw new IllegalArgumentException("Illegal character in request line: 0x" + Integer.toHexString(lastByte));
+        }
+
         final int aStart = findNonSPLenient(asciiBytes, startContent, end);
         final int aEnd = findSPLenient(asciiBytes, aStart, end);
 

codec-http/src/test/java/io/netty/handler/codec/http/HttpRequestDecoderTest.java
@@ -687,6 +687,11 @@ public class HttpRequestDecoderTest {
     public void testLeadingWhitespaceInFirstHeaderName() {
         testInvalidHeaders0("POST / HTTP/1.1\r\n\tContent-Length: 1\r\n\r\nX");
     }
+  
+   @Test
+    public void testNulInInitialLine() {
+        testInvalidHeaders0("GET / HTTP/1.1\r\u0000\nHost: whatever\r\n\r\n");
+    }
 
     private static void testInvalidHeaders0(String requestStr) {
         testInvalidHeaders0(Unpooled.copiedBuffer(requestStr, CharsetUtil.US_ASCII));

commit 43511992c26fcf2d2e0b1154367f3865e8d94661

Allow HTTP responses without reason-phrase
Motivation:

https://github.com/netty/netty/pull/14180 introduced some changes which broke the handling of HTTP responses without a reason-phrase. This change fixes it by correctly following the RFC

Modifications:

Correctly handle responses without reason-phrase

Result:

Fixes regression introduced by https://github.com/netty/netty/pull/14180

codec-http/src/main/java/io/netty/handler/codec/http/HttpObjectDecoder.java
@@ -950,9 +950,15 @@ public abstract class HttpObjectDecoder extends ByteToMessageDecoder {
 
         byte lastByte = asciiBytes[end - 1];
         if (isControlOrWhitespaceAsciiChar(lastByte)) {
-            // There should no extra control or whitespace char.
-            // See https://datatracker.ietf.org/doc/html/rfc2616#section-5.1
-            throw new IllegalArgumentException("Illegal character in request line: 0x" + Integer.toHexString(lastByte));
+            if (isDecodingRequest() || !isOWS(lastByte)) {
+                // There should no extra control or whitespace char in case of a request.
+                // In case of a response there might be a SP if there is no reason-phrase given.
+                // See
+                //  - https://datatracker.ietf.org/doc/html/rfc2616#section-5.1
+                //  - https://datatracker.ietf.org/doc/html/rfc9112#name-status-line
+                throw new IllegalArgumentException(
+                        "Illegal character in request line: 0x" + Integer.toHexString(lastByte));
+            }
         }
 
         final int aStart = findNonSPLenient(asciiBytes, startContent, end);

codec-http/src/test/java/io/netty/handler/codec/http/HttpResponseDecoderTest.java
@@ -1046,6 +1046,20 @@ public class HttpResponseDecoderTest {
     }
 
     @Test
+    public void testStatusWithoutReasonPhrase() {
+        String responseStr = "HTTP/1.1 200 \r\n" +
+                "Content-Length: 0\r\n\r\n";
+        EmbeddedChannel channel = new EmbeddedChannel(new HttpResponseDecoder());
+        assertTrue(channel.writeInbound(Unpooled.copiedBuffer(responseStr, CharsetUtil.US_ASCII)));
+        HttpResponse response = channel.readInbound();
+        assertTrue(response.decoderResult().isSuccess());
+        assertEquals(HttpResponseStatus.OK, response.status());
+        HttpContent c = channel.readInbound();
+        c.release();
+        assertFalse(channel.finish());
+    }
+
+    @Test
     public void testHeaderNameStartsWithControlChar1c() {
         testHeaderNameStartsWithControlChar(0x1c);
     }