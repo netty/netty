e7a17cc2e9af8d5e729fc8cc8907464b708bf4f2
CLFutureX <775523362@qq.com>
2024-07-17 11:06:14 +0800

    Avoid adding tasks only to remove them later

--- common/src/main/java/io/netty/util/concurrent/SingleThreadEventExecutor.java
+++ common/src/main/java/io/netty/util/concurrent/SingleThreadEventExecutor.java
@@ -830,26 +830,10 @@ public abstract class SingleThreadEventExecutor extends AbstractScheduledEventEx
 
     private void execute(Runnable task, boolean immediate) {
         boolean inEventLoop = inEventLoop();
-        addTask(task);
         if (!inEventLoop) {
             startThread();
-            if (isShutdown()) {
-                boolean reject = false;
-                try {
-                    if (removeTask(task)) {
-                        reject = true;
-                    }
-                } catch (UnsupportedOperationException e) {
-                    // The task queue does not support removal so the best thing we can do is to just move on and
-                    // hope we will be able to pick-up the task before its completely terminated.
-                    // In worst case we will log on termination.
-                }
-                if (reject) {
-                    reject();
-                }
-            }
         }
-
+        addTask(task);
         if (!addTaskWakesUp && immediate) {
             wakeup(inEventLoop);
         }
