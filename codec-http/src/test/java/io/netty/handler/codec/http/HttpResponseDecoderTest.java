a872fbf53e5f873780c1e13e6b945fb81a52da3c
Norman Maurer <norman_maurer@apple.com>
2024-07-18 22:07:57 +0200

    Validate HTTP version while decoding (#14187)
    Motivation:
    
    We didnt strictly validate HTTP versions while decoding.
    
    Modifications:
    
    - Add extra validation logic in HttpRequestDecoder / HttpResponseDecoder
    - Add unit tests
    
    Result:
    
    Fixes https://github.com/netty/netty/issues/14186

--- codec-http/src/test/java/io/netty/handler/codec/http/HttpResponseDecoderTest.java
+++ codec-http/src/test/java/io/netty/handler/codec/http/HttpResponseDecoderTest.java
@@ -21,6 +21,8 @@ import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.codec.PrematureChannelClosureException;
 import io.netty.util.CharsetUtil;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -1126,6 +1128,14 @@ public class HttpResponseDecoderTest {
         responseBuffer.writeByte(controlChar);
         responseBuffer.writeCharSequence(": chunked\r\n\r\n", CharsetUtil.US_ASCII);
         testInvalidHeaders0(responseBuffer);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "HTP/1.1", "HTTP", "HTTP/1x", "Something/1.1", "HTTP/1",
+            "HTTP/1.11", "HTTP/11.1", "HTTP/A.1", "HTTP/1.B"})
+    public void testInvalidVersion(String version) {
+        testInvalidHeaders0(Unpooled.copiedBuffer(
+                version + " 200 OK\n\r\nHost: whatever\r\n\r\n", CharsetUtil.US_ASCII));
     }
 
     private static void testInvalidHeaders0(ByteBuf responseBuffer) {
