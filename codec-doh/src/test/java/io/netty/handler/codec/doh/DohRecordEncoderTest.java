937f9d1b01c1e3ff0aed0172d4174305ee7ea359
Iman Zolfaghari <6662935+imz87@users.noreply.github.com>
2024-08-07 10:33:09 +0330

    Update codec-doh/src/test/java/io/netty/handler/codec/doh/DohRecordEncoderTest.java
    Co-authored-by: Norman Maurer <norman_maurer@apple.com>

--- codec-doh/src/test/java/io/netty/handler/codec/doh/DohRecordEncoderTest.java
+++ codec-doh/src/test/java/io/netty/handler/codec/doh/DohRecordEncoderTest.java
@@ -28,8 +28,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Random;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
