8f191ad4d7d66f9ee204f8e50e947e81e0acedac
Norman Maurer <norman_maurer@apple.com>
2024-08-09 14:41:49 -1000

    Ensure AbstractCoalescingBufferQueue does not end up in inconsistent state on error
    Motivation:
    
    The code in AbstractCoalescingBufferQueue.remove(...) could lead to inconsistent internal state when for example an exception is thrown because of reference counting problems. Because of this is was possible that while the queue was empty it still showed readableBytes() > 0. This could then lead to various issues internally within netty where we depend on that the queue will eventually returns readableBytes() == 0.
    
    Modifications:
    
    - Ensure we always decrement readable bytes, even in the cause of an error
    - Also correctly remove the listener if an error happens during handling the buffer.
    
    Result:
    
    Fixes https://github.com/netty/netty/issues/11959

--- transport/src/main/java/io/netty/channel/AbstractCoalescingBufferQueue.java
+++ transport/src/main/java/io/netty/channel/AbstractCoalescingBufferQueue.java
@@ -154,9 +154,10 @@ public abstract class AbstractCoalescingBufferQueue {
         ByteBuf toReturn = null;
         ByteBuf entryBuffer = null;
         int originalBytes = bytes;
+        Object entry = null;
         try {
             for (;;) {
-                Object entry = bufAndListenerPairs.poll();
+                entry = bufAndListenerPairs.poll();
                 if (entry == null) {
                     break;
                 }
@@ -196,12 +197,22 @@ public abstract class AbstractCoalescingBufferQueue {
                 }
             }
         } catch (Throwable cause) {
+            if (entry instanceof ByteBuf) {
+                // Poll the next element if it's a listener that belongs to the ByteBuf.
+                entry = bufAndListenerPairs.peek();
+                if (entry instanceof ChannelFutureListener) {
+                    aggregatePromise.addListener((ChannelFutureListener) entry);
+                    bufAndListenerPairs.poll();
+                }
+            }
             safeRelease(entryBuffer);
             safeRelease(toReturn);
             aggregatePromise.setFailure(cause);
             throwException(cause);
+        } finally {
+            // Always decrement to keep things consistent
+            decrementReadableBytes(originalBytes - bytes);
         }
-        decrementReadableBytes(originalBytes - bytes);
         return toReturn;
     }
 
