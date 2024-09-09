bd0eb261d568497fab5ba785047554bf96deaff6
Norman Maurer <norman_maurer@apple.com>
2024-09-09 18:59:17 +0200

    Ensure pooled memory is released when AdaptivePoolingAllocator is GC'ed (#14300)
    Motivation:
    
    We need to ensure we release all pooled memory in
    AdaptivePoolingAllocator when the instance is GC'ed. Missing to do so
    might lead to leaks.
    
    Modifications:
    
    Add finalize() methods to the allocator that ensures everything is
    freed.
    
    Result:
    
    No more leaks due GC of allocator itself.

--- buffer/src/main/java/io/netty/buffer/AdaptivePoolingAllocator.java
+++ buffer/src/main/java/io/netty/buffer/AdaptivePoolingAllocator.java
@@ -313,6 +313,32 @@ final class AdaptivePoolingAllocator implements AdaptiveByteBufAllocator.Adaptiv
         return centralQueue.offer(buffer);
     }
 
+    // Ensure that we release all previous pooled resources when this object is finalized. This is needed as otherwise
+    // we might end up with leaks. While these leaks are usually harmless in reality it would still at least be
+    // very confusing for users.
+    @Override
+    protected void finalize() throws Throwable {
+        try {
+            super.finalize();
+        } finally {
+            free();
+        }
+    }
+
+    private void free() {
+        for (;;) {
+            Chunk chunk = centralQueue.poll();
+            if (chunk == null) {
+                break;
+            }
+            chunk.release();
+        }
+
+        for (Magazine magazine : magazines) {
+            magazine.free();
+        }
+    }
+
     @SuppressJava6Requirement(reason = "Guarded by version check")
     @SuppressWarnings("checkstyle:finalclass") // Checkstyle mistakenly believes this class should be final.
     private static class AllocationStatistics extends StampedLock {
@@ -493,6 +519,18 @@ final class AdaptivePoolingAllocator implements AdaptiveByteBufAllocator.Adaptiv
 
         boolean trySetNextInLine(Chunk chunk) {
             return NEXT_IN_LINE.compareAndSet(this, null, chunk);
+        }
+
+        void free() {
+            // Release the current Chunk and the next that was stored for later usage.
+            if (current != null) {
+                current.release();
+                current = null;
+            }
+            Chunk next = NEXT_IN_LINE.getAndSet(this, null);
+            if (next != null) {
+                next.release();
+            }
         }
     }
 
