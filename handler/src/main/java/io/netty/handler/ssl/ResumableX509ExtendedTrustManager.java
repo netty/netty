21e08501360da7b47f7a3b0aa58b5259804239d0
Chris Vest <christianvest_hansen@apple.com>
2024-09-23 14:28:11 -0700

    Javadocs for ResumableX509ExtendedTrustManager

--- handler/src/main/java/io/netty/handler/ssl/ResumableX509ExtendedTrustManager.java
+++ handler/src/main/java/io/netty/handler/ssl/ResumableX509ExtendedTrustManager.java
@@ -20,7 +20,53 @@ import java.security.cert.X509Certificate;
 import javax.net.ssl.SSLEngine;
 import javax.net.ssl.X509TrustManager;
 
+/**
+ * An interface that {@code TrustManager} instances can implement, to be notified of resumed SSL sessions.
+ * <p>
+ * A {@link javax.net.ssl.TrustManager} is called during the TLS handshake, and make decisions about whether
+ * the connected peer can be trusted or not. TLS include a feature where previously established sessions can
+ * be resumed without going through the trust verification steps.
+ * <p>
+ * However, some trust manager implementations also extract identity information and store it in the
+ * {@link javax.net.ssl.SSLSession} for use by the application. When a session is resumed,
+ * the {@code TrustManager} is not called, and the session map is empty, leading to confusion and errors in
+ * applications that don't expected this behavior.
+ * <p>
+ * The trust manager implementation can fix this by implementing this interface. When a session is resumed,
+ * the {@link SslHandler} will call the relevant {@code resume*} method, before completing the handshake
+ * promise and sending the {@link SslHandshakeCompletionEvent#SUCCESS} event down the pipeline.
+ * <p>
+ * A trust manager that does not add values to the handshake session in its {@code check*} methods,
+ * will typically not have any need to implement this interface.
+ * <p>
+ * The implementing trust manager class must extend {@code X509ExtendedTrustManager}, otherwise this interface
+ * will be ignored by the {@link SslHandler}.
+ */
 public interface ResumableX509ExtendedTrustManager extends X509TrustManager {
+    /**
+     * Given the partial or complete certificate chain recovered from the session ticket,
+     * and the {@link SSLEngine} being used, restore the application state of the associated
+     * SSL session.
+     * <p>
+     * This method should obtain the {@link javax.net.ssl.SSLSession} from the {@link SSLEngine#getSession()}
+     * method.
+     *
+     * @param chain The peer certificate chain.
+     * @param engine The begine used for this connection.
+     * @throws CertificateException If the certificate chain is no longer trusted.
+     */
     void resumeClientTrusted(X509Certificate[] chain, SSLEngine engine) throws CertificateException;
+
+    /**
+     * Given the partial or complete certificate chain recovered of the peer, and the {@link SSLEngine}
+     * being used, restore the application state of the associated SSL session.
+     * <p>
+     * This method should obtain the {@link javax.net.ssl.SSLSession} from the {@link SSLEngine#getSession()}
+     * method.
+     *
+     * @param chain The peer certificate chain.
+     * @param engine The begine used for this connection.
+     * @throws CertificateException If the certificate chain is no longer trusted.
+     */
     void resumeServerTrusted(X509Certificate[] chain, SSLEngine engine) throws CertificateException;
 }
