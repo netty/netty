c036b9917f20a2704150a0c034ad3477c2b737b0
Idel Pivnitskiy <idel.pivnitskiy@apple.com>
2024-09-26 13:04:09 -0700

    DnsNameResolver: allow users to skip bind() during bootstrap (#14375)
    Motivation:
    
    We prefer to bind by default for better debug logging (see #13817).
    However, some users expressed concerns with this behavior change because
    it eagerly binds a UDP port even when allocated resolver is not used.
    
    Modifications:
    
    - Keep default behavior to bind, but allow configuring `null` for
    `DnsNameResolverBuilder.localAddress(...)` to preserve pre-existing
    behavior.
    
    Result:
    
    Users can force pre-existing behavior of lazy port binding on resolve.

--- resolver-dns/src/main/java/io/netty/resolver/dns/DnsNameResolver.java
+++ resolver-dns/src/main/java/io/netty/resolver/dns/DnsNameResolver.java
@@ -383,6 +383,7 @@ public class DnsNameResolver extends InetNameResolver {
              searchDomains, ndots, decodeIdn, false, 0);
     }
 
+    @SuppressWarnings("deprecation")
     DnsNameResolver(
             EventLoop eventLoop,
             ChannelFactory<? extends DatagramChannel> channelFactory,
@@ -501,11 +502,13 @@ public class DnsNameResolver extends InetNameResolver {
             }
         });
 
+        final ChannelFuture future;
         if (localAddress == null) {
-            localAddress = new InetSocketAddress(0);
+            b.option(ChannelOption.DATAGRAM_CHANNEL_ACTIVE_ON_REGISTRATION, true);
+            future = b.register();
+        } else {
+            future = b.bind(localAddress);
         }
-
-        ChannelFuture future = b.bind(localAddress);
         if (future.isDone()) {
             Throwable cause = future.cause();
             if (cause != null) {
