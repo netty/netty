d5f4bfb6c9ca14bd5820fa61a9ce3352492de872
Norman Maurer <norman_maurer@apple.com>
2024-10-01 11:20:41 +0200

    Refactor DnsNameResolver to be able to use different strategies when … (#14374)
    …it comes to creating Channels for queries.
    
    Motivation:
    
    At the moment we reuse the same Channel (and so underyling socket) for
    all queries. It has show that this can cause problems in some
    deployments, we should allow to use different strategies.
    
    Modifications:
    
    - Refactor code to make it possible to use different strategies when it
    comes to creating / reusing channels for resolve operations
    
    Result:
    
    More flexible configuration and possible fix for
    https://github.com/netty/netty/issues/14364
    
    ---------
    
    Co-authored-by: Idel Pivnitskiy <idel.pivnitskiy@apple.com>

--- resolver-dns/src/main/java/io/netty/resolver/dns/DnsAddressResolveContext.java
+++ resolver-dns/src/main/java/io/netty/resolver/dns/DnsAddressResolveContext.java
@@ -26,7 +26,6 @@ import io.netty.channel.Channel;
 import io.netty.channel.EventLoop;
 import io.netty.handler.codec.dns.DnsRecord;
 import io.netty.handler.codec.dns.DnsRecordType;
-import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.Promise;
 
 final class DnsAddressResolveContext extends DnsResolveContext<InetAddress> {
@@ -35,12 +34,12 @@ final class DnsAddressResolveContext extends DnsResolveContext<InetAddress> {
     private final AuthoritativeDnsServerCache authoritativeDnsServerCache;
     private final boolean completeEarlyIfPossible;
 
-    DnsAddressResolveContext(DnsNameResolver parent, Channel channel, Future<? extends Channel> channelReadyFuture,
+    DnsAddressResolveContext(DnsNameResolver parent, Channel channel,
                              Promise<?> originalPromise, String hostname, DnsRecord[] additionals,
                              DnsServerAddressStream nameServerAddrs, int allowedQueries, DnsCache resolveCache,
                              AuthoritativeDnsServerCache authoritativeDnsServerCache,
                              boolean completeEarlyIfPossible) {
-        super(parent, channel, channelReadyFuture, originalPromise, hostname, DnsRecord.CLASS_IN,
+        super(parent, channel, originalPromise, hostname, DnsRecord.CLASS_IN,
               parent.resolveRecordTypes(), additionals, nameServerAddrs, allowedQueries);
         this.resolveCache = resolveCache;
         this.authoritativeDnsServerCache = authoritativeDnsServerCache;
@@ -49,13 +48,12 @@ final class DnsAddressResolveContext extends DnsResolveContext<InetAddress> {
 
     @Override
     DnsResolveContext<InetAddress> newResolverContext(DnsNameResolver parent, Channel channel,
-                                                      Future<? extends Channel> channelReadyFuture,
                                                       Promise<?> originalPromise,
                                                       String hostname,
                                                       int dnsClass, DnsRecordType[] expectedTypes,
                                                       DnsRecord[] additionals,
                                                       DnsServerAddressStream nameServerAddrs, int allowedQueries) {
-        return new DnsAddressResolveContext(parent, channel, channelReadyFuture, originalPromise, hostname, additionals,
+        return new DnsAddressResolveContext(parent, channel, originalPromise, hostname, additionals,
                 nameServerAddrs, allowedQueries, resolveCache, authoritativeDnsServerCache, completeEarlyIfPossible);
     }
 
@@ -84,7 +82,8 @@ final class DnsAddressResolveContext extends DnsResolveContext<InetAddress> {
     @Override
     void cache(String hostname, DnsRecord[] additionals,
                DnsRecord result, InetAddress convertedResult) {
-        resolveCache.cache(hostname, additionals, convertedResult, result.timeToLive(), channel().eventLoop());
+        resolveCache.cache(hostname, additionals, convertedResult, result.timeToLive(),
+                channel().eventLoop());
     }
 
     @Override
