dc356e82896d2062fdaf4e6f08b1d817ea9ea706
Chris Vest <christianvest_hansen@apple.com>
2024-10-22 13:09:02 -0700

    Fix SSL session resumption with ClientAuth.OPTIONAL and add tests with session tickets (#14404)
    Motivation:
    If TLS is `OPTIONAL` then there won't always be a verified peer.
    This means there will be no calls on the server `TrustManager` to check
    if the client is trusted.
    That makes it look like a resumed session to the resumption controller.
    But there won't be any verified peer on the session, and trying to get
    the peer certificates will throw an exception.
    
    It's also a valid configuration to enable session tickets with TLSv1.2,
    and doing so makes a meaningful change to the `SSLEngine` behavior that
    we need to test for.
    
    Modification:
    - Collect the OpenSSL parameterization code for `SSLEngineTest`.
    - Add tickets being enabled or not as a parameter.
    - Fix a test that did not anticipate the session ticket behavior.
    - Make the `ResumptionController` consider if the engine is in client
    mode, or if it requires client authentication, and only propagate
    `SSLPeerUnverifiedException` if so.
    - Make the `ResumptionController` swallow the
    `SSLPeerUnverifiedException` when client auth is OPTIONAL or NONE.
    - Add a test for this scenario.
    
    Result:
    We now have test coverage with TLSv1.2 session tickets.
    The `ResumableX509ExtendedTrustManager` interface can now be used
    together with `ClientAuth.OPTIONAL`.

--- handler/src/test/java/io/netty/handler/ssl/ConscryptOpenSslEngineInteropTest.java
+++ handler/src/test/java/io/netty/handler/ssl/ConscryptOpenSslEngineInteropTest.java
@@ -40,8 +40,7 @@ public class ConscryptOpenSslEngineInteropTest extends ConscryptSslEngineTest {
         List<SSLEngineTestParam> params = super.newTestParams();
         List<SSLEngineTestParam> testParams = new ArrayList<SSLEngineTestParam>();
         for (SSLEngineTestParam param: params) {
-            testParams.add(new OpenSslEngineTestParam(true, param));
-            testParams.add(new OpenSslEngineTestParam(false, param));
+            OpenSslEngineTestParam.expandCombinations(param, testParams);
         }
         return testParams;
     }
@@ -212,16 +211,8 @@ public class ConscryptOpenSslEngineInteropTest extends ConscryptSslEngineTest {
         return Java8SslTestUtils.wrapSSLEngineForTesting(engine);
     }
 
-    @SuppressWarnings("deprecation")
     @Override
     protected SslContext wrapContext(SSLEngineTestParam param, SslContext context) {
-        if (context instanceof OpenSslContext) {
-            if (param instanceof OpenSslEngineTestParam) {
-                ((OpenSslContext) context).setUseTasks(((OpenSslEngineTestParam) param).useTasks);
-            }
-            // Explicit enable the session cache as its disabled by default on the client side.
-            ((OpenSslContext) context).sessionContext().setSessionCacheEnabled(true);
-        }
-        return context;
+        return OpenSslEngineTestParam.wrapContext(param, context);
     }
 }
