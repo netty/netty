52daadde212b371989c41af2fbac1009920edf3e
Norman Maurer <norman_maurer@apple.com>
2024-11-27 12:31:18 +0100

    Decrease usedMemory of magazine when the chunk get deallocated or mov… (#14515)
    …ed to central queue (#14508)
    
    Motivation:
    
    For `AdaptivePoolingAllocator`, the `magazine.usedMemory` should be
    decreased when the chunk be deallocated or be offered to the
    `centralQueue`.
    
    Modification:
    
    Decreased the `magazine.usedMemory` when the chunk be deallocated or be
    offered to the `centralQueue`.
    
    Result:
    
    The `magazine.usedMemory` becomes more accurate.
    
    Co-authored-by: old driver <29225782+laosijikaichele@users.noreply.github.com>

--- buffer/src/main/java/io/netty/buffer/AdaptivePoolingAllocator.java
+++ buffer/src/main/java/io/netty/buffer/AdaptivePoolingAllocator.java
@@ -820,6 +820,8 @@ final class AdaptivePoolingAllocator {
                 delegate.setIndex(0, 0);
                 allocatedBytes = 0;
                 if (!mag.trySetNextInLine(this)) {
+                    // As this Chunk does not belong to the mag anymore we need to decrease the used memory .
+                    mag.usedMemory.getAndAdd(-capacity());
                     if (!parent.offerToQueue(this)) {
                         // The central queue is full. Ensure we release again as we previously did use resetRefCnt()
                         // which did increase the reference count by 1.
