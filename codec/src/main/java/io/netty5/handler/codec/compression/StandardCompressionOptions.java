32d1e7879dc0993cff885f54274962e285bbbc46
Norman Maurer <norman_maurer@apple.com>
2025-01-13 05:48:37 -0400

    Provides Brotli settings without `com.aayushatharva.brotli4j` depende… (#14655)
    …ncy (#14629)
    
    Motivation:
    
    Currently, in Netty, it is difficult to configure **Brotli** compression
    parameters such as `quality`, `window`, and `mode` without adding the
    `com.aayushatharva.brotli4j` dependency.
    
    Modification:
    
    Add new enum to allow configure Brotli without the need to use brotlij
    classes.
    
    Result:
    
    Cleanup API.
    
    Co-authored-by: KOSEUNGBIN <kosb15@naver.com>

--- codec/src/main/java/io/netty5/handler/codec/compression/StandardCompressionOptions.java
+++ codec/src/main/java/io/netty5/handler/codec/compression/StandardCompressionOptions.java
@@ -16,6 +16,9 @@
 package io.netty5.handler.codec.compression;
 
 import com.aayushatharva.brotli4j.encoder.Encoder;
+import io.netty5.util.internal.ObjectUtil;
+
+import java.util.Objects;
 
 /**
  * Standard Compression Options for {@link BrotliOptions},
@@ -38,10 +41,20 @@ public final class StandardCompressionOptions {
     /**
      * Create a new {@link BrotliOptions}
      *
-     * @param parameters {@link Encoder.Parameters} Instance
-     * @throws NullPointerException If {@link Encoder.Parameters} is {@code null}
+     * @param quality Specifies the compression level.
+     * @param window  Specifies the size of the sliding window when compressing.
+     * @param mode    optimizes the compression algorithm based on the type of input data.
+     * @throws NullPointerException If {@link BrotliMode} is {@code null}
      */
-    public static BrotliOptions brotli(Encoder.Parameters parameters) {
+    public static BrotliOptions brotli(int quality, int window, BrotliMode mode) {
+        ObjectUtil.checkInRange(quality, 0, 11, "quality");
+        ObjectUtil.checkInRange(window, 10, 24, "window");
+        Objects.requireNonNull(mode, "mode");
+
+        Encoder.Parameters parameters = new Encoder.Parameters()
+                .setQuality(quality)
+                .setWindow(window)
+                .setMode(mode.adapt());
         return new BrotliOptions(parameters);
     }
 
@@ -57,12 +70,9 @@ public final class StandardCompressionOptions {
     /**
      * Create a new {@link ZstdOptions}
      *
-     * @param  blockSize
-     *           is used to calculate the compressionLevel
-     * @param  maxEncodeSize
-     *           specifies the size of the largest compressed object
-     * @param  compressionLevel
-     *           specifies the level of the compression
+     * @param blockSize        is used to calculate the compressionLevel
+     * @param maxEncodeSize    specifies the size of the largest compressed object
+     * @param compressionLevel specifies the level of the compression
      */
     public static ZstdOptions zstd(int compressionLevel, int blockSize, int maxEncodeSize) {
         return new ZstdOptions(compressionLevel, blockSize, maxEncodeSize);
@@ -70,13 +80,12 @@ public final class StandardCompressionOptions {
 
     /**
      * Create a new {@link SnappyOptions}
-     *
      */
     public static SnappyOptions snappy() {
         return new SnappyOptions();
     }
 
-     /**
+    /**
      * Default implementation of {@link GzipOptions} with
      * {@code compressionLevel()} set to 6, {@code windowBits()} set to 15 and {@code memLevel()} set to 8.
      */
@@ -90,7 +99,6 @@ public final class StandardCompressionOptions {
      * @param compressionLevel {@code 1} yields the fastest compression and {@code 9} yields the
      *                         best compression.  {@code 0} means no compression.  The default
      *                         compression level is {@code 6}.
-     *
      */
     public static GzipOptions gzip(int compressionLevel) {
         return new GzipOptions(compressionLevel);
@@ -110,7 +118,6 @@ public final class StandardCompressionOptions {
      * @param compressionLevel {@code 1} yields the fastest compression and {@code 9} yields the
      *                         best compression.  {@code 0} means no compression.  The default
      *                         compression level is {@code 6}.
-     *
      */
     public static DeflateOptions deflate(int compressionLevel) {
         return new DeflateOptions(compressionLevel);
