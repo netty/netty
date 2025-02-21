7cafa31736fac84482029250f076e24b64e7c7b4
Norman Maurer <norman_maurer@apple.com>
2025-02-21 07:45:40 +0000

    Move duplicated code to internal package and add tests

--- transport-classes-io_uring/src/main/java/io/netty/channel/uring/IoUringAdaptiveBufferRingAllocator.java
+++ transport-classes-io_uring/src/main/java/io/netty/channel/uring/IoUringAdaptiveBufferRingAllocator.java
@@ -17,14 +17,9 @@ package io.netty.channel.uring;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
+import io.netty.util.internal.AdaptiveCalculator;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
-
-import static io.netty.util.internal.ObjectUtil.checkPositive;
-import static java.lang.Math.max;
-import static java.lang.Math.min;
 
 /**
  * {@link IoUringBufferRingAllocator} implementation which uses an adaptive strategy to allocate buffers, which
@@ -36,60 +31,8 @@ public final class IoUringAdaptiveBufferRingAllocator implements IoUringBufferRi
     public static final int DEFAULT_INITIAL = 4096;
     public static final int DEFAULT_MAXIMUM = 65536;
 
-    private static final int INDEX_INCREMENT = 4;
-    private static final int INDEX_DECREMENT = 1;
-
-    private static final int[] SIZE_TABLE;
-
-    static {
-        List<Integer> sizeTable = new ArrayList<Integer>();
-        for (int i = 16; i < 512; i += 16) {
-            sizeTable.add(i);
-        }
-
-        // Suppress a warning since i becomes negative when an integer overflow happens
-        for (int i = 512; i > 0; i <<= 1) {
-            sizeTable.add(i);
-        }
-
-        SIZE_TABLE = new int[sizeTable.size()];
-        for (int i = 0; i < SIZE_TABLE.length; i ++) {
-            SIZE_TABLE[i] = sizeTable.get(i);
-        }
-    }
-
-    private static int getSizeTableIndex(final int size) {
-        for (int low = 0, high = SIZE_TABLE.length - 1;;) {
-            if (high < low) {
-                return low;
-            }
-            if (high == low) {
-                return high;
-            }
-
-            int mid = low + high >>> 1;
-            int a = SIZE_TABLE[mid];
-            int b = SIZE_TABLE[mid + 1];
-            if (size > b) {
-                low = mid + 1;
-            } else if (size < a) {
-                high = mid - 1;
-            } else if (size == a) {
-                return mid;
-            } else {
-                return mid + 1;
-            }
-        }
-    }
-
     private final ByteBufAllocator allocator;
-    private final int minIndex;
-    private final int maxIndex;
-    private final int minCapacity;
-    private final int maxCapacity;
-    private int index;
-    private int nextReceiveBufferSize;
-    private boolean decreaseNow;
+    private final AdaptiveCalculator calculator;
 
     public IoUringAdaptiveBufferRingAllocator() {
         this(ByteBufAllocator.DEFAULT);
@@ -114,43 +57,12 @@ public final class IoUringAdaptiveBufferRingAllocator implements IoUringBufferRi
      */
     public IoUringAdaptiveBufferRingAllocator(ByteBufAllocator allocator, int minimum, int initial, int maximum) {
         this.allocator = Objects.requireNonNull(allocator, "allocator");
-        checkPositive(minimum, "minimum");
-        if (initial < minimum) {
-            throw new IllegalArgumentException("initial: " + initial);
-        }
-        if (maximum < initial) {
-            throw new IllegalArgumentException("maximum: " + maximum);
-        }
-
-        int minIndex = getSizeTableIndex(minimum);
-        if (SIZE_TABLE[minIndex] < minimum) {
-            this.minIndex = minIndex + 1;
-        } else {
-            this.minIndex = minIndex;
-        }
-
-        int maxIndex = getSizeTableIndex(maximum);
-        if (SIZE_TABLE[maxIndex] > maximum) {
-            this.maxIndex = maxIndex - 1;
-        } else {
-            this.maxIndex = maxIndex;
-        }
-
-        int initialIndex = getSizeTableIndex(initial);
-        if (SIZE_TABLE[initialIndex] > initial) {
-            this.index = initialIndex - 1;
-        } else {
-            this.index = initialIndex;
-        }
-        this.minCapacity = minimum;
-        this.maxCapacity = maximum;
-
-        nextReceiveBufferSize = max(SIZE_TABLE[index], minCapacity);
+        this.calculator = new AdaptiveCalculator(minimum, initial, maximum);
     }
 
     @Override
     public ByteBuf allocate() {
-        return allocator.directBuffer(nextReceiveBufferSize);
+        return allocator.directBuffer(nextSize());
     }
 
     @Override
@@ -160,23 +72,12 @@ public final class IoUringAdaptiveBufferRingAllocator implements IoUringBufferRi
         // the selector to check for more data. Going back to the selector can add significant latency for large
         // data transfers.
         if (attempted == actual) {
-            record(actual);
+            calculator.record(actual);
         }
     }
 
-    private void record(int actualReadBytes) {
-        if (actualReadBytes <= SIZE_TABLE[max(0, index - INDEX_DECREMENT)]) {
-            if (decreaseNow) {
-                index = max(index - INDEX_DECREMENT, minIndex);
-                nextReceiveBufferSize = max(SIZE_TABLE[index], minCapacity);
-                decreaseNow = false;
-            } else {
-                decreaseNow = true;
-            }
-        } else if (actualReadBytes >= nextReceiveBufferSize) {
-            index = min(index + INDEX_INCREMENT, maxIndex);
-            nextReceiveBufferSize = min(SIZE_TABLE[index], maxCapacity);
-            decreaseNow = false;
-        }
+    // Visible for testing
+    int nextSize() {
+        return calculator.nextSize();
     }
 }
