netty/netty

commit f8789296a5db4050d9c88ff9a303cb2a3d87e463

IoUring: Guard against reading to fast when using a buffer ring (#14873)
Motivation:

When using a buffer ring combined with multishot recv and recv bundles
it's possible that we read so fast that we eventually will run out of
memory. We should try to slow down a bit when we have too many buffers
used.

Modifications:

- Keep track of how many unreleased buffers we have used out of the ring
and only use it when this number did not reach a threshold.

Result:

Guard against OOME.

transport-classes-io_uring/src/main/java/io/netty/channel/uring/IoUringBufferRing.java
@@ -16,42 +16,48 @@
 package io.netty.channel.uring;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.WrappedByteBuf;
 import io.netty.util.internal.PlatformDependent;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 final class IoUringBufferRing {
     private final long ioUringBufRingAddr;
     private final long tailFieldAddress;
     private final short entries;
+    private final int maxUnreleasedBuffers;
     private final short mask;
     private final short bufferGroupId;
     private final int ringFd;
-    private final ByteBuf[] buffers;
+    private final IoUringBufferRingByteBuf[] buffers;
     private final IoUringBufferRingAllocator allocator;
     private final IoUringBufferRingExhaustedEvent exhaustedEvent;
     private final boolean incremental;
+    private final AtomicInteger unreleasedBuffers = new AtomicInteger();
+    private volatile boolean usable;
     private boolean corrupted;
 
     IoUringBufferRing(int ringFd, long ioUringBufRingAddr,
-                      short entries, short bufferGroupId, boolean incremental,
+                      short entries, int maxUnreleasedBuffers, short bufferGroupId, boolean incremental,
                       IoUringBufferRingAllocator allocator) {
         assert entries % 2 == 0;
         this.ioUringBufRingAddr = ioUringBufRingAddr;
         this.tailFieldAddress = ioUringBufRingAddr + Native.IO_URING_BUFFER_RING_TAIL;
         this.entries = entries;
+        this.maxUnreleasedBuffers = maxUnreleasedBuffers;
         this.mask = (short) (entries - 1);
         this.bufferGroupId = bufferGroupId;
         this.ringFd = ringFd;
-        this.buffers = new ByteBuf[entries];
+        this.buffers = new IoUringBufferRingByteBuf[entries];
         this.incremental = incremental;
         this.allocator = allocator;
         this.exhaustedEvent = new IoUringBufferRingExhaustedEvent(bufferGroupId);
     }
 
     boolean isUsable() {
-        return !corrupted;
+        return !corrupted && usable;
     }
 
     void fill() {
@@ -87,7 +93,7 @@ final class IoUringBufferRing {
             throw e;
         }
         byteBuf.writerIndex(byteBuf.capacity());
-        buffers[bid] = byteBuf;
+        buffers[bid] = new IoUringBufferRingByteBuf(byteBuf);
 
         //  see:
         //  https://github.com/axboe/liburing/
@@ -124,7 +130,7 @@ final class IoUringBufferRing {
      */
     ByteBuf useBuffer(short bid, int readableBytes, boolean more) {
         assert readableBytes > 0;
-        ByteBuf byteBuf = buffers[bid];
+        IoUringBufferRingByteBuf byteBuf = buffers[bid];
 
         allocator.lastBytesRead(byteBuf.readableBytes(), readableBytes);
         if (incremental && more && byteBuf.readableBytes() > readableBytes) {
@@ -134,6 +140,7 @@ final class IoUringBufferRing {
 
         // The buffer is considered to be used, null out the slot.
         buffers[bid] = null;
+        byteBuf.markUsed();
         return byteBuf.writerIndex(byteBuf.readerIndex() +
                 Math.min(readableBytes, byteBuf.readableBytes()));
     }
@@ -162,5 +169,41 @@ final class IoUringBufferRing {
             }
         }
         Arrays.fill(buffers, null);
+    }
+
+    private final class IoUringBufferRingByteBuf extends WrappedByteBuf {
+        IoUringBufferRingByteBuf(ByteBuf buf) {
+            super(buf);
+        }
+
+        void markUsed() {
+            if (unreleasedBuffers.incrementAndGet() == maxUnreleasedBuffers) {
+                usable = false;
+            }
+        }
+
+        @Override
+        public boolean release() {
+            if (super.release()) {
+                released();
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public boolean release(int decrement) {
+            if (super.release(decrement)) {
+                released();
+                return true;
+            }
+            return false;
+        }
+
+        private void released() {
+            if (unreleasedBuffers.decrementAndGet() == maxUnreleasedBuffers / 2) {
+                usable = true;
+            }
+        }
     }
 }

transport-native-io_uring/src/test/java/io/netty/channel/uring/IoUringBufferRingTest.java
@@ -83,10 +83,10 @@ public class IoUringBufferRingTest {
         final BlockingQueue<ByteBuf> bufferSyncer = new LinkedBlockingQueue<>();
         IoUringIoHandlerConfig ioUringIoHandlerConfiguration = new IoUringIoHandlerConfig();
         IoUringBufferRingConfig bufferRingConfig = new IoUringBufferRingConfig(
-                (short) 1, (short) 2, incremental, new IoUringFixedBufferRingAllocator(1024));
+                (short) 1, (short) 2, 2 * 16, incremental, new IoUringFixedBufferRingAllocator(1024));
 
         IoUringBufferRingConfig bufferRingConfig1 = new IoUringBufferRingConfig(
-                (short) 2, (short) 16, incremental, new IoUringFixedBufferRingAllocator(1024)
+                (short) 2, (short) 16, 16 * 16, incremental, new IoUringFixedBufferRingAllocator(1024)
         );
         ioUringIoHandlerConfiguration.setBufferRingConfig(bufferRingConfig, bufferRingConfig1);
 
@@ -172,7 +172,7 @@ public class IoUringBufferRingTest {
         IoUringIoHandlerConfig ioUringIoHandlerConfiguration = new IoUringIoHandlerConfig();
         IoUringBufferRingConfig bufferRingConfig = new IoUringBufferRingConfig(
                 // let's use a small chunkSize so we are sure a recv will span multiple buffers.
-                (short) 1, (short) 16, incremental, new IoUringFixedBufferRingAllocator(bufferRingChunkSize));
+                (short) 1, (short) 16, 16 * 16, incremental, new IoUringFixedBufferRingAllocator(bufferRingChunkSize));
 
         ioUringIoHandlerConfiguration.setBufferRingConfig(bufferRingConfig);
 

buffer/src/main/java/io/netty/buffer/WrappedByteBuf.java
@@ -37,7 +37,7 @@ import java.nio.charset.Charset;
  * indices on the fly because of internal optimizations made by {@link ByteBufUtil#writeAscii(ByteBuf, CharSequence)}
  * and {@link ByteBufUtil#writeUtf8(ByteBuf, CharSequence)}.
  */
-class WrappedByteBuf extends ByteBuf {
+public class WrappedByteBuf extends ByteBuf {
 
     protected final ByteBuf buf;
 

transport-classes-io_uring/src/main/java/io/netty/channel/uring/IoUringBufferRingConfig.java
@@ -26,34 +26,44 @@ import java.util.Objects;
 public final class IoUringBufferRingConfig {
     private final short bgId;
     private final short bufferRingSize;
+    private final int maxUnreleasedBuffers;
     private final boolean incremental;
     private final IoUringBufferRingAllocator allocator;
 
     /**
      * Create a new configuration.
      *
-     * @param bgId              the buffer group id to use (must be non-negative).
-     * @param bufferRingSize    the size of the ring
-     * @param allocator         the {@link IoUringBufferRingAllocator} to use to allocate
-     *                          {@link io.netty.buffer.ByteBuf}s.
+     * @param bgId                  the buffer group id to use (must be non-negative).
+     * @param bufferRingSize        the size of the ring
+     * @param maxUnreleasedBuffers  the maximum buffers that were allocated out of this buffer ring and are
+     *                              unreleased yet. Once this threshold is hit the usage of the buffer ring will
+     *                              be temporary disabled.
+     * @param allocator             the {@link IoUringBufferRingAllocator} to use to allocate
+     *                              {@link io.netty.buffer.ByteBuf}s.
      */
-    public IoUringBufferRingConfig(short bgId, short bufferRingSize, IoUringBufferRingAllocator allocator) {
-        this(bgId, bufferRingSize, IoUring.isRegisterBufferRingIncSupported(), allocator);
+    public IoUringBufferRingConfig(short bgId, short bufferRingSize, int maxUnreleasedBuffers,
+                                   IoUringBufferRingAllocator allocator) {
+        this(bgId, bufferRingSize, maxUnreleasedBuffers, IoUring.isRegisterBufferRingIncSupported(), allocator);
     }
 
     /**
      * Create a new configuration.
      *
-     * @param bgId              the buffer group id to use (must be non-negative).
-     * @param bufferRingSize    the size of the ring
-     * @param incremental       {@code true} if the buffer ring is using incremental buffer consumption.
-     * @param allocator         the {@link IoUringBufferRingAllocator} to use to allocate
-     *                          {@link io.netty.buffer.ByteBuf}s.
+     * @param bgId                  the buffer group id to use (must be non-negative).
+     * @param bufferRingSize        the size of the ring
+     * @param maxUnreleasedBuffers  the maximum buffers that can be allocated out of this buffer ring and are
+     *                              unreleased yet. Once this threshold is hit the usage of the buffer ring will
+     *                              be temporarily disabled.
+     * @param incremental           {@code true} if the buffer ring is using incremental buffer consumption.
+     * @param allocator             the {@link IoUringBufferRingAllocator} to use to allocate
+     *                              {@link io.netty.buffer.ByteBuf}s.
      */
-    public IoUringBufferRingConfig(short bgId, short bufferRingSize, boolean incremental,
-                                   IoUringBufferRingAllocator allocator) {
+    public IoUringBufferRingConfig(short bgId, short bufferRingSize, int maxUnreleasedBuffers,
+                                   boolean incremental, IoUringBufferRingAllocator allocator) {
         this.bgId = (short) ObjectUtil.checkPositiveOrZero(bgId, "bgId");
         this.bufferRingSize = checkBufferRingSize(bufferRingSize);
+        this.maxUnreleasedBuffers = ObjectUtil.checkInRange(
+                maxUnreleasedBuffers, bufferRingSize, Integer.MAX_VALUE, "maxUnreleasedBuffers");
         if (incremental && !IoUring.isRegisterBufferRingIncSupported()) {
             throw new IllegalArgumentException("Incremental buffer ring is not supported");
         }
@@ -77,6 +87,16 @@ public final class IoUringBufferRingConfig {
      */
     public short bufferRingSize() {
         return bufferRingSize;
+    }
+
+    /**
+     * Returns the maximum buffers that can be allocated out of this buffer ring and are
+     * unreleased yet
+     *
+     * @return the max unreleased buffers.
+     */
+    public int maxUnreleasedBuffers() {
+        return maxUnreleasedBuffers;
     }
 
     /**

transport-classes-io_uring/src/main/java/io/netty/channel/uring/IoUringIoHandler.java
@@ -202,9 +202,9 @@ public final class IoUringIoHandler implements IoHandler {
         if (ioUringBufRingAddr < 0) {
             throw Errors.newIOException("ioUringRegisterBuffRing", (int) ioUringBufRingAddr);
         }
-        return new IoUringBufferRing(
-                ringFd, ioUringBufRingAddr,
-                bufferRingSize, bufferGroupId, bufferRingConfig.isIncremental(), bufferRingConfig.allocator()
+        return new IoUringBufferRing(ringFd, ioUringBufRingAddr,
+                bufferRingSize, bufferRingConfig.maxUnreleasedBuffers(),
+                bufferGroupId, bufferRingConfig.isIncremental(), bufferRingConfig.allocator()
         );
     }
 

transport-native-io_uring/src/test/java/io/netty/channel/uring/IoUringSocketTestPermutation.java
@@ -46,7 +46,7 @@ public class IoUringSocketTestPermutation extends SocketTestPermutation {
             WORKERS, new DefaultThreadFactory("testsuite-io_uring-worker", true),
             IoUringIoHandler.newFactory(new IoUringIoHandlerConfig()
                     .setBufferRingConfig(
-                            new IoUringBufferRingConfig(BGID, (short) 16,
+                            new IoUringBufferRingConfig(BGID, (short) 16, 16 * 16,
                                     new IoUringFixedBufferRingAllocator(1024)))));
     @Override
     public List<BootstrapComboFactory<ServerBootstrap, Bootstrap>> socket() {

commit e7d9dfa863917e12288aff2a04c133d17f96ec33

IoUring: Fix IoUringBufferRing
Motivation:

f8789296a5db4050d9c88ff9a303cb2a3d87e463 introduced a regression that caused the buffer ring to be never used at all as it never was marked as usable. Beside this it also didn't wrap buffers correctly in case of slice / duplicate / readOnly / order calls. This could lead to the situation that a buffer ring would never become usable again.

Modification:

- Correctly set usable to true on first fill of buffer ring
- Override methods and correctly wrap
- Adjust tests to guard against regressions

Result:

Buffer ring is usable again and correctly is marked as usable in all cases

transport-classes-io_uring/src/main/java/io/netty/channel/uring/IoUringBufferRing.java
@@ -16,11 +16,14 @@
 package io.netty.channel.uring;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.DuplicatedByteBuf;
+import io.netty.buffer.SlicedByteBuf;
+import io.netty.buffer.SwappedByteBuf;
 import io.netty.buffer.WrappedByteBuf;
 import io.netty.util.internal.PlatformDependent;
 
+import java.nio.ByteOrder;
 import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 final class IoUringBufferRing {
@@ -64,6 +67,7 @@ final class IoUringBufferRing {
         for (short i = 0; i < entries; i++) {
             fillBuffer(i);
         }
+        usable = true;
     }
 
     /**
@@ -171,7 +175,8 @@ final class IoUringBufferRing {
         Arrays.fill(buffers, null);
     }
 
-    private final class IoUringBufferRingByteBuf extends WrappedByteBuf {
+    // Package-private for testing
+    final class IoUringBufferRingByteBuf extends WrappedByteBuf {
         IoUringBufferRingByteBuf(ByteBuf buf) {
             super(buf);
         }
@@ -180,6 +185,66 @@ final class IoUringBufferRing {
             if (unreleasedBuffers.incrementAndGet() == maxUnreleasedBuffers) {
                 usable = false;
             }
+        }
+
+        @SuppressWarnings("deprecation")
+        @Override
+        public ByteBuf order(ByteOrder endianness) {
+            if (endianness == order()) {
+                return this;
+            }
+            return new SwappedByteBuf(this);
+        }
+
+        @Override
+        public ByteBuf slice() {
+            return slice(readerIndex(), readableBytes());
+        }
+
+        @Override
+        public ByteBuf retainedSlice() {
+            return slice().retain();
+        }
+
+        @SuppressWarnings("deprecation")
+        @Override
+        public ByteBuf slice(int index, int length) {
+            return new SlicedByteBuf(this, index, length);
+        }
+
+        @Override
+        public ByteBuf retainedSlice(int index, int length) {
+            return slice(index, length).retain();
+        }
+
+        @Override
+        public ByteBuf readSlice(int length) {
+            ByteBuf slice = slice(readerIndex(), length);
+            skipBytes(length);
+            return slice;
+        }
+
+        @Override
+        public ByteBuf readRetainedSlice(int length) {
+            ByteBuf slice = retainedSlice(readerIndex(), length);
+            try {
+                skipBytes(length);
+            } catch (Throwable cause) {
+                slice.release();
+                throw cause;
+            }
+            return slice;
+        }
+
+        @SuppressWarnings("deprecation")
+        @Override
+        public ByteBuf duplicate() {
+            return new DuplicatedByteBuf(this);
+        }
+
+        @Override
+        public ByteBuf retainedDuplicate() {
+            return duplicate().retain();
         }
 
         @Override

transport-native-io_uring/src/test/java/io/netty/channel/uring/IoUringBufferRingTest.java
@@ -26,7 +26,6 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.MultiThreadIoEventLoopGroup;
-import io.netty.channel.unix.Socket;
 import io.netty.util.NetUtil;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -35,13 +34,13 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.net.InetSocketAddress;
-import java.net.ServerSocket;
 import java.util.UUID;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
@@ -131,8 +130,19 @@ public class IoUringBufferRingTest {
         ByteBuf writeBuffer = Unpooled.directBuffer(randomStringLength);
         ByteBufUtil.writeAscii(writeBuffer, randomString);
         ByteBuf userspaceIoUringBufferElement1 = sendAndRecvMessage(clientChannel, writeBuffer, bufferSyncer);
+        if (incremental) {
+            // Need to unwrap as its a slice.
+            assertInstanceOf(IoUringBufferRing.IoUringBufferRingByteBuf.class, userspaceIoUringBufferElement1.unwrap());
+        } else {
+            assertInstanceOf(IoUringBufferRing.IoUringBufferRingByteBuf.class, userspaceIoUringBufferElement1);
+        }
         ByteBuf userspaceIoUringBufferElement2 = sendAndRecvMessage(clientChannel, writeBuffer, bufferSyncer);
-
+        if (incremental) {
+            // Need to unwrap as its a slice.
+            assertInstanceOf(IoUringBufferRing.IoUringBufferRingByteBuf.class, userspaceIoUringBufferElement2.unwrap());
+        } else {
+            assertInstanceOf(IoUringBufferRing.IoUringBufferRingByteBuf.class, userspaceIoUringBufferElement2);
+        }
         ByteBuf readBuffer = sendAndRecvMessage(clientChannel, writeBuffer, bufferSyncer);
         readBuffer.release();
 