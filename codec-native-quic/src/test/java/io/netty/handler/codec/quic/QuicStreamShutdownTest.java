76704039598a4cc9ca1a77c4a14b298fa48fbd20
Chris Vest <christianvest_hansen@apple.com>
2025-04-24 10:18:48 -0700

    Fix checkstyle issue

--- codec-native-quic/src/test/java/io/netty/handler/codec/quic/QuicStreamShutdownTest.java
+++ codec-native-quic/src/test/java/io/netty/handler/codec/quic/QuicStreamShutdownTest.java
@@ -123,7 +123,6 @@ public class QuicStreamShutdownTest extends AbstractQuicTest {
                                             }
                                         });
                                     }
-
                                 }
                             });
                         }
