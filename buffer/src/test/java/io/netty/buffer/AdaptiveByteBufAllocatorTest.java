cf2e60b4a458f5cf27a552199d955b60fdc69f21
Norman Maurer <norman_maurer@apple.com>
2025-04-26 19:19:11 +0200

    Add import

--- buffer/src/test/java/io/netty/buffer/AdaptiveByteBufAllocatorTest.java
+++ buffer/src/test/java/io/netty/buffer/AdaptiveByteBufAllocatorTest.java
@@ -28,6 +28,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class AdaptiveByteBufAllocatorTest extends AbstractByteBufAllocatorTest<AdaptiveByteBufAllocator> {
     @Override
