9a41e2d9a57317b78d3d7cf0ebeed9cb1b4452cd
Dmytro Dumanskiy <doom369@gmail.com>
2025-05-15 12:43:09 +0300

    remove unused import

--- common/src/main/java/io/netty/util/internal/ConcurrentSet.java
+++ common/src/main/java/io/netty/util/internal/ConcurrentSet.java
@@ -17,7 +17,6 @@ package io.netty.util.internal;
 
 import java.io.Serializable;
 import java.util.AbstractSet;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
