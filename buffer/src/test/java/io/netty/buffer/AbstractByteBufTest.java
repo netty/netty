netty/netty

commit 31c033a1d95eb4a74002c5f17ea01c967b07add9

Optimize ByteBuf.setCharSequence for adaptive allocator (#15165)
Motivation:
The `setCharSequence` method is used a fair bit when encoding HTTP
headers. I noticed the AdaptiveByteBuf was sending the call down a path
with a more indirections than strictly necessary.

Modification:
Implement `setCharSequence` in the `AdaptiveByteBuf` so we skip a level
of indirections. This is especially profitable for direct/off-heap
buffers, where we see up to a 50% performance improvement in the
benchmark. The benefit is greater the longer the string is.

Interestingly, the benchmark also show that it isn't profitable to do
this for the `getCharSequence` sibling method.

Result:
Direct buffers from the adaptive allocator now have much faster
`setCharSequence`, which will help with HTTP header encoding.

buffer/src/main/java/io/netty/buffer/AdaptivePoolingAllocator.java
@@ -39,6 +39,7 @@ import java.nio.channels.ClosedChannelException;
 import java.nio.channels.FileChannel;
 import java.nio.channels.GatheringByteChannel;
 import java.nio.channels.ScatteringByteChannel;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.Queue;
 import java.util.Set;
@@ -1367,6 +1368,12 @@ final class AdaptivePoolingAllocator {
             } catch (ClosedChannelException ignored) {
                 return -1;
             }
+        }
+
+        @Override
+        public int setCharSequence(int index, CharSequence sequence, Charset charset) {
+            checkIndex(index, sequence.length());
+            return rootParent().setCharSequence(idx(index), sequence, charset);
         }
 
         @Override

microbench/src/main/java/io/netty/buffer/AbstractByteBufGetCharSequenceBenchmark.java
@@ -27,9 +27,10 @@ import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
-@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Warmup(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
 @Measurement(iterations = 10, time = 1, timeUnit = TimeUnit.SECONDS)
 public class AbstractByteBufGetCharSequenceBenchmark extends AbstractMicrobenchmark {
+    private static final AdaptiveByteBufAllocator ADAPTIVE_ALLOC = new AdaptiveByteBufAllocator();
 
     public enum ByteBufType {
         DIRECT {
@@ -52,6 +53,30 @@ public class AbstractByteBufGetCharSequenceBenchmark extends AbstractMicrobenchm
                 return Unpooled.wrappedBuffer(bytes, 0, length);
             }
         },
+        POOLED_HEAD {
+            @Override
+            ByteBuf newBuffer(byte[] bytes, int length) {
+                return PooledByteBufAllocator.DEFAULT.heapBuffer(length).writeBytes(bytes, 0, length);
+            }
+        },
+        POOLED_DIRECT {
+            @Override
+            ByteBuf newBuffer(byte[] bytes, int length) {
+                return PooledByteBufAllocator.DEFAULT.directBuffer(length).writeBytes(bytes, 0, length);
+            }
+        },
+        ADAPTIVE_HEAD {
+            @Override
+            ByteBuf newBuffer(byte[] bytes, int length) {
+                return ADAPTIVE_ALLOC.heapBuffer(length).writeBytes(bytes, 0, length);
+            }
+        },
+        ADAPTIVE_DIRECT {
+            @Override
+            ByteBuf newBuffer(byte[] bytes, int length) {
+                return ADAPTIVE_ALLOC.directBuffer(length).writeBytes(bytes, 0, length);
+            }
+        },
         COMPOSITE {
             @Override
             ByteBuf newBuffer(byte[] bytes, int length) {
@@ -67,11 +92,18 @@ public class AbstractByteBufGetCharSequenceBenchmark extends AbstractMicrobenchm
                 }
                 return buffer;
             }
-        };
+        }
+        ;
         abstract ByteBuf newBuffer(byte[] bytes, int length);
     }
 
-    @Param({ "8", "64", "1024", "10240", "1073741824" })
+    @Param({
+            "8",
+            "64",
+            "1024",
+            "10240",
+            "1073741824"
+    })
     public int size;
 
     @Param({
@@ -84,6 +116,7 @@ public class AbstractByteBufGetCharSequenceBenchmark extends AbstractMicrobenchm
     public ByteBufType bufferType;
 
     private ByteBuf buffer;
+    private String str;
     private Charset charset;
 
     @Override
@@ -96,6 +129,7 @@ public class AbstractByteBufGetCharSequenceBenchmark extends AbstractMicrobenchm
     public void setup() {
         byte[] bytes = new byte[size + 2];
         Arrays.fill(bytes, (byte) 'a');
+        str = new String(bytes, 0, size);
 
         // Use an offset to not allow any optimizations because we use the exact passed in byte[] for heap buffers.
         buffer = bufferType.newBuffer(bytes, size);
@@ -110,6 +144,11 @@ public class AbstractByteBufGetCharSequenceBenchmark extends AbstractMicrobenchm
     @Benchmark
     public int getCharSequence() {
         return traverse(buffer.getCharSequence(buffer.readerIndex(), size, charset));
+    }
+
+    @Benchmark
+    public int setCharSequence() {
+        return buffer.setCharSequence(0, str, charset);
     }
 
     @Benchmark

commit b542b1c4be133b864462cbda9b00e040ceba529b

Adaptive: Fix possible buffer corruption caused by incorrect setCharSequence(...) implementation
Motivation:

https://github.com/netty/netty/commit/31c033a1d95eb4a74002c5f17ea01c967b07add9 introduced a regression that can lead to data corruption when calling setCharSequence(...) with a Charset that needs more bytes to encode the sequence than CharSequence.length(). The problem is that we just delegate to rootParent() without checking if the AdapativeByteBuf has enough capacity itself. If this is not the case we might still end up to write the CharSequence into the rootParent() and so override the content of another ByteBuf that also uses the same parent.

Modifications:

- Correctly implement setCharSequence and also ensure writeCharSequence is consistent
- Add unit tests

Result:

Fix regression introduced by https://github.com/netty/netty/commit/31c033a1d95eb4a74002c5f17ea01c967b07add9

buffer/src/main/java/io/netty/buffer/AdaptivePoolingAllocator.java
@@ -16,6 +16,7 @@
 package io.netty.buffer;
 
 import io.netty.util.ByteProcessor;
+import io.netty.util.CharsetUtil;
 import io.netty.util.IllegalReferenceCountException;
 import io.netty.util.NettyRuntime;
 import io.netty.util.Recycler.EnhancedHandle;
@@ -1503,8 +1504,46 @@ final class AdaptivePoolingAllocator {
 
         @Override
         public int setCharSequence(int index, CharSequence sequence, Charset charset) {
-            checkIndex(index, sequence.length());
-            return rootParent().setCharSequence(idx(index), sequence, charset);
+            return setCharSequence0(index, sequence, charset, false);
+        }
+
+        private int setCharSequence0(int index, CharSequence sequence, Charset charset, boolean expand) {
+            if (charset.equals(CharsetUtil.UTF_8)) {
+                int length = ByteBufUtil.utf8MaxBytes(sequence);
+                if (expand) {
+                    ensureWritable0(length);
+                    checkIndex0(index, length);
+                } else {
+                    checkIndex(index, length);
+                }
+                // Directly pass in the rootParent() with the adjusted index
+                return ByteBufUtil.writeUtf8(rootParent(), idx(index), length, sequence, sequence.length());
+            }
+            if (charset.equals(CharsetUtil.US_ASCII) || charset.equals(CharsetUtil.ISO_8859_1)) {
+                int length = sequence.length();
+                if (expand) {
+                    ensureWritable0(length);
+                    checkIndex0(index, length);
+                } else {
+                    checkIndex(index, length);
+                }
+                // Directly pass in the rootParent() with the adjusted index
+                return ByteBufUtil.writeAscii(rootParent(), idx(index), sequence, length);
+            }
+            byte[] bytes = sequence.toString().getBytes(charset);
+            if (expand) {
+                ensureWritable0(bytes.length);
+                // setBytes(...) will take care of checking the indices.
+            }
+            setBytes(index, bytes);
+            return bytes.length;
+        }
+
+        @Override
+        public int writeCharSequence(CharSequence sequence, Charset charset) {
+            int written = setCharSequence0(writerIndex, sequence, charset, true);
+            writerIndex += written;
+            return written;
         }
 
         @Override

buffer/src/test/java/io/netty/buffer/AbstractByteBufTest.java
@@ -40,6 +40,7 @@ import java.nio.channels.GatheringByteChannel;
 import java.nio.channels.ScatteringByteChannel;
 import java.nio.channels.WritableByteChannel;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -60,9 +61,11 @@ import static io.netty.buffer.Unpooled.directBuffer;
 import static io.netty.buffer.Unpooled.unreleasableBuffer;
 import static io.netty.buffer.Unpooled.wrappedBuffer;
 import static io.netty.util.internal.EmptyArrays.EMPTY_BYTES;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -6320,5 +6323,29 @@ public abstract class AbstractByteBufTest {
         buf.writeByte(1);
         assertEquals(max - 1, buf.maxFastWritableBytes());
         buf.release();
+    }
+
+    @Test
+    public void testSetCharSequenceWithTooLongSequence() {
+        ByteBuf buffer = buffer(4, 128);
+        CharSequence sequence = "ÖÄÜ€";
+        int maxBytes = ByteBufUtil.utf8MaxBytes(sequence);
+        assertThat(buffer.writableBytes()).isLessThan(maxBytes);
+        assertThrows(IndexOutOfBoundsException.class,
+                () -> buffer.setCharSequence(0, sequence, StandardCharsets.UTF_8));
+        buffer.release();
+    }
+
+    @Test
+    public void testWriteCharSequence() {
+        ByteBuf buffer = buffer(4, 128);
+        CharSequence sequence = "ÖÄÜ€";
+        int maxBytes = ByteBufUtil.utf8MaxBytes(sequence);
+        assertThat(buffer.writableBytes()).isLessThan(maxBytes);
+        int capacity = buffer.capacity();
+        // This should expand the buffer.
+        buffer.writeCharSequence(sequence, StandardCharsets.UTF_8);
+        assertNotSame(capacity, buffer.capacity());
+        buffer.release();
     }
 }