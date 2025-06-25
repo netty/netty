21417a80dc1635e275110e1beb1bc8ce281a0324
Chris Vest <christianvest_hansen@apple.com>
2025-06-25 10:24:27 -0700

    Move the Java EA version check to a script
    Motivation:
    Apparently the shell environment inside a workflow 'run' clause is too unpredictable, and breaks SDKMAN.
    
    Modification:
    Move the check to a separate bash script and invoke that instead.
    
    Result:
    Hopefully the check runs properly now.

---
+++ .github/scripts/check_java_ea_version.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# ----------------------------------------------------------------------------
+# Copyright 2025 The Netty Project
+#
+# The Netty Project licenses this file to you under the Apache License,
+# version 2.0 (the "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at:
+#
+#   https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+# ----------------------------------------------------------------------------
+set -e
+
+export EXPECTED_VERSION='25.ea.26'
+
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+export JAVA_VERSION=$(sdk list java | grep 25.*-open|head -n 1|cut -f 3 -d '|')
+# Keep this version in sync with docker-compose.centos-7.25.yaml
+echo "Java version: '$JAVA_VERSION'" | tee version.txt
+
+if fgrep -q $EXPECTED_VERSION version.txt ; then
+    echo "Version check failed"
+    exit 1
+else
+    echo "Version check passed"
+    exit 0
+fi
