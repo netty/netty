fd956d9f9be646ed31bb35846297ae96a86e0798
Chris Vest <christianvest_hansen@apple.com>
2025-08-18 15:04:23 -0700

    Always load BouncyCastle classes with the Netty classloader (#15533)
    Motivation:
    Between Java security settings, different classloaders, and native image
    compilation, loading the BouncyCastle classes are somewhat complicated.
    
    Modification:
    Collect all the BouncyCastle class loading and availability checking in
    a new BouncyCastleUtil class. Make use of this new class in our
    BouncyCastle implementations of ALPN, PEM reading, and self-signed
    certificate generation.
    
    Result:
    We should hopefully now be loading the classes correctly in all cases.
    
    Fixes https://github.com/netty/netty/issues/15508
    
    ---------
    
    Co-authored-by: Norman Maurer <norman_maurer@apple.com>

--- handler/src/main/java/io/netty/handler/ssl/JdkAlpnApplicationProtocolNegotiator.java
+++ handler/src/main/java/io/netty/handler/ssl/JdkAlpnApplicationProtocolNegotiator.java
@@ -16,6 +16,7 @@
 package io.netty.handler.ssl;
 
 import io.netty.buffer.ByteBufAllocator;
+import io.netty.handler.ssl.util.BouncyCastleUtil;
 
 import javax.net.ssl.SSLEngine;
 
@@ -28,7 +29,7 @@ import javax.net.ssl.SSLEngine;
 public final class JdkAlpnApplicationProtocolNegotiator extends JdkBaseApplicationProtocolNegotiator {
     private static final boolean AVAILABLE = Conscrypt.isAvailable() ||
                                              JdkAlpnSslUtils.supportsAlpn() ||
-                                             BouncyCastle.isAvailable();
+                                             BouncyCastleUtil.isBcTlsAvailable();
 
     private static final SslEngineWrapperFactory ALPN_WRAPPER = AVAILABLE ? new AlpnWrapper() : new FailureWrapper();
 
@@ -131,7 +132,7 @@ public final class JdkAlpnApplicationProtocolNegotiator extends JdkBaseApplicati
                 return isServer ? ConscryptAlpnSslEngine.newServerEngine(engine, alloc, applicationNegotiator)
                         : ConscryptAlpnSslEngine.newClientEngine(engine, alloc, applicationNegotiator);
             }
-            if (BouncyCastle.isInUse(engine)) {
+            if (BouncyCastleUtil.isBcJsseInUse(engine)) {
                 return new BouncyCastleAlpnSslEngine(engine, applicationNegotiator, isServer);
             }
             // ALPN support was recently backported to Java8 as
