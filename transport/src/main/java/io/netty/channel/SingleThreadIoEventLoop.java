b4431c59fb88ce65b5a89ef163af4d3989a0d242
Norman Maurer <norman_maurer@apple.com>
2025-09-02 09:34:46 +0200

    Move null check

--- transport/src/main/java/io/netty/channel/SingleThreadIoEventLoop.java
+++ transport/src/main/java/io/netty/channel/SingleThreadIoEventLoop.java
@@ -84,9 +84,10 @@ public class SingleThreadIoEventLoop extends SingleThreadEventLoop implements Io
      */
     public SingleThreadIoEventLoop(IoEventLoopGroup parent, ThreadFactory threadFactory,
                                    IoHandlerFactory ioHandlerFactory) {
-        super(parent, threadFactory, false, ioHandlerFactory.isChangingThreadSupported());
+        super(parent, threadFactory, false,
+                ObjectUtil.checkNotNull(ioHandlerFactory, "ioHandlerFactory").isChangingThreadSupported());
         this.maxTaskProcessingQuantumNs = DEFAULT_MAX_TASK_PROCESSING_QUANTUM_NS;
-        this.ioHandler = ObjectUtil.checkNotNull(ioHandlerFactory, "ioHandlerFactory").newHandler(this);
+        this.ioHandler = ioHandlerFactory.newHandler(this);
     }
 
     /**
@@ -98,9 +99,10 @@ public class SingleThreadIoEventLoop extends SingleThreadEventLoop implements Io
      *                          handle IO.
      */
     public SingleThreadIoEventLoop(IoEventLoopGroup parent, Executor executor, IoHandlerFactory ioHandlerFactory) {
-        super(parent, executor, false, ioHandlerFactory.isChangingThreadSupported());
+        super(parent, executor, false,
+                ObjectUtil.checkNotNull(ioHandlerFactory, "ioHandlerFactory").isChangingThreadSupported());
         this.maxTaskProcessingQuantumNs = DEFAULT_MAX_TASK_PROCESSING_QUANTUM_NS;
-        this.ioHandler = ObjectUtil.checkNotNull(ioHandlerFactory, "ioHandlerFactory").newHandler(this);
+        this.ioHandler = ioHandlerFactory.newHandler(this);
     }
 
     /**
@@ -123,13 +125,14 @@ public class SingleThreadIoEventLoop extends SingleThreadEventLoop implements Io
     public SingleThreadIoEventLoop(IoEventLoopGroup parent, ThreadFactory threadFactory,
                                    IoHandlerFactory ioHandlerFactory, int maxPendingTasks,
                                    RejectedExecutionHandler rejectedExecutionHandler, long maxTaskProcessingQuantumMs) {
-        super(parent, threadFactory, false,  ioHandlerFactory.isChangingThreadSupported(),
+        super(parent, threadFactory, false,
+                ObjectUtil.checkNotNull(ioHandlerFactory, "ioHandlerFactory").isChangingThreadSupported(),
                 maxPendingTasks, rejectedExecutionHandler);
         this.maxTaskProcessingQuantumNs =
                 ObjectUtil.checkPositiveOrZero(maxTaskProcessingQuantumMs, "maxTaskProcessingQuantumMs") == 0 ?
                         DEFAULT_MAX_TASK_PROCESSING_QUANTUM_NS :
                         TimeUnit.MILLISECONDS.toNanos(maxTaskProcessingQuantumMs);
-        this.ioHandler = ObjectUtil.checkNotNull(ioHandlerFactory, "ioHandlerFactory").newHandler(this);
+        this.ioHandler = ioHandlerFactory.newHandler(this);
     }
 
     /**
@@ -151,13 +154,14 @@ public class SingleThreadIoEventLoop extends SingleThreadEventLoop implements Io
                                    IoHandlerFactory ioHandlerFactory, int maxPendingTasks,
                                    RejectedExecutionHandler rejectedExecutionHandler,
                                    long maxTaskProcessingQuantumMs) {
-        super(parent, executor, false, ioHandlerFactory.isChangingThreadSupported(),
+        super(parent, executor, false,
+                ObjectUtil.checkNotNull(ioHandlerFactory, "ioHandlerFactory").isChangingThreadSupported(),
                 maxPendingTasks, rejectedExecutionHandler);
         this.maxTaskProcessingQuantumNs =
                 ObjectUtil.checkPositiveOrZero(maxTaskProcessingQuantumMs, "maxTaskProcessingQuantumMs") == 0 ?
                         DEFAULT_MAX_TASK_PROCESSING_QUANTUM_NS :
                         TimeUnit.MILLISECONDS.toNanos(maxTaskProcessingQuantumMs);
-        this.ioHandler = ObjectUtil.checkNotNull(ioHandlerFactory, "ioHandlerFactory").newHandler(this);
+        this.ioHandler = ioHandlerFactory.newHandler(this);
     }
 
     /**
@@ -177,10 +181,11 @@ public class SingleThreadIoEventLoop extends SingleThreadEventLoop implements Io
                                       IoHandlerFactory ioHandlerFactory, Queue<Runnable> taskQueue,
                                       Queue<Runnable> tailTaskQueue,
                                       RejectedExecutionHandler rejectedExecutionHandler) {
-        super(parent, executor, false,  ioHandlerFactory.isChangingThreadSupported(),
+        super(parent, executor, false,
+                ObjectUtil.checkNotNull(ioHandlerFactory, "ioHandlerFactory").isChangingThreadSupported(),
                 taskQueue, tailTaskQueue, rejectedExecutionHandler);
         this.maxTaskProcessingQuantumNs = DEFAULT_MAX_TASK_PROCESSING_QUANTUM_NS;
-        this.ioHandler = ObjectUtil.checkNotNull(ioHandlerFactory, "ioHandlerFactory").newHandler(this);
+        this.ioHandler = ioHandlerFactory.newHandler(this);
     }
 
     @Override
