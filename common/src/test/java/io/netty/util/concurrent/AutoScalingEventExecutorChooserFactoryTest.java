a82425c828958b0789d8021baf9c663d85e0cf5e
George Banasios <banasiosgeorge@gmail.com>
2025-09-17 08:45:50 +0300

    ci 1/2

--- common/src/test/java/io/netty/util/concurrent/AutoScalingEventExecutorChooserFactoryTest.java
+++ common/src/test/java/io/netty/util/concurrent/AutoScalingEventExecutorChooserFactoryTest.java
@@ -123,7 +123,7 @@ public class AutoScalingEventExecutorChooserFactoryTest {
         }
     }
 
-    @RepeatedTest(9)
+    @RepeatedTest(10)
     @Timeout(30)
     void testScaleUp() throws Exception {
         TestEventExecutorGroup group = new TestEventExecutorGroup(1, 3, 50, TimeUnit.MILLISECONDS);
