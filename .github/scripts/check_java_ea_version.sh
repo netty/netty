ba2c981a2f9cd75aaf35bf36a39a39aff05a36e0
Norman Maurer <norman_maurer@apple.com>
2025-09-18 07:43:32 +0200

    Fix script

--- .github/scripts/check_java_ea_version.sh
+++ .github/scripts/check_java_ea_version.sh
@@ -21,7 +21,7 @@ export EXPECTED_VERSION='26.ea.15'
 
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-export JAVA_VERSION=$(sdk list java | grep 25.*-open|head -n 1|cut -f 3 -d '|')
+export JAVA_VERSION=$(sdk list java | grep 26.*-open|head -n 1|cut -f 3 -d '|')
 echo "Java version: '$JAVA_VERSION'" | tee version.txt
 
 if fgrep -q $EXPECTED_VERSION version.txt ; then
