bbc30a1ac5b3a8aa191fb8033f26649262cb81ef
Norman Maurer <norman_maurer@apple.com>
2025-09-18 13:42:18 +0200

    Use JDK25 and add JDK26 EA build (#15674)
    Motivation:
    
    JDK25 was released so we should use the final release to build on the
    CI. Beside this we also should start to try build with JDK26 so we know
    everything works as expected
    
    Modifications:
    
    - Update JDK25 version
    - Add docker-compose config fur latest JDK26 EA build
    - Update scripts to check for correct EA version
    - Add profiles for JDK26
    
    Result:
    
    Build with latest JDK25 and JDK26EA release on the ci

--- .github/scripts/check_java_ea_version.sh
+++ .github/scripts/check_java_ea_version.sh
@@ -16,12 +16,12 @@
 # ----------------------------------------------------------------------------
 set -e
 
-# Keep this version in sync with docker-compose.centos-7.25.yaml
-export EXPECTED_VERSION='25.ea.36'
+# Keep this version in sync with docker-compose.centos-7.26.yaml
+export EXPECTED_VERSION='26.ea.15'
 
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
-export JAVA_VERSION=$(sdk list java | grep 25.*-open|head -n 1|cut -f 3 -d '|')
+export JAVA_VERSION=$(sdk list java | grep 26.*-open|head -n 1|cut -f 3 -d '|')
 echo "Java version: '$JAVA_VERSION'" | tee version.txt
 
 if fgrep -q $EXPECTED_VERSION version.txt ; then
