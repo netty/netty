4d542be9186cfd3a75bd3e19ef06037b32009721
yawkat <jonas.konrad@oracle.com>
2025-10-09 11:37:02 +0200

    New HttpDecompressionHandler implementation

--- codec-http/src/main/java/io/netty/handler/codec/http/HttpDecompressionHandler.java
+++ codec-http/src/main/java/io/netty/handler/codec/http/HttpDecompressionHandler.java
@@ -16,15 +16,20 @@
 package io.netty.handler.codec.http;
 
 import io.netty.channel.ChannelDuplexHandler;
-import io.netty.handler.codec.compression.BackpressureDecompressionHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.CodecException;
+import io.netty.handler.codec.DecoderResult;
 import io.netty.handler.codec.compression.Brotli;
 import io.netty.handler.codec.compression.BrotliDecompressor;
+import io.netty.handler.codec.compression.DecompressionException;
 import io.netty.handler.codec.compression.Decompressor;
 import io.netty.handler.codec.compression.SnappyFrameDecompressor;
 import io.netty.handler.codec.compression.ZlibCodecFactory;
 import io.netty.handler.codec.compression.ZlibWrapper;
 import io.netty.handler.codec.compression.Zstd;
 import io.netty.handler.codec.compression.ZstdDecompressor;
+import io.netty.util.internal.RecyclableArrayList;
 
 import static io.netty.handler.codec.http.HttpHeaderValues.BR;
 import static io.netty.handler.codec.http.HttpHeaderValues.DEFLATE;
@@ -36,10 +41,43 @@ import static io.netty.handler.codec.http.HttpHeaderValues.ZSTD;
 
 /**
  * Decompresses an {@link HttpMessage} and an {@link HttpContent} compressed in
- * {@code gzip} or {@code deflate} encoding.  For more information on how this
- * handler modifies the message, please refer to {@link BackpressureHttpContentDecoder}.
+ * {@code gzip} or {@code deflate} encoding.
+ * <p>
+ * This class acts as a replacement for {@link HttpContentDecompressor}, with two advantages: It does not use an
+ * {@link EmbeddedChannel}, improving performance, and it correctly handles backpressure, so that decompressors will
+ * not produce unrestricted amounts of output data before downstream handlers signal that they are ready to receive
+ * this data.
  */
-public class HttpDecompressionHandler extends BackpressureHttpContentDecoder {
+public class HttpDecompressionHandler extends ChannelDuplexHandler {
+    /**
+     * Whether the current input, as seen by {@link #channelRead}, is part of a compressed message, i.e. hasn't been
+     * finished by a {@link LastHttpContent} yet. Note that this is not exactly equal to the decompressed state.
+     * {@code messageCompressed} can be {@code true} even when decompression is done, when the decompressed format
+     * contained a delimiter that signifies an end of input, but we've not received the {@link LastHttpContent} yet.
+     * Similarly, this flag can be {@code false} when end of input has been reached but there is still some compressed
+     * data in the output buffer.
+     */
+    private boolean messageCompressed;
+    /**
+     * The decompressor for the current message.
+     */
+    private Decompressor decompressor;
+
+    private LastHttpContent lastHttpContent;
+
+    private RecyclableArrayList heldBack;
+
+    private boolean reading;
+    private final int messagesPerRead;
+
+    private long downstreamMessageCount;
+    private long downstreamMessageTarget;
+    private long readStartMessageCount;
+
+    public HttpDecompressionHandler(int messagesPerRead) { // TODO
+        this.messagesPerRead = messagesPerRead;
+    }
+
     protected Decompressor.AbstractDecompressorBuilder newDecompressorBuilder(String contentEncoding) {
         if (GZIP.contentEqualsIgnoreCase(contentEncoding) ||
                 X_GZIP.contentEqualsIgnoreCase(contentEncoding)) {
@@ -67,13 +105,267 @@ public class HttpDecompressionHandler extends BackpressureHttpContentDecoder {
         return null;
     }
 
+    /**
+     * Returns the expected content encoding of the decoded content.
+     * This getMethod returns {@code "identity"} by default, which is the case for
+     * most decoders.
+     *
+     * @param contentEncoding the value of the {@code "Content-Encoding"} header
+     * @return the expected content encoding of the new content
+     */
+    protected String getTargetContentEncoding(
+            @SuppressWarnings("UnusedParameters") String contentEncoding) throws Exception {
+        return HttpContentDecoder.IDENTITY;
+    }
+
     @Override
-    protected final ChannelDuplexHandler newContentDecoder(String contentEncoding) throws Exception {
-        Decompressor.AbstractDecompressorBuilder builder = newDecompressorBuilder(contentEncoding);
-        if (builder != null) {
-            return BackpressureDecompressionHandler.create(builder);
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (!reading) {
+            reading = true;
+            readStartMessageCount = downstreamMessageCount;
+        }
+
+        if (heldBack != null) {
+            heldBack.add(msg);
+            return;
+        }
+
+        if (!messageCompressed &&
+                msg instanceof HttpMessage &&
+                !(msg instanceof HttpResponse && ((HttpResponse) msg).status().code() == 100)) {
+            final HttpMessage message = (HttpMessage) msg;
+            final HttpHeaders headers = message.headers();
+
+            // Determine the content encoding.
+            String contentEncoding = headers.get(HttpHeaderNames.CONTENT_ENCODING);
+            if (contentEncoding != null) {
+                contentEncoding = contentEncoding.trim();
+            } else {
+                String transferEncoding = headers.get(HttpHeaderNames.TRANSFER_ENCODING);
+                if (transferEncoding != null) {
+                    int idx = transferEncoding.indexOf(',');
+                    if (idx != -1) {
+                        contentEncoding = transferEncoding.substring(0, idx).trim();
+                    } else {
+                        contentEncoding = transferEncoding.trim();
+                    }
+                } else {
+                    contentEncoding = HttpContentDecoder.IDENTITY;
+                }
+            }
+
+            Decompressor.AbstractDecompressorBuilder decompressorBuilder = newDecompressorBuilder(contentEncoding);
+            if (decompressorBuilder != null) {
+                messageCompressed = true;
+                decompressor = decompressorBuilder.build(ctx.alloc());
+                downstreamMessageTarget = downstreamMessageCount + messagesPerRead;
+                lastHttpContent = null;
+                downstreamMessageCount++; // the HttpMessage
+
+                // Remove content-length header:
+                // the correct value can be set only after all chunks are processed/decoded.
+                // If buffering is not an issue, add HttpObjectAggregator down the chain, it will set the header.
+                // Otherwise, rely on LastHttpContent message.
+                if (headers.contains(HttpHeaderNames.CONTENT_LENGTH)) {
+                    headers.remove(HttpHeaderNames.CONTENT_LENGTH);
+                    headers.set(HttpHeaderNames.TRANSFER_ENCODING, HttpHeaderValues.CHUNKED);
+                }
+
+                // set new content encoding,
+                CharSequence targetContentEncoding = getTargetContentEncoding(contentEncoding);
+                if (HttpHeaderValues.IDENTITY.contentEquals(targetContentEncoding)) {
+                    // Do NOT set the 'Content-Encoding' header if the target encoding is 'identity'
+                    // as per: https://tools.ietf.org/html/rfc2616#section-14.11
+                    headers.remove(HttpHeaderNames.CONTENT_ENCODING);
+                } else {
+                    headers.set(HttpHeaderNames.CONTENT_ENCODING, targetContentEncoding);
+                }
+
+                if (message instanceof HttpContent) {
+                    ctx.fireChannelRead(stripContent(message));
+                } else {
+                    ctx.fireChannelRead(message);
+                    return;
+                }
+            }
+        }
+
+        if (!messageCompressed) {
+            ctx.fireChannelRead(msg);
+            downstreamMessageCount++;
+            return;
+        }
+
+        HttpContent content = (HttpContent) msg;
+
+        if (decompressor == null) {
+            if (content.content().isReadable()) {
+                content.release();
+                throw new DecompressionException("Additional input after compressed data");
+            }
+            content.release();
         } else {
-            return null;
+            assert decompressor.status() == Decompressor.Status.NEED_INPUT : "heldBack should be set";
+            if (content.content().isReadable()) {
+                decompressor.addInput(content.content());
+                forwardOutput(ctx);
+            } else {
+                content.release();
+            }
+        }
+
+        if (content instanceof LastHttpContent) {
+            if (decompressor == null) {
+                // done
+                messageCompressed = false;
+                ctx.fireChannelRead(msg);
+            } else if (decompressor.status() == Decompressor.Status.NEED_INPUT) {
+                decompressor.endOfInput();
+                messageCompressed = false;
+                lastHttpContent = stripData((LastHttpContent) content);
+                forwardOutput(ctx);
+            } else {
+                assert heldBack != null : "should have been set by forwardOutput";
+                heldBack.add(stripData((LastHttpContent) content));
+            }
+        }
+    }
+
+    private static HttpMessage stripContent(HttpMessage message) {
+        HttpMessage copy;
+        if (message instanceof HttpRequest) {
+            HttpRequest r = (HttpRequest) message; // HttpRequest or FullHttpRequest
+            copy = new DefaultHttpRequest(r.protocolVersion(), r.method(), r.uri());
+        } else if (message instanceof HttpResponse) {
+            HttpResponse r = (HttpResponse) message; // HttpResponse or FullHttpResponse
+            copy = new DefaultHttpResponse(r.protocolVersion(), r.status());
+        } else {
+            throw new CodecException("Object of class " + message.getClass().getName() +
+                    " is not an HttpRequest or HttpResponse");
+        }
+        copy.headers().set(message.headers());
+        copy.setDecoderResult(message.decoderResult());
+        return copy;
+    }
+
+    private static LastHttpContent stripData(LastHttpContent content) {
+        HttpHeaders trailingHeaders = content.trailingHeaders();
+        if (trailingHeaders == null || trailingHeaders.isEmpty()) {
+            return LastHttpContent.EMPTY_LAST_CONTENT;
+        } else {
+            return new ComposedLastHttpContent(trailingHeaders, DecoderResult.SUCCESS);
+        }
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        do {
+            if (readStartMessageCount == downstreamMessageCount) {
+                // we didn't forward any messages, so we need to ask upstream for more
+                reading = false;
+                if (!ctx.channel().config().isAutoRead()) {
+                    ctx.read();
+                }
+                return;
+            }
+
+            // accept that we might not have hit the target.
+            downstreamMessageTarget = downstreamMessageCount;
+
+            ctx.fireChannelReadComplete();
+            reading = false;
+
+        } while (fulfillDemandOutsideRead(ctx));
+    }
+
+    /**
+     * @return {@code true} if {@link #channelReadComplete(ChannelHandlerContext)} should be called next. This is to
+     * avoid recursion
+     */
+    private boolean fulfillDemandOutsideRead(ChannelHandlerContext ctx) throws Exception {
+        assert !reading;
+
+        if (decompressor == null) {
+            return false;
+        }
+        if (downstreamMessageTarget <= downstreamMessageCount && !ctx.channel().config().isAutoRead()) {
+            return false;
+        }
+
+        RecyclableArrayList heldBack = this.heldBack;
+        if (heldBack == null) {
+            return false;
+        }
+
+        reading = true;
+        readStartMessageCount = downstreamMessageCount;
+        forwardOutput(ctx);
+        if (decompressor == null || decompressor.status() == Decompressor.Status.NEED_INPUT) {
+            this.heldBack = null;
+            if (heldBack.isEmpty() && readStartMessageCount == downstreamMessageCount) {
+                heldBack.recycle();
+                return false;
+            } else {
+                // this sets reading = true
+                for (Object msg : heldBack) {
+                    channelRead(ctx, msg);
+                }
+                heldBack.recycle();
+                return true; // channelReadComplete(ctx)
+            }
+        } else {
+            if (readStartMessageCount != downstreamMessageCount) {
+                return true; // channelReadComplete(ctx)
+            }
+            ctx.read();
+            return false;
+        }
+    }
+
+    @Override
+    public void read(ChannelHandlerContext ctx) throws Exception {
+        if (decompressor == null && !messageCompressed) {
+            ctx.read();
+            return;
+        }
+
+        downstreamMessageTarget = downstreamMessageCount + messagesPerRead;
+        if (!reading) {
+            if (fulfillDemandOutsideRead(ctx)) {
+                channelReadComplete(ctx);
+            }
+        }
+    }
+
+    private void forwardOutput(ChannelHandlerContext ctx) {
+        loop:
+        while (true) {
+            Decompressor.Status status = decompressor.status();
+            switch (status) {
+                case NEED_OUTPUT:
+                    if (downstreamMessageTarget <= downstreamMessageCount && !ctx.channel().config().isAutoRead()) {
+                        if (heldBack == null) {
+                            heldBack = RecyclableArrayList.newInstance();
+                        }
+                        break loop;
+                    }
+                    downstreamMessageCount++;
+                    ctx.fireChannelRead(new DefaultHttpContent(decompressor.takeOutput()));
+                    break;
+                case NEED_INPUT:
+                    break loop;
+                case COMPLETE:
+                    decompressor.close();
+                    decompressor = null;
+                    downstreamMessageCount++;
+                    if (lastHttpContent != null) {
+                        ctx.fireChannelRead(lastHttpContent);
+                        lastHttpContent = null;
+                    }
+                    break loop;
+                default:
+                    throw new AssertionError("Unknown status: " + status);
+            }
         }
     }
 }
--- codec-http/src/test/java/io/netty/handler/codec/http/HttpDecompressionHandlerTest.java
+++ codec-http/src/test/java/io/netty/handler/codec/http/HttpDecompressionHandlerTest.java
@@ -15,11 +15,257 @@
  */
 package io.netty.handler.codec.http;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
 import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.compression.DecompressionException;
+import io.netty.handler.codec.compression.Decompressor;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.netty.handler.codec.compression.Decompressor.Status.COMPLETE;
+import static io.netty.handler.codec.compression.Decompressor.Status.NEED_INPUT;
+import static io.netty.handler.codec.compression.Decompressor.Status.NEED_OUTPUT;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class HttpDecompressionHandlerTest extends HttpContentDecompressorTest {
+    private static final String READ_COMPLETE = "readComplete";
+    private static final String LAST = "last";
+    private static final DefaultHttpRequest REQUEST = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, "/", new DefaultHttpHeaders()
+            .add(HttpHeaderNames.CONTENT_TRANSFER_ENCODING, "mock")
+            .add(HttpHeaderNames.TRANSFER_ENCODING, HttpHeaderValues.CHUNKED));
+
     @Override
     protected ChannelHandler createDecompressor() {
-        return new HttpDecompressionHandler();
+        return new HttpDecompressionHandler(64);
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void maxMessages(boolean autoRead) {
+        EmbeddedChannel channel = new EmbeddedChannel(
+                new MockDecompressor.Builder()
+                        .needInput()
+                        .needOutput(4)
+                        .complete()
+                        .makeHandler(2),
+                new HttpContentNumberDecoder()
+        );
+        channel.config().setAutoRead(autoRead);
+
+        channel.writeInbound(REQUEST, new DefaultHttpContent(numberedBuffer(0)));
+
+        HttpRequest request = channel.readInbound();
+        assertEquals("/", request.uri());
+        assertEquals(1, channel.<Integer>readInbound());
+
+        if (!autoRead) {
+            assertEquals(READ_COMPLETE, channel.readInbound());
+
+            assertNull(channel.readInbound());
+            channel.read();
+        }
+
+        assertEquals(2, channel.<Integer>readInbound());
+        assertEquals(3, channel.<Integer>readInbound());
+
+        if (!autoRead) {
+            assertEquals(READ_COMPLETE, channel.readInbound());
+
+            assertNull(channel.readInbound());
+            channel.read();
+        }
+
+        assertEquals(4, channel.<Integer>readInbound());
+        assertEquals(READ_COMPLETE, channel.readInbound());
+        assertNull(channel.readInbound());
+
+        channel.finish();
+    }
+
+    private static ByteBuf numberedBuffer(int index) {
+        ByteBuf buf = ByteBufAllocator.DEFAULT.buffer(4);
+        buf.writeInt(index);
+        return buf;
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void lastHttpContent(boolean autoRead) {
+        EmbeddedChannel channel = new EmbeddedChannel(
+                new MockDecompressor.Builder()
+                        .needInput()
+                        .needOutput(2)
+                        .needInput()
+                        .needOutput(1)
+                        .complete()
+                        .makeHandler(2),
+                new HttpContentNumberDecoder()
+        );
+        channel.config().setAutoRead(autoRead);
+
+        channel.writeInbound(REQUEST, new DefaultLastHttpContent(numberedBuffer(0)));
+
+        HttpRequest request = channel.readInbound();
+        assertEquals("/", request.uri());
+        assertEquals(1, channel.<Integer>readInbound());
+
+        if (!autoRead) {
+            assertEquals(READ_COMPLETE, channel.readInbound());
+
+            assertNull(channel.readInbound());
+            channel.read();
+        }
+
+        assertEquals(2, channel.<Integer>readInbound());
+        assertEquals(4, channel.<Integer>readInbound());
+
+        assertEquals(LAST, channel.readInbound());
+        assertEquals(READ_COMPLETE, channel.readInbound());
+        assertNull(channel.readInbound());
+
+        channel.finish();
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    public void downstreamReadsInReadComplete(boolean autoRead) {
+        EmbeddedChannel channel = new EmbeddedChannel(
+                new MockDecompressor.Builder()
+                        .needInput()
+                        .needOutput(2)
+                        .needInput()
+                        .needOutput(1)
+                        .complete()
+                        .makeHandler(2),
+                new HttpContentNumberDecoder(),
+                new ChannelInboundHandlerAdapter() {
+                    @Override
+                    public void channelReadComplete(ChannelHandlerContext ctx) {
+                        ctx.read();
+                    }
+                }
+        );
+        channel.config().setAutoRead(autoRead);
+
+        channel.writeInbound(REQUEST, new DefaultLastHttpContent(numberedBuffer(0)));
+
+        HttpRequest request = channel.readInbound();
+        assertEquals("/", request.uri());
+        assertEquals(1, channel.<Integer>readInbound());
+        if (!autoRead) {
+            assertEquals(READ_COMPLETE, channel.readInbound());
+        }
+        assertEquals(2, channel.<Integer>readInbound());
+        assertEquals(4, channel.<Integer>readInbound());
+        assertEquals(LAST, channel.readInbound());
+        assertEquals(READ_COMPLETE, channel.readInbound());
+        assertNull(channel.readInbound());
+
+        channel.finish();
+    }
+
+    private static final class MockDecompressor implements Decompressor {
+        private final List<Status> events;
+        private int index;
+
+        MockDecompressor(List<Status> events) {
+            this.events = events;
+        }
+
+        @Override
+        public Status status() throws DecompressionException {
+            return events.get(index);
+        }
+
+        @Override
+        public void addInput(ByteBuf buf) throws DecompressionException {
+            assertEquals(NEED_INPUT, status());
+            assertEquals(index++, buf.readInt());
+            buf.release();
+        }
+
+        @Override
+        public void endOfInput() throws DecompressionException {
+            assertEquals(NEED_INPUT, status());
+            index++;
+        }
+
+        @Override
+        public ByteBuf takeOutput() throws DecompressionException {
+            assertEquals(NEED_OUTPUT, status());
+            return numberedBuffer(index++);
+        }
+
+        @Override
+        public void close() throws DecompressionException {
+            assertEquals(COMPLETE, status());
+        }
+
+        static final class Builder extends AbstractDecompressorBuilder {
+            private final List<Status> events = new ArrayList<>();
+
+            MockDecompressor.Builder needInput() {
+                events.add(NEED_INPUT);
+                return this;
+            }
+
+            MockDecompressor.Builder needOutput(int count) {
+                for (int i = 0; i < count; i++) {
+                    events.add(NEED_OUTPUT);
+                }
+                return this;
+            }
+
+            MockDecompressor.Builder complete() {
+                events.add(COMPLETE);
+                return this;
+            }
+
+            @Override
+            public Decompressor build(ByteBufAllocator allocator) throws DecompressionException {
+                return new MockDecompressor(events);
+            }
+
+            ChannelHandler makeHandler(int messagesPerRead) {
+                return new HttpDecompressionHandler(messagesPerRead) {
+                    @Override
+                    protected AbstractDecompressorBuilder newDecompressorBuilder(String contentEncoding) {
+                        return Builder.this;
+                    }
+                };
+            }
+        }
+    }
+
+    private static class HttpContentNumberDecoder extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            if (msg instanceof HttpContent) {
+                ByteBuf buf = ((HttpContent) msg).content();
+                if (buf.isReadable()) {
+                    ctx.fireChannelRead(buf.readInt());
+                }
+                buf.release();
+                if (msg instanceof LastHttpContent) {
+                    ctx.fireChannelRead(LAST);
+                }
+            } else {
+                ctx.fireChannelRead(msg);
+            }
+        }
+
+        @Override
+        public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+            ctx.fireChannelRead(READ_COMPLETE);
+            ctx.fireChannelReadComplete();
+        }
     }
 }
