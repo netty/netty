be11a616bb4f674f0e52500eaca25ccd587c0509
Norman Maurer <norman_maurer@apple.com>
2025-10-17 12:48:40 +0200

    Update to latest JDK26 EA release (#15759)
    Motivation:
    
    There was a new JDK26 EA release
    
    Modifications:
    
    Update to latest
    
    Result:
    
    Build with latest JDK26 EA release

--- .github/scripts/check_java_ea_version.sh
+++ .github/scripts/check_java_ea_version.sh
@@ -17,7 +17,7 @@
 set -e
 
 # Keep this version in sync with docker-compose.centos-7.26.yaml
-export EXPECTED_VERSION='26.ea.18'
+export EXPECTED_VERSION='26.ea.19'
 
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
