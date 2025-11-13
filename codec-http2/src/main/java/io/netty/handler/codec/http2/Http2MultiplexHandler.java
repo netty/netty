eb2a106e862e6fe2b53dc765a37db3f05745d45c
Chris Vest <christianvest_hansen@apple.com>
2025-11-13 13:19:03 -0800

    Revert "Use real, instead of synthetic, HTTP/2 stream id"
    This reverts commit 1ebed73e1b0ede498299d7fce9123807aa8442b1.

--- codec-http2/src/main/java/io/netty/handler/codec/http2/Http2MultiplexHandler.java
+++ codec-http2/src/main/java/io/netty/handler/codec/http2/Http2MultiplexHandler.java
@@ -114,6 +114,7 @@ public final class Http2MultiplexHandler extends Http2ChannelDuplexHandler {
                     Http2CodecUtil.SMALLEST_MAX_CONCURRENT_STREAMS);
 
     private boolean parentReadInProgress;
+    private int idCount;
 
     // Need to be volatile as accessed from within the Http2MultiplexHandlerStreamChannel in a multi-threaded fashion.
     private volatile ChannelHandlerContext ctx;
@@ -387,7 +388,7 @@ public final class Http2MultiplexHandler extends Http2ChannelDuplexHandler {
     private final class Http2MultiplexHandlerStreamChannel extends AbstractHttp2StreamChannel {
 
         Http2MultiplexHandlerStreamChannel(DefaultHttp2FrameStream stream, ChannelHandler inboundHandler) {
-            super(stream, inboundHandler);
+            super(stream, ++idCount, inboundHandler);
         }
 
         @Override
