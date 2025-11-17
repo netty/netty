bef5e8c4cf616aaeb207698411ce4b0217f3795f
Norman Maurer <norman_maurer@apple.com>
2025-11-17 10:03:01 +0100

    Apply suggestion from @normanmaurer

--- codec-compression/src/test/java/io/netty/handler/codec/compression/ZstdEncoderTest.java
+++ codec-compression/src/test/java/io/netty/handler/codec/compression/ZstdEncoderTest.java
@@ -88,7 +88,7 @@ public class ZstdEncoderTest extends AbstractEncoderTest {
         ByteBuf out = channel.readOutbound();
         assertThat(out.readableBytes()).isPositive();
         out.release();
-        asssertNull(channel.readOutbound());
+        assertNull(channel.readOutbound());
     }
 
     @Override
