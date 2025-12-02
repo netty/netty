e1f2b0d178c6468293bb159ebc8cc90c9532bbc4
CLFutureX <775523362@qq.com>
2025-12-02 11:17:04 +0800

    format

--- testsuite/src/main/java/io/netty/testsuite/transport/AbstractSingleThreadEventLoopTest.java
+++ testsuite/src/main/java/io/netty/testsuite/transport/AbstractSingleThreadEventLoopTest.java
@@ -67,10 +67,11 @@ public abstract class AbstractSingleThreadEventLoopTest {
     protected static final int SINGLE_REFRESH_THRESHOLD_COUNT = 1 << 10;
 
     protected static final EventExecutorChooserFactory AUTO_SCALING_CHOOSER_FACTORY =
-            new AutoScalingEventExecutorChooserFactory(SCALING_MIN_THREADS, SCALING_MAX_THREADS, SCALING_WINDOW_SECONDS,
-                                                       SCALING_WINDOW_UNIT, SCALEDOWN_THRESHOLD, SCALEUP_THRESHOLD,
-                                                       SCALING_MAX_THREADS, SCALING_MAX_THREADS, SCALING_PATIENCE_CYCLES,
-                                                       SINGLE_REFRESH_THRESHOLD_COUNT
+            new AutoScalingEventExecutorChooserFactory(
+                    SCALING_MIN_THREADS, SCALING_MAX_THREADS, SCALING_WINDOW_SECONDS,
+                    SCALING_WINDOW_UNIT, SCALEDOWN_THRESHOLD, SCALEUP_THRESHOLD,
+                    SCALING_MAX_THREADS, SCALING_MAX_THREADS, SCALING_PATIENCE_CYCLES,
+                    SINGLE_REFRESH_THRESHOLD_COUNT
     );
 
     @Test
