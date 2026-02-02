1677d937ccdfae43e7059e0dc7085c9cdbd6794d
Chris Vest <christianvest_hansen@apple.com>
2026-02-02 10:17:41 -0800

    Make netty-common a real module
    Motivation:
    Netty 5 will require Java 25 at a minimum, so we can declare real java platform modules instead of relying on the module-info maven plugin.
    
    Modification:
    - Remove the module-info plugin from the netty-common pom file.
    - Replace the `module-info.yml` file with a `module-info.java` file.
    
    Result:
    We now use real java platform modules in netty-common, and if this works well, we can do the same in our other subprojects.

---
+++ common/src/main/java/module-info.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2026 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License, version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at:
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+module io.netty.common {
+    requires jdk.unsupported;
+    requires java.logging;
+    requires static org.jctools.core;
+    requires static jdk.jfr;
+    requires static org.apache.commons.logging;
+    requires static org.apache.log4j;
+    requires static org.apache.logging.log4j;
+    requires static org.jetbrains.annotations;
+    requires static org.slf4j;
+    requires static org.graalvm.nativeimage;
+    requires static reactor.blockhound;
+
+    exports io.netty.util;
+    exports io.netty.util.collection;
+    exports io.netty.util.concurrent;
+}
