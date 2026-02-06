41c31f5b718abbcb94fdbc1286223681c1d797aa
Norman Maurer <norman_maurer@apple.com>
2026-02-06 11:10:55 +0100

    HTTP3: Allow to support non-standard HTTP3 settings (#16171)
    Motivation:
    
    To be able to add extensions that are still in draft we need a way to
    also allow non-standard HTTP3 settings. This was not possible before.
    
    Modifications:
    
    - Allow to plugin a NonStandardHttp3SettingsValidator to support
    non-standard settings
    - Add unit test
    
    Result:
    
    Be able to support non-standard HTTP3 settings and so allow to add
    support for drafts

--- codec-http3/src/main/java/io/netty/handler/codec/http3/Http3ConnectionHandler.java
+++ codec-http3/src/main/java/io/netty/handler/codec/http3/Http3ConnectionHandler.java
@@ -42,6 +42,7 @@ public abstract class Http3ConnectionHandler extends ChannelInboundHandlerAdapte
     final Http3ControlStreamOutboundHandler remoteControlStreamHandler;
     final QpackDecoder qpackDecoder;
     final QpackEncoder qpackEncoder;
+    final Http3Settings.NonStandardHttp3SettingsValidator nonStandardSettingsValidator;
     private boolean controlStreamCreationInProgress;
 
     final long maxTableCapacity;
@@ -61,9 +62,15 @@ public abstract class Http3ConnectionHandler extends ChannelInboundHandlerAdapte
      */
     Http3ConnectionHandler(boolean server, @Nullable ChannelHandler inboundControlStreamHandler,
                            @Nullable LongFunction<ChannelHandler> unknownInboundStreamHandlerFactory,
-                           @Nullable Http3SettingsFrame localSettings, boolean disableQpackDynamicTable) {
+                           @Nullable Http3SettingsFrame localSettings, boolean disableQpackDynamicTable,
+                           @Nullable Http3Settings.NonStandardHttp3SettingsValidator nonStandardSettingsValidator) {
         this.unknownInboundStreamHandlerFactory = unknownInboundStreamHandlerFactory;
         this.disableQpackDynamicTable = disableQpackDynamicTable;
+        if (nonStandardSettingsValidator != null) {
+            this.nonStandardSettingsValidator = nonStandardSettingsValidator;
+        } else {
+            this.nonStandardSettingsValidator = (id, value) -> false;
+        }
         if (localSettings == null) {
             localSettings = new DefaultHttp3SettingsFrame();
         } else {
@@ -81,7 +88,8 @@ public abstract class Http3ConnectionHandler extends ChannelInboundHandlerAdapte
         qpackEncoder = new QpackEncoder();
         codecFactory = Http3FrameCodec.newFactory(qpackDecoder, maxFieldSectionSize, qpackEncoder);
         remoteControlStreamHandler =  new Http3ControlStreamOutboundHandler(server, localSettings,
-                codecFactory.newCodec(Http3FrameTypeValidator.NO_VALIDATION, NO_STATE, NO_STATE));
+                codecFactory.newCodec(Http3FrameTypeValidator.NO_VALIDATION, NO_STATE, NO_STATE,
+                        this.nonStandardSettingsValidator));
         localControlStreamHandler = new Http3ControlStreamInboundHandler(server, inboundControlStreamHandler,
                 qpackEncoder, remoteControlStreamHandler);
     }
@@ -121,7 +129,8 @@ public abstract class Http3ConnectionHandler extends ChannelInboundHandlerAdapte
      */
     final ChannelHandler newCodec(Http3RequestStreamCodecState encodeState,
                                   Http3RequestStreamCodecState decodeState) {
-        return codecFactory.newCodec(Http3RequestStreamFrameTypeValidator.INSTANCE, encodeState, decodeState);
+        return codecFactory.newCodec(
+                Http3RequestStreamFrameTypeValidator.INSTANCE, encodeState, decodeState, nonStandardSettingsValidator);
     }
 
     final ChannelHandler newRequestStreamValidationHandler(
