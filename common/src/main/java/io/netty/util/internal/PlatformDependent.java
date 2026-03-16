netty/netty

commit c79ce23a7b4e1be6985ad5e9127f0244f1052621

Make unpooled buffers avoid shared arenas (#16443)
Motivation:
Shared arenas can be expensive to close if we do it a lot, for instance
in cases when we use the UnpooledByteBufAllocator.

Modification:
Make Unpooled buffers avoid shared arenas and instead fall back to
GC/reference processing buffer cleaning. Make the pooling allocators
explicitly state that they permit "expensive" clean operations to that
they use shared arenas.

Result:
Greatly improves system-wide performance and scalability when allocating
a lot of unpooled buffers, at the cost of delayed cleaning/deallocation
and in turn higher memory usage.

This should also address a number of flaky test failures we see, that
are caused by timeouts due to increased time we were spending in closing
shared arenas.

If the use GC and reference counting causes adverse performance effects,
e.g. performance regressions or poor GC behavior, then the previous
behavior (letting unpooled buffers use shared memory segment arenas) can
be restored by setting the `io.netty.ignoreExpensiveClean=true` system
property.

common/src/main/java/io/netty/util/internal/PlatformDependent.java
@@ -128,6 +128,8 @@ public final class PlatformDependent {
     private static final String LINUX_ID_PREFIX = "ID=";
     private static final String LINUX_ID_LIKE_PREFIX = "ID_LIKE=";
     public static final boolean BIG_ENDIAN_NATIVE_ORDER = ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN;
+    private static final boolean IGNORE_EXPENSIVE_CLEAN =
+            SystemPropertyUtil.getBoolean("io.netty.ignoreExpensiveClean", false);
 
     private static final boolean JFR;
     private static final boolean VAR_HANDLE;
@@ -147,12 +149,27 @@ public final class PlatformDependent {
                 public void clean() {
                     // NOOP
                 }
+
+                @Override
+                public boolean hasMemoryAddress() {
+                    return hasDirectByteBufferAddress(byteBuffer);
+                }
+
+                @Override
+                public long memoryAddress() {
+                    return directBufferAddress(byteBuffer);
+                }
             };
         }
 
         @Override
         public void freeDirectBuffer(ByteBuffer buffer) {
             // NOOP
+        }
+
+        @Override
+        public boolean hasExpensiveClean() {
+            return false;
         }
     };
 
@@ -597,6 +614,22 @@ public final class PlatformDependent {
      * @return The {@link CleanableDirectBuffer} instance that contain the buffer and its deallocation mechanism.
      */
     public static CleanableDirectBuffer allocateDirect(int capacity) {
+        return allocateDirect(capacity, false);
+    }
+
+    /**
+     * Allocate a direct {@link ByteBuffer} of the given capacity, and return it alongside its deallocation mechanism.
+     * @param capacity The desired capacity of the direct byte buffer.
+     * @param permitExpensiveClean Whether to allow expensive clean operations or not. If expensive clean operations
+     * are not permitted ({@code false}), then the buffer cleaning may instead be delegated to the GC and reference
+     * processing. Pooling allocators would typically permit expensive clean operations, while unpooled buffers
+     * would not.
+     * @return The {@link CleanableDirectBuffer} instance that contain the buffer and its deallocation mechanism.
+     */
+    public static CleanableDirectBuffer allocateDirect(int capacity, boolean permitExpensiveClean) {
+        if (!IGNORE_EXPENSIVE_CLEAN && !permitExpensiveClean && CLEANER.hasExpensiveClean()) {
+            return NOOP.allocate(capacity);
+        }
         return CLEANER.allocate(capacity);
     }
 
@@ -613,6 +646,21 @@ public final class PlatformDependent {
         LEGACY_CLEANER.freeDirectBuffer(buffer);
     }
 
+    /**
+     * Check if it is possible to call {@link #directBufferAddress(ByteBuffer)} on the given buffer.
+     * @param buffer The specific buffer instance to check for.
+     * @return {@code true} if {@link #directBufferAddress(ByteBuffer)} can be called on the given buffer,
+     * otherwise {@code false}.
+     */
+    public static boolean hasDirectByteBufferAddress(ByteBuffer buffer) {
+        return PlatformDependent0.hasDirectByteBufferAdderss(buffer);
+    }
+
+    /**
+     * Obtain the native memory address of the given direct byte buffer, or throw an exception if it's not possible.
+     * @param buffer The buffer to get the native memory address for.
+     * @return The native memory address of the give buffer.
+     */
     public static long directBufferAddress(ByteBuffer buffer) {
         return PlatformDependent0.directBufferAddress(buffer);
     }

buffer/src/main/java/io/netty/buffer/AdaptiveByteBufAllocator.java
@@ -111,9 +111,7 @@ public final class AdaptiveByteBufAllocator extends AbstractByteBufAllocator
 
         @Override
         public AbstractByteBuf allocate(int initialCapacity, int maxCapacity) {
-            return PlatformDependent.hasUnsafe() ?
-                    UnsafeByteBufUtil.newUnsafeDirectByteBuf(allocator, initialCapacity, maxCapacity) :
-                    new UnpooledDirectByteBuf(allocator, initialCapacity, maxCapacity);
+            return UnsafeByteBufUtil.newDirectByteBuf(allocator, initialCapacity, maxCapacity);
         }
     }
 }

buffer/src/main/java/io/netty/buffer/PoolArena.java
@@ -764,7 +764,7 @@ abstract class PoolArena<T> implements PoolArenaMetric {
         }
 
         private static CleanableDirectBuffer allocateDirect(int capacity) {
-            return PlatformDependent.allocateDirect(capacity);
+            return PlatformDependent.allocateDirect(capacity, true);
         }
 
         @Override

buffer/src/main/java/io/netty/buffer/PooledByteBufAllocator.java
@@ -402,9 +402,7 @@ public class PooledByteBufAllocator extends AbstractByteBufAllocator implements 
         if (directArena != null) {
             buf = directArena.allocate(cache, initialCapacity, maxCapacity);
         } else {
-            buf = PlatformDependent.hasUnsafe() ?
-                    UnsafeByteBufUtil.newUnsafeDirectByteBuf(this, initialCapacity, maxCapacity) :
-                    new UnpooledDirectByteBuf(this, initialCapacity, maxCapacity);
+            buf = UnsafeByteBufUtil.newDirectByteBuf(this, initialCapacity, maxCapacity);
             onAllocateBuffer(buf, false, false);
         }
         return toLeakAwareBuffer(buf);

buffer/src/main/java/io/netty/buffer/UnpooledByteBufAllocator.java
@@ -189,6 +189,12 @@ public final class UnpooledByteBufAllocator extends AbstractByteBufAllocator imp
         }
 
         @Override
+        protected CleanableDirectBuffer allocateDirectBuffer(int capacity, boolean permitExpensiveClean) {
+            CleanableDirectBuffer buffer = super.allocateDirectBuffer(capacity, permitExpensiveClean);
+            return new DecrementingCleanableDirectBuffer(alloc(), buffer);
+        }
+
+        @Override
         CleanableDirectBuffer reallocateDirect(CleanableDirectBuffer oldBuffer, int initialCapacity) {
             int capacity = oldBuffer.buffer().capacity();
             CleanableDirectBuffer buffer = super.reallocateDirect(oldBuffer, initialCapacity);
@@ -205,6 +211,12 @@ public final class UnpooledByteBufAllocator extends AbstractByteBufAllocator imp
         @Override
         protected CleanableDirectBuffer allocateDirectBuffer(int capacity) {
             CleanableDirectBuffer buffer = super.allocateDirectBuffer(capacity);
+            return new DecrementingCleanableDirectBuffer(alloc(), buffer);
+        }
+
+        @Override
+        protected CleanableDirectBuffer allocateDirectBuffer(int capacity, boolean permitExpensiveClean) {
+            CleanableDirectBuffer buffer = super.allocateDirectBuffer(capacity, permitExpensiveClean);
             return new DecrementingCleanableDirectBuffer(alloc(), buffer);
         }
 
@@ -228,6 +240,12 @@ public final class UnpooledByteBufAllocator extends AbstractByteBufAllocator imp
         @Override
         protected CleanableDirectBuffer allocateDirectBuffer(int initialCapacity) {
             CleanableDirectBuffer buffer = super.allocateDirectBuffer(initialCapacity);
+            return new DecrementingCleanableDirectBuffer(alloc(), buffer);
+        }
+
+        @Override
+        protected CleanableDirectBuffer allocateDirectBuffer(int initialCapacity, boolean permitExpensiveClean) {
+            CleanableDirectBuffer buffer = super.allocateDirectBuffer(initialCapacity, permitExpensiveClean);
             return new DecrementingCleanableDirectBuffer(alloc(), buffer);
         }
 

buffer/src/main/java/io/netty/buffer/UnpooledDirectByteBuf.java
@@ -53,6 +53,10 @@ public class UnpooledDirectByteBuf extends AbstractReferenceCountedByteBuf {
      * @param maxCapacity     the maximum capacity of the underlying direct buffer
      */
     public UnpooledDirectByteBuf(ByteBufAllocator alloc, int initialCapacity, int maxCapacity) {
+        this(alloc, initialCapacity, maxCapacity, false);
+    }
+
+    UnpooledDirectByteBuf(ByteBufAllocator alloc, int initialCapacity, int maxCapacity, boolean permitExpensiveClean) {
         super(maxCapacity);
         ObjectUtil.checkNotNull(alloc, "alloc");
         checkPositiveOrZero(initialCapacity, "initialCapacity");
@@ -63,7 +67,7 @@ public class UnpooledDirectByteBuf extends AbstractReferenceCountedByteBuf {
         }
 
         this.alloc = alloc;
-        setByteBuffer(allocateDirectBuffer(initialCapacity), false);
+        setByteBuffer(allocateDirectBuffer(initialCapacity, permitExpensiveClean), false);
     }
 
     /**
@@ -118,7 +122,11 @@ public class UnpooledDirectByteBuf extends AbstractReferenceCountedByteBuf {
     }
 
     protected CleanableDirectBuffer allocateDirectBuffer(int capacity) {
-        return PlatformDependent.allocateDirect(capacity);
+        return PlatformDependent.allocateDirect(capacity, false);
+    }
+
+    CleanableDirectBuffer allocateDirectBuffer(int capacity, boolean permitExpensiveClean) {
+        return PlatformDependent.allocateDirect(capacity, permitExpensiveClean);
     }
 
     void setByteBuffer(CleanableDirectBuffer cleanableDirectBuffer, boolean tryFree) {

buffer/src/main/java/io/netty/buffer/UnpooledUnsafeDirectByteBuf.java
@@ -45,6 +45,11 @@ public class UnpooledUnsafeDirectByteBuf extends UnpooledDirectByteBuf {
         super(alloc, initialCapacity, maxCapacity);
     }
 
+    UnpooledUnsafeDirectByteBuf(ByteBufAllocator alloc, int initialCapacity, int maxCapacity,
+                                boolean permitExpensiveClean) {
+        super(alloc, initialCapacity, maxCapacity, permitExpensiveClean);
+    }
+
     /**
      * Creates a new direct buffer by wrapping the specified initial buffer.
      *

buffer/src/main/java/io/netty/buffer/UnpooledUnsafeNoCleanerDirectByteBuf.java
@@ -31,6 +31,11 @@ class UnpooledUnsafeNoCleanerDirectByteBuf extends UnpooledUnsafeDirectByteBuf {
     }
 
     @Override
+    protected CleanableDirectBuffer allocateDirectBuffer(int capacity, boolean ignorePermitExpensiveClean) {
+        return PlatformDependent.allocateDirectBufferNoCleaner(capacity);
+    }
+
+    @Override
     protected ByteBuffer allocateDirect(int initialCapacity) {
         throw new UnsupportedOperationException();
     }

commit 59699df04fd24ba2b74012f0949b7bbc1469c875

Log value of io.netty.ignoreExpensiveClean property during initialization
Motivation:

c79ce23a7b4e1be6985ad5e9127f0244f1052621 introduced a system property but we failed to log it's value during initialization.

Modifications:

Log property value in debug mode just as all the otehr properties

Result:

Log used value

common/src/main/java/io/netty/util/internal/PlatformDependent.java
@@ -244,6 +244,8 @@ public final class PlatformDependent {
             logger.debug("-Dio.netty.noPreferDirect: {}", EXPLICIT_NO_PREFER_DIRECT);
         }
 
+        logger.debug("-Dio.netty.ignoreExpensiveClean: {}", IGNORE_EXPENSIVE_CLEAN);
+
         /*
          * We do not want to log this message if unsafe is explicitly disabled. Do not remove the explicit no unsafe
          * guard.