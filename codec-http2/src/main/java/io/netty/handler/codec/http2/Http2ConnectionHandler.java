2af7f353467df68e00c59800835025dc897d7948
Norman Maurer <norman_maurer@apple.com>
2026-04-14 10:51:00 -0700

    Fix compile error

--- codec-http2/src/main/java/io/netty/handler/codec/http2/Http2ConnectionHandler.java
+++ codec-http2/src/main/java/io/netty/handler/codec/http2/Http2ConnectionHandler.java
@@ -479,24 +479,29 @@ public class Http2ConnectionHandler extends ByteToMessageDecoder implements Http
     }
 
     @Override
-    public void connect(ChannelHandlerContext ctx, SocketAddress remoteAddress, SocketAddress localAddress,
-                        ChannelPromise promise) throws Exception {
+    public void connect(final ChannelHandlerContext ctx, SocketAddress remoteAddress, SocketAddress localAddress,
+                        final ChannelPromise promise) throws Exception {
         // Ensure we send the preface before we notify the connect promise as the user might try to write
         // directly in the listener attached to the promise and we need to ensure the preface is always the first
         // thing that is written.
-        ctx.connect(remoteAddress, localAddress, ctx.newPromise()).addListener(f -> {
-            if (f.isSuccess()) {
-                try {
-                    byteDecoder.sendPrefaceIfNeeded(ctx);
-                } catch (Throwable e) {
-                    promise.setFailure(e);
-                    return;
+        ctx.connect(remoteAddress, localAddress, ctx.newPromise()).addListener(
+                new ChannelFutureListener() {
+                    @Override
+                    public void operationComplete(ChannelFuture f) {
+                        if (f.isSuccess()) {
+                            try {
+                                byteDecoder.sendPrefaceIfNeeded(ctx);
+                            } catch (Throwable e) {
+                                promise.setFailure(e);
+                                return;
+                            }
+                            promise.setSuccess();
+                        } else {
+                            promise.setFailure(f.cause());
+                        }
+                    }
                 }
-                promise.setSuccess();
-            } else {
-                promise.setFailure(f.cause());
-            }
-        });
+        );
     }
 
     @Override
