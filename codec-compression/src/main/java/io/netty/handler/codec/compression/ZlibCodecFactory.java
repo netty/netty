914dfb712ae066d37fb212d6c14251b088d95683
yawkat <jonas.konrad@oracle.com>
2026-07-28 08:27:26 +0000

    ZlibCodecFactory: Remove decompressor builder
    Co-Authored-By: multicode <multicode@yawk.at>

--- codec-compression/src/main/java/io/netty/handler/codec/compression/ZlibCodecFactory.java
+++ codec-compression/src/main/java/io/netty/handler/codec/compression/ZlibCodecFactory.java
@@ -15,10 +15,8 @@
  */
 package io.netty.handler.codec.compression;
 
-import io.netty.buffer.ByteBufAllocator;
 import io.netty.util.internal.PlatformDependent;
 import io.netty.util.internal.SystemPropertyUtil;
-import io.netty.util.internal.UnstableApi;
 import io.netty.util.internal.logging.InternalLogger;
 import io.netty.util.internal.logging.InternalLoggerFactory;
 
@@ -201,64 +199,7 @@ public final class ZlibCodecFactory {
         }
     }
 
-    /**
-     * Create a new decompressor builder that delegates to {@link JZlibDecompressor} or {@link JdkZlibDecompressor}.
-     *
-     * @return The decompressor builder
-     */
-    @UnstableApi
-    public static ZlibDecompressorBuilder decompressorBuilder() {
-        return new ZlibDecompressorBuilder();
-    }
-
     private ZlibCodecFactory() {
         // Unused
-    }
-
-    @UnstableApi
-    public static class ZlibDecompressorBuilder extends Decompressor.AbstractDecompressorBuilder {
-        private final ZlibDecompressor.AbstractZlibDecompressorBuilder delegate;
-
-        ZlibDecompressorBuilder() {
-            this.delegate = noJdkZlibDecoder ? new JZlibDecompressor.Builder() : new JdkZlibDecompressor.Builder();
-        }
-
-        /**
-         * Set the wrapper format for the deflated data. Defaults to {@link ZlibWrapper#ZLIB}.
-         *
-         * @param wrapper The wrapper format
-         * @return This builder
-         */
-        public ZlibDecompressorBuilder wrapper(ZlibWrapper wrapper) {
-            delegate.wrapper(wrapper);
-            return this;
-        }
-
-        /**
-         * Set the preset dictionary to use. Defaults to no dictionary.
-         *
-         * @param dictionary The dictionary
-         * @return This builder
-         */
-        public ZlibDecompressorBuilder dictionary(byte[] dictionary) {
-            delegate.dictionary(dictionary);
-            return this;
-        }
-
-        /**
-         * Set the maximum output buffer size. Defaults to 1M.
-         *
-         * @param maxAllocation The maximum output buffer size.
-         * @return This builder
-         */
-        public ZlibDecompressorBuilder maxAllocation(int maxAllocation) {
-            delegate.maxAllocation(maxAllocation);
-            return this;
-        }
-
-        @Override
-        public Decompressor build(ByteBufAllocator allocator) throws DecompressionException {
-            return delegate.build(allocator);
-        }
     }
 }
