3630659c817e848e4cc4f11703dc36adc80d537a
Norman Maurer <norman_maurer@apple.com>
2026-08-17 16:49:12 +0200

    STOMP codec content-length long-to-int truncation causes infinite decode loop DoS
    Motivation:
    
    We did not guard against a too large content-length and so could end up in an infinite loop
    
    Modifications:
    
    - Add guard
    - Add unit test
    
    Result:
    
    Correctly reject too large content-length

--- codec-stomp/src/main/java/io/netty/handler/codec/stomp/StompSubframeDecoder.java
+++ codec-stomp/src/main/java/io/netty/handler/codec/stomp/StompSubframeDecoder.java
@@ -235,6 +235,10 @@ public class StompSubframeDecoder extends ReplayingDecoder<State> {
         if (contentLength < 0) {
             throw new DecoderException(StompHeaders.CONTENT_LENGTH + " must be non-negative");
         }
+        if (contentLength > Integer.MAX_VALUE) {
+            throw new TooLongFrameException(StompHeaders.CONTENT_LENGTH + " exceeds the maximum allowed value: "
+                    + contentLength);
+        }
         return contentLength;
     }
 
