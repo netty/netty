0ba96ff8c36ce2bd62001384f32b46bca3139a0b
kjg <kimjg2477@gmail.com>
2026-09-08 18:52:36 +0900

    perf: Use LinkedHashSet to deduplicate search domains

--- resolver-dns/src/main/java/io/netty/resolver/dns/DnsNameResolverBuilder.java
+++ resolver-dns/src/main/java/io/netty/resolver/dns/DnsNameResolverBuilder.java
@@ -32,9 +32,8 @@ import io.netty.util.internal.logging.InternalLoggerFactory;
 
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedHashSet;
 
 import static io.netty.util.internal.ObjectUtil.checkNotNull;
 import static io.netty.util.internal.ObjectUtil.intValue;
@@ -557,7 +556,7 @@ public final class DnsNameResolverBuilder {
     public DnsNameResolverBuilder searchDomains(Iterable<String> searchDomains) {
         checkNotNull(searchDomains, "searchDomains");
 
-        final List<String> list = new ArrayList<String>(4);
+        final LinkedHashSet<String> domains = new LinkedHashSet<String>(4);
 
         for (String f : searchDomains) {
             if (f == null) {
@@ -565,14 +564,10 @@ public final class DnsNameResolverBuilder {
             }
 
             // Avoid duplicate entries.
-            if (list.contains(f)) {
-                continue;
-            }
-
-            list.add(f);
+            domains.add(f);
         }
 
-        this.searchDomains = list.toArray(EmptyArrays.EMPTY_STRINGS);
+        this.searchDomains = domains.toArray(EmptyArrays.EMPTY_STRINGS);
         return this;
     }
 
